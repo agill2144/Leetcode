a58fe67ba985694ee5383465758525cdd6caad72
agill2144 <120353980+agill2144@users.noreply.github.com>
2022-12-18 22:09:17 -0800

    Time: 470 ms (79.54%), Space: 92.9 MB (6.25%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.go
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.go
@@ -1,3 +1,31 @@
+// func validPath(n int, edges [][]int, source int, destination int) bool {
+//     adjMatrix := map[int][]int{}
+//     for i := 0; i < len(edges); i++ {
+//         adjMatrix[edges[i][0]] = append(adjMatrix[edges[i][0]], edges[i][1])
+//         adjMatrix[edges[i][1]] = append(adjMatrix[edges[i][1]], edges[i][0])
+//     }
+
+//     q := []int{source}
+//     visited := map[int]bool{source:true}
+    
+//     for len(q) != 0 {
+//         dq := q[0]
+//         q = q[1:]
+//         if dq == destination {return true}
+        
+//         childs := adjMatrix[dq]
+//         for _, child := range childs {
+//             _, ok := visited[child]
+//             if !ok {
+//                 visited[child] = true
+//                 q = append(q, child)
+//             }
+//         }
+
+//     }
+//     return false
+// }
+
 func validPath(n int, edges [][]int, source int, destination int) bool {
     adjMatrix := map[int][]int{}
     for i := 0; i < len(edges); i++ {
@@ -5,23 +33,26 @@ func validPath(n int, edges [][]int, source int, destination int) bool {
         adjMatrix[edges[i][1]] = append(adjMatrix[edges[i][1]], edges[i][0])
     }
 
-    q := []int{source}
-    visited := map[int]bool{source:true}
-    
-    for len(q) != 0 {
-        dq := q[0]
-        q = q[1:]
-        if dq == destination {return true}
+    visited := map[int]bool{}
+    var dfs func(n int) bool
+    dfs = func(n int) bool {
+        // base
+        _, ok := visited[n]
+        if ok {return false}
         
-        childs := adjMatrix[dq]
-        for _, child := range childs {
-            _, ok := visited[child]
-            if !ok {
-                visited[child] = true
-                q = append(q, child)
-            }
+        
+        // logic
+        
+        if n == destination {
+            return true
         }
-
+        visited[n] = true
+        childs := adjMatrix[n]
+        for _, child := range childs {
+            found := dfs(child)
+            if found {return true}
+        }
+        return false
     }
-    return false
+    return dfs(source)
 }
