2fa0bd972de2b945bbbbb709671cc9dc3e912a72
agill2144 <120353980+agill2144@users.noreply.github.com>
2022-12-18 18:07:40 -0800

    Time: 3 ms (24.81%), Space: 2.1 MB (65.41%) - LeetHub

--- 0394-decode-string/0394-decode-string.go
+++ 0394-decode-string/0394-decode-string.go
@@ -1,62 +1,27 @@
-// func decodeString(s string) string {
-//     numSt := []int{}
-//     strSt := []*strings.Builder{}
-//     curr := new(strings.Builder)
-//     num := 0
-    
-//     for i := 0; i < len(s); i++ {
-//         if s[i] >= '0' && s[i] <= '9' {
-//             num = num * 10 + int(s[i]-'0')
-//         } else if s[i] == '[' {
-//             strSt = append(strSt, curr)
-//             numSt = append(numSt, num)
-//             num = 0
-//             curr = new(strings.Builder)
-//         } else if s[i] == ']' {
-//             topNum := numSt[len(numSt)-1]; numSt = numSt[:len(numSt)-1]
-//             flattend := new(strings.Builder)
-//             for i := 0; i < topNum; i++ {
-//                 flattend.WriteString(curr.String())
-//             }
-//             curr = strSt[len(strSt)-1]; strSt = strSt[:len(strSt)-1]
-//             curr.WriteString(flattend.String())
-//         } else {
-//             curr.WriteByte(s[i])
-//         }
-//     }
-//     return curr.String()
-// }
-
-
 func decodeString(s string) string {
-    i := 0
-    var dfs func(str string) string
-    dfs = func(str string) string {
-        // base
-        
-        // logic
-        num := 0
-        curr := new(strings.Builder)
-        for i < len(str) {
-            if str[i] >= '0' && str[i] <= '9' {
-                num = num * 10 + int(str[i]-'0')
-                i++
-            } else if str[i] == '[' {
-                i++
-                decoded := dfs(str)
-                for k := 0; k < num; k++ {
-                    curr.WriteString(decoded)
-                }
-                num = 0
-            } else if str[i] == ']' {
-                i++
-                return curr.String()
-            } else {
-                curr.WriteByte(str[i])
-                i++
+    numSt := []int{}
+    strSt := []*strings.Builder{}
+    num := 0
+    curr := new(strings.Builder)
+    for i := 0; i < len(s); i++ {
+        if s[i] >= '0' && s[i] <= '9' {
+            num = num * 10 + int(s[i]-'0')
+        } else if s[i] == '[' {
+            numSt = append(numSt, num)
+            strSt = append(strSt, curr)
+            curr = new(strings.Builder)
+            num = 0
+        } else if s[i] == ']' {
+            k := numSt[len(numSt)-1]; numSt = numSt[:len(numSt)-1]
+            decoded := new(strings.Builder)
+            for j := 0; j < k; j++ {
+                decoded.WriteString(curr.String())
             }
+            curr = strSt[len(strSt)-1]; strSt = strSt[:len(strSt)-1]
+            curr.WriteString(decoded.String())
+        } else {
+            curr.WriteByte(s[i])
         }
-        return curr.String()
     }
-    return dfs(s)
+    return curr.String()
 }
