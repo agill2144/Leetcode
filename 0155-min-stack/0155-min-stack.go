19a38fd2db0f4506271ad7bed9c405dc86b76d14
agill2144 <120353980+agill2144@users.noreply.github.com>
2022-12-20 18:11:17 -0800

    Time: 39 ms (29.01%), Space: 8.9 MB (22.86%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.go
@@ -0,0 +1,49 @@
+type MinStack struct {
+    st [][]int // [val, minSoFar]
+}
+
+
+func Constructor() MinStack {
+    return MinStack{
+        st: [][]int{},
+    }
+}
+
+
+func (this *MinStack) Push(val int)  {
+    item := []int{val, val}
+    if len(this.st) == 0 {
+        this.st = append(this.st, item)
+        return
+    }
+    topMin := this.st[len(this.st)-1][1]
+    if topMin < val {
+        item[1] = topMin
+    }
+    this.st = append(this.st, item)
+}
+
+
+func (this *MinStack) Pop()  {
+    this.st = this.st[:len(this.st)-1]
+}
+
+
+func (this *MinStack) Top() int {
+    return this.st[len(this.st)-1][0]
+}
+
+
+func (this *MinStack) GetMin() int {
+    return this.st[len(this.st)-1][1]
+}
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(val);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
