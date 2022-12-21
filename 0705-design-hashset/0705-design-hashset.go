a01363bc56ab06152af4d1f5abbe9205c8296f7b
agill2144 <120353980+agill2144@users.noreply.github.com>
2022-12-20 18:53:05 -0800

    Time: 126 ms (70.45%), Space: 10.1 MB (15.15%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.go
+++ 0705-design-hashset/0705-design-hashset.go
@@ -1,62 +1,61 @@
+package main
+
 const outterArraySize = 1000
 const innerArraySize = 1001
 
 /*
-    TODO:
-    1. Why is inner array size bigger by just 1 ?
-    2. Why is our double hashing func using / operator instead of % operator ( like the 1st hashing func )
+   TODO:
+   1. Why is inner array size bigger by just 1 ?
+   2. Why is our double hashing func using / operator instead of % operator ( like the 1st hashing func )
+   - This is because using the same hash func in the nested array will result into a collision in the nested array
+   - so something SHOULD BE DIFFERENT than the outter array
 */
 
-func hashIdx(key int) int {  return key % outterArraySize }
-func hashIdx2(key int) int { return key / innerArraySize  }
-
+// time: o(1)
+// space: o(1)
+func hashIdx(key int) int  { return key % outterArraySize }
+func hashIdx2(key int) int { return key / innerArraySize }
 
 type MyHashSet struct {
-    items [][]bool
+	items [][]bool
 }
 
 func Constructor() MyHashSet {
-    items := make([][]bool, 1000)
-    return MyHashSet{
-        items: items,
-    }    
+	items := make([][]bool, 1000)
+	return MyHashSet{
+		items: items,
+	}
 }
 
-
-func (this *MyHashSet) Add(key int)  {
-    idx := hashIdx(key)
-    idx2 := hashIdx2(key)
-    if this.items[idx] == nil {
-        this.items[idx] = make([]bool, innerArraySize)
-    }
-    this.items[idx][idx2] = true
+// time: o(1)
+// space: o(1)
+func (this *MyHashSet) Add(key int) {
+	idx := hashIdx(key)
+	idx2 := hashIdx2(key)
+	if this.items[idx] == nil {
+		this.items[idx] = make([]bool, innerArraySize)
+	}
+	this.items[idx][idx2] = true
 }
 
-
-func (this *MyHashSet) Remove(key int)  {
-    idx := hashIdx(key)
-    idx2 := hashIdx2(key)
-    if this.items[idx] == nil {
-        return
-    }
-    this.items[idx][idx2] = false
+// time: o(1)
+// space: o(1)
+func (this *MyHashSet) Remove(key int) {
+	idx := hashIdx(key)
+	idx2 := hashIdx2(key)
+	if this.items[idx] == nil {
+		return
+	}
+	this.items[idx][idx2] = false
 }
 
-
+// time: o(1)
+// space: o(1)
 func (this *MyHashSet) Contains(key int) bool {
-    idx := hashIdx(key)
-    idx2 := hashIdx2(key)
-    if this.items[idx] == nil {
-        return false
-    }
-    return this.items[idx][idx2]
+	idx := hashIdx(key)
+	idx2 := hashIdx2(key)
+	if this.items[idx] == nil {
+		return false
+	}
+	return this.items[idx][idx2]
 }
-
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Add(key);
- * obj.Remove(key);
- * param_3 := obj.Contains(key);
- */
