a16176de7369d3035b9ff380b0c19663120bc085
agill2144 <120353980+agill2144@users.noreply.github.com>
2022-12-22 13:12:06 -0800

    Time: 16 ms (94.07%), Space: 6 MB (83.05%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.go
+++ 0605-can-place-flowers/0605-can-place-flowers.go
@@ -1,17 +1,17 @@
 func canPlaceFlowers(flowerbed []int, n int) bool {
-    if n <= 0 {return true}
-    if len(flowerbed) == 1 && flowerbed[0] != 1 && n == 1 {return true}
-
+    if n == 0 {return true}
+    if flowerbed == nil || len(flowerbed) == 0 {return false}
+    
     for i := 0; i < len(flowerbed); i++ {
         if n == 0 {return true}
-        curr := flowerbed[i]        
-        prevIsEmpty := (i == 0) || (flowerbed[i-1] == 0)
-        nextIsEmpty := (i == len(flowerbed)-1) || (flowerbed[i+1] == 0)
-        if curr != 1 && prevIsEmpty && nextIsEmpty {
+        prevEmpty := (i == 0)|| (flowerbed[i-1] == 0)
+        nextEmpty := (i == len(flowerbed)-1) || (flowerbed[i+1] == 0)
+        currEmpty := flowerbed[i] == 0
+        
+        if currEmpty && prevEmpty && nextEmpty {
             flowerbed[i] = 1
             n--
         }
     }
-    return n==0
+    return n == 0
 }
-
