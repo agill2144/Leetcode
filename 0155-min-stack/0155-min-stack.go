8a4f2fe8b1be1a2cf9a70b431636abe0a0e9b5a3
agill2144 <120353980+agill2144@users.noreply.github.com>
2022-12-21 19:44:46 -0800

    Time: 14 ms (96.80%), Space: 8.2 MB (74.84%) - LeetHub

--- 0155-min-stack/0155-min-stack.go
+++ 0155-min-stack/0155-min-stack.go
@@ -1,3 +1,39 @@
+/*
+    approach: brute force
+    - getMin() linear scan of stack to find the smallest element
+    
+    time:
+    push() : o(1)
+    pop() : o(1)
+    getMin() : o(n)
+    
+
+    Can we maintian a min in a variable?
+    - But then we lose previous min if a latest min gets popped
+    - so then maintain another stack that keeps track of all mins
+    - when a pop is removing the min element, then we will also remove the top of min stack
+    - but what about dupes? if an element is being pushed again and its also the top of the min stack
+    - then we will push the same dupe again! so that we can keep same number of mins that are also in the regular stack
+    
+    approach: 2 stacks
+    - ceate 2 stack ( regular and min stack )
+    - push(x) will push to regular first
+        - if the min stack is empty, push there and exit
+        - if x is smaller than top of min OR if its the same as top of min ( x <= topOfMinSt )
+            - then push this to min stack
+    - pop() 
+        - pop from top of regular stack
+        - if the number being popped is the same as top of the min stack, pop from min stack too
+    
+    - getMin()
+        - return the top of min stack
+    time:
+    push() : o(1)
+    pop() : o(1)
+    getMin() : o(n)
+    
+    space: o(2n) since we used 2 stacks
+*/
 type MinStack struct {
     st []int
     min []int
