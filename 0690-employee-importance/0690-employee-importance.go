3e9824a3d61a465bbb554991db94c090aefd832b
agill2144 <120353980+agill2144@users.noreply.github.com>
2022-12-24 13:07:59 -0800

    Time: 16 ms (80.00%), Space: 6.8 MB (80.00%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.go
+++ 0690-employee-importance/0690-employee-importance.go
@@ -9,17 +9,17 @@
 
 func getImportance(employees []*Employee, id int) int {
     if employees == nil || len(employees) == 0 {return 0}
-    
+
     empRef := map[int]*Employee{}
+    q := []*Employee{}
     for i := 0; i < len(employees); i++ {
         emp := employees[i]
         empRef[emp.Id] = emp
+        if emp.Id == id {q = append(q, emp)}
     }
-    
-    if _, ok := empRef[id]; !ok {return 0}
+    if len(q) == 0 {return 0}
     
     sum := 0
-    q := []*Employee{empRef[id]}
     for len(q) != 0 {
         dq := q[0]
         q = q[1:]
