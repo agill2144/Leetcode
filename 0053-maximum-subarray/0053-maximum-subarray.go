383224e74bca4330fd73ba6a4e2b5dca3d666810
agill2144 <120353980+agill2144@users.noreply.github.com>
2022-12-27 22:07:41 -0800

    Time: 99 ms (88.27%), Space: 8.4 MB (99.59%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.go
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -1,3 +1,53 @@
+
+/*
+    approach: brute force
+    - nested loop
+    - keep track of running sum
+    - save running into max whenever needed
+    
+    time: o(n^2)
+    space: o(1)
+    
+
+func maxSubArray(nums []int) int {
+    max := math.MinInt64
+    for i := 0; i < len(nums); i++ {
+        running := 0
+        for j := i; j < len(nums); j++ {
+            running += nums[j]
+            if running > max {
+                max = running
+            }
+        }
+    }
+    return max
+}
+*/
+
+
+/*
+    approach: linear running sum
+    - we do what we used to with runningSum pattern
+    - but instead of continuing adding nums as we loop
+    - we will still add current num to runningSum
+    - but since the inputs can be negative integers
+    - the current number on its own may be bigger than currentRunningSum
+        - this is when we will reset our window and our window starts from this number onwards ( since current number is > than the prev runningSum )
+    - so, in that case , if current number > runningSum = then runningSum becomes currentNumber
+    - Save the max between max,runningSum
+    - So the idea becomes instead of blindly adding to runningSum , we will pick and choose whether
+        - adding current number to runningSum is better or
+        - currentNumber is better than the above addition result
+    - Then we will save the max between previous max and new updated runningSum in each iteration
+    
+    - TLDR: 
+        - runningSum pattern
+        - runningSum = max(runningSum, runningSum+nums[i])
+        - max = max(max, runningSum)
+    
+    time: o(n)
+    space: o(1)
+*/
 func maxSubArray(nums []int) int {
     if nums == nil || len(nums) == 0 {return 0}
     curr := nums[0]
