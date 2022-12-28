93286f5d30b09e16f9f17ab38ff4789aa89db109
agill2144 <120353980+agill2144@users.noreply.github.com>
2022-12-27 21:48:30 -0800

    Time: 78 ms (100.00%), Space: 7.3 MB (64.29%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.go
@@ -0,0 +1,41 @@
+/*
+        Subarray == sliding window  ||  prefix/running sum/prod
+        Check if the number have negative
+        if they dont , sliding window could work. 
+     
+        In this case we wont have negatives
+        1 <= nums[i] <= 1000
+        
+        What type of sliding window?
+        - Dynamic
+        
+        What defines keep increasing the window size?
+        - If the window/running prod < k
+        
+        When do we start shriking?
+        - if the window prod >= k ( while windowProd >= k, slide the window forward )
+        
+        When we slide the window forward, an element leaves our subarray, what state needs to be updated?
+        - Since we maintain running prod, the num going out of the window will need to be divided from running prod
+        
+        What are we looking to return once we do have a window of running prod < k ?
+        - count number of subarrays 
+*/
+func numSubarrayProductLessThanK(nums []int, k int) int {
+    if nums == nil || len(nums) == 0 || k <= 1 {
+        return 0
+    }
+
+    prod := 1
+    left := 0
+    count := 0
+    for right := 0; right < len(nums); right++ {
+        prod *= nums[right]
+        for prod >= k {
+            prod /= nums[left]
+            left++
+        }
+        count += right-left+1
+    }
+    return count
+}
