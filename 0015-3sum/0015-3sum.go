28e5974775e5725144ece7a4e6ff0ef7d310655b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-04 22:07:58 -0800

    Time: 989 ms (10.27%), Space: 11.1 MB (5.27%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -9,18 +9,19 @@ func threeSum(nums []int) [][]int {
     set := map[int]struct{}{}
     outSeen := map[string]struct{}{}
     
-    for i := 0; i < len(nums); i++ {
+    for i := 0; i < len(nums); i++ { // o(n)
         outterNum := nums[i]
         if _, ok := set[outterNum]; ok {continue}
         set[outterNum] = struct{}{}
         target := 0-outterNum
         
         seen := map[int]struct{}{}
-        for j := i+1; j < len(nums); j++ {
+        for j := i+1; j < len(nums); j++ { // o(n)
             compl := target-nums[j]  
             if _, ok := seen[compl]; ok {
                 triplet := []int{outterNum, nums[j], compl}
-                sort.Ints(triplet)
+                sort.Ints(triplet) // (o3log3)
+                // gross :/ 
                 if _, ok := outSeen[fmt.Sprintf("%v", triplet)]; ok {continue}
                 outSeen[fmt.Sprintf("%v", triplet)] = struct{}{}
                 out = append(out, triplet)
@@ -28,6 +29,8 @@ func threeSum(nums []int) [][]int {
             seen[nums[j]] = struct{}{}
         }
     } 
+    // total time = o(n) x o(n) x o(3log3) = o(n^2)
+    // space = o(n) + o(n) + o(n) ; 3 sets
     return out
 }
 
