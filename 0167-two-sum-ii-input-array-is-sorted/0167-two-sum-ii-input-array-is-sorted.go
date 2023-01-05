9297d6ae951cd5d31fd194f46baa978729ae8542
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-04 21:29:35 -0800

    Time: 21 ms (55.82%), Space: 5.5 MB (43.13%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
@@ -1,14 +1,37 @@
-func twoSum(nums []int, target int) []int {
-    left := 0
-    right := len(nums)-1
+// two ptrs
+// time: o(n)
+// space: o(1)
+// func twoSum(nums []int, target int) []int {
+//     left := 0
+//     right := len(nums)-1
     
-    for left < right {
-        sum := nums[left] + nums[right]
-        if sum == target {return []int{left+1, right+1}}
-        if sum > target {
-            right--
-        } else {
-            left++
+//     for left < right {
+//         sum := nums[left] + nums[right]
+//         if sum == target {return []int{left+1, right+1}}
+//         if sum > target {
+//             right--
+//         } else {
+//             left++
+//         }
+//     }
+//     return nil
+// }
+
+func twoSum(nums []int, target int) []int{
+    for i := 0; i < len(nums); i++ {
+        num := nums[i]
+        comp := target-num
+        left := i+1
+        right := len(nums)-1
+        for left <= right {
+            mid := left + (right-left)/2
+            if nums[mid] == comp {
+                return []int{i+1, mid+1}
+            } else if nums[mid] > comp {
+                right = mid-1
+            } else {
+                left = mid+1
+            }
         }
     }
     return nil
