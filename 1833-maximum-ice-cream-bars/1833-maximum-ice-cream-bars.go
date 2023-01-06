19eddfdd557edaecb3676cbf994807c9f5870dd7
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-06 11:43:28 -0800

    Time: 193 ms (76.47%), Space: 9.5 MB (35.29%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.go
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.go
@@ -2,14 +2,10 @@ func maxIceCream(costs []int, coins int) int {
     sort.Ints(costs)
     i := 0
     count := 0
-    for coins > 0 && i < len(costs) {
-        if costs[i] <= coins {
-            coins -= costs[i]
-            count++
-            i++
-        } else {
-            break
-        }
+    for coins > 0 && i < len(costs) && costs[i] <= coins {
+        coins -= costs[i]
+        count++
+        i++
     }
     return count
 }
