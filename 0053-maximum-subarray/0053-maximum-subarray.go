52fa54e9281b72d0d09132a1276bee49c35a7c0e
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-09 22:10:53 -0800

    Time: 94 ms (94.84%), Space: 10 MB (13.60%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.go
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -2,12 +2,13 @@ func maxSubArray(nums []int) int {
     if nums == nil || len(nums) == 0 {return -1}
     if len(nums) == 1 {return nums[0]}
     
-    curr := nums[0]
-    out := curr
+    dp := make([]int, len(nums))
+    dp[0] = nums[0]
+    out := dp[0]
 
     for i := 1; i < len(nums); i++ {
-        curr = max(nums[i], nums[i]+curr)
-        out = max(out, curr)
+        dp[i] = max(nums[i], nums[i]+dp[i-1])
+        out = max(out, dp[i])
     }
     return out
 }
@@ -16,3 +17,30 @@ func max(x, y int) int {
     if x > y {return x}
     return y
 }
+
+
+
+/*
+    approach: brute force
+    - nested loop
+    - keep track of running sum
+    - save running into max whenever needed
+    
+    time: o(n^2)
+    space: o(1)
+    
+
+func maxSubArray(nums []int) int {
+    max := math.MinInt64
+    for i := 0; i < len(nums); i++ {
+        running := 0
+        for j := i; j < len(nums); j++ {
+            running += nums[j]
+            if running > max {
+                max = running
+            }
+        }
+    }
+    return max
+}
+*/
