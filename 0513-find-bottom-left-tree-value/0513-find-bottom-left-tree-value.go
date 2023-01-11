d4bd7528ebf95c986618c9c75002274d57e4abc7
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-10 22:31:44 -0800

    Time: 4 ms (89.13%), Space: 5.2 MB (65.22%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findBottomLeftValue(root *TreeNode) int {
+    maxLevel := 0
+    out := -1
+    var dfs func(r *TreeNode, level int)
+    dfs = func(r *TreeNode, level int) {
+        // base
+        if r == nil {return}
+
+        // logic
+        dfs(r.Right, level+1)
+        dfs(r.Left, level+1)
+        if r.Left == nil && r.Right == nil {
+            if level >= maxLevel {
+                maxLevel = level
+                out = r.Val
+            }
+        }
+    }
+    dfs(root, 0)
+    return out
+}
