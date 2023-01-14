bd8a28d6497d227960c799084f046a410bc13ea2
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-14 13:41:54 -0800

    Time: 299 ms (100.00%), Space: 31.4 MB (40.00%) - LeetHub

--- 1586-binary-search-tree-iterator-ii/1586-binary-search-tree-iterator-ii.go
+++ 1586-binary-search-tree-iterator-ii/1586-binary-search-tree-iterator-ii.go
@@ -6,63 +6,62 @@
  *     Right *TreeNode
  * }
  */
-
-type treeNodeWrapper struct {
-    node *TreeNode
-    alreadyProcessed bool
-}
 type BSTIterator struct {
-    st []treeNodeWrapper
-    prevSt []treeNodeWrapper
+    st *stackNode
+    prev *stackNode
+    visited map[*TreeNode]struct{}
 }
 
 
 func Constructor(root *TreeNode) BSTIterator {
-    b := BSTIterator{st: []treeNodeWrapper{}, prevSt: []treeNodeWrapper{}}
-    for root != nil {
-        b.st = append(b.st, treeNodeWrapper{node: root, alreadyProcessed: false})
-        root = root.Left
-    }
+    st := &stackNode{treeNode: &TreeNode{Val: math.MinInt64}}
+    prev := &stackNode{treeNode: &TreeNode{Val: math.MinInt64}}
+    b := BSTIterator{visited: map[*TreeNode]struct{}{}, st: st, prev:prev}
+    b.inorderLeft(root)
     return b
 }
 
 
 func (this *BSTIterator) HasNext() bool {
-    return len(this.st) > 0
+    dummyHead := this.st
+    actualHead := dummyHead.Next
+    return actualHead != nil
 }
 
 
 func (this *BSTIterator) Next() int {
-    top := this.st[len(this.st)-1]
-    this.st = this.st[:len(this.st)-1]
-    if !top.alreadyProcessed {
-        this.inorderLeft(top.node.Right)
-        top.alreadyProcessed = true
+    top, newHead := pop(this.st)
+    this.st = newHead
+    treeNode := top.treeNode
+    _, ok := this.visited[treeNode]
+    if !ok {
+        this.inorderLeft(treeNode.Right)
+        this.visited[treeNode] = struct{}{}
     }
-    this.prevSt = append(this.prevSt, top)
-    return top.node.Val
+    this.prev = add(this.prev, treeNode)
+    return treeNode.Val
 }
 
 func (this *BSTIterator) inorderLeft(root *TreeNode) {
     for root != nil {
-        this.st = append(this.st, treeNodeWrapper{node: root})
+        newHead := add(this.st, root)
+        this.st = newHead
         root = root.Left
     }
 }
-
-
 func (this *BSTIterator) HasPrev() bool {
-    return len(this.prevSt) > 1
+    dummyHead := this.prev
+    actualHead := dummyHead.Next
+    return actualHead != nil && actualHead.Next != nil
 }
 
 
 func (this *BSTIterator) Prev() int {
-    top := this.prevSt[len(this.prevSt)-1]; 
-    this.prevSt = this.prevSt[:len(this.prevSt)-1]
-    this.st = append(this.st, top)
-    return this.prevSt[len(this.prevSt)-1].node.Val
+    top, newPrevHead := pop(this.prev)
+    this.prev = newPrevHead
+    this.st = add(this.st, top.treeNode)
+    return this.prev.treeNode.Val
 }
-
 
 
 /**
@@ -73,3 +72,31 @@ func (this *BSTIterator) Prev() int {
  * param_3 := obj.HasPrev();
  * param_4 := obj.Prev();
  */
+
+type stackNode struct {
+    treeNode *TreeNode
+    Next *stackNode
+}
+
+func add(head *stackNode, x *TreeNode) *stackNode {
+    if head == nil {
+        return &stackNode{
+            treeNode: x,
+        }
+    }
+    newHead := &stackNode{treeNode: x}
+    newHead.Next = head
+    head = newHead
+    return head
+} 
+
+func pop(head *stackNode) (*stackNode, *stackNode) {
+    if head == nil || head.Next == nil {
+        return head, nil
+    }
+    out := head
+    newHead := head.Next
+    head.Next = nil
+    head = newHead
+    return out, head
+} 
