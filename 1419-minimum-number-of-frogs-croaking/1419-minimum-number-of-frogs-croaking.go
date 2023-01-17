898b178d2c5339f1a158d941d2f092c429fb2348
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-16 22:40:09 -0800

    Time: 9 ms (93.75%), Space: 4.1 MB (93.75%) - LeetHub

---
+++ 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.go
@@ -0,0 +1,35 @@
+func minNumberOfFrogs(croakOfFrogs string) int {
+    c := 0
+    r := 0
+    o := 0
+    a := 0
+    k := 0
+    activeFrogs := 0
+    ans := -1
+    
+    for i := 0; i < len(croakOfFrogs); i++ {
+        char := croakOfFrogs[i]
+        if char == 'c' {
+            c++
+            activeFrogs++
+        } else if char == 'r' {
+            r++
+        } else if char == 'o' {
+            o++
+        } else if char == 'a' {
+            a++
+        } else if char == 'k' {
+            k++
+            activeFrogs--
+        }
+        if r > c || o > r || a > o || k > a {return -1}
+        ans = max(ans, activeFrogs)
+    }
+    if activeFrogs != 0 {return -1}
+    return ans
+}
+
+func max(x, y int) int {
+    if x > y {return x}
+    return y
+}
