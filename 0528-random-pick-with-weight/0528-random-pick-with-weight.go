7c0d3c049af6d9182c4c1356d4f27780edda916a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-17 12:42:54 -0800

    Time: 743 ms (9.09%), Space: 9.2 MB (13.64%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.go
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.go
@@ -1,44 +1,50 @@
-
 type Solution struct {
-    prefixSum []int
+    sums []int
 }
 
 
 func Constructor(w []int) Solution {
-    prefixSum := make([]int, len(w))
+    total := 0
+    rSums := []int{}
     for i := 0; i < len(w); i++ {
-        if i == 0 {
-            prefixSum[i] = w[i]
-        } else {
-            prefixSum[i] = prefixSum[i-1] + w[i]
-        }
+        total += w[i]
+        rSums = append(rSums, total)
     }
-    for i := 0; i < len(w); i++ {
-        fmt.Println(prefixSum[i])
+    return Solution{
+        sums: rSums,
     }
-    return Solution{prefixSum}
 }
 
 
 func (this *Solution) PickIndex() int {
-    n := len(this.prefixSum)
-    if n == 0 {
-        return -1
-    }
-    total := this.prefixSum[n-1]
-    randNum := rand.Intn(total)
-    return this.BinarySearch(randNum)
-}
+    n := len(this.sums)
+    if n == 0 {return -1}
+    
+    rand.Seed(time.Now().UnixNano())
+    min := 1
+    max := this.sums[n-1]
+    target := rand.Intn(max - min + 1) + min
 
-func (this *Solution) BinarySearch(target int) int {
-    left, right := 0, len(this.prefixSum)-1
-    for left < right {
-        mid := left + (right - left) / 2
-        if this.prefixSum[mid] > target {
-            right = mid
-        } else {
+    left := 0
+    right := n-1
+    
+    for left <= right {
+        mid := left + (right-left)/2
+        
+        if this.sums[mid] == target || (target < this.sums[mid] && (mid == 0 || target > this.sums[mid-1])) {
+            return mid
+        } else if target > this.sums[mid] {
             left = mid+1
+        } else {
+            right = mid-1
         }
     }
-    return left
+    return -1
 }
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(w);
+ * param_1 := obj.PickIndex();
+ */
