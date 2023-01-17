0e87040c4761fde7954e5f91244ba0d424cb7f3e
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-16 18:14:05 -0800

    Time: 3 ms (53.98%), Space: 2.2 MB (95.50%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rightSideView(root *TreeNode) []int {
+    if root == nil {return nil}
+    out := []int{}
+    q := []*TreeNode{root}
+    for len(q) != 0 {
+        qSize := len(q)
+        for qSize != 0 {
+            dq := q[0]
+            q = q[1:]
+            if qSize == 1 {
+                out = append(out, dq.Val)
+            }
+            if dq.Left != nil {
+                q = append(q, dq.Left)
+            }
+            if dq.Right != nil {
+                q = append(q, dq.Right)
+            }
+            qSize--
+        }
+    }
+    return out
+}
