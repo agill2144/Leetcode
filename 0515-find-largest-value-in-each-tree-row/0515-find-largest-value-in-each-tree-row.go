c50697395b66a4eef7a67985d2bb29fde2fc9c0d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-24 20:06:23 -0500

    Time: 9 ms (63.33%), Space: 5.6 MB (80.00%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.go
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.go
@@ -6,26 +6,50 @@
  *     Right *TreeNode
  * }
  */
-func largestValues(root *TreeNode) []int {
+// func largestValues(root *TreeNode) []int {
+//     if root == nil {return nil}
+//     out := []int{}
+//     q := []*TreeNode{root}
+//     for len(q) != 0 {
+//         levelMax := math.MinInt64
+//         qSize := len(q)
+//         for qSize != 0 {
+//             dq := q[0]
+//             q = q[1:]
+//             if dq.Val > levelMax {levelMax = dq.Val}
+//             if dq.Left != nil {
+//                 q = append(q, dq.Left)
+//             }
+//             if dq.Right != nil {
+//                 q = append(q, dq.Right)
+//             }
+//             qSize--
+//         }
+//         out = append(out, levelMax)
+//     }
+//     return out
+// }
+
+func largestValues(root *TreeNode) []int{
     if root == nil {return nil}
     out := []int{}
-    q := []*TreeNode{root}
-    for len(q) != 0 {
-        levelMax := math.MinInt64
-        qSize := len(q)
-        for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            if dq.Val > levelMax {levelMax = dq.Val}
-            if dq.Left != nil {
-                q = append(q, dq.Left)
-            }
-            if dq.Right != nil {
-                q = append(q, dq.Right)
-            }
-            qSize--
+    
+    var dfs func(r *TreeNode, level int)
+    dfs = func(r *TreeNode, level int) {
+        // base
+        if r == nil {return}
+        
+        // logic
+        if len(out) == level {
+            out = append(out, r.Val)
+        } else if r.Val > out[level] {
+            out[level] = r.Val
         }
-        out = append(out, levelMax)
+        dfs(r.Left, level+1)
+        dfs(r.Right, level+1)
+        
     }
+    dfs(root,0)
+    
     return out
 }
