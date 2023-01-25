585e105c8e7142f72d87d1155b959856729d9f2e
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-24 19:53:11 -0500

    Time: 4 ms (61.73%), Space: 3 MB (50.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.go
+++ 0036-valid-sudoku/0036-valid-sudoku.go
@@ -1,21 +1,27 @@
+// time: o(1) - the matrix is given to be 9x9 in all test cases
+// space: o(1) -  the matrix is given to be 9x9 in all test cases
+// because worst case board is all filled out and our box map has 9^2 elements... 
+// so its constant and never grows more than o(numOfElementsInRowSet) + o(numOfElementsInColSet) + o(9^2)
+// space: o(9)+o(9)+o(81) -- so o(1) - these are constant numbers IMO
+
 func isValidSudoku(board [][]byte) bool {
     m := len(board)
     n := len(board[0])
-    boxMap := map[string]*set{}
+    boxMap := map[int]*set{}
     
     for i := 0; i < m; i++ {
         rowSet := newSet()
         colSet := newSet()
-
         for j := 0; j < n; j++ {
-            
             if board[i][j] != '.' {
                 if rowSet.contains(board[i][j]) {
                     return false
                 }
                 rowSet.add(board[i][j])
                 
-                boxKey := fmt.Sprintf("%v:%v", i/3, j/3)
+                boxKey := 0
+                boxKey = boxKey * 10 + (i/3)
+                boxKey = boxKey * 10 + (j/3)                
                 boxSet , ok := boxMap[boxKey]
                 if !ok {
                     boxSet = newSet()
