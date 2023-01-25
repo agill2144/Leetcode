e10f5cab1df5648110ace6574713d59c23261ea8
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-24 21:25:47 -0500

    Time: 157 ms (13.27%), Space: 8.6 MB (11.50%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.go
+++ 0463-island-perimeter/0463-island-perimeter.go
@@ -1,22 +1,20 @@
 func islandPerimeter(grid [][]int) int {
-    out := 0
     q := [][]int{}
+    dirs := [][]int{{1,0},{-1,0},{0,-1}, {0,1}}
     m := len(grid)
     n := len(grid[0])
-    dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
+    out := 0
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
             if grid[i][j] == 1 {
                 q = append(q, []int{i,j})
                 grid[i][j] = -1
-                
                 for len(q) != 0 {
                     dq := q[0]
                     q = q[1:]
+                    out += 4
                     cr := dq[0]
                     cc := dq[1]
-                    out += 4
-                    
                     for _, dir := range dirs {
                         r := cr + dir[0]
                         c := cc + dir[1]
@@ -29,6 +27,7 @@ func islandPerimeter(grid [][]int) int {
                         }
                     }
                 }
+                
             }
         }
     }
