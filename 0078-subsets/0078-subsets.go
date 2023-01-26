685dffc76659738ec1938eae7ce5780848827c94
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-25 20:48:56 -0500

    Time: 2 ms (53.07%), Space: 2.2 MB (49.76%) - LeetHub

--- 0078-subsets/0078-subsets.go
+++ 0078-subsets/0078-subsets.go
@@ -1,20 +1,40 @@
-func subsets(nums []int) [][]int {
-    out := [][]int{}
-    var dfs func(start int, path []int)
-    dfs = func(start int, path []int) {
-        // base
+/*
+    approach: backtracking and for-loop recursion
+    time: o(2^n) x o(n)
+        - the extra o(n) is from creating a deep copy of path in each recursion
+    space: o(n) from recursion stack + o(n) for each path at each recursion
+*/
+// func subsets(nums []int) [][]int {
+//     out := [][]int{}
+//     var dfs func(start int, path []int)
+//     dfs = func(start int, path []int) {
+//         // base
             
-        newL := make([]int, len(path))
-        copy(newL, path)
-        out = append(out, newL)
+//         newL := make([]int, len(path))
+//         copy(newL, path)
+//         out = append(out, newL)
         
-        // logic
-        for i := start; i < len(nums); i++ {
-            path = append(path, nums[i])
-            dfs(i+1,path)
-            path = path[:len(path)-1]
+//         // logic
+//         for i := start; i < len(nums); i++ {
+//             path = append(path, nums[i])
+//             dfs(i+1,path)
+//             path = path[:len(path)-1]
+//         }
+//     }
+//     dfs(0, nil)
+//     return out
+// }
+
+
+func subsets(nums []int) [][]int {
+    out := [][]int{{}}
+    for i := 0; i < len(nums); i++ {
+        for _, list := range out {
+            newL := make([]int, len(list))
+            copy(newL, list)
+            newL = append(newL, nums[i])
+            out = append(out, newL)
         }
     }
-    dfs(0, nil)
     return out
 }
