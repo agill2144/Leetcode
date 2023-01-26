1ae4588eebd67b9fe853e2691f1d973cea74f3a2
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-25 20:44:47 -0500

    Time: 0 ms (100.00%), Space: 2.2 MB (98.11%) - LeetHub

--- 0078-subsets/0078-subsets.go
+++ 0078-subsets/0078-subsets.go
@@ -1,13 +1,20 @@
 func subsets(nums []int) [][]int {
-    out := [][]int{{}}
-    for i := 0; i < len(nums); i++ {
-        num := nums[i]
-        for _, nestedList := range out {
-            newL := make([]int, len(nestedList))
-            copy(newL, nestedList)
-            newL = append(newL, num)
-            out = append(out, newL)
+    out := [][]int{}
+    var dfs func(start int, path []int)
+    dfs = func(start int, path []int) {
+        // base
+            
+        newL := make([]int, len(path))
+        copy(newL, path)
+        out = append(out, newL)
+        
+        // logic
+        for i := start; i < len(nums); i++ {
+            path = append(path, nums[i])
+            dfs(i+1,path)
+            path = path[:len(path)-1]
         }
     }
+    dfs(0, nil)
     return out
 }
