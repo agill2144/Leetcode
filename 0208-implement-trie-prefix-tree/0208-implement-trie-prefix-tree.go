879396bb5968b2a1a5e55529473cafc9edf9ea74
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-27 11:47:12 -0500

    Time: 57 ms (78.13%), Space: 17.6 MB (41.56%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.go
@@ -0,0 +1,58 @@
+type trieNode struct {
+    isEnd bool
+    childs [26]*trieNode
+}
+type Trie struct {
+    root *trieNode
+}
+
+
+func Constructor() Trie {
+    root := new(trieNode)
+    root.childs = [26]*trieNode{}
+    return Trie{root: root}
+}
+
+
+func (this *Trie) Insert(word string)  {
+    curr := this.root
+    for i := 0; i < len(word); i++ {
+        idx := word[i]-'a'
+        if curr.childs[idx] == nil {
+            curr.childs[idx] = &trieNode{childs: [26]*trieNode{}}
+        }
+        curr = curr.childs[idx]
+    }
+    curr.isEnd = true
+}
+
+
+func (this *Trie) Search(word string) bool {
+    curr := this.root
+    for i := 0; i < len(word); i++ {
+        idx := word[i]-'a'
+        if curr.childs[idx] == nil { return false }
+        curr = curr.childs[idx]
+    }
+    return curr.isEnd
+}
+
+
+func (this *Trie) StartsWith(prefix string) bool {
+    curr := this.root
+    for i := 0; i < len(prefix); i++ {
+        idx := prefix[i]-'a'
+        if curr.childs[idx] == nil { return false }
+        curr = curr.childs[idx]
+    }
+    return true
+}
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
