b25869a4ff8821db64538f3d8e52a0cfdc37eaf6
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-28 16:13:03 -0500

    Time: 39 ms (62.50%), Space: 25.5 MB (33.33%) - LeetHub

--- 0648-replace-words/0648-replace-words.go
+++ 0648-replace-words/0648-replace-words.go
@@ -8,6 +8,8 @@ func newTrieNode() *trieNode {
     return &trieNode{childs: [26]*trieNode{}}
 }
 
+// time: o(n) where n is the len of word
+// space: o(26*n) where n is the len of word ( for each char we allocate 26 sized array )
 func (r *trieNode) insert(word string) {
     curr := r
     for i := 0; i < len(word); i++ {
@@ -20,6 +22,8 @@ func (r *trieNode) insert(word string) {
     curr.isEnd = true
 }
 
+// time: o(n) - we found the entire word as is, no early exits 
+// space: o(n) - we found the entire word as is, and we allocated entire word space in str builder
 func (r *trieNode) findPrefix(word string) (string, bool) {
     curr := r
     tmp := new(strings.Builder)
@@ -36,14 +40,26 @@ func (r *trieNode) findPrefix(word string) (string, bool) {
 
 
 func replaceWords(dictionary []string, sentence string) string {
+    
     root := newTrieNode()
+    // o(d) * o(k) where d is number of words in dict, k is the avg len of each word
+    // therefore o(dk)
     for i := 0; i < len(dictionary); i++ {
         root.insert(dictionary[i])
     }
+    
+    // let w == len of words in sentence
+    // o(w) space
     sSplit := strings.Split(sentence, " ")
-    out := new(strings.Builder)
+    
+    // o(w) space 
+    out := new(strings.Builder) 
+
+    // o(w) * o(k) where w is the number of words we have in sentence, k is the avg len of each word
+    // for each w word, we do a prefixSearch where prefixSearch takes o(k) time worse case
+    // therefore o(wk)
     for idx, word := range sSplit {
-        prefix, found := root.findPrefix(word)
+        prefix, found := root.findPrefix(word) // time: o(k), space: o(k)
         if found {
             out.WriteString(prefix)
         } else {
@@ -53,5 +69,6 @@ func replaceWords(dictionary []string, sentence string) string {
             out.WriteString(" ")
         }
     }
+    
     return out.String()
 }
