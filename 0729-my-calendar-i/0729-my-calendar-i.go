3f5161ed2d75fc77b658f28a37f9aa9368513cc4
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-28 19:59:47 -0500

    Time: 90 ms (66.20%), Space: 7.3 MB (46.48%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.go
+++ 0729-my-calendar-i/0729-my-calendar-i.go
@@ -13,21 +13,37 @@ func (this *MyCalendar) Book(start int, end int) bool {
         this.cal = append(this.cal, []int{start, end})
         return true
     }
-    prevEvent := this.searchLeft(start)
-    if prevEvent == -1 {prevEvent = 0}
-    prev := this.cal[prevEvent]
+    // o(logn)
+    prevIdx := this.searchLeft(start)
+    if prevIdx == -1 {prevIdx = 0}
+    prev := this.cal[prevIdx]
     if start < prev[1] && end > prev[0] {return false}
 
-    nextEvent := this.searchRight(start)
-    if nextEvent == -1 {nextEvent = 0}
-    next := this.cal[nextEvent]
+    // o(logn)
+    nextIdx := this.searchRight(start)
+    if nextIdx == -1 {nextIdx = 0}
+    next := this.cal[nextIdx]
     if start < next[1] && end > next[0] {return false}
+    
 
-    this.cal = append(this.cal, []int{start, end})
-    sort.SliceStable(this.cal, func(i, j int) bool{
-        return this.cal[i][0] < this.cal[j][0]
-    })
+    // // o(nlogn)
+    // this.cal = append(this.cal, []int{start, end})
+    // sort.SliceStable(this.cal, func(i, j int) bool{
+    //     return this.cal[i][0] < this.cal[j][0]
+    // })
+    this.insert(start, end)
     return true 
+}
+
+func (this *MyCalendar) insert(start, end int) {  
+    curr := []int{start, end}
+    this.cal = append(this.cal, curr)
+    for i := len(this.cal)-1; i > 0; i-- {
+        prev := this.cal[i-1]
+        if curr[0] < prev[0] {
+            this.cal[i], this.cal[i-1] = this.cal[i-1], this.cal[i]
+        }
+    }
 }
 
 func (this *MyCalendar) searchLeft(start int) int {
