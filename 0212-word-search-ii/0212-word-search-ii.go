384bf3704dc7010466b2a50a73f5cb0bfb169299
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-28 20:56:03 -0500

    Time: 1779 ms (31.69%), Space: 10.3 MB (9.86%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.go
@@ -0,0 +1,76 @@
+func findWords(board [][]byte, words []string) []string {
+    root := &trieNode{childs: [26]*trieNode{}}
+    for _, word := range words {
+        insert(root, word)
+    }
+    m := len(board)
+    n := len(board[0])
+    dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
+    outSet := map[string]struct{}{}
+
+    var dfs func(r *trieNode, i, j int, path string)
+    dfs = func(r *trieNode, i, j int, path string) {
+        // base
+        if i < 0 || i == m || j < 0 || j == n || board[i][j] == '#' {return}
+        
+        // logic
+        path += string(board[i][j])
+        newR, found := startsWith(root, path)
+        if !found {return}
+        if newR.isEnd {
+            if _, ok := outSet[path]; !ok {
+                outSet[path] = struct{}{}
+            }
+        }
+        
+        
+        tmp := board[i][j]
+        board[i][j] = '#'
+        for _, dir := range dirs {
+            dfs(newR, i+dir[0], j+dir[1], path)
+        }
+        board[i][j]= tmp
+    }
+    
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            dfs(root, i,j, "")
+        }
+    }
+    out := []string{}
+    for k, _ := range outSet {
+        out = append(out, k)
+    }
+    return out
+}
+
+
+
+
+type trieNode struct {
+    isEnd bool
+    childs [26]*trieNode
+}
+
+
+
+func insert(root *trieNode, word string)  {
+    curr := root
+    for _, char := range word {
+        if curr.childs[char-'a'] == nil {
+            curr.childs[char-'a'] = &trieNode{isEnd: false, childs: [26]*trieNode{}}
+        }
+        curr = curr.childs[char-'a']
+    }
+    curr.isEnd = true
+}
+
+func startsWith(root *trieNode, prefix string) (*trieNode, bool) {
+    curr := root
+    for _, char := range prefix {
+        idx := char-'a'
+        if curr.childs[idx] == nil {return nil, false}
+        curr = curr.childs[idx]
+    }
+    return curr, true
+}
