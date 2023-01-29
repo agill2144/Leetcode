9b3efc78ce8297c4d551e582622752a722327c57
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-28 19:49:39 -0500

    Time: 179 ms (18.31%), Space: 7.2 MB (97.18%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.go
+++ 0729-my-calendar-i/0729-my-calendar-i.go
@@ -9,19 +9,50 @@ func Constructor() MyCalendar {
 
 
 func (this *MyCalendar) Book(start int, end int) bool {
-    for i := 0; i < len(this.cal); i++ {
-        if end > this.cal[i][0] && start < this.cal[i][1] {return false}
+    if len(this.cal) == 0 {
+        this.cal = append(this.cal, []int{start, end})
+        return true
     }
+    prevEvent := this.searchLeft(start)
+    if prevEvent == -1 {prevEvent = 0}
+    prev := this.cal[prevEvent]
+    if start < prev[1] && end > prev[0] {return false}
+
+    nextEvent := this.searchRight(start)
+    if nextEvent == -1 {nextEvent = 0}
+    next := this.cal[nextEvent]
+    if start < next[1] && end > next[0] {return false}
+
     this.cal = append(this.cal, []int{start, end})
-    return true
-   
+    sort.SliceStable(this.cal, func(i, j int) bool{
+        return this.cal[i][0] < this.cal[j][0]
+    })
+    return true 
 }
 
-func (this *MyCalendar) nextSmallestStartTime(start int) int {
-    ans := -1
+func (this *MyCalendar) searchLeft(start int) int {
     left := 0
     right := len(this.cal)-1
-    
+    ans := -1
+    for left <= right {
+        mid := left + (right-left)/2
+        if this.cal[mid][0] <= start {
+            if this.cal[mid][0] == start {
+                return mid
+            }
+            ans = mid
+            left = mid+1
+        } else {
+            right = mid-1
+        }
+    }
+    return ans
+}
+
+func (this *MyCalendar) searchRight(start int) int {
+    left := 0
+    right := len(this.cal)-1
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
         if this.cal[mid][0] >= start {
