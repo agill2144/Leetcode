46fd7980b0fe8aa541c26aa623b3f78f0bb99a4f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-28 21:15:07 -0500

    Time: 3093 ms (16.90%), Space: 7.6 MB (39.44%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.go
+++ 0212-word-search-ii/0212-word-search-ii.go
@@ -1,77 +1,79 @@
-type Trie struct {
-    children map[byte]*Trie
-    word string
-}
-
-func (t *Trie) Add(word string) {
-    c := t
-    for i := 0; i < len(word); i++ {
-        v := word[i]
-        if n, ok := c.children[v]; ok {
-            c = n
-        } else {
-            c.children[v] = &Trie{children: make(map[byte]*Trie)}
-            c = c.children[v]
-        }
-    }
-    c.word = word
-}
-
 func findWords(board [][]byte, words []string) []string {
-    // Construct trie from input words
-    trie := &Trie{children: make(map[byte]*Trie)}
-    for _, word := range words {
-        trie.Add(word)
+    trie := NewTrie()
+    for _, w := range words {
+        trie.InsertWord(w)
     }
 
-    moves := [][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
-    adj := func(row, col int) [][2]int {
-        var ans [][2]int
-        for _, m := range moves {
-            r, c := row+m[0], col+m[1]
-            if r >= 0 && r < len(board) && c >= 0 && c < len(board[0]) {
-                ans = append(ans, [2]int{r, c})
-            }
-        }
-        return ans
-    }
-
-    ans := make(map[string]bool)
-
-    var dfs func(int, int, *Trie)
-    dfs = func(row, col int, t *Trie) {
-        v := board[row][col]
-        board[row][col] = '_'
-        // Backtracking with defer!
-        defer func() {
-            board[row][col] = v
-        }()
-        if tn, ok := t.children[v]; ok {
-            t = tn
-        } else {
-            return
-        }
-        if len(t.word) > 0 {
-            ans[t.word] = true
-        }
-
-        for _, n := range adj(row, col) {
-            v := board[n[0]][n[1]]
-            if v == '_' {
-                continue
-            }
-            dfs(n[0], n[1], t)
+    wordsFound := make(map[string]struct{}, 0)
+    for i := range board {
+        for j := range board[0] {
+            dfs(i, j, board, make(map[pair]struct{}), "", wordsFound, trie.root)
         }
     }
 
-    for row := 0; row < len(board); row++ {
-        for col := 0; col < len(board[0]); col++ {
-            dfs(row, col, trie)
+    ans := make([]string, 0, len(wordsFound))
+    for w := range wordsFound {
+        ans = append(ans, w)
+    }
+
+    return ans
+}
+
+type pair struct {
+    r, c int
+}
+
+func dfs(r, c int, board [][]byte, visited map[pair]struct{}, curString string, wordsFound map[string]struct{}, curNode *TrieNode) {
+    if curNode.isEnd {
+        wordsFound[curString] = struct{}{}
+    }
+
+    inBounds := r >= 0 && r < len(board) && c >= 0 && c < len(board[0])
+    _, isVisited := visited[pair{r, c}]
+    if !inBounds || isVisited {
+        return
+    }
+    if _, ok := curNode.data[board[r][c]]; !ok {
+        return
+    }
+
+    visited[pair{r, c}] = struct{}{}
+
+    newString := curString+string(board[r][c])
+    dfs(r+1, c, board, visited, newString, wordsFound, curNode.data[board[r][c]])
+    dfs(r-1, c, board, visited, newString, wordsFound, curNode.data[board[r][c]])
+    dfs(r, c+1, board, visited, newString, wordsFound, curNode.data[board[r][c]])
+    dfs(r, c-1, board, visited, newString, wordsFound, curNode.data[board[r][c]])
+    
+    delete(visited, pair{r, c})
+}
+
+type Trie struct {
+    root *TrieNode
+}
+
+func NewTrie() *Trie {
+    return &Trie{root: NewTrieNode()}
+}
+
+type TrieNode struct {
+    data map[byte]*TrieNode
+    isEnd bool
+}
+
+func NewTrieNode() *TrieNode {
+    return &TrieNode{data: make(map[byte]*TrieNode)}
+}
+
+func (t *Trie) InsertWord(word string) {
+    cur := t.root
+
+    for i := range word {
+        if _, ok := cur.data[word[i]]; !ok {
+            cur.data[word[i]] = NewTrieNode()
         }
+        cur = cur.data[word[i]]
     }
-    var ans2 []string
-    for w, _ := range ans {
-        ans2 = append(ans2, w)
-    }
-    return ans2
+
+    cur.isEnd = true
 }
