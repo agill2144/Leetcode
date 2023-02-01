e3d2f9d9abcc70ca83cc59a5922b97bf4122928f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-01 13:54:00 -0500

    Time: 88 ms (37.50%), Space: 7.4 MB (37.50%) - LeetHub

---
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.go
@@ -0,0 +1,44 @@
+func maxFreq(s string, maxLetters int, minSize int, maxSize int) int {
+    if maxSize > len(s) || maxSize < minSize || len(s) == 0 {return 0}
+
+    wordMap := map[string]int{}
+    maxCount := 0
+    windowMap := map[string]int{}    
+    left := 0
+    
+    for right := 0; right < len(s); right++ {
+        strChar := string(s[right])
+        windowMap[strChar]++
+        
+        for len(windowMap) > maxLetters {
+            leftStrChar := string(s[left])
+            windowMap[leftStrChar]--
+            if val := windowMap[leftStrChar]; val == 0 {
+                delete(windowMap, leftStrChar)
+            }
+            left++
+        }
+        
+        windowStr := s[left:right+1]
+        // fmt.Println(windowStr)
+        // min = 3
+        // max = 6
+        // 3
+        // 4 || 5 || 6
+        for len(windowStr) >= minSize && len(windowStr) <= maxSize {
+            wordMap[windowStr]++
+            if val := wordMap[windowStr]; val > maxCount {maxCount = val}
+            // if len(windowStr) == minSize {break}
+            leftStrChar := string(s[left])
+            windowMap[leftStrChar]--
+            if val := windowMap[leftStrChar]; val == 0 {
+                delete(windowMap, leftStrChar)
+            }
+            left++
+            windowStr = s[left:right+1]
+        }
+    }
+    
+    fmt.Println(wordMap, "")
+    return maxCount
+}
