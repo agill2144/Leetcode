180937f24567e47e6909c021abcf147c519bddfc
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-02 17:59:37 -0500

    Time: 110 ms (56.39%), Space: 9.1 MB (39.53%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
@@ -1,5 +1,40 @@
+/*
+    approach: min heap
+    time: o(nlogk)
+    space: o(k)
+*/
 func findKthLargest(nums []int, k int) int {
-    if k > len(nums) {return -1}
-    sort.Ints(nums)
-    return nums[len(nums)-k]
+    mn := &minHeap{items: []int{}}
+    for i := 0; i < len(nums); i++ {
+        heap.Push(mn, nums[i])
+        if len(mn.items) > k {
+            heap.Pop(mn)
+        }
+    }
+    return mn.items[0]
 }
+
+type minHeap struct {
+    items []int
+}
+
+func (m *minHeap) Less(i, j int) bool {return m.items[i] < m.items[j]}
+func (m *minHeap) Swap(i, j int) {m.items[i],m.items[j] = m.items[j], m.items[i]}
+func (m *minHeap) Len() int {return len(m.items)}
+func (m *minHeap) Push(x interface{}) { m.items = append(m.items, x.(int))}
+func (m *minHeap) Pop() interface{} {
+    out := m.items[len(m.items)-1]
+    m.items = m.items[:len(m.items)-1]
+    return out
+}
+
+// approach: sort
+// time: o(nlogn) 
+    // - golang uses quickSort under the hood
+// avg time: o(nlogn) and worst case is o(n^2) because quick sort
+// space: o(n) used by quick sort
+// func findKthLargest(nums []int, k int) int {
+//     if k > len(nums) {return -1}
+//     sort.Ints(nums)
+//     return nums[len(nums)-k]
+// }
