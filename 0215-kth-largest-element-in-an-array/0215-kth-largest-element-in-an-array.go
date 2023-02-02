cff8c595d476d493750492d48da855b4ddeee7f7
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-02 18:45:27 -0500

    Time: 86 ms (75.58%), Space: 7.8 MB (99.42%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
@@ -1,32 +1,72 @@
 /*
+    approach: quick select
+    - like quick sort except you decide after each iteration, which side to sort on
+*/
+
+func findKthLargest(nums []int, k int) int {
+    if k > len(nums) {return -1}
+    targetIdx := len(nums)-k
+    
+    var quickSelect func(l, r int) (int, bool)
+    quickSelect = func(l, r int) (int, bool) {
+        // base
+        if l > r {return -1, false}
+        
+        // logic
+        ns := l
+        pivotIdx := r
+        for i := l; i < pivotIdx; i++ {
+            if nums[i] < nums[pivotIdx] {
+                nums[i], nums[ns] = nums[ns], nums[i]
+                ns++
+            }
+        }
+        nums[ns], nums[pivotIdx] = nums[pivotIdx], nums[ns]
+        // fmt.Println(nums, ns, targetIdx)
+        if ns == targetIdx {
+            return nums[ns], true
+        }
+        if targetIdx > ns {
+            val, ok := quickSelect(ns+1, r)
+            if ok {return val, ok}
+        } else { 
+            return quickSelect(l, ns-1)
+        }
+        return -1, false
+    }
+    val, _ := quickSelect(0, len(nums)-1)
+    return val
+}
+
+/*
     approach: min heap
     time: o(nlogk)
     space: o(k)
 */
-func findKthLargest(nums []int, k int) int {
-    mn := &minHeap{items: []int{}}
-    for i := 0; i < len(nums); i++ {
-        heap.Push(mn, nums[i])
-        if len(mn.items) > k {
-            heap.Pop(mn)
-        }
-    }
-    return mn.items[0]
-}
+// func findKthLargest(nums []int, k int) int {
+//     mn := &minHeap{items: []int{}}
+//     for i := 0; i < len(nums); i++ {
+//         heap.Push(mn, nums[i])
+//         if len(mn.items) > k {
+//             heap.Pop(mn)
+//         }
+//     }
+//     return mn.items[0]
+// }
 
-type minHeap struct {
-    items []int
-}
+// type minHeap struct {
+//     items []int
+// }
 
-func (m *minHeap) Less(i, j int) bool {return m.items[i] < m.items[j]}
-func (m *minHeap) Swap(i, j int) {m.items[i],m.items[j] = m.items[j], m.items[i]}
-func (m *minHeap) Len() int {return len(m.items)}
-func (m *minHeap) Push(x interface{}) { m.items = append(m.items, x.(int))}
-func (m *minHeap) Pop() interface{} {
-    out := m.items[len(m.items)-1]
-    m.items = m.items[:len(m.items)-1]
-    return out
-}
+// func (m *minHeap) Less(i, j int) bool {return m.items[i] < m.items[j]}
+// func (m *minHeap) Swap(i, j int) {m.items[i],m.items[j] = m.items[j], m.items[i]}
+// func (m *minHeap) Len() int {return len(m.items)}
+// func (m *minHeap) Push(x interface{}) { m.items = append(m.items, x.(int))}
+// func (m *minHeap) Pop() interface{} {
+//     out := m.items[len(m.items)-1]
+//     m.items = m.items[:len(m.items)-1]
+//     return out
+// }
 
 // approach: sort
 // time: o(nlogn) 
