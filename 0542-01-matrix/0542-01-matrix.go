bb7eaef5b120c590ff4b9794a651643cffa710c0
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-05 11:30:52 -0500

    Time: 55 ms (81.90%), Space: 7.5 MB (84.16%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.go
+++ 0542-01-matrix/0542-01-matrix.go
@@ -1,31 +1,48 @@
 func updateMatrix(mat [][]int) [][]int {
     m := len(mat)
     n := len(mat[0])
-    q := [][]int{}
+    topLeftDirs := [][]int{{-1,0},{0,-1}}
+    bottomRight := [][]int{{1,0},{0,1}}
+
     for  i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            if mat[i][j] == 0 {
-                q = append(q, []int{i,j})
-            } else {
-                mat[i][j] = -1
+            if mat[i][j] == 0 {continue}
+
+            minVal := math.MaxInt64-10
+            for _, dir := range topLeftDirs {
+                nr := i+dir[0]
+                nc := j+dir[1]
+                if nr >= 0 && nc >= 0 && mat[nr][nc] < minVal {
+                    minVal = mat[nr][nc]
+                }
             }
+            mat[i][j] = minVal + 1
         }
     }
-    dirs := [][]int{ {1,0},{-1,0},{0,-1},{0,1} }
     
-    for len(q) != 0 {
-        dq := q[0]
-        q = q[1:]
-        cr := dq[0]
-        cc := dq[1]
-        for _ , dir := range dirs {
-            nr := cr+dir[0]
-            nc := cc+dir[1]
-            if nr >= 0 && nr < m && nc >= 0 && nc < n && mat[nr][nc] == -1 {
-                mat[nr][nc] = mat[cr][cc]+1
-                q = append(q, []int{nr, nc})
+    for i := m-1; i >= 0; i-- {
+        for j := n-1; j >= 0; j-- {
+            if mat[i][j] == 0 {continue}
+            
+            minVal := math.MaxInt64-10
+            for _ , dir := range bottomRight {
+                nr := i+dir[0]
+                nc := j+dir[1]
+                if nr < m && nc < n && mat[nr][nc] < minVal {
+                    minVal = mat[nr][nc]
+                }
             }
+            minVal += 1
+            
+            mat[i][j] = min(mat[i][j], minVal)
         }
     }
+    
+
     return mat
+}
+
+func min(x, y int) int {
+    if x < y {return x}
+    return y
 }
