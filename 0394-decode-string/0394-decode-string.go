f4cbc419645c14b7d4349de9df9c29baf41f3838
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-05 21:20:07 -0500

    Time: 0 ms (100.00%), Space: 1.9 MB (68.98%) - LeetHub

--- 0394-decode-string/0394-decode-string.go
+++ 0394-decode-string/0394-decode-string.go
@@ -1,36 +1,70 @@
+/*
+    approach: 2 stacks
+    - flatten / expand a bracket and combine with the parent
+        - to expand a bracket, we need previous number
+        - to combine a expanded bracket, we need previous parent.
+    - previous / looking back for a char we last saw = stack is possible.
+    - lets start with a numStack and strStack.
+    - we will also need to maintain a current running string and current running num.
+    - if we run into a char thats either a char or a number, we can concatnate that with their appropriate vars.
+    - if we run into a open bracket '['
+        - we know that the inside string of this opening bracket would need access to curr number and current parent we have in hand
+        - because the curr number and current parent in hand is the previous digit and char for the inner string.
+        - therefore if we pushed both number and parent str in their stacks, we can have access to them in o(1) time because of stack (they would be at the top)
+    - after we go thru chars inside [..] - we will have a curr string in hand, curr number may be 0 if there were no nesting.
+    - when we run into a closing bracket, its time to expand the curr string
+        - which means we need previous number ( top of number stack ), lets call this k
+        - repeat the curr string k times
+        - now we need to combine with parent string ( top of parent stack ), lets call this parent
+        - now to parent, append the expanded str
+        - finally re-assign curr str to new parent str because its expanded and combined with it parent while curr was just whatever we had [...] inside a closing bracket
+    
+    Tldr: decode a bracket and combine it with its parent
+        - decoding a bracket requires access to prev num and prev parent ( stack )
+    
+    We can do this with 1 stack too. Space would be the same regardless.
+    
+    time: o(s*k^countNumberOfNesting)
+        s is the len of input string
+        k is the digit in the input string
+    or time can be;
+        o(lenOfOutputStr)
+    
+    space: o(numSt) + o(strSt)
+    
+*/
 func decodeString(s string) string {
-    ptr := 0
-    var dfs func() string
-    dfs = func() string {
-        // base
-        // if start == len(s) {return ""}
+    numSt := []int{}
+    strSt := []*strings.Builder{}
+    
+    curr := new(strings.Builder)
+    num := 0
+    
+    for i := 0; i < len(s); i++ {
+        char := s[i] // byte, i.e the ascii value
         
-        // logic
-        curr := new(strings.Builder)
-        n := 0
-        for ptr < len(s) {
-            char := s[ptr]
-            if char >= '0' && char <= '9' {
-                n = n * 10 + int(char-'0')
-                ptr++
-            } else if char == '[' {
-                ptr++
-                inner := dfs()
-                decoded := new(strings.Builder)
-                for k := 0; k < n; k++ {
-                    decoded.WriteString(inner)
-                }
-                n = 0
-                curr.WriteString(decoded.String())
-            } else if char == ']' {
-                ptr++
-                return curr.String()
-            } else if char >= 'a' && char <= 'z' {
-                curr.WriteByte(char)
-                ptr++
+        // if ascii value is >= ascii of 0 and <= ascii of 9, then this is a digit
+        if char >= '0' && char <= '9' {
+            num = num * 10 + int(char-'0')
+        } else if char >= 'a' && char <= 'z' {
+            curr.WriteByte(char)
+        } else if char == '[' {
+            numSt = append(numSt, num)
+            strSt = append(strSt, curr)
+            curr = new(strings.Builder)
+            num = 0
+        } else if char == ']' {
+            k := numSt[len(numSt)-1]
+            numSt = numSt[:len(numSt)-1]
+            expandedCurr := new(strings.Builder)
+            for i := 0; i < k; i++ {
+                expandedCurr.WriteString(curr.String())
             }
+            parent := strSt[len(strSt)-1]
+            strSt = strSt[:len(strSt)-1]
+            parent.WriteString(expandedCurr.String())
+            curr = parent
         }
-        return curr.String()
     }
-    return dfs()
+    return curr.String()
 }
