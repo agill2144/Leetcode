c9e59bc28b254cee9ce0ef4682fc7752e25b1675
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-06 20:17:50 -0500

    Time: 122 ms (56.52%), Space: 8.5 MB (78.26%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.go
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.go
@@ -1,22 +1,19 @@
 func totalFruit(fruits []int) int {
     basket := map[int]int{}
     left := 0
-    count := 0
     maxCount := 0
     for i := 0; i < len(fruits); i++ {
         basket[fruits[i]]++
-        count++
         for len(basket) > 2 {
             leftFruit := fruits[left]
             basket[leftFruit]--
             if val := basket[leftFruit]; val == 0 {
                 delete(basket,leftFruit)
             }
-            count--
             left++
         }
-        if count > maxCount {
-            maxCount = count
+        if i-left+1 > maxCount {
+            maxCount = i-left+1
         }
     }
     return maxCount
