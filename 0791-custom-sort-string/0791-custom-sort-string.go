65ad6b556f393f5d050e73ee2d387fbed0d06dde
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-06 21:40:55 -0500

    Time: 1 ms (67.50%), Space: 1.9 MB (100.00%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.go
+++ 0791-custom-sort-string/0791-custom-sort-string.go
@@ -1,3 +1,15 @@
+/*
+    s must be in the same form as order
+    - freqMap of s chars
+    - loop over chars in order string
+        - if this char exists in sFreqMap
+        - repeat this char that many times in the output string
+        - since we have already used this char from order string, we can delete this char from the sFreqMap
+    - Finally append whatever else is remaining from s ( compare with sFreqMap )
+    
+    time: o(len(s)) + o(len(order))
+    space: o(len(s))
+*/
 func customSortString(order string, s string) string {
     sFreqMap := map[byte]int{} // space: o(1), there are constant number of characters in this world.
     
@@ -13,6 +25,9 @@ func customSortString(order string, s string) string {
     for i := 0; i < len(order); i++ {
         count, ok := sFreqMap[order[i]]
         if ok {
+            // o(s) ; worst case, all chars == this char in order
+            // order = "b"
+            // s = "bbbbbbbbbbb"
             for k := 0; k < count; k++ {
                 out.WriteByte(order[i])
             }
@@ -27,7 +42,7 @@ func customSortString(order string, s string) string {
         }
     }
     
-    // total time : o(2s)+o(order)
+    // total time : o(s)+o(order*maxRepeatsOfACharInS)
     // total space: o(1)
     return out.String()
 }
