fc373e73d3e0f6e9814fce3ff83ae5ef859e1094
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-06 19:55:27 -0500

    Time: 1 ms (66.75%), Space: 2.2 MB (40.57%) - LeetHub

--- 0078-subsets/0078-subsets.go
+++ 0078-subsets/0078-subsets.go
@@ -1,23 +1,12 @@
 func subsets(nums []int) [][]int {
-    out := [][]int{}
-    var dfs func(start int, path []int)
-    dfs = func(start int, path []int) {
-        // base
-        
-        newL := make([]int, len(path))
-        copy(newL, path)
-        out = append(out, newL)
-
-        // logic
-        for i := start ; i < len(nums); i++ {
-            // action
-            path = append(path, nums[i])
-            // recurse
-            dfs(i+1, path)
-            // backtrack
-            path = path[:len(path)-1]
+    out := [][]int{{}}
+    for i := 0; i < len(nums); i++ {
+        for _, list := range out {
+            newL := make([]int, len(list))
+            copy(newL, list)
+            newL = append(newL, nums[i])
+            out = append(out, newL)
         }
     }
-    dfs(0, []int{})
     return out
 }
