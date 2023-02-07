6dc89da2ae1ceee356e8cb7cdfcc7cf44dc489c2
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-06 20:30:14 -0500

    Time: 76 ms (88.34%), Space: 8.5 MB (67.06%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
@@ -1,30 +1,24 @@
 func findKthLargest(nums []int, k int) int {
-    var dfs func(l, r int) (int, bool)
-    dfs = func(l, r int) (int, bool) {
-        // base
-        // if l >= r {return -1, false}
-        
-        // logic
-        pivotIdx := r
-        nextSmaller := l
-        for i := l; i < pivotIdx; i++ {
-            if nums[i] < nums[pivotIdx] {
-                nums[i], nums[nextSmaller] = nums[nextSmaller], nums[i]
-                nextSmaller++
-            }
-        }
-        nums[nextSmaller], nums[pivotIdx] = nums[pivotIdx], nums[nextSmaller]
-        if len(nums) - k == nextSmaller {
-            return nums[nextSmaller], true
-        }
-        if len(nums)-k > nextSmaller {
-            if val, ok := dfs(nextSmaller+1, r); ok {
-                return val, ok
-            }
-        }
-        return dfs(l, nextSmaller-1)
-    }
-    val, _ := dfs(0, len(nums)-1)
-    return val
+    left := 0
+    right := len(nums)-1
     
+    for left <= right {
+        
+        pivotIdx := right
+        ns := left
+        for i := left; i < pivotIdx; i++ {
+            if nums[i] < nums[pivotIdx] {
+                nums[i], nums[ns] = nums[ns], nums[i]
+                ns++
+            }
+        }
+        nums[ns], nums[pivotIdx] = nums[pivotIdx], nums[ns]
+        if ns == len(nums)-k {return nums[ns]}
+        if len(nums)-k > ns {
+            left = ns+1
+        } else {
+            right = ns-1
+        }
+    }
+    return -1
 }
