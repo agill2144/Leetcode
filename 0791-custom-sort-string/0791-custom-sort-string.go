cc0fbfb99bf707c3d32a38d474e4d8efd0e2443b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-06 21:35:58 -0500

    Time: 2 ms (52.50%), Space: 1.9 MB (100.00%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.go
+++ 0791-custom-sort-string/0791-custom-sort-string.go
@@ -1,9 +1,15 @@
 func customSortString(order string, s string) string {
-    sFreqMap := map[byte]int{}
+    sFreqMap := map[byte]int{} // space: o(1), there are constant number of characters in this world.
+    
+    // time: o(s)
     for i := 0; i < len(s); i++ {
         sFreqMap[s[i]]++
     }
+    
     out := new(strings.Builder)
+    
+    
+    // time: o(order)
     for i := 0; i < len(order); i++ {
         count, ok := sFreqMap[order[i]]
         if ok {
@@ -14,10 +20,14 @@ func customSortString(order string, s string) string {
         delete(sFreqMap, order[i])
     }
     
+    // time: o(s)
     for i := 0; i < len(s); i++ {
         if _, ok := sFreqMap[s[i]]; ok {
             out.WriteByte(s[i])
         }
     }
+    
+    // total time : o(2s)+o(order)
+    // total space: o(1)
     return out.String()
 }
