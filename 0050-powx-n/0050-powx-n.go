ae7f246bfc1f28d26a460113b8c28574f5ba0f73
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-08 00:01:26 -0500

    Time: 3 ms (48.93%), Space: 1.9 MB (45.26%) - LeetHub

--- 0050-powx-n/0050-powx-n.go
+++ 0050-powx-n/0050-powx-n.go
@@ -18,6 +18,15 @@
 
 
 /*
+approach:  recursion + binary search like
+- 3^10 is same as 3^5 * 3^5
+- now we solved for 1 of the 3^5, we dont have to solve for the second the 3^5.
+- Therefore we can half the exponent each time until the exponent reaches 0
+- anything raised to power of 0 is 1 ( base case )
+- however when we are solving for 3^5 ; this is not the same as 3^2 * 3^2 ( we halved the 5 therefore the 2 ).. so we are just missing 1 multiplication of base n.
+- Therefore when our exponent is odd, it cannot be perfectly halved and we loose multiplying by 1 more base. Therefore multiply the base 1 more time in this case.
+time : o(logn)
+space: o(logn) - we made half recursion calls. therefore logn space for recursive stack.
 */
 func myPow(x float64, n int) float64 {
     if n < 0 {
