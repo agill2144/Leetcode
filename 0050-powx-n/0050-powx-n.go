2af57d7d8220242f37fcf35867c0b64a88a0883f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-07 23:56:36 -0500

    Time: 0 ms (100.00%), Space: 1.9 MB (45.26%) - LeetHub

--- 0050-powx-n/0050-powx-n.go
+++ 0050-powx-n/0050-powx-n.go
@@ -1,22 +1,15 @@
 /*
-    approach: brute force 
-    - loop n times and multiply x...
-    - if n < 0 , then x = 1/x and n = -n
-    - for loop 0 to n(exlcuding n and multiply x each time to a result var)
-    - return result
-    time: o(n) - we loop n times
+    approach: brute force
+    - multiple x to itself n times
+    time: o(n)
     space: o(1)
-    - Outcome: TLE
 */
-
-// brute force
-// o(n) time and o(1) space
 // func myPow(x float64, n int) float64 {
 //     if n < 0 {
 //         x = 1/x
 //         n *= -1
 //     }
-//     result := 1.00
+//     result := 1.0
 //     for i := 0; i < n; i++ {
 //         result *= x
 //     }
@@ -25,43 +18,24 @@
 
 
 /*
-    approach: logN recursive
-    - if n is negative, make it positive and change the x to 1/x, then we will
-    - half the exponent in each recursive call
-    - once we reach the leaf of our recursion tree ( the base case when exponent will be 0)
-        - then return 1.00
-    - once we go back from leaf to a parent call
-    - we will check if n(exponent)  is an even number
-        - if yes, return result * result
-        - if not, return result * result * x (base)
-            - why? 
-            - because even exponent(n) was perfectly divided in half, odd exponent miss multiplying the base by 1 therefore the extra  x $x(base)
-    - How is this optimizing the brute force????
-        - Example: 2^10
-        - Our brute force loop runs 10 times
-        - Whereas the binary search approach runs 5 times - therefore optimized
-    
 */
-
-
 func myPow(x float64, n int) float64 {
     if n < 0 {
-        x = 1/x
         n *= -1
+        x = 1/x
     }
     var dfs func(base float64, exp int) float64
-    dfs = func(base float64, exp int) float64{
+    dfs = func(base float64, exp int) float64 {
         // base
-        if exp == 0 {
-            return 1
-        }
+        if exp == 0 {return 1}
+        
         // logic
-        result := dfs(base, exp/2)
-        if exp % 2 == 0 {
-            return result*result
-        } else{
-            return result*result*base
+        tmp := dfs(base, exp/2)
+        result := tmp * tmp
+        if exp % 2 != 0 {
+            result *= base
         }
+        return result
     }
     return dfs(x, n)
 }
