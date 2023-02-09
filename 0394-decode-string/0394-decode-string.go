fbbcb246a86be8942dd273e420970119e0fda0df
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-09 00:30:11 -0500

    Time: 1 ms (63.70%), Space: 1.9 MB (66.34%) - LeetHub

--- 0394-decode-string/0394-decode-string.go
+++ 0394-decode-string/0394-decode-string.go
@@ -1,27 +1,35 @@
 func decodeString(s string) string {
-    curr := new(strings.Builder)
-    num := 0
-    strSt := []*strings.Builder{}
-    numSt := []int{}
-    for i := 0; i < len(s); i++ {
-        char := s[i]
-        if char >= '0' && char <= '9' {
-            num = num * 10 + int(char-'0')
-        } else if char >= 'a' && char <= 'z' {
-            curr.WriteByte(char)
-        } else if char == '[' {
-            strSt = append(strSt, curr)
-            numSt = append(numSt, num)
-            curr = new(strings.Builder)
-            num = 0
-        } else if char == ']' {
-            times := numSt[len(numSt)-1]; numSt = numSt[:len(numSt)-1]
-            parent := strSt[len(strSt)-1]; strSt = strSt[:len(strSt)-1]
-            for k := 0; k < times; k++ {
-                parent.WriteString(curr.String())
+    var dfs func() string
+    i := 0
+    dfs = func() string {
+        // base 
+        if i == len(s) {return ""}        
+        curr := new(strings.Builder)
+        num := 0
+
+        // logic
+        for i < len(s) {
+            char := s[i]
+            if char >= '0' && char <= '9' {
+                num = num * 10 + int(char-'0')
+                i++
+            } else if char >= 'a' && char <= 'z' {
+                curr.WriteByte(char)
+                i++
+            } else if char == '[' {
+                i++
+                innerStr := dfs()
+                for k := 0; k < num; k++ {
+                    curr.WriteString(innerStr)
+                }
+                num = 0
+            } else if char == ']' {
+                i++
+                return curr.String()
             }
-            curr = parent
+            
         }
+        return curr.String()
     }
-    return curr.String()
+    return dfs()
 }
