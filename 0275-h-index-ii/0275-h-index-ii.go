1a3d25bccfe16a25c9b0e68973af15379c4d4af4
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-10 15:30:27 -0500

    Time: 16 ms (85.37%), Space: 6.7 MB (56.10%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.go
+++ 0275-h-index-ii/0275-h-index-ii.go
@@ -1,14 +1,13 @@
 func hIndex(citations []int) int {
     left := 0
     right := len(citations)-1
-    
+    n := len(citations)
     hIdx := 0
     for left <= right {
         mid := left + (right-left)/2
-        diff := len(citations) - mid
-        if citations[mid] >= diff {
-            if citations[mid] == diff {return diff}
-            hIdx = diff
+        tmp := n-mid
+        if citations[mid] >= tmp {
+            hIdx = tmp
             right = mid-1
         } else {
             left = mid+1
