b3740f022c1a6542afab19f3ce583040bd745cab
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-11 15:36:53 -0500

    Time: 23 ms (20.56%), Space: 6.5 MB (11.97%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
@@ -1,128 +1,74 @@
-/*
-    approach: binary search
-    time: o(log nums1)
-    space: o(1)
-*/
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-    if len(nums2) < len(nums1) {return findMedianSortedArrays(nums2, nums1)}
-    
-    n1 := len(nums1)
-    n2 := len(nums2)
-    
-    left := 0
-    right := n1
-    
-    /*
-        [1,4,6,8]
-        [2,5,7,9,10,11,12,13]
-    */
-    for left <= right {
-        partX := left + (right-left) / 2
-        partY := (n1+n2)/2 - partX
-        
-        l1 := math.MinInt64
-        if partX != 0 { l1 = nums1[partX-1]}
-        l2 := math.MinInt64
-        if partY != 0 { l2 = nums2[partY-1]}
-        
-        r1 := math.MaxInt64
-        if partX < n1 { r1 = nums1[partX]}
-        r2 := math.MaxInt64
-        if partY < n2 { r2 = nums2[partY]}
-        
-        if l1 <= r2 && l2 <= r1 {
-            if (n1+n2) % 2 == 0 {
-                return (math.Min(float64(r1), float64(r2)) + math.Max(float64(l1), float64(l2)))/2.0
-            } else {
-                return float64(min(r1,r2))
-            }
-        } else if l1 > r2 {
-            right = partX-1
+    merged := []int{}
+    n1 := 0
+    n2 := 0
+    for n1 < len(nums1) && n2 < len(nums2) {
+        if nums1[n1] < nums2[n2] {
+            merged = append(merged, nums1[n1])
+            n1++
         } else {
-            left = partX+1
+            merged = append(merged, nums2[n2])
+            n2++            
         }
     }
-    return -1
+    for n1 < len(nums1) { merged = append(merged, nums1[n1]) ; n1++ }
+    for n2 < len(nums2) { merged = append(merged, nums2[n2]) ; n2++ }
+    
+    left := &maxHeap{items: []int{}}
+    right := &minHeap{items: []int{}}
+    
+    
+    for i := 0; i < len(merged); i++ {
+        heap.Push(left, merged[i])
+        // ensure left top <= right top
+        if left.Len() != 0 && right.Len() != 0 && 
+        left.items[0] > right.items[0] {
+            heap.Push(right, heap.Pop(left))
+        }
+        // ensure its balanced, so that the median is at the top of surface of both heaps
+        // otherwise median could get burried somewhere deep inside a heap
+        if left.Len() > right.Len() + 1 {
+            heap.Push(right, heap.Pop(left))
+        } else if right.Len() > left.Len() {
+            heap.Push(left, heap.Pop(right))
+        }
+
+    }
+    if len(left.items) > len(right.items) {
+        return float64(left.items[0])
+    } else if len(right.items) > len(left.items) {
+        return float64(right.items[0])
+    }
+    return (float64(left.items[0]) + float64(right.items[0]))/2.0
 }
 
-func min(x, y int) int {
-    if x < y {return x}
-    return y
-}
 
-func max(x, y int) int {
-    if x > y {return x}
-    return y
+func abs(x int) int {
+    if x < 0 {return x * -1}
+    return x
 }
 
 
 
-/*
-    approach : merge + sort and then grab middle
-    - merge both arrays into a new m+n array
-    - Sort the new array
-    - if len(newArray) is even, grab the middle and return
-    - or else grab the 2 middle elements and return the avg between them (even len array)
-    
-    time: o(m+nlogm+n)
-    space: o(1)
-*/
-// func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-//     merged := []int{}
-//     for i := 0; i < len(nums1); i++ {
-//         merged = append(merged, nums1[i])
-//     }
-//     for i := 0; i < len(nums2); i++ {
-//         merged = append(merged, nums2[i])
-//     }
-//     sort.Ints(merged)
-//     midIdx := len(merged) / 2
-//     if len(merged) % 2 != 0 {
-//         // odd array len, return the mid
-//         return float64(merged[midIdx])
-//     }
-//     var avg float64 = float64(merged[midIdx]+merged[midIdx-1])/2.0 
-//     return avg
-// }
 
+type maxHeap struct {items []int}
+func(m *maxHeap) Less(i, j int)bool {return m.items[i] > m.items[j]}
+func(m *maxHeap) Swap(i, j int)     {m.items[i], m.items[j] = m.items[j], m.items[i]}
+func(m *maxHeap) Len()int           {return len(m.items)}
+func(m *maxHeap) Push(x interface{}) {m.items = append(m.items, x.(int))}
+func(m *maxHeap) Pop()interface{} {
+    out := m.items[len(m.items)-1]
+    m.items = m.items[:len(m.items)-1]
+    return out
+}
 
-/*
-    approach : merge using two pointers so no need to sort, and then grab middle
-    - merge both arrays into a new m+n array using 2 pointers
-    - if len(newArray) is even, grab the middle and return
-    - or else grab the 2 middle elements and return the avg between them (even len array)
-    
-    time: o(m+n)
-    space: o(1)
-*/
-// func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-//     merged := []int{}
-//     n1 := 0
-//     n2 := 0
-//     for n1 < len(nums1) && n2 < len(nums2) {
-//         if nums1[n1] < nums2[n2] {
-//             merged = append(merged, nums1[n1])
-//             n1++
-//         } else {
-//             merged = append(merged, nums2[n2])
-//             n2++
-//         }
-//     }
-//     if n1 < len(nums1) {
-//         merged = append(merged, nums1[n1:]...)
-//     }
-//     if n2 < len(nums2) {
-//         merged = append(merged, nums2[n2:]...)
-//     }
-    
-//     midIdx := len(merged) / 2
-//     if len(merged) % 2 != 0 {
-//         // odd array len, return the mid
-//         return float64(merged[midIdx])
-//     }
-//     var avg float64 = float64(merged[midIdx]+merged[midIdx-1])/2.0 
-//     return avg
-// }
-
-
-
+type minHeap struct {items []int}
+func(m *minHeap) Less(i, j int)bool {return m.items[i] < m.items[j]}
+func(m *minHeap) Swap(i, j int)     {m.items[i], m.items[j] = m.items[j], m.items[i]}
+func(m *minHeap) Len()int           {return len(m.items)}
+func(m *minHeap) Push(x interface{}) {m.items = append(m.items, x.(int))}
+func(m *minHeap) Pop()interface{} {
+    out := m.items[len(m.items)-1]
+    m.items = m.items[:len(m.items)-1]
+    return out
+}
