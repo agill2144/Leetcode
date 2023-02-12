3d2ef45753b6a16706b3fe25095f2c56a215947f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-11 20:02:25 -0500

    Time: 29 ms (77.39%), Space: 6.7 MB (100.00%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.go
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.go
@@ -1,29 +1,26 @@
 func kthSmallest(matrix [][]int, k int) int {
-    mx := &maxHeap{items: []int{}}
     m := len(matrix)
     n := len(matrix[0])
-    for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
-            heap.Push(mx, matrix[i][j])
-            if len(mx.items) > k {
-                heap.Pop(mx)
-            }
+    left := matrix[0][0]
+    right := matrix[m-1][n-1]
+    
+    for left < right {
+        mid := left + (right-left)/2
+        count := countLessOrEqual(mid, matrix)
+        if count < k {
+            left = mid+1
+        } else {
+            right = mid
         }
     }
-    return mx.items[0]
+    return left
 }
-
-
-type maxHeap struct {
-    items []int
-}
-
-func (m *maxHeap) Less(i, j int) bool {return m.items[i] > m.items[j]}
-func (m *maxHeap) Swap(i, j int) {m.items[i],m.items[j] = m.items[j], m.items[i]}
-func (m *maxHeap) Len() int {return len(m.items)}
-func (m *maxHeap) Push(x interface{}) { m.items = append(m.items, x.(int))}
-func (m *maxHeap) Pop() interface{} {
-    out := m.items[len(m.items)-1]
-    m.items = m.items[:len(m.items)-1]
-    return out
+func countLessOrEqual(target int, matrix[][]int) int {
+    count := 0
+    for i := 0; i < len(matrix); i++ {
+        for j := 0; j < len(matrix[0]); j++ {
+            if matrix[i][j] <= target {count++}
+        }
+    }
+    return count
 }
