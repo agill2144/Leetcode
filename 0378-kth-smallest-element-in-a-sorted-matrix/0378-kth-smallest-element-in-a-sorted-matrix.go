74a229ab077582da754fb21810eb48b21664fe8f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-12 21:20:15 -0500

    Time: 19 ms (97.37%), Space: 6.7 MB (100.00%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.go
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.go
@@ -1,32 +1,43 @@
 func kthSmallest(matrix [][]int, k int) int {
+    // binary search on values ( smallest valie at 0,0 and largest value at m-1,n-1 )
+    // each mid is being evaluated, are you my kth value?
+    // for mid to be the kth value, our matrix must contain atleast k or more elements less than or equal to mid value
+    // if count of lessThanOrEqual to mid >= k, this is a potential kth value, however we want the smallest one, therefore search left ( we may have overshot )
+    // if count of lessThanOrEqual to mid < k, search right
+    // for mid to be a kth value, there must be atleast k elements that are <= mid
+    
     m := len(matrix)
     n := len(matrix[0])
     left := matrix[0][0]
     right := matrix[m-1][n-1]
-    ans := -1    
+    kthSmallestVal := -1
+    
     for left <= right {
-        mid := left + (right-left)/2
-        count := countLessOrEqual(mid, matrix)
-        // there can be dupes, we want the smallest possible position that satifies count >= k
-        // we may have overshot, so continue searching left.
+        mid := left+(right-left)/2
+        count := countLessThanOrEqualTo(mid, matrix)
         if count >= k {
-            ans = mid
+            kthSmallestVal = mid
             right = mid-1
         } else {
             left = mid+1
         }
     }
-    return ans
+    return kthSmallestVal
 }
-func countLessOrEqual(target int, matrix[][]int) int {
-    count := 0
+
+// use stair case search to find count <= target
+func countLessThanOrEqualTo(target int, matrix [][]int) int {
     m := len(matrix)
     n := len(matrix[0])
+    count := 0
     r := m-1
     c := 0
     for r >= 0 && c < n {
         if matrix[r][c] <= target {
+            // all elements above this cell are less than or equal to target
+            // therefore add all elements in this col
             count += r+1
+            // and eval the next col
             c++
         } else {
             r--
