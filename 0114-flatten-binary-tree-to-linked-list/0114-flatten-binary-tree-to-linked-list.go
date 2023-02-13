521c45cf97cc0a7c93447fc0647ee043631fa050
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-12 20:32:58 -0500

    Time: 4 ms (48.36%), Space: 2.8 MB (44.26%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func flatten(root *TreeNode)  {
+    var dfs func(r *TreeNode)
+    dfs = func(r *TreeNode) {
+        // base
+        if r == nil {return}        
+        
+        // logic
+        dfs(r.Left)
+        if r.Left != nil  {
+            tmpRight := r.Right
+            r.Right = r.Left
+            r.Left = nil 
+            for r.Right != nil {
+                r = r.Right
+            }
+            r.Right = tmpRight
+        }
+        dfs(r.Right)
+    }
+    dfs(root)
+}
