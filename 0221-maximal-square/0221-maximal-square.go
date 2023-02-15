6a0b940ecc34aa9077d26ea747dddde004e53fcb
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-15 13:34:25 -0500

    Time: 13 ms (33.66%), Space: 6.6 MB (97.03%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.go
+++ 0221-maximal-square/0221-maximal-square.go
@@ -2,17 +2,18 @@ func maximalSquare(matrix [][]byte) int {
     m := len(matrix)
     n := len(matrix[0])
     maxSize := 0
-    dp := make([][]int, m+1)
-    for idx, _ := range dp {dp[idx] = make([]int, n+1)}
-    
-    for i := 1; i < len(dp); i++ {
-        for j := 1; j < len(dp[0]); j++ {
+    dp := make([]int, n+1)
+    diagUp := 0    
+    for i := 1; i < m+1; i++ {
+        for j := 1; j < n+1; j++ {
+            newDiagUp := dp[j]
             if matrix[i-1][j-1] == '1' {
-                dp[i][j] = int(math.Min(float64(dp[i-1][j-1]), math.Min(float64(dp[i][j-1]), float64(dp[i-1][j]))))+1
-                if dp[i][j] > maxSize {
-                    maxSize = dp[i][j]
-                }
+                dp[j] = int(math.Min(float64(dp[j-1]), math.Min(float64(dp[j]), float64(diagUp))))+1
+            } else {
+                dp[j] = 0
             }
+            if dp[j] > maxSize {maxSize = dp[j]}
+            diagUp = newDiagUp
         }
     }
     return maxSize*maxSize
