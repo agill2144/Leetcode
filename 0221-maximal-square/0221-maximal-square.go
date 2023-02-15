de67f13f5579cf3daee482f81d3605d18e9fe135
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-15 11:54:20 -0500

    Time: 7 ms (81.19%), Space: 6.6 MB (38.61%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.go
+++ 0221-maximal-square/0221-maximal-square.go
@@ -1,19 +1,20 @@
-
 func maximalSquare(matrix [][]byte) int {
     m := len(matrix)
     n := len(matrix[0])
     dp := make([][]int, m+1)
-    for i := 0; i < m+1; i++ {dp[i] = make([]int, n+1)}
-    max := 0 
-    for i := 1; i < len(dp); i++ {
+    for i := 0; i < len(dp); i++ {dp[i] = make([]int, n+1)}
+    
+    max := 0
+    for i := 1; i < len(dp); i++{
         for j := 1; j < len(dp[0]); j++ {
             if matrix[i-1][j-1] == '1' {
-                dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j],dp[i][j-1]))+1
+                dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))+1
                 if dp[i][j] > max {max = dp[i][j]}
             }
         }
     }
     return max*max
+
 }
 
 func min(x, y int) int {
