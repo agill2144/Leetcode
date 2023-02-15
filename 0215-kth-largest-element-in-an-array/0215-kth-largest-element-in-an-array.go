a5b77f8db25c50fab57a48c4d3896837fee744e8
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-14 21:09:09 -0500

    Time: 85 ms (72.91%), Space: 8.5 MB (64.25%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
@@ -1,33 +1,24 @@
 func findKthLargest(nums []int, k int) int {
-    if k <= 0 || k > len(nums) {return -1} 
-    mn := &minHeap{items: []int{}}
-    for i := 0; i < len(nums); i++ {
-        heap.Push(mn, nums[i])
-        if mn.Len() > k {
-            heap.Pop(mn)
+    left := 0
+    right := len(nums)-1
+    for left <= right {
+        ns := left
+        pivotIdx := right
+        for i := left; i < right; i++ {
+            if nums[i] < nums[pivotIdx] {
+                nums[ns], nums[i] = nums[i], nums[ns]
+                ns++
+            }
+        }
+        
+        nums[ns], nums[pivotIdx] = nums[pivotIdx], nums[ns]
+        if ns == len(nums)-k {
+            return nums[ns]
+        } else if len(nums)-k > ns {
+            left = ns+1
+        } else {
+            right = ns-1
         }
     }
-    return mn.items[0]
-}
-
-
-/*
-    container/heap.go
-    
-    type Interface interface {
-        sort.Interface
-        Pop() interface{}
-        Push(x interface{})
-    }
-    
-*/
-type minHeap struct { items []int }
-func (m *minHeap) Less(i, j int) bool {return m.items[i] < m.items[j]}
-func (m *minHeap) Len() int {return len(m.items)}
-func (m *minHeap) Swap(i, j int) {m.items[i],m.items[j] = m.items[j], m.items[i]}
-func (m *minHeap) Push(x interface{}) {m.items = append(m.items, x.(int))}
-func (m *minHeap) Pop() interface{} {
-    out := m.items[len(m.items)-1]
-    m.items = m.items[:len(m.items)-1]
-    return out
+    return -1
 }
