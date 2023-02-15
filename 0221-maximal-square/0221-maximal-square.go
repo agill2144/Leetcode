2edeee246c5f2ae18693ad806c37820e112af228
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-15 13:25:32 -0500

    Time: 2404 ms (5.94%), Space: 6.6 MB (97.03%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.go
+++ 0221-maximal-square/0221-maximal-square.go
@@ -1,28 +1,35 @@
 func maximalSquare(matrix [][]byte) int {
     m := len(matrix)
     n := len(matrix[0])
-    dp := make([]int, n+1)
-    
-    diagUp := 0
-    max := 0
-    for i := 1; i < m+1; i++{
-        for j := 1; j < n+1; j++ {
-            nextDiagUp := dp[j]
-            if matrix[i-1][j-1] == '1' {
-                dp[j] = min(diagUp, min(dp[j], dp[j-1]))+1
-                if dp[j] > max {max = dp[j]}
-            } else {
-                dp[j] = 0
+    maxSize := 0
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if matrix[i][j] == '1' {
+                size := 1
+                flag := true
+                for i+size < m && j + size < n && flag {
+                    
+                    // go up in rows
+                    for k := i+size; k>= i; k-- {
+                        if matrix[k][j+size] != '1'{
+                            flag = false
+                            break
+                        } 
+                    }
+                    
+                    // go left in col
+                    for k := j+size; k >= j; k-- {
+                        if matrix[i+size][k] != '1' {
+                            flag = false
+                            break
+                        }
+                    }
+                    
+                    if flag {size++}
+                }
+                if size > maxSize {maxSize = size}
             }
-            diagUp = nextDiagUp
-
         }
     }
-    return max*max
-
-}
-
-func min(x, y int) int {
-    if x < y {return x}
-    return y
+    return maxSize*maxSize
 }
