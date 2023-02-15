586d999b86b078873df25a5d74e2b4a3500264e1
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-15 17:25:41 -0500

    Time: 3 ms (100.00%), Space: 3.9 MB (95.07%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.go
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.go
@@ -1,14 +1,15 @@
 func calculate(s string) int {
-    calc := 0
-    curr := 0
-    var sign byte = '+'
     tail := 0
+    var sign byte = '+'
+    curr := 0
+    calc := 0
+    
     for i := 0; i < len(s); i++ {
         char := s[i]
         if char >= '0' && char <= '9' {
             curr = curr * 10 + int(char-'0')
         }
-        if char == '+' || char == '-' || char == '*' || char == '/' || i == len(s)-1 {
+        if char == '+' || char == '-' || char == '/' || char == '*' || i == len(s)-1 {
             if sign == '+' {
                 calc += curr
                 tail = curr
@@ -16,14 +17,14 @@ func calculate(s string) int {
                 calc -= curr
                 tail = -curr
             } else if sign == '*' {
-                calc = calc - tail + (tail*curr)
+                calc = calc - tail + (tail * curr)
                 tail = tail * curr
             } else if sign == '/' {
                 calc = calc - tail + (tail / curr)
                 tail = tail / curr
             }
-            curr = 0
             sign = char
+            curr = 0
         }
     }
     return calc
