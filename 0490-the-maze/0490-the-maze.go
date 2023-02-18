dfe4a95f2f3a1ba3cab200efbeec704ed834bf12
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-18 16:35:26 -0500

    Time: 27 ms (93.33%), Space: 6.7 MB (93.33%) - LeetHub

--- 0490-the-maze/0490-the-maze.go
+++ 0490-the-maze/0490-the-maze.go
@@ -1,3 +1,4 @@
+// time: o(mn)
 func hasPath(maze [][]int, start []int, destination []int) bool {
     dr := destination[0]
     dc := destination[1]
@@ -8,29 +9,27 @@ func hasPath(maze [][]int, start []int, destination []int) bool {
     n := len(maze[0])
     
     dirs := [][]int{{-1,0},{0,1},{1,0},{0,-1}}
-    q := [][]int{{sr,sc}}
-    
-    for len(q) != 0 {
-        dq := q[0]
-        q = q[1:]
-        cr := dq[0]
-        cc := dq[1]
-    
+    var dfs func(r, c int) bool
+    dfs = func(r, c int) bool {
+        // base
+        if maze[r][c] == 2 {return false}
+        if r == dr && c == dc {return true}
+        
+        // logic
+        maze[r][c] = 2
         for _ , dir := range dirs {
-            r := cr + dir[0]
-            c := cc + dir[1]
-            for r < m && r >= 0 && c < n && c >= 0 && maze[r][c] != 1 {
-                r += dir[0]
-                c += dir[1]
+            nr := r + dir[0]
+            nc := c + dir[1]
+            for nr < m && nr >= 0 && nc < n && nc >= 0 && maze[nr][nc] != 1 {
+                nr += dir[0]
+                nc += dir[1]
             }
-            r -= dir[0]
-            c -= dir[1]
-            if maze[r][c] != 2 {
-                maze[r][c] = 2
-                if r == dr && c == dc {return true}
-                q = append(q, []int{r,c})
-            }
+            nr -= dir[0]
+            nc -= dir[1]
+            if ok := dfs(nr, nc); ok {return true}
         }
+        
+        return false
     }
-    return false
+    return dfs(sr, sc)
 }
