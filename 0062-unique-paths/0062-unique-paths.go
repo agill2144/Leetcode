acde377413728c325264e8b3bdfa89fde18c5611
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-18 17:53:43 -0500

    Time: 1 ms (62.74%), Space: 2 MB (29.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.go
+++ 0062-unique-paths/0062-unique-paths.go
@@ -1,27 +1,53 @@
 /*
+    approach: top down with memoization
+*/
+func uniquePaths(m int, n int) int {
+    dr := m-1
+    dc := n-1
+    memo := make([][]int, m+1)
+    for i := 0; i < m+1; i++ { memo[i] = make([]int, n+1)}
+    
+    var dfs func(r, c int) int
+    dfs = func(r, c int) int {
+        // base
+        if r == dr && c == dc {
+            return 1
+        }
+        if r == m || c == n {return 0}
+
+        if memo[r][c+1] == 0 { memo[r][c+1] = dfs(r,c+1) }
+        if memo[r+1][c] == 0 { memo[r+1][c] = dfs(r+1,c) }
+        
+        return memo[r+1][c] + memo[r][c+1]
+    }
+    return dfs(0,0)
+}
+
+
+/*
     approach: bottom up dp with space optimzation
     - transpose the matrix such that 0,0 is the exit point and solve bottom up
     
     time: o(mn)
-    space: o(mn)
+    space: o(n)
 */
-func uniquePaths(m int, n int) int {
-    dp := make([]int, n)
+// func uniquePaths(m int, n int) int {
+//     dp := make([]int, n)
     
-    for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
-            if i == 0 {dp[j] = 1; continue}
-            top := dp[j]
-            left := 0
-            if j != 0 {
-                left = dp[j-1]
-            }
-            dp[j] = top+left
-        }        
-    }
-    return dp[n-1]
+//     for i := 0; i < m; i++ {
+//         for j := 0; j < n; j++ {
+//             if i == 0 {dp[j] = 1; continue}
+//             top := dp[j]
+//             left := 0
+//             if j != 0 {
+//                 left = dp[j-1]
+//             }
+//             dp[j] = top+left
+//         }        
+//     }
+//     return dp[n-1]
     
-}
+// }
 
 /*
     approach: bottom up dp 
