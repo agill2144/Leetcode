2fe5600b62b86d4d8187152bd22c836ca8668a3c
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-19 18:38:21 -0500

    Time: 2 ms (69.68%), Space: 2.1 MB (74.52%) - LeetHub

--- 0139-word-break/0139-word-break.go
+++ 0139-word-break/0139-word-break.go
@@ -1,19 +1,13 @@
-
-/*
-    approach: bottom up dp
-    - at any char, find out whether the string before this char can be split into valid splits or not
-*/
 func wordBreak(s string, wordDict []string) bool {
+    set := map[string]struct{}{}
+    for i := 0; i < len(wordDict); i++ { set[wordDict[i]] = struct{}{} }
     dp := make([]bool, len(s)+1)
     dp[0] = true
-    root := &trieNode{childs: [26]*trieNode{}}
-    for i := 0; i < len(wordDict); i++ { insertWord(root, wordDict[i])}
-    
-    for i := 1; i < len(dp); i++ {
+    for i := 0; i < len(dp); i++ {
         for j := 0; j < i; j++ {
             if dp[j] {
                 subStr := string(s[j:i])
-                if search(root, subStr) {
+                if _, ok := set[subStr]; ok {
                     dp[i] = true
                     break
                 }
@@ -23,22 +17,41 @@ func wordBreak(s string, wordDict []string) bool {
     return dp[len(dp)-1]
 }
 
-
-
 /*
-    approach: brute force
-    - find a partition path that works
-    - using for loop recursion and pivot, form each substr
-    - if a substr exists in wordDict, recurse with the remaining words
-    - keep going until our pivot goes out of bounds
-    - when pivot goes out of bounds, it means we were able to find substrs that do exists in the wordDict
+    we have to split s into substrs such that each splitted substr exists in wordDict
+    
+    approach: brute force 
+    - for loop based dfs
+    - explore all possible splits
+    - using a start / pivot ptr, make a substr
+    - once a substr is found, then we know for sure this is 1 valid split
+    - now go explore the rest of the string ( start = i + 1 ); recursively
+    - once we put pivot out of bounds, it means that we were able to find substrings that exists in wordDict.
         - return true
-    - otherwise at the end return false
+    - otherwise return false
+    s = len(s)
+    
+    to make wordDict searchable; we have 2 choices
+    1. hashset 
+    2. trie ( saves more memory compared to hashset because it uses same characters for repeating characters in a path )
+
+
+    why recursion? why cant we just use fast and slow ptr
+    - make each substr [slow:fast+1]
+    - if substr exists, move slow ptr to fast+1
+    - if slow ptr eventually reaches the end and goes out of bounds, it means we have all substrs that exists in wordDict.
+    - However this does not for all test cases
+    - s = "aaaaaaa", wordDict = ["aaaa","aaa"]
+        - we will find "aaa" first
+        - then we will another "aaa"
+        - then we wont be able to find the last "a"
+        - because the above didnt work, we have to be exhaustive and try other substrs
+        - "aaaa" can be formed at the end
+        - therefore inorder to be exhaustive, we have to use a for loop based recursion and try all possible sub-strings
 */
 // func wordBreak(s string, wordDict []string) bool {
-//     root := &trieNode{childs: [26]*trieNode{}}
-//     for i := 0; i < len(wordDict); i++ { insertWord(root, wordDict[i])}
-    
+//     set := map[string]struct{}{}
+//     for i := 0; i < len(wordDict); i++ { set[wordDict[i]] = struct{}{} }
 //     var dfs func(start int) bool
 //     dfs = func(start int) bool {
 //         // base
@@ -47,39 +60,13 @@ func wordBreak(s string, wordDict []string) bool {
 //         // logic
 //         for i := start; i < len(s); i++ {
 //             subStr := string(s[start:i+1])
-//             if search(root, subStr) {
-//                 if yes := dfs(i+1); yes {return true}
+//             if _, ok := set[subStr]; ok {
+//                 if yes := dfs(i+1); yes {
+//                     return true
+//                 }
 //             }
 //         }
 //         return false
 //     }
 //     return dfs(0)
 // }
-
-
-type trieNode struct {
-    childs [26]*trieNode
-    isEnd bool
-}
-
-func insertWord(root *trieNode, word string)  {
-    curr := root
-    for i := 0; i < len(word); i++ {
-        idx := word[i]-'a'
-        if curr.childs[idx] == nil {
-            curr.childs[idx] = &trieNode{childs: [26]*trieNode{}}
-        }
-        curr = curr.childs[idx]
-    }
-    curr.isEnd = true
-}
-
-func search(r *trieNode, word string) bool {
-    curr := r
-    for i := 0; i < len(word); i++ {
-        idx := word[i]-'a'
-        if curr.childs[idx] == nil { return false }
-        curr = curr.childs[idx]
-    }
-    return curr.isEnd
-}
