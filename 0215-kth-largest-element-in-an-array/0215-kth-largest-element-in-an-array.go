ff82c563a9a12848f9e88ae2e41e2b5af8d4303d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-22 20:04:11 -0500

    Time: 88 ms (62.17%), Space: 8.3 MB (91.80%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
@@ -1,21 +1,23 @@
 func findKthLargest(nums []int, k int) int {
-    mh := &minHeap{items: []int{}}
-    for i := 0; i < len(nums); i++ {
-        heap.Push(mh, nums[i])
-        if mh.Len() > k {
-            heap.Pop(mh)
+    left := 0
+    right := len(nums)-1
+    for left <= right {
+        pivotIdx := right
+        nextSmaller := left
+        for i := left; i < pivotIdx; i++ {
+            if nums[i] < nums[pivotIdx] {
+                nums[i], nums[nextSmaller] = nums[nextSmaller], nums[i]
+                nextSmaller++
+            }
+        }
+        nums[pivotIdx], nums[nextSmaller] = nums[nextSmaller], nums[pivotIdx]
+        if nextSmaller == len(nums)-k {
+            return nums[nextSmaller]
+        } else if len(nums)-k > nextSmaller {
+            left = nextSmaller+1
+        } else {
+            right = nextSmaller-1
         }
     }
-    return mh.items[0]
-}
-
-type minHeap struct{items []int}
-func(m *minHeap) Len() int {return len(m.items)}
-func(m *minHeap) Less(i,j int) bool {return m.items[i] < m.items[j]}
-func(m *minHeap) Swap(i,j int) {m.items[i], m.items[j] = m.items[j], m.items[i]}
-func(m *minHeap) Push(x interface{}) { m.items = append(m.items, x.(int))}
-func(m *minHeap) Pop() interface{} {
-    out := m.items[len(m.items)-1]
-    m.items = m.items[:len(m.items)-1]
-    return out
+    return -1
 }
