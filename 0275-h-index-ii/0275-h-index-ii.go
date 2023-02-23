2cd957900a532ca10196637b80d28709174d358d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-23 01:26:48 -0500

    Time: 18 ms (67.50%), Space: 7 MB (7.50%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.go
+++ 0275-h-index-ii/0275-h-index-ii.go
@@ -1,13 +1,17 @@
 func hIndex(citations []int) int {
-    left := 0
-    right := len(citations)-1
+    // we are looking for an index where len(arr)-idx >= citations[idx]
+    // we are looking for an h index where h of the papers have >= h citations
+    // we are looking for the first trip because because beyond that trip , all papers are going to have > h citations
+    // because this is sorted array
     n := len(citations)
+    left := 0
+    right := n-1
     hIdx := 0
     for left <= right {
         mid := left + (right-left)/2
-        tmp := n-mid
-        if citations[mid] >= tmp {
-            hIdx = tmp
+        diff := n-mid
+        if citations[mid] >= diff {
+            hIdx = diff
             right = mid-1
         } else {
             left = mid+1
