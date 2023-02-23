3ed9a35c182ea43547db5f23f3a9290062ead8ea
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-23 11:34:11 -0500

    Time: 21 ms (45.00%), Space: 7 MB (7.50%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.go
+++ 0275-h-index-ii/0275-h-index-ii.go
@@ -1,21 +1,25 @@
+/*
+    approach: linear scan
+    - hIdx for an ith position is basically how many papers have >= citations[i]
+    - example
+        - [0,1,3,5,6]
+        - i = 0; how many papers have >= 0 citations ; 5
+        - i = 1; how many papers have >= 1 citations ; 4
+        - i = 2; how many papers have >= 3 citations ; 3
+        - i = 3; how many papers have >= 5 citations ; 2
+        - i = 4; how many papers have >= 6 citations ; 1
+        - for an ith position how many papers are to the right side of it ( including itself )
+        - that is; len(citations)-i
+    - Now we can get hIdx for each ith position we need to find where h >= h and n-h <= h is met
+    - linear scan from left to right while calculating hIdx for each ith position
+    - as soon as our condition is met, we can break and return the hIdx for that ith position
+    
+*/
 func hIndex(citations []int) int {
-    // we are looking for an index where len(arr)-idx >= citations[idx]
-    // we are looking for an h index where h of the papers have >= h citations
-    // we are looking for the first trip because because beyond that trip , all papers are going to have > h citations
-    // because this is sorted array
     n := len(citations)
-    left := 0
-    right := n-1
-    hIdx := 0
-    for left <= right {
-        mid := left + (right-left)/2
-        diff := n-mid
-        if citations[mid] >= diff {
-            hIdx = diff
-            right = mid-1
-        } else {
-            left = mid+1
-        }
+    for i := 0; i < n; i++ {
+        h := n-i
+        if h <= citations[i] {return h} 
     }
-    return hIdx
+    return 0
 }
