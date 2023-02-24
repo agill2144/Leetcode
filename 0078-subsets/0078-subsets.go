a241e387de3ad71d732d1c02d27f499c604eaf3e
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-24 15:36:56 -0500

    Time: 2 ms (51.64%), Space: 2.4 MB (13.32%) - LeetHub

--- 0078-subsets/0078-subsets.go
+++ 0078-subsets/0078-subsets.go
@@ -1,3 +1,43 @@
+/*
+    approach: backtracking and for-loop recursion
+    time: o(2^n) x o(n)
+        - the extra o(n) is from creating a deep copy of path in each recursion
+    space: o(n) from recursion stack + o(n) for each path at each recursion
+*/
+// func subsets(nums []int) [][]int {
+//     out := [][]int{}
+//     var dfs func(start int, path []int)
+//     dfs = func(start int, path []int) {
+//         // base
+            
+//         newL := make([]int, len(path))
+//         copy(newL, path)
+//         out = append(out, newL)
+        
+//         // logic
+//         for i := start; i < len(nums); i++ {
+//             path = append(path, nums[i])
+//             dfs(i+1,path)
+//             path = path[:len(path)-1]
+//         }
+//     }
+//     dfs(0, nil)
+//     return out
+// }
+
+/*
+    approach: make a copy of list each time and append nums[i] to it.
+    - start with out array with an empty array { {} }
+    - then for each ith number, loop over all the lists we have so far in output array
+    - make a deep copy of each list
+    - add ith number
+    - save this new list to output array 
+    time: o(2^n) x o(n) 
+        - the largest nested loop will be for the last ith number in nums array
+        - that loop will run o(2^n-1) iterations
+        - the extra o(n) is for deep copy
+    space: o(2^n-1) ; the last iteration will create a newL of size 2^n-1, and this will be largest aux array.
+*/
 func subsets(nums []int) [][]int {
     out := [][]int{{}}
     for i := 0; i < len(nums); i++ {
