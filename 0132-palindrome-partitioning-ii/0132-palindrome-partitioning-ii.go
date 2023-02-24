32ffa197f1a0089d8c53cf6fd34db145d865d728
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-24 18:45:19 -0500

    Time: 115 ms (61.76%), Space: 2 MB (82.35%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.go
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.go
@@ -5,12 +5,11 @@
 */
 func minCut(s string) int {
     dp := make([]int, len(s))
-    palindromeCache := map[string]bool{}
     dp[0] = 0
     for i := 1; i < len(dp); i++ {dp[i] = math.MaxInt64}
     for i := 1; i < len(s); i++ {
         for j := i; j >= 0; j-- {
-            if isPalindrome(s, j, i, palindromeCache) {
+            if isPalindrome(s, j, i) {
                 if j == 0 {dp[i] = 0; break}
                 dp[i] = min(dp[i], dp[j-1]+1)
             }
@@ -22,13 +21,12 @@ func min(x, y int) int {
     if x < y {return x}
     return y
 }
-func isPalindrome(s string, left, right int, palindromeCache map[string]bool) bool {
-    if val, ok := palindromeCache[string(s[left:right+1])]; ok {return val}
+func isPalindrome(s string, left, right int) bool {
     for left < right {
-        if s[left] != s[right] {palindromeCache[string(s[left:right+1])]=false; return false}
+        if s[left] != s[right] {return false}
         left++
         right--
     }
-    palindromeCache[string(s[left:right+1])] = true
+    
     return true
 }
