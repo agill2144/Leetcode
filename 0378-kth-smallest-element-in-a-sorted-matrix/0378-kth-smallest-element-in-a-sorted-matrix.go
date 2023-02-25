9dd6f5412353a5a5017ae24160e5a6e618fd692c
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-25 15:51:46 -0500

    Time: 22 ms (92.59%), Space: 7.1 MB (57.41%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.go
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.go
@@ -41,16 +41,21 @@ func kthSmallest(matrix [][]int, k int) int {
     return kth
 }
 
-func countLessThanOrEqualTo(matrix [][]int, target int) int {
+// use stair case search to find count <= target
+func countLessThanOrEqualTo(matrix [][]int,target int) int {
+    m := len(matrix)
+    n := len(matrix[0])
     count := 0
-    m := len(matrix); n := len(matrix[0])
     r := m-1
     c := 0
     for r >= 0 && c < n {
         if matrix[r][c] <= target {
+            // all elements above this cell are less than or equal to target
+            // therefore add all elements in this col
             count += r+1
+            // and eval the next col
             c++
-        } else{
+        } else {
             r--
         }
     }
