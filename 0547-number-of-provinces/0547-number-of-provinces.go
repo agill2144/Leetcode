d040091885df60bda80499670d32d40cdeb591c0
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-28 17:24:58 -0500

    Time: 21 ms (74.89%), Space: 7.2 MB (6.61%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.go
+++ 0547-number-of-provinces/0547-number-of-provinces.go
@@ -1,33 +1,46 @@
+/*
+    approach: dfs
+    - find number of connected components in a undirected graph
+    - given graphs that are disconnected 
+    - count number of graphs
+    - the input is a adj matrix
+        - in adj matrix, i<->j is an edge if at matrix[i][j] = 1
+        - because its undirected, we have to add the other direction too, therefore matrix[j][i] will also be 1
+        - there will be nodes in the adjMatrix, who do not have an edge to anyone, and thats a valid graph too
+            - these are called self nodes where i == j
+*/
 func findCircleNum(isConnected [][]int) int {
     adjList := map[int][]int{}
     n := len(isConnected)
-    
     for i := 0; i < n; i++ {
         for j := 0; j < n; j++ {
-            if isConnected[i][j] == 1 && i != j {
+            if isConnected[i][j] == 1 && i != j { 
+                // add both edges because this is an undirected graph
                 adjList[i] = append(adjList[i], j)
                 adjList[j] = append(adjList[j], i)
+                
             }
         }
     }
+    
     visited := map[int]struct{}{}
-    var dfs func(node int) 
+    var dfs func(node int)
     dfs = func(node int) {
         // base
-        if _, ok := visited[node]; ok { return }
+        if _, ok := visited[node]; ok {return}
         
         // logic
         visited[node] = struct{}{}
-        for _, edge := range adjList[node] {
-            if _, ok := visited[edge]; !ok {dfs(edge)}
+        for _, x := range adjList[node] {
+            dfs(x)
         }
     }
     
     count := 0
     for i := 0; i < n; i++ {
         if _, ok := visited[i]; !ok {
-            count++
             dfs(i)
+            count++
         }
     }
     return count
