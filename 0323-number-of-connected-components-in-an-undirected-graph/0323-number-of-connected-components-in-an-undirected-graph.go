c508ec2aeb98c4f9829f795cfaff5e441d9592e4
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-28 16:52:04 -0500

    Time: 8 ms (84.96%), Space: 6.9 MB (23.89%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.go
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.go
@@ -1,21 +1,49 @@
+/*
+    approach: dfs
+    - we are simply counting the number of graphs
+    - However these are undirected graph
+    - meaning we will need to add edges both ways ( the input does not have that )
+    - we will maintain a visited set globally to keep track of already explored nodes
+        - this will help us avoid same node exploration because of undirected graph
+    - One caveat is since we are counting number of components, this means running 1 dfs on a starting node
+        will likely not traverse the rest of the disconnected components.
+    - There we need to run dfs on ALL nodes
+    - which means we need to know the range of nodes
+    - and the questions says: You have a graph of n nodes
+    - its from 0 to n-1
+    - therefore dfs from 0 to n(excluding n)
+    - we need a adjList because edges input array is not enough to look up a node neighbors...
+    
+    time: 
+*/
 func countComponents(n int, edges [][]int) int {
+    // create adjList from edges list
     adjList := map[int][]int{}
     for i := 0; i < len(edges); i++ {
+        // 2 way connection because this is undirected graph!
         adjList[edges[i][0]] = append(adjList[edges[i][0]], edges[i][1])
-        adjList[edges[i][1]] = append(adjList[edges[i][1]], edges[i][0])
+        adjList[edges[i][1]] = append(adjList[edges[i][1]], edges[i][0])        
     }
+    
+    // visited set to keep track of visited nodes
     visited := map[int]struct{}{}
+    
     var dfs func(node int)
     dfs = func(node int) {
         // base
+        // if node is already visited, return and exit early
         if _, ok := visited[node]; ok {return}
         
         // logic
+        // visit this node
         visited[node] = struct{}{}
+        // explore its neighbors
         for _, edge := range adjList[node] {
             dfs(edge)
         }
     }
+    
+    // now run dfs on all the nodes
     count := 0
     for i := 0; i < n; i++ {
         if _, ok := visited[i]; !ok {
