05921da931af21aa47e71e373ccc16f31af4dc36
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-01 02:29:53 -0500

    Time: 10 ms (62.73%), Space: 6.7 MB (6.36%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.go
+++ 0261-graph-valid-tree/0261-graph-valid-tree.go
@@ -1,27 +1,24 @@
 func validTree(n int, edges [][]int) bool {
-    if n  == 0 {return true}
     adjList := map[int][]int{}
     for i := 0; i < len(edges); i++ {
-        n1 := edges[i][0]
-        n2 := edges[i][1]
-        adjList[n1] = append(adjList[n1], n2)
-        adjList[n2] = append(adjList[n2], n1)
+        adjList[edges[i][0]] = append(adjList[edges[i][0]], edges[i][1]) 
+        adjList[edges[i][1]] = append(adjList[edges[i][1]], edges[i][0]) 
     }
     visited := map[int]struct{}{}
-    var dfs func(node int, prev int) bool 
-    dfs = func(node, prev int) bool {
+    var dfs func(node int, parent int) bool
+    dfs = func(node, parent int) bool {
         // base
         if _, ok := visited[node]; ok {return false}
         
         // logic
         visited[node] = struct{}{}
         for _, child := range adjList[node] {
-            if child == prev {continue}
-            if ok := dfs(child, node); !ok {return false}
+            if child == parent {continue}
+            if ok := dfs(child, node); !ok {return false} 
         }
         return true
     }
-    ok := dfs(0, -1)
-    if !ok {return false}
+    out := dfs(0, -1)
+    if !out {return false}
     return len(visited) == n
 }
