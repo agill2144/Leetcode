8a825f88a87be2dbac63cf5166e21e7eeda2b7a6
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-03 00:37:58 -0500

    Time: 470 ms, Memory: 91.7 MB - LeetHub

--- find-if-path-exists-in-graph/find-if-path-exists-in-graph.go
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.go
@@ -1,58 +1,25 @@
 func validPath(n int, edges [][]int, source int, destination int) bool {
     adjList := map[int][]int{}
     for i := 0; i < len(edges); i++ {
-        src := edges[i][0]
-        dest := edges[i][1]
-        adjList[src] = append(adjList[src], dest)
-        adjList[dest] = append(adjList[dest], src)
-
+        adjList[edges[i][0]] = append(adjList[edges[i][0]], edges[i][1])
+        adjList[edges[i][1]] = append(adjList[edges[i][1]], edges[i][0])
     }
     visited := map[int]struct{}{}
-    var dfs func(start int) bool
-    dfs = func(start int) bool {
+
+    var dfs func(node int, prev int) bool
+    dfs = func(node int, prev int) bool {
         // base
-        if start == destination { return true }
-        if _, ok := visited[start]; ok {return false}
+        if node == destination {return true}
+        if _, ok := visited[node]; ok {return false }
         
         // logic
-        visited[start] = struct{}{}
-        for _, edge := range adjList[start] {
-            if ok := dfs(edge); ok {return true}
+        visited[node] = struct{}{}
+        for _, neighbor := range adjList[node] {
+            if neighbor == prev {continue}
+            found := dfs(neighbor, node)
+            if found {return true}
         }
         return false
     }
-    return dfs(source)
-    
+    return dfs(source, -1)
 }
-
-// func validPath(n int, edges [][]int, source int, destination int) bool {
-//     adjList := map[int][]int{}
-//     for i := 0; i < len(edges); i++ {
-//         src := edges[i][0]
-//         dest := edges[i][1]
-//         adjList[src] = append(adjList[src], dest)
-//         adjList[dest] = append(adjList[dest], src)
-
-//     }
-//     memo := map[int]bool{}
-//     visited := map[int]struct{}{}
-//     var dfs func(start int) bool
-//     dfs = func(start int) bool {
-//         // base
-//         if start == destination { return true }
-//         if _, ok := visited[start]; ok {return false}
-        
-//         // logic
-//         if _, ok := memo[start]; !ok {
-//             visited[start] = struct{}{}
-//             for _, edge := range adjList[start] {
-//                 if ok := dfs(edge); ok {memo[start]=true; return true}
-//             }
-//             delete(visited, start)
-//             memo[start] = false
-//         }
-//         return false
-//     }
-//     return dfs(source)
-    
-// }
