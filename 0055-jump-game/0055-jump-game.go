83d676cbe35d40a3ce2f587ef5718a484bd9265b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-04 22:51:49 -0500

    Time: 65 ms (49.38%), Space: 7.1 MB (35.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.go
+++ 0055-jump-game/0055-jump-game.go
@@ -1,51 +1,12 @@
 func canJump(nums []int) bool {
-    canJumpToThisIdx := nums[0]
-    for i := 1; i <= canJumpToThisIdx; i++ {
-        if canJumpToThisIdx >= len(nums)-1 {return true}
-        jumpDist := i+nums[i]
-        if jumpDist > canJumpToThisIdx {canJumpToThisIdx = jumpDist}
+    if len(nums) <= 1 {return true}
+    if nums[0] == 0 {return false}
+    
+    target := len(nums)-1
+    for i := len(nums)-2; i >= 0; i-- {
+        if i+nums[i] >= target {
+            target = i
+        }
     }
-    return canJumpToThisIdx >= len(nums)-1
+    return target == 0 
 }
-
-func max(x, y int) int {
-    if x >= y {return x}
-    return y
-}
-
-// func canJump(nums []int) bool {
-//     dp := make([]bool, len(nums))
-//     dp[len(dp)-1] = true
-//     for i := len(nums)-2; i >= 0; i-- {
-//         for j := nums[i]; j>= 1; j-- {
-//             if (j+i == len(nums)-1) || (j+i < len(nums) && dp[j+i]) {
-//                 dp[i] = true
-//                 break
-//             }
-//         }
-//     }
-//     return dp[0]
-// }
-
-// func canJump(nums []int) bool {
-//     memo := map[int]bool{}
-//     var dfs func(start int) bool
-//     dfs = func(start int) bool {
-//         // base
-//         if start >= len(nums)-1 {
-//             if start == len(nums)-1 {return true}
-//             return false
-//         }
-        
-//         // logic
-//         if _, ok := memo[start]; !ok {
-//             numJumps := nums[start]
-//             for j := numJumps; j >= 1; j-- {
-//                 if ok := dfs(start+j); ok {memo[start] = true; return true}
-//             }
-//             memo[start] = false
-//         }
-//         return memo[start]
-//     }
-//     return dfs(0)
-// }
