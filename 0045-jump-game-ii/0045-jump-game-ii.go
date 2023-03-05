49c4d1c3cf03bd3f2a8ec1b34f96c276c109f57d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-04 19:39:03 -0500

    Time: 82 ms (15.68%), Space: 6.5 MB (10.57%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.go
+++ 0045-jump-game-ii/0045-jump-game-ii.go
@@ -1,7 +1,30 @@
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        dp = [float('inf')] * len(nums)
-        dp[-1] = 0
-        for i in range(len(nums) - 2, -1, -1):
-            dp[i] = min(dp[i:(i + nums[i] + 1)]) + 1
-        return dp[0]
+/*
+    bottom up dp
+    time: o(n) + o(n*j)
+    space: o(n)
+*/
+func jump(nums []int) int {
+    dp := make([]int, len(nums))
+    
+    // fill it with Inf value
+    for i := 0; i < len(dp); i++ {dp[i] = math.MaxInt64-100}
+    dp[len(dp)-1] = 0
+    
+    for i := len(nums)-2; i>=0;  i-- {
+        jumpDist := nums[i]
+        if jumpDist == 0 {continue}
+        if i+jumpDist >= len(nums) {dp[i] = 1; continue}
+        for j := jumpDist; j >= 1; j-- {
+            if i+j < len(nums) {
+                dp[i] = min(dp[i], 1+dp[i+j])
+            }
+        }
+    }
+    
+    return dp[0]
+}
+
+func min(x, y int) int {
+    if x < y {return x}
+    return y
+}
