9c32e9d84b1bbf4fcb12453c11a5b2e6d967c46b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-05 23:47:53 -0500

    Time: 73 ms (62.30%), Space: 12.1 MB (55.74%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.go
+++ 1020-number-of-enclaves/1020-number-of-enclaves.go
@@ -15,8 +15,14 @@ func numEnclaves(grid [][]int) int {
             dfs(r+dir[0], c+dir[1])
         }
     }
-    for j := 0; j < n; j++ {dfs(0, j); dfs(m-1, j)}
-    for i := 0; i < m; i++ {dfs(i, 0); dfs(i, n-1)}
+    for j := 0; j < n; j++ {
+        if grid[0][j] == 1 {dfs(0, j)}
+        if grid[m-1][j] == 1 {dfs(m-1, j)}
+    }
+    for i := 0; i < m; i++ {
+        if grid[i][0] == 1 {dfs(i, 0)}
+        if grid[i][n-1] == 1 {dfs(i, n-1)}
+    }
     
     count := 0
     for i := 1; i < m; i++ {
