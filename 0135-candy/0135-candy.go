3ed51e693d8cedef9595a9ac6e591b365b4668d5
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-08 00:30:09 -0500

    Time: 12 ms (96.72%), Space: 6.4 MB (45.90%) - LeetHub

--- 0135-candy/0135-candy.go
+++ 0135-candy/0135-candy.go
@@ -1,21 +1,3 @@
-/*
-    approach: greedy
-    - First of all every child gets 1 candy ( regardless of their ratings compared with their neighboring childs )
-    - Then for each ith child we will check whether it has more ratings than its neighboring child ; i-1 and i+1
-        - HOWEVER, we have not calculated the final candies for the right child if we are looping from left to right
-        - THEREFORE we cannot compare an ith child with its right child
-    - So we will do this in 2 passes;
-        - Left to right loop, compare ith rating with i-1th rating
-        - if the i-1th rating is more, we KNOW FOR SURE, THAT THIS iTH CHILD GETS MORE THAN PREVIOUS CHILD
-        - THEREFORE CANDIES[i] = previous child candies + 1
-        - Now we can do the 2nd pass, in which we will compare ith child ratings with its right neighbor
-        - if ratings[i] is higher than the right neighbor, than this ith child SHOULD GET MORE CANDIES COMPARED TO ITS RIGHT NEIGHBOR
-        - However if candies[i] is already more than its right neighbor, than we are fine, no need to give more
-        - otherwise, candies[i] = candies[i+1]+1 ( 1 more than its right neihbor )
-    
-    time : o(2n)
-    space: o(n)
-*/
 func candy(ratings []int) int {
     candies := make([]int, len(ratings))
     candies[0] = 1
@@ -25,9 +7,8 @@ func candy(ratings []int) int {
             candies[i] = candies[i-1]+1
         }
     }
-    
     total := candies[len(candies)-1]
-    for i := len(candies)-2; i>=0 ;i-- {
+    for i := len(candies)-2; i >= 0; i-- {
         if ratings[i] > ratings[i+1] && candies[i] <= candies[i+1] {
             candies[i] = candies[i+1]+1
         }
