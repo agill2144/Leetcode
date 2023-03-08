efed6d87791aeef9b341bb77d8b0f7ae4c5d5eb2
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-08 01:28:23 -0500

    Time: 71 ms (17.72%), Space: 6.3 MB (18.69%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.go
+++ 0045-jump-game-ii/0045-jump-game-ii.go
@@ -1,34 +1,18 @@
-/*
-    bottom up dp
-    time: o(n) + o(n*j)
-    space: o(n)
-*/
 func jump(nums []int) int {
     dp := make([]int, len(nums))
-    
-    // fill it with Inf value
-    for i := 0; i < len(dp); i++ {dp[i] = math.MaxInt64-100}
+    for i := 0; i < len(dp); i++{dp[i] = math.MaxInt64-100}
     dp[len(dp)-1] = 0
-    
-    for i := len(nums)-2; i>=0;  i-- {
-        jumpDist := nums[i]
-        if jumpDist == 0 {continue}
-        
-        // if we can take the max jump and reach or go beyond (since we took max), 
-        // then number of jumps is 1, nothing is going to be < 1 jump
-        if i+jumpDist >= len(nums)-1 {dp[i] = 1; continue}
-        
-        // otherwise, explore all jumps
-        for j := jumpDist; j >= 1; j-- {
-            if i+j < len(nums) {
-                dp[i] = min(dp[i], 1+dp[i+j])
+
+    for i := len(nums)-1; i >= 0; i-- {
+        numJumps := nums[i]
+        for j := numJumps; j >= 1; j-- {
+            if j+i < len(nums) {
+                dp[i] = min(dp[i], dp[i+j]+1)
             }
         }
     }
-    
     return dp[0]
 }
-
 func min(x, y int) int {
     if x < y {return x}
     return y
