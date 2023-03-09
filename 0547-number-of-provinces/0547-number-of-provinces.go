6dea877605871bb6a046acdd66e40bb788fb51e3
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-09 01:19:21 -0500

    Time: 29 ms (13.91%), Space: 7.1 MB (11.30%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.go
+++ 0547-number-of-provinces/0547-number-of-provinces.go
@@ -1,47 +1,34 @@
-/*
-    approach: dfs
-    - find number of connected components in a undirected graph
-    - given graphs that are disconnected 
-    - count number of graphs
-    - the input is a adj matrix
-        - in adj matrix, i<->j is an edge if at matrix[i][j] = 1
-        - because its undirected, we have to add the other direction too, therefore matrix[j][i] will also be 1
-        - there will be nodes in the adjMatrix, who do not have an edge to anyone, and thats a valid graph too
-            - these are called self nodes where i == j
-*/
 func findCircleNum(isConnected [][]int) int {
     adjList := map[int][]int{}
     n := len(isConnected)
-    for i := 0; i < n; i++ {
+    visited := map[int]struct{}{}
+    for i := 0; i < n ; i++ {
         for j := 0; j < n; j++ {
-            if isConnected[i][j] == 1 && i != j { 
-                // add both edges because this is an undirected graph
+            if isConnected[i][j] == 1 && i != j {
                 adjList[i] = append(adjList[i], j)
-                adjList[j] = append(adjList[j], i)
+                adjList[j] = append(adjList[j], i)                
             }
         }
     }
     
-    visited := map[int]struct{}{}
-    var dfs func(node int)
-    dfs = func(node int) {
+    var dfs func(node, prev int) 
+    dfs = func(node, prev int) {
         // base
         if _, ok := visited[node]; ok {return}
+        visited[node] = struct{}{}
         
         // logic
-        visited[node] = struct{}{}
-        for _, x := range adjList[node] {
-            // optimization: no need to further recurse if x is already visited
-            if _, ok := visited[x]; ok {continue}
-            dfs(x)
+        for _, neighbor := range adjList[node] {
+            if neighbor == prev {continue}
+            dfs(neighbor, node)
         }
     }
     
     count := 0
     for i := 0; i < n; i++ {
         if _, ok := visited[i]; !ok {
-            dfs(i)
             count++
+            dfs(i, -1)
         }
     }
     return count
