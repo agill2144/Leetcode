0bdea9b9913f4b8f70e8344ce9ac551e3981ccfa
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-11 16:19:13 -0500

    Time: 2 ms (65.63%), Space: 2.2 MB (14.06%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.go
@@ -0,0 +1,15 @@
+func numberOfArithmeticSlices(nums []int) int {
+    count := 0
+    for i := 0; i < len(nums)-1; i++ {
+        diff := nums[i+1]-nums[i]
+        for j := i+2; j < len(nums); j++ {
+            if nums[j]-nums[j-1] != diff {
+                break
+            }
+            if j-i+1 >= 3{
+                count++
+            }
+        }
+    }
+    return count
+}
