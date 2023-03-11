96cda243b4d62acd7b5723f02a2d0eb9913602d5
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-11 16:40:09 -0500

    Time: 2 ms (65.63%), Space: 2.2 MB (14.06%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.go
+++ 0413-arithmetic-slices/0413-arithmetic-slices.go
@@ -1,17 +1,29 @@
+func numberOfArithmeticSlices(nums []int) int {
+    dp := make([]int, len(nums))
+    count := 0
+    for i := 2; i < len(nums); i++ {
+        if nums[i]-nums[i-1] == nums[i-1]-nums[i-2] {
+            dp[i] = dp[i-1]+1
+            count += dp[i]
+        }
+    }
+    return count
+}
+
 /*
     approach: brute force
     - form each subarray
     time: o(n^2)
     space: o(1)
 */
-func numberOfArithmeticSlices(nums []int) int {
-    count := 0
-    for i := 0; i < len(nums)-1; i++ {
-        diff := nums[i+1]-nums[i]
-        for j := i+2; j < len(nums); j++ {
-            if nums[j] - nums[j-1] != diff {break}
-            if j-i+1 >= 3 {count++}
-        }
-    }
-    return count
-}
+// func numberOfArithmeticSlices(nums []int) int {
+//     count := 0
+//     for i := 0; i < len(nums)-1; i++ {
+//         diff := nums[i+1]-nums[i]
+//         for j := i+2; j < len(nums); j++ {
+//             if nums[j] - nums[j-1] != diff {break}
+//             if j-i+1 >= 3 {count++}
+//         }
+//     }
+//     return count
+// }
