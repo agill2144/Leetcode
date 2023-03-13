3a794d1066e6ce6d2b7aab59a59f9cad1ecb6be0
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-12 22:18:50 -0400

    Time: 5 ms (91.58%), Space: 7.5 MB (83.51%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.go
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.go
@@ -6,40 +6,21 @@
  *     Right *TreeNode
  * }
  */
- func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    var dfs func(r *TreeNode) *TreeNode
+    dfs = func(r *TreeNode) *TreeNode {
+        // base
+        if r == nil {return nil}
         
-     var dfs func(r, target *TreeNode, path []*TreeNode) []*TreeNode
-     dfs = func(r, target *TreeNode, path []*TreeNode) []*TreeNode {
-         // base
-         if r == nil {return nil}
-         
-         // logic
-         path = append(path, r)
-
-         if r == target {
-             newL := make([]*TreeNode, len(path))
-             copy(newL, path)
-             return newL
-         }
-         if ok := dfs(r.Left, target, path); ok != nil {return ok}
-         if ok := dfs(r.Right, target, path); ok != nil {return ok}
-         path = path[:len(path)-1]
-         return nil
-     }
-     
-     pathToP := dfs(root, p, nil)
-     pathToQ := dfs(root, q, nil)
-     var lastCommon *TreeNode
-     pPtr := 0
-     qPtr := 0
-     for pPtr < len(pathToP) && qPtr < len(pathToQ) {
-         if pathToP[pPtr] == pathToQ[qPtr] {
-             lastCommon = pathToP[pPtr]
-             pPtr++
-             qPtr++
-         } else {
-             break
-         }
-     }
-     return lastCommon
+        // logic
+        if r == p || r == q {return r}
+        left := dfs(r.Left)
+        right := dfs(r.Right)
+        
+        if left != nil && right != nil {return r}
+        if left == nil && right != nil {return right}
+        if left != nil && right == nil {return left}
+        return nil        
+    }
+    return dfs(root)
 }
