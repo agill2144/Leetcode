81b845fddb75e3dfbb6f8580040ca7060bfee519
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-18 16:30:52 -0400

    Update 1472-design-browser-history.go

--- 1472-design-browser-history/1472-design-browser-history.go
+++ 1472-design-browser-history/1472-design-browser-history.go
@@ -1,63 +1,131 @@
-type node struct {
-    val string
-    next *node
-    prev *node
-}
 type BrowserHistory struct {
-    head *node
-    tail *node
-    curr *node
+    dll *dll
+    curr *listNode
 }
 
 
 func Constructor(homepage string) BrowserHistory {
-    newNode := &node{val: homepage}
-    return BrowserHistory{
-        head: newNode,
-        tail: newNode,
-        curr: newNode,
-    }
+    dll := new(dll)
+    curr := dll.addToEnd(homepage)
+    return BrowserHistory{dll: dll, curr: curr}
 }
 
 
 func (this *BrowserHistory) Visit(url string)  {
-    newNode := &node{val: url}
-    // 1-2 3-4
-    //   c   t
-    tmp := this.curr.next
-    if tmp != nil && tmp.prev != nil {
-        tmp.prev = nil
-    }
-    this.curr.next = nil
-    this.tail = this.curr
-    
-    newNode.prev = this.tail
-    this.tail.next = newNode
-    this.tail = newNode
-    this.curr = newNode
+    this.dll.removeEverythingAfterNode(this.curr)
+    this.curr = this.dll.addToEnd(url)
 }
 
 
 func (this *BrowserHistory) Back(steps int) string {
-    currPtr := this.curr
-    for currPtr.prev != nil && steps != 0 {
-        currPtr = currPtr.prev
+    for steps != 0 && this.curr != this.dll.head {
+        this.curr = this.curr.prev
         steps--
-    }
-    this.curr = currPtr
-    return currPtr.val
+    } 
+    return this.curr.val
 }
 
 
 func (this *BrowserHistory) Forward(steps int) string {
-    currPtr := this.curr
-    for currPtr.next != nil && steps != 0 {
-        currPtr = currPtr.next
+    for steps != 0 && this.curr != this.dll.tail {
+        this.curr = this.curr.next
         steps--
-    }
-    this.curr = currPtr
-    return currPtr.val
+    } 
+    return this.curr.val
 }
+
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * obj := Constructor(homepage);
+ * obj.Visit(url);
+ * param_2 := obj.Back(steps);
+ * param_3 := obj.Forward(steps);
+ */
+
+
+type listNode struct {
+    val string
+    next *listNode
+    prev *listNode
+}
+
+type dll struct {
+    head *listNode
+    tail *listNode
+}
+
+
+func (d *dll) addToEnd(val string) *listNode {
+    newNode := &listNode{val: val}
+    if d.head == nil {
+        d.head = newNode
+        d.tail = newNode
+        return newNode
+    }
+    newNode.prev = d.tail
+    d.tail.next = newNode
+    d.tail = newNode
+    return newNode
+}
+
+
+func (d *dll) removeEverythingAfterNode(n *listNode) {
+    if n == d.tail {return}
+    if n == d.head {
+        if d.head.next != nil {d.head.next.prev = nil}
+        d.head.next = nil    
+        d.tail = d.head
+        return
+    }
+    n.next.prev = nil
+    n.next = nil
+    d.tail = n
+}
+
+
+
+// array implementation
+// arrays = cpu cache, so somewhat performant compared to LL
+// type BrowserHistory struct {
+//     items []string
+//     curr int
+// }
+
+
+// func Constructor(homepage string) BrowserHistory {
+//     return BrowserHistory{
+//         items: []string{homepage},
+//         curr: 0,
+//     }
+// }
+
+
+// func (this *BrowserHistory) Visit(url string)  {
+//     if this.curr != len(this.items) -1 {
+//         this.items = this.items[:this.curr+1]
+//     }
+//     this.items = append(this.items, url)
+//     this.curr++
+// }
+
+
+// func (this *BrowserHistory) Back(steps int) string {
+//     for steps != 0 && this.curr != 0 {
+//         this.curr--
+//         steps--
+//     }
+//     return this.items[this.curr]
+// }
+
+
+// func (this *BrowserHistory) Forward(steps int) string {
+//     for this.curr != len(this.items)-1 && steps != 0 {
+//         this.curr++
+//         steps--
+//     }
+//     return this.items[this.curr]
+// }
 
 
 /**
