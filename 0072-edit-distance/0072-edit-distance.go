0b8cd76dd203f5e13bdbe6a1f9285bcaf784ed99
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-19 18:03:27 -0400

    Time: 6 ms (50.00%), Space: 2.5 MB (98.11%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.go
+++ 0072-edit-distance/0072-edit-distance.go
@@ -1,23 +1,31 @@
 func minDistance(word1 string, word2 string) int {
     m := len(word1)+1
     n := len(word2)+1
-    dp := make([][]int, m)
-    for i := 0; i < m; i++{
-        dp[i] = make([]int, n)
-        dp[i][0] = i
-    }
-    for j := 0; j < n ; j++ {dp[0][j] = j}
-    
+    dp := make([]int, n)
+    for i := 0; i < n ; i++ {dp[i] = i}
+
     for i := 1; i < m; i++ {
+        // new row, since j loop starts from 1
+        // we know that diagUp is i-1;j-1
+        // in 1d array thats dp[0]
+        // save that FIRST
+        diagUp := dp[0]
+        // why becuase, we need the updated left value
+        // dp[0] in each new row, will be whatever i was ( look at dp matrix )
+        // so we need old dp[0] as a diagUp 
+        // and we need new dp[0] for the left value
+        dp[0] = i
         for j := 1; j < n; j++ {
+            nextDiagUp := dp[j]
             if word1[i-1] == word2[j-1] {
-                dp[i][j] = dp[i-1][j-1]
+                dp[j] = diagUp
             } else {
-                dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))+1
+                dp[j] = min(diagUp, min(dp[j], dp[j-1]))+1
             }
+            diagUp = nextDiagUp
         }
     }
-    return dp[m-1][n-1]
+    return dp[n-1]
 }
 
 func min(x, y int) int {
