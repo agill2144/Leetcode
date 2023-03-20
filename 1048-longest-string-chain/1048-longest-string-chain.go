dcee24708c62493b17dbcb116f2bfafb4d64c309
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-19 23:56:51 -0400

    Time: 24 ms (60.00%), Space: 6.8 MB (35.00%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.go
+++ 1048-longest-string-chain/1048-longest-string-chain.go
@@ -1,25 +1,33 @@
 func longestStrChain(words []string) int {
-    sort.Slice(words, func(i, j int)bool{
-        return len(words[i]) < len(words[j])
-    })
-
+    // sort.Slice(words, func(i, j int)bool{
+    //     return len(words[i]) < len(words[j])
+    // })
+    bucket := make([][]string, 17)
     dpMap := map[string]int{}
-    for i := 0; i < len(words); i++ {dpMap[words[i]] = 1}
-    longestChain := 1
+
     for i := 0; i < len(words); i++ {
         word := words[i]
-        
-        if len(word) == 1 {continue}
-        
-        for j := 0; j < len(word); j++ {
-            subStr := word[0:j] + word[j+1:]
-            if count, ok := dpMap[subStr]; ok {
-                dpMap[word] = max(count+1, dpMap[word])
-                longestChain = max(dpMap[word], longestChain)
+        if bucket[len(word)] == nil {
+            bucket[len(word)] = []string{}
+        }
+        bucket[len(word)] = append(bucket[len(word)], word)
+        dpMap[word] = 1
+    }
+    
+   
+    
+    longestChain := 1
+    for i := 0; i < len(bucket); i++ {
+        for _, word := range bucket[i] {
+            if len(word) == 1 {continue}
+            for j := 0; j < len(word); j++ {
+                subStr := word[0:j] + word[j+1:]
+                if count, ok := dpMap[subStr]; ok {
+                    dpMap[word] = max(count+1, dpMap[word])
+                    longestChain = max(dpMap[word], longestChain)
+                }
             }
         }
-        
-        
     }
     return longestChain
 }
