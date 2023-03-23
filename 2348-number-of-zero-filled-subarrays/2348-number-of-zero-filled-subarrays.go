69c9338ee096199e59e1d170660a7024d6e7f52e
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-23 00:06:31 -0400

    Time: 156 ms (35.95%), Space: 10.4 MB (34.86%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.go
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.go
@@ -1,14 +1,10 @@
 func zeroFilledSubarray(nums []int) int64 {
     dp := make([]int, len(nums))
     var total int64 = 0
-    if nums[0] == 0 {
-        dp[0] = 1
-        total = 1
-    }
-    for i := 1; i < len(nums); i++ {
+    for i := 0; i < len(nums); i++ {
         if nums[i] == 0 {
             dp[i]++
-            if nums[i-1] == 0 {
+            if i !=0 && nums[i-1] == 0 {
                 dp[i] += dp[i-1]
             }
         }
