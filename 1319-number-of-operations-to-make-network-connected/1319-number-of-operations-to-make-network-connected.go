69d316351755dc8997220e8d5a6b0979eb56e3c6
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-22 21:01:19 -0400

    Time: 101 ms (11.76%), Space: 17.6 MB (14.71%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.go
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.go
@@ -1,3 +1,35 @@
+/*
+    - there are disconnected components
+    - meaning dfs on all
+    - there can be cycles and this is undirected, so maintain a visited array and a prev node as part of dfs
+    
+    A connected graph with the fewest edges is a tree with n - 1 edges connecting n nodes. 
+    So, in order for a graph with n nodes to be connected, 
+    the graph must have at least n - 1 edges. 
+    It is impossible to connect a graph that has fewer than n - 1 edges. 
+    Otherwise, it is always possible to connect the graph.
+    
+    - count number of connected components
+    - to connect all individual connected components,
+    - we would need to numConnectedComponents-1 edges
+    
+    1---2
+    |       
+    |      4---5
+    3
+           6
+           
+    The above input has 3 connected components
+    TO CONNECT ALL, how many edges do we need?
+    3-1 = 2
+    connect 3-4
+    connect 3-6
+    
+    Therefore to make number of individual connected components connect to each other, we need numConnectedComponents-1 edges in total
+    
+    time: o(v+e)
+    space: o(v+e)
+*/
 func makeConnected(n int, connections [][]int) int {
     if len(connections) < n-1 {return -1}
     adjList := map[int][]int{}
