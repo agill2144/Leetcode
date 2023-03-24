d5c8bc75e344683f392925c2ef0cde1e21df6efb
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-24 17:20:03 -0400

    Time: 315 ms (20.00%), Space: 25.5 MB (20.00%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.go
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.go
@@ -1,10 +1,10 @@
 func minReorder(n int, connections [][]int) int {
-    set := map[[2]int]struct{}{}
-    adjList := map[int][]int{}
+    adjList := map[int][][]int{}
     for i := 0; i < len(connections); i++ {
-        adjList[connections[i][0]] = append(adjList[connections[i][0]], connections[i][1])
-        adjList[connections[i][1]] = append(adjList[connections[i][1]], connections[i][0])
-        set[[2]int{connections[i][0], connections[i][1]}] = struct{}{}
+        src := connections[i][0]
+        dest := connections[i][1]
+        adjList[src] = append(adjList[src], []int{dest, 1})
+        adjList[dest] = append(adjList[dest], []int{src, 0})
     }
     
     count := 0
@@ -16,14 +16,45 @@ func minReorder(n int, connections [][]int) int {
         
         // logic
         visited[node] = struct{}{}
-        if _, ok := set[[2]int{node, prev}]; !ok && prev != -1 {
-            count++
-        }
         for _, edge := range adjList[node] {
-            if edge == prev {continue}
-            dfs(edge, node)
+            if edge[0] == prev {continue}
+            count += edge[1]
+            dfs(edge[0], node)
         }
     }
     dfs(0,-1)
     return count
 }
+
+// some what brute force space ( extra set of connections )
+// func minReorder(n int, connections [][]int) int {
+//     set := map[[2]int]struct{}{}
+//     adjList := map[int][]int{}
+//     for i := 0; i < len(connections); i++ {
+//         adjList[connections[i][0]] = append(adjList[connections[i][0]], connections[i][1])
+//         adjList[connections[i][1]] = append(adjList[connections[i][1]], connections[i][0])
+//         set[[2]int{connections[i][0], connections[i][1]}] = struct{}{}
+//     }
+    
+//     count := 0
+//     visited := map[int]struct{}{}
+//     var dfs func(node, prev int)
+//     dfs = func(node, prev int) {
+//         // base
+//         if _, ok := visited[node]; ok {return}
+        
+//         // logic
+//         visited[node] = struct{}{} 
+//         // is there a connection/edge from node to prev?
+//         // this is why connectons are saved in a  
+//         if _, ok := set[[2]int{node, prev}]; !ok && prev != -1 {
+//             count++
+//         }
+//         for _, edge := range adjList[node] {
+//             if edge == prev {continue}
+//             dfs(edge, node)
+//         }
+//     }
+//     dfs(0,-1)
+//     return count
+// }
