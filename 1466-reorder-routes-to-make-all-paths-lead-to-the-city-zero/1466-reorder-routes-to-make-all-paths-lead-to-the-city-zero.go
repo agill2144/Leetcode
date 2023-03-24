cd58eb503b7a5869dcb6b30fb9601213d50cc168
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-24 04:33:55 -0400

    Time: 278 ms (36.00%), Space: 22.5 MB (20.00%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.go
@@ -0,0 +1,30 @@
+func minReorder(n int, connections [][]int) int {
+    set := map[[2]int]struct{}{}
+    adjList := map[int][]int{}
+    for i := 0; i < len(connections); i++ {
+        adjList[connections[i][0]] = append(adjList[connections[i][0]], connections[i][1])
+        adjList[connections[i][1]] = append(adjList[connections[i][1]], connections[i][0])
+        set[[2]int{connections[i][0], connections[i][1]}] = struct{}{}
+    }
+    
+    count := 0
+    visited := map[int]struct{}{}
+    var dfs func(node, prev int)
+    dfs = func(node, prev int) {
+        // base
+        
+        
+        // logic
+        visited[node] = struct{}{}
+        pair := [2]int{node, prev}
+        if _, ok := set[pair]; !ok && prev != -1 {
+            count++
+        }
+        for _, edge := range adjList[node] {
+            if edge == prev {continue}
+            dfs(edge, node)
+        }
+    }
+    dfs(0,-1)
+    return count
+}
