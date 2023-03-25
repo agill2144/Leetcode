b83ac16557548feea3f18c007b5a0605b62eef7a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-24 22:51:30 -0400

    Time: 80 ms (88.89%), Space: 8 MB (56.94%) - LeetHub

--- 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.go
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.go
@@ -16,8 +16,6 @@ func copyRandomBinaryTree(root *Node) *NodeCopy {
         if r == nil {return nil}
         
         // logic
-        
-        
         rootCopy, exists := origToCp[r]
         if !exists {
             rootCopy = &NodeCopy{Val: r.Val}
@@ -30,11 +28,7 @@ func copyRandomBinaryTree(root *Node) *NodeCopy {
                 origToCp[r.Random] = randomCp
             }
             rootCopy.Random = randomCp
-            
-        }            
-        
-
-        
+        }                    
         rootCopy.Left = dfs(r.Left)
         rootCopy.Right = dfs(r.Right)
         return rootCopy
@@ -43,3 +37,49 @@ func copyRandomBinaryTree(root *Node) *NodeCopy {
     
     return cp
 }
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Random *Node
+ * }
+ */
+
+// approach: use hashmap to map original node to copy node ( same as copy linked list with random ptr )
+// 2 pass ; 1st to create copy tree
+// 2nd to connect random ptrs
+// func copyRandomBinaryTree(root *Node) *NodeCopy {
+//     origToCp := map[*Node]*NodeCopy{}
+//     var dfs func(r *Node) *NodeCopy
+//     dfs = func(r *Node) *NodeCopy {
+//         // base
+//         if r == nil {return nil}
+        
+//         // logic
+//         rootCopy := &NodeCopy{Val: r.Val}
+//         origToCp[r] = rootCopy
+//         rootCopy.Left = dfs(r.Left)
+//         rootCopy.Right = dfs(r.Right)
+//         return rootCopy
+//     }
+//     cp := dfs(root)
+    
+//     var dfsRandomConnector func(r *Node)
+//     dfsRandomConnector = func(r *Node) {
+//         // base
+//         if r == nil {return}
+        
+//         // logic
+//         if r.Random != nil {
+//             origToCp[r].Random = origToCp[r.Random]
+//         }
+//         dfsRandomConnector(r.Left)
+//         dfsRandomConnector(r.Right)
+//     }
+    
+//     dfsRandomConnector(root)
+//     return cp
+// }
