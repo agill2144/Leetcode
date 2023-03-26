bb2d2c03a451348d28de9742455f0d5aae8b8c78
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-26 14:46:30 -0400

    Time: 1940 ms (32.95%), Space: 10.6 MB (9.09%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.go
+++ 0212-word-search-ii/0212-word-search-ii.go
@@ -1,61 +1,52 @@
 func findWords(board [][]byte, words []string) []string {
-    // insert words into trie
-    // o(nk)
-    root := &trieNode{childs: [26]*trieNode{}}
-    for _, word := range words {
-        insert(root, word)
-    }
+    if words == nil || len(words) == 0 {return nil}
     m := len(board)
     n := len(board[0])
-    dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
-    out := []string{}
-
-    var dfs func(r *trieNode, i, j int, path string)
-    dfs = func(r *trieNode, i, j int, path string) {
+    
+    root := &trieNode{childs: [26]*trieNode{}}
+    for i := 0; i < len(words); i++ {
+        insert(root, words[i])
+    }
+    outSet := map[string]struct{}{}
+    dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
+    var dfs func(r, c int, path string)
+    dfs = func(r, c int, path string)  {
         // base
-        if i < 0 || i == m || j < 0 || j == n || board[i][j] == '#' {return}
+        if r < 0 || r == m || c < 0 || c == n || board[r][c] == '#' {return}
+        
         
         // logic
-        path += string(board[i][j])
-        newRoot := search(root, path)
-        // not found
-        if newRoot == nil {return}
-        if newRoot.isEnd && !newRoot.addedToAns {
-            newRoot.addedToAns = true
-            out = append(out, path)
-        }
+        path += string(board[r][c])
+        found, isEnd := search(root, path)
+        if !found {return}
+        if isEnd { outSet[path] = struct{}{} }
         
-        
-        tmp := board[i][j]
-        board[i][j] = '#'
+        tmp := board[r][c]
+        board[r][c] = '#'
         for _, dir := range dirs {
-            dfs(newRoot, i+dir[0], j+dir[1], path)
+            dfs(r+dir[0], c+dir[1], path)
         }
-        board[i][j]= tmp
-
+        board[r][c] = tmp
     }
     
-    // loop thru entire matrix
-    // form each word at each cell, explore all 4 dirs 
-    // and use trie to continue direction exploration
-    // o(mn) * 3^wk
-    // w = len of words and k being avg of each 
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            dfs(root, i,j, "")
+            dfs(i,j, "")
         }
     }
-    
+    out := []string{}
+    for k, _ := range outSet {
+        out = append(out, k)
+    }
     return out
 }
-
 
 
 
 type trieNode struct {
     isEnd bool
     childs [26]*trieNode
-    addedToAns bool // acting as my set :D 
+    
 }
 
 
@@ -71,12 +62,12 @@ func insert(root *trieNode, word string)  {
     curr.isEnd = true
 }
 
-func search(root *trieNode, prefix string) *trieNode {
+func search(root *trieNode, prefix string) (bool, bool) {
     curr := root
     for _, char := range prefix {
         idx := char-'a'
-        if curr.childs[idx] == nil {return nil}
+        if curr.childs[idx] == nil {return false, false}
         curr = curr.childs[idx]
     }
-    return curr
+    return true, curr.isEnd
 }
