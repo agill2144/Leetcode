76599dc0f9975e3feff8a53dd0311715b775ab38
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-26 21:35:18 -0400

    Time: 9 ms (57.92%), Space: 3.9 MB (82.51%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.go
@@ -0,0 +1,20 @@
+func minPathSum(grid [][]int) int {
+    m := len(grid)
+    n := len(grid[0])
+    for i := m-1; i >= 0; i-- {
+        for j := n-1; j >= 0; j-- {
+            if i==m-1 && j==n-1 {continue}
+            rightCell := math.MaxInt64-1000
+            if j != n-1 {rightCell = grid[i][j+1]}
+            bottomCell := math.MaxInt64-1000
+            if i != m-1 {bottomCell = grid[i+1][j]}
+            grid[i][j] += min(rightCell, bottomCell)
+        }
+    }
+    return grid[0][0]
+}
+
+func min(x, y int) int {
+    if x < y {return x}
+    return y
+}
