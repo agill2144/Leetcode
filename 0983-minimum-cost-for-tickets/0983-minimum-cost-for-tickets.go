54ccc0895b4cdbfe092054709ad24718c5707410
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-27 20:39:09 -0400

    Time: 3 ms (58.14%), Space: 2.4 MB (86.05%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.go
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.go
@@ -7,24 +7,26 @@ func mincostTickets(days []int, costs []int) int {
     
     j := 0
     for i := 0; i < len(days); i++ {
+        // ensure j is at the same position as current day ( days[i] )
         for j != days[i] {
             if j != 0 {
                 dp[j] = dp[j-1]
             }
             j++
         }
+        
         oneDayBack := 0
-        if j - 1 >= 0 { oneDayBack = dp[j-1]}
         sevenDaysBack := 0
-        if j - 7 >= 0 { sevenDaysBack = dp[j-7]}
         thirtyDaysBack := 0
+
+        if j - 1 >= 0 { oneDayBack = dp[j-1]}
+        if j - 7 >= 0 { sevenDaysBack = dp[j-7]}
         if j - 30 >= 0 { thirtyDaysBack = dp[j-30]}
 
         dp[j] = min(oneDayPass+oneDayBack, min(sevenDayPass+sevenDaysBack, thirtyDayPass+thirtyDaysBack))
         j++
            
     }
-    // fmt.Println(dp)
     return dp[len(dp)-1]
 }
 
