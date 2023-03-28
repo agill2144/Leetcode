37dc6d200c5c2a013708eda60dc26ce038f962fa
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-27 20:38:21 -0400

    Time: 0 ms (100.00%), Space: 2.4 MB (86.05%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.go
@@ -0,0 +1,34 @@
+func mincostTickets(days []int, costs []int) int {
+    dp := make([]int, days[len(days)-1]+1)
+    
+    oneDayPass := costs[0]
+    sevenDayPass := costs[1]
+    thirtyDayPass := costs[2]
+    
+    j := 0
+    for i := 0; i < len(days); i++ {
+        for j != days[i] {
+            if j != 0 {
+                dp[j] = dp[j-1]
+            }
+            j++
+        }
+        oneDayBack := 0
+        if j - 1 >= 0 { oneDayBack = dp[j-1]}
+        sevenDaysBack := 0
+        if j - 7 >= 0 { sevenDaysBack = dp[j-7]}
+        thirtyDaysBack := 0
+        if j - 30 >= 0 { thirtyDaysBack = dp[j-30]}
+
+        dp[j] = min(oneDayPass+oneDayBack, min(sevenDayPass+sevenDaysBack, thirtyDayPass+thirtyDaysBack))
+        j++
+           
+    }
+    // fmt.Println(dp)
+    return dp[len(dp)-1]
+}
+
+func min(x, y int) int {
+    if x < y {return x}
+    return y
+}
