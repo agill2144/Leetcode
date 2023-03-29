1bfd10bb7057f2313677b6d1e69854d2e57dfe78
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-29 18:15:22 -0400

    Time: 106 ms (28.36%), Space: 10 MB (15.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.go
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -1,13 +1,13 @@
 func maxSubArray(nums []int) int {
     if nums == nil || len(nums) == 0 {return 0}
     
-    dp := make([]int, len(nums))
-    dp[0] = nums[0]
+    curr := nums[0]
     maxTotal := nums[0]
     
     for i := 1; i < len(nums); i++ {
-        dp[i] = max(nums[i], nums[i]+dp[i-1])
-        if dp[i] > maxTotal {maxTotal = dp[i]}
+        newCurr := max(nums[i], curr + nums[i])
+        curr = newCurr
+        if curr > maxTotal {maxTotal = curr}
     }
     return maxTotal    
 }
