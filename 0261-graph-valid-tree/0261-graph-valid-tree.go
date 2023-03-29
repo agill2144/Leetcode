600a11e497153f03fff043421781fcea6bd37726
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-29 18:59:22 -0400

    Time: 8 ms (83.87%), Space: 6.6 MB (17.20%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.go
+++ 0261-graph-valid-tree/0261-graph-valid-tree.go
@@ -1,30 +1,37 @@
+/*
+    graph is a valid tree if
+    1. fully connected
+    2. contains no cycles
+
+    we can simply do a dfs to check for cycles from node 0
+    if graph is fully connected then the number of visited nodes
+    in visited set should equal to n
+
+    time: o(v+e)
+    space: o(v+e)
+*/
 func validTree(n int, edges [][]int) bool {
     adjList := map[int][]int{}
     for i := 0; i < len(edges); i++ {
-        adjList[edges[i][0]] = append(adjList[edges[i][0]], edges[i][1]) 
-        adjList[edges[i][1]] = append(adjList[edges[i][1]], edges[i][0]) 
+        adjList[edges[i][0]] = append(adjList[edges[i][0]], edges[i][1])
+        adjList[edges[i][1]] = append(adjList[edges[i][1]], edges[i][0])        
     }
     visited := map[int]struct{}{}
-    var dfs func(node int, parent int) bool
-    dfs = func(node, parent int) bool {
+    var dfs func(node, prev int) bool
+    dfs = func(node, prev int) bool {
         // base
-        if _, ok := visited[node]; ok {return false}
+        if _, ok := visited[node]; ok {return true} // cycle detected
         
         // logic
         visited[node] = struct{}{}
-        for _, child := range adjList[node] {
-            if child == parent {continue}
-            if ok := dfs(child, node); !ok {return false} 
+        for _, nei := range adjList[node]{
+            if nei == prev {continue}
+            if hasCycle := dfs(nei, node); hasCycle {return true}
         }
-        return true
+        return false
     }
-    out := dfs(0, -1)
-    if !out {return false}
-/*
-Recall that a graph, G, is a tree iff the following two conditions are met:
-
-G is fully connected. In other words, for every pair of nodes in G, there is a path between them. - visited nodes count == n
-G contains no cycles. In other words, there is exactly one path between each pair of nodes in G. - use DFS
-*/
-    return len(visited) == n
+    
+    ok := dfs(0, -1)
+    if ok {return false}
+    return n == len(visited)
 }
