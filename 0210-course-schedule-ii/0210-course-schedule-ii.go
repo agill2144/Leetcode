016a458970f525aa097657ab2582555734cb95d1
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-31 17:26:15 -0400

    Time: 16 ms (30.88%), Space: 6.6 MB (44.85%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.go
+++ 0210-course-schedule-ii/0210-course-schedule-ii.go
@@ -1,40 +1,47 @@
+// top sort + cycle detection using dfs
+
 func findOrder(numCourses int, prerequisites [][]int) []int {
-    indegrees := make([]int, numCourses)
     adjList := map[int][]int{}
     for i := 0; i < len(prerequisites); i++ {
-        course := prerequisites[i][0]
-        prereq := prerequisites[i][1]
-        adjList[prereq] = append(adjList[prereq], course)
-        indegrees[course]++
+        // a depends on b
+        // b is indepndent
+        a := prerequisites[i][0]
+        b := prerequisites[i][1]
+        adjList[b] = append(adjList[b], a)
     }
+    // count := 0
+    st := []int{}
+    visited := make([]bool, numCourses)
     
-    q := []int{}
-    for i := 0; i < len(indegrees); i++ {
-        if indegrees[i] == 0 {
-            q = append(q, i)
+    var dfs func(node int, path []bool) bool
+    dfs = func(node int, path []bool) bool {
+        // base
+        // found a cycle
+        if path[node] {return true}
+        
+        // skip already visited node
+        if visited[node] {return false}
+        
+        // logic
+        path[node] = true
+        visited[node] = true
+        for _, nei := range adjList[node] {
+            if dfs(nei, path) {return true}
+        }
+        
+        path[node] = false
+        st = append(st, node)
+        return false
+    }
+    for i := 0; i < numCourses; i++ {
+        if !visited[i] {
+            path := make([]bool, numCourses)
+            if dfs(i,path) {return nil}
         }
     }
-
-    // all courses have 1 or more dependencies, therefore cannot start from anywhere
-    if len(q) == 0 {
-        return nil
+    if len(st) != numCourses {return nil}
+    for i := 0; i < len(st)/2; i++ {
+        st[i], st[len(st)-1-i] = st[len(st)-1-i], st[i]
     }
-    out := []int{}
-    coursesTaken := 0
-    for len(q) != 0 {
-        dq := q[0]
-        q = q[1:]
-        coursesTaken++
-        out = append(out, dq)
-        childs := adjList[dq]
-        for _, child := range childs {
-            indegrees[child]--
-            if indegrees[child] == 0 {
-                q = append(q, child)
-            }
-        }
-    }
-    if coursesTaken != numCourses {return nil}
-    return out
-    
+    return st
 }
