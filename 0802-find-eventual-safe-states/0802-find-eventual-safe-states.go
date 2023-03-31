ba121af955a0284f9b21385469132ff7e866487a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-31 15:34:03 -0400

    Time: 222 ms (7.14%), Space: 25 MB (7.14%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
@@ -1,28 +1,39 @@
 func eventualSafeNodes(graph [][]int) []int {
-	safeNode := map[int]bool{}
+    toPtrBool := func(b bool) *bool {return &b}
 
-	var dfs func(i int) bool
-	dfs = func(i int) bool {
-		if safe, f := safeNode[i]; f {
-			return safe
-		}
-
-		safeNode[i] = false
-		for _, nei := range graph[i] {
-			if !dfs(nei) {
-				return false
-			}
-		}
-		safeNode[i] = true
-		return true
-	}
-
-	res := []int{}
-	for i := 0; i < len(graph); i++ {
-		if dfs(i) {
-			res = append(res, i)
-		}
-	}
-	return res
+    safeNodes := make([]*bool, len(graph))
+    visited := make([]bool, len(graph))
+    var dfs func(node int, path []bool) bool 
+    dfs = func(node int, path []bool) bool {
+        // base
+        if path[node] {return true}
+        if visited[node] {
+            // a visited node could be connected to a cycle, if thats the case, its also not safe...
+            if safeNodes[node] != nil && *safeNodes[node] == false {return true}
+            return false
+        }
+        
+        // logic
+        path[node] = true
+        visited[node] = true
+        for _, nei := range graph[node] {
+            ok := dfs(nei, path)
+            if ok {safeNodes[node] = toPtrBool(false); return true}
+        }
+        path[node] = false
+        safeNodes[node] = toPtrBool(true)
+        return false
+    }
+    for i := 0; i < len(graph); i++ {
+        if !visited[i] {
+            path := make([]bool, len(graph))
+            dfs(i, path)
+        }
+    }
+        
+    out := []int{}
+    for i := 0; i < len(graph); i++ {
+        if *safeNodes[i] {out = append(out, i)}
+    }
+    return out
 }
-
