a83323e06c0dad7b6c0e17ecb5ea447f70abc362
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-03-31 15:51:29 -0400

    Time: 188 ms (10.34%), Space: 14 MB (6.90%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
@@ -2,22 +2,20 @@ func eventualSafeNodes(graph [][]int) []int {
     toPtrBool := func(b bool) *bool {return &b}
 
     safeNodes := make([]*bool, len(graph))
-    visited := make([]bool, len(graph))
     var dfs func(node int, path []bool) bool 
 
     // hasCycleOrConnectedToCycle
     dfs = func(node int, path []bool) bool {
         // base
         if path[node] {return true}
-        if visited[node] {
+        if safeNodes[node] != nil {
             // a visited node could be connected to a cyclic node, if thats the case, its also not safe...
-            if safeNodes[node] != nil {return !*safeNodes[node]}
-            return false
+            return !*safeNodes[node]
         }
         
         // logic
         path[node] = true
-        visited[node] = true
+        safeNodes[node] = toPtrBool(false)
         for _, nei := range graph[node] {
             if dfs(nei, path) {safeNodes[node] = toPtrBool(false); return true}
         }
@@ -26,7 +24,7 @@ func eventualSafeNodes(graph [][]int) []int {
         return false
     }
     for i := 0; i < len(graph); i++ {
-        if !visited[i] {
+        if safeNodes[i] == nil {
             path := make([]bool, len(graph))
             dfs(i, path)
         }
