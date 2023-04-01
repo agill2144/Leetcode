cb185acb7e56dea6e79121a2a5b0d54a31540d38
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-01 17:17:35 -0400

    Time: 60 ms (98.28%), Space: 7.7 MB (98.28%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.go
+++ 1020-number-of-enclaves/1020-number-of-enclaves.go
@@ -2,39 +2,43 @@ func numEnclaves(grid [][]int) int {
     m := len(grid)
     n := len(grid[0])
     
+    
     q := [][]int{}
     dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
-    var bfs func(r, c int)
-    bfs = func(r, c int) {
-        grid[r][c] = -1
-        q = append(q, []int{r, c})
-        for len(q) != 0 {
-            dq := q[0]; q = q[1:]
-            for _, dir := range dirs {
-                nr := dq[0] + dir[0]
-                nc := dq[1] + dir[1]
-                if nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1 {
-                    grid[nr][nc] = -1
-                    q = append(q, []int{nr,nc})
+    
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if i == 0 || j == 0 || i == m-1 || j == n-1 {
+                if grid[i][j] == 1 {
+                    q = append(q, []int{i,j})
+                    grid[i][j] = -1
                 }
             }
         }
     }
-    for i := 0; i < m; i++ {
-        if grid[i][0] == 1 {bfs(i,0)}
-        if grid[i][n-1] == 1 {bfs(i,n-1)}
-    }
+
     
-    for j := 0; j < n; j++ {
-        if grid[0][j] == 1 {bfs(0,j)}
-        if grid[m-1][j] == 1 {bfs(m-1,j)}
+    
+    for len(q) != 0 {
+        dq := q[0]; q = q[1:]
+        for _, dir := range dirs {
+            nr := dq[0] + dir[0]
+            nc := dq[1] + dir[1]
+            if nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1 {
+                grid[nr][nc] = -1
+                q = append(q, []int{nr,nc})
+            }
+        }
     }
     count := 0
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            if grid[i][j] == 1{count++}
+            if grid[i][j] == 1 {
+                count++
+            }
         }
     }
+
     return count
 }
 
