0498c4dfab93ebdf3a13521102ba6016fe9d49ff
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-01 23:20:10 -0400

    Time: 22 ms (68.18%), Space: 7.6 MB (40.91%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.go
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.go
@@ -4,24 +4,24 @@ func numDistinctIslands(grid [][]int) int {
     n := len(grid[0])
     set := map[string]int{}
     
-    var dfs func(r, c int, path *string)
-    dfs = func(r, c int, path *string) {
+    var dfs func(r, c int, path *strings.Builder)
+    dfs = func(r, c int, path *strings.Builder) {
         // base
         if r < 0 || r == m || c < 0 || c == n || grid[r][c] != 1 {return}
         
         // logic
         grid[r][c] = -1
         for _, dir := range dirs {
-            *path = *path+dir[2].(string)
+            path.WriteString(dir[2].(string))
             dfs(r+dir[0].(int), c+dir[1].(int), path)
         }
     }
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
             if grid[i][j] == 1 {
-                path := ""
-                dfs(i,j,&path)
-                set[path]++
+                path := new(strings.Builder)
+                dfs(i,j,path)
+                set[path.String()]++
             }
         }
     }
