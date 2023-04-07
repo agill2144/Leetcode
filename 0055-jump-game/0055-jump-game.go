866886ea31243937ed7182724bc91df34f76cd28
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-07 15:48:45 -0400

    Time: 62 ms (54.37%), Space: 7 MB (69.85%) - LeetHub

--- 0055-jump-game/0055-jump-game.go
+++ 0055-jump-game/0055-jump-game.go
@@ -1,15 +1,11 @@
 func canJump(nums []int) bool {
-    dp := make([]bool, len(nums))
-    dp[len(nums)-1] = true
-    
-    for i := len(nums)-2; i>=0; i-- {
+    n := len(nums)
+    target := n-1    
+    for i := n-2; i>=0; i-- {
         jumpDist := nums[i]
-        for j := 1; j <= jumpDist; j++ {
-            if i+j < len(nums) && dp[i+j] {
-                dp[i] = true
-                break
-            }
+        if i+jumpDist >= target {
+            target = i
         }
     }
-    return dp[0]
+    return target == 0
 }
