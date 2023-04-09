b7b0debf5043e1ceaf702b96f501c8246c5d6da0
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-09 16:06:16 -0400

    Time: 174 ms (40.00%), Space: 13.1 MB (22.50%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
@@ -1,10 +1,11 @@
 func eventualSafeNodes(graph [][]int) []int {
-    visited := make([]bool, len(graph))
-    valid := make([]*bool, len(graph))
-    
+    n := len(graph)
+    visited := make([]bool, n)
+    valid := make([]*bool, n)
     toPtrBool := func(b bool) *bool {return &b}
-    var isValid func(node int, path []bool) bool
-    isValid = func(node int, path []bool) bool {
+    
+    var dfs func(node int, path []bool) bool
+    dfs = func(node int, path []bool) bool {
         // base
         if path[node] {return false}
         if visited[node] {
@@ -13,30 +14,41 @@ func eventualSafeNodes(graph [][]int) []int {
         }
         
         // logic
-        path[node] = true
+        // action
         visited[node] = true
+        path[node] = true
         for _, nei := range graph[node] {
-            if !isValid(nei, path) {
+            // if any of this node's neighbor, is invalid (part of a cycle)
+            // then this makes this node invalid, and we need to return false
+            // so that other nodes connected to this node can also see that they
+            // are connected to an invalid node and therefore they are also invalid
+            // basically propagate the invalid path all the way up in the current recursion stack
+            // recursion
+            if !dfs(nei, path) {
                 valid[node] = toPtrBool(false)
-                valid[nei] = toPtrBool(false)
                 return false
             }
         }
         
-        path[node] = false
+        // if this node never exited with a false from exploring all of its neighbors,
+        // then this node is a valid node
         valid[node] = toPtrBool(true)
+        // backtrack
+        path[node] = false
+        
+        // since this node is valid, we can return true
         return true
     }
-    
-    for i := 0; i < len(graph); i++ {
+    for i := 0; i < n; i++ {
         if !visited[i] {
-            path := make([]bool, len(graph))
-            isValid(i, path)
+            dfs(i, make([]bool, n))
         }
     }
     out := []int{}
-    for i := 0; i < len(graph); i++ {
-        if *valid[i] {out = append(out, i)}
+    for i := 0; i < n; i++ {
+        if valid[i] != nil && *valid[i] {
+            out = append(out, i)
+        }
     }
     return out
 }
