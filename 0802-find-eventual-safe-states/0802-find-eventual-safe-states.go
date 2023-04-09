aab5e42c92e101f0e1c3b0f48d3b9f5808be0cb3
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-09 16:14:12 -0400

    Time: 284 ms (5.00%), Space: 39.6 MB (5.00%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
@@ -1,9 +1,39 @@
+/*
+    approach: DFS cycle detection
+    - as soon as we find a cycle, all the nodes connected to this cycle are invalid
+    - since using DFS means, connected nodes (i.e recursion path ) is in our recursive stack
+    - once a cycle is detected , we can return false
+    - once a false is detected from its neighbor, than that means this node is also invalid;
+        - mark it invalid, and propagate the false result up by returning false and exiting early
+    - if none of the neighbors of a node returned false, that means all of this nodes path exited without getting into a cycle.
+    - when this happens, this means this node is valid.
+    
+    - Detecting cycle on a directed graph 
+        - use dfs
+        - use path to keep track of "who is in my path"
+        - a path becomes invalid, if we run into a node that's already in the path ( i.e cycle detected )
+    
+    - Since this is a directed graph, we have to run dfs on all nodes
+        - we can use a visited set / array to not re-run dfs on already processed nodes
+    - But if dfs runs, and its neighbors is already visited, check if they are valid 
+    - thats where we need to maintain a valid array ( ptr to bool array )
+        - so that we have 3 identifiers
+            - nil = not explored
+            - false = invalid node
+            - true = valid node
+        - if we dont use ptr to bool, the whole array will have false values
+        - and then we wont be able to identify invalid vs not-yet-explored nodes.
+        
+    time: o(v+e)
+    space: o(v) + o(v) + o(v+e)
+*/
 func eventualSafeNodes(graph [][]int) []int {
     n := len(graph)
     visited := make([]bool, n)
     valid := make([]*bool, n)
     toPtrBool := func(b bool) *bool {return &b}
-    
+
+    // returns false if a cycle is detected
     var dfs func(node int, path []bool) bool
     dfs = func(node int, path []bool) bool {
         // base
@@ -39,6 +69,7 @@ func eventualSafeNodes(graph [][]int) []int {
         // since this node is valid, we can return true
         return true
     }
+    
     for i := 0; i < n; i++ {
         if !visited[i] {
             dfs(i, make([]bool, n))
