30690e2085185d1d3479c5407314619fa85bab3c
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-10 20:45:52 -0400

    Time: 28 ms (62.96%), Space: 7.7 MB (40.74%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.go
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.go
@@ -1,27 +1,14 @@
-// func removeStars(s string) string {
-//     st := []byte{}
-//     for i := 0; i < len(s); i++ {
-//         if s[i] == '*' {
-//             // pop top
-//             if len(st) != 0 {st = st[:len(st)-1]}
-//         } else {
-//             st = append(st, s[i])
-//         }
-//     }
-//     out := new(strings.Builder)
-//     for i := 0; i < len(st); i++ {out.WriteByte(st[i])}
-//     return out.String()
-// }
-
 func removeStars(s string) string {
-    out := ""
+    st := []byte{}
     for i := 0; i < len(s); i++ {
         if s[i] == '*' {
             // pop top
-            if len(out) != 0 {out = out[:len(out)-1]}
+            if len(st) != 0 {st = st[:len(st)-1]}
         } else {
-            out += string(s[i])
+            st = append(st, s[i])
         }
     }
-    return out
+    out := new(strings.Builder)
+    for i := 0; i < len(st); i++ {out.WriteByte(st[i])}
+    return out.String()
 }
