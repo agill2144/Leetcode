0553167be4d8723aa9c5b9a7701c5b42b59679c4
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-11 20:44:31 -0400

    Time: 0 ms (100.00%), Space: 3.2 MB (35.79%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.go
@@ -0,0 +1,26 @@
+func simplifyPath(path string) string {
+    if len(path) <= 1 {return path}
+    pathSplit := strings.Split(path, "/")
+    st := []string{}
+    for _, ele := range pathSplit {
+        if ele == "" || ele == "." {continue}
+        if ele == ".." {
+            if len(st) != 0 {st = st[:len(st)-1]}
+            continue
+        }
+        
+        st = append(st, ele)
+    }
+    if len(st) == 0 {return "/"}
+    
+    out := new(strings.Builder)
+    out.WriteString("/")
+    for i := 0; i < len(st); i++ {
+        out.WriteString(st[i])
+        if i != len(st)-1 {
+            out.WriteString("/")
+        }
+    }
+    
+    return out.String()
+}
