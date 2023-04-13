462cbe8a01980b6ede2795da11adb5d397447ab9
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-12 23:46:29 -0400

    Time: 229 ms (10.64%), Space: 23.6 MB (14.89%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
@@ -1,3 +1,26 @@
+/*
+    approach: DFS cycle detection
+    - A node is a safe node if all their paths lead to a terminal or another safe node
+    - i.e all of a node's path did not end up in a cycle.
+    - Therefore we can apply cycle detection using DFS on a directed graph
+    - Then if the neigbhor exploration returns false, we do not mark this node safe
+    - If all the neighbor explorations exited successfully, than we can mark this node safe
+    
+    - There can be disconnected components and this is a directed graph, therefore need to run dfs on all nodes
+    - if a node is already visited, we cannot exit early and say that this node is valid ( i.e will not run into a cycle at all )
+        - therefore we need to continue a path exploration even if a node is already visited but not in our current recursion stack
+    - This is where we can "memoize" an answer for each node
+    - if a node exited with false, then we can save this answer that this node is not safe
+    - if a node exited with true, then we can save this answer that this nide is safe
+    - now if a node is already visited, we can check with memoized answers - if none return true
+    - thats where we need to maintain a valid array ( ptr to bool array )
+        - so that we have 3 identifiers
+            - nil = not explored
+            - false = explored and invalid node
+            - true = explored and valid node
+        - if we dont use ptr to bool, the whole array will have false values
+        - and then we wont be able to identify invalid vs not-yet-explored nodes.
+*/
 func eventualSafeNodes(graph [][]int) []int {
     n := len(graph)
     visited := make([]bool, n)
