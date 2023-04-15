644bc0cc76370157ccc042c96b72c287610753ac
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-15 17:17:31 -0400

    Time: 0 ms (100.00%), Space: 2.3 MB (86.89%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.go
+++ 0269-alien-dictionary/0269-alien-dictionary.go
@@ -1,32 +1,39 @@
 func alienOrder(words []string) string {
     adjList := map[byte][]byte{}
-    indegrees := make([]int, 26)
     for i := 0; i < len(words); i++ {
         for j := 0; j < len(words[i]); j++ {
             adjList[words[i][j]] = []byte{}
         }
     }
+    indegrees := make([]int, 26)
     for i := 0; i < len(words)-1; i++ {
         word1 := words[i]
         word2 := words[i+1]
-        w1, w2 := 0, 0
-        for w1 < len(word1) && w2 < len(word2) && word1[w1] == word2[w2] {
+        w1, w2 := 0,0
+        
+        for w1 < len(word1) && w2 < len(word2) {
+            w1Char := word1[w1]
+            w2Char := word2[w2]
+            if w1Char != w2Char {
+                adjList[w1Char] = append(adjList[w1Char], w2Char)
+                indegrees[w2Char-'a']++
+                break
+            }
             w1++
             w2++
         }
-        // invalid sequence
         if w2 == len(word2) && w1 < len(word1) {return ""}
-        if (w2 == len(word2) && w1 == len(word1)) || (w1 == len(word1) && w2 < len(word2)) {continue}
-        adjList[word1[w1]] = append(adjList[word1[w1]], word2[w2])
-        indegrees[word2[w2]-'a']++
     }
+    
     q := []byte{}
-    for char, _ := range adjList {
-        if indegrees[char-'a'] == 0 {
-            q = append(q, char)
+    for key, _ := range adjList {
+        if indegrees[key-'a'] == 0 {
+            q = append(q, key)
         }
     }
-    if len(q) == 0 {return ""}
+    if len(q) == 0 {
+        return ""
+    }
     out := new(strings.Builder)
     for len(q) != 0 {
         dq := q[0]
@@ -37,11 +44,10 @@ func alienOrder(words []string) string {
             if indegrees[nei-'a'] == 0 {
                 q = append(q, nei)
             }
-        }        
+        }
     }
-
-    if len(out.String()) != len(adjList) {
-        return ""
-    }    
+    for i := 0; i < len(indegrees); i++ {
+        if indegrees[i] > 0 {return ""}
+    }
     return out.String()
 }
