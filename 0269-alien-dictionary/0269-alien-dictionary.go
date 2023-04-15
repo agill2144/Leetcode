fc9bda15504da83ba1f788d90c0f9507e3f0eae3
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-15 16:43:49 -0400

    Time: 3 ms (60.66%), Space: 2.2 MB (86.89%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.go
+++ 0269-alien-dictionary/0269-alien-dictionary.go
@@ -1,79 +1,47 @@
 func alienOrder(words []string) string {
-    result := new(strings.Builder)
-    
-    adjList, indegrees := buildGraph(words)
+    adjList := map[byte][]byte{}
+    indegrees := make([]int, 26)
+    for i := 0; i < len(words); i++ {
+        for j := 0; j < len(words[i]); j++ {
+            adjList[words[i][j]] = []byte{}
+        }
+    }
+    for i := 0; i < len(words)-1; i++ {
+        word1 := words[i]
+        word2 := words[i+1]
+        w1, w2 := 0, 0
+        for w1 < len(word1) && w2 < len(word2) && word1[w1] == word2[w2] {
+            w1++
+            w2++
+        }
+        // invalid sequence
+        if w2 == len(word2) && w1 < len(word1) {return ""}
+        if (w2 == len(word2) && w1 == len(word1)) || (w1 == len(word1) && w2 < len(word2)) {continue}
+        adjList[word1[w1]] = append(adjList[word1[w1]], word2[w2])
+        indegrees[word2[w2]-'a']++
+    }
     q := []byte{}
     for char, _ := range adjList {
         if indegrees[char-'a'] == 0 {
             q = append(q, char)
         }
     }
-    // if we have no independent node(s) to start with , return empty string
-    if len(q) == 0 {
-        return ""
-    }
+    if len(q) == 0 {return ""}
+    out := new(strings.Builder)
     for len(q) != 0 {
         dq := q[0]
         q = q[1:]
-        result.WriteByte(dq)
-        childSet := adjList[dq]
-        for child, _ := range childSet.items {
-            indegrees[child-'a']--
-            if indegrees[child-'a'] == 0 {
-                q = append(q, child)
+        out.WriteByte(dq)
+        for _, nei := range adjList[dq] {
+            indegrees[nei-'a']--
+            if indegrees[nei-'a'] == 0 {
+                q = append(q, nei)
             }
-        }
+        }        
     }
-    outStr := result.String()
-    // if we have not used all characters from adjList ( keys ), that means there
-    // are still nodes in indegrees to process that could not be processed
-    // meaning there is no valid top sort
-    // i.e return ""
-    if len(outStr) != len(adjList) {
+
+    if len(out.String()) != len(adjList) {
         return ""
-    }
-    return outStr
+    }    
+    return out.String()
 }
-
-func buildGraph(words []string) (map[byte]*set, []int) {
-    out := map[byte]*set{}
-    for i := 0; i < len(words); i++ {
-        for j := 0; j < len(words[i]); j++ {
-            out[words[i][j]] = newSet()
-        }
-    }
-    indegrees := make([]int, 26)
-    for i := 0; i < len(words)-1; i++ {
-        word1 := words[i]
-        word2 := words[i+1]
-        
-        /*
-        func HasPrefix(s, prefix string) bool
-        HasPrefix tests whether the string s begins with prefix.
-        */
-        if strings.HasPrefix(string(word1), string(word2)) && len(word2) < len(word1) {
-            return map[byte]*set{}, nil
-        }
-        
-        for j := 0 ; j < len(word1) && j < len(word2); j++ {
-            w1Char := word1[j]
-            w2Char := word2[j]
-            if w1Char != w2Char {
-                // w1 is the parent/independent
-                // w2 is the child ( i.e it has an incoming edge )
-                if !out[w1Char].contains(w2Char){
-                    indegrees[w2Char-'a']++
-                    out[w1Char].add(w2Char)
-                }
-                break
-            } 
-        }
-    }
-    return out, indegrees   
-}
-
-type set struct{ items map[byte]struct{} }
-func newSet() *set { return &set{items: map[byte]struct{}{}} }
-func (s *set) add(x byte){ s.items[x] = struct{}{} }
-func (s *set) remove(x byte) { delete(s.items, x)}
-func (s *set) contains(x byte) bool { _, ok := s.items[x]; return ok}
