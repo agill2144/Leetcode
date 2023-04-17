4e71520738366d87aaebf128c40c73b26671ae87
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-16 17:50:45 -0700

    Time: 337 ms (26.66%), Space: 7.1 MB (62.78%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.go
+++ 0127-word-ladder/0127-word-ladder.go
@@ -1,36 +1,40 @@
 func ladderLength(beginWord string, endWord string, wordList []string) int {
     adjList := map[string][]string{}
     wordList = append(wordList, beginWord)
+    listHasEndWord := false
     for i := 0; i < len(wordList); i++ {
-        parent := wordList[i]
+        parentWord := wordList[i]
+        if parentWord == endWord {listHasEndWord = true}
         for j := 0; j < len(wordList); j++ {
-            if j == i {continue}
+            if i == j {continue}
             child := wordList[j]
+            countDiffChars := 0
             p := 0
             c := 0
-            count := 0
-            for p < len(parent) {
-                if parent[p] == child[c] {count++}
+            for p < len(parentWord) && c < len(child) {
+                if parentWord[p] != child[c] {countDiffChars++}
                 p++
                 c++
             }
-            if count == len(parent)-1 {
-                adjList[parent] = append(adjList[parent], child)
+            if countDiffChars == 1 {
+                adjList[parentWord] = append(adjList[parentWord], child)
             }
         }
     }
-    visitedWord := map[string]struct{}{beginWord: struct{}{}}
-    q := []string{beginWord}
+    if !listHasEndWord {return 0}
     level := 1
+    q := []string{beginWord}
+    visited := map[string]struct{}{beginWord:struct{}{}}
     for len(q) != 0 {
         qSize := len(q)
         for qSize != 0 {
-            dq := q[0]; q = q[1:]
-            if dq == endWord { return level } 
-            for _, child := range adjList[dq] {
-                if _, ok := visitedWord[child]; !ok {
-                    q = append(q, child)
-                    visitedWord[child] = struct{}{}
+            dq := q[0]
+            q = q[1:]
+            if dq == endWord {return level}
+            for _, nei := range adjList[dq] {
+                if _, ok := visited[nei]; !ok {
+                    visited[nei] = struct{}{}
+                    q = append(q, nei)
                 }
             }
             qSize--
