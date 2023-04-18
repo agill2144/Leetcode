b34595dbe1bb442f54fe9e139e163ab869bd9b2d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-17 20:45:49 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (45.00%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.go
@@ -0,0 +1,15 @@
+func mergeAlternately(word1 string, word2 string) string {
+    out := new(strings.Builder)
+    w1 := 0
+    w2 := 0
+    for w1 < len(word1) && w2 < len(word2) {
+        out.WriteByte(word1[w1])
+        out.WriteByte(word2[w2])
+        w1++; w2++
+    }
+    
+    for w1 < len(word1) { out.WriteByte(word1[w1]); w1++ }
+    for w2 < len(word2) { out.WriteByte(word2[w2]); w2++ }
+    return out.String()
+
+}
