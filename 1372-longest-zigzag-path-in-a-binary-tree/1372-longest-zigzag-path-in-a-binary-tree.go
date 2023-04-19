b6bfb6a12a0dc6e541b34ab3c405b67881db4018
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-18 18:32:07 -0700

    Time: 153 ms (5.00%), Space: 10.6 MB (95.00%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
@@ -7,31 +7,32 @@
  * }
  */
 func longestZigZag(root *TreeNode) int {
-    q := [][]interface{}{{root, 0, ""}}
+    q := [][]interface{}{{root, 0, 0}}
+    // 1 = right
+    // -1 = left
     max := 0
     for len(q) != 0 {
         dq := q[0]
         q = q[1:]
         node := dq[0].(*TreeNode)
         size := dq[1].(int)
-        dir := dq[2].(string)
+        dir := dq[2].(int)
         if size > max {max = size}
         
         if node.Left != nil {
             newLeftSize := 1
-            if dir == "" || dir == "R" {
+            if dir == 0 || dir == 1 {
                 newLeftSize += size
             }
-            q = append(q, []interface{}{node.Left, newLeftSize, "L"})
+            q = append(q, []interface{}{node.Left, newLeftSize, -1})
         }
         if node.Right != nil {
             newRightSize := 1
-            if dir == "" || dir == "L" {
+            if dir == 0 || dir == -1 {
                 newRightSize += size
             }
-            q = append(q, []interface{}{node.Right, newRightSize, "R"})            
+            q = append(q, []interface{}{node.Right, newRightSize, 1})
         }
-        
     }
     
     
