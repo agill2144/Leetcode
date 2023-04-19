dc39d9b0d6851044aef9453d8a12979555311314
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-18 20:56:42 -0700

    Time: 125 ms (45.00%), Space: 11.1 MB (90.00%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
@@ -6,65 +6,50 @@
  *     Right *TreeNode
  * }
  */
+
+/*
+    keep track of which side per node we are coming from
+    and keep track of number of steps at each node.
+    
+    when current dq'd node came from left
+    and this node has a left child, this is a straight path
+    therefore enqueue left child with steps resetted to 1
+    which also means right child of current node is a continuation of a zigzag,
+    therefore enqueue right child with current steps + 1
+    
+    time; o(n)
+    space: o(n)
+    
+*/
 func longestZigZag(root *TreeNode) int {
-    var dfs func(r *TreeNode, goLeft bool, size int)
-    ans := 0
-    dfs = func(r *TreeNode, goLeft bool, size int) {
-        // base
-        if r == nil {return}
+    q := [][]interface{}{{root, 0, 0}}
+    // 1 = right
+    // -1 = left
+    max := 0
+    for len(q) != 0 {
+        dq := q[0]
+        q = q[1:]
+        node := dq[0].(*TreeNode)
+        size := dq[1].(int)
+        dir := dq[2].(int)
+        if size > max {max = size}
         
-        ans = max(ans, size)
-        
-        
-        // logic
-        if goLeft {
-            dfs(r.Left, false, size+1)
-            dfs(r.Right, true, 1)
-        } else { // supposed to go right
-            dfs(r.Left, false, 1)
-            dfs(r.Right, true, size+1)
+        if node.Left != nil {
+            newLeftSize := 1
+            if dir == 0 || dir == 1 {
+                newLeftSize += size
+            }
+            q = append(q, []interface{}{node.Left, newLeftSize, -1})
         }
-
+        if node.Right != nil {
+            newRightSize := 1
+            if dir == 0 || dir == -1 {
+                newRightSize += size
+            }
+            q = append(q, []interface{}{node.Right, newRightSize, 1})
+        }
     }
-    dfs(root, true, 0)
-    dfs(root, false, 0)
-    return ans
-}
-
-func max(x, y int) int {
-    if x > y {return x}
-    return y
-}
-
-// func longestZigZag(root *TreeNode) int {
-//     q := [][]interface{}{{root, 0, 0}}
-//     // 1 = right
-//     // -1 = left
-//     max := 0
-//     for len(q) != 0 {
-//         dq := q[0]
-//         q = q[1:]
-//         node := dq[0].(*TreeNode)
-//         size := dq[1].(int)
-//         dir := dq[2].(int)
-//         if size > max {max = size}
-        
-//         if node.Left != nil {
-//             newLeftSize := 1
-//             if dir == 0 || dir == 1 {
-//                 newLeftSize += size
-//             }
-//             q = append(q, []interface{}{node.Left, newLeftSize, -1})
-//         }
-//         if node.Right != nil {
-//             newRightSize := 1
-//             if dir == 0 || dir == -1 {
-//                 newRightSize += size
-//             }
-//             q = append(q, []interface{}{node.Right, newRightSize, 1})
-//         }
-//     }
     
     
-//     return max
-// }
+    return max
+}
