8e5c2d1cdc9fec1afa0f44e34d242d349840bf44
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-19 14:32:31 -0700

    Time: 108 ms (95.00%), Space: 13.5 MB (70.00%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
@@ -22,34 +22,59 @@
     
 */
 func longestZigZag(root *TreeNode) int {
-    q := [][]interface{}{{root, 0, 0}}
     // 1 = right
     // -1 = left
-    max := 0
-    for len(q) != 0 {
-        dq := q[0]
-        q = q[1:]
-        node := dq[0].(*TreeNode)
-        size := dq[1].(int)
-        dir := dq[2].(int)
-        if size > max {max = size}
+    ans := 0
+    var dfs func(r *TreeNode, steps int, dir int)
+    dfs = func(r *TreeNode, steps int, dir int) {
+        // base
+        if r == nil {return}
         
-        if node.Left != nil {
-            newLeftSize := 1
-            if dir == 0 || dir == 1 {
-                newLeftSize += size
-            }
-            q = append(q, []interface{}{node.Left, newLeftSize, -1})
-        }
-        if node.Right != nil {
-            newRightSize := 1
-            if dir == 0 || dir == -1 {
-                newRightSize += size
-            }
-            q = append(q, []interface{}{node.Right, newRightSize, 1})
+        // logic
+        ans = max(ans, steps)
+        if dir == 0 || dir == 1 {
+            dfs(r.Left, steps+1, -1)
+            dfs(r.Right, 1, 1)            
+        } else if dir == 0 || dir == -1 {
+            dfs(r.Right, steps+1, 1)            
+            dfs(r.Left, 1, -1)
         }
     }
-    
-    
-    return max
+    dfs(root, 0, 0)
+    return ans
 }
+
+func max(x, y int) int {
+    if x > y {return x}
+    return y
+}
+// func longestZigZag(root *TreeNode) int {
+//     q := [][]interface{}{{root, 0, 0}}
+//     // 1 = right
+//     // -1 = left
+//     max := 0
+//     for len(q) != 0 {
+//         dq := q[0]
+//         q = q[1:]
+//         node := dq[0].(*TreeNode)
+//         size := dq[1].(int)
+//         dir := dq[2].(int)
+//         if size > max {max = size}
+        
+//         if node.Left != nil {
+//             newLeftSize := 1
+//             if dir == 0 || dir == 1 {
+//                 newLeftSize += size
+//             }
+//             q = append(q, []interface{}{node.Left, newLeftSize, -1})
+//         }
+//         if node.Right != nil {
+//             newRightSize := 1
+//             if dir == 0 || dir == -1 {
+//                 newRightSize += size
+//             }
+//             q = append(q, []interface{}{node.Right, newRightSize, 1})
+//         }
+//     }
+//     return max
+// }
