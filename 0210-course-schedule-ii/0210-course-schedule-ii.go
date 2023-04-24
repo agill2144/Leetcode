6cde2fbbd1413152d4d88f4f08591401382eec3d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-23 17:18:50 -0700

    Time: 4 ms (98.55%), Space: 7.5 MB (8.36%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.go
+++ 0210-course-schedule-ii/0210-course-schedule-ii.go
@@ -1,47 +1,38 @@
-// top sort + cycle detection using dfs
-
 func findOrder(numCourses int, prerequisites [][]int) []int {
     adjList := map[int][]int{}
     for i := 0; i < len(prerequisites); i++ {
-        // a depends on b
-        // b is indepndent
-        a := prerequisites[i][0]
-        b := prerequisites[i][1]
-        adjList[b] = append(adjList[b], a)
+        src := prerequisites[i][0]
+        dest := prerequisites[i][1]
+        adjList[src] = append(adjList[src], dest)
     }
-    // count := 0
-    st := []int{}
     visited := make([]bool, numCourses)
-    
+    st := []int{}
     var dfs func(node int, path []bool) bool
-    dfs = func(node int, path []bool) bool {
+    dfs = func(node int , path []bool) bool {
         // base
-        // found a cycle; return false to indicate this is not a valid path
-        if path[node] {return false} 
-        
-        // skip already visited node
+        if path[node] {return false}
         if visited[node] {return true}
         
+        
         // logic
-        path[node] = true
         visited[node] = true
+        
+        // action
+        path[node] = true
         for _, nei := range adjList[node] {
-            if !dfs(nei, path) {return true}
+            // recurse
+            if !dfs(nei, path) {return false}
         }
         
+        // backtrack
         path[node] = false
         st = append(st, node)
         return true
     }
     for i := 0; i < numCourses; i++ {
         if !visited[i] {
-            path := make([]bool, numCourses)
-            if !dfs(i,path) {return nil}
+            if !dfs(i, make([]bool, numCourses)) {return nil}
         }
-    }
-    if len(st) != numCourses {return nil}
-    for i := 0; i < len(st)/2; i++ {
-        st[i], st[len(st)-1-i] = st[len(st)-1-i], st[i]
     }
     return st
 }
