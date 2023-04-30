cebb8ddf2e28089c0ec174015324cb354a10a0c3
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-30 13:56:18 -0700

    Time: 15 ms (13.64%), Space: 6.6 MB (45.45%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.go
@@ -0,0 +1,52 @@
+func shortestAlternatingPaths(n int, redEdges [][]int, blueEdges [][]int) []int {
+    /*
+        colors
+        0 -> Start
+        1 -> blue
+        2 -> red
+    */
+    
+    
+    adjList := map[int][][]int{}
+    for i := 0; i < len(redEdges); i++ {
+        src := redEdges[i][0]
+        dest := redEdges[i][1]
+        adjList[src] = append(adjList[src], []int{dest, 2})
+    }
+
+    for i := 0; i < len(blueEdges); i++ {
+        src := blueEdges[i][0]
+        dest := blueEdges[i][1]
+        adjList[src] = append(adjList[src], []int{dest, 1})
+    }
+    visited := make([][]bool, n)
+    for i := 0; i < n; i++ {
+        visited[i] = make([]bool, 3)
+    }
+    
+    res := make([]int, n)
+    for i := 0; i < len(res); i++ {res[i] = math.MaxInt64}
+    q := [][]int{{0,0,0}}
+    visited[0][0] = true
+
+    for len(q) != 0 {
+        dq := q[0]
+        q = q[1:]
+        node := dq[0]
+        dist := dq[1]
+        color := dq[2]
+        if dist < res[node] {
+            res[node] = dist
+        }
+        for _, nei := range adjList[node] {
+            neiNode := nei[0]
+            neiColor := nei[1]
+            if !visited[neiNode][neiColor] && neiColor != color {
+                q = append(q, []int{neiNode, dist+1, neiColor})            
+                visited[neiNode][neiColor] = true
+            }
+        }
+    }
+    for i := 0; i < len(res); i++ {if res[i] == math.MaxInt64 { res[i] = -1} }
+    return res    
+}
