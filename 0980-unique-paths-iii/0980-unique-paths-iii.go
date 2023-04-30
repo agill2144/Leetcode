7d496dfc8d8c1cf7cdde8a6387c2874f87d1b24c
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-04-29 17:35:32 -0700

    Time: 3 ms (25.00%), Space: 2 MB (75.00%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.go
+++ 0980-unique-paths-iii/0980-unique-paths-iii.go
@@ -1,4 +1,6 @@
+// Note: WE MUST VISIT ALL CELLS THAT ARE REACHABLE FOR A PATH TO COUNT AS 1
 func uniquePathsIII(grid [][]int) int {
+    
     dirs := [][]int{
         {1,0},
         {-1,0},
@@ -6,32 +8,32 @@ func uniquePathsIII(grid [][]int) int {
         {0,-1},
     }
     sr, sc := -1,-1
-    nonObs := 0
+    reachableCells := 0
     for i := 0; i < len(grid); i++ {
         for j := 0; j < len(grid[0]); j++ {
-            if grid[i][j] >= 0 {nonObs++}
+            if grid[i][j] >= 0 {reachableCells++}
             if grid[i][j] == 1 {sr, sc = i,j}
         }
     }
     
     count := 0
-    var dfs func(r, c, remain int)
-    dfs = func(r, c, remain int) {
+    var dfs func(r, c, cellsReached int)
+    dfs = func(r, c, cellsReached int) {
         // base
         if r < 0 || r == len(grid) || c < 0 || c == len(grid[0]) || grid[r][c] == -1 || grid[r][c] == 50 {
             return
         }
-        if grid[r][c] == 2 && remain == 1 {count++; return}
+        if grid[r][c] == 2 && cellsReached == reachableCells {count++; return}
         
         // logic
         tmp := grid[r][c]
-        remain--
+        cellsReached++
         grid[r][c] = 50
         for _, dir := range dirs {
-            dfs(r+dir[0], c+dir[1], remain)
+            dfs(r+dir[0], c+dir[1], cellsReached)
         }
         grid[r][c] = tmp
     }
-    dfs(sr, sc, nonObs)
+    dfs(sr, sc, 1)
     return count
 }
