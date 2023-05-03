79f179bda6751bbba5d499835720365ef4dd2a13
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-02 21:35:31 -0700

    Time: 2 ms (71.64%), Space: 2.1 MB (100.00%) - LeetHub

--- 0068-text-justification/0068-text-justification.go
+++ 0068-text-justification/0068-text-justification.go
@@ -6,15 +6,21 @@ func fullJustify(words []string, maxWidth int) []string {
         charCount := len(words[i])
         j := i+1
         for j < n {
-            numWords := j-i+1
-            minSpaces := numWords-1
-            newCharCount := charCount + len(words[j]) + minSpaces
-            if newCharCount > maxWidth {break}
-            charCount += len(words[j])
+            numWords := j-i+1 // including this word
+            minSpaces := numWords-1 // min number of spaces (1) will always be within these words, therefore numWords-1
+            newCharCount := charCount + len(words[j]) + minSpaces // get the total char count with min spaces
+            if newCharCount > maxWidth {break} // break out if we exceed the maxWidth
+            charCount += len(words[j]) // otherwise, keep on adding the number of chars ( without min space )
             j++
         }
+        // calc the remaining char count left if we have maxWidth and charCount
         remainingCount := maxWidth-charCount
+        // number of words does not include the jth word, because we probably broke at that word
         numWords := j-i
+        // there are 2 main cases here
+        // 1. if number of words == 1 or we are on the last line; then use min space in middle and remaining spaces will be added to the end.
+        // 2. otherwise we need to evenly spread our spaces
+        
         // if its the last line OR number of words in this line is only 1, then we add all spaces to the right
         if numWords == 1 || j >= n {
             out = append(out, addSpacesToRight(i, j, words, remainingCount))
@@ -22,6 +28,7 @@ func fullJustify(words []string, maxWidth int) []string {
         } else {
             out = append(out, addSpacesToMiddleEvenly(i,j, words, remainingCount))
         }
+        // move our i ptr to j
         i = j        
     }
     return out
