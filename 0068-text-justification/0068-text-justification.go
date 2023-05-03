26b868b63ea00755da9b5849f0bdf5b7a0bd6769
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-02 21:27:20 -0700

    Time: 2 ms (71.64%), Space: 2.1 MB (100.00%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.go
@@ -0,0 +1,58 @@
+func fullJustify(words []string, maxWidth int) []string {
+    out := []string{}
+    i := 0
+    n := len(words)
+    for i < n {
+        lineLen := len(words[i])
+        j := i+1
+        for j < n {
+            numWords := j-i+1
+            minSpaces := numWords-1
+            newLineLen := lineLen + len(words[j]) + minSpaces
+            if newLineLen > maxWidth {break}
+            lineLen += len(words[j])
+            j++
+        }
+        diff := maxWidth-lineLen
+        numWords := j-i
+        if numWords == 1 || j >= n {
+            out = append(out, addSpacesToRight(i, j, words, diff))
+        } else {
+            out = append(out, addSpacesToMiddleEvenly(i,j, words, diff))
+        }
+        i = j        
+    }
+    return out
+}
+
+func addSpacesToRight(i, j int, words []string, diff int) string {
+    tmp := new(strings.Builder)
+    remainingSpaces := diff - (j-i-1)
+    for k := i; k < j; k++ {
+        tmp.WriteString(words[k])
+        if k < j-1 {
+            tmp.WriteString(" ")
+        }
+    }
+    for remainingSpaces != 0 {tmp.WriteString(" "); remainingSpaces--}
+    return tmp.String()
+}
+
+func addSpacesToMiddleEvenly(i, j int, words []string, diff int) string {
+    tmp := new(strings.Builder)
+    numSpaces :=  j-i-1
+    spaceSize := diff/numSpaces
+    extraSpaces := diff%numSpaces
+    for k := i; k < j; k++ {
+        tmp.WriteString(words[k])
+        totalSpaces := spaceSize
+        if extraSpaces > 0 {
+            totalSpaces++
+            extraSpaces--
+        }
+        if k < j-1 {
+            for l := 0; l < totalSpaces; l++ {tmp.WriteString(" ")}
+        }
+    }
+    return tmp.String()
+}
