d9c5134c975734c7e7c5d58a9a56ecfb1d671110
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-15 19:55:56 -0700

    Time: 432 ms (39.28%), Space: 68.1 MB (38.57%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.go
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.go
@@ -1,23 +1,21 @@
 func validPath(n int, edges [][]int, source int, destination int) bool {
     adjList := map[int][]int{}
-    for i := 0; i < len(edges); i++ {
+    for i := 0; i < len(edges); i++{
         adjList[edges[i][0]] = append(adjList[edges[i][0]], edges[i][1])
         adjList[edges[i][1]] = append(adjList[edges[i][1]], edges[i][0])
     }
-    visited := map[int]struct{}{}
-
-    var dfs func(node int, prev int) bool
-    dfs = func(node int, prev int) bool {
+    visited := make([]bool, n)
+    var dfs func(node, prev int) bool
+    dfs = func(node, prev int) bool {
         // base
         if node == destination {return true}
-        if _, ok := visited[node]; ok {return false }
+        if visited[node] {return false}
         
         // logic
-        visited[node] = struct{}{}
-        for _, neighbor := range adjList[node] {
-            if neighbor == prev {continue}
-            found := dfs(neighbor, node)
-            if found {return true}
+        visited[node] = true
+        for _, nei := range adjList[node] {
+            if nei == prev {continue}
+            if dfs(nei, node) {return true}
         }
         return false
     }
