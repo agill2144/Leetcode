fe065124eab7f54ec72e448afd3a62faf16813cd
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-17 21:46:25 -0700

    Time: 102 ms (88.46%), Space: 7.5 MB (83.33%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
@@ -4,10 +4,20 @@
 // as well as all other parent nodes that are part of recursion, also have a path that went to a node that ran into a cycle
 // therefore its parent and its parents are also invalid because they have a path somewhere that went to a cycle.
 // time: o(v+e) + o(v)
-// space: o(v) + o(v) + o(v)
+// space: o(v) + o(v) + o(v) + o(v+e)
 func eventualSafeNodes(graph [][]int) []int {
     n := len(graph)
     visited := make([]bool, n)
+    // why ptr to a bool ?
+    // a node could be visited ( ie in the current recursion stack )
+    // but we have not yet identified whether a visited node is valid or not
+    // using a regular bool arr, will be filled with false
+    // and if a node is visited, we cannot assume the value in valid arry is correct
+    // therefore we need a third state to determine whether a node is visited and if it is
+    // do we have an answer for it or not ? 
+    // ptr to bool helps us in that
+    // if we have answered a node, then its value must not be nil, use that value as a memoized value
+    // if this node is in our recursion stack but we have not yet identified whether its a safe node or not, then its value will be nil instead of false
     valid := make([]*bool, n)
     toPtrBool := func(b bool) *bool {return &b}
     
