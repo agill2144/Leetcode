8de35154acb0a60e57c972cb56b907eabddad4b6
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-23 20:55:23 -0700

    Time: 369 ms (14.81%), Space: 7.1 MB (59.79%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.go
+++ 0127-word-ladder/0127-word-ladder.go
@@ -1,46 +1,50 @@
 func ladderLength(beginWord string, endWord string, wordList []string) int {
     adjList := map[string][]string{}
-    hasEndWord := false
+    
     wordList = append(wordList, beginWord)
+    foundEnd := false
     for i := 0; i < len(wordList); i++ {
         parent := wordList[i]
-        if parent == endWord {hasEndWord=true}
+        if parent == endWord {foundEnd = true}
         for j := 0; j < len(wordList); j++ {
             if i == j {continue}
             child := wordList[j]
-            countDiffChars := 0
-            p := 0
-            c := 0
+            p, c := 0, 0
+            diff := 0
             for p < len(parent) && c < len(child) {
-                if parent[p] != child[c] {countDiffChars++}
+                if parent[p] != child[c] {
+                    diff++
+                } 
                 p++
                 c++
             }
-            if countDiffChars == 1 {
+            if diff == 1 {
                 adjList[parent] = append(adjList[parent], child)
             }
         }
     }
-    if !hasEndWord {return 0}
-    
+    if !foundEnd {return 0}
     q := []string{beginWord}
+    visited := map[string]struct{}{}
+    visited[beginWord] = struct{}{}
+    
     level := 1
-    visited := map[string]struct{}{beginWord: struct{}{}}
     for len(q) != 0 {
-        qSize := len(q)
+        qSize := len(q) 
         for qSize != 0 {
             dq := q[0]
             q = q[1:]
             if dq == endWord {return level}
-
             for _, nei := range adjList[dq] {
-                if _, ok := visited[nei]; ok {continue}
-                q = append(q, nei)
-                visited[nei] = struct{}{}
+                if _, ok := visited[nei]; !ok {
+                    q = append(q, nei)
+                    visited[nei] = struct{}{}
+                }
             }
             qSize--
         }
-        level++
+        level++        
     }
+    
     return 0
 }
