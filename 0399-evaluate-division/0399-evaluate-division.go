c1d9d1b98022c7a7dfd708fa8fccc47fc6ede470
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-24 22:00:19 -0700

    Time: 2 ms (55.85%), Space: 2.2 MB (15.01%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.go
@@ -0,0 +1,47 @@
+func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
+    type adjNode struct {
+        weight float64
+        node string
+    }
+    adjList := map[string][]*adjNode{}
+    for i := 0; i < len(equations); i++ {
+        a := equations[i][0]
+        b := equations[i][1]
+        w := values[i]
+        adjList[a] = append(adjList[a], &adjNode{node: b, weight: w})
+        adjList[b] = append(adjList[b], &adjNode{node: a, weight: 1.0/w})
+    }
+    ans := []float64{}
+    
+    var dfs func(node, end string, prod float64, visited map[string]struct{}) float64
+    dfs = func(node, end string, prod float64, visited map[string]struct{}) float64 {
+        // base
+        if node == end {
+            return prod
+        }
+        
+        // logic
+        visited[node] = struct{}{}
+        for _, nei := range adjList[node] {
+            if _, ok := visited[nei.node]; ok {continue}
+            newProd := prod * nei.weight
+            if val := dfs(nei.node, end, newProd, visited); val != -1.0 {delete(visited,node); return val}
+        }
+        delete(visited, node)
+        return -1.0
+    }
+
+    v := map[string]struct{}{}
+    for i := 0; i < len(queries); i++ {
+        query := queries[i]
+        src := query[0]
+        dest := query[1]
+        if _, ok := adjList[src]; !ok {ans = append(ans, -1.0); continue}
+        if _, ok := adjList[dest]; !ok {ans = append(ans, -1.0); continue}
+        ans = append(ans, dfs(src, dest, 1.0, v))
+        
+    
+    }
+    return ans
+}
+
