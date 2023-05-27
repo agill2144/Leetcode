3632227e3ae89e90b0e5b46bdb59ae9940332550
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-27 14:31:34 -0700

    Time: 33 ms (61.90%), Space: 7.4 MB (50.00%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.go
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.go
@@ -9,8 +9,8 @@ func longestIncreasingPath(matrix [][]int) int {
     for i := 0; i < m; i++ {memo[i] = make([]int, n)}
     
 
-    var dfs func(r, c, count int) int
-    dfs = func(r, c, count int) int {
+    var dfs func(r, c int) int
+    dfs = func(r, c int) int {
         // base
         // already solved? return the solved value
         if memo[r][c] != 0 {
@@ -18,13 +18,21 @@ func longestIncreasingPath(matrix [][]int) int {
         }
         
         // logic
-        // otherwise solve for this cell, initially the count will be 1
+        // otherwise solve for this cell
+        // initially the count will be 1
         memo[r][c] = 1
         for _, dir := range dirs {
             nr := r+dir[0]
             nc := c+dir[1]
-            if nr >= 0 && nr < m && nc >= 0 && nc < n && matrix[nr][nc] > matrix[r][c] {        
-                memo[r][c] = max(memo[r][c], dfs(nr, nc, 1)+1)
+            if nr >= 0 && nr < m && nc >= 0 && nc < n && matrix[nr][nc] > matrix[r][c] {
+                // if the neighboring cell is inbound
+                // and its value > current cell
+                // recurse and find out best ans for neighbor
+                // the +1 is part of the recursive call because
+                // the neighbor cell is increasing compared to current
+                // so if current cell longest-increasing path is of size 1 and
+                // we discovered a new adjacent cell to explore, the new size will be currentCount + 1
+                memo[r][c] = max(memo[r][c], dfs(nr, nc)+1)
             }
         }
         return memo[r][c]
@@ -34,7 +42,7 @@ func longestIncreasingPath(matrix [][]int) int {
     
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            dfs(i, j, 1)
+            dfs(i, j)
             if memo[i][j] > maxCount {maxCount = memo[i][j]} 
         }
     }
