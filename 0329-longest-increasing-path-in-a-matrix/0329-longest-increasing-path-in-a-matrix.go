671dd6675af91302974dee8fcd515b4fa511a829
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-27 14:18:07 -0700

    Time: 29 ms (80.95%), Space: 7.1 MB (94.05%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.go
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.go
@@ -1,48 +1,39 @@
-func longestIncreasingPath(matrix [][]int) int {
-    dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
-    m := len(matrix)
-    n := len(matrix[0])
-    maxCount := 0
-    
-    memo := make([][]int, m)
-    for i := 0; i < m; i++ {memo[i] = make([]int, n)}
-//     for i := 0; i < m; i++ {
-//         for j := 0; j < n; j++ {
-//             memo[i][j] = -1 // not-yet-solved
-//         }
-//     }
-    
-    var dfs func(r, c, count int) int
-    dfs = func(r, c, count int) int {
-        // base
-        if memo[r][c] != 0 {
-            return memo[r][c]
-        }
-        
-        // logic
-        memo[r][c] = count
-        for _, dir := range dirs {
-            nr := r+dir[0]
-            nc := c+dir[1]
-            if nr >= 0 && nr < m && nc >= 0 && nc < n && matrix[nr][nc] > matrix[r][c] {        
-                memo[r][c] = max(memo[r][c], dfs(nr, nc, 1)+1)
-            }
-        }
-        return memo[r][c]
-    }
-    
-
-    
+func countPaths(grid [][]int) int {
+    m := len(grid)
+    n := len(grid[0])
+    dp := make([][]int, m)
+    for i := 0; i < m; i++ {dp[i] = make([]int, n)}
+    upDirs := [][]int{{-1,0},{0,-1}}
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            dfs(i, j, 1)
-            if memo[i][j] > maxCount {maxCount = memo[i][j]} 
+            curr := grid[i][j]
+            total := 0
+            for _, dir := range upDirs {
+                r := i + dir[0]
+                c := j + dir[1]
+                if r >= 0 && r < m && c >= 0 && c < n && grid[r][c] > curr {
+                    total++
+                }
+            }
+            dp[i][j] = total+1
         }
     }
-    return maxCount
-}
-
-func max(x, y int) int {
-    if x > y {return x}
-    return y
+    count := 0    
+    downDirs := [][]int{{1,0},{0,1}}
+    for i := m-1; i >= 0; i-- {
+        for j := n-1; j >= 0; j-- {
+            curr := grid[i][j]
+            total := dp[i][j]
+            for _, dir := range downDirs {
+                r := i + dir[0]
+                c := j + dir[1]
+                if r >= 0 && r < m && c >= 0 && c < n && grid[r][c] > curr {
+                    total+=dp[r][c]
+                }
+            }
+            dp[i][j] = total
+            count += dp[i][j]
+        }
+    }
+    return count
 }
