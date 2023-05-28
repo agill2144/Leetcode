452c8fa7e396ee825fd9c39531fca035fb0c7640
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-28 16:37:33 -0700

    Time: 121 ms (55.95%), Space: 7.9 MB (60.71%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.go
@@ -1,48 +1,90 @@
 func eventualSafeNodes(graph [][]int) []int {
-    safeNodes := make([]*bool, len(graph))
-    toPtrBool := func(b bool) *bool {return &b} 
+    adjList := map[int][]int{}
+    indegrees := make([]int, len(graph))
     for i := 0; i < len(graph); i++ {
-        if len(graph[i]) == 0 {
-            safeNodes[i] = toPtrBool(true)
+        child := i
+        indegrees[child] += len(graph[i])
+        for _, parent := range graph[i] {
+            adjList[parent] = append(adjList[parent], child)
+            
         }
     }
     
-    var dfs func(node int, path []bool) bool
-    dfs = func(node int, path []bool) bool {
-        // base
-        if path[node] {return false}
-        if safeNodes[node] != nil {
-            return *safeNodes[node]
+    q := []int{}
+    for i := 0; i < len(indegrees); i++ {
+        if indegrees[i] == 0 {
+            q = append(q, i)
         }
-        
-        // logic
-        path[node] = true
-        for _, nei := range graph[node] {
-            if !dfs(nei, path) {
-                path[node] = false
-                safeNodes[node] = toPtrBool(false)
-                return false
+    }
+    if len(q) == 0 {return nil}
+
+    valid := make([]bool, len(graph))
+    for len(q) != 0 {
+        dq := q[0]
+        q = q[1:]
+        valid[dq] = true
+        for _, nei := range adjList[dq] {
+            indegrees[nei]--
+            if indegrees[nei] == 0 {
+                q = append(q, nei)
             }
         }
-        path[node] = false
-        safeNodes[node] = toPtrBool(true)
-        return true
     }
-
-    path := make([]bool, len(graph))
-    for i := 0; i < len(graph); i++ {
-        if safeNodes[i] == nil {
-            dfs(i, path)
-        }
-    }
-    
     out := []int{}
-    for i := 0; i < len(safeNodes); i++ {
-        if safeNodes[i] != nil && *safeNodes[i] {
+    for i := 0; i < len(valid); i++ {
+        if valid[i] {
             out = append(out, i)
         }
     }
     return out
+    
 }
+// func eventualSafeNodes(graph [][]int) []int {
+//     safeNodes := make([]*bool, len(graph))
+//     toPtrBool := func(b bool) *bool {return &b} 
+//     for i := 0; i < len(graph); i++ {
+//         if len(graph[i]) == 0 {
+//             safeNodes[i] = toPtrBool(true)
+//         }
+//     }
+    
+//     var dfs func(node int, path []bool) bool
+//     dfs = func(node int, path []bool) bool {
+//         // base
+//         if path[node] {return false}
+//         if safeNodes[node] != nil {
+//             return *safeNodes[node]
+//         }
+        
+//         // logic
+//         path[node] = true
+//         for _, nei := range graph[node] {
+//             if !dfs(nei, path) {
+//                 path[node] = false
+//                 safeNodes[node] = toPtrBool(false)
+//                 return false
+//             }
+//         }
+//         path[node] = false
+//         safeNodes[node] = toPtrBool(true)
+//         return true
+//     }
 
-// o(v) + o(v+e) + o(v) + o(vlogv)
+//     path := make([]bool, len(graph))
+//     for i := 0; i < len(graph); i++ {
+//         if safeNodes[i] == nil {
+//             dfs(i, path)
+//         }
+//     }
+    
+//     out := []int{}
+//     for i := 0; i < len(safeNodes); i++ {
+//         if safeNodes[i] != nil && *safeNodes[i] {
+//             out = append(out, i)
+//         }
+//     }
+//     return out
+// }
+
+// o(v) + o(v+e) + o(v) + o(vlogv) --- if sorting
+// o(v) + o(v+e) + o(v) -- if not sorting
