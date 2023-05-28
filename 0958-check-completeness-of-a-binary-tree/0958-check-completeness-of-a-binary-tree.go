287023097cf1196682fb6b21553185cb042788d8
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-27 21:46:31 -0700

    Time: 5 ms (37.93%), Space: 3.2 MB (79.31%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.go
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.go
@@ -6,24 +6,20 @@
  *     Right *TreeNode
  * }
  */
-// insert all nil nodes blindly, and as soon as we run into a nil node, flag it and do not add its child
-// if we run into a regular node and we have already seen a nil node, then this cannot be a complete tree
-// because a regular node should have came before a nil node if that regular node was on the farthest left side possible.
-// This works because in bfs we process left to right. that is left first.
-// if while processing from left to right, we encounter a nil node, flag it ( nothing to the right of this should be a regular node )
 func isCompleteTree(root *TreeNode) bool {
     if root == nil {return true}
-
-    nullSeen := false
     q := []*TreeNode{root}
+    nilSeen := false    
     for len(q) != 0 {
+
         dq := q[0]
         q = q[1:]
-        if dq != nil && nullSeen {return false}
-        if dq == nil {nullSeen = true; continue}
-        
+        if dq != nil && nilSeen {return false}
+        if dq == nil {nilSeen = true; continue}
+
         q = append(q, dq.Left)
         q = append(q, dq.Right)
-    }
+        
+    } 
     return true
 }
