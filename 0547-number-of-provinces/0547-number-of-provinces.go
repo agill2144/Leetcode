5f81f9ba1c6d5dcc5a38c37149caa1ea3ae62819
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-29 15:42:33 -0700

    Time: 23 ms (50.43%), Space: 7 MB (49.15%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.go
+++ 0547-number-of-provinces/0547-number-of-provinces.go
@@ -1,45 +1,31 @@
-/*
-    approach: dfs
-    - find number of connected components in a undirected graph
-    - given graphs that are disconnected 
-    - count number of graphs
-    - the input is a adj matrix
-        - in adj matrix, i<->j is an edge if at matrix[i][j] = 1
-        - because its undirected, we have to add the other direction too, therefore matrix[j][i] will also be 1
-        - there will be nodes in the adjMatrix, who do not have an edge to anyone, and thats a valid graph too
-            - these are called self nodes where i == j
-*/
 func findCircleNum(isConnected [][]int) int {
+    m := len(isConnected)
+    n := len(isConnected[0])
     adjList := map[int][]int{}
-    n := len(isConnected)
-    for i := 0; i < n; i++ {
+    for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            if isConnected[i][j] == 1 && i != j { 
-                // add both edges because this is an undirected graph
+            if isConnected[i][j] == 1 && i != j {
                 adjList[i] = append(adjList[i], j)
-                adjList[j] = append(adjList[j], i)
             }
         }
     }
-    
-    visited := map[int]struct{}{}
-    var dfs func(node int, prev int)
-    dfs = func(node int, prev int) {
+    visited := make([]bool, m)
+    var dfs func(node int)
+    dfs = func(node int) {
         // base
-        if _, ok := visited[node]; ok {return}
+        if visited[node] {return}
         
         // logic
-        visited[node] = struct{}{}
+        visited[node] = true
         for _, nei := range adjList[node] {
-            if nei == prev {continue}
-            dfs(nei, node)
+            dfs(nei)
         }
     }
     
     count := 0
     for i := 0; i < n; i++ {
-        if _, ok := visited[i]; !ok {
-            dfs(i,-1)
+        if !visited[i] {
+            dfs(i)
             count++
         }
     }
