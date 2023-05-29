ad99a230a05080274991ee83daf8f365867e23e8
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-05-29 16:06:12 -0700

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.py
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.py
@@ -4,35 +4,64 @@
 class Solution:
     
     #Function to detect cycle in a directed graph.
-    def isCyclic(self, v, adj):
-        # code here
-        indegrees = [0] * v
-        for i in range(len(adj)):
-            for child in adj[i]:
-                indegrees[child] += 1
+    # def isCyclic(self, v, adj):
+    #     # code here
+    #     indegrees = [0] * v
+    #     for i in range(len(adj)):
+    #         for child in adj[i]:
+    #             indegrees[child] += 1
         
+    #     visited = [False] * v
+        
+    #     q = []
+    #     for i in range(len(indegrees)):
+    #         if indegrees[i] == 0:
+    #             q.append(i)
+    #             visited[i] = True
+        
+    #     count = 0
+    #     while len(q) != 0:
+    #         dq = q[0]
+    #         q = q[1:]
+    #         count += 1
+    #         for nei in adj[dq]:
+    #             indegrees[nei] -= 1
+    #             if indegrees[nei] == 0 and not visited[nei]:
+    #                 q.append(nei)
+        
+    #     if count == v:
+    #         return False
+        
+    #     return True
+    
+    
+    def isCyclic(self, v, adj):
         visited = [False] * v
         
-        q = []
-        for i in range(len(indegrees)):
-            if indegrees[i] == 0:
-                q.append(i)
-                visited[i] = True
+        def dfs(node, path):
+            # base
+            if path[node]:
+                return False
+            if visited[node]:
+                return True
         
-        count = 0
-        while len(q) != 0:
-            dq = q[0]
-            q = q[1:]
-            count += 1
-            for nei in adj[dq]:
-                indegrees[nei] -= 1
-                if indegrees[nei] == 0 and not visited[nei]:
-                    q.append(nei)
+            # logic
+            path[node] = True
+            visited[node] = True
+            for nei in adj[node]:
+                if not dfs(nei, path):
+                    return False
+            path[node] = False
+            return True
         
-        if count == v:
-            return False
+        path = [False] * v
+        for i in range(v):
+            if not visited[i]:
+                if not dfs(i, path):
+                    return True
         
-        return True
+        return False
+
 
 
 #{ 
