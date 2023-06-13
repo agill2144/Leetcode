625698e3e6830b0c9481df00c0ae8a823894cee7
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-12 22:09:37 -0700

    Time: 129 ms (39.47%), Space: 8.5 MB (42.11%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.go
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.go
@@ -1,62 +1,57 @@
+type Vertex struct {
+    node int
+    probFromStart float64
+}
+type Edge struct {
+    destNode int
+    prob float64
+}
+
+type PriorityQueue []*Vertex
+func (pq PriorityQueue) Len() int { return len(pq) }
+func (pq PriorityQueue) Less(i, j int) bool { return pq[i].probFromStart > pq[j].probFromStart }
+func (pq PriorityQueue) Swap(i, j int) {
+    pq[i], pq[j] = pq[j], pq[i]
+}
+func (pq *PriorityQueue) Push(x any) {
+    *pq = append(*pq, x.(*Vertex))
+}
+func (pq *PriorityQueue) Pop() any {
+    n := len(*pq)
+    vertex := (*pq)[n-1]
+    *pq = (*pq)[:n-1]
+    return vertex
+}
+
 func maxProbability(n int, edges [][]int, succProb []float64, start int, end int) float64 {
-    adjList := map[float64][][]float64{}
-    for i := 0; i < len(edges); i++ {
-        src := float64(edges[i][0])
-        dest := float64(edges[i][1])
-        srcToDestProb := succProb[i]
-        adjList[src] = append(adjList[src], []float64{dest, srcToDestProb})
-        adjList[dest] = append(adjList[dest], []float64{src, srcToDestProb})
+    edgesFrom := make([][]Edge, n)
+    for u := range edgesFrom { edgesFrom[u] = []Edge{} }
+    for i := range edges {
+        u, v, w := edges[i][0], edges[i][1], succProb[i]
+        if w == 0 { continue }
+        edgesFrom[u] = append(edgesFrom[u], Edge{v, w})
+        edgesFrom[v] = append(edgesFrom[v], Edge{u, w})
     }
-    pq := &maxHeap{items: [][]float64{}}
-    heap.Push(pq, []float64{float64(start), 1.0})
-    probs := make([]float64, n)
-    probs[start] = 1.0
-    
-    for pq.Len() != 0 {
-        dq := heap.Pop(pq).([]float64)
-        node := dq[0]
-        prob := dq[1]
-        if node == float64(end) {
-            return prob
-        }
-        
-        for _, nei := range adjList[node] {
-            neiNode := nei[0]
-            neiProb := nei[1]
-            newNeiProb := prob * neiProb
-            if newNeiProb > probs[int(neiNode)] {
-                probs[int(neiNode)] = newNeiProb
-                heap.Push(pq, []float64{neiNode, newNeiProb})
-            }
+
+    visited := make([]bool, n)  // initially all false
+    maxProbFromStart := make([]float64, n)  // initially all 0
+    pq := PriorityQueue{&Vertex{start, 1}}
+
+    for pq.Len() > 0 {
+        vertex := heap.Pop(&pq).(*Vertex)
+
+        if vertex.node == end { return vertex.probFromStart }
+
+        if visited[vertex.node] { continue }
+        visited[vertex.node] = true
+
+        for _, e := range edgesFrom[vertex.node] {
+            if visited[e.destNode] { continue }
+            newProbFromStart := vertex.probFromStart * e.prob
+            if newProbFromStart <= maxProbFromStart[e.destNode] { continue }
+            maxProbFromStart[e.destNode] = newProbFromStart
+            heap.Push(&pq, &Vertex{e.destNode, newProbFromStart})
         }
     }
-    return 0.0
-}
-
-
-type maxHeap struct {
-    items [][]float64 // {node, prob}
-}
-
-/*
-    type Interface interface {
-        Sort
-        Pop() T
-        Push(x T)
-    }
-*/
-
-func (m *maxHeap) Len() int {return len(m.items)}
-func (m *maxHeap) Swap(i, j int) {m.items[i], m.items[j] = m.items[j], m.items[i]}
-func (m *maxHeap) Push(x interface{}) {m.items = append(m.items, x.([]float64))}
-func (m *maxHeap) Less(i, j int) bool {
-    if m.items[i][1] == m.items[j][1] {
-        return m.items[i][0] > m.items[j][0]
-    }
-    return m.items[i][1] > m.items[j][1]
-}
-func (m *maxHeap) Pop() interface{} {
-    out := m.items[len(m.items)-1]
-    m.items = m.items[:len(m.items)-1]
-    return out
+    return 0
 }
