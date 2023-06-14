0cc55b5ab91e2dd5636cd7550d5d2c74d8a8ef5d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-13 21:27:07 -0700

    Time: 108 ms (87.50%), Space: 19.3 MB (75.00%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.go
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.go
@@ -1,27 +1,27 @@
 func minTime(n int, edges [][]int, hasApple []bool) int {
-    adjList := map[int][]int{}
-    for i := 0; i < len(edges); i++ {
-        adjList[edges[i][0]] = append(adjList[edges[i][0]], edges[i][1]) 
-        adjList[edges[i][1]] = append(adjList[edges[i][1]], edges[i][0]) 
-    }
-
-    count := 0
-    var dfs func(node int, prev int) int
-    dfs = func(node, prev int) int {
-        // base
-        // logic
-        childHasApples := false
-        for _, nei := range adjList[node] {
-            if nei == prev {continue}
-            childAppleCount := dfs(nei, node)
-            if childAppleCount != 0 {
-                childHasApples = true
-            }
-            count += childAppleCount
-        }
-        if childHasApples || hasApple[node] {return 2}
-        return 0
-    }
-    dfs(0, -1)
-    return count
+	connections := make([][]int32, n)
+	for _, edge := range edges {
+		connections[edge[0]] = append(connections[edge[0]], int32(edge[1]))
+		connections[edge[1]] = append(connections[edge[1]], int32(edge[0]))
+	}
+	var dfs func(nodeIdx int32) int32
+	dfs = func(nodeIdx int32) int32 {
+		var result int32
+		children := connections[nodeIdx]
+		connections[nodeIdx] = nil
+		for _, child := range children {
+			if connections[child] != nil {
+				result += dfs(child)
+			}
+		}
+		if result != 0 || hasApple[nodeIdx] {
+			result++
+		}
+		return result
+	}
+	result := int(dfs(0))
+	if result != 0 {
+		result = (result - 1) << 1
+	}
+	return result
 }
