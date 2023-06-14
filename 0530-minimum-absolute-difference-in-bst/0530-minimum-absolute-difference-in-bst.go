384243591eedd9d3dafd3f7d94444f1a48cb5da6
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-13 19:11:47 -0700

    Time: 4 ms (98.47%), Space: 6.8 MB (38.57%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.go
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.go
@@ -8,21 +8,21 @@
  */
 func getMinimumDifference(root *TreeNode) int {
     var prev *TreeNode
-    min := math.MaxInt64
-    var dfs func(r *TreeNode) 
+    minDiff := math.MaxInt64
+    var dfs func(r *TreeNode)
     dfs = func(r *TreeNode) {
         // base
-        if r == nil {return}        
+        if r == nil {return}
+        
         // logic
         dfs(r.Left)
         if prev != nil {
-            if r.Val - prev.Val < min {
-                min = r.Val - prev.Val
-            }
+            diff := r.Val - prev.Val
+            if diff < minDiff {minDiff = diff}
         }
         prev = r
         dfs(r.Right)
     }
     dfs(root)
-    return min
+    return minDiff
 }
