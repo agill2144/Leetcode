524712e8eda8b5fb002b0ac5c72255be1bfa712a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-14 21:25:08 -0700

    Time: 218 ms (83.33%), Space: 18.7 MB (88.89%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.go
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.go
@@ -7,53 +7,48 @@
  * }
  */
 func kthLargestLevelSum(root *TreeNode, k int) int64 {
-    mh := &minHeap{items: []int64{}}
+    if root == nil {return 0}
+    nums := []int64{}
     q := []*TreeNode{root}
-    levels := 0
-    
     for len(q) != 0 {
         qSize := len(q)
-        var levelSum int64 = 0
+        var levelSum int64
         for qSize != 0 {
             dq := q[0]
             q = q[1:]
             levelSum += int64(dq.Val)
-            if dq.Left != nil {
-                q = append(q, dq.Left)
-            }
-            if dq.Right != nil {
-                q = append(q, dq.Right)
-            }
+            if dq.Left != nil {q = append(q, dq.Left)}
+            if dq.Right != nil {q = append(q, dq.Right)}
             qSize--
         }
-        levels++
-        heap.Push(mh, levelSum)
-        if mh.Len() > k {
-            heap.Pop(mh)
+        nums = append(nums, levelSum)
+    }
+    
+    targetIdx := len(nums)-k
+    if targetIdx < 0 || targetIdx == len(nums) {return -1}
+    l := 0
+    r := len(nums)-1
+    for l <= r {
+        
+        nextSmaller := l
+        pivotIdx := r
+        for i := l; i < pivotIdx; i++ {
+            if nums[i] < nums[pivotIdx] {
+                nums[i], nums[nextSmaller] = nums[nextSmaller], nums[i]
+                nextSmaller++
+            }
         }
+        nums[nextSmaller], nums[pivotIdx] = nums[pivotIdx], nums[nextSmaller]
+        if targetIdx == nextSmaller {
+            return nums[nextSmaller]
+        }
+        
+        if targetIdx > nextSmaller {
+            l = nextSmaller+1
+        } else {
+            r = nextSmaller-1
+        }
+        
     }
-    if k > levels {return -1}
-    return int64(mh.items[0])
-}
-
-
-/**
-    type Interface interface{
-        sort.Interface
-        Pop()
-        Push()
-    }
-**/
-
-type minHeap struct {
-    items []int64
-}
-func (m *minHeap) Len() int {return len(m.items)}
-func (m *minHeap) Swap(i, j int) {m.items[i], m.items[j] = m.items[j], m.items[i]}
-func (m *minHeap) Less(i, j int) bool { return m.items[i] < m.items[j] }
-func (m *minHeap) Push(x interface{}) {m.items = append(m.items, x.(int64))}
-func (m *minHeap) Pop() interface{} {
-    out := m.items[len(m.items)-1]
-    m.items = m.items[:len(m.items)-1]
-    return out
+    return nums[targetIdx]
 }
