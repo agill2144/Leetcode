c561908f830c6a131e51860360f7ff03985c69d1
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-19 13:00:06 -0700

    Time: 1 ms (70.37%), Space: 1.9 MB (35.19%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.go
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.go
@@ -2,7 +2,9 @@
 func minMutation(startGene string, endGene string, bank []string) int {
     adjList := map[string][]string{}
     foundEnd := false
-    foundStart := false 
+    foundStart := false
+    
+    // o(b)
     for i := 0; i < len(bank); i++ {
         if bank[i] == endGene {foundEnd = true}
         if bank[i] == startGene{foundStart = true}
@@ -10,13 +12,16 @@ func minMutation(startGene string, endGene string, bank []string) int {
     if !foundEnd {return -1}
     if !foundStart {bank = append(bank, startGene)}
 
+    // o(b)
     for i := 0; i < len(bank); i++ {
         parent := bank[i]
+        // o(b)
         for j := 0; j < len(bank); j++ {
             if i == j {continue}
             diff := 0
             child := bank[j]
             p,c := 0,0
+            // o(k) where k is avg len of word
             for p < len(parent) && c < len(child) {
                 if parent[p] != child[c] {
                     diff++
@@ -30,9 +35,13 @@ func minMutation(startGene string, endGene string, bank []string) int {
         }
     }
     
+    // time so far = o(b) + o(b)xo(b)xo(k) = o(b) + o(b^2) * o(k)
+    
+    
     visited := map[string]struct{}{}
     q := []string{startGene}
     level := 0
+    // o(b+e) ; where e is total number of edges
     for len(q) != 0 {
         qSize := len(q)
         for qSize != 0 {
