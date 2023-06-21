1f92d00a977a0a44986bd036ebfee5f205a27f59
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-20 22:28:22 -0700

    Time: 1 ms (68.67%), Space: 2.1 MB (14.46%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.go
+++ 0140-word-break-ii/0140-word-break-ii.go
@@ -1,30 +1,27 @@
 func wordBreak(s string, wordDict []string) []string {
-    wordSet := map[string]struct{}{}
-    for i := 0; i < len(wordDict); i++ {
-        wordSet[wordDict[i]] = struct{}{}
-    }
+    set := map[string]struct{}{}
+    for i := 0; i < len(wordDict); i++ { set[wordDict[i]] = struct{}{} }
+
     out := []string{}
-    
-    var dfs func(start int, sentence string) 
-    dfs = func(start int, sentence string) {
+    var dfs func(start int, sent []string)
+    dfs = func(start int, sent []string) {
         // base
         if start == len(s) {
-            out = append(out, sentence)
+            out = append(out, strings.Join(sent, " "))
             return
         }
-        
+
+
         // logic
-        for i := start ; i < len(s); i++ {
-            subStr := s[start:i+1]
-            if _, ok := wordSet[subStr]; ok {
-                newSent := sentence + " " + subStr
-                if sentence == "" {
-                    newSent = subStr
-                }
-                dfs(i+1, newSent)
+        for i := start; i < len(s); i++ {
+            subStr := string(s[start:i+1])
+            if _, ok := set[subStr]; ok {
+                sent = append(sent, subStr)
+                dfs(i+1, sent)
+                sent = sent[:len(sent)-1]
             }
         }
     }
-    dfs(0, "")
+    dfs(0, []string{})
     return out
 }
