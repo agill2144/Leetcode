2f244b342a3296ce7768cea4fe99ae1dd1d981b9
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-23 22:38:34 -0700

    Time: 13 ms (65.49%), Space: 6.5 MB (30.97%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.go
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.go
@@ -6,29 +6,31 @@ func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
         cost := flights[i][2]
         adjList[from] = append(adjList[from], []int{to, cost})
     }
-    steps := make([]int, n)
-    for i := 0; i < n; i++ {steps[i] = math.MaxInt64}
-    steps[src] = 0
-    pq := &minHeap{items: [][]int{{src,0,0}}} // <node, cost, k>
+    stops := make([]int, n)
+    for i := 0; i < len(stops); i++ {stops[i] = math.MaxInt64}
+    stops[src] = 0
+    pq := &minHeap{items: [][]int{{src,0,0}}}
     for pq.Len() != 0 {
         dq := heap.Pop(pq).([]int)
         currNode := dq[0]
         currCost := dq[1]
         currSteps := dq[2]
         if currNode == dst {return currCost}
-        steps[currNode] = currSteps
         for _, adjNodeList := range adjList[currNode] {
             adjNode := adjNodeList[0]
             adjNodeCost := adjNodeList[1]+currCost
             adjNodeSteps := currSteps+1
             // if this node is already visited, only process if we are reaching with shorter amount of steps
             // and make sure we are within k+1 steps limit
-            if adjNodeSteps <= k+1 && adjNodeSteps < steps[adjNode] {
+            if adjNodeSteps <= k+1 && adjNodeSteps < stops[adjNode] {
+                stops[currNode] = currSteps
+
                 heap.Push(pq, []int{adjNode, adjNodeCost, adjNodeSteps})
             }
         }
     }
     return -1
+
 }
 
 
@@ -40,7 +42,7 @@ func (m *minHeap) Len() int {return len(m.items)}
 func (m *minHeap) Swap(i, j int) {m.items[i], m.items[j] = m.items[j], m.items[i]}
 func (m *minHeap) Push(x interface{}) {m.items = append(m.items, x.([]int))}
 func (m *minHeap) Less(i, j int) bool {
-    //  prioritize min cost
+    //  minimize cost
     return m.items[i][1] < m.items[j][1]
 }
 func (m *minHeap) Pop() interface{} {
