397a38fcbbbf9ca46c10f0900803ef764f9cbef5
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-27 23:20:13 -0700

    Time: 167 ms (29.79%), Space: 12.7 MB (6.38%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.go
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.go
@@ -3,56 +3,39 @@ func maxProbability(n int, edges [][]int, succProb []float64, start int, end int
     for i := 0; i < len(edges); i++ {
         src := float64(edges[i][0])
         dest := float64(edges[i][1])
-        srcToDestProb := succProb[i]
-        adjList[src] = append(adjList[src], []float64{dest, srcToDestProb})
-        adjList[dest] = append(adjList[dest], []float64{src, srcToDestProb})
+        w := succProb[i]
+        adjList[src] = append(adjList[src], []float64{dest,w})
+        adjList[dest] = append(adjList[dest], []float64{src,w})
     }
-    pq := &maxHeap{items: [][]float64{}}
-    heap.Push(pq, []float64{float64(start), 1.0})
     probs := make([]float64, n)
+    pq := &maxHeap{items: [][]float64{ {float64(start), 1.0} }}
     probs[start] = 1.0
-    
     for pq.Len() != 0 {
         dq := heap.Pop(pq).([]float64)
         node := dq[0]
-        prob := dq[1]
-        if node == float64(end) {
-            return prob
-        }
+        currProb := dq[1]
+        if node == float64(end) {return currProb}
         
         for _, nei := range adjList[node] {
             neiNode := nei[0]
-            neiProb := nei[1]
-            newNeiProb := prob * neiProb
-            if newNeiProb > probs[int(neiNode)] {
-                probs[int(neiNode)] = newNeiProb
-                heap.Push(pq, []float64{neiNode, newNeiProb})
+            neiProb := currProb * nei[1]
+            if neiProb > probs[int(neiNode)] {
+                probs[int(neiNode)] = neiProb
+                heap.Push(pq, []float64{neiNode, neiProb})
             }
         }
     }
     return 0.0
 }
 
-
 type maxHeap struct {
-    items [][]float64 // {node, prob}
+    items [][]float64 // [ [node, probSoFar] ]
 }
-
-/*
-    type Interface interface {
-        Sort
-        Pop() T
-        Push(x T)
-    }
-*/
 
 func (m *maxHeap) Len() int {return len(m.items)}
 func (m *maxHeap) Swap(i, j int) {m.items[i], m.items[j] = m.items[j], m.items[i]}
 func (m *maxHeap) Push(x interface{}) {m.items = append(m.items, x.([]float64))}
 func (m *maxHeap) Less(i, j int) bool {
-    if m.items[i][1] == m.items[j][1] {
-        return m.items[i][0] > m.items[j][0]
-    }
     return m.items[i][1] > m.items[j][1]
 }
 func (m *maxHeap) Pop() interface{} {
