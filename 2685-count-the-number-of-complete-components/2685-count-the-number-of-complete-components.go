a292f12a2d43984699e9bc77351422ad5d98da0b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-29 09:06:12 -0700

    Time: 128 ms (32.69%), Space: 7.7 MB (95.19%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.go
@@ -0,0 +1,38 @@
+func countCompleteComponents(n int, edges [][]int) int {
+    adjList := map[int][]int{}
+    for i := 0; i < len(edges); i++ {
+        src := edges[i][0]
+        dest := edges[i][1]
+        adjList[src] = append(adjList[src], dest)
+        adjList[dest] = append(adjList[dest], src)
+    }
+    nodesCount := 0
+    edgesCount := 0
+    visited := make([]bool, n)
+    var dfs func(node, prev int)
+    dfs = func(node, prev int) {
+        // base
+        if visited[node] {return}
+        
+        // logic
+        visited[node] = true
+        nodesCount++
+        edgesCount += len(adjList[node])
+        for _, nei := range adjList[node] {
+            if nei == prev {continue}
+            dfs(nei, node)
+        }
+    }
+    completeCount := 0
+    for i := 0; i < n; i++ {
+        if !visited[i] {
+            dfs(i, -1)
+            expected := nodesCount*(nodesCount-1)/2
+            if edgesCount/2 == expected {completeCount++}
+            
+            nodesCount = 0
+            edgesCount = 0
+        }
+    }
+    return completeCount
+}
