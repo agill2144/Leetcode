cf1766d5275a23c57b85dd9653b4e3f6d03c9f27
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-30 12:02:23 -0700

    Time: 77 ms (40.91%), Space: 7.3 MB (68.18%) - LeetHub

---
+++ 0687-longest-univalue-path/0687-longest-univalue-path.go
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func longestUnivaluePath(root *TreeNode) int {
+    maxLen := 0
+    var dfs func(r *TreeNode) (int)
+    dfs = func(r *TreeNode) (int) {
+        // base
+        if r == nil {return 0}
+        
+        // logic
+        left := dfs(r.Left)
+        right := dfs(r.Right)
+        aLeft, aRight := 0,0
+        
+        if r.Left != nil && r.Val == r.Left.Val {
+            aLeft += (left+1)
+        }
+        if r.Right != nil && r.Val == r.Right.Val {
+            aRight += (right+1)
+        }
+        maxLen = max(maxLen, aLeft+aRight)
+        return max(aLeft, aRight)
+    }
+    dfs(root)
+    return maxLen
+}
+
+func max(x, y int) int {
+    if x > y {return x}
+    return y
+}
