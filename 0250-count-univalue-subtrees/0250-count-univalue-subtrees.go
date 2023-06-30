41d8976b3762b5c877f0b22860ab316834f85521
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-06-30 11:15:03 -0700

    Time: 2 ms (62.50%), Space: 2.9 MB (100.00%) - LeetHub

--- 0250-count-univalue-subtrees/0250-count-univalue-subtrees.go
+++ 0250-count-univalue-subtrees/0250-count-univalue-subtrees.go
@@ -8,31 +8,39 @@
  */
 func countUnivalSubtrees(root *TreeNode) int {
     count := 0
-    var dfs func(r *TreeNode) (int, bool)
-    dfs = func(r *TreeNode) (int,bool) {
+    var dfs func(r *TreeNode) bool
+    dfs = func(r *TreeNode) bool {
         // base
-        if r == nil {return math.MinInt64, true}
+        if r == nil {return true}
+        
         // logic
-        left, leftValid := dfs(r.Left)
-        right, rightValid := dfs(r.Right)
-        if !leftValid || !rightValid {
-            return r.Val, false
+        left := dfs(r.Left)
+        right := dfs(r.Right)
+        if r.Left == nil && r.Right == nil {
+            count++
+            return true
         }
-    
-        if left == math.MinInt64 && right == math.MinInt64 {
-            count++
-            return r.Val, true
-        } else if left != math.MinInt64 && right != math.MinInt64 && left == right && right == r.Val{
-            count++
-            return r.Val, true
-        } else if left == math.MinInt64 && right != math.MinInt64 && right == r.Val {
-            count++
-            return r.Val, true
-        } else if right == math.MinInt64 && left != math.MinInt64 && left == r.Val  {
-            count++
-            return r.Val, true
+        if r.Left != nil && r.Right != nil && left && right {
+            if r.Val == r.Left.Val && r.Val == r.Right.Val {
+                count++
+                return true
+            }
         }
-        return r.Val, false
+        if r.Left == nil && r.Right != nil && right {
+            if r.Val == r.Right.Val {
+                count++
+                return true
+            }
+        }
+        
+        if r.Left != nil && r.Right == nil && left {
+            if r.Val == r.Left.Val {
+                count++
+                return true
+            }
+        }
+        
+        return false
     }
     dfs(root)
     return count
