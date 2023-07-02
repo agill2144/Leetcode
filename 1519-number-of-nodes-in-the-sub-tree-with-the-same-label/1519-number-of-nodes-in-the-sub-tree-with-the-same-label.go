b2f83a39731753ca15655bca44d846c831010976
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-01 23:12:46 -0700

    Time: 728 ms (42.86%), Space: 88.2 MB (42.86%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.go
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.go
@@ -1,28 +1,35 @@
 func countSubTrees(n int, edges [][]int, labels string) []int {
-    m := make(map[int][]int)
-    for _, edge := range edges {
-        m[edge[0]] = append(m[edge[0]], edge[1])
-        m[edge[1]] = append(m[edge[1]], edge[0])
+    adjList := map[int][]int{}
+    for i := 0; i < len(edges); i++ {
+        adjList[edges[i][0]] = append(adjList[edges[i][0]], edges[i][1])
+        adjList[edges[i][1]] = append(adjList[edges[i][1]], edges[i][0])
     }
 
-    result := make([]int, n)
-    dfs(0, -1, m, labels, &result)
-    return result
-}
-
-func dfs(node int, parent int, m map[int][]int, labels string, result *[]int) map[byte]int {
-    counter := make(map[byte]int)
-    counter[labels[node]]++
-
-    for _, n := range m[node] {
-        if n == parent {
-            continue
+    out := make([]int, n)    
+    var dfs func(node int, prev int) map[byte]int
+    dfs = func(node int, prev int) map[byte]int {
+        // base
+        
+        
+        // logic
+        freqMap := map[byte]int{}
+        for _, nei := range adjList[node] {
+            if nei == prev {continue}
+            // bottom up, take childs values
+            // merge with parent
+            neiFreqMap := dfs(nei, node)
+            //the time of this merge loop is  = o(26)
+            // there are only 26 labels ( a-z )
+            for k, v := range neiFreqMap {
+                freqMap[k] += v
+            }
         }
-        cnt := dfs(n, node, m, labels, result)
-        for k, v := range cnt {
-            counter[k] += v
-        }
+        // update parent
+        freqMap[labels[node]]++
+        // save parent count
+        out[node] = freqMap[labels[node]]
+        return freqMap
     }
-    (*result)[node] = counter[labels[node]]
-    return counter
+    dfs(0, -1)
+    return out
 }
