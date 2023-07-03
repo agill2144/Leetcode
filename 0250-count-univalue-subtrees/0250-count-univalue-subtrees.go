1b8f9711d0f810e1f3a9e2e01c63281a00fa5827
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-02 18:33:33 -0700

    Time: 5 ms (41.27%), Space: 2.9 MB (7.94%) - LeetHub

--- 0250-count-univalue-subtrees/0250-count-univalue-subtrees.go
+++ 0250-count-univalue-subtrees/0250-count-univalue-subtrees.go
@@ -20,24 +20,23 @@ func countUnivalSubtrees(root *TreeNode) int {
             count++
             return true
         }
+        
+        // "A uni-value subtree means all nodes of the subtree have the same value."
+        // if a node has both childs, they must ALL be the same value
         if r.Left != nil && r.Right != nil && left && right {
             if r.Val == r.Left.Val && r.Val == r.Right.Val {
                 count++
                 return true
             }
         }
-        if r.Left == nil && r.Right != nil && right {
-            if r.Val == r.Right.Val {
-                count++
-                return true
-            }
-        }
         
-        if r.Left != nil && r.Right == nil && left {
-            if r.Val == r.Left.Val {
-                count++
-                return true
-            }
+        if left && r.Left != nil && r.Left.Val == r.Val && r.Right == nil {
+            count++
+            return true
+        }
+        if right && r.Right != nil && r.Right.Val == r.Val && r.Left == nil {
+            count++
+            return true
         }
         
         return false
