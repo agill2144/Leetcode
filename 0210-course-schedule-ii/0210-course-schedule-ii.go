a4faa934e5523db8ac465ea79182c61f20f2f28b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-03 19:38:16 -0700

    Time: 7 ms (91.02%), Space: 6.4 MB (74.22%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.go
+++ 0210-course-schedule-ii/0210-course-schedule-ii.go
@@ -1,41 +1,38 @@
 func findOrder(numCourses int, prerequisites [][]int) []int {
     adjList := map[int][]int{}
-    indegrees := make([]int, numCourses)
     for i := 0; i < len(prerequisites); i++ {
-        a := prerequisites[i][1]
-        b := prerequisites[i][0]
-        indegrees[b]++
-        adjList[a] = append(adjList[a], b)
+        a, b := prerequisites[i][0], prerequisites[i][1]
+        // a depends on b
+        // b is independent
+        adjList[b] = append(adjList[b], a)
     }
+    
+    st := []int{}
     visited := make([]bool, numCourses)
     var dfs func(node int, path []bool) bool
-    st := []int{}
     dfs = func(node int, path []bool) bool {
         // base
         if path[node] {return false}
         if visited[node] {return true}
         
         // logic
-        path[node] = true
         visited[node] = true
+        path[node] = true
         for _, nei := range adjList[node] {
             if !dfs(nei, path) {return false}
         }
-        st = append(st, node)
         path[node] = false
+        st = append(st, node)
         return true
     }
+
     p := make([]bool, numCourses)
     for i := 0; i < numCourses; i++ {
-        if !visited[i] {
-            if !dfs(i, p) {return nil}
-        }
+        if !visited[i] && !dfs(i, p) {return nil}
     }
-    out := []int{}
-    for len(st) != 0 {
-        top := st[len(st)-1]
-        st = st[:len(st)-1]
-        out = append(out,top)
+    if len(st) != numCourses {return nil}
+    for i := 0; i < len(st)/2 ; i++ {
+        st[i], st[len(st)-1-i] = st[len(st)-1-i], st[i] 
     }
-    return out
+    return st
 }
