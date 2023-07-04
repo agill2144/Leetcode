8d6c06a7665dea4272ef842d866bde0280b49882
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-03 21:44:21 -0700

    Time: 0 ms (100.00%), Space: 2.3 MB (20.00%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.go
+++ 0269-alien-dictionary/0269-alien-dictionary.go
@@ -1,17 +1,29 @@
+/*
+    questions to ask:
+    1. guranteed sorted correctly ?
+        - No: then check while creating the adjList
+        - one case could be the that the 2nd string is smaller and parent is larger
+            ["amrit", "amit"] - not sorted correctly
+    2. Should the ouput string contain only the characters that are used to construct this sorted order or ALL characters
+        - if just the chars that are used to sort, then our adjList can only contain the diff chars 
+            - keep in mind, if there were no diffs detected, the output in this case will be empty ( no order could be determined )
+        - if all characters of the input words should be part of output
+            - we can simply add each character to adjList first and then find its childrens
+            
+*/
 func alienOrder(words []string) string {
     adjList := map[byte][]byte{}
-    
-    for _, word := range words {
-        for i := 0; i < len(word); i++ {
-            char := word[i]
-            adjList[char] = []byte{}
+    for i := 0; i < len(words); i++ {
+        for k := 0; k < len(words[i]); k++ {
+            adjList[words[i][k]] = []byte{}
         }
     }
+    
+    // find the differences between 2 words
     for i := 0; i < len(words)-1; i++ {
         parent := words[i]
         child := words[i+1]
         p, c := 0, 0
-        
         for p < len(parent) && c < len(child) {
             if parent[p] != child[c] {
                 adjList[parent[p]] = append(adjList[parent[p]], child[c])
@@ -20,38 +32,43 @@ func alienOrder(words []string) string {
             p++
             c++
         }
-        
-        if p < len(parent) && c == len(child) {return ""}
+        // if all characters were same, while parent ptr is still inbound and
+        // child ptr no longer has words, it means child was smaller than parent ( with same exact chars )
+        // if sorted correctly, child should be placed before but in this case its not sorted correctly
+        // exit early
+        if p < len(parent) && c == len(child) {
+            return ""
+        }
     }
-
+    
+    // cycle detection algo on all chars that we care about
+    // the keys of adjList
+    // why not also the childs ? or just the child chars ?
+    // well key is the parent, i.e key char came before child character
+    // therefore key/parent has no dependencies and childs do
     st := []byte{}
     visited := make([]bool, 26)
     var dfs func(node byte, path []bool) bool
     dfs = func(node byte, path []bool) bool {
         // base
-        nodeIdx := node-'a'
-        if path[nodeIdx] {return false}
-        if visited[nodeIdx] {return true}
+        if path[node-'a'] {return false}// cycle detected
+        if visited[node-'a'] {return true}
+        
         
         // logic
-        visited[nodeIdx] = true
-        path[nodeIdx] = true
+        visited[node-'a'] = true
+        path[node-'a'] = true
         for _, nei := range adjList[node] {
             if !dfs(nei, path) {return false}
         }
-        path[nodeIdx] = false
         st = append(st, node)
+        path[node-'a'] = false
         return true
     }
-
     p := make([]bool, 26)
-    for i := 'a'; i <= 'z'; i++ {
-        char := byte(i)
-        charIdx := char-'a'
-        if _, ok := adjList[char]; ok {
-            if !visited[charIdx] {
-                if !dfs(char, p) {return ""}
-            }
+    for k, _ := range adjList {
+        if !visited[k-'a'] {
+            if !dfs(k, p) {return ""}
         }
     }
     out := new(strings.Builder)
