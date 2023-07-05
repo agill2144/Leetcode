33c746e5cdbd46af878c000d3966fdb7605c2943
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-04 19:37:07 -0700

    Time: 139 ms (59.31%), Space: 6.9 MB (75.76%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.go
+++ 0127-word-ladder/0127-word-ladder.go
@@ -1,11 +1,12 @@
 func ladderLength(beginWord string, endWord string, wordList []string) int {
-    foundEnd := false
+    if beginWord == endWord || len(wordList) == 0 {return 0}
     wordSet := map[string]struct{}{}
+    foundEnd := false
     for i := 0; i < len(wordList); i++ {
-        word := wordList[i]
-        if word == endWord {foundEnd = true}
-        wordSet[word] = struct{}{}
+        if wordList[i] == endWord{foundEnd = true}
+        wordSet[wordList[i]] = struct{}{}
     }
+    
     if !foundEnd {return 0}
     
     q := []string{beginWord}
@@ -16,14 +17,15 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
             dq := q[0]
             q = q[1:]
             if dq == endWord {return levels}
+            
             for i := 0; i < len(dq); i++ {
                 for j := 0; j < 26; j++ {
-                    char := byte(j+'a')
-                    nei := dq[:i] + string(char) + dq[i+1:]
+                    char := string(byte(j+'a'))
+                    nei := dq[:i] + char + dq[i+1:]
+                    if nei == endWord {return levels+1}
                     if _, ok := wordSet[nei]; ok {
-                        if nei == endWord {return levels+1}
-                        delete(wordSet, nei)
                         q = append(q, nei)
+                        delete(wordSet, nei)
                     }
                 }
             }
