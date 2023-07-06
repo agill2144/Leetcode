389466942e47e49a5920cf2239d45446d9e4ac4b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-05 18:10:33 -0700

    Time: 73 ms (18.46%), Space: 7.5 MB (26.15%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.go
+++ 1615-maximal-network-rank/1615-maximal-network-rank.go
@@ -1,37 +1,55 @@
 func maximalNetworkRank(n int, roads [][]int) int {
-    adjList := map[int][]int{}
+    adjList := map[int]*set{}
     for i := 0; i < len(roads); i++ {
         u,v := roads[i][0], roads[i][1]
-        adjList[u] = append(adjList[u], v)
-        adjList[v] = append(adjList[v], u)
+        
+        _, ok := adjList[u]
+        if !ok {adjList[u] = newSet()}
+        _, ok = adjList[v]
+        if !ok {adjList[v] = newSet()}
+
+        adjList[u].add(v)
+        adjList[v].add(u)
     }
     
     
     maxCount := 0
+    
+    // why are we forming all nodes here ?
+    // Look at example3
+    // "Notice that all the cities do not have to be connected"
+    // this means we can pick 2 nodes far away from each other , we are looking for
+    // which 2 nodes produce the max number of edges , therefore trying all
     for i := 0; i < n; i++ {
         for j := 0; j < n; j++ {
             if j == i {continue}
             u, v := i, j
-            uEdges := adjList[u]
-            vEdges := adjList[v]
-            total := len(uEdges) + len(vEdges)
-            if sliceContains(uEdges, v) || sliceContains(vEdges, u) {
+            uAdjNodes := adjList[u]
+            if uAdjNodes == nil {uAdjNodes=newSet()}
+            vAdjNodes := adjList[v]
+            if vAdjNodes == nil {vAdjNodes=newSet()}
+            total := len(uAdjNodes.items) + len(vAdjNodes.items)
+            if uAdjNodes.contains(v) || vAdjNodes.contains(u) {
                 total--
             }
             if total > maxCount {maxCount = total}        
         }
-        
-        // fmt.Println("Pair: ", u,v, total)
-        // fmt.Println("U count: ", u, uEdges)
-        // fmt.Println("V count: ", v, vEdges)
-        // fmt.Println("--------------------------")        
     }
     return maxCount
 }
 
-func sliceContains(slice []int, n int) bool {
-    for _, ele := range slice {
-        if ele == n {return true}
-    }
-    return false
+
+type set struct{
+    items map[int]struct{}
+}
+
+func newSet() *set {
+    return &set{items:map[int]struct{}{}}
+}
+func (s *set) add(x int) {
+    s.items[x] = struct{}{}
+}
+func (s *set) contains(x int) bool {
+    _, ok := s.items[x]
+    return ok
 }
