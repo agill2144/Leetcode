70d9cf27b6d8b0d6086a4f938b06e9979afd357f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-05 17:40:16 -0700

    Time: 90 ms (48.39%), Space: 15.9 MB (16.13%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.go
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.go
@@ -1,84 +1,55 @@
-/*
-    - there are disconnected components
-    - meaning dfs on all
-    - there can be cycles and this is undirected, so maintain a visited array and a prev node as part of dfs
-    
-    A connected graph with the fewest edges is a tree with n - 1 edges connecting n nodes. 
-    So, in order for a graph with n nodes to be connected, 
-    the graph must have at least n - 1 edges. 
-    It is impossible to connect a graph that has fewer than n - 1 edges. 
-    Otherwise, it is always possible to connect the graph.
-    
-    What I didnt understand
-    - I tried to find redundant connections ( i.e cycles ) and disconnect ONLY those and connect those to other components
-    - no, we are looking for number of connectedComponents and count number of edges to connect them
-    - This line "You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected"
-        suggests to disconnect existing ones to connect new ones, but logically we can only disconnect existing edge if it was redundant.
-    
-    - count number of connected components
-    - to connect all individual connected components,
-    - we would need to numConnectedComponents-1 edges
-    
-    1--2
-    | /    4---5
-    |/
-    3 
-        6
-        |
-        7
-           
-    The above input has 3 connected components
-    TO CONNECT ALL, how many edges do we need?
-    3-1 = 2
-    connect 3-4
-    connect 3-6
-    
-    Therefore to make number of individual connected components connect to each other, we need numConnectedComponents-1 edges in total
-    JUST LIKE TO CONNECT N NODES TOGETHER, WE NEED ATLEAST N-1 EDGES 
-    
-    time: o(v+e)
-    space: o(v+e)
-*/
 func makeConnected(n int, connections [][]int) int {
+    
+    // if there are already less than n-1 edges, aint no way
+    // we can connect all
     if len(connections) < n-1 {return -1}
+    
+    
     adjList := map[int][]int{}
     for i := 0; i < len(connections); i++ {
-        adjList[connections[i][0]] = append(adjList[connections[i][0]], connections[i][1])
-        adjList[connections[i][1]] = append(adjList[connections[i][1]], connections[i][0])        
+        u,v := connections[i][0], connections[i][1]
+        adjList[u] = append(adjList[u], v)
+        adjList[v] = append(adjList[v], u)
     }
-    cycles := 0
-    visited := map[int]struct{}{}
-    var dfs func(curr, prev int)
-    dfs = func(curr, prev int) {
+    
+    visited := make([]bool, n)
+    cyclicEdgesCount := 0
+    var dfs func(node, prev int)
+    dfs = func(node, prev int ) {
         // base
-        if _, ok := visited[curr]; ok {
-            cycles++            
+        if visited[node] {
+            cyclicEdgesCount++
             return
         }
         
         // logic
-        visited[curr] = struct{}{}
-        for _, node := range adjList[curr] {
-            if node == prev {continue}
-            dfs(node, curr)
+        visited[node] = true
+        for _, nei := range adjList[node] {
+            if nei == prev {continue}
+            dfs(nei, node)
         }
     }
-    numConnectedComp := 0
+    
+    connectedComponentsCount := 0
     for i := 0; i < n; i++ {
-        if _, ok := visited[i]; !ok {
-            numConnectedComp++
+        if !visited[i] {
             dfs(i, -1)
+            connectedComponentsCount++
         }
     }
     
-    // if there are no disconnected components
-    // we do not need to connect anything
-    if numConnectedComp == 0 {return 0}
+    // to connect 2 individual connected components we need 1 edge ( connectedComponentsCount-1 )
+    // to connect 3 individual connected components we need 2 edges ( connectedComponentsCount-1 )
+    // to connect 4 individual connected components we need 3 edges  ( connectedComponentsCount-1 )
+    extraEdgesNeeded := connectedComponentsCount-1
     
-    // or if there are disconnected components
-    // and we do not have enough redundant/cyclic edges to connect all components into 1 single (numConnectedComp-1);
-    // we cannot connect it
-    if cycles < numConnectedComp-1  {return -1}
-
-    return numConnectedComp-1
+    // we must use existing redundant edges ( we cant create new ones, we have to disconnect existing redundant ones )
+    // thats what cyclicEdgesCount is keeping track of
+    // if we dont have enough, return and exit early with -1
+    if cyclicEdgesCount < extraEdgesNeeded {return -1}
+    
+    // otherwise we have enough redundant edges that can be used
+    // we need to know the min edges needed which is what extraEdgesNeeded is telling us
+    return extraEdgesNeeded
+    
 }
