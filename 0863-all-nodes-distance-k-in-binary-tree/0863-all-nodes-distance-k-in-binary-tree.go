fd4c17b7a59f7c85a6d6dcde77ecc7e3480a9258
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-10 18:38:30 -0700

    Time: 5 ms (24.56%), Space: 3.8 MB (26.32%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.go
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func distanceK(root *TreeNode, target *TreeNode, k int) []int {
+    
+    // turn the tree into a graph
+    // A TREE IS A UNDIRECTED GRAPH
+    // when you see tree, also consider undirected graph
+    
+    adjList := map[*TreeNode][]*TreeNode{}
+    var buildGraph func(r *TreeNode, parent *TreeNode)
+    buildGraph = func(r *TreeNode, parent *TreeNode) {
+        // base
+        if r == nil {return}
+        
+        // logic
+        if parent != nil {adjList[r] = append(adjList[r], parent)}
+        if r.Left != nil {adjList[r] = append(adjList[r], r.Left)}
+        if r.Right != nil {adjList[r] = append(adjList[r], r.Right)}
+        buildGraph(r.Left, r)
+        buildGraph(r.Right, r)
+    }
+    buildGraph(root, nil)
+    
+    out := []int{}
+    visited := map[*TreeNode]struct{}{} 
+    var dfs func(node, prev *TreeNode, currDist int)
+    dfs = func(node, prev *TreeNode, currDist int) {
+        // base
+        if node == nil {return}
+        if _, ok := visited[node]; ok {return}
+        
+        // logic
+        visited[node] = struct{}{}
+        if currDist == k {
+            out = append(out, node.Val)
+            // going beyond this currDist, will exceed k
+            // therefore return and exit early
+            return
+        }
+        for _, nei := range adjList[node] {
+            if nei == prev {continue}
+            dfs(nei, node, currDist+1)
+        }
+    }
+    dfs(target, nil, 0)
+    return out
+}
