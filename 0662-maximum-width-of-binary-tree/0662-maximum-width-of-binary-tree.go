367c9d6d38e3507e8d09d4d4cabcf8bc20f0fef1
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-15 17:32:46 -0700

    Time: 1 ms (93.33%), Space: 4.5 MB (71.67%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.go
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.go
@@ -21,6 +21,22 @@ func widthOfBinaryTree(root *TreeNode) int {
             dq := q[0]
             q = q[1:]
             node := dq.node
+            /*
+                Once we know the first and last idx position of a level, then
+                the width math is the same as sliding window size ( right-left+1 )
+                or lastIdx-firstIdx+1
+                
+                But we have to consider null nodes in between too....
+                instead of adding null nodes and using ptrs and vars to keep track of queue state
+                we can enqueue a pair ( node, idx ), initially the idx will be 0 because starting node
+                
+                
+                if we know the index of a node is C, then
+                we can define the index of its left child node as 2*c ( if there is a left child )
+                and for its right child it will be 2*c+1 ( if there is a right child )
+               
+                
+            */
             if  node.Left != nil {
                 q = append(q, &pair{node:node.Left, idx:2*dq.idx})
             }
