14c2627aedc23bf0413bc3e6252ae2bc6ef629a6
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-15 21:26:52 -0700

    Time: 2 ms (68.75%), Space: 3.9 MB (23.75%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.go
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.go
@@ -6,101 +6,45 @@
  *     Right *TreeNode
  * }
  */
-// func distanceK(root *TreeNode, target *TreeNode, k int) []int {
-    
-//     // turn the tree into a graph
-//     // A TREE IS A UNDIRECTED GRAPH
-//     // when you see tree, also consider undirected graph
-    
-//     // other appraoch is create parent ptrs so then we can traverse from target
-//     // but we cannot modify input TreeNode struct, as it does not have "parent" field
-//     // ok then use a hashmap to keep track of each parent per node :shrug ( implemented as another approach below )
-//     // then ( whichever way we have parent ptrs )
-//     // start from dfs(target, 0)
-//     // each recursion will dfs(r.Left, currDist+1), dfs(r.Right, currDist+1), dfs(r.parent, currDist+1)
-//     // save when currDist becomes k, and return back since going further when currDist is already k, will not help
-    
-//     // build the adjList
-//     adjList := map[*TreeNode][]*TreeNode{}
-//     var buildGraph func(r *TreeNode, parent *TreeNode)
-//     buildGraph = func(r *TreeNode, parent *TreeNode) {
-//         // base
-//         if r == nil {return}
-        
-//         // logic
-//         if parent != nil {adjList[r] = append(adjList[r], parent)}
-//         if r.Left != nil {adjList[r] = append(adjList[r], r.Left)}
-//         if r.Right != nil {adjList[r] = append(adjList[r], r.Right)}
-//         buildGraph(r.Left, r)
-//         buildGraph(r.Right, r)
-//     }
-//     buildGraph(root, nil)
-    
-//     // dfs graph traversal on a undirected graph ( undirected graphs when represented as a tree do not have cycles )
-//     out := []int{}
-//     // so we dont visit same node again
-//     visited := map[*TreeNode]struct{}{} 
-//     var dfs func(node, prev *TreeNode, currDist int)
-//     dfs = func(node, prev *TreeNode, currDist int) {
-//         // base
-//         if node == nil {return}
-//         if _, ok := visited[node]; ok {return}
-        
-//         // logic
-//         visited[node] = struct{}{}
-//         if currDist == k {
-//             out = append(out, node.Val)
-//             // going beyond this currDist, will exceed k
-//             // therefore return and exit early
-//             return
-//         }
-//         for _, nei := range adjList[node] {
-//             if nei == prev {continue}
-//             dfs(nei, node, currDist+1)
-//         }
-//     }
-//     dfs(target, nil, 0)
-//     return out
-// }
-
-
 func distanceK(root *TreeNode, target *TreeNode, k int) []int {
-    parents := map[*TreeNode]*TreeNode{}
-    var dfs func(r *TreeNode, parent *TreeNode)
-    dfs = func(r, parent *TreeNode) {
+    adjList := map[int][]int{}
+    var dfs func(r *TreeNode, p *TreeNode)
+    dfs = func(r, p *TreeNode) {
         // base
         if r == nil {return}
         
         // logic
-        if parent != nil {
-            parents[r] = parent
-        }
-        dfs(r.Left, r)
-        dfs(r.Right, r)
+        if p != nil {adjList[r.Val] = append(adjList[r.Val], p.Val)}
+        if r.Left != nil {adjList[r.Val] = append(adjList[r.Val], r.Left.Val); dfs(r.Left, r)}
+        if r.Right != nil {adjList[r.Val] = append(adjList[r.Val], r.Right.Val); dfs(r.Right, r)}
     }
     dfs(root, nil)
     
+    visited := map[int]struct{}{}
+    visited[target.Val] = struct{}{}
+    q := []int{target.Val}
+    levels := 0
     out := []int{}
-    var distDfs func(node *TreeNode, currDist int)
-    visited := map[*TreeNode]struct{}{}
-    distDfs = func(node *TreeNode, currDist int)  {
-        // base
-        if node == nil {return}
-        if _, ok := visited[node]; ok {return}
-        
-        // logic
-        visited[node] = struct{}{}
-        if currDist == k {
-            out = append(out, node.Val)
-            return
+
+    for len(q) != 0 {
+        qSize := len(q)
+        for qSize != 0 {
+            dq := q[0]
+            q = q[1:]
+            if levels == k {
+                out = append(out, dq)
+                qSize--
+                continue
+            }
+            for _,nei := range adjList[dq] {
+                if _, ok := visited[nei]; !ok {
+                    q = append(q, nei)
+                    visited[nei] = struct{}{}
+                }
+            }
+            qSize--
         }
-        distDfs(node.Left, currDist+1)
-        distDfs(node.Right, currDist+1)
-        if _, ok := parents[node]; ok {
-            distDfs(parents[node], currDist+1)            
-        }
+        levels++
     }
-    
-    distDfs(target, 0)
     return out
 }
