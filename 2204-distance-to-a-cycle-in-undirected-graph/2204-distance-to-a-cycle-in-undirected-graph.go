850626b0e2c48cb741f14133eb4b02d454220d61
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-15 21:44:46 -0700

    Time: 255 ms (100.00%), Space: 53.8 MB (100.00%) - LeetHub

---
+++ 2204-distance-to-a-cycle-in-undirected-graph/2204-distance-to-a-cycle-in-undirected-graph.go
@@ -0,0 +1,57 @@
+func distanceToCycle(n int, edges [][]int) []int {
+    adjList := map[int][]int{}
+    for i := 0; i < len(edges); i++ {
+        u,v := edges[i][0], edges[i][1]
+        adjList[u] = append(adjList[u], v)
+        adjList[v] = append(adjList[v], u)
+    }
+    
+    // find nodes part of the cycle
+    cyclicNodes := make([]bool, n)
+    visited := make([]bool, n)
+    cyclicNode := -1
+    done := false
+    var dfs func(node, prev int) bool
+    dfs = func(node, prev int) bool {
+        // base
+        if visited[node] {cyclicNode = node; return false}
+        
+        // logic
+        visited[node] = true
+        for _, nei := range adjList[node] {
+            if nei == prev {continue}
+            if !dfs(nei, node) && !done {
+                cyclicNodes[node] = true
+                if cyclicNode == node {done = true}
+                return false
+            }
+        }
+        return true
+    }
+    dfs(0, -1)
+    visited = make([]bool, n)     
+    dist := make([]int, n)
+    q := [][]int{}
+
+    for i := 0; i < len(cyclicNodes); i++ {
+        if cyclicNodes[i] {
+            dist[i] = 0
+            visited[i] = true
+            q = append(q, []int{i, 0})
+        }
+    }
+    for len(q) != 0 {
+        dq := q[0]
+        q = q[1:]
+        node := dq[0]
+        currDist := dq[1]
+        for _, nei := range adjList[node] {
+            if !visited[nei] {
+                dist[nei] = currDist+1
+                q = append(q, []int{nei, currDist+1})
+                visited[nei] = true
+            }
+        }
+    }
+    return dist
+}
