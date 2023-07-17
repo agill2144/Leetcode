ae6276337665373fa7a18a0683b1ff63a46612c5
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-16 21:12:53 -0700

    Added solution - LeetHub

--- Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.java
+++ Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.java
@@ -32,72 +32,89 @@ import java.io.*;
 // User function Template for Java
 
 class Solution {
-    public static List<Integer> shortestPath(int n, int m, int[][] edges) {
-        Map<Integer, List<int[]>> adjList = new HashMap<>();
-        for (int i = 0; i < edges.length; i++) {
+    static class Node implements Comparable<Node> {
+        int node;
+        int distance;
+
+        public Node(int node, int distance) {
+            this.node = node;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.distance, other.distance);
+        }
+    }
+
+    public static List<Integer> shortestPath(int n, int m, int edges[][]) {
+        List<List<int[]>> adjList = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
             int u = edges[i][0];
             int v = edges[i][1];
             int w = edges[i][2];
-            adjList.putIfAbsent(u, new ArrayList<>());
-            adjList.putIfAbsent(v, new ArrayList<>());
-            adjList.get(u).add(new int[] {v, w});
-            adjList.get(v).add(new int[] {u, w});
+            adjList.get(u).add(new int[]{v, w});
+            adjList.get(v).add(new int[]{u, w});
         }
 
-        int src = 1;
-        int dest = n;
         int[] dist = new int[n + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
+        int[] parent = new int[n + 1];
+        int src = 1, dest = n;
         dist[src] = 0;
-        PriorityQueue<QueueNode> pq = new PriorityQueue<>();
-        pq.offer(new QueueNode(new ArrayList<>(Arrays.asList(src)), 0));
+        parent[src] = src;
 
-        while (!pq.isEmpty()) {
-            QueueNode dq = pq.poll();
-            List<Integer> currPath = dq.path;
-            int currNode = currPath.get(currPath.size() - 1);
-            int currDist = dq.dist;
+        boolean found = false;
+        PriorityQueue<Node> minHeap = new PriorityQueue<>();
+        minHeap.offer(new Node(src, 0));
+
+        while (!minHeap.isEmpty()) {
+            Node node = minHeap.poll();
+            int currNode = node.node;
+            int currDist = node.distance;
 
             if (currNode == dest) {
-                return currPath;
+                found = true;
+                break;
             }
 
-            if (currDist > dist[currNode]) {
-                continue;
-            }
+            for (int[] nei : adjList.get(currNode)) {
+                int adjNode = nei[0];
+                int adjNodeDist = currDist + nei[1];
 
-            List<int[]> adjNodes = adjList.get(currNode);
-            if (adjNodes != null) {
-                for (int[] nei : adjNodes) {
-                    int adjNode = nei[0];
-                    int adjNodeDist = currDist + nei[1];
-    
-                    if (adjNodeDist < dist[adjNode]) {
-                        dist[adjNode] = adjNodeDist;
-                        List<Integer> newPath = new ArrayList<>(currPath);
-                        newPath.add(adjNode);
-                        pq.offer(new QueueNode(newPath, adjNodeDist));
-                    }
+                if (adjNodeDist < dist[adjNode]) {
+                    dist[adjNode] = adjNodeDist;
+                    parent[adjNode] = currNode;
+                    minHeap.offer(new Node(adjNode, adjNodeDist));
                 }
             }
         }
 
-        return Arrays.asList(-1);
-    }
-
-    static class QueueNode implements Comparable<QueueNode> {
-        List<Integer> path;
-        int dist;
-
-        public QueueNode(List<Integer> path, int dist) {
-            this.path = path;
-            this.dist = dist;
+        if (!found) {
+            return Arrays.asList(-1);
         }
 
-        @Override
-        public int compareTo(QueueNode other) {
-            return Integer.compare(this.dist, other.dist);
+        List<Integer> out = new ArrayList<>();
+        int ptr = dest;
+        while (ptr != parent[ptr]) {
+            out.add(ptr);
+            ptr = parent[ptr];
         }
+        out.add(src);
+
+        int left = 0, right = out.size() - 1;
+        while (left < right) {
+            int temp = out.get(left);
+            out.set(left, out.get(right));
+            out.set(right, temp);
+            left++;
+            right--;
+        }
+
+        return out;
     }
 }
-
