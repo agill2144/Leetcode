6566bcd535d81e9df76050d3359a1d36e657f0f4
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-30 13:26:34 -0700

    Time: 25 ms (100.00%), Space: 6.6 MB (94.74%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.go
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.go
@@ -3,31 +3,31 @@ func findMaxFish(grid [][]int) int {
     n := len(grid[0])
     
     maxFishes := 0
-    runningTotal := 0
     dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
-    var dfs func(r, c int)
-    dfs = func(r, c int) {
+    var dfs func(r, c int) int
+    dfs = func(r, c int) int {
         // base
-        if r < 0 || r == m || c < 0 || c == n || grid[r][c] == 0 {return}
+        if r < 0 || r == m || c < 0 || c == n || grid[r][c] == 0 {return 0}
         
         
         // logic
         // add to runningTotal and mark it visited
-        runningTotal += grid[r][c]
-        if runningTotal > maxFishes {maxFishes = runningTotal}
+        runningTotal := grid[r][c]
         grid[r][c] = 0
         
         for _, dir := range dirs {
-            dfs(r+dir[0], c+dir[1])
+            runningTotal += dfs(r+dir[0], c+dir[1])
         }
+        return runningTotal
         
     }
     
     for i := 0; i < m ; i++ {
         for j := 0; j < n ; j++ {
             if grid[i][j] != 0 {
-                runningTotal = 0
-                dfs(i, j)
+                if val := dfs(i, j); val > maxFishes {
+                    maxFishes = val
+                }
             }
         }
     }
