b933cde20fe6fdce783c7beb8d12ed42c37853c0
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-30 12:59:00 -0700

    Time: 23 ms (66.11%), Space: 7.1 MB (25.36%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.go
+++ 0547-number-of-provinces/0547-number-of-provinces.go
@@ -1,30 +1,29 @@
 func findCircleNum(isConnected [][]int) int {
-    adjList := map[int][]int{}
     n := len(isConnected)
-    for i := 0; i < n; i++ {
+   
+    visited := make([]bool, n)
+    var dfs func(row int)
+    dfs = func(row int) {
+        // base
+        if visited[row] {return}
+        
+        // logic
+        visited[row] = true
         for j := 0; j < n; j++ {
-            if isConnected[i][j] == 1 && i != j {
-                adjList[i] = append(adjList[i], j)
-                adjList[j] = append(adjList[j], i)
+            if isConnected[row][j] == 1 && row != j && !visited[j] {
+                dfs(j)
             }
         }
     }
-    visited := make([]bool, n)
-    var dfs func(node, prev int)
-    dfs = func(node, prev int) {
-        // base
-        if visited[node] {return}
-        
-        // logic
-        visited[node] = true
-        for _, nei := range adjList[node] {
-            if nei == prev {continue}
-            dfs(nei, node)
+        count := 0
+
+    
+    for i := 0; i < n; i++ {
+        if !visited[i] {
+            dfs(i)
+            count++
         }
     }
-    count := 0
-    for i := 0; i < n; i++ {
-        if !visited[i] {dfs(i, -1); count++}
-    }
+   
     return count
 }
