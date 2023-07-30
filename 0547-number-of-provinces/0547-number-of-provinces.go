dcf0d65262166b4bd8c00ba887f90c721710832a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-30 13:00:54 -0700

    Time: 12 ms (99.90%), Space: 7.1 MB (12.68%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.go
+++ 0547-number-of-provinces/0547-number-of-provinces.go
@@ -1,3 +1,4 @@
+// traverse the input without building adjList
 func findCircleNum(isConnected [][]int) int {
     n := len(isConnected)
    
@@ -10,14 +11,13 @@ func findCircleNum(isConnected [][]int) int {
         // logic
         visited[row] = true
         for j := 0; j < n; j++ {
-            if isConnected[row][j] == 1 && row != j && !visited[j] {
+            if isConnected[row][j] == 1 && row != j {
                 dfs(j)
             }
         }
     }
-        count := 0
-
     
+    count := 0
     for i := 0; i < n; i++ {
         if !visited[i] {
             dfs(i)
@@ -27,3 +27,35 @@ func findCircleNum(isConnected [][]int) int {
    
     return count
 }
+
+// build adjList and traverse
+// func findCircleNum(isConnected [][]int) int {
+//     adjList := map[int][]int{}
+//     n := len(isConnected)
+//     for i := 0; i < n; i++ {
+//         for j := 0; j < n; j++ {
+//             if isConnected[i][j] == 1 && i != j {
+//                 adjList[i] = append(adjList[i], j)
+//                 adjList[j] = append(adjList[j], i)
+//             }
+//         }
+//     }
+//     visited := make([]bool, n)
+//     var dfs func(node, prev int)
+//     dfs = func(node, prev int) {
+//         // base
+//         if visited[node] {return}
+        
+//         // logic
+//         visited[node] = true
+//         for _, nei := range adjList[node] {
+//             if nei == prev {continue}
+//             dfs(nei, node)
+//         }
+//     }
+//     count := 0
+//     for i := 0; i < n; i++ {
+//         if !visited[i] {dfs(i, -1); count++}
+//     }
+//     return count
+// }
