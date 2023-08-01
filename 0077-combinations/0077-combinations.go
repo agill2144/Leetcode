2beb43eb181f995a73b81f690446d8182335ef9a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-31 20:35:36 -0700

    Time: 11 ms (78.06%), Space: 8 MB (30.80%) - LeetHub

--- 0077-combinations/0077-combinations.go
+++ 0077-combinations/0077-combinations.go
@@ -2,22 +2,24 @@ func combine(n int, k int) [][]int {
     out := [][]int{}
     var dfs func(start int, path []int)
     dfs = func(start int, path []int) {
-        // base
-        
-        // logic
+        // base        
         if len(path) == k {
-            newPath := make([]int, len(path))
+            newPath := make([]int, k)
             copy(newPath, path)
             out = append(out, newPath)
             return
         }
+        if start > n {return}
         
-        for i := start; i <= n; i++ {
+        // logic        
+        for i := start; i <= n ; i++ {
+            // action
             path = append(path, i)
+            // recurse
             dfs(i+1, path)
+            // backtrack
             path = path[:len(path)-1]
         }
-        
     }
     dfs(1, []int{})
     return out
