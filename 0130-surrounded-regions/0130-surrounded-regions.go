765db506ca732f7a17fda9aa24b356587c30c4bf
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-07-31 20:49:49 -0700

    Time: 19 ms (73.53%), Space: 6.5 MB (87.39%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.go
+++ 0130-surrounded-regions/0130-surrounded-regions.go
@@ -2,18 +2,19 @@ func solve(board [][]byte)  {
     m := len(board)
     n := len(board[0])
     
-    dirs := [][]int{{1,0},{-1,0}, {0,1}, {0,-1}}
-    var dfs func(r, c int)
+    dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
+    
+    
+    // used to mark all O's that are not flippable
+    var dfs func(r , c int)
     dfs = func(r, c int) {
         // base
         if r < 0 || r == m || c < 0 || c == n || board[r][c] != 'O' {return}
         
         // logic
-        board[r][c] = '-' // not flipable
+        board[r][c] = '.' // . reprensents a previous O that cannot be flipped
         for _, dir := range dirs {
-            nr := r + dir[0]
-            nc := c + dir[1]
-            dfs(nr, nc)
+            dfs(r+dir[0], c+dir[1])
         }
     }
     
@@ -21,17 +22,17 @@ func solve(board [][]byte)  {
         if board[i][0] == 'O' {dfs(i, 0)}
         if board[i][n-1] == 'O' {dfs(i, n-1)}
     }
-    for i := 0; i < n; i++ {
-        if board[0][i] == 'O' {dfs(0, i)}
-        if board[m-1][i] == 'O' {dfs(m-1, i)}
+    for j := 0; j < n; j++ {
+        if board[0][j] == 'O' {dfs(0, j)}
+        if board[m-1][j] == 'O' {dfs(m-1, j)}
     }
     
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            if board[i][j] == '-' {
-                board[i][j] = 'O'
-            } else if board[i][j] == 'O' {
+            if board[i][j] == 'O' {
                 board[i][j] = 'X'
+            } else if board[i][j] == '.' {
+                board[i][j] = 'O'
             }
         }
     }
