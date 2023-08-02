0639e621087ac953bc252527aff944f77b5f8d18
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-01 21:08:13 -0700

    Time: 1 ms (62.65%), Space: 2 MB (99.20%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.go
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.go
@@ -1,46 +1,47 @@
 type MyQueue struct {
-    s1, s2 []int
+    st1 []int
+    st2 []int
 }
 
 
 func Constructor() MyQueue {
     return MyQueue{
-        s1: []int{},
-        s2: []int{},
+        st1: []int{},
+        st2: []int{},
     }
 }
 
 
 func (this *MyQueue) Push(x int)  {
-    this.s1 = append(this.s1, x)
+    this.st1 = append(this.st1, x)
 }
 
-
-func(this *MyQueue) flipOneIntoTwo() {
-    if len(this.s2) > 0 {return}
-    for len(this.s1) != 0 {
-        top := this.s1[len(this.s1)-1]
-        this.s1 = this.s1[:len(this.s1)-1]
-        this.s2 = append(this.s2, top)
+func(this *MyQueue) flipSt1IntoSt2() {
+    if len(this.st2) > 0 {return}
+    for len(this.st1) != 0 {
+        top := this.st1[len(this.st1)-1]
+        this.st1 = this.st1[:len(this.st1)-1]
+        this.st2 = append(this.st2, top)
     }
 }
 
+
 func (this *MyQueue) Pop() int {
-    this.flipOneIntoTwo()    
-    top := this.s2[len(this.s2)-1]
-    this.s2 = this.s2[:len(this.s2)-1]
+    this.flipSt1IntoSt2()
+    top := this.st2[len(this.st2)-1]
+    this.st2 = this.st2[:len(this.st2)-1]
     return top
 }
 
 
 func (this *MyQueue) Peek() int {
-    this.flipOneIntoTwo()
-    return this.s2[len(this.s2)-1]
+    this.flipSt1IntoSt2()
+    return this.st2[len(this.st2)-1]
 }
 
 
 func (this *MyQueue) Empty() bool {
-    return len(this.s1) == 0 && len(this.s2) == 0    
+    return len(this.st1) == 0 && len(this.st2) == 0 
 }
 
 
