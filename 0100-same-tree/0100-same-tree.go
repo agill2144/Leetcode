3ede7e9f658e08facc96dd06a483c96e52263240
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-03 12:35:01 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (79.94%) - LeetHub

--- 0100-same-tree/0100-same-tree.go
+++ 0100-same-tree/0100-same-tree.go
@@ -7,8 +7,8 @@
  * }
  */
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-    if p != nil && q == nil {return false}
     if p == nil && q != nil {return false}
+    if p != nil && q == nil {return false}
     if p == nil && q == nil {return true}
     if p.Val != q.Val {return false}
     return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
