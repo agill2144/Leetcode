c0a24b4d2656c512188a0c67d77b44980ee746ad
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-02 20:15:10 -0700

    Time: 0 ms (100.00%), Space: 2 MB (51.17%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.go
@@ -0,0 +1,37 @@
+func letterCombinations(digits string) []string {
+    if len(digits) == 0 {return nil}
+
+    digMap := map[byte][]string{
+        '2':[]string{"a","b", "c"},
+        '3':[]string{"d","e", "f"},
+        '4':[]string{"g","h", "i"},
+        '5':[]string{"j","k", "l"},
+        '6':[]string{"m","n", "o"},
+        '7':[]string{"p","q", "r", "s"},
+        '8':[]string{"t","u", "v"},
+        '9':[]string{"w","x", "y", "z"},
+    }
+
+    out := []string{}
+    var dfs func(ptr int, word string)
+    dfs = func(ptr int, word string) {
+        // base
+        if ptr == len(digits) {
+            if len(word) == len(digits) {out = append(out, word)}
+            return
+        }
+        
+        // logic
+        for i := ptr; i < len(digits); i++ {
+            currDig := digits[i]
+            chars := digMap[currDig]
+            for j := 0; j < len(chars); j++ {
+                word += chars[j]
+                dfs(i+1, word)
+                word = word[:len(word)-1]
+            }
+        }
+    }
+    dfs(0, "")
+    return out
+}
