4c440b5748a7f081466982c59854ba24e55858d6
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-07 20:37:31 -0700

    Time: 2 ms (86.36%), Space: 3.2 MB (98.86%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
@@ -1,5 +1,3 @@
-
-// 
 // when we cannot determine a sorted half, shrink our window! FUCKING SHIT
 // because of this worst case TC = o(n) ; example: [1,1,1,1,1,1,2,1,1,1,1] ; target = 2
 // our target is surrounded by n-1 dupes, therefore our left and right ptr see most of the elements , i.e seeing all elements
@@ -14,6 +12,8 @@ func search(nums []int, target int) bool {
         mid := left+(right-left)/2
         if nums[mid] == target {return true}
         
+        // we are at a point where we cannot confidently make a decision on picking a sorted side
+        // therefore shrink our window and continue searching
         if nums[mid] == nums[left] && nums[mid] == nums[right] {
             left++
             right--
