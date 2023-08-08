ea3664d09207e123d618da0da5de5431cd59ba1d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-07 19:46:52 -0700

    Time: 0 ms (100.00%), Space: 3.2 MB (98.86%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
@@ -0,0 +1,32 @@
+func search(nums []int, target int) bool {
+    left := 0
+    right := len(nums)-1
+    
+    for left <= right {
+        mid := left+(right-left)/2
+        if nums[mid] == target {return true}
+        
+        if nums[mid] == nums[left] && nums[mid] == nums[right] {
+            // when we cannot determine a sorted half, shrink our window!
+            left++
+            right--
+            continue
+        }
+        
+        // now classic ; search in rotated sorted array by finding the sorted half
+        if nums[left] <= nums[mid] {
+            if target >= nums[left] && target <= nums[mid] {
+                right = mid-1
+            } else {
+                left = mid+1
+            } 
+        } else {
+            if target <= nums[right] && target >= nums[mid] {
+                left = mid+1
+            } else {
+                right = mid-1
+            }
+        }
+    }
+    return false
+}
