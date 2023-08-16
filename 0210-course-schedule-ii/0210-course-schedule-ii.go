4de2dd4955c9d149ca2076a7e1f9fedcadd56a7f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-16 01:20:15 -0700

    Time: 7 ms (92.24%), Space: 6.7 MB (24.22%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.go
+++ 0210-course-schedule-ii/0210-course-schedule-ii.go
@@ -1,16 +1,27 @@
+// topo sort can tell us whether there was a cycle in directed graph or not
+// by checking if the len of topo sort == v 
+// if len of topo sort == v, it means we were able to process all v nodes,
+// if not, it means there were some cycles
 func findOrder(numCourses int, prerequisites [][]int) []int {
     // [a,b] ; a depends on b ; b is independent b->a
-    
-    
+
+    // v = number of courses
+    // e = total number of edges
+
+    // space = o(v+e); each vth course could have e edges {v:[edges...]}
     adjList := map[int][]int{}
+    
+    // time = o(e)
     for i := 0; i < len(prerequisites); i++ {
         a,b := prerequisites[i][0], prerequisites[i][1]
         adjList[b] = append(adjList[b], a)
     }
-    
+
+    // time = o(v)
     visited := make([]bool, numCourses)
     st := []int{}
     
+    // time = o(v+e), we 
     var dfs func(node int, path []bool) bool
     dfs = func(node int, path []bool) bool {
         // base
@@ -21,7 +32,8 @@ func findOrder(numCourses int, prerequisites [][]int) []int {
         visited[node] = true
         path[node] = true
         for _, nei := range adjList[node] {
-            if !dfs(nei, path) {return false}
+                if !dfs(nei, path) {return false}            
+            
         }
         st = append(st, node)
         path[node] = false
