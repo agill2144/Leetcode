0e548e42a8a5d1ef71e6c5013cd3722337aa36bf
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-16 01:17:09 -0700

    Time: 11 ms (75.78%), Space: 6.5 MB (67.70%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.go
+++ 0210-course-schedule-ii/0210-course-schedule-ii.go
@@ -1,20 +1,16 @@
-// time = o(v+e) + o(v+e) + o(v)
-// space = o(v+e) + o(v) + o(v)
 func findOrder(numCourses int, prerequisites [][]int) []int {
-    // o(v+e) space
-    adjList := map[int][]int{}
+    // [a,b] ; a depends on b ; b is independent b->a
     
-    // o(v+e) time
+    
+    adjList := map[int][]int{}
     for i := 0; i < len(prerequisites); i++ {
-        a, b := prerequisites[i][0], prerequisites[i][1]
-        // a depends on b
-        // b is independent
+        a,b := prerequisites[i][0], prerequisites[i][1]
         adjList[b] = append(adjList[b], a)
     }
     
-    st := []int{}
-    // o(v) space
     visited := make([]bool, numCourses)
+    st := []int{}
+    
     var dfs func(node int, path []bool) bool
     dfs = func(node int, path []bool) bool {
         // base
@@ -27,20 +23,22 @@ func findOrder(numCourses int, prerequisites [][]int) []int {
         for _, nei := range adjList[node] {
             if !dfs(nei, path) {return false}
         }
-        path[node] = false
         st = append(st, node)
+        path[node] = false
         return true
     }
-
-    // o(v) space
+    
     p := make([]bool, numCourses)
-    // o(v+e) time and o(v) recursive stack space
     for i := 0; i < numCourses; i++ {
-        if !visited[i] && !dfs(i, p) {return nil}
+        if !visited[i] {
+            if !dfs(i, p) {return nil}
+        }
     }
+    // could not take all courses
     if len(st) != numCourses {return nil}
-    for i := 0; i < len(st)/2 ; i++ {
-        st[i], st[len(st)-1-i] = st[len(st)-1-i], st[i] 
+    for i := 0; i < len(st)/2; i++ {
+        st[i], st[len(st)-1-i] = st[len(st)-1-i], st[i]
     }
     return st
+    
 }
