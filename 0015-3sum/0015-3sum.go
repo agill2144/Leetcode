1ae33e0ee64f7e2887580a143ee107719bf4cac3
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-16 21:12:27 -0700

    Time: 35 ms (98.49%), Space: 8.1 MB (24.20%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -1,34 +1,77 @@
-// for each ith element, do twoSum using complement search
-// what will be the target for the twoSum complement search; 
-//  - what number do we need to add to ith element for it to equal to 0
-//  - 0-ithNumber
-// so our twoSum complement search will search for 0-ithNumber ( i being the outerloop idx )
-// time : o(n) * o(n) = o(n^2)
+
+
 func threeSum(nums []int) [][]int {
+    sort.Ints(nums)
     out := [][]int{}
-    outSeen := map[[3]int]struct{}{}
-    
-    for i := 0; i < len(nums); i++ { // o(n)
-        outterNum := nums[i]
-        target := 0-outterNum
+    for i := 0; i < len(nums); i++ {
         
-        seen := map[int]struct{}{}
-        for j := i+1; j < len(nums); j++ { // o(n)
-            compl := target-nums[j]  
-            if _, ok := seen[compl]; ok {
-                triplet := []int{outterNum, nums[j], compl}
-                sort.Ints(triplet) // (o3log3)
-                tmp := [3]int{triplet[0], triplet[1], triplet[2]} 
-                if _, ok := outSeen[tmp]; !ok {
-                    outSeen[tmp] = struct{}{}
-                    out = append(out, triplet)
-                }
+        // skip ith value which is a dupe of previous
+        if i != 0 && nums[i] == nums[i-1] {continue}
+        
+        // 2Sum two-ptr from here
+        left := i+1
+        right := len(nums)-1
+        for left < right {
+            sum := nums[left] + nums[right] + nums[i]
+            if sum == 0 {
+                out = append(out, []int{nums[i], nums[left], nums[right]})
+                // we have used both values, move both ptrs
+                left++
+                right--
+                // escape dupes
+                for left < right && nums[left] == nums[left-1] {left++}
+                for left < right && nums[right] == nums[right+1] {right--}
+                
+            } else if sum > 0 {
+                // sum is too big, move away from bigger number ( i.e right ptr since arr is sorted )
+                right--
+            } else {
+                // sum is too small, move away from smaller number
+                left++
             }
-            seen[nums[j]] = struct{}{}
         }
-    } 
-    // total time = o(n) x o(n) x o(3log3) = o(n^2)
-    // space = o(n) + o(n) + o(n) ; 3 sets
+    }
     return out
 }
 
+
+// complement search
+// time = o(n) * o(n) = o(n^2)
+// space = o(k) for tracking uniq triplets * o(n) for 2sum complement search set = o(nk)
+// func threeSum(nums []int) [][]int {
+//     // To keep track of uniq triplets
+//     // k uniq triplets
+//     // o(k) space
+//     uniqTripSet := map[[3]int]struct{}{}
+//     // we are not using a idx map, becuase we have to form triplets using their values instead of idx
+//     ans := [][]int{}
+//     // o(n) time
+//     for i := 0; i < len(nums); i++ {
+//         ithVal := nums[i]
+//         target := 0-ithVal
+        
+//         // why is this set inside ?
+//         // this is 2sum from here
+//         // in 2sum, we needed idx of pairs
+//         // in this, we need values, therefore set
+//         // space = o(n)
+//         set := map[int]struct{}{}
+        
+//         // inner loop , time = o(n) * o(3log3) = o(n)
+//         for j := i+1; j < len(nums); j++ {
+//             jthVal := nums[j]
+//             diff := target-jthVal
+//             if _, ok := set[diff]; ok {
+//                 triplet := []int{ithVal,jthVal,diff}
+//                 sort.Ints(triplet)
+//                 tmp := [3]int{triplet[0], triplet[1], triplet[2]}
+//                 if _, ok := uniqTripSet[tmp]; !ok {
+//                     ans = append(ans, triplet) 
+//                     uniqTripSet[tmp] = struct{}{}
+//                 }
+//             }
+//             set[jthVal] = struct{}{}
+//         }
+//     }
+//     return ans
+// }
