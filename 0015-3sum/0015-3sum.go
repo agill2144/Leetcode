d581fcc7d823ca444ecf2137b49391fb8c2cae57
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-16 20:48:26 -0700

    Time: 1441 ms (7.82%), Space: 14.1 MB (5.04%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -1,37 +1,26 @@
-// for each ith element, do twoSum using complement search
-// what will be the target for the twoSum complement search; 
-//  - what number do we need to add to ith element for it to equal to 0
-//  - 0-ithNumber
-// so our twoSum complement search will search for 0-ithNumber ( i being the outerloop idx )
-// time : o(n) * o(n) = o(n^2)
 func threeSum(nums []int) [][]int {
-    out := [][]int{}
-    set := map[int]struct{}{}
-    outSeen := map[[3]int]struct{}{}
-    
-    for i := 0; i < len(nums); i++ { // o(n)
-        outterNum := nums[i]
-        if _, ok := set[outterNum]; ok {continue}
-        set[outterNum] = struct{}{}
-        target := 0-outterNum
-        
-        seen := map[int]struct{}{}
-        for j := i+1; j < len(nums); j++ { // o(n)
-            compl := target-nums[j]  
-            if _, ok := seen[compl]; ok {
-                triplet := []int{outterNum, nums[j], compl}
-                sort.Ints(triplet) // (o3log3)
-                tmp := [3]int{triplet[0], triplet[1], triplet[2]} 
-                if _, ok := outSeen[tmp]; !ok {
-                    outSeen[tmp] = struct{}{}
-                    out = append(out, triplet)
+    // To keep track on uniq triplets
+    uniqTripSet := map[[3]int]struct{}{}
+    // we are not using a idx map, becuase we have to form triplets using their values instead of idx
+    ans := [][]int{}
+    for i := 0; i < len(nums); i++ {
+        ithVal := nums[i]
+        target := 0-ithVal
+        set := map[int]struct{}{}
+        for j := i+1; j < len(nums); j++ {
+            jthVal := nums[j]
+            diff := target-jthVal
+            if _, ok := set[diff]; ok {
+                triplet := []int{ithVal,jthVal,diff}
+                sort.Ints(triplet)
+                tmp := [3]int{triplet[0], triplet[1], triplet[2]}
+                if _, ok := uniqTripSet[tmp]; !ok {
+                    ans = append(ans, triplet) 
+                    uniqTripSet[tmp] = struct{}{}
                 }
             }
-            seen[nums[j]] = struct{}{}
+            set[jthVal] = struct{}{}
         }
-    } 
-    // total time = o(n) x o(n) x o(3log3) = o(n^2)
-    // space = o(n) + o(n) + o(n) ; 3 sets
-    return out
+    }
+    return ans
 }
-
