99b03e26bc87e2e6ad6d8bdd6d9d721da0af7736
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-16 19:43:01 -0700

    Time: 12 ms (62.42%), Space: 5.4 MB (32.63%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
@@ -1,35 +1,53 @@
-// binary search the complement
-// time = o(n) * o(logN)
-// space = o(1)
+// two ptrs
+
 func twoSum(nums []int, target int) []int {
-    for i := 0; i < len(nums); i++ {
-        diff := target - nums[i]
-        val := binarySearch(diff, i+1, nums)
-        if val != i && val != -1 {
-            if i < val {
-                return []int{i+1, val+1}            
-            } else {
-                return []int{val+1, i+1}                            
-            }
+    left := 0
+    right := len(nums)-1
+    for left < right {
+        sum := nums[left] + nums[right]
+        if sum == target {
+            return []int{left+1, right+1}
+        } else if sum > target {
+            right--
+        } else {
+            left++
         }
     }
     return nil
-
 }
 
+// binary search the complement
+// time = o(n) * o(logN)
+// space = o(1)
+// func twoSum(nums []int, target int) []int {
+//     for i := 0; i < len(nums); i++ {
+//         diff := target - nums[i]
+//         val := binarySearch(diff, i+1, nums)
+//         if val != i && val != -1 {
+//             if i < val {
+//                 return []int{i+1, val+1}            
+//             } else {
+//                 return []int{val+1, i+1}                            
+//             }
+//         }
+//     }
+//     return nil
 
-func binarySearch(target int, left int, nums []int) int {
-    right := len(nums)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        if nums[mid] == target {
-            return mid
-        } else if target > nums[mid] {
-            left = mid+1
-        } else {
-            right = mid-1
-        }
-    }
-    return -1
-}
+// }
+
+
+// func binarySearch(target int, left int, nums []int) int {
+//     right := len(nums)-1
+//     for left <= right {
+//         mid := left + (right-left)/2
+//         if nums[mid] == target {
+//             return mid
+//         } else if target > nums[mid] {
+//             left = mid+1
+//         } else {
+//             right = mid-1
+//         }
+//     }
+//     return -1
+// }
 
