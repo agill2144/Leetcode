88d50b3a952dfdf07568f8443577ae93123bb445
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-26 12:02:56 -0700

    Time: 5 ms (64.53%), Space: 3.2 MB (5.91%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
@@ -1,36 +1,30 @@
-// when we cannot determine a sorted half, shrink our window! FUCKING SHIT
-// because of this worst case TC = o(n) ; example: [1,1,1,1,1,1,2,1,1,1,1] ; target = 2
-// our target is surrounded by n-1 dupes, therefore our left and right ptr see most of the elements , i.e seeing all elements
-// avg tc = o(logn)
-// space = o(1)
 func search(nums []int, target int) bool {
     left := 0
     right := len(nums)-1
-    
     for left <= right {
         mid := left+(right-left)/2
-        if nums[mid] == target {return true}
+        if nums[mid] == target {
+            return true
+        }
         
-        // we are at a point where we cannot confidently make a decision on picking a sorted side
-        // therefore shrink our window and continue searching
-        if nums[mid] == nums[left] && nums[mid] == nums[right] {
-            left++
-            right--
+        // when we cannot determine the sorted half ( when left == mid == right )
+        // shrink the window and restart binary search
+        if nums[left] == nums[mid] && nums[mid] == nums[right] {
+            left++; right--
             continue
         }
         
-        // now classic ; search in rotated sorted array by finding the sorted half
         if nums[left] <= nums[mid] {
             if target >= nums[left] && target <= nums[mid] {
                 right = mid-1
             } else {
                 left = mid+1
-            } 
+            }
         } else {
-            if target <= nums[right] && target >= nums[mid] {
+            if target >= nums[mid] && target <= nums[right]{
                 left = mid+1
             } else {
-                right = mid-1
+                right=mid-1
             }
         }
     }
