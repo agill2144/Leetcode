27f11f054e76660ec00c0ec378305e3487f0efef
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-25 17:27:56 -0700

    Time: 32 ms (75.51%), Space: 7.3 MB (26.53%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.go
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.go
@@ -1,26 +1,43 @@
-// dp vibes
 func findLongestChain(pairs [][]int) int {
     sort.Slice(pairs, func(i, j int)bool {
-        return pairs[i][0] < pairs[j][0]
+        return pairs[i][1] < pairs[j][1]
     })
-    
-    dp := make([]int, len(pairs))
-    dp[0] = 1
-    someMax := 1
+    // keep a running num and keep incrementing count if curr pair is forming a chain
+    // then update the running num
+    count := 1
+    running := pairs[0][1]
     for i := 1; i < len(pairs); i++ {
-        curr := pairs[i]
-        dp[i] = 1
-        for j := i-1; j >= 0; j-- {
-            prev := pairs[j]
-            if curr[0] > prev[1] {
-                dp[i] = max(dp[i], dp[j]+1)
-            }
-            someMax = max(someMax, dp[i])
+        if pairs[i][0] > running {
+            count++
+            running = pairs[i][1]
         }
-        
     }
-    return someMax
+    return count
 }
+
+// dp vibes
+// func findLongestChain(pairs [][]int) int {
+//     sort.Slice(pairs, func(i, j int)bool {
+//         return pairs[i][0] < pairs[j][0]
+//     })
+    
+//     dp := make([]int, len(pairs))
+//     dp[0] = 1
+//     someMax := 1
+//     for i := 1; i < len(pairs); i++ {
+//         curr := pairs[i]
+//         dp[i] = 1
+//         for j := i-1; j >= 0; j-- {
+//             prev := pairs[j]
+//             if curr[0] > prev[1] {
+//                 dp[i] = max(dp[i], dp[j]+1)
+//             }
+//             someMax = max(someMax, dp[i])
+//         }
+        
+//     }
+//     return someMax
+// }
 
 func max(x, y int) int {
     if x > y {return x}
