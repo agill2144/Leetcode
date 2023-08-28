c2a14572b677eaded64123a3f47f3caf34f27838
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-27 22:29:15 -0700

    Time: 45 ms (55.36%), Space: 7 MB (82.14%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.go
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.go
@@ -4,7 +4,13 @@ func subarraysDivByK(nums []int, k int) int {
     sum := 0
     for i := 0; i < len(nums); i++ {
         sum += nums[i]
-        rem := sum % k 
+        rem := (sum % k + k) % k
+        
+        /*
+            in golang C++, when we do -1 % 2 , we get the result as -1 , 
+            whereas in other langs like python, the remainder is 1 ( the correct ans )
+            so the remedy here is to just add the divisor(k) whenever the remainder is -ve
+        */
         if rem < 0 {rem += k}
         val, ok := freq[rem]
         if ok {
