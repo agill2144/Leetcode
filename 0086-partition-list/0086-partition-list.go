8f0413644551b73fc799df23deb1126d554f93a7
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-29 19:46:49 -0700

    Time: 3 ms (39.87%), Space: 2.5 MB (18.19%) - LeetHub

--- 0086-partition-list/0086-partition-list.go
+++ 0086-partition-list/0086-partition-list.go
@@ -6,31 +6,24 @@
  * }
  */
 func partition(head *ListNode, x int) *ListNode {
-    if head == nil || head.Next == nil {return head}
-    
-    left := &ListNode{Val: -1}
+    left := &ListNode{Val: 0}
     leftTail := left
-    right := &ListNode{Val: -1}
+    right := &ListNode{Val: 0}
     rightTail := right
-
+    
     curr := head
     for curr != nil {
-        next := curr.Next
-        if curr.Val < x {
-            curr.Next = nil
-            leftTail.Next = curr
-            leftTail = leftTail.Next
-        } else {
-            curr.Next = nil
-            rightTail.Next = curr
+        if curr.Val >= x {
+            rightTail.Next = &ListNode{Val: curr.Val}
             rightTail = rightTail.Next
+        } else {
+            leftTail.Next = &ListNode{Val: curr.Val}
+            leftTail = leftTail.Next
         }
-        curr = next
+        curr = curr.Next
     }
-    // speed up from dummy head nodes
     left = left.Next
     right = right.Next
-    // if there is a left side to connect
     if left != nil {
         leftTail.Next = right
         return left
