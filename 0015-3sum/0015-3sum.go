60988017119fe6e4a3d5e9074a30ff6ca5bf4992
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-31 23:03:25 -0700

    Time: 47 ms (67.17%), Space: 7.7 MB (60.33%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -1,37 +1,70 @@
+
 func threeSum(nums []int) [][]int {
     sort.Ints(nums)
     out := [][]int{}
-    set := map[[3]int]struct{}{}
-    for i := 0; i < len(nums); i++ { // o(n)
-        target := 0-nums[i]
-        for j := i+1; j < len(nums); j++ { // o(n)
-            k := binarySearch(nums, target-nums[j], j+1) // o(logn)
-            if k != -1 {
-                tmp := []int{nums[i], nums[j], nums[k]}
-                if _, ok := set[[3]int{tmp[0], tmp[1], tmp[2]}]; !ok {
-                    set[[3]int{tmp[0], tmp[1], tmp[2]}] = struct{}{}
-                    out = append(out, tmp)
-                }
+    for i := 0; i < len(nums); i++ {
+        if i != 0 && nums[i] == nums[i-1] {continue}
+        left := i+1
+        right := len(nums)-1
+        for left < right {
+            sum := nums[i] + nums[left] + nums[right]
+            if sum == 0 {
+                out = append(out, []int{nums[i], nums[left], nums[right]})
+                left++
+                for left < right && nums[left] == nums[left-1] {left++}
+                right-- 
+                for left < right && nums[right] == nums[right+1] {right--}
+            } else if sum < 0 {
+                left++
+            } else {
+                right--
             }
         }
     }
     return out
 }
 
-func binarySearch(nums []int, target, left int) int {
-    right := len(nums)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        if nums[mid] == target {
-            return mid
-        } else if target > nums[mid] {
-            left = mid+1
-        } else {
-            right = mid-1
-        }
-    }
-    return -1
-}
+/*
+    approach: sort + binary search
+    - continue using a set to ensure unique triplets
+    time = o(n) * o(nlogn) = o(n^2logn)
+    space = o(k) unique triplets in set
+*/
+
+// func threeSum(nums []int) [][]int {
+//     sort.Ints(nums)
+//     out := [][]int{}
+//     set := map[[3]int]struct{}{}
+//     for i := 0; i < len(nums); i++ { // o(n)
+//         target := 0-nums[i]
+//         for j := i+1; j < len(nums); j++ { // o(n)
+//             k := binarySearch(nums, target-nums[j], j+1) // o(logn)
+//             if k != -1 {
+//                 tmp := []int{nums[i], nums[j], nums[k]}
+//                 if _, ok := set[[3]int{tmp[0], tmp[1], tmp[2]}]; !ok {
+//                     set[[3]int{tmp[0], tmp[1], tmp[2]}] = struct{}{}
+//                     out = append(out, tmp)
+//                 }
+//             }
+//         }
+//     }
+//     return out
+// }
+
+// func binarySearch(nums []int, target, left int) int {
+//     right := len(nums)-1
+//     for left <= right {
+//         mid := left + (right-left)/2
+//         if nums[mid] == target {
+//             return mid
+//         } else if target > nums[mid] {
+//             left = mid+1
+//         } else {
+//             right = mid-1
+//         }
+//     }
+//     return -1
+// }
 
 /*
     approach: better brute force
