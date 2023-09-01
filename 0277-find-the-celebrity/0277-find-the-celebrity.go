2851ba1e27cad49c87649662b0357b3b7cdf41f7
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-31 19:09:00 -0700

    Time: 48 ms (58.82%), Space: 6.9 MB (100.00%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.go
+++ 0277-find-the-celebrity/0277-find-the-celebrity.go
@@ -4,22 +4,27 @@
  */
 func solution(knows func(a int, b int) bool) func(n int) int {
     return func(n int) int {
-        a := 0
+        potential := 0
         for i := 0; i < n; i++ {
-            if knows(a, i) {
-                // if a knows b, that means a->b, so now b is the new possibility
-                // a celeberity must NOT know anyone, if a know b, a is no longer the right ans
-                a = i
+            if knows(potential, i) {
+                // if $potential knows i, that means $potential->i, so now i is the new possibility
+                // a celeberity must NOT know anyone, if $potential knows i, $potential is no longer the right ans
+                potential = i
             }
         }
         
-        // now check if everyone else know a
+        // now check if everyone else knows $potential
         for i := 0; i < n; i++ {
-            if i == a {continue}
-            if knows(a, i) || !knows(i, a) {return -1}
+            if potential == i {continue}
+            /*
+                $potential knows someone else or
+                someone else does not know $potential,
+                we dont have an ans
+            */
+            if knows(potential, i) || !knows(i, potential) {return -1}
         }
         
-        return a
+        return potential
 
     }
 }
