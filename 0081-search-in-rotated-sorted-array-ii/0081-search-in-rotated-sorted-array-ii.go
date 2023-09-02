edbcdc19d5e13ecf6594c7606a6a3a6c11d689ba
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-02 13:22:03 -0700

    Time: 3 ms (77.23%), Space: 3.2 MB (6.14%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
@@ -2,29 +2,24 @@ func search(nums []int, target int) bool {
     left := 0
     right := len(nums)-1
     for left <= right {
-        mid := left+(right-left)/2
-        if nums[mid] == target {
-            return true
-        }
+        mid := left + (right-left)/2
+        if nums[mid] == target {return true}
         
-        // when we cannot determine the sorted half ( when left == mid == right )
-        // shrink the window and restart binary search
-        if nums[left] == nums[mid] && nums[mid] == nums[right] {
-            left++; right--
-            continue
+        if nums[mid] == nums[left] && nums[mid] == nums[right] {
+            left++; right--; continue
         }
         
         if nums[left] <= nums[mid] {
-            if target >= nums[left] && target <= nums[mid] {
-                right = mid-1
+            if target <= nums[mid] && target >= nums[left] {
+                right=mid-1
             } else {
                 left = mid+1
             }
         } else {
-            if target >= nums[mid] && target <= nums[right]{
+            if target >= nums[mid] && target <= nums[right] {
                 left = mid+1
             } else {
-                right=mid-1
+                right = mid-1
             }
         }
     }
