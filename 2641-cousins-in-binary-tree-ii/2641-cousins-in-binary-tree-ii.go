2f01d17ecc226bb641eb03c372506d88f4cc31ff
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-03 11:19:42 -0700

    Time: 302 ms (88.10%), Space: 32.1 MB (40.48%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.go
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.go
@@ -7,106 +7,58 @@
  * }
  */
 
-
+/*
+    We have to replace a node value with sum of its cousins value
+    Cousins are all nodes on the same level with different parents
+    
+    approach: two pass dfs
+    - 1st pass: get level sum for each level and store it by level ( hashmap )
+    - 2nd pass;
+        - have each node look down and replace values of its childs
+        - Each node has 2 childs, i.e this node is a parent of left and right child
+        - so this node can look down and sum its childs sum ( siblings )
+        - then this same node can get the next level sum ( currLevel + 1 ) from hashmap
+        - and remove its childs sum from the levelSum
+        - this becomes the new value for both of its childs
+    
+    time = o(n)
+    space = o(n)
+*/
 func replaceValueInTree(root *TreeNode) *TreeNode {
+    if root == nil {return root}
     levelSum := map[int]int{}
-    var dfs func(r *TreeNode, level int)
-    dfs = func(r *TreeNode, level int) {
+    var dfs1 func(r *TreeNode, level int)
+    dfs1 = func(r *TreeNode, level int) {
         // base
         if r == nil {return}
         
         // logic
         levelSum[level] += r.Val
-        dfs(r.Left, level+1)
-        dfs(r.Right, level+1)
+        dfs1(r.Left, level+1)
+        dfs1(r.Right, level+1)
     }
-    var siblingSumDfs func(r *TreeNode, level int)
-    siblingSumDfs = func(r *TreeNode, level int) {
+    // populate level sum
+    dfs1(root, 0)
+    
+    var dfs2 func(r *TreeNode, level int)
+    dfs2 = func(r *TreeNode, level int) {
         // base
         if r == nil {return}
         
         // logic
-        if r == root {r.Val = 0}
-        nextLevelSum ,ok := levelSum[level+1]
+        nextLevelSum, ok := levelSum[level+1]
         if ok {
-            // if we have next level's sum
-            // $this node knows its childs
-            // these 2 childs are siblings ( they belong to the same parent )
-            // so standing at $this node, we can get sibling sum 
-            // and remove sibling sum from nextLevelTotalSum
-            // that will be sum of all cousins in the next level
-            // then $this node can set its child values to the diff value
-            siblingSum := 0
-            if r.Left != nil {siblingSum += r.Left.Val}
-            if r.Right != nil {siblingSum += r.Right.Val}
-            if r.Left != nil {r.Left.Val = nextLevelSum - siblingSum}
-            if r.Right != nil {r.Right.Val = nextLevelSum - siblingSum}
+            childSum := 0
+            if r.Left != nil {childSum += r.Left.Val}
+            if r.Right != nil {childSum += r.Right.Val}
+            cousinSum := nextLevelSum - childSum
+            if r.Left != nil {r.Left.Val = cousinSum}
+            if r.Right != nil {r.Right.Val = cousinSum}
         }
-        siblingSumDfs(r.Left, level+1)
-        siblingSumDfs(r.Right, level+1)
+        dfs2(r.Left, level+1)
+        dfs2(r.Right, level+1)        
     }
-    dfs(root, 0)
-    siblingSumDfs(root, 0)
+    dfs2(root, 0)
+    root.Val = 0
     return root
 }
-
-
-// time = o(n)
-// o(n) time for initial bfs +
-// o(n) time for dfs
-
-// space = o(n+maxWidth+h)
-// o(maxWidth) queue space +
-// o(n) space for levels map +   
-// o(h) space for dfs recursive stack
-// func replaceValueInTree(root *TreeNode) *TreeNode {
-//     type level struct {
-//         sum int
-//         parentChildSum map[*TreeNode]int
-//     }
-//     levels := map[int]*level{}
-//     q := [][]*TreeNode{{root, nil}} // {node, parent}
-//     currLevel := 0
-//     for len(q) != 0 {
-//         qSize := len(q)
-//         parentSum := map[*TreeNode]int{}
-//         levelSum := 0
-//         for qSize != 0 {
-//             dq := q[0]
-//             q = q[1:]
-//             node := dq[0]
-//             parent := dq[1]
-            
-//             levelSum += node.Val
-//             parentSum[parent] += node.Val
-            
-//             if node.Left != nil {q = append(q, []*TreeNode{node.Left, node})}
-//             if node.Right != nil {q = append(q, []*TreeNode{node.Right, node})}
-//             qSize--
-//         }
-//         levels[currLevel] = &level{
-//             sum: levelSum,
-//             parentChildSum: parentSum,
-//         }
-//         currLevel++
-//     }
-//     var dfs func(r, p *TreeNode, level int)
-//     dfs = func(r, p *TreeNode, level int) {
-//         // base
-//         if r == nil {return}
-        
-//         // logic
-//         if p == nil {
-//             r.Val = 0
-//         } else {
-//             totalLevelSum := levels[level].sum
-//             // siblings are childs with same value, their sum is saved by parent in each level
-//             siblingSum := levels[level].parentChildSum[p]
-//             r.Val = totalLevelSum - siblingSum
-//         }
-//         dfs(r.Left, r, level+1)
-//         dfs(r.Right, r, level+1)
-//     }
-//     dfs(root, nil, 0)
-//     return root
-// }
