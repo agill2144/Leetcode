ff1d585c4e9ff5d60a69ce7a47ca4734b7074924
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-04 18:57:21 -0700

    Time: 0 ms (100.00%), Space: 2.4 MB (90.77%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.go
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.go
@@ -6,21 +6,24 @@
  *     Right *TreeNode
  * }
  */
+
+
 func minDiffInBST(root *TreeNode) int {
-    diff := math.MaxInt64
-    var dfs func(r *TreeNode) 
+    minDiff := math.MaxInt64
     var prev *TreeNode
+    var dfs func(r *TreeNode)
     dfs = func(r *TreeNode) {
         // base
         if r == nil {return}
+        
         // logic
         dfs(r.Left)
         if prev != nil {
-            if r.Val - prev.Val < diff {diff = r.Val-prev.Val}
+            if r.Val - prev.Val < minDiff {minDiff = r.Val-prev.Val}
         }
         prev = r
         dfs(r.Right)
     }
     dfs(root)
-    return diff
+    return minDiff
 }
