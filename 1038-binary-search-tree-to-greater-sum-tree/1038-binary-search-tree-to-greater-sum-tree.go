0e71fad9e25aa3ffc79edd2fc854bc1b73836c78
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-04 19:42:59 -0700

    Time: 0 ms (100.00%), Space: 2.2 MB (96.61%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.go
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func bstToGst(root *TreeNode) *TreeNode {
+    /*
+        1. get the total sum
+        2. Remove the runningSum when running inorder bst + add curr val
+    */
+    total := 0
+    var dfs func(r *TreeNode)
+    dfs = func(r *TreeNode) {
+        // base
+        if r == nil {return}
+        
+        // logic
+        total += r.Val
+        dfs(r.Left)
+        dfs(r.Right)
+    }
+    dfs(root)
+    
+    running := 0
+    var dfs2 func(r *TreeNode)
+    dfs2 = func(r *TreeNode) {
+        // base
+        if r == nil {return}
+        
+        // logic
+        dfs2(r.Left)
+        running += r.Val
+        r.Val = total-running+r.Val
+        dfs2(r.Right)
+    }
+    dfs2(root)
+    return root
+}
