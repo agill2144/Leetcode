2d94b8d3c1e8ae7e149219e6f11bdcee6fae4ee2
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-05 22:22:28 -0700

    Time: 0 ms (100.00%), Space: 5.4 MB (30.74%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.go
+++ 0437-path-sum-iii/0437-path-sum-iii.go
@@ -6,35 +6,68 @@
  *     Right *TreeNode
  * }
  */
+
+
 func pathSum(root *TreeNode, targetSum int) int {
     if root == nil {return 0}
     count := 0
-    var dfs func(r *TreeNode, sum int)
-    dfs = func(r *TreeNode, sum int) {
+    var dfs func(r *TreeNode, rSum int, path map[int]int)
+    dfs = func(r *TreeNode, rSum int, path map[int]int) {
         // base
         if r == nil {return}
         
         // logic
-        sum += r.Val
-        if sum == targetSum {
-            count++
+        rSum += r.Val
+        if rSum == targetSum {count++}
+        diff := rSum-targetSum
+        val, ok := path[diff]
+        if ok {
+            count+=val
         }
-        dfs(r.Left, sum)
-        dfs(r.Right, sum)
+        path[rSum]++
+        dfs(r.Left, rSum, path)
+        dfs(r.Right, rSum, path)
+        path[rSum]--
     }
-    
-    var dfs2 func(r *TreeNode)
-    dfs2 = func(r *TreeNode) {
-        // base
-        if r == nil {return}
-        
-        // logic
-        dfs(r, 0)
-        dfs2(r.Left)
-        dfs2(r.Right)
-    }
-    
-    dfs2(root)
+    dfs(root, 0, map[int]int{})
     return count
 }
+
+/*
+    approach: brute force
+    - top down, make all possible paths from each node
+    time = o(n^2)
+    space = o(n^2)
+*/
+// func pathSum(root *TreeNode, targetSum int) int {
+//     if root == nil {return 0}
+//     count := 0
+//     var dfs func(r *TreeNode, sum int)
+//     dfs = func(r *TreeNode, sum int) {
+//         // base
+//         if r == nil {return}
+        
+//         // logic
+//         sum += r.Val
+//         if sum == targetSum {
+//             count++
+//         }
+//         dfs(r.Left, sum)
+//         dfs(r.Right, sum)
+//     }
+    
+//     var dfs2 func(r *TreeNode)
+//     dfs2 = func(r *TreeNode) {
+//         // base
+//         if r == nil {return}
+        
+//         // logic
+//         dfs(r, 0)
+//         dfs2(r.Left)
+//         dfs2(r.Right)
+//     }
+    
+//     dfs2(root)
+//     return count
+// }
 
