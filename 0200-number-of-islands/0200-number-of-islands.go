f77741ff9a59917fcc796808993f44a6386252cf
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-05 21:11:56 -0700

    Time: 5 ms (65.12%), Space: 3.8 MB (99.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.go
+++ 0200-number-of-islands/0200-number-of-islands.go
@@ -1,26 +1,41 @@
+/*
+    this problem is just finding number of connected components
+    - we can do this in place
+    - or use a visited data structure ( like bool matrix ) ; just like all other graph problems
+    
+    
+    can be done using dfs
+    can be done using bfs
+    - at each cell with value == 1
+        - run dfs/bfs
+        - increment your counter
+        - dfs/bfs will go mark all the cells in that component visited
+
+    time = o(mn)
+    space = o(mn) in dfs we could end up adding all cells in our recursive stack
+*/
 func numIslands(grid [][]byte) int {
+    // in place dfs 
+    dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
     m := len(grid)
     n := len(grid[0])
-    visited := make([][]bool, m)
-    for i := 0; i < m; i++ {
-        visited[i] = make([]bool, n)
-    }
-    dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
+    
     var dfs func(r, c int)
     dfs = func(r, c int) {
         // base
-        if r < 0 || r == m || c < 0 || c == n || grid[r][c] == '0' || visited[r][c] {return}
+        if r < 0 || r == m || c < 0 || c == n || grid[r][c] != '1' {return}
         
         // logic
-        visited[r][c] = true
+        grid[r][c] = '0'
         for _, dir := range dirs {
             dfs(r+dir[0], c+dir[1])
         }
     }
+    
     count := 0
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            if grid[i][j] == '1' && !visited[i][j] {
+            if grid[i][j] == '1' {
                 dfs(i, j)
                 count++
             }
