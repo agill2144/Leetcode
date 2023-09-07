bc1d6390ff4d067095a0b1557193d1b3eea16874
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-06 22:16:29 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (46.94%) - LeetHub

--- 0394-decode-string/0394-decode-string.go
+++ 0394-decode-string/0394-decode-string.go
@@ -1,35 +1,35 @@
 func decodeString(s string) string {
-    i := 0    
-    var dfs func() string
-    dfs = func() string {
-        // base
-        if i == len(s) {return ""}
-        
-        // logic
-        curr := new(strings.Builder)
-        n := 0
-        for i < len(s) {
-            char := s[i]
-            if char >= 'a' && char <= 'z' {
-                curr.WriteByte(char)
-                i++
-            } else if char >= '0' && char <= '9' {
-                n = n * 10 + int(char-'0')
-                i++
-            } else if char == '[' {
-                i++
-                inner := dfs()
-                for i := 0; i < n ; i++ {
-                    curr.WriteString(inner)
-                }
-                n = 0
-            } else if char == ']' {
-                i++
-                return curr.String()
-            }
-        }
-        return curr.String()
-    }
+    numSt := []int{}
+    parentSt := []*strings.Builder{}
     
-    return dfs()
+    curr := new(strings.Builder)
+    num := 0
+    
+    for i := 0; i < len(s); i++ {
+        char := s[i]
+        if char == '[' {
+            numSt = append(numSt, num)
+            parentSt = append(parentSt, curr)
+            curr = new(strings.Builder)
+            num = 0
+        } else if char == ']' {
+            k := numSt[len(numSt)-1]
+            numSt = numSt[:len(numSt)-1]
+            tmp := new(strings.Builder)
+            for k != 0 {
+                tmp.WriteString(curr.String())
+                k--
+            }
+            parent := parentSt[len(parentSt)-1]
+            parentSt = parentSt[:len(parentSt)-1]
+            parent.WriteString(tmp.String())
+            curr = parent
+            
+        } else if char >= '0' && char <= '9' {
+            num = num * 10 + int(char-'0')
+        } else {
+            curr.WriteByte(char)
+        }
+    }
+    return curr.String()
 }
