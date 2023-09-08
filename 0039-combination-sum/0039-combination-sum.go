4a5beb33d7f64b9cb59231722dcf6e41d893bc0c
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-07 22:20:18 -0700

    Time: 0 ms (100.00%), Space: 4.5 MB (20.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.go
+++ 0039-combination-sum/0039-combination-sum.go
@@ -1,23 +1,24 @@
 func combinationSum(candidates []int, target int) [][]int {
     out := [][]int{}
-    var dfs func(t , start int, path []int)
-    dfs = func(t, start int, path []int) {
+    var dfs func(ptr int, t int, path []int)
+    dfs = func(ptr int, t int, path []int) {
         // base
-        if t == 0 {
-            newL := make([]int, len(path))
-            copy(newL, path)
-            out = append(out, newL)
+        if t <= 0 {
+            if t == 0 {
+                newL := make([]int, len(path))
+                copy(newL, path)
+                out = append(out, newL)
+            }
             return
         }
-        if t < 0 || start == len(candidates) {return}
+        if ptr == len(candidates) || t < 0 {return}
         
         // logic
-        for i := start; i < len(candidates); i++ {
-            path = append(path, candidates[i])
-            dfs(t-candidates[i], i, path)
-            path = path[:len(path)-1]
-        }
+        dfs(ptr+1, t, path)
+        path = append(path, candidates[ptr])
+        dfs(ptr, t-candidates[ptr], path)
+        path = path[:len(path)-1]
     }
-    dfs(target, 0, nil)
+    dfs(0, target, []int{})
     return out
 }
