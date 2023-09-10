6c680c4a3a2adf5cc3d9877bbc172ca4aae1cf6c
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-10 14:17:47 -0700

    Time: 26 ms (66.67%), Space: 2 MB (71.60%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.go
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.go
@@ -1,29 +1,44 @@
 func distributeCookies(cookies []int, k int) int {
-    out := math.MaxInt64
-    var dfs func(start int, cookieIdx int, childs []int, max int)
-    dfs = func(start int, cookieIdx int, childs []int, max int) {
-        // base
-        if cookieIdx == len(cookies) {
-            if max < out {out = max}
-            return
-        }
-        
-        // logic
-        for i := start; i < len(childs); i++ {
-            // action
-            childs[i] += cookies[cookieIdx]
-            
-            // we could just update the max arg, but then we would have to save the old value and backtrack
-            newMax := max
-            if childs[i] > newMax {newMax = childs[i]}
+    totalMax:=1000000 // keep current best result
+    childs:=make([]int,k)
 
-            // recurse
-            dfs(start, cookieIdx+1, childs, newMax)
-            
-            // backtrack
-            childs[i] -= cookies[cookieIdx]
+    var dfs func(ind int)int 
+    dfs = func(ind int)int { //ind is index of cookies
+        if len(cookies)==ind{ // all cookies are distributed, so find unfairness
+            maximal:=0
+            for i:=0;i<len(childs);i++{
+                maximal = max(maximal,childs[i])
+            }
+            totalMax = min(totalMax,maximal) // update best result
+            return maximal
         }
+
+        minimal:=1000000
+        cook:=cookies[ind]
+        for i:=0;i<len(childs);i++{
+            childs[i]+=cook
+            if childs[i]<totalMax{ // skip variants that leads to worse result
+                minimal = min(minimal,dfs(ind+1))
+            }
+            childs[i]-=cook
+        }
+        return minimal
     }
-    dfs(0,0,make([]int, k),0)
-    return out
+
+    dfs(0)
+    return totalMax
+}
+
+func max( a,b int)int {
+    if a<b {
+        return b
+    }
+    return a
+}
+
+func min (a, b int)int{
+    if a<b {
+        return a
+    }
+    return b
 }
