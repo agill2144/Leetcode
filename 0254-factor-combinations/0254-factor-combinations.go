cd9cc42eeb5d3b4595143d735c45038743f624c9
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-09 22:57:35 -0700

    Time: 426 ms (75.00%), Space: 6.7 MB (25.00%) - LeetHub

---
+++ 0254-factor-combinations/0254-factor-combinations.go
@@ -0,0 +1,31 @@
+func getFactors(n int) [][]int {
+    if n == 1 {return nil}
+    factors := []int{}
+    for i := 2; i < n-1; i++ {
+        if n % i == 0 {
+            factors = append(factors, i)
+        }
+    }
+    out := [][]int{}
+    var dfs func(start, prod int, path []int)
+    dfs = func(start, prod int, path []int) {
+        // base
+        if prod >= n {
+            if prod == n {
+                newL := make([]int, len(path))
+                copy(newL, path)
+                out = append(out, newL)
+            }
+            return
+        }
+        
+        // logic
+        for i := start; i < len(factors); i++ {
+            path = append(path, factors[i])
+            dfs(i, prod*factors[i], path)
+            path = path[:len(path)-1]
+        }
+    }
+    dfs(0, 1, []int{})
+    return out
+}
