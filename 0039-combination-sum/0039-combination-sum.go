a81e7cf69afb8faa20d5b6ca9d3b2b1c75c5fa4f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-09 18:30:48 -0700

    Time: 0 ms (100.00%), Space: 3.2 MB (53.17%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.go
+++ 0039-combination-sum/0039-combination-sum.go
@@ -1,7 +1,7 @@
 func combinationSum(candidates []int, target int) [][]int {
     out := [][]int{}
-    var dfs func(ptr int, t int, path []int)
-    dfs = func(ptr int, t int, path []int) {
+    var dfs func(start int, t int, path []int)
+    dfs = func(start, t int, path []int) {
         // base
         if t <= 0 {
             if t == 0 {
@@ -11,14 +11,14 @@ func combinationSum(candidates []int, target int) [][]int {
             }
             return
         }
-        if ptr == len(candidates) || t < 0 {return}
         
         // logic
-        dfs(ptr+1, t, path)
-        path = append(path, candidates[ptr])
-        dfs(ptr, t-candidates[ptr], path)
-        path = path[:len(path)-1]
+        for i := start; i < len(candidates); i++ {
+            path = append(path, candidates[i])
+            dfs(i, t-candidates[i], path)
+            path = path[:len(path)-1]
+        }
     }
-    dfs(0, target, []int{})
+    dfs(0, target,[]int{})
     return out
 }
