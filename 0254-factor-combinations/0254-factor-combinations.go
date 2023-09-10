6f29b516f3529f5776f407b0d5339abb808557ae
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-10 12:18:54 -0700

    Time: 221 ms (75.00%), Space: 6.8 MB (25.00%) - LeetHub

--- 0254-factor-combinations/0254-factor-combinations.go
+++ 0254-factor-combinations/0254-factor-combinations.go
@@ -3,12 +3,21 @@
 func getFactors(n int) [][]int {
     if n == 1 {return nil}
     factors := []int{}
+    
+    // Note: factors should be in the range [2, n - 1].
+    // n is not divisible after n/2 
+    // eg; n=12, 12/2 = 6; 12 is not divisible by 7 to 12
+    // eg; n=40, 40/2 = 20; 40 is not divisible by 21 to 40
+    // time = o(n)
+    // space = o(n)
     for i := 2; i <= n/2; i++ {
         if n % i == 0 {
             factors = append(factors, i)
         }
     }
     out := [][]int{}
+    // time = o(2^n+t)
+    // space = o(t) for recursive stack and at a path we allocate o(t) again for deep copy. therefore o(t^2)
     var dfs func(start, prod int, path []int)
     dfs = func(start, prod int, path []int) {
         // base
