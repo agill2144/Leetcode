f7c464f48eb31a5fd577fcc39011d292a08a7ef2
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-10 18:08:28 -0700

    Time: 29 ms (21.07%), Space: 6.9 MB (75.05%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.go
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -1,35 +1,29 @@
 func merge(intervals [][]int) [][]int {
+    if len(intervals) <= 1 {return intervals}
+
     sort.SliceStable(intervals, func(i, j int)bool{
         return intervals[i][0] < intervals[j][0]
     })
-    out := [][]int{intervals[0]}
+    
+    merged := [][]int{intervals[0]}
+    
     for i := 1; i < len(intervals); i++ {
-        start := intervals[i][0]
-        end := intervals[i][1]
-        
-        prev := out[len(out)-1]
-        prevStart := prev[0]
-        prevEnd := prev[1]
-        
-        if start > prevEnd {
-            out = append(out, []int{start, end})
+        if intervals[i][0] <= merged[len(merged)-1][1] {
+            merged[len(merged)-1][0] = min(intervals[i][0], merged[len(merged)-1][0])
+            merged[len(merged)-1][1] = max(intervals[i][1], merged[len(merged)-1][1])
         } else {
-            newEnd := max(end, prevEnd)
-            newStart := min(start, prevStart)
-            out = out[:len(out)-1]
-            out = append(out, []int{newStart, newEnd})
+            merged = append(merged, intervals[i])
         }
     }
-    return out
-    
-}
-
-func max(x, y int) int {
-    if x > y {return x}
-    return y
+    return merged
 }
 
 func min(x, y int) int {
     if x < y {return x}
+    return y
+}
+
+func max(x, y int) int {
+    if x > y {return x}
     return y
 }
