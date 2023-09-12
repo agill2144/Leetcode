4c8b72a5ee961b777ca3019d036457f304260d9a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-11 21:52:35 -0700

    Time: 64 ms (50.52%), Space: 7.7 MB (57.99%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.go
+++ 0542-01-matrix/0542-01-matrix.go
@@ -2,13 +2,12 @@ func updateMatrix(mat [][]int) [][]int {
     m := len(mat)
     n := len(mat[0])
     q := [][]int{}
-    visited := make([][]bool, m)
     for i := 0; i < m ;i++ {
-        visited[i] = make([]bool, n)
         for j := 0; j < n; j++ {
             if mat[i][j] == 0 {
                 q = append(q, []int{i,j})
-                visited[i][j] = true
+            } else {
+                mat[i][j] = -1
             }
         }
     }
@@ -22,9 +21,8 @@ func updateMatrix(mat [][]int) [][]int {
         for _, dir := range dirs {
             nr := cr+dir[0]
             nc := cc+dir[1]
-            if nr >= 0 && nr < m && nc >= 0 && nc < n && mat[nr][nc] == 1 && !visited[nr][nc] {
+            if nr >= 0 && nr < m && nc >= 0 && nc < n && mat[nr][nc] == -1 {
                 mat[nr][nc] = mat[cr][cc] + 1
-                visited[nr][nc] = true
                 q = append(q, []int{nr, nc})
             }  
         }
