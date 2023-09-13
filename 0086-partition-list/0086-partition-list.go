063d91bcb88140113b87471ef34e7e9344e0332a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-12 17:51:33 -0700

    Time: 3 ms (38.26%), Space: 2.4 MB (17.89%) - LeetHub

--- 0086-partition-list/0086-partition-list.go
+++ 0086-partition-list/0086-partition-list.go
@@ -6,29 +6,32 @@
  * }
  */
 func partition(head *ListNode, x int) *ListNode {
+    if head == nil || head.Next == nil {return head}
     left := &ListNode{Val: 0}
     leftTail := left
-    right := &ListNode{Val: 0}
+    
+    right := &ListNode{Val:0}
     rightTail := right
     
     curr := head
     for curr != nil {
         next := curr.Next
-        curr.Next = nil
-        if curr.Val >= x {
-            rightTail.Next = curr
-            rightTail = rightTail.Next
-        } else {
+        if curr.Val < x {
             leftTail.Next = curr
             leftTail = leftTail.Next
+        } else {
+            rightTail.Next = curr
+            rightTail = rightTail.Next                
         }
+        curr.Next = nil
         curr = next
     }
     left = left.Next
     right = right.Next
-    if left != nil {
-        leftTail.Next = right
-        return left
+    
+    if left == nil {
+        return right
     }
-    return right
+    leftTail.Next = right
+    return left
 }
