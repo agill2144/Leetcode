de0a3d30e3cac14e7496129490a9197a92a423e0
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-12 21:38:49 -0700

    Time: 85 ms (62.79%), Space: 7.6 MB (44.19%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.go
+++ 0282-expression-add-operators/0282-expression-add-operators.go
@@ -8,27 +8,23 @@ func addOperators(num string, target int) []string {
                 out = append(out, expr)
             }
             return
-        }
+        } 
         
         // logic
         for i := start; i < len(num); i++ {
             currNumStr := string(num[start:i+1])
-            if len(currNumStr) > 1 && currNumStr[0] == '0' {return}
-
             currNum, _ := strconv.Atoi(currNumStr)
-            if len(expr) == 0 {
-                dfs(i+1, currNum, currNum, currNumStr)    
+            if len(currNumStr) > 1 && currNumStr[0] == '0' {return}
+            
+            if expr == "" {
+                dfs(i+1, currNum, +currNum, expr+currNumStr)
             } else {
-                // +
-                dfs(i+1, calc+currNum, +currNum, expr + "+" + currNumStr)
-                // -
-                dfs(i+1, calc-currNum, -currNum, expr + "-" + currNumStr)
-                // *
-                dfs(i+1, (calc-contribution)+(currNum*contribution), currNum*contribution, expr + "*" + currNumStr)
+                dfs(i+1, calc+currNum, +currNum, expr+"+"+currNumStr)
+                dfs(i+1, calc-currNum, -currNum, expr+"-"+currNumStr)
+                dfs(i+1, calc-contribution + (contribution * currNum), contribution * currNum, expr+"*"+currNumStr)
             }
         }
     }
-    
-    dfs(0,0,0,"")
+    dfs(0, 0, 0, "")
     return out
 }
