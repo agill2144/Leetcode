ee324b70f7590fa68ce76b1c8378211e73e7d92f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-16 13:50:23 -0700

    Time: 0 ms (100.00%), Space: 2.9 MB (27.79%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.go
+++ 0040-combination-sum-ii/0040-combination-sum-ii.go
@@ -5,33 +5,38 @@ func combinationSum2(candidates []int, target int) [][]int {
     }
     deduped := [][]int{}
     for k, v := range freq {
-        deduped = append(deduped, []int{k, v})
+        deduped = append(deduped, []int{k,v})
     }
     
     out := [][]int{}
-    var dfs func(start, t int, path []int)
+    var dfs func(start int, t int, path []int)
     dfs = func(start, t int, path []int) {
         // base
         if t <= 0 {
             if t == 0 {
-                newL := make([]int, len(path))
-                copy(newL, path)
-                out = append(out, newL)
+                newP := make([]int, len(path))
+                copy(newP, path)
+                out = append(out, newP)
             }
             return
         }
         
-        
         // logic
         for i := start; i < len(deduped); i++ {
-            if deduped[i][1] == 0 {continue}
-            path = append(path, deduped[i][0])
+            num, count := deduped[i][0], deduped[i][1]
+            if count == 0 {continue}
+            // action
+            path = append(path, num)
             deduped[i][1]--
-            dfs(i, t-deduped[i][0], path)
+            
+            // recurse
+            dfs(i, t-num,path)
+
+            // backtrack
             path = path[:len(path)-1]
             deduped[i][1]++
         }
     }
-    dfs(0, target, []int{})
+    dfs(0, target, nil)
     return out
-} 
+}
