8c45be7214d17bac6126023d05bb586e3539eaa1
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-15 18:52:36 -0700

    Time: 43 ms (78.01%), Space: 7.7 MB (62.11%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -1,26 +1,152 @@
+/*
+    approach: sort + two ptrs
+    time = o(nlogn) + o(n^2)
+    space = depending on the sorting algo, o(n) for merge sort
+*/
 func threeSum(nums []int) [][]int {
     sort.Ints(nums)
     out := [][]int{}
     for i := 0; i < len(nums); i++ {
+        
+        /*
+            why this ?
+            - ith position in output array is fixed at idx 0 (look at when we save in output array)
+            - which means if an ith value at idx 0 formed a triplet, AND i loop moves forward AND
+            - the ith value is the same again ( as prev idx ), 
+            - does it make sense to use the same ith value at 0th idx again
+                - because same ith value as prev, will form another triplet
+                - but this new triplet was already saved from previous ith idx
+                - therefore we skip ith value if it matches prev idx
+            - All because ith value is fixed to idx 0
+        */
         if i != 0 && nums[i] == nums[i-1] {continue}
+        
         left := i+1
         right := len(nums)-1
+        // why isnt the <= ?
+        // We cant use 2 numbers from same idx position ?
+        // We cant use the same number 2 times from the same idx position
         for left < right {
             sum := nums[i] + nums[left] + nums[right]
             if sum == 0 {
                 out = append(out, []int{nums[i], nums[left], nums[right]})
                 left++
-                for left <= right && nums[left] == nums[left-1] {left++}
-                right--
-                for right >= left && nums[right]==nums[right+1] {right--}
-            } else if sum > 0 {
-                right--
-            } else {
+                for left < right && nums[left] == nums[left-1] {left++}
+                right-- 
+                for left < right && nums[right] == nums[right+1] {right--}
+            } else if sum < 0 {
                 left++
+            } else {
+                right--
             }
         }
     }
     return out
-
-
 }
+
+/*
+    approach: sort + binary search
+    - continue using a set to ensure unique triplets
+    time = o(n) * o(nlogn) = o(n^2logn)
+    space = o(k) unique triplets in set
+*/
+
+// func threeSum(nums []int) [][]int {
+//     sort.Ints(nums)
+//     out := [][]int{}
+//     set := map[[3]int]struct{}{}
+//     for i := 0; i < len(nums); i++ { // o(n)
+//         target := 0-nums[i]
+//         for j := i+1; j < len(nums); j++ { // o(n)
+//             k := binarySearch(nums, target-nums[j], j+1) // o(logn)
+//             if k != -1 {
+//                 tmp := []int{nums[i], nums[j], nums[k]}
+//                 if _, ok := set[[3]int{tmp[0], tmp[1], tmp[2]}]; !ok {
+//                     set[[3]int{tmp[0], tmp[1], tmp[2]}] = struct{}{}
+//                     out = append(out, tmp)
+//                 }
+//             }
+//         }
+//     }
+//     return out
+// }
+
+// func binarySearch(nums []int, target, left int) int {
+//     right := len(nums)-1
+//     for left <= right {
+//         mid := left + (right-left)/2
+//         if nums[mid] == target {
+//             return mid
+//         } else if target > nums[mid] {
+//             left = mid+1
+//         } else {
+//             right = mid-1
+//         }
+//     }
+//     return -1
+// }
+
+/*
+    approach: better brute force
+    - given ith value, how do we make this ith value == 0
+    - 0-ith value, this is our new target
+    - now we need to find a pair of 2 numbers that add up to target
+    - use a linear complement search using a hashmap ( two sum )
+    
+    time = o(n^2)
+    space = o(k) + o(n)
+    - k uniq triplets
+    - n for the nested idxMap for complement search
+*/
+// func threeSum(nums []int) [][]int {
+//     set := map[[3]int]struct{}{}
+//     out := [][]int{}
+//     for i := 0; i < len(nums); i++ {
+//         target := 0-nums[i]
+//         idxMap := map[int]int{}
+//         for j := i+1; j < len(nums); j++ {
+//             complement := target-nums[j]
+//             idx, ok := idxMap[complement]
+//             if ok {
+//                 tmp := []int{nums[i], nums[idx], nums[j]}
+//                 sort.Ints(tmp)
+//                 if _, ok := set[[3]int{tmp[0], tmp[1], tmp[2]}]; !ok {
+//                     set[[3]int{tmp[0], tmp[1], tmp[2]}] = struct{}{}
+//                     out = append(out, tmp)
+//                 }
+//             } else {
+//                 idxMap[nums[j]] = j
+//             }
+//         }
+//     }
+//     return out
+// }
+
+/*
+    approach: brute force
+    - check all triplets
+    - using nested iterations
+    - use set to keep track of unique triplets ( sort the triplets first )
+    
+    time = o(n^3)
+    space = o(k) k uniq triplets
+*/
+// func threeSum(nums []int) [][]int {
+//     set := map[[3]int]struct{}{}
+//     out := [][]int{}
+//     for i := 0; i < len(nums); i++ {
+//         for j := i+1; j < len(nums); j++ {
+//             for k := j+1; k < len(nums); k++ {
+//                 if nums[i]+nums[j]+nums[k] == 0 {
+//                     tmp := []int{nums[i],nums[j],nums[k]}
+//                     sort.Ints(tmp)
+//                     if _, ok := set[[3]int{tmp[0], tmp[1], tmp[2]}]; !ok {
+//                         set[[3]int{tmp[0], tmp[1], tmp[2]}] = struct{}{}
+//                         out = append(out, tmp)
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return out
+// }    
