1ef287cb63d13e09afdc9ee95e06f407e0d72b52
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-17 10:51:42 -0700

    Time: 2 ms (74.10%), Space: 2.7 MB (35.19%) - LeetHub

--- 0046-permutations/0046-permutations.go
+++ 0046-permutations/0046-permutations.go
@@ -1,3 +1,18 @@
+/*
+    approach: backtracking
+    - recursively place an element at each idx
+    - we have to place each element at a specific index
+    - place 1 at idx 0, then 2 at idx 0, then 3 at idx 0
+    - and then repeat for idx 1 and remaining indicies
+    - However we cannot place the same element twice right
+    - therefore before placing an element at an idx, check if the arr already contains the same element
+        - dont place it it element already exists, place only if it does not exist
+    - once we have a path/arr of same size as input array, we have an answer to save
+        - make a deepCopy of the path/array and save it
+
+    time = o(n*n!)
+    space = o(n^2) ( recursive stack and a deepcopy at the bottom most recursive call )
+*/
 func permute(nums []int) [][]int {
     out := [][]int{}
     var dfs func(path []int)
