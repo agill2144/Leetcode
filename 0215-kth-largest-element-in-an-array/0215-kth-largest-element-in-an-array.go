5624ba4251c47ebfd7f6cd575370c2cda550713b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-24 17:11:46 -0700

    Time: 2239 ms (6.72%), Space: 8.7 MB (40.45%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.go
@@ -1,34 +1,71 @@
 /*
+    approach: quick select
+    - sort the side we care about
+    - which side do we care about ? 
+        - this depends on on the kth element
+        - targetIdx = len(nums)-k
+    - using a pivot idx, and a window ( left, right )
+        - place pivot at the right ptr
+        - and push all elements from left to pivotIdx (excluding pivotIdx)
+        - to the left side
+        -
+*/
+func findKthLargest(nums []int, k int) int {
+    targetIdx := len(nums)-k
+    l := 0
+    r := len(nums)-1
+    for l <= r {
+        smallerThanPivot := l
+        pivotIdx := r
+        i := l
+        for i < pivotIdx {
+            if nums[i] < nums[pivotIdx] {
+                nums[i], nums[smallerThanPivot] = nums[smallerThanPivot], nums[i]
+                smallerThanPivot++
+            }
+            i++
+        }
+        nums[smallerThanPivot], nums[pivotIdx] = nums[pivotIdx], nums[smallerThanPivot]
+        if smallerThanPivot == targetIdx {return nums[smallerThanPivot]}
+        if targetIdx > smallerThanPivot {
+            l = smallerThanPivot+1
+        } else {
+            r = smallerThanPivot-1
+        }
+    }
+    return -1
+}
+/*
     approach: min heap
     - use min heap to keep track of k elements
     time = o(nlogk)
     space = o(k)
     
 */
-func findKthLargest(nums []int, k int) int {
-    mn := &minHeap{items: []int{}}
-    for i := 0; i < len(nums); i++ {
-        heap.Push(mn, nums[i])
-        if len(mn.items) > k {
-            heap.Pop(mn)
-        }
-    }
-    return mn.items[0]
-}
+// func findKthLargest(nums []int, k int) int {
+//     mn := &minHeap{items: []int{}}
+//     for i := 0; i < len(nums); i++ {
+//         heap.Push(mn, nums[i])
+//         if len(mn.items) > k {
+//             heap.Pop(mn)
+//         }
+//     }
+//     return mn.items[0]
+// }
 
-type minHeap struct {
-    items []int
-}
+// type minHeap struct {
+//     items []int
+// }
 
-func (m *minHeap) Less(i, j int) bool {return m.items[i] < m.items[j]}
-func (m *minHeap) Swap(i, j int) {m.items[i],m.items[j] = m.items[j], m.items[i]}
-func (m *minHeap) Len() int {return len(m.items)}
-func (m *minHeap) Push(x interface{}) { m.items = append(m.items, x.(int))}
-func (m *minHeap) Pop() interface{} {
-    out := m.items[len(m.items)-1]
-    m.items = m.items[:len(m.items)-1]
-    return out
-}
+// func (m *minHeap) Less(i, j int) bool {return m.items[i] < m.items[j]}
+// func (m *minHeap) Swap(i, j int) {m.items[i],m.items[j] = m.items[j], m.items[i]}
+// func (m *minHeap) Len() int {return len(m.items)}
+// func (m *minHeap) Push(x interface{}) { m.items = append(m.items, x.(int))}
+// func (m *minHeap) Pop() interface{} {
+//     out := m.items[len(m.items)-1]
+//     m.items = m.items[:len(m.items)-1]
+//     return out
+// }
 
 /*
     brute force:
