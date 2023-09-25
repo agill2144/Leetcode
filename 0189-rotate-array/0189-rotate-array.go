32ae9a9dec234b9918aa726ea6e978fff4a7a28f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-24 18:46:37 -0700

    Time: 33 ms (48.10%), Space: 8 MB (38.10%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.go
+++ 0189-rotate-array/0189-rotate-array.go
@@ -1,20 +1,21 @@
 func rotate(nums []int, k int)  {
-    if k >= len(nums) { k = k % len(nums) }
+    // what if k > len(nums)
+    // - ensure k is relative to the size of the arr
+    if k > len(nums) {k = k % len(nums)}
+    // reverse entire array first 
+    // so that the last k are now at the front of the arr
+    reverse(0, len(nums)-1, nums)
+    // reverse first k ( idx 0 to k-1 )
+    reverse(0, k-1, nums)
+    // undo the first reverse from kth idx:end
+    reverse(k, len(nums)-1, nums)
     
-    // reverse whole array
-    reverse(0, len(nums)-1,nums)
-
-    // reverse first k elements
-    reverse(0, k-1,nums)
-    
-    // reverse k: elements
-    reverse(k, len(nums)-1,nums)
 }
 
 func reverse(left, right int, nums []int) {
-    for left <= right {
+    for left < right {
         nums[left], nums[right] = nums[right], nums[left]
         left++
         right--
     }
-}
+} 
