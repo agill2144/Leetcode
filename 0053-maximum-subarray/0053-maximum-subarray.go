75e496002abfe6fe46d1096692bc8437bdb5f248
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-25 20:51:47 -0700

    Time: 97 ms (53.03%), Space: 8.6 MB (46.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.go
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -1,25 +1,15 @@
 func maxSubArray(nums []int) int {
     runningSum := 0
-    runningStart := 0
     maxSum := math.MinInt64
-    start := -1
-    end := -1
     for i := 0; i < len(nums); i++ {
         runningSum += nums[i]
         if runningSum > maxSum {
             maxSum = runningSum
-            start = runningStart
-            end = i
         }
         if runningSum < 0 {
             runningSum = 0
-            runningStart = i+1
         }
     }
-    // throwing values away so go-complier does not complain of unused values
-    // we could print it too
-    _ = start
-    _ = end
     return maxSum
 }
 
