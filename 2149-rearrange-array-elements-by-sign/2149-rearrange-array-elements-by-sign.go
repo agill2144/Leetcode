8c890459a5b6c53d6c4cc914c9354f162e088ae7
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-26 23:02:34 -0700

    Time: 237 ms (26.67%), Space: 34.7 MB (6.67%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.go
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.go
@@ -1,16 +1,30 @@
+/*
+    approach: brute force
+    - collect all positives in a list
+    - collect all negatives in another list
+    - use 2 ptrs to create alternating seq output array
+    
+    time = o(n)
+    space = o(n)
+*/
 func rearrangeArray(nums []int) []int {
-    out := make([]int, len(nums))
-    pPtr := 0 // positive ptr
-    nPtr := 1 // negative ptr
-    for i := 0; i < len(nums); i++{
-        // collect all negatives
-        if nums[i] < 0 {
-            out[nPtr] = nums[i]
-            nPtr+=2
+    p := []int{}
+    n := []int{}
+    for i := 0; i < len(nums); i++ {
+        if nums[i] > 0 {
+            p = append(p, nums[i])
         } else {
-            out[pPtr] = nums[i]
-            pPtr += 2
+            n = append(n, nums[i])
         }
     }
+    out := []int{}
+    pPtr := 0
+    nPtr := 0
+    for pPtr < len(p) {
+        out = append(out, p[pPtr])
+        out = append(out, n[nPtr])
+        pPtr++
+        nPtr++
+    }
     return out
-}   
+}
