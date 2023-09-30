daf9b8a7ee736a2dc419f923572aeaf422fe421b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-30 14:03:48 -0700

    Time: 138 ms (13.70%), Space: 8.9 MB (32.65%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -1,4 +1,28 @@
 /*
+    when selling on ith day, we only care about
+    cheapest price seen so far from 0 to i-1
+    - therefore we can keep track of cheapest price
+    - in a rolling minPrice var
+    
+    time = o(n)
+    space = o(1)
+*/
+func maxProfit(prices []int) int {
+    if prices == nil {return 0}
+    minPrice := prices[0]
+    maxProfit := 0
+    for i := 1; i < len(prices); i++ {
+        profit := prices[i]-minPrice
+        if profit > maxProfit {
+            maxProfit = profit
+        }
+        if prices[i] < minPrice {
+            minPrice = prices[i]
+        }
+    }
+    return maxProfit
+}
+/*
     we must buy the stock before selling on ith day
     - so if we want to maximize profit on the ith day
     - it makes sense to buy it on the cheapest possible day prior to ith day
@@ -12,27 +36,27 @@
     time o(n)
     space o(n)
 */
-func maxProfit(prices []int) int {
-    minBuyPrices := make([]int, len(prices))
-    minBuyPrices[0] = prices[0]
-    for i := 1; i < len(prices); i++ {
-        prevPrice := prices[i-1]
-        prevMin := minBuyPrices[i-1]
-        if prevPrice < prevMin {
-            minBuyPrices[i] = prevPrice
-        } else {
-            minBuyPrices[i] = prevMin
-        }
-    }
-    maxProfit := 0
-    for i := 1; i < len(prices); i++ {
-        buyPrice := minBuyPrices[i]
-        sellPrice := prices[i]
-        profit := sellPrice-buyPrice
-        if profit > maxProfit  {
-            maxProfit = profit
-        }
-    }
-    return maxProfit
+// func maxProfit(prices []int) int {
+//     minBuyPrices := make([]int, len(prices))
+//     minBuyPrices[0] = prices[0]
+//     for i := 1; i < len(prices); i++ {
+//         prevPrice := prices[i-1]
+//         prevMin := minBuyPrices[i-1]
+//         if prevPrice < prevMin {
+//             minBuyPrices[i] = prevPrice
+//         } else {
+//             minBuyPrices[i] = prevMin
+//         }
+//     }
+//     maxProfit := 0
+//     for i := 1; i < len(prices); i++ {
+//         buyPrice := minBuyPrices[i]
+//         sellPrice := prices[i]
+//         profit := sellPrice-buyPrice
+//         if profit > maxProfit  {
+//             maxProfit = profit
+//         }
+//     }
+//     return maxProfit
     
-}
+// }
