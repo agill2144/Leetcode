b396f37d78a8c58d0adfa4a6006e09e3a4cf1365
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-30 13:10:05 -0700

    Time: 118 ms (9.60%), Space: 12.2 MB (5.58%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.go
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -1,39 +1,16 @@
-/*
-    approach: kadanes algorithm
-    - greedy like
-    - keep track of a sum and a maxSum
-    - for each num, add it to sum
-    - save this sum if better than prev maxSum
-    - if sum goes negative, its not worth keeping / continuing this subarray, reset to 0
-        - what happens if next number is positive? 
-            - would we rather keep (-sum + next-positive num) or just (next-positive num)?
-        - what happens if all numbers are negative?
-            - -sum + -nextNegativeNum is only going to make the sum farther away from 0.
-            - i.e smaller in sum, we want the maxSum.
-            - therefore if all numbers are negative, than we will end up saving single number thats max among all
-    - if the sum is positive, its worth continuing with this subarray
-        - if this this subarray was an ans , we have already saved it in maxSum
-        - We are hoping next numbers dont make the sum negative, 
-        - and hoping carrying the positive sum may lead to a larger sum
-    
-    time = o(n)
-    space = o(1)
-    
-*/
 func maxSubArray(nums []int) int {
     sum := 0
     maxSum := math.MinInt64
     for i := 0; i < len(nums); i++ {
         sum += nums[i]
-        maxSum = max(sum, maxSum)
+        if sum > maxSum {maxSum = sum}
+        
+        // is it worth keeping this rolling sum / subarray ?
+        // its not worth keeping if the sum is negative 
+        // its worth keeping if the sum in positive
         if sum < 0 {
             sum = 0
         }
     }
     return maxSum
-}
-
-func max(x, y int) int {
-    if x > y {return x}
-    return y
 }
