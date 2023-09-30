3990ca49c3cceb8f2ca5d1f0031e31ba1318da42
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-30 15:24:05 -0700

    Time: 5 ms (75.77%), Space: 3.1 MB (50.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
@@ -1,20 +1,48 @@
 func lengthOfLongestSubstring(s string) int {
-    left := 0
-    windowState := map[byte]struct{}{}
     windowSize := 0
+    left := 0
+    charIdx := map[byte]int{}
     for i := 0; i < len(s); i++ {
         char := s[i]
-        _, ok := windowState[char]
-        for ok {
-            leftChar := s[left]
-            delete(windowState, leftChar)
-            left++
-            _, ok = windowState[char]
+        lastSeenAtIdx, ok := charIdx[char]
+        if ok && left <= lastSeenAtIdx {
+            left = lastSeenAtIdx+1
         }
-        windowState[char] = struct{}{}
+        charIdx[char]=i
         if i-left+1 > windowSize {
             windowSize = i-left+1
         }
     }
     return windowSize
 }
+/*
+    naive sliding window
+    - try to insert a char in window 
+    - while we cannot insert this char ( because this is a dupe )
+        - start removing chars from the left side
+    - once we have successfully inserted a char
+    - check size of window and save if its better
+    
+    time = o(s)
+    space = o(26) = o(1)
+*/
+// func lengthOfLongestSubstring(s string) int {
+//     left := 0
+//     windowState := map[byte]struct{}{}
+//     windowSize := 0
+//     for i := 0; i < len(s); i++ {
+//         char := s[i]
+//         _, ok := windowState[char]
+//         for ok {
+//             leftChar := s[left]
+//             delete(windowState, leftChar)
+//             left++
+//             _, ok = windowState[char]
+//         }
+//         windowState[char] = struct{}{}
+//         if i-left+1 > windowSize {
+//             windowSize = i-left+1
+//         }
+//     }
+//     return windowSize
+// }
