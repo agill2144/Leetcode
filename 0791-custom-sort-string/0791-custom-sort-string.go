d7219143e843eee2deb19183b82d57269e9af593
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-30 15:13:44 -0700

    Time: 1 ms (79.63%), Space: 2 MB (96.30%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.go
+++ 0791-custom-sort-string/0791-custom-sort-string.go
@@ -1,48 +1,50 @@
 /*
-    s must be in the same form as order
-    - freqMap of s chars
-    - loop over chars in order string
-        - if this char exists in sFreqMap
-        - repeat this char that many times in the output string
-        - since we have already used this char from order string, we can delete this char from the sFreqMap
-    - Finally append whatever else is remaining from s ( compare with sFreqMap )
+    we want to sort s based on a custom "order"
+    therefore we should collect all chars from order string that also exist in "s"
+    because order matters, chars in $order must be placed first
     
-    time: o(len(s)) + o(len(order))
-    space: o(len(s))
+    appraoch: using a hashset
+    - store chars of s in a set
+    - loop thru the order string to collect chars that should be placed first
+    - if the order[i] does exist in s set, take it and add it to our output
+        - delete this char from set
+        - so that we know for sure its used
+    - then loop over s string one last time, to collect remaining chars that did not exist in $order
+    - there is a possibility that not all chars of $s exist in $order,
+    - therefore we must also preserve the order of chars in $s that do not exist in $order
+    
+    s = len(s)
+    t = len(order)
+    
+    time = o(s) + o(t) + o(s)
+    space = o(s)
 */
 func customSortString(order string, s string) string {
-    sFreqMap := map[byte]int{} // space: o(1), there are constant number of characters in this world.
     
-    // time: o(s)
+    // make the chars of $s searchable 
+    freqMap := map[byte]int{}
     for i := 0; i < len(s); i++ {
-        sFreqMap[s[i]]++
+        freqMap[s[i]]++
     }
     
     out := new(strings.Builder)
-    
-    
-    // time: o(order)
+    // collect the chars from order that also exist in $s
     for i := 0; i < len(order); i++ {
-        count, ok := sFreqMap[order[i]]
-        if ok {
-            // o(s) ; worst case, all chars == this char in order
-            // order = "b"
-            // s = "bbbbbbbbbbb"
-            for k := 0; k < count; k++ {
-                out.WriteByte(order[i])
-            }
+        char := order[i]
+        count := freqMap[char]
+        for count > 0 {
+            out.WriteByte(char)
+            count--
         }
-        delete(sFreqMap, order[i])
+        delete(freqMap, char)
     }
     
-    // time: o(s)
+    // collect the remaining left over chars from $s that did not exist in $order
     for i := 0; i < len(s); i++ {
-        if _, ok := sFreqMap[s[i]]; ok {
+        count := freqMap[s[i]]
+        if count > 0 {
             out.WriteByte(s[i])
         }
     }
-    
-    // total time : o(s)+o(order*maxRepeatsOfACharInS)
-    // total space: o(1)
     return out.String()
 }
