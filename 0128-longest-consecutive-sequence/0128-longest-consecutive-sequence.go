d4902ef0a8ba9920f0b5ab7ca7bb82a2a00a4802
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-09-30 14:45:58 -0700

    Time: 1831 ms (5.19%), Space: 9.3 MB (87.75%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.go
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.go
@@ -1,25 +1,27 @@
 func longestConsecutive(nums []int) int {
+    if nums == nil || len(nums) == 0 {return 0}
     set := map[int]struct{}{}
-    for i := 0; i < len(nums); i++ {
-        set[nums[i]] = struct{}{}
+    for i := 0 ; i < len(nums); i++ {
+        set[nums[i]]=struct{}{}
     }
-    out := 0
+    maxConsec := 0
     for i := 0; i < len(nums); i++ {
         num := nums[i]
-        prev := num-1
-        if _, ok := set[prev]; ok {
-            continue
-        }
-        size := 1
+        // only proceed with this num if this is a starting of a seq
+        // i.e no number before this exists in nums array
+        // however if the prev number of this number exists, 
+        // skip because we would want to start from that prev num
+        _, ok := set[num-1]
+        if ok {continue}
+        
+        count := 1
         for true {
-            if _, ok := set[num+1]; ok {
-                size++
-                num++
-            } else {
-                break
-            }
+            _, found := set[num+1]
+            if !found {break}
+            count++
+            num++
         }
-        if size > out {out = size}
+        if count > maxConsec {maxConsec = count}
     }
-    return out
+    return maxConsec
 }
