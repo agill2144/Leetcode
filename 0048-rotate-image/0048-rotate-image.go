ad71794b8f83bef9cd5b48fb592f789bf4e4a1a0
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-02 23:12:34 -0700

    Time: 1 ms (82.45%), Space: 2.3 MB (52.19%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.go
+++ 0048-rotate-image/0048-rotate-image.go
@@ -1,19 +1,35 @@
 func rotate(matrix [][]int)  {
     n := len(matrix)
-    tmp := make([][]int, n)
+    // transpose
     for i := 0; i < n; i++ {
-        tmp[i] = make([]int, n)
-    }
-    
-    for i := 0; i < n; i++ {
-        for j := 0; j < n; j++ {
-            tmp[j][n-1-i] = matrix[i][j]
+        for j := i+1; j < n; j++ {
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         }
     }
-    
     for i := 0; i < n; i++ {
-        for j := 0; j < n; j++ {
-            matrix[i][j] = tmp[i][j]
-        }
-    } 
+        for j := 0; j < n/2; j++ {
+            matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+        }        
+    }
+
 }
+
+// func rotate(matrix [][]int)  {
+//     n := len(matrix)
+//     tmp := make([][]int, n)
+//     for i := 0; i < n; i++ {
+//         tmp[i] = make([]int, n)
+//     }
+    
+//     for i := 0; i < n; i++ {
+//         for j := 0; j < n; j++ {
+//             tmp[j][n-1-i] = matrix[i][j]
+//         }
+//     }
+    
+//     for i := 0; i < n; i++ {
+//         for j := 0; j < n; j++ {
+//             matrix[i][j] = tmp[i][j]
+//         }
+//     } 
+// }
