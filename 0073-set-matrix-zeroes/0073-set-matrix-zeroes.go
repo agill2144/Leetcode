2ee2094a10f401c97052d72cfd697365e46f012a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-03 18:28:46 -0700

    Time: 15 ms (38.85%), Space: 6.6 MB (22.93%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.go
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.go
@@ -2,29 +2,21 @@
 func setZeroes(matrix [][]int)  {
     m := len(matrix)
     n := len(matrix[0])
-    
-    dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
+    rows := make([]bool, m)
+    cols := make([]bool, n)
+
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
             if matrix[i][j] == 0 {
-                
-                for _, dir := range dirs {
-                    r := i+dir[0]
-                    c := j+dir[1]
-                    for r >= 0 && r < m && c >= 0 && c < n && matrix[r][c] != 0 {
-                        matrix[r][c] = math.MaxInt64
-                        r += dir[0]
-                        c += dir[1]
-                    }
-                }
-                
+                rows[i] = true
+                cols[j] = true
             }
         }
     }
     
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            if matrix[i][j] == math.MaxInt64 {
+            if rows[i] || cols[j] {
                 matrix[i][j] = 0
             }
         }
