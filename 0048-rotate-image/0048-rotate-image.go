8f02e5e17e9b44893f95c350f0bbf5dfab9bc1de
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-03 19:10:25 -0700

    Time: 0 ms (100.00%), Space: 2.3 MB (51.76%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.go
+++ 0048-rotate-image/0048-rotate-image.go
@@ -1,35 +1,18 @@
+/*
+    approach: transpose and reverse each row
+    
+*/
 func rotate(matrix [][]int)  {
     n := len(matrix)
-    // transpose
     for i := 0; i < n; i++ {
         for j := i+1; j < n; j++ {
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         }
     }
+    
     for i := 0; i < n; i++ {
         for j := 0; j < n/2; j++ {
-            matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
-        }        
+            matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+        }
     }
-
 }
-
-// func rotate(matrix [][]int)  {
-//     n := len(matrix)
-//     tmp := make([][]int, n)
-//     for i := 0; i < n; i++ {
-//         tmp[i] = make([]int, n)
-//     }
-    
-//     for i := 0; i < n; i++ {
-//         for j := 0; j < n; j++ {
-//             tmp[j][n-1-i] = matrix[i][j]
-//         }
-//     }
-    
-//     for i := 0; i < n; i++ {
-//         for j := 0; j < n; j++ {
-//             matrix[i][j] = tmp[i][j]
-//         }
-//     } 
-// }
