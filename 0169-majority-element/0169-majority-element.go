371fcde0f0cc706d5d3a37eef079b776a4b37e97
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-04 19:18:10 -0700

    Time: 14 ms (90.84%), Space: 6.3 MB (39.10%) - LeetHub

--- 0169-majority-element/0169-majority-element.go
+++ 0169-majority-element/0169-majority-element.go
@@ -11,5 +11,10 @@ func majorityElement(nums []int) int {
             count--
         }
     }
-    return ele
+    count = 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] == ele {count++}
+    }
+    if count > len(nums)/2 {return ele}
+    return -1
 }
