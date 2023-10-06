54a4826a6aaf770153ba49b4d361dda410b67e34
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-05 21:50:36 -0700

    Time: 1 ms (77.99%), Space: 2.3 MB (23.29%) - LeetHub

--- 0078-subsets/0078-subsets.go
+++ 0078-subsets/0078-subsets.go
@@ -1,11 +1,22 @@
+/*
+    approach: for loop based recursion / backtracking
+    - like combination sum
+    - form all possible combinations
+    - at each recursive call, we have an ans to save
+    
+    time ; n* 2^n
+    n * because at each recursive call, we create another array , deepCp and save the ans
+    space ;
+    o(n) recursion stack * o(n) for new deepCp array ; n^2 
+*/
 func subsets(nums []int) [][]int {
     out := [][]int{{}}
     for i := 0; i < len(nums); i++ {
-        for _, currList := range out {
-            newL := make([]int, len(currList))
-            copy(newL, currList)
-            newL = append(newL, nums[i])
-            out = append(out, newL)
+        for _, curr := range out {
+            newP := make([]int, len(curr))
+            copy(newP, curr)
+            newP = append(newP, nums[i])
+            out = append(out, newP)
         }
     }
     return out
