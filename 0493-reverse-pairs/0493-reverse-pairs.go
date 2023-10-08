388b5f39d4f128ffef0ca6330c728a1eb48f0f2f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-08 12:11:55 -0700

    Time: 226 ms (10.53%), Space: 8.6 MB (47.37%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.go
@@ -0,0 +1,52 @@
+func reversePairs(nums []int) int {
+    count := 0
+    var dfs func(left, right int)
+    dfs = func(left, right int) {
+        // base
+        if left == right {return}
+        
+        // logic
+        mid := left + (right-left)/2
+        dfs(left, mid)
+        dfs(mid+1, right)
+        
+        
+        j := mid+1
+        for i := left; i <= mid; i++ {
+            for j <= right {
+                if nums[i] > nums[j]*2 {
+                    j++
+                } else {
+                    break
+                }
+            }
+            count += j-(mid+1)
+        }
+        
+        l := left
+        r := mid+1
+        aux := []int{}
+        for l <= mid && r <= right {
+            if nums[l] < nums[r] {
+                aux = append(aux, nums[l])
+                l++
+            } else {
+                aux = append(aux, nums[r])
+                r++
+            }
+        }
+        
+        for l <= mid {aux = append(aux, nums[l]);l++}
+        for r <= right {aux = append(aux, nums[r]);r++}
+        
+        
+        auxPtr := 0
+        for i := left; i <= right; i++ {
+            nums[i] = aux[auxPtr]
+            auxPtr++
+        }
+        
+    }
+    dfs(0, len(nums)-1)
+    return count
+}
