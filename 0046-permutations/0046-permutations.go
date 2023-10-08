5a20c092461d53d50e8a8428f1981a3bb5ed0bdf
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-07 18:12:09 -0700

    Time: 2 ms (76.55%), Space: 2.6 MB (89.76%) - LeetHub

--- 0046-permutations/0046-permutations.go
+++ 0046-permutations/0046-permutations.go
@@ -1,31 +1,22 @@
 func permute(nums []int) [][]int {
     out := [][]int{}
-    var dfs func(path []int)
-    dfs = func(path []int) {
+    var dfs func(start int)
+    dfs = func(start int) {
         // base
-        if len(path) == len(nums) {
-            newP := make([]int, len(path))
-            copy(newP, path)
+        if start == len(nums) {
+            newP := make([]int, len(nums))
+            copy(newP, nums)
             out = append(out, newP)
             return
         }
         
         // logic
-        for i := 0; i < len(nums); i++ {
-            if !contains(path, nums[i]) {
-                path = append(path, nums[i])
-                dfs(path)
-                path = path[:len(path)-1]
-            }
+        for i := start; i < len(nums); i++ {
+            nums[i], nums[start] = nums[start], nums[i]
+            dfs(start+1)
+            nums[i], nums[start] = nums[start], nums[i]
         }
     }
-    dfs(nil)
+    dfs(0)
     return out
-}
-
-func contains(arr []int, target int) bool {
-    for _, ele := range arr {
-        if ele == target {return true}
-    }
-    return false
 }
