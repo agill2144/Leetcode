68cd4ea1f9e4b6fc3d6be9ae0915e8ee1236f065
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-08 16:43:31 -0700

    Time: 2 ms (62.29%), Space: 2.3 MB (7.02%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.go
+++ 0048-rotate-image/0048-rotate-image.go
@@ -1,43 +1,27 @@
 /*
-    approach:
-    - transpose and reverse each row
-    - transposing a matrix is when a row becomes a col
-        - row0 becomes col0
-        - row1 becomes col1
-        - row2 becomes col2
-
-    - VERY IMPORTANT: even after transposing a matrix
-        the diagonal from top left to bottom right ( 0,0 -> m-1,n-1 )
-        stays the same original matrix
+    approach: brute force
+    - create a new matrix
+    - map input i,j -> new matrix j,(m-i-1)
+    - copy new matrix values to input matrix
     
-    - How to transpose a matrix?
-    - swap values from 1 side of diagonal line(that does not change) with other side of diagonal line
-    [1,2,3]        [1,4,7]
-    [4,5,6]   ->   [2,5,8]
-    [7,8,9]        [3,6,9]    
-
-    The diagonal 1-5-9 stays the same ( even in the transposed matrix )
-    swap from one side of diagonal with other
-    
-    - matrix[0][1] swapped with matrix[1][0]
-    - matrix[0][2] swapped with matrix[2][0]
-    - matrix[1][2] swapped with matrix[2][1]    
-    - swap(matrix[i][j], matrix[j][i])
-    
-    time = o(n^2) + o(n^2)
-    space = o(1)
+    time = o(mn) + o(mn)
+    space = o(mn)
 */
 func rotate(matrix [][]int)  {
     n := len(matrix)
+    tmp := make([][]int, n)
     for i := 0; i < n; i++ {
-        for j := i+1; j < n; j++ {
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        }
+        tmp[i] = make([]int, n)
     }
     
     for i := 0; i < n; i++ {
-        for j := 0; j < n/2; j++ {
-            matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+        for j := 0; j < n; j++ {
+            tmp[j][n-1-i] = matrix[i][j]
         }
     }
+     for i := 0; i < n; i++ {
+        for j := 0; j < n; j++ {
+            matrix[i][j] = tmp[i][j]
+        }
+     }
 }
