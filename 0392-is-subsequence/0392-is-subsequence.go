e77344090cc041cf13a8b2b84efa53ffb43183c9
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-07 18:51:26 -0700

    Time: 1 ms (83.72%), Space: 2 MB (96.06%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.go
+++ 0392-is-subsequence/0392-is-subsequence.go
@@ -1,15 +1,14 @@
-// two pointers
-// once we visited all characters of s from left to right, we have a valid subsequence s in t
-// time: o(t) if t is the bigger string
-// space: o(1)
 func isSubsequence(s string, t string) bool {
-    sPtr := 0
-    tPtr := 0
+    
+    sPtr, tPtr := 0,0
+    
     for sPtr < len(s) && tPtr < len(t) {
         if s[sPtr] == t[tPtr] {
             sPtr++
+            tPtr++
+        } else {
+            tPtr++
         }
-        tPtr++
     }
     return sPtr == len(s)
 }
