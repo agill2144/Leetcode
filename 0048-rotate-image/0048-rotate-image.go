2567323d031b11c33b5c441fadc724001486148a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-08 17:06:12 -0700

    Time: 2 ms (62.29%), Space: 2.2 MB (51.30%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.go
+++ 0048-rotate-image/0048-rotate-image.go
@@ -1,27 +1,15 @@
-/*
-    approach: brute force
-    - create a new matrix
-    - map input i,j -> new matrix j,(m-i-1)
-    - copy new matrix values to input matrix
-    
-    time = o(mn) + o(mn)
-    space = o(mn)
-*/
+
 func rotate(matrix [][]int)  {
     n := len(matrix)
-    tmp := make([][]int, n)
     for i := 0; i < n; i++ {
-        tmp[i] = make([]int, n)
+        for j := i+1; j < n; j++ {
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        }
     }
     
     for i := 0; i < n; i++ {
-        for j := 0; j < n; j++ {
-            tmp[j][n-1-i] = matrix[i][j]
+        for j := 0; j < n/2; j++ {
+            matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
         }
     }
-     for i := 0; i < n; i++ {
-        for j := 0; j < n; j++ {
-            matrix[i][j] = tmp[i][j]
-        }
-     }
 }
