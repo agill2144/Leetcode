6ef0cd72b87e24a912e711d0848af65bcdde7199
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-08 17:30:19 -0700

    Time: 29 ms (18.91%), Space: 7 MB (58.48%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.go
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -1,19 +1,20 @@
 func merge(intervals [][]int) [][]int {
-    sort.Slice(intervals, func(i, j int)bool{
+    sort.Slice(intervals, func(i, j int)bool {
         return intervals[i][0] < intervals[j][0]
     })
-    st := [][]int{}
-    for i := 0; i < len(intervals); i++ {
-        if len(st) != 0 && intervals[i][0] <= st[len(st)-1][1] {
-            st[len(st)-1][0] = min(intervals[i][0], st[len(st)-1][0])
-            st[len(st)-1][1] = max(intervals[i][1], st[len(st)-1][1])
+    out := [][]int{intervals[0]}
+    for i := 1; i < len(intervals); i++ {
+        prev := out[len(out)-1]
+        curr := intervals[i]
+        if curr[0] <= prev[1] {
+            out[len(out)-1][0] = min(prev[0], curr[0])
+            out[len(out)-1][1] = max(prev[1], curr[1])
         } else {
-            st = append(st, intervals[i])
+            out = append(out, curr)
         }
     }
-    return st
+    return out
 }
-
 
 func min(x, y int) int {
     if x < y {return x}
