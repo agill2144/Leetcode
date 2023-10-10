b6a48ee0dbbec87cbe3bc144e266f93d716829f8
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-09 17:31:55 -0700

    Time: 15 ms (51.40%), Space: 5 MB (51.40%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.go
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.go
@@ -1,29 +1,33 @@
 func findAnagrams(s string, p string) []int {
-    if len(p) > len(s) {return nil}
-    
     pMap := map[byte]int{}
-    for i := 0; i < len(p); i++ {pMap[p[i]]++}
-    count := len(pMap)
-    out := []int{}
-    
+    for i := 0; i < len(p); i++ {
+        pMap[p[i]]++
+    }
     left := 0
-    for right := 0; right < len(s); right++ {
-        char := s[right]
-        val, ok := pMap[char]
-        if ok {
+    out := []int{}
+    count := len(pMap)
+    for i := 0; i < len(s); i++{
+        char := s[i]
+        if _, ok := pMap[char]; ok {
             pMap[char]--
-            if val == 1 {count--} 
+            // we have a full match for this char ?
+            // decrement its counter
+            if pMap[char] == 0 {count--}
         }
-        if right-left+1 == len(p) {
-            if count == 0 {out = append(out, left)}
+        
+        if i-left+1 == len(p) {
+            // count will be 0 when all chars were fully matched 
+            // with current window
+            if count == 0 {
+                out = append(out, left)
+            }
             leftChar := s[left]
             if _, ok := pMap[leftChar]; ok {
                 pMap[leftChar]++
-                if val := pMap[leftChar]; val == 1 {count++}
+                if pMap[leftChar] == 1 {count++}
             }
             left++
         }
     }
     return out
-    
 }
