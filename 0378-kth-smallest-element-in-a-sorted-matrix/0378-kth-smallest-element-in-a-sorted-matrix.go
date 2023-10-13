045b8ac220d36e11e63da29b98e2f126f3422d81
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-13 13:12:58 -0700

    Time: 21 ms (92.39%), Space: 7.4 MB (41.30%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.go
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.go
@@ -3,32 +3,28 @@ func kthSmallest(matrix [][]int, k int) int {
     n := len(matrix[0])
     left := matrix[0][0]
     right := matrix[m-1][n-1]
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
-        count, ok := countLessThanOrEqualTo(matrix, mid)
-        if count == k && ok {
-            return mid
-        }
-        if count < k {
+        if countLessThanOrEqualTo(matrix, mid) < k {
             left = mid+1
         } else {
+            ans = mid
             right = mid-1
         }
     }
-    return left
+    return ans
 }
 
 // use stair case search to find count <= target
-func countLessThanOrEqualTo(matrix [][]int,target int) (int, bool) {
+func countLessThanOrEqualTo(matrix [][]int,target int) int {
     m := len(matrix)
     n := len(matrix[0])
     count := 0
     r := m-1
     c := 0
-    found := false
     for r >= 0 && c < n {
         if matrix[r][c] <= target {
-            if matrix[r][c] == target {found = true}            
             // all elements above this cell are less than or equal to target
             // therefore add all elements in this col
             count += r+1
@@ -38,5 +34,5 @@ func countLessThanOrEqualTo(matrix [][]int,target int) (int, bool) {
             r--
         }
     }
-    return count, found
+    return count
 }
