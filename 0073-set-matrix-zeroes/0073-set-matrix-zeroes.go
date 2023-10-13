8ab6d886b62f81f76d508ba5c86e11dc57a3ff40
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-13 14:03:12 -0700

    Time: 11 ms (75.32%), Space: 6.6 MB (50.93%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.go
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.go
@@ -1,21 +1,26 @@
 func setZeroes(matrix [][]int)  {
     m := len(matrix)
     n := len(matrix[0])
-    rows := make([]bool, m)
-    cols := make([]bool, n)
+    rowZero := false
+    colZero := false
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
             if matrix[i][j] == 0 {
-                rows[i] = true
-                cols[j] = true
+                if i == 0 {rowZero = true}
+                if j == 0 {colZero = true}
+                if i != 0 {matrix[i][0] = 0}
+                if j != 0 {matrix[0][j] = 0}
             }
         }
     }
-    for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
-            if rows[i] || cols[j] {
+    for i := 1; i < m; i++ {
+        for j := 1; j < n; j++ {
+            if matrix[i][0] == 0 || matrix[0][j] == 0 {
                 matrix[i][j] = 0
             }
         }
     }
+    if rowZero {for j:= 0; j < n; j++ {matrix[0][j]=0}}
+    if colZero {for i := 0; i < m; i++ {matrix[i][0]=0}}
+
 }
