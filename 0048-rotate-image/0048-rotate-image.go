2f307f82f312ed7a2e7b8c6d2099259e2dc96369
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-12 22:14:30 -0700

    Time: 0 ms (100.00%), Space: 2.2 MB (51.46%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.go
+++ 0048-rotate-image/0048-rotate-image.go
@@ -1,17 +1,13 @@
 func rotate(matrix [][]int)  {
     n := len(matrix)
-    cp := make([][]int, n)
-    for i := 0; i < n; i++ {cp[i] = make([]int, n)}
-
-    for i := 0;  i < n; i++ {
-        for j := 0; j < n; j++ {
-            cp[j][n-i-1] = matrix[i][j]
+    for i := 0; i < n; i++ {
+        for j := i+1; j < n; j++ {
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         }
     }
-
     for i := 0; i < n; i++ {
-        for j := 0; j < n; j++ {
-            matrix[i][j] = cp[i][j]
-        }
+        for j := 0; j < n/2; j++ {
+            matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+        }        
     }
 }
