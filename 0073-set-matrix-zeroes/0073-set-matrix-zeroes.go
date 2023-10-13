8dbed8ed969b3ad67429289f3504a3cf01a34f2d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-13 13:58:19 -0700

    Time: 14 ms (53.95%), Space: 6.5 MB (71.59%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.go
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.go
@@ -1,23 +1,27 @@
 func setZeroes(matrix [][]int)  {
+    tmpZero := math.MaxInt64
     m := len(matrix)
     n := len(matrix[0])
-    // collect rows and cols that should be replaced with 0's
-    rows := make([]bool, m)
-    cols := make([]bool, n)
-
+    
+    dirs := [][]int{{1,0},{-1,0},{0,-1},{0,1}}
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
             if matrix[i][j] == 0 {
-                rows[i] = true
-                cols[j] = true
+                for _, dir := range dirs {
+                    r := i + dir[0]
+                    c := j + dir[1]
+                    for r >= 0 && r < m && c >= 0 && c < n && matrix[r][c] != 0 {
+                        matrix[r][c] = tmpZero
+                        r += dir[0]
+                        c += dir[1]
+                    }
+                }
             }
         }
     }
-    
-    // replace cell val is cell is in either row or col that should be replaced with 0's
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            if rows[i] || cols[j] {
+            if matrix[i][j] == tmpZero {
                 matrix[i][j] = 0
             }
         }
