a7042b0ae0d7860ec30a425e38566ebcf0ad798d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-14 14:28:43 -0700

    Time: 5 ms (88.46%), Space: 4 MB (90.17%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.go
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.go
@@ -1,31 +1,30 @@
 func calculate(s string) int {
-    st := []int{}
+    contr := 0
+    calc := 0
     curr := 0
     var lastOp byte = '+'
     for i := 0; i < len(s); i++ {
-        char := s[i]
-        
+        char := s[i]        
         if char >= '0' && char <= '9' {
             curr = curr * 10 + int(char-'0')
         }
         if char == '+' || char == '-' || char == '*' || char == '/' || i == len(s)-1 {
             if lastOp == '+' {
-                st = append(st, curr)
+                calc += curr
+                contr = curr
             } else if lastOp == '-' {
-                st = append(st, -curr)
+                calc -= curr
+                contr = -curr
             } else if lastOp == '*' {
-                st[len(st)-1] = st[len(st)-1]*curr
+                calc = calc-contr + contr * curr
+                contr = contr * curr
             } else if lastOp == '/' {
-                st[len(st)-1] = st[len(st)-1]/curr
+                calc = calc-contr + contr / curr
+                contr = contr / curr
             }
             curr = 0
             lastOp = char
         }
     }
-    total := 0
-    for len(st) != 0 {
-        total += st[len(st)-1]
-        st = st[:len(st)-1]
-    }
-    return total
+    return calc
 }
