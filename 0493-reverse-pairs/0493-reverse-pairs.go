0f5fbc0afa3d2b5b8352cddbad494234d1938385
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-13 20:23:49 -0700

    Time: 146 ms (37.50%), Space: 8.5 MB (70.83%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.go
+++ 0493-reverse-pairs/0493-reverse-pairs.go
@@ -3,49 +3,48 @@ func reversePairs(nums []int) int {
     var dfs func(left, right int)
     dfs = func(left, right int) {
         // base
-        if left == right {return}
+        if left >= right {return}
         
         // logic
         mid := left + (right-left)/2
         dfs(left, mid)
         dfs(mid+1, right)
         
-        
+        i := left
         j := mid+1
-        for i := left; i <= mid; i++ {
+        for i <= mid {
             for j <= right {
-                if nums[i] <= nums[j]*2 {
+                if nums[i] > nums[j]*2 {
+                    count += mid-i+1
+                    j++
+                } else {
                     break
                 }
-                j++
             }
-            count += j-(mid+1)
+            i++
         }
         
+        merged := []int{}
         l := left
         r := mid+1
-        aux := []int{}
         for l <= mid && r <= right {
-            if nums[l] < nums[r] {
-                aux = append(aux, nums[l])
+            if nums[l] <= nums[r] {
+                merged = append(merged, nums[l])
                 l++
-            } else {
-                aux = append(aux, nums[r])
+            }  else {
+                merged = append(merged, nums[r])
                 r++
             }
         }
-        
-        for l <= mid {aux = append(aux, nums[l]);l++}
-        for r <= right {aux = append(aux, nums[r]);r++}
-        
-        
-        auxPtr := 0
+        for l <= mid {merged = append(merged, nums[l]); l++}
+        for r <= right {merged = append(merged, nums[r]); r++}
+        ptr := 0
         for i := left; i <= right; i++ {
-            nums[i] = aux[auxPtr]
-            auxPtr++
+            nums[i] = merged[ptr]
+            ptr++
         }
-        
     }
     dfs(0, len(nums)-1)
     return count
+    
 }
