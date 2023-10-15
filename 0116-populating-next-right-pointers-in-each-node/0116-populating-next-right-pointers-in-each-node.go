d63c27b28caf295dc0f259708fd090832505f176
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-14 20:09:09 -0700

    Time: 6 ms (48.00%), Space: 7.1 MB (13.60%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.go
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.go
@@ -7,87 +7,29 @@
  *     Next *Node
  * }
  */
-/*
-    approach: bfs inspired level by level without space
-    - consider level by level
-    - from each node, connect left->right and go to next node and repeat until current level is out of nodes
-    - so treat each level like a Linkedlist and keep moving right
-    - do not go to next level until current level is done ( across subtrees )
-    time: o(n)
-    space: o(1)
-    
-*/
+
 func connect(root *Node) *Node {
-    r := root
-    for root != nil {
-        cl := root
-        nl := root.Left
-        for cl != nil {
-            if cl.Left != nil && cl.Right != nil {
-                cl.Left.Next = cl.Right
-            }
-            if cl.Next != nil && cl.Right != nil {
-                cl.Right.Next = cl.Next.Left
-            }
-            cl = cl.Next
-        }
-        root = nl
+    if root == nil {
+        return root
     }
-    return r
+    
+    q := []*Node{root}
+    for len(q) != 0 {
+        qSize := len(q)
+        for qSize != 0 {
+            dq := q[0]
+            q = q[1:]
+            if qSize > 1 {
+                dq.Next = q[0]
+            }
+            if dq.Left != nil {
+                q = append(q, dq.Left)
+            }
+            if dq.Right != nil {
+                q = append(q, dq.Right)
+            }
+            qSize--
+        }
+    }
+    return root
 }
-
-/*
-    approach: dfs
-    - at a root node, connect left->right
-    - if at a node we have a next ptr, then make the cross tree connection
-    time: o(n)
-    space: o(h)
-*/
-// func connect(root *Node) *Node {
-//     var dfs func(r *Node)
-//     dfs = func(r *Node) {
-//         // base
-//         if r == nil {return}
-//         if r.Left == nil && r.Right == nil {return}
-        
-//         // logic
-//         r.Left.Next = r.Right
-//         if r.Next != nil {
-//             r.Right.Next = r.Next.Left
-//         }
-//         dfs(r.Left)
-//         dfs(r.Right)
-//     }
-//     dfs(root)
-//     return root
-// }
-
-
-/*
-    approach: bfs
-    - go level by level
-    - for each dq'd node, if the top of queue is not the last node in that level, make a next ptr connection to that top node
-    time: o(n)
-    space: o(maxWidth) or o(n) in a skewed tree
-*/
-// func connect(root *Node) *Node {
-//     if root == nil {return root}
-//     q := []*Node{root}
-//     for len(q) != 0 {
-//         qSize := len(q)
-//         for i := 0; i < qSize; i++ {
-//             dq := q[0]
-//             q = q[1:]
-//             if i != qSize-1 {
-//                 dq.Next = q[0]
-//             }
-//             if dq.Left != nil {
-//                 q = append(q, dq.Left)
-//             }
-//             if dq.Right != nil {
-//                 q = append(q, dq.Right)
-//             }
-//         }
-//     }
-//     return root
-// }
