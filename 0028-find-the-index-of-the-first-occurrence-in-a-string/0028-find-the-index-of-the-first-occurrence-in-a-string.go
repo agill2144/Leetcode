7b7ca58dc88a80906450f0fe6a3e760316bd2cad
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-14 22:35:32 -0700

    Time: 1 ms (78.61%), Space: 2 MB (50.80%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
@@ -1,36 +1,43 @@
 // The most magical fucking algo : KMP
 func strStr(haystack string, needle string) int {
-    lps := lps(needle)
-    i := 0 // haystack ptr
-    j := 0 // needle ptr
+    lps := findLPS(needle)
+    i := 0
+    j := 0
     for i < len(haystack) {
         if haystack[i] == needle[j] {
             j++
             i++
             if j == len(needle) {
-                return i - len(needle)
+                return i-len(needle)
             }
         } else if haystack[i] != needle[j] && j > 0 {
+            /*
+                When you encounter a mismatch at haystack[i] and needle[j],
+                You adjust j based on the LPS (Longest Prefix Suffix) array. 
+                The LPS array helps you determine how much of the pattern
+                can be reused when a mismatch occurs.
+            */
             j = lps[j-1]
-        } else if haystack[i] != needle[j] && j == 0{
-            i++ // j is already at 0th idx, cant move that back anymore
+        } else if haystack[i] != needle[j] && j == 0 {
+            // j is already at 0th idx, cant move that back anymore
+            i++
         }
     }
     return -1
 }
 
-func lps(needle string) []int {
+func findLPS(needle string) []int {
     out := make([]int, len(needle))
-    i := 1 // suffix window right ptr
-    j := 0 // prefix window right ptr
+    j := 0
+    i := 1
     for i < len(needle) {
         if needle[i] == needle[j] {
             j++
             out[i] = j
             i++
-        } else if needle[i] != needle[j] && j > 0 {
+        } else if needle[j] != needle[i] && j > 0 {
             j = out[j-1]
-        } else if needle[i] != needle[j] && j == 0 {
+        } else if needle[j] != needle[i] && j == 0 {
             i++
         }
     }
