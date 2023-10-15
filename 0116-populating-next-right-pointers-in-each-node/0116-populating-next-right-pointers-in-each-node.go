80b96bce8211f09d0c4baa05876bdaa2d0607594
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-14 20:19:31 -0700

    Time: 6 ms (48.00%), Space: 6.5 MB (82.40%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.go
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.go
@@ -9,27 +9,65 @@
  */
 
 func connect(root *Node) *Node {
-    if root == nil {
-        return root
-    }
-    
-    q := []*Node{root}
-    for len(q) != 0 {
-        qSize := len(q)
-        for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            if qSize > 1 {
-                dq.Next = q[0]
+    if root == nil {return nil}
+    l := root
+    for l != nil {
+        i := l
+        for i != nil {
+            if i.Left != nil && i.Right != nil {
+                i.Left.Next = i.Right
             }
-            if dq.Left != nil {
-                q = append(q, dq.Left)
+            if i.Next != nil {
+                if i.Next.Left != nil {
+                    if i.Right != nil {
+                        i.Right.Next = i.Next.Left
+                    } else if i.Left != nil {
+                        i.Left.Next = i.Next.Left
+                    }
+                } else if i.Next.Right != nil {
+                    if i.Right != nil {
+                        i.Right.Next = i.Next.Right
+                    } else if i.Left != nil {
+                        i.Left.Next = i.Next.Right
+                    }
+                }
             }
-            if dq.Right != nil {
-                q = append(q, dq.Right)
-            }
-            qSize--
+            i = i.Next
+        }
+        if l.Left != nil {
+            l = l.Left
+        } else {
+            l = l.Right
         }
     }
     return root
 }
+
+// approach: BFS
+// time = o(n)
+// space = o(maxWidthOfTree) for queue
+// func connect(root *Node) *Node {
+//     if root == nil {
+//         return root
+//     }
+    
+//     q := []*Node{root}
+//     for len(q) != 0 {
+//         qSize := len(q)
+//         for qSize != 0 {
+//             dq := q[0]
+//             q = q[1:]
+//             if qSize > 1 {
+//                 dq.Next = q[0]
+//             }
+//             if dq.Left != nil {
+//                 q = append(q, dq.Left)
+//             }
+//             if dq.Right != nil {
+//                 q = append(q, dq.Right)
+//             }
+//             qSize--
+//         }
+//     }
+//     return root
+// }
