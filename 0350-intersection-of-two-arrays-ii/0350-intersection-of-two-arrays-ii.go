1e9cd23ddb5e5e2e95074502fa74af74861b5c22
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-16 20:31:34 -0700

    Time: 0 ms (100.00%), Space: 2.8 MB (96.64%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.go
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.go
@@ -1,21 +1,21 @@
 func intersect(nums1 []int, nums2 []int) []int {
-    if len(nums2) < len(nums1) {
-        return intersect(nums2, nums1)
-    }
-    
-    // nums1 is always smaller
-    n1Freq := map[int]int{}
-    for i := 0; i < len(nums1); i++ {
-        n1Freq[nums1[i]]++
-    }
+    sort.Ints(nums1)
+    sort.Ints(nums2)
     
     out := []int{}
-    for i := 0; i < len(nums2); i++ {
-        num := nums2[i]
-        val, ok := n1Freq[num]
-        if ok && val > 0 {
-            out = append(out, num)
-            n1Freq[num]--
+    n1 := 0
+    n2 := 0
+    for n1 < len(nums1) && n2 < len(nums2) {
+        if nums1[n1] == nums2[n2] {
+            out = append(out, nums1[n1])
+            n1++
+            n2++
+        } else if nums1[n1] < nums2[n2] {
+             // [ 1 ..
+            //  [ 2 ..
+            n1++
+        } else {
+            n2++
         }
     }
     return out
