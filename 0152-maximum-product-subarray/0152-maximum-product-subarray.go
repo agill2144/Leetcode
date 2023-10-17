f9f944545c010aab05e7bee9254e36283219a7af
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-16 19:09:14 -0700

    Time: 6 ms (70.91%), Space: 3.4 MB (97.12%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.go
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.go
@@ -2,13 +2,21 @@ func maxProduct(nums []int) int {
     prefix := 1
     suffix := 1
     out := math.MinInt64
-    for i := 0; i < len(nums); i++  {
-        if prefix == 0 {prefix=1}
-        if suffix == 0 {suffix=1}
-
-        prefix *= nums[i]            
+    
+    for i := 0; i < len(nums); i++ {
+        prefix *= nums[i]
         suffix *= nums[len(nums)-1-i]
         out = max(out, max(prefix, suffix))
+        if prefix == 0{
+            // we multiplied with 0
+            // reset and start forming a new subarray
+            prefix = 1
+        }
+        if suffix == 0 {
+            // we multiplied with 0
+            // reset and start forming a new subarray
+            suffix = 1
+        }
     }
     return out
 }
