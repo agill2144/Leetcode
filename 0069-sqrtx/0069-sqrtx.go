60717f158995004441b26941d6c06dedc7b9bf4b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-19 22:29:41 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (10.48%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.go
+++ 0069-sqrtx/0069-sqrtx.go
@@ -1,3 +1,17 @@
+// how to find sqrt of a number x ?
+// in a linear manner; check if 1*1 == x?
+// then check 2*2
+// then check 3*3
+// time = o(ologn)
+// space = o(1)
+// binary searching on ANSWERS!!!
+// We find the list of possible answers
+// and that list is usually a range with a max(right) value
+// binary search for the left most value whose square comes as close as possible to x ( from the left side )
+// what is left most ? if below is a number line from 1 to x
+// 1----------------------------------x------
+//                                   ^ this is the left most ( less than x but greatest possible )
+// we may also find exactly x , we can return that
 func mySqrt(x int) int {
     if x < 2 {return x}
     
