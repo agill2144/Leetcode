861dd2a0e48b6d11eebe7595de88893eaebc392c
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-19 22:24:55 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (97.85%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.go
+++ 0069-sqrtx/0069-sqrtx.go
@@ -1,14 +1,15 @@
 func mySqrt(x int) int {
     if x < 2 {return x}
     
-    // right := 1
-    // for right * right < x {
-    //     right *= 2
-    // }
+    // use binary search to find the right window
+    // or we could just use right := x/2
+    right := 1
+    for right * right < x {
+        right *= 2
+    }
     
     ans := -1
     left := 1
-    right := x/2    
     for left <= right {
         mid := left + (right-left)/2
         if mid * mid <= x {
