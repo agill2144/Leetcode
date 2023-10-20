f84763add0dccd02c518ac641d4d3c22e9b50883
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-19 22:23:07 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (10.48%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.go
@@ -0,0 +1,22 @@
+func mySqrt(x int) int {
+    if x == 0 {return 0}
+    right := 1
+    for right * right < x {
+        right *= 2
+    }
+    
+    ans := -1
+    left := 1
+    
+    for left <= right {
+        mid := left + (right-left)/2
+        if mid * mid <= x {
+            if mid * mid == x {return mid}
+            ans = mid
+            left = mid+1
+        } else {
+            right = mid-1
+        }
+    }
+    return ans
+}
