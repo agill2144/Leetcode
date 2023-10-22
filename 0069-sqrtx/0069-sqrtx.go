1a288a6e642324eef4b78251a90aa8c3545aa3da
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-21 19:28:16 -0700

    Time: 0 ms (100.00%), Space: 2.1 MB (10.27%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.go
+++ 0069-sqrtx/0069-sqrtx.go
@@ -1,4 +1,39 @@
 /*
+    approach: binary search on answers!
+    - we know the sqrt is between 1 and x
+    - THIS IS A SORTED RANGE
+    - which means we can easily binary search on these answers
+    
+    *BINARY SEARCH ON ANSWERS IS A COMMON PATTER*
+    - Identify if there is a range we can use
+    - ITS a range, ranges are sorted
+    - then binary search on that range
+
+    time = o(logx)
+    space = o(1)
+*/
+func mySqrt(x int) int {
+    ans := 0
+    left := 1
+    right := x
+    
+    for left <= right {
+        mid := left + (right-left)/2
+        if mid * mid <= x {
+            if mid*mid == x {return mid}
+            ans = mid
+            // we may have undershot, look for a higher number ( we want the left most )
+            left = mid+1
+        } else {
+            // our sq is too high, pick a smaller number
+            right = mid-1
+        }
+    }
+    return ans
+}
+
+
+/*
     approach: linear scan
     - square root of a number ( square! , i.e a^2 )
     - so we need to find such $a whose square == x
@@ -11,14 +46,14 @@
     time = o(x)
     space = o(1)
 */
-func mySqrt(x int) int {
-    ans := 0
-    for i := 1; i <= x; i++ {
-        if i*i <= x {
-            ans = i
-        } else {
-            break
-        }
-    }
-    return ans
-}
+// func mySqrt(x int) int {
+//     ans := 0
+//     for i := 1; i <= x; i++ {
+//         if i*i <= x {
+//             ans = i
+//         } else {
+//             break
+//         }
+//     }
+//     return ans
+// }
