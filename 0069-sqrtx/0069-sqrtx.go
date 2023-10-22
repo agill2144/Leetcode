e678e81a6d61512e2aa535059e37b4fbbfd63ba3
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-21 19:22:44 -0700

    Time: 15 ms (16.38%), Space: 2.1 MB (10.27%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.go
+++ 0069-sqrtx/0069-sqrtx.go
@@ -1,37 +1,23 @@
-// how to find sqrt of a number x ?
-// in a linear manner; check if 1*1 == x?
-// then check 2*2
-// then check 3*3
-// time = o(ologn)
-// space = o(1)
-// binary searching on ANSWERS!!!
-// We find the list of possible answers
-// and that list is usually a range with a max(right) value
-// binary search for the left most value whose square comes as close as possible to x ( from the left side )
-// what is left most ? if below is a number line from 1 to x
-// 1----------------------------------x------
-//                                   ^ this is the left most ( less than x but greatest possible )
-// we may also find exactly x , we can return that
+/*
+    approach: linear scan
+    - square root of a number ( square! , i.e a^2 )
+    - so we need to find such $a whose square == x
+    - we can start from 1 and go all the upto x itself
+    - and x is not a perfect square, we want the left most possible ans
+    
+    eg1; x = 25; return 5; because 5^2 = 25
+    eg2; x = 28; return 5; because 5^2 = 25, 6^2 = 26, which is greater than x, and we want the leftmost possible ans
+    
+    time = o(x)
+    space = o(1)
+*/
 func mySqrt(x int) int {
-    if x < 2 {return x}
-    
-    // use binary search to find the right window
-    // or we could just use right := x/2
-    right := 1
-    for right * right < x {
-        right *= 2
-    }
-    
-    ans := -1
-    left := 1
-    for left <= right {
-        mid := left + (right-left)/2
-        if mid * mid <= x {
-            if mid * mid == x {return mid}
-            ans = mid
-            left = mid+1
+    ans := 0
+    for i := 1; i <= x; i++ {
+        if i*i <= x {
+            ans = i
         } else {
-            right = mid-1
+            break
         }
     }
     return ans
