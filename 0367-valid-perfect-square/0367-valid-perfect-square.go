6b9aedf3b9f2dbeb0fbea74f76bb60bda09b10a5
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-21 19:31:09 -0700

    Time: 1 ms (81.19%), Space: 1.9 MB (95.54%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.go
+++ 0367-valid-perfect-square/0367-valid-perfect-square.go
@@ -1,4 +1,35 @@
 /*
+    approach: binary search on answers!
+    - we know the sqrt is between 1 and x
+    - THIS IS A SORTED RANGE
+    - which means we can easily binary search on these answers
+    
+    *BINARY SEARCH ON ANSWERS IS A COMMON PATTER*
+    - Identify if there is a range we can use
+    - ITS a RANGE, ranges are sorted
+    - then binary search on that range
+    
+    *The best way to identify the range, is to see it in brute force
+*/
+func isPerfectSquare(num int) bool {
+    left := 1
+    right := num
+
+    for left <= right {
+        mid := left + (right-left)/2
+        if mid*mid == num {return true}
+        if mid*mid > num {
+            right = mid-1
+        } else {
+            left = mid+1
+        }
+    }
+    // could not find such $a whose square equals to num ( no such $a^2 == num )
+    // therefore return false
+    return false
+}
+
+/*
     approach: linear scan
     - square root of a number ( square! , i.e a^2 )
     - so we need to find such $a whose square == x
@@ -10,10 +41,10 @@
     time = o(x)
     space = o(1)
 */
-func isPerfectSquare(num int) bool {
-    for i := 0; i <= num; i++ {
-        if i*i == num {return true}
-        if i*i > num {return false}
-    }
-    return false
-}
+// func isPerfectSquare(num int) bool {
+//     for i := 0; i <= num; i++ {
+//         if i*i == num {return true}
+//         if i*i > num {return false}
+//     }
+//     return false
+// }
