4a8fc4626f4481f2cbcd8c09dce73c185c840054
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-28 14:25:30 -0700

    Time: 1 ms (100.00%), Space: 2.2 MB (16.67%) - LeetHub

---
+++ 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.go
@@ -0,0 +1,65 @@
+func movesToMakeZigzag(nums []int) int {
+    n := len(nums)
+    odd := make([]int, n)
+    copy(odd, nums)
+    oddMoves := math.MaxInt64
+    for i := 1; i < n; i+=2 {
+        curr := odd[i]
+        left := math.MinInt64
+        right := math.MinInt64
+        if i != 0 {left = odd[i-1]}
+        if i != n-1 {right = odd[i+1]}
+        if curr > left && curr > right {continue}
+        
+        if left >= curr {
+            if oddMoves == math.MaxInt64 {oddMoves = 0}
+            newVal := curr-1
+            oddMoves += (left - newVal)
+            odd[i-1] = newVal
+        }
+        
+        if right != math.MinInt64 && right >= curr {
+            if oddMoves == math.MaxInt64 {oddMoves = 0}
+            newVal := curr-1
+            oddMoves += (right-newVal)
+            odd[i+1] = newVal
+        }
+    }
+    evenMoves := math.MaxInt64
+    for i := 0; i < n; i+=2 {
+        curr := nums[i]
+        left := math.MinInt64
+        right := math.MinInt64
+        if i != 0 {left = nums[i-1]}
+        if i != n-1 {right = nums[i+1]}
+        
+        if curr > left && curr > right {continue}
+        
+        if left != math.MaxInt64 && left >= curr {
+            if evenMoves == math.MaxInt64 {evenMoves = 0}
+            newVal := curr-1
+            evenMoves += (left-newVal)
+            nums[i-1] = newVal
+        }
+        
+        if right >= curr {
+            if evenMoves == math.MaxInt64 {evenMoves = 0}
+            newVal := curr-1
+            evenMoves += (right-newVal)
+            nums[i+1] = newVal
+        }
+    }
+    if evenMoves == math.MaxInt64 {evenMoves = 0}
+    if oddMoves == math.MaxInt64 {oddMoves = 0}
+    return min(evenMoves, oddMoves)
+}
+
+func abs(x int) int {
+    if x < 0 {return x*-1}
+    return x
+}
+
+func min(x, y int) int {
+    if x < y {return x}
+    return y
+}
