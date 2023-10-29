f87de74fdff4c82825f8d573040f4af6a59cc066
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-28 19:49:56 -0700

    Time: 82 ms (94.78%), Space: 8.8 MB (33.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.go
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -1,14 +1,12 @@
 func maxSubArray(nums []int) int {
+    max := math.MinInt64
     sum := 0
-    maxSum := math.MinInt64
     for i := 0; i < len(nums); i++ {
         sum += nums[i]
-        if sum > maxSum {
-            maxSum = sum
+        if sum > max {
+            max = sum
         }
-        if sum < 0 {
-            sum = 0
-        }
+        if sum < 0 {sum = 0}
     }
-    return maxSum
+    return max
 }
