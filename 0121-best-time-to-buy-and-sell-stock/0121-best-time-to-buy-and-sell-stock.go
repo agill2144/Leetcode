ce57a28b672e5748834a5218b671edf4257235ef
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-29 12:26:53 -0700

    Time: 100 ms (98.11%), Space: 8 MB (89.11%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -1,20 +1,14 @@
 func maxProfit(prices []int) int {
-    buyPrices := make([]int, len(prices))
-    buyPrices[0] = prices[0]
-    for i := 1; i < len(prices); i++ {
-        minBuyPrice := prices[i]
-        if buyPrices[i-1] < minBuyPrice {
-            minBuyPrice = buyPrices[i-1]
-        }
-        buyPrices[i] = minBuyPrice
-    }
+    buyPrice := prices[0]
     maxProfit := 0
     for i := 0; i < len(prices); i++ {
         sell := prices[i]
-        buy := buyPrices[i]
-        profit := sell-buy
+        profit := sell-buyPrice
         if profit > maxProfit {
             maxProfit = profit
+        }
+        if sell < buyPrice {
+            buyPrice = sell
         }
     }
     return maxProfit
