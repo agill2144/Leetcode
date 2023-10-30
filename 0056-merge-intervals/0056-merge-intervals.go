fcacf834cb28314aa38f3dc7f98a2110ed64f358
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-29 19:23:13 -0700

    Time: 18 ms (75.77%), Space: 6.3 MB (98.27%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.go
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -1,16 +1,19 @@
 func merge(intervals [][]int) [][]int {
-    sort.Slice(intervals, func(i, j int)bool {
+    sort.Slice(intervals, func(i, j int)bool{
         return intervals[i][0] < intervals[j][0]
     })
     out := [][]int{intervals[0]}
     for i := 1; i < len(intervals); i++ {
-        prev := out[len(out)-1]
-        curr := intervals[i]
-        if curr[0] <= prev[1] {
-            out[len(out)-1][0] = min(prev[0], curr[0])
-            out[len(out)-1][1] = max(prev[1], curr[1])
+        start := intervals[i][0]
+        end := intervals[i][1]
+        prevStart := out[len(out)-1][0]
+        prevEnd := out[len(out)-1][1]
+        
+        if start <= prevEnd {
+            out[len(out)-1][0] = min(start, prevStart)
+            out[len(out)-1][1] = max(end, prevEnd)
         } else {
-            out = append(out, curr)
+            out = append(out, intervals[i])
         }
     }
     return out
@@ -20,6 +23,7 @@ func min(x, y int) int {
     if x < y {return x}
     return y
 }
+
 func max(x, y int) int {
     if x > y {return x}
     return y
