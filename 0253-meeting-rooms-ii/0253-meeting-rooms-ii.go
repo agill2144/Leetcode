75c2bb71e46ad13598340539d063e339e16e0cb3
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-30 20:59:57 -0700

    Time: 8 ms (77.08%), Space: 4.2 MB (99.48%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.go
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.go
@@ -1,67 +1,32 @@
 func minMeetingRooms(intervals [][]int) int {
-    sort.Slice(intervals, func(i, j int)bool{
+    sort.Slice(intervals, func(i, j int)bool {
         return intervals[i][0] < intervals[j][0]
     })
-    rooms := &minHeap{items: []int{}}
+    // tracks meeting end times
+    rooms := []int{}
+    
     for i := 0; i < len(intervals); i++ {
         start := intervals[i][0]
         end := intervals[i][1]
-        // can this meeting take an existing room?
-        if rooms.Len() != 0 {
-            if start >= rooms.items[0] {
-                // yes, make this room avail ( i.e remove from heap )
-                heap.Pop(rooms)
+        if len(rooms) == 0 {
+            rooms = append(rooms, end)
+        } else {
+            // find which room's meeting has ended
+            // -- take existing room if any
+            found := false
+            for j := 0; j < len(rooms); j++ {
+                if start >= rooms[j] {
+                    // found an existing room that we can take
+                    rooms[j] = end
+                    found = true
+                    break
+                }
+            }
+            // -- otherwise create a new room
+            if !found {
+                rooms = append(rooms, end)                
             }
         }
-        heap.Push(rooms, end)
     }
-    return len(rooms.items)
+    return len(rooms)
 }
-
-type minHeap struct {
-	items []int
-}
-func (m *minHeap) Swap(i, j int) { m.items[i],m.items[j] = m.items[j], m.items[i]}
-func (m *minHeap) Less(i, j int) bool {return m.items[i] < m.items[j]}
-func (m *minHeap) Len() int {return len(m.items)}
-func (m *minHeap) Push(x interface{}) {m.items = append(m.items, x.(int))}
-func (m *minHeap) Pop()interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
-}
-// brute force
-// TC = o(nlogn) + o(n^2)
-// space = o(n)
-// func minMeetingRooms(intervals [][]int) int {
-//     sort.Slice(intervals, func(i, j int)bool{
-//         return intervals[i][0] < intervals[j][0]
-//     })
-
-//     // rooms keeping track of which meeting is ending at what time
-//     // if we have a new meeting, we can check with what rooms are
-//     // free, and the rooms that will be free are the ones whose 
-//     // meetings have already ended compared currentTime
-//     rooms := []int{}
-//     for i := 0; i < len(intervals); i++ {
-//         start := intervals[i][0]
-//         end := intervals[i][1]
-//         if len(rooms) == 0 {
-//             rooms = append(rooms, end)
-//         } else {
-//             foundExistingRoom := false
-//             for j := 0; j < len(rooms); j++ {
-//                 if start >= rooms[j] {
-//                     rooms[j] = end
-//                     foundExistingRoom = true
-//                     break
-//                 }
-//             }
-//             // if none of the rooms are avail, we need a new one
-//             if !foundExistingRoom {
-//                 rooms = append(rooms, end)
-//             }
-//         }
-//     }
-//     return len(rooms)
-// }
