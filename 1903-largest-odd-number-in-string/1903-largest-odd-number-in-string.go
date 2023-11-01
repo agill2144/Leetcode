7065470f5d7c71f92d25058dd2f5b4ab0cca9771
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-31 21:10:43 -0700

    Time: 11 ms (44.00%), Space: 6.7 MB (80.00%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.go
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.go
@@ -1,11 +1,9 @@
 func largestOddNumber(num string) string {
-    n := 0
-    largest := ""
-    for i := 0; i < len(num); i++ {
-        n  = n*10+int(num[i]-'0')
+    for i := len(num)-1; i >= 0; i-- {
+        n := int(num[i]-'0')
         if n % 2 != 0 {
-            largest = num[0:i+1]
+            return num[0:i+1]
         }
-    }
-    return largest
+    }    
+    return ""
 }
