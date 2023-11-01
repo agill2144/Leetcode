ebca9ceb86b3d22de86aa7198645c337b165b070
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-10-31 21:03:37 -0700

    Time: 0 ms (100.00%), Space: 2.5 MB (87.96%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.go
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.go
@@ -1,23 +1,20 @@
 func removeOuterParentheses(s string) string {
-    dq := []byte{}
     count := 0
     out := new(strings.Builder)
     for i := 0; i < len(s); i++ {
-        dq = append(dq, s[i])
         if s[i] == '(' {
+            if count > 0 {
+                out.WriteByte(s[i])
+            }
             count++
         } else if s[i] == ')' {
             count--
+            if count > 0 {
+                out.WriteByte(s[i])
+            }  
+
         }
         
-        if count == 0 {
-            dq = dq[1:]
-            dq = dq[:len(dq)-1]
-            for len(dq) != 0 {
-                out.WriteByte(dq[0])
-                dq = dq[1:]
-            }            
-        }
     }
     
     return out.String()
