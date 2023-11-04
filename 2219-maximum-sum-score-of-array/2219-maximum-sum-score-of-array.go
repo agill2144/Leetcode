1943793caf5cc39b1ba9e010e76f065ec5a249a1
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-11-04 16:58:24 -0700

    Time: 48 ms (75.00%), Space: 8 MB (87.50%) - LeetHub

--- 2219-maximum-sum-score-of-array/2219-maximum-sum-score-of-array.go
+++ 2219-maximum-sum-score-of-array/2219-maximum-sum-score-of-array.go
@@ -1,15 +1,31 @@
+/*
+    approach: brute force
+    - nested iterations
+    time = o(n^2)
+    space = o(1)
+    
+    approach: prefix / suffix / running Sum
+    - first pass to get total
+    - second pass to get left and right sum
+        - if we have a runningSum, and we have total sum
+        - to get the leftSum ( we already have it , runningSum )
+        - to get the rightSum, remove leftSum from total and add ith val
+    
+    time = o(2n) ; o(n)
+    space = o(1)
+*/
 func maximumSumScore(nums []int) int64 {
     var total int64
     for i := 0; i < len(nums); i++ {
         total += int64(nums[i])
     }
-    var ans int64 = math.MinInt64
-    var leftRunning int64
+    var ans int64  = math.MinInt64
+    var prefixSum int64
     for i := 0; i < len(nums); i++ {
-        leftRunning += int64(nums[i])
-        var right int64 = (total - leftRunning) + int64(nums[i])
-        ans = max(ans, max(leftRunning, right))
-    }
+        prefixSum += int64(nums[i])
+        suffixSum := (total-prefixSum)+int64(nums[i])
+        ans = max(ans, max(prefixSum, suffixSum))
+    }  
     return ans
 }
 
