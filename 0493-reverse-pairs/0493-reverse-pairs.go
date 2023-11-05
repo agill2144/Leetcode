d77718bd20173f94216e9d3be81558a448e4669d
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-11-04 18:21:26 -0700

    Time: 125 ms (73.58%), Space: 9.4 MB (36.79%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.go
+++ 0493-reverse-pairs/0493-reverse-pairs.go
@@ -1,35 +1,27 @@
-/*
-    approach: divide and conquer ( merge sort )
-*/
 func reversePairs(nums []int) int {
     count := 0
-    var dfs func(left, right int)
+    var dfs func(left, right int) 
     dfs = func(left, right int) {
         // base
-        if left >=  right {return}
+        if left >= right {return}
         
         // logic
         mid := left + (right-left)/2
         dfs(left, mid)
         dfs(mid+1, right)
         
-        // left and right halves are sorted
-        // check if left (ith) element > right (jth) element
+        i := left
         j := mid+1
-        for i := left; i <= mid; i++ {
+        for i <= mid {
             for j <= right {
-                // if ith val > jth val * 2 
-                // then all the next ith val (from left side, which are sorted )
-                // WILL also be greater, therefore count += rest of the elements from left side
-                if nums[i] > nums[j]*2{
-                    count += (mid-i+1)
+                if nums[i] > nums[j]*2 {
+                    count += (mid-i)+1
                     j++
-                // if left val is not > right val, break, because we need a bigger ith val
-                // leave j where it is
                 } else {
                     break
                 }
             }
+            i++
         }
         
         // merge sorted array
@@ -52,24 +44,8 @@ func reversePairs(nums []int) int {
             nums[i] = aux[auxPtr]
             auxPtr++
         }
-        
+
     }
     dfs(0, len(nums)-1)
     return count
 }
-
-// approach: brute force
-// check all possibe pairs
-// time = o(n^2)
-// space = o(1)
-// func reversePairs(nums []int) int {
-//     count := 0
-//     for i := 0; i < len(nums); i++ {
-//         for j := i+1; j < len(nums); j++ {
-//             if nums[i] > nums[j]*2 {
-//                 count++
-//             }
-//         }
-//     }
-//     return count
-// }
