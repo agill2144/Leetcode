6533cf4da2930a47190f47137adafc61d6a1fe36
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-11-16 03:12:49 -0800

    Time: 1 ms (81.21%), Space: 2.1 MB (54.46%) - LeetHub

--- 0394-decode-string/0394-decode-string.go
+++ 0394-decode-string/0394-decode-string.go
@@ -1,31 +1,32 @@
 func decodeString(s string) string {
-    parentSt := []*strings.Builder{}
-    numSt := []int{}
-    num := 0
-    curr := new(strings.Builder)
+    parent :=  []*strings.Builder{}
+    num := []int{}
+    currNum := 0
+    currStr := new(strings.Builder)
+    
     for i := 0; i < len(s); i++ {
-        if s[i] >= 'a' && s[i] <= 'z' {
-            curr.WriteByte(s[i])
-        } else if s[i] >= '0' && s[i] <= '9' {
-            num = num * 10 + int(s[i]-'0')
-        } else if s[i] == '[' {
-            parentSt = append(parentSt, curr)
-            numSt = append(numSt, num)
-            curr = new(strings.Builder)
-            num = 0
+        if s[i] >= '0' && s[i] <= '9' {
+           currNum = currNum * 10 + int(s[i]-'0')
+        } else if s[i] >= 'a' && s[i] <= 'z' {
+            currStr.WriteByte(s[i])
+        }else if s[i] == '[' {
+            num = append(num, currNum)
+            parent = append(parent, currStr)
+            currNum = 0
+            currStr = new(strings.Builder)
         } else if s[i] == ']' {
-            k := numSt[len(numSt)-1]
-            numSt = numSt[:len(numSt)-1]
-            tmp := new(strings.Builder)
+            k := num[len(num)-1]
+            num = num[:len(num)-1]
+            decoded := new(strings.Builder)
             for k != 0 {
-                tmp.WriteString(curr.String())
+                decoded.WriteString(currStr.String())
                 k--
             }
-            parentStr := parentSt[len(parentSt)-1]
-            parentSt = parentSt[:len(parentSt)-1]
-            parentStr.WriteString(tmp.String())
-            curr = parentStr
+            p := parent[len(parent)-1]
+            parent = parent[:len(parent)-1]
+            p.WriteString(decoded.String())
+            currStr = p
         }
     }
-    return curr.String()
+    return currStr.String()
 }
