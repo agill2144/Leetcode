6d15def2f531f755a03b969cfe5efceb17da906f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-05 20:35:00 -0800

    Time: 6 ms (99.34%), Space: 6.2 MB (46.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.go
+++ 0169-majority-element/0169-majority-element.go
@@ -1,8 +1,40 @@
 func majorityElement(nums []int) int {
-    freqMap := map[int]int{}
+    ele := 0
+    count := 0
     for i := 0; i < len(nums); i++ {
-        freqMap[nums[i]]++
-        if val := freqMap[nums[i]]; val > len(nums)/2 {return nums[i]}
+        if count == 0 {
+            count++
+            ele = nums[i]
+        } else if nums[i] == ele {
+            count++
+        } else {
+            count--
+        }
     }
+    count = 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] == ele {
+            count++
+        }
+    }
+    if count > len(nums)/2 {return ele}
     return -1
 }
+
+/*
+    majority element is a number that appears more than n/2 times
+    approach: freq map
+    - maintain a freq for each number
+    - if at any point a number freq > n/2 times, we have found the majority element
+    
+    time = o(n)
+    space = o(n)
+*/
+// func majorityElement(nums []int) int {
+//     freqMap := map[int]int{}
+//     for i := 0; i < len(nums); i++ {
+//         freqMap[nums[i]]++
+//         if val := freqMap[nums[i]]; val > len(nums)/2 {return nums[i]}
+//     }
+//     return -1
+// }
