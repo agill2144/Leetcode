d7e1b9c4682b2f73d70b01ffd6b30cd0eb28ebb9
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-06 19:01:25 -0800

    Time: 11 ms (37.66%), Space: 6.9 MB (20.78%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.go
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.go
@@ -1,3 +1,4 @@
+// largest odd number will be the first odd digit we run into from the right side
 func largestOddNumber(num string) string {
     for i := len(num)-1; i >= 0; i-- {
         if int(num[i]-'0') % 2 != 0 {return num[:i+1]}
