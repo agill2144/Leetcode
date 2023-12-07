04c7eff9584acad3ab47143c721de96b59497ec3
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-07 15:52:53 -0800

    Time: 0 ms (100.00%), Space: 2.4 MB (73.66%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.go
+++ 0118-pascals-triangle/0118-pascals-triangle.go
@@ -1,14 +1,20 @@
 func generate(numRows int) [][]int {
-    n := numRows
-    out := make([][]int, n)
-    for i := 0; i < len(out); i++ {
-        out[i] = make([]int, i+1)
-        for j := 0; j < len(out[i]); j++ {
-            if j == 0 || j == len(out[i])-1 {
-                out[i][j] = 1
-            } else {
-                out[i][j] = out[i-1][j] + out[i-1][j-1]
+    out := make([][]int, numRows)
+    for i := 0; i < numRows; i++ {
+        desiredSize := i+1
+        out[i] = make([]int, desiredSize)
+        out[i][0] = 1
+        out[i][desiredSize-1] = 1
+        for j := 1; j < desiredSize-1; j++ {
+            up := 0
+            upLeft := 0
+            if i-1 >= 0 {
+                up = out[i-1][j]
             }
+            if j-1 >= 0 {
+                upLeft = out[i-1][j-1]
+            }
+            out[i][j] = up+upLeft
         }
     }
     return out
