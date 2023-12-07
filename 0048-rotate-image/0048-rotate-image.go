45368d6b183432bafd1c17d3f7311b4ef10c7443
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-06 20:03:10 -0800

    Time: 1 ms (80.98%), Space: 2.3 MB (67.15%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.go
+++ 0048-rotate-image/0048-rotate-image.go
@@ -1,13 +1,16 @@
+// transpose and reverse each row
 func rotate(matrix [][]int)  {
-    m := len(matrix)
-    n := len(matrix[0])
-    for i := 0; i < m; i++ {
-        for j := i+1; j < n; j++ {
-            matrix[i][j],matrix[j][i] = matrix[j][i], matrix[i][j]
+    n := len(matrix)
+    
+    // transpose
+    for i := 0; i < n; i++ {
+        for j := i+1; j < n ; j++ {
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         }
     }
     
-    for i := 0; i < m; i++ {
+    // reverse each row
+    for i := 0; i < n; i++ {
         for j := 0; j < n/2; j++ {
             matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
         }
