08a0997e0a5ab4b0f39635663b954cbd8362d42a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-07 22:55:32 -0800

    Time: 159 ms (26.24%), Space: 11 MB (8.19%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -1,24 +1,62 @@
 func threeSum(nums []int) [][]int {
+    sort.Ints(nums)
     set := map[[3]int]struct{}{}
     out := [][]int{}
     for i := 0; i < len(nums); i++ {
-        target := 0-nums[i]
-        numsSet := map[int]struct{}{}
+        target := 0 - nums[i]
+        if i != 0 && nums[i] == nums[i-1] {continue}
         for j := i+1; j < len(nums); j++ {
-            newTarget := target-nums[j]
-            if _, ok := numsSet[newTarget]; ok {
-                tmp := [3]int{nums[i], nums[j], newTarget}
-                sort.Ints(tmp[:])
+            diff := target - nums[j]
+            if diff < nums[i] {continue}
+            found := binarySearch(diff, j+1, nums)
+            if found {
+                tmp := [3]int{nums[i],nums[j], diff}
                 if _, ok := set[tmp]; !ok {
                     set[tmp] = struct{}{}
                     out = append(out, tmp[:])
                 }
             }
-            numsSet[nums[j]] = struct{}{}
         }
     }
     return out
 }
+
+func binarySearch(target, left int, nums []int) bool {
+    right := len(nums)-1
+    for left <= right {
+        mid := left + (right-left) / 2
+        if nums[mid] == target {
+            return true
+        } else if target > nums[mid] {
+            left = mid+1
+        } else {
+            right = mid-1
+        }
+    }
+    return false
+}
+
+// func threeSum(nums []int) [][]int {
+//     set := map[[3]int]struct{}{}
+//     out := [][]int{}
+//     for i := 0; i < len(nums); i++ { // o(n)
+//         target := 0-nums[i]
+//         numsSet := map[int]struct{}{}
+//         for j := i+1; j < len(nums); j++ { // o(n)
+//             newTarget := target-nums[j]
+//             if _, ok := numsSet[newTarget]; ok {
+//                 tmp := []int{nums[i], nums[j], newTarget} 
+//                 sort.Ints(tmp) // o(3log3)
+//                 if _, ok := set[[3]int{tmp[0],tmp[1],tmp[2]}]; !ok {
+//                     set[[3]int{tmp[0],tmp[1],tmp[2]}] = struct{}{}
+//                     out = append(out, tmp)
+//                 }
+//             }
+//             numsSet[nums[j]] = struct{}{}
+//         }
+//     }
+//     return out
+// }
 
 
 // func threeSum(nums []int) [][]int {
