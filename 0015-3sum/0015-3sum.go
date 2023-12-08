2827c244fbc7f522343568d70877bc8231c993cf
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-07 23:15:01 -0800

    Time: 38 ms (88.93%), Space: 7.6 MB (78.48%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -3,38 +3,68 @@ func threeSum(nums []int) [][]int {
     set := map[[3]int]struct{}{}
     out := [][]int{}
     for i := 0; i < len(nums); i++ {
-        target := 0 - nums[i]
         if i != 0 && nums[i] == nums[i-1] {continue}
-        for j := i+1; j < len(nums); j++ {
-            diff := target - nums[j]
-            if diff < nums[i] {continue}
-            found := binarySearch(diff, j+1, nums)
-            if found {
-                tmp := [3]int{nums[i],nums[j], diff}
+        target := 0-nums[i]
+        left := i+1
+        right := len(nums)-1
+        for left < right {
+            total := nums[left] + nums[right]
+            if total == target {
+                tmp := [3]int{nums[i],nums[left], nums[right]}
                 if _, ok := set[tmp]; !ok {
                     set[tmp] = struct{}{}
                     out = append(out, tmp[:])
                 }
+                left++
+                for left < right && nums[left] == nums[left-1] {left++}
+                right--
+                for right > left && nums[right] == nums[right+1] {right--}
+            } else if total > target {
+                right--
+            } else {
+                left++
             }
         }
     }
     return out
 }
+// func threeSum(nums []int) [][]int {
+//     sort.Ints(nums)
+//     set := map[[3]int]struct{}{}
+//     out := [][]int{}
+//     for i := 0; i < len(nums); i++ {
+//         target := 0 - nums[i]
+//         if i != 0 && nums[i] == nums[i-1] {continue}
+//         for j := i+1; j < len(nums); j++ {
+//             diff := target - nums[j]
+//             if diff < nums[i] {continue}
+//             found := binarySearch(diff, j+1, nums)
+//             if found {
+//                 tmp := [3]int{nums[i],nums[j], diff}
+//                 if _, ok := set[tmp]; !ok {
+//                     set[tmp] = struct{}{}
+//                     out = append(out, tmp[:])
+//                 }
+//             }
+//         }
+//     }
+//     return out
+// }
 
-func binarySearch(target, left int, nums []int) bool {
-    right := len(nums)-1
-    for left <= right {
-        mid := left + (right-left) / 2
-        if nums[mid] == target {
-            return true
-        } else if target > nums[mid] {
-            left = mid+1
-        } else {
-            right = mid-1
-        }
-    }
-    return false
-}
+// func binarySearch(target, left int, nums []int) bool {
+//     right := len(nums)-1
+//     for left <= right {
+//         mid := left + (right-left) / 2
+//         if nums[mid] == target {
+//             return true
+//         } else if target > nums[mid] {
+//             left = mid+1
+//         } else {
+//             right = mid-1
+//         }
+//     }
+//     return false
+// }
 
 // func threeSum(nums []int) [][]int {
 //     set := map[[3]int]struct{}{}
