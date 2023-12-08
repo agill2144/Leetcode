c930461f2abc398e11c8526b833ee2e5938d9813
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-07 23:19:11 -0800

    Time: 11 ms (62.02%), Space: 2.9 MB (49.26%) - LeetHub

--- 0018-4sum/0018-4sum.go
+++ 0018-4sum/0018-4sum.go
@@ -1,25 +1,31 @@
 func fourSum(nums []int, target int) [][]int {
+    sort.Ints(nums)
     set := map[[4]int]struct{}{}
     out := [][]int{}
     for i := 0; i < len(nums); i++ {
-        iTarget := target-nums[i]
-        
+        if i != 0 && nums[i] == nums[i-1] {continue}
+        t1 := target-nums[i]
         for j := i+1; j < len(nums); j++ {
-            jTarget := iTarget-nums[j]
-
-            s := map[int]struct{}{}
-            for k := j+1; k < len(nums); k++ {
-                diff := jTarget - nums[k]
-                if _, ok := s[diff]; ok {
-                    tmp := []int{nums[i],nums[j],nums[k], diff}
-                    sort.Ints(tmp)
-                    newTmp := [4]int{tmp[0],tmp[1], tmp[2],tmp[3]}
-                    if _, ok := set[newTmp]; !ok {
-                        set[newTmp] = struct{}{}
-                        out = append(out, tmp)
-                    }                    
+            t2 := t1 - nums[j]
+            left := j+1
+            right := len(nums)-1
+            for left < right {
+                total := nums[left] + nums[right]
+                if total == t2 {
+                    tmp := [4]int{nums[i], nums[j], nums[left], nums[right]}
+                    if _, ok := set[tmp]; !ok {
+                        set[tmp] = struct{}{}
+                        out = append(out, tmp[:])
+                    }
+                    left++
+                    for left < right && nums[left] == nums[left-1] {left++}
+                    right--
+                    for right > left && nums[right] == nums[right+1] {right--}
+                } else if total > t2 {
+                    right--
+                } else {
+                    left++
                 }
-                s[nums[k]] = struct{}{}
             }
         }
     }
