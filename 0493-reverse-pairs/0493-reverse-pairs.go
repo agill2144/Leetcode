9cb33dd3bddc9e5efd3b33e0a0a7d284a8cc0aaf
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-09 14:54:30 -0800

    Time: 2125 ms (5.00%), Space: 10 MB (34.17%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.go
+++ 0493-reverse-pairs/0493-reverse-pairs.go
@@ -1,21 +1,25 @@
 func reversePairs(nums []int) int {
     count := 0
-    var dfs func(left, right int) 
+    var dfs func(left, right int)
     dfs = func(left, right int) {
         // base
         if left >= right {return}
+        
         
         // logic
         mid := left + (right-left)/2
         dfs(left, mid)
         dfs(mid+1, right)
         
+        // left side = i
+        // right side = j
+        // looking for nums[i] > nums[j]
         i := left
-        j := mid+1
         for i <= mid {
+            j := mid+1
             for j <= right {
                 if nums[i] > nums[j]*2 {
-                    count += (mid-i)+1
+                    count++
                     j++
                 } else {
                     break
@@ -24,7 +28,7 @@ func reversePairs(nums []int) int {
             i++
         }
         
-        // merge sorted array
+        // merge sort
         aux := []int{}
         l := left
         r := mid+1
@@ -39,13 +43,14 @@ func reversePairs(nums []int) int {
         }
         for l <= mid {aux = append(aux, nums[l]); l++}
         for r <= right {aux = append(aux, nums[r]); r++}
+        
         auxPtr := 0
-        for i := left ; i <= right; i++ {
+        for i := left; i <= right; i++ {
             nums[i] = aux[auxPtr]
             auxPtr++
         }
-
     }
     dfs(0, len(nums)-1)
     return count
+    
 }
