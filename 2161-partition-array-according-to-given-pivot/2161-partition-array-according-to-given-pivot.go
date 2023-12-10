30f2362ba3df57ad55bf79fb033ce9da79121c6e
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-10 14:51:06 -0800

    Time: 181 ms (62.67%), Space: 8.9 MB (82.67%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.go
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.go
@@ -1,20 +1,40 @@
 func pivotArray(nums []int, pivot int) []int {
-    m := map[string][]int{"less": []int{}, "equal": []int{}, "greater": []int{}}
+    out := []int{}
+    equal := 0
+    greater := []int{}
     for i := 0; i < len(nums); i++ {
         if nums[i] < pivot {
-            m["less"] = append(m["less"], nums[i])
+            out = append(out, nums[i])
         } else if nums[i] == pivot {
-            m["equal"] = append(m["equal"], nums[i])
+            equal++
         } else if nums[i] > pivot {
-            m["greater"] = append(m["greater"], nums[i])
+            greater = append(greater, nums[i])
         }
     }
-    out := []int{}
-    if len(m["less"]) > 0 {out = append(out, m["less"]...)}
-    if len(m["equal"]) > 0 {out = append(out, m["equal"]...)}
-    if len(m["greater"]) > 0 {out = append(out, m["greater"]...)}
+    for equal > 0 {
+        out = append(out, pivot)
+        equal--
+    }
+    if len(greater) > 0 {out = append(out, greater...)}
     return out
 }
+// func pivotArray(nums []int, pivot int) []int {
+//     m := map[string][]int{"less": []int{}, "equal": []int{}, "greater": []int{}}
+//     for i := 0; i < len(nums); i++ {
+//         if nums[i] < pivot {
+//             m["less"] = append(m["less"], nums[i])
+//         } else if nums[i] == pivot {
+//             m["equal"] = append(m["equal"], nums[i])
+//         } else if nums[i] > pivot {
+//             m["greater"] = append(m["greater"], nums[i])
+//         }
+//     }
+//     out := []int{}
+//     if len(m["less"]) > 0 {out = append(out, m["less"]...)}
+//     if len(m["equal"]) > 0 {out = append(out, m["equal"]...)}
+//     if len(m["greater"]) > 0 {out = append(out, m["greater"]...)}
+//     return out
+// }
 
 // func pivotArray(nums []int, pivot int) []int {
 //     out := []int{}
