fc282370567b9b6f9e4e64bf45a1ee8d3dd667a9
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-10 14:49:04 -0800

    Time: 168 ms (86.67%), Space: 9 MB (82.67%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.go
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.go
@@ -1,22 +1,40 @@
 func pivotArray(nums []int, pivot int) []int {
-    out := []int{}
-    countEq := 0
+    m := map[string][]int{"less": []int{}, "equal": []int{}, "greater": []int{}}
     for i := 0; i < len(nums); i++ {
         if nums[i] < pivot {
-            out = append(out, nums[i])
+            m["less"] = append(m["less"], nums[i])
         } else if nums[i] == pivot {
-            countEq++
+            m["equal"] = append(m["equal"], nums[i])
+        } else if nums[i] > pivot {
+            m["greater"] = append(m["greater"], nums[i])
         }
     }
-    for countEq > 0 {
-        out = append(out, pivot)
-        countEq--
-    }
-    
-    for i := 0; i < len(nums); i++ {
-        if nums[i] > pivot {
-            out = append(out, nums[i])
-        }
-    }
+    out := []int{}
+    if len(m["less"]) > 0 {out = append(out, m["less"]...)}
+    if len(m["equal"]) > 0 {out = append(out, m["equal"]...)}
+    if len(m["greater"]) > 0 {out = append(out, m["greater"]...)}
     return out
 }
+
+// func pivotArray(nums []int, pivot int) []int {
+//     out := []int{}
+//     countEq := 0
+//     for i := 0; i < len(nums); i++ {
+//         if nums[i] < pivot {
+//             out = append(out, nums[i])
+//         } else if nums[i] == pivot {
+//             countEq++
+//         }
+//     }
+//     for countEq > 0 {
+//         out = append(out, pivot)
+//         countEq--
+//     }
+    
+//     for i := 0; i < len(nums); i++ {
+//         if nums[i] > pivot {
+//             out = append(out, nums[i])
+//         }
+//     }
+//     return out
+// }
