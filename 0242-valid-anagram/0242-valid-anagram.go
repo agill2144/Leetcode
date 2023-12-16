f3d3251da999cd4cb9af0f2154c4e924ad24f44f
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-16 13:58:10 -0800

    Time: 2 ms (91.05%), Space: 2.8 MB (34.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -1,8 +1,31 @@
-// sort and do equality check
+// char freq count
 func isAnagram(s string, t string) bool {
-    sList := strings.Split(s,"")
-    tList := strings.Split(t,"")
-    sort.Strings(sList)
-    sort.Strings(tList)
-    return strings.Join(sList,"") == strings.Join(tList, "")
+    if len(s) != len(t) {return false}
+    sCount := make([]int, 26)
+    totalCount := 0
+    for i := 0; i < len(s); i++ {
+        sCount[int(s[i]-'a')]++
+        totalCount++
+    }
+    for i := 0; i < len(t); i++ {
+        tCharIdx := int(t[i]-'a')
+        if sCount[tCharIdx] == 0 {return false}
+        sCount[tCharIdx]--
+        totalCount--
+    }
+    return totalCount == 0
 }
+
+// sort and do equality check
+// func isAnagram(s string, t string) bool {
+//     // o(s)
+//     sList := strings.Split(s,"")
+//     // o(s)
+//     tList := strings.Split(t,"")
+//     // o(sLogs)
+//     sort.Strings(sList)
+//     // o(tLogt)
+//     sort.Strings(tList)
+//     // o(s) + o(t)
+//     return strings.Join(sList,"") == strings.Join(tList, "")
+// }
