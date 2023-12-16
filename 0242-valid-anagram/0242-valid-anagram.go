0afdfdf034e81068cccd99d6f57a1967241f40e0
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-16 13:55:19 -0800

    Time: 20 ms (5.32%), Space: 6.5 MB (5.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -1,17 +1,8 @@
+// sort and do equality check
 func isAnagram(s string, t string) bool {
-    if len(s) != len(t) {return false}
-    sFreqMap := map[byte]int{}
-    for i := 0; i < len(s); i++ {
-        sFreqMap[s[i]]++
-    }
-    for i := 0; i < len(t); i++ {
-        char := t[i]
-        count, ok := sFreqMap[char]
-        if !ok || count == 0 {
-            return false
-        }
-        sFreqMap[char]--
-    }
-    return true
-    
+    sList := strings.Split(s,"")
+    tList := strings.Split(t,"")
+    sort.Strings(sList)
+    sort.Strings(tList)
+    return strings.Join(sList,"") == strings.Join(tList, "")
 }
