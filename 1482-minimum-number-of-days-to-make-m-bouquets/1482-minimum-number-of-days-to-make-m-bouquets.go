0ef45fd40836185b6789a46912ac914fbd496887
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-16 15:53:52 -0800

    Time: 109 ms (74.29%), Space: 10.2 MB (14.29%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.go
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.go
@@ -1,5 +1,4 @@
 func minDays(bloomDay []int, m int, k int) int {
-    // if m*k > len(bloomDay) {return -1}
     left := math.MaxInt64
     right := math.MinInt64
     for i := 0; i < len(bloomDay); i++ {
@@ -10,10 +9,11 @@ func minDays(bloomDay []int, m int, k int) int {
             right = bloomDay[i]
         }
     }
-    ans := -1    
+    
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
-        // check whether we can make m bouquets of k adjacent BLOOMED flowers
+        
         numBouquets := 0
         count := 0
         for i := 0; i < len(bloomDay); i++ {
@@ -25,12 +25,8 @@ func minDays(bloomDay []int, m int, k int) int {
                 }
             } else {
                 count = 0
-            }
+            }   
         }
-        // we want to find the earliest day where we can make m bouquets with each 
-        // containing k contagious flowers
-        // we found an ans or a day where its possible, but we may have overshot the minDay,
-        // therefore save this as a potential ans, and search left
         if numBouquets >= m {
             ans = mid
             right = mid-1
