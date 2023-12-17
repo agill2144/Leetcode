b5762ab85fb2324e96a8c2230298119dafa19204
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-17 13:07:26 -0800

    Time: 24 ms (86.30%), Space: 8.2 MB (6.85%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.go
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.go
@@ -1,22 +1,25 @@
 func smallestDivisor(nums []int, threshold int) int {
     left := 1
     right := 0
-    for i := 0; i < len(nums); i++ {if nums[i] > right {right = nums[i]}}
-
+    for i := 0; i < len(nums); i++ {
+        if nums[i] > right {
+            right = nums[i]
+        }
+    }
     ans := -1
     for left <= right {
         mid := left + (right-left)/2
         
         sum := 0
-        for k := 0; k < len(nums); k++{
-            sum += int(math.Ceil(float64(nums[k])/float64(mid)))            
+        for i := 0; i < len(nums); i++ {
+            sum += int(math.Ceil(float64(nums[i])/float64(mid)))
         }
         if sum <= threshold {
             ans = mid
             right = mid-1
         } else {
             left = mid+1
-        }        
+        }
     }
     return ans
 }
