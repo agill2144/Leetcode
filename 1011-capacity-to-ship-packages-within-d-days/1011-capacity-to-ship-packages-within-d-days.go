a07be9d558224bbbb95af7dc94c4cf74a5279bc5
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-19 19:57:44 -0800

    Time: 41 ms (50.63%), Space: 7.2 MB (26.58%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.go
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.go
@@ -1,32 +1,37 @@
 func shipWithinDays(weights []int, days int) int {
-    left := math.MinInt64
-    right := 0
+    start := math.MinInt64
+    end := 0
     for i := 0; i < len(weights); i++ {
-        if weights[i] > left {left = weights[i]}
-        right += weights[i]
+        if weights[i] > start {start = weights[i]}
+        end += weights[i]
     }
-
+    left := start
+    right := end
     ans := -1
     for left <= right {
-        mid := left + (right-left)/2
-        
-        // how many day will we need if our capacity is $mid
+        // mid is acting as our capacity to evaluate
+        mid := left + (right-left)/2        
+        numDays := 0        
         total := 0
-        daysTaken := 1
-        for i := 0; i < len(weights); i++ {
-            total += weights[i]
-            if total > mid {
-                daysTaken++
-                total = weights[i]
+        for j := 0; j < len(weights); j++ {
+            total += weights[j]
+            if total == mid {
+                numDays++
+                total = 0
+            } else if total > mid{
+                numDays++
+                total = weights[j]
             }
+            if j == len(weights)-1 && total != 0 {numDays++; total=0}
         }
         
-        if daysTaken <= days {
+        if numDays > days {
+            left = mid+1
+        } else {
             ans = mid
             right = mid-1
-        } else {
-            left = mid+1
         }
+        
     }
     return ans
 }
