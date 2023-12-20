76f0ee2e565ee5436c828018292092e47d18679e
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-19 21:40:21 -0800

    Time: 4 ms (75.00%), Space: 2.8 MB (29.84%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.go
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.go
@@ -4,11 +4,14 @@ func findKthPositive(arr []int, k int) int {
     for left <= right {
         mid := left + (right-left)/2
         val := arr[mid]
-        correctIdx := val-1
-        countMissingOnLeft := correctIdx - mid
+        desiredIdx := val-1
+        countMissingOnLeft := desiredIdx - mid
         if countMissingOnLeft < k {
+            // there are less than k numbers missing to the left of mid,
             left = mid+1
         } else {
+            // there are more than k numbers missing to the left of mid,
+            // we want kth missing, therefore search left
             right = mid-1
         }
     }
