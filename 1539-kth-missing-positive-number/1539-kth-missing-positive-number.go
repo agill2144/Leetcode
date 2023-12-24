e412f8b7d0efb49ff98e1e164742051ece993cdf
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-24 13:36:22 -0800

    Time: 4 ms (78.20%), Space: 3.2 MB (11.28%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.go
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.go
@@ -1,3 +1,17 @@
+/*
+    approach: brute force
+    - create a list of missing numbers
+    - if kth missing lies within our missing list
+        - if k-1 idx exists within the missing numbers list
+        - return value at missing[k-1]
+    - otherwise kth missing lies outside missing list
+        - so that means arr[len(arr)-1]+k, HOWEVER
+        - we have take into account that number of missing elements we have found
+        - therefore arr[len(arr)-1]+k-len(missing)
+    
+    time = o(maxVal)
+    space = o(maxVal)
+*/
 func findKthPositive(arr []int, k int) int {
     missing := []int{}
     counter := 1
