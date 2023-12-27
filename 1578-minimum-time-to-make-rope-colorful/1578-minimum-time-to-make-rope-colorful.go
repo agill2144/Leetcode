c04266ccbdd691fc211af071bbbc263d88f7cd5c
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-26 20:57:38 -0800

    Time: 112 ms (77.27%), Space: 8.6 MB (45.45%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.go
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.go
@@ -1,3 +1,21 @@
+/*
+    greedy greedy greedy
+    maintain 2 min times with a counter keeping track of consec
+    1. smallest min
+    2. second smallest min
+
+    if this color is same as prev color
+    - increment consecutive counter
+    - set/update min and second min times
+    - once we have seen 2 consecutive counts ( 2 same colors )
+        - add min to total
+        - promote second min to mind
+        - reset second to maxInt
+        - reset counter = 1
+    
+    time = o(n)
+    space = o(1)
+*/
 func minCost(colors string, neededTime []int) int {
     total := 0
     second := math.MaxInt64
