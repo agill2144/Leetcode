47caa045c9eb910958adc64a9e22788a5044de8b
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-31 14:22:10 -0800

    Time: 0 ms (100.00%), Space: 2.1 MB (26.67%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.go
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.go
@@ -1,17 +1,17 @@
 func maxLengthBetweenEqualCharacters(s string) int {
     longest := math.MinInt64
+    charIdx := map[byte]int{}
     for i := 0; i < len(s); i++ {
-        for j := i+1; j < len(s); j++ {
-            if s[j] == s[i] {
-                if (j-i+1)-2 >= longest {
-                    longest = (j-i+1)-2
-                    
-                }
+        char := s[i]
+        idx, ok := charIdx[char]
+        if ok {
+            if (i-idx+1)-2 > longest {
+                longest = (i-idx+1)-2
             }
+        } else {
+            charIdx[char] = i
         }
     }
-    if longest == math.MinInt64 {
-        longest = -1
-    }
+    if longest == math.MinInt64 {return -1}
     return longest
 }
