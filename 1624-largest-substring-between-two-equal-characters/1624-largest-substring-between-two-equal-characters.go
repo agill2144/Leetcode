8d2aa53a5c9771bfff6b991dd1825aa6957e3a99
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-12-31 14:24:58 -0800

    Time: 3 ms (13.33%), Space: 2 MB (53.33%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.go
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.go
@@ -1,3 +1,12 @@
+// we care about finding the longest possible substr between 2 identical chars
+// therefore we need to keep track of each char and its idx position
+// when we run into a char we have seen before, it means we have a possible substr
+// check the size between the 2 positions ( idx from map and i and remove the outtermost 2 chars )
+// and save this size if its bigger than our ans this far
+// BECAUSE we want the longest possible size of a substr, we wont override idx positions for char
+// that already exists in our idxMap, this will help us be greedy by keeping idx positions far away 
+// time = o(s)
+// space = o(1) ; we only have 26 keys at max (a to z)
 func maxLengthBetweenEqualCharacters(s string) int {
     longest := math.MinInt64
     charIdx := map[byte]int{}
