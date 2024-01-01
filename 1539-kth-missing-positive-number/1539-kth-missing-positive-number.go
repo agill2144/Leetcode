98583c0db9dcbe3c822b670dac5197be0f618e9f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-01 15:58:24 -0800

    Time: 7 ms (14.79%), Space: 2.8 MB (27.46%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.go
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.go
@@ -1,47 +1,19 @@
 func findKthPositive(arr []int, k int) int {
-    left := 0
-    right := len(arr)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        currentMidVal := arr[mid]
-        correctMidVal := mid+1
-        countMissingOnLeft := currentMidVal-correctMidVal
-        if countMissingOnLeft < k {
-            left = mid+1
+    ptr := 1
+    missingCount := 0
+    i := 0
+    for i < len(arr) {
+        if arr[i] != ptr {
+            missingCount++
+            if missingCount == k {
+                return ptr
+            }
+            // check the next value
+            ptr++
         } else {
-            right = mid-1
+            ptr++
+            i++
         }
     }
-    return left+k
+    return arr[len(arr)-1] + (k-missingCount)  
 }
-
-/*
-    approach: brute force
-    - create a list of missing numbers
-    - if kth missing lies within our missing list
-        - if k-1 idx exists within the missing numbers list
-        - return value at missing[k-1]
-    - otherwise kth missing lies outside missing list
-        - so that means arr[len(arr)-1]+k, HOWEVER
-        - we have take into account that number of missing elements we have found
-        - therefore arr[len(arr)-1]+k-len(missing)
-    
-    time = o(maxVal)
-    space = o(maxVal)
-*/
-// func findKthPositive(arr []int, k int) int {
-//     missing := []int{}
-//     counter := 1
-//     i := 0
-//     for i < len(arr) {
-//         if arr[i] == counter {
-//             i++
-//             counter++
-//         } else {
-//             missing = append(missing, counter)
-//             counter++
-//         }
-//     }
-//     if k-1 <= len(missing)-1 {return missing[k-1]}
-//     return arr[len(arr)-1]+k-len(missing)
-// }
