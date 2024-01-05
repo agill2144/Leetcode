c18989634c9f62a9a036b4f0e3162e7dec27e618
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-04 18:27:32 -0800

    Time: 36 ms (42.44%), Space: 3.6 MB (51.73%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.go
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.go
@@ -1,22 +1,44 @@
 func lengthOfLIS(nums []int) int {
-    n := len(nums)
-    dp := make([]int, n)
-    for i := 0; i < n; i++ {dp[i] = 1}
-
-    maxLen := 1
-    for i := 1; i < n; i++{
+    dp := make([]int, len(nums))
+    for i := 0; i < len(dp); i++ {
+        dp[i] = 1
+    }
+    longest := 1
+    for i := 1; i < len(nums); i++ {
         curr := nums[i]
         for j := i-1; j >= 0; j-- {
             if curr > nums[j] {
                 dp[i] = max(dp[i], dp[j]+1)
             }
-            maxLen = max(dp[i], maxLen)
         }
+        longest = max(longest, dp[i]) 
     }
-    return maxLen
+    return longest
 }
 
 func max(x, y int) int {
     if x > y {return x}
     return y
 }
+// brute force ; backtracking
+// form all possible combinations
+// tc = exponential
+// func lengthOfLIS(nums []int) int {
+//     largest := 0
+//     var dfs func(start int, path []int)
+//     dfs = func(start int,  path []int) {
+//         // base
+//         if len(path) > largest {largest = len(path)}
+        
+//         // logic
+//         for i := start; i < len(nums); i++ {
+//             if len(path) == 0 || nums[i] > path[len(path)-1] {
+//                 path = append(path, nums[i])
+//                 dfs(i+1, path)
+//                 path = path[:len(path)-1]
+//             }
+//         }
+//     }
+//     dfs(0, nil)
+//     return largest
+// }
