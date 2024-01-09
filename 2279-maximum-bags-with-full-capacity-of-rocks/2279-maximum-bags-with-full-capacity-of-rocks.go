007090645875c027e530b971e91c3b7914df4dcc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-08 21:47:58 -0800

    Time: 191 ms (6.25%), Space: 9 MB (6.25%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.go
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.go
@@ -1,17 +1,29 @@
+// greedy
+// time = o(n) + o(nlogn) + o(n) ; o(nlogn)
+// space = o(n)
 func maximumBags(capacity []int, rocks []int, additionalRocks int) int {
+    
+    // 1. figure out the availibilites left
     avail := []int{}
-    for i := 0; i < len(rocks); i++ {
+    for i := 0; i < len(rocks); i++ { 
         avail = append(avail, capacity[i]-rocks[i])
     }
+    // 2. sort them and fill up the bag that has most space (greedy)
     sort.Ints(avail)
     count := 0
     for i := 0; i < len(avail); i++ {
+        // 3. if this bag is already full, count it and move on
         if avail[i] == 0 {
             count++
             continue
         }
+        // 4. rocks > avail or rocks < avail ; in both cases, we want the min val
+        // this is the number of rocks we can add to ith bag
         toBeAdded := min(avail[i], additionalRocks)
+        // 5. if adding these many more rocks makes the bag full
         if avail[i]-toBeAdded == 0 {
+            // 6. count the bag as full
+            // and remove the number of rocks we just added from numberOfTotalRocks
             count++
             additionalRocks -= toBeAdded
         }
