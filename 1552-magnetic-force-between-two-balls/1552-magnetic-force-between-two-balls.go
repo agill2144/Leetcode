bb1848bb472196429bfa0e42b1e55a71fa515907
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-09 22:20:50 -0800

    Time: 172 ms (46.15%), Space: 7.9 MB (92.31%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.go
@@ -0,0 +1,31 @@
+func maxDistance(position []int, m int) int {
+    // min ans is maximized ; "minimum magnetic force between any two balls is maximum"
+    sort.Ints(position)
+    left := 1
+    right := position[len(position)-1]
+    ans := -1
+    for left <= right {
+        mid := left + (right-left)/2
+        
+        // mid is our at-min val
+        // how many balls can be placed if at-min the dist is mid
+        count := 1
+        lastPlaced := 0
+        for i := 1; i < len(position); i++ {
+            currPos := position[i]
+            lastPos := position[lastPlaced]
+            if currPos - lastPos >= mid {
+                count++
+                lastPlaced = i
+            }
+        }
+        
+        if count >= m {
+            ans = mid
+            left = mid+1
+        } else {
+            right = mid-1
+        }
+    }
+    return ans
+}
