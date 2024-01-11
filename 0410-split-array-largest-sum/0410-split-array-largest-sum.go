0bb78ef67a3feda3c21216c7a2fbbaa58cf4e98a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-10 23:16:26 -0800

    Time: 1 ms (86.14%), Space: 2.2 MB (67.33%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.go
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.go
@@ -1,44 +1,35 @@
-// time = o(n) + o( (log$totalSum-$maxNum+1)*n )
 func splitArray(nums []int, k int) int {
+    if k > len(nums) {return -1}
     left := math.MinInt64
     right := 0
     for i := 0; i < len(nums); i++ {
         if nums[i] > left {left = nums[i]}
         right += nums[i]
     }
-    
     ans := -1
     for left <= right {
         mid := left + (right-left)/2
-        partitionsPlaced := totalPartitionsPlaced(nums, mid)
+        // largest sum of any subarray is minimized.
+        // max ans is minimized
+        // so at max if our split sum is mid
+        // how many subarrays would we get?
         
-        // if k = 2; this means, create 2 subarrays with 1 partition
-        // if k = 3; this means, create 3 subarrays with 2 partition
-        // therefore number of partitions placed must be < k-1 
-        // if we have placed more than k-1 partition, our partition sum is too small,
-        // we need to increase our allowed partition sum, therefore left = mid+1
-        if partitionsPlaced > k-1 {
-            left = mid + 1
-        } else {
-            // we have a potential ans, but since we need the smallest possible ans
-            // save this ans, and continue searching for a smaller ans
-            // i.e search left; i.e move left
+        count := 1
+        rSum := 0
+        for i := 0; i < len(nums); i++ {
+            rSum += nums[i]
+            if rSum > mid {
+                count++
+                rSum = nums[i]
+            }
+        }
+        if count <= k {
             ans = mid
             right = mid-1
+        } else {
+            left = mid+1
         }
     }
     return ans
-}
-
-func totalPartitionsPlaced(nums []int, maxSum int) int {
-    rSum := 0
-    partitions := 0
-    for i := 0; i < len(nums); i++ {
-        rSum += nums[i]
-        if rSum > maxSum {
-            partitions++
-            rSum = nums[i]
-        }
-    }
-    return partitions
+    
 }
