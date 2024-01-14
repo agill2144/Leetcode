1e6822249b0665e5c677b404b24791e3bb69ef6b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-13 17:02:24 -0800

    Time: 17 ms (86.55%), Space: 6.7 MB (71.72%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.go
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.go
@@ -4,13 +4,19 @@ func closeStrings(word1 string, word2 string) bool {
     w1Freq := make([]int, 26)
     w2Total := 0
     w2Freq := make([]int, 26)
+    // o(w1)
     for i := 0; i < len(word1); i++ {w1Freq[int(word1[i]-'a')]++; w1Total++}
+    // o(w2)
     for i := 0; i < len(word2); i++ {w2Freq[int(word2[i]-'a')]++; w2Total++}
+    // o(1)
     for i := 0; i < 26; i++ {
         if (w1Freq[i] == 0 && w2Freq[i] == 0) ||  (w1Freq[i] > 0 && w2Freq[i] > 0) {continue}
         return false
     }
+    // o(26Log26) ; o(1)
     sort.Ints(w1Freq)
+    // o(26Log26) ; o(1)
     sort.Ints(w2Freq)
+    // o(26) ; o(1)
     return reflect.DeepEqual(w1Freq,w2Freq)
 }
