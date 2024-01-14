12137b49d329c3985b941c95b639f7a3dc30b500
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-14 12:22:24 -0800

    Time: 0 ms (100.00%), Space: 2.2 MB (65.69%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.go
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.go
@@ -1,35 +1,44 @@
+
 func splitArray(nums []int, k int) int {
     if k > len(nums) {return -1}
-    left := math.MinInt64
-    right := 0
+    // that the largest sum of any subarray is minimized
+    // max ans is minized 
+    // meaning there multiple placements of split to make k total splits
+    // after each placement, we want the largest from each placment
+    // then, from all the placements, we want the smallest such sum
+    
+    start := math.MinInt64
+    end := 0
     for i := 0; i < len(nums); i++ {
-        if nums[i] > left {left = nums[i]}
-        right += nums[i]
+        if nums[i] > start {start = nums[i]}
+        end += nums[i]
     }
-    ans := -1
+
+    ans := 0
+    left := start
+    right := end
     for left <= right {
-        mid := left + (right-left)/2
-        // largest sum of any subarray is minimized.
-        // max ans is minimized
-        // so at max if our split sum is mid
-        // how many subarrays would we get?
+        mid := left + (right-left) / 2
         
-        count := 1
+        maxSumAllowed := mid
+        countSubArr := 1
         rSum := 0
-        for i := 0; i < len(nums); i++ {
-            rSum += nums[i]
-            if rSum > mid {
-                count++
-                rSum = nums[i]
+        for j := 0; j < len(nums); j++ {
+            rSum += nums[j]
+            if rSum > maxSumAllowed {
+                countSubArr++
+                rSum = nums[j]
             }
         }
-        if count <= k {
+        
+        if countSubArr <= k {
             ans = mid
             right = mid-1
         } else {
             left = mid+1
         }
+
     }
-    return ans
     
+    return ans
 }
