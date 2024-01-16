ddcb7a954390fee8e03459c2ed1de4ead35865e9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-15 22:28:35 -0800

    Time: 34 ms (92.31%), Space: 7 MB (34.62%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.go
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.go
@@ -1,37 +1,56 @@
 func shipWithinDays(weights []int, days int) int {
-    start := math.MinInt64
-    end := 0
+    left := math.MinInt64
+    right := 0
     for i := 0; i < len(weights); i++ {
-        if weights[i] > start {start = weights[i]}
-        end += weights[i]
+        if weights[i] > left {left = weights[i]}
+        right += weights[i]
     }
-    left := start
-    right := end
     ans := -1
     for left <= right {
-        // mid is acting as our capacity to evaluate
-        mid := left + (right-left)/2        
-        numDays := 0        
-        total := 0
-        for j := 0; j < len(weights); j++ {
-            total += weights[j]
-            if total == mid {
-                numDays++
-                total = 0
-            } else if total > mid{
-                numDays++
-                total = weights[j]
-            }
-            if j == len(weights)-1 && total != 0 {numDays++; total=0}
-        }
+        mid := left + (right-left)/2
         
-        if numDays > days {
+        rSum := 0
+        totalDays := 1
+        atMax := mid
+        for i := 0; i < len(weights); i++ {
+            rSum += weights[i]
+            if rSum > atMax {
+                rSum = weights[i]
+                totalDays++
+            }
+        }
+        if totalDays > days {
             left = mid+1
         } else {
             ans = mid
             right = mid-1
         }
-        
     }
     return ans
 }
+
+// func shipWithinDays(weights []int, days int) int {
+//     start := math.MinInt64
+//     end := 0
+//     for i := 0; i < len(weights); i++ {
+//         if weights[i] > start {start = weights[i]}
+//         end += weights[i]
+//     }
+
+//     for i := start; i <= end; i++ {
+//         atMax := i
+//         rSum := 0
+//         totalDays := 1
+        
+//         for j := 0; j < len(weights); j++ {
+//             rSum += weights[j]
+//             if rSum > atMax {
+//                 totalDays++
+//                 if rSum == atMax {rSum = 0} else {rSum = weights[j]} 
+                
+//             }    
+//         }        
+//         if totalDays <= days {return i}
+//     }
+//     return -1
+// }
