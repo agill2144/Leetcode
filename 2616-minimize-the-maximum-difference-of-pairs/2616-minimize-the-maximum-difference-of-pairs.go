0c1ae9979600022893df1ebe074108d892fe62a2
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-15 18:23:42 -0800

    Time: 136 ms (6.67%), Space: 8.2 MB (100.00%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.go
@@ -0,0 +1,61 @@
+func minimizeMax(nums []int, p int) int {
+    // such that the maximum difference amongst all the pairs is minimized
+    // max ans is minimized
+    sort.Ints(nums)
+    ans := 0
+    left := 0
+    right := nums[len(nums)-1]
+    for left <= right {
+        
+        // i.e atMax diff 
+        mid := left + (right-left)/2
+
+        // count pairs whose diff <= $mid
+        count := 0
+        for i := 0; i < len(nums)-1; i++ {
+            curr := nums[i]
+            next := nums[i+1]
+            diff := next-curr
+            if diff <= mid {
+                count++
+                i++
+            }
+        }
+        
+        if count >= p {
+            ans = mid
+            right = mid-1
+        } else {
+            left = mid+1
+        }
+    }
+    return ans
+}
+
+
+// func minimizeMax(nums []int, p int) int {
+//     // such that the maximum difference amongst all the pairs is minimized
+//     // max ans is minimized
+//     sort.Ints(nums)
+//     ans := 0
+//     for i := 0; i <= nums[len(nums)-1]; i++ {
+
+//         count := 0
+//         atMax := i
+//         j := 0
+//         for j < len(nums)-1 {
+//             curr := nums[j]
+//             next := nums[j+1]
+//             diff := next-curr
+//             if diff <= atMax { 
+//                 count++
+//                 j+=2
+//             } else {
+//                 j++
+//             }
+//         }
+//         if count < p {continue}
+//         return i
+//     }
+//     return ans
+// }
