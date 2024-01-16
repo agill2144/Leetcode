8d79f0567d8acc9c74d1f81e7a881c5d96243db7
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-15 16:04:39 -0800

    Time: 59 ms (70.59%), Space: 7.1 MB (58.82%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.go
+++ 0687-longest-univalue-path/0687-longest-univalue-path.go
@@ -6,31 +6,31 @@
  *     Right *TreeNode
  * }
  */
-
-// dont understand AT ALL BUT IMPORTANT enough to get bookmarked
 func longestUnivaluePath(root *TreeNode) int {
-    maxLen := 0
-    var dfs func(r *TreeNode) (int)
-    dfs = func(r *TreeNode) (int) {
+    var dfs func(r *TreeNode) int
+    ans := 0
+    dfs = func(r *TreeNode) int {
         // base
         if r == nil {return 0}
         
         // logic
         left := dfs(r.Left)
         right := dfs(r.Right)
-        aLeft, aRight := 0,0
         
-        if r.Left != nil && r.Val == r.Left.Val {
-            aLeft += (left+1)
-        }
-        if r.Right != nil && r.Val == r.Right.Val {
-            aRight += (right+1)
-        }
-        maxLen = max(maxLen, aLeft+aRight)
-        return max(aLeft, aRight)
+
+        // either left is long or right is long
+        leftCount := 0
+        rightCount := 0
+        if r.Left != nil && r.Val == r.Left.Val { leftCount += left+1 }
+        if r.Right != nil && r.Val == r.Right.Val { rightCount += right+1 }
+        
+        
+        ans = max(ans, leftCount+rightCount)
+        return max(leftCount, rightCount)              
     }
+    
     dfs(root)
-    return maxLen
+    return ans
 }
 
 func max(x, y int) int {
