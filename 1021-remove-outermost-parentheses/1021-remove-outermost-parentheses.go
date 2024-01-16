9274e6436a020ddaaf16456825025dfff10d9367
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-15 16:31:11 -0800

    Time: 0 ms (100.00%), Space: 2.6 MB (76.84%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.go
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.go
@@ -1,3 +1,12 @@
+/*
+    approach: counting
+    - use the counter method that we use to validate paranthesis
+    - dry-run to see when to add open-paran to output ( count > 1 )
+    - dry-run to see when to add close-paran to output (count >= 1 )
+    
+    time = o(s)
+    space = o(s)
+*/
 func removeOuterParentheses(s string) string {
     count := 0
     out := new(strings.Builder)
