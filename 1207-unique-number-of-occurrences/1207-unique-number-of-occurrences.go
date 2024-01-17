112413855be3177a23886d588c36ac2f67f3791a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-16 18:49:38 -0800

    Time: 2 ms (76.48%), Space: 2.5 MB (10.45%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.go
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.go
@@ -1,10 +1,12 @@
 func uniqueOccurrences(arr []int) bool {
-    freq := map[int]int{}
-    for i := 0; i < len(arr); i++ {freq[arr[i]]++}
-    set := map[int]struct{}{}
-    for _, v := range freq {
-        if _, ok := set[v]; ok {return false}
-        set[v] = struct{}{}
+    freqMap := map[int]int{}
+    for i := 0; i < len(arr); i++ {
+        freqMap[arr[i]]++
+    }
+    occ := map[int]struct{}{}
+    for _, v := range freqMap {
+        if _, ok := occ[v]; ok {return false}
+        occ[v] = struct{}{}
     }
     return true
 }
