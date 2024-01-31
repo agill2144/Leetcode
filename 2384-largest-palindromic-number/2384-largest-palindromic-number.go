a1e4fd5e7f9a3b6ab4cf565e6d258109c810668c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-01-30 19:13:15 -0800

    Time: 37 ms (44.23%), Space: 8.2 MB (44.23%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.go
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.go
@@ -1,104 +1,94 @@
-
-// use heaps to be build largest possible palindrome
 func largestPalindromic(num string) string {
- 
-    freqMap := map[string]int{}
+    freq := make([]int, 11) // idx -> num mapping
     for i := 0; i < len(num); i++ {
-        freqMap[string(num[i])]++
+        char := num[i]
+        freq[int(char-'0')]++
     }
+    out := new(strings.Builder)
+    var middle string
+    back := []string{}
+    for i := len(freq)-1; i >= 0; i-- {
+        if freq[i] <= 1 {
+            continue
+        }
+        count := freq[i] / 2
+        freq[i] = freq[i] % 2
+        for k := 0; k < count; k++ {
+            if out.String() == "" && i == 0 {continue}
+            out.WriteString(fmt.Sprintf("%v", i))
+            back = append(back, fmt.Sprintf("%v", i))
+        }
+    }
+    for i := len(freq)-1; i >= 0; i-- {
+        if freq[i] >= 1 {
+            middle = fmt.Sprintf("%v", i)
+            break
+        }
+    }
+    out.WriteString(middle)
+    for i := len(back)-1; i >= 0; i-- {
+        out.WriteString(back[i])
+    }
+    if out.String() == "" {out.WriteString("0")}
+    return out.String()
     
-    mx := &maxHeap{items: []string{}}
-    usedKeys := &maxHeap{items: []string{}}
-
-    for k , _ := range freqMap {
-        heap.Push(mx, k)
-    }
-    
-    out := make([]*string, len(num))
-    left := 0
-    right := len(out)-1
-    
-    
-    for mx.Len() != 0 { 
-        top := heap.Pop(mx).(string)
-        freq := freqMap[top]
-        
-        // when we have left over counts for a key, push it to other pq
-        if freq == 1 || freq % 2 != 0 {
-            heap.Push(usedKeys, top)
-        }
-        
-        // the only time where we cannot use a key to build the palindrome is when freq is only 1
-        if freq == 1 {continue}
-        // or
-        // when a value to push to output is 0 and this is the first value in output, cannot have leading 0s
-        if top == "0" {
-            if left == 0 {
-                delete(freqMap, top)
-                continue
-            }
-        }
-        
-        if freq % 2 == 0 {
-            for freq != 0 {
-                out[left] = &top
-                out[right] = &top
-                left++
-                right--
-                freq-=2
-            }
-            delete(freqMap, top)
-        } else if freq > 1 {
-            n := freq-1
-            k := 0
-            for k < n {
-                out[left] = &top
-                out[right] = &top
-                left++
-                right--
-                k+=2                
-            }
-            freqMap[top] = freq-n
-        }
-    }
-    // keys we could not use the first time, because of their odd or 1 freq count
-    // take the biggest one
-    if usedKeys.Len() >= 1 {
-        top := heap.Pop(usedKeys).(string)
-        if out[left] == nil {
-            out[left] = &top
-        } else {
-            out[right] = &top
-        }
-    }
-    // build out the final string
-    outStr := new(strings.Builder)
-    for i := 0; i < len(out); i++ {
-        if out[i] != nil {
-            outStr.WriteString(*out[i])
-        }
-    }
-    if outStr.String() =="" {return "0"}
-    return outStr.String()
 }
 
-type maxHeap struct {
-    items []string
+
+type listNode struct {
+    val int
+    next *listNode
+    prev *listNode
 }
-func (m *maxHeap) Less(i,j int) bool {
-    return strings.Compare(m.items[i], m.items[j]) == 1
+type linkedList struct {
+    head *listNode
+    tail *listNode
 }
-func (m *maxHeap) Swap(i,j int) {
-    m.items[i],m.items[j] = m.items[j],m.items[i]
+func (l *linkedList) addToHead(val int) {
+    newHead := &listNode{val: val}
+    if l.head == nil {
+        l.head = newHead
+        l.tail = newHead
+        return
+    }
+    
+    newHead.next = l.head
+    l.head.prev = newHead
+    l.head = newHead
 }
-func (m *maxHeap) Len() int {
-    return len(m.items)
+func (l *linkedList) addToTail(val int) {
+    newTail := &listNode{val: val}
+    if l.head == nil {
+        l.addToHead(val)
+        return
+    }
+    l.tail.next = newTail
+    newTail.prev = l.tail
+    l.tail = newTail
 }
-func (m *maxHeap) Push(x interface{}) {
-    m.items = append(m.items, x.(string))
+func (l *linkedList) removeHead() int {
+    out := l.head.val
+    if l.head.next == nil {
+        l.head = nil
+        l.tail = nil
+        return out
+    }
+    newHead := l.head.next
+    l.head.next = nil
+    newHead.prev = nil
+    l.head = newHead
+    return out
 }
-func (m *maxHeap) Pop() interface{} {
-    out := m.items[len(m.items)-1]
-    m.items = m.items[:len(m.items)-1]
+func (l *linkedList) removeTail() int {
+    out := l.tail.val
+    if l.head == l.tail {
+        l.head = nil
+        l.tail = nil
+        return out
+    }
+    newTail := l.tail.prev
+    newTail.next = nil
+    l.tail.prev = nil
+    l.tail = newTail
     return out
 }
