4a0a551077faa3090fca57cfd5c0df215947bf39
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-12 19:13:16 -0800

    Time: 0 ms (100.00%) | Memory: 2.2 MB (86.28%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.go
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.go
@@ -20,12 +20,13 @@ func sumNumbers(root *TreeNode) int {
         // logic
         // action
         path = (path*10)+r.Val
-        // recurse
-        dfs(r.Left)
-        dfs(r.Right)
         if r.Left == nil && r.Right == nil {
             total += path
         }
+        // recurse
+        dfs(r.Left)
+        dfs(r.Right)
+        
         // backtrack - i.e remove the last digit
         path = path/10
     }
