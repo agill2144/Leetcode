6ab912ccff645340db5d95a5ea25c00825f04ee7
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-12 20:19:47 -0800

    Time: 14 ms (32.26%) | Memory: 6.2 MB (35.48%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.go
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.go
@@ -1,20 +1,32 @@
 func findPairs(nums []int, k int) int {
-    set := map[[2]int]struct{}{}
+    ansSet := map[[2]int]struct{}{}
+    set := map[int]struct{}{}
     for i := 0; i < len(nums); i++ {
-        for j := i+1; j < len(nums); j++ {
-            diff := abs(nums[j]-nums[i])
-            if diff == k {
-                tmp := [2]int{nums[i], nums[j]}
-                if nums[j] < nums[i] {
-                    tmp = [2]int{nums[j], nums[i]}
-                }
-                if _, ok := set[tmp]; !ok {
-                    set[tmp] = struct{}{}
-                }
-            }
+        // if curr num is num
+        num := nums[i]
+        // what do we need to remove from num
+        // to make it equal to k ?
+        // if num = 10; k = 2
+        // we need to remove 8 ( i.e 10-2 = 8 )
+        // we can also 10 from 12 to make it equal to k
+        // i.e searching for 2 complements num+k and num-k
+        c1 := num+k
+        if _, ok := set[c1]; ok {
+            tmp := [2]int{c1, num}
+            if num < c1 {tmp = [2]int{num, c1}}
+            ansSet[tmp] = struct{}{}
         }
+
+        c2 := num-k
+        if _, ok := set[c2]; ok {
+            tmp := [2]int{c2, num}
+            if num < c2 {tmp = [2]int{num, c2}}
+            ansSet[tmp] = struct{}{}
+        }
+
+        set[num] = struct{}{}
     }
-    return len(set)
+    return len(ansSet)
 }
 
 func abs(x int) int {
