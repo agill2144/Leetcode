cf898aa5706752d4ba3b95651ff3453034037828
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-12 19:00:35 -0800

    Time: 1 ms (69.98%) | Memory: 2.2 MB (10.54%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumNumbers(root *TreeNode) int {
+    total := 0
+    var dfs func(r *TreeNode, path int)
+    dfs = func(r *TreeNode, path int) {
+        // base
+        if r == nil {return}
+
+        // logic
+        path = path*10+r.Val
+        dfs(r.Left,path)
+        dfs(r.Right, path)
+        if r.Left == nil && r.Right == nil {
+            total += path
+        }
+    }
+    dfs(root, 0)
+    return total
+}
