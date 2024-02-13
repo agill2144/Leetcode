45e41f0ebd41cb8269afc17b3efcd7c508e804ce
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-12 18:22:37 -0800

    Time: 0 ms (100.00%) | Memory: 5.5 MB (55.58%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+    var dfs func(r *TreeNode, min, max int) bool
+    dfs = func(r *TreeNode, min, max int )bool {
+        // base
+        if r == nil {return true}
+
+        // logic
+        if !dfs(r.Left, min, r.Val) {return false}
+        if !dfs(r.Right, r.Val, max) {return false}
+        return r.Val > min && r.Val < max
+    }
+    return dfs(root, math.MinInt64, math.MaxInt64)
+}
