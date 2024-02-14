a4b88a5a15af1399c1f1da6ac91880d0f25ab43d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-13 18:23:38 -0800

    Time: 0 ms (100.00%) | Memory: 4.7 MB (43.43%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.go
+++ 113-path-sum-ii/path-sum-ii.go
@@ -16,8 +16,6 @@ func pathSum(root *TreeNode, targetSum int) [][]int {
         // logic
         path = append(path, r.Val)
         sum += r.Val
-        dfs(r.Left, sum, path)
-        dfs(r.Right, sum, path)
         if r.Left == nil && r.Right == nil {
             if sum == targetSum {
                 newP := make([]int, len(path))
@@ -25,6 +23,8 @@ func pathSum(root *TreeNode, targetSum int) [][]int {
                 out = append(out, newP)
             }
         }        
+        dfs(r.Left, sum, path)
+        dfs(r.Right, sum, path)
         path = path[:len(path)-1]
     }
     dfs(root, 0, nil)
