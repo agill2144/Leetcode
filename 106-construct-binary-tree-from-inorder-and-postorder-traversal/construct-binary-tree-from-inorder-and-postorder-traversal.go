aa589400ed2e207c6ffcedf80074339ae25b9b69
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-14 20:24:56 -0800

    Time: 5 ms (59.20%) | Memory: 4.3 MB (23.41%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.go
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.go
@@ -6,7 +6,23 @@
  *     Right *TreeNode
  * }
  */
+/*
+    Same as https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+    - but start from the back of postorder instead
+    - postorder = left-right-root
+    - anytime we build trees, we start from root
+    - root is the last element in postorder
+    - and walk back , use inorder to find its right and left subtree
 
+    - why are we building right subtree first instead of left subtree?
+    - becuase of the order we are given in
+    - we are given postorder, and postorder flows in; left,right,root
+    - and if we are start from the back of postorder, we are starting from root
+    - and walking back from root, it will be right, therefore we will be seeing right subtree elements first
+    - in pre-order, we built left first and then right because preorder list is generated using preorder flow ; root,left,right
+        - i.e root first, and then left, therefore we have left subtree elements first, therefore build left first
+        - postorder is opposite of this
+*/
 func buildTree(inorder []int, postorder []int) *TreeNode {
     idxMap := map[int]int{}
     for i := 0; i < len(inorder); i++ {
