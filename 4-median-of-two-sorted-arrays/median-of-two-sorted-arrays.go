c713865a81c9c909a427611b5eb47dd01db0c15a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-17 21:46:06 -0800

    Time: 10 ms (62.01%) | Memory: 4.8 MB (64.61%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
@@ -9,43 +9,36 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
         return findMedianSortedArrays(nums2, nums1)
     }
     total := n1+n2
-    mid1 := float64(findKth(nums1, nums2, total/2))
-    mid2 := float64(findKth(nums1, nums2, (total/2)+1))
-    if (n1+n2) % 2 == 0 {
-        return (mid1 + mid2) / 2.0
-    }
-    return max(mid1, mid2)
-}
-
-func findKth(nums1, nums2 []int, k int) int {
-    n1 := len(nums1)
-    n2 := len(nums2)
-    left := max(0, k-n2)
-    right := min(k, n1)
+    half := total/2
+    left := 0
+    right := n1
     for left <= right {
         mid := left + (right-left)/2
-        mid2 := k-mid
-        l1 := math.MinInt64
-        l2 := math.MinInt64
-        if mid != 0 {l1 = nums1[mid-1]}
-        if mid2 != 0 {l2 = nums2[mid2-1]}
-        r1 := math.MaxInt64
-        r2 := math.MaxInt64
-        if mid != n1 {r1 = nums1[mid]}
-        if mid2 != n2 {r2 = nums2[mid2]}
+        mid2 := half-mid
+
+        l1, l2 := math.MinInt64,math.MinInt64
+        if mid-1 >= 0 {l1 = nums1[mid-1]}
+        if mid2-1 >= 0 {l2 = nums2[mid2-1]}
+        r1, r2 := math.MaxInt64,math.MaxInt64
+        if mid < n1 {r1 = nums1[mid]}
+        if mid2 < n2 {r2 = nums2[mid2]}
 
         if l1 <= r2 && l2 <= r1 {
-            return max(l1, l2)
+            if total % 2 == 0 {
+                return (float64(max(l1,l2)) + float64(min(r1,r2))) / 2.0
+            }
+            return float64(min(r1,r2))
         }
 
-        if l1 > r2 {
-            right = mid-1
-        } else {
+        if l2 > r1 {
             left = mid+1
+        } else {
+            right = mid-1
         }
     }
-    return -1
+    return 0.0
 }
+
 
 
 /*
