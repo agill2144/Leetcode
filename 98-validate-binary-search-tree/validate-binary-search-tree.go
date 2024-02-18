e0ccdea86d431975ffc6a2814d743ec546d11e66
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-17 22:53:41 -0800

    Time: 6 ms (58.37%) | Memory: 5.4 MB (55.51%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.go
+++ 98-validate-binary-search-tree/validate-binary-search-tree.go
@@ -6,27 +6,20 @@
  *     Right *TreeNode
  * }
  */
- /*
-    inorder traversal will traverse in a sorted order ( if the bst was sorted )
-    whenever we see a bst , think sorted == inorder
-    bst == sorted == inorder
-
-    approach: inorder traversal with a gloabl prev
-    - running a inorder traversal will give us a sorted order
-    - if we were checking whether a array is sorted or not, we will each element will previous element
-    - therefore we will maintain a prev ptr that tells us the previous value
-    - 
- */
 func isValidBST(root *TreeNode) bool {
-    var dfs func(r *TreeNode, min, max int) bool
-    dfs = func(r *TreeNode, min, max int) bool {
+    var prev *TreeNode
+    var dfs func(r *TreeNode) bool
+    dfs = func(r *TreeNode) bool {
         // base
         if r == nil {return true}
         // logic
-        if r.Val <= min || r.Val >= max {return false}
-        if !dfs(r.Left, min, r.Val) {return false}
-        if !dfs(r.Right, r.Val, max) {return false}
-        return true
+        left := dfs(r.Left)
+        if !left {return false}
+        if prev != nil {
+            if prev.Val >= r.Val {return false}
+        }
+        prev = r
+        return dfs(r.Right)
     }
-    return dfs(root, math.MinInt64, math.MaxInt64)
+    return dfs(root)
 }
