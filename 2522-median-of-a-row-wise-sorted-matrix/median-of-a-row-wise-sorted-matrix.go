7b2747aefb633fd536f0de2c1372ff460c5dd041
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-17 22:18:54 -0800

    Time: 127 ms (57.14%) | Memory: 11.5 MB (42.86%) - LeetSync

--- 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
+++ 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
@@ -1,19 +1,61 @@
 /*
+*/
+func matrixMedian(grid [][]int) int {
+    m := len(grid)
+    n := len(grid[0])
+    total := m*n
+    half := total/2
+    left := 0
+    right := 1000002
+    for left <= right {
+        mid := left + (right-left)/2
+        count := 0
+        for i := 0; i < m; i++ {
+            count += countOnLeft(grid[i], mid)
+        }
+        if count <= half {
+            left = mid+1
+        } else {
+            right = mid-1
+        }
+    }
+    return left
+}
+
+func countOnLeft(arr []int, target int) int {
+    left := 0
+    right := len(arr)-1
+    // find the right most position of target on left of target
+    idx := -1
+    for left <= right {
+        mid := left + (right-left)/2
+        if arr[mid] <= target {
+            idx = mid
+            left = mid+1
+        } else {
+            right = mid-1
+        }
+    }
+    return idx+1
+}
+/*
     approach: brute force
     - create a 1d array
     - sort it
     - return the middle
+    time = o(mn) + o(mn log mn)
+    space = o(mn)
 */
-func matrixMedian(grid [][]int) int {
-    merged := []int{}
-    m := len(grid)
-    n := len(grid[0])
-    mid := (m*n)/2
-    for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
-            merged = append(merged, grid[i][j])
-        }
-    }
-    sort.Ints(merged)
-    return merged[mid]
-}
+// func matrixMedian(grid [][]int) int {
+//     merged := []int{}
+//     m := len(grid)
+//     n := len(grid[0])
+//     mid := (m*n)/2
+//     for i := 0; i < m; i++ {
+//         for j := 0; j < n; j++ {
+//             merged = append(merged, grid[i][j])
+//         }
+//     }
+//     sort.Ints(merged)
+//     return merged[mid]
+// }
