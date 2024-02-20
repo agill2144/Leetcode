7ea4b41f5e644142fef8afa66c07f6231af53e5a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-19 16:33:36 -0800

    Time: 10 ms (91.69%) | Memory: 6.3 MB (70.86%) - LeetSync

--- 268-missing-number/missing-number.go
+++ 268-missing-number/missing-number.go
@@ -1,8 +1,37 @@
-
+/*
+    approach: sum!
+    - take a curr sum of all nums
+    - take the expected sum from 1 to n
+        - n * (n+1)/2
+    - return the diff
+*/
 func missingNumber(nums []int) int {
-    sum := 0
-    for i := 0; i < len(nums); i++ {sum += nums[i]}
+    actualSum := 0
     n := len(nums)
-    expected := n*(n+1)/2
-    return expected-sum
+    for i := 0; i < n; i++ {
+        actualSum += nums[i]
+    }
+    expectedSum := n*(n+1)/2
+    return expectedSum-actualSum
 }
+
+
+/*
+    approach: set
+    - nums are supposed to be within 0 and n ( inclusive )
+    - toss nums in a set so we can easily check whether a ele exists
+    - then run a loop from 0 to n and return the first number we dont find in set
+    time: o(2n)
+    space = o(n)
+*/
+// func missingNumber(nums []int) int {
+//     set := map[int]struct{}{}
+//     n := len(nums)
+//     for i := 0; i < len(nums); i++ {
+//         set[nums[i]] = struct{}{}
+//     }
+//     for i := 0; i <= n; i++ {
+//         if _, ok := set[i]; !ok {return i}
+//     }
+//     return n
+// }
