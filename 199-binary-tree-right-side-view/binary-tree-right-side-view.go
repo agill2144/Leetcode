98e6f374729d4a774f3dc4e1c0241e7a9f0f0ef0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-20 18:09:36 -0800

    Time: 2 ms (64.12%) | Memory: 2.3 MB (88.30%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.go
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.go
@@ -6,23 +6,42 @@
  *     Right *TreeNode
  * }
  */
-func rightSideView(root *TreeNode) []int {
-    if root == nil {return nil}
+
+func rightSideView(root *TreeNode) []int{
     out := []int{}
-    q := []*TreeNode{root}
-    for len(q) != 0 {
-        qSize := len(q)
-        for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            qSize--
-            if qSize == 0 {
-                // this was the last element in the level
-                out = append(out, dq.Val)
-            }
-            if dq.Left != nil {q = append(q, dq.Left)}
-            if dq.Right != nil {q = append(q, dq.Right)}
+    var dfs func(r *TreeNode, depth int)
+    dfs = func(r *TreeNode, depth int) {
+        // base
+        if r== nil {return}
+        // logic
+        if len(out) == depth {
+            out = append(out, r.Val)
         }
+        dfs(r.Right, depth+1)
+        dfs(r.Left, depth+1)
     }
+    dfs(root, 0)
     return out
 }
+// time = o(n)
+// space = o(maxWidth)
+// func rightSideView(root *TreeNode) []int {
+//     if root == nil {return nil}
+//     out := []int{}
+//     q := []*TreeNode{root}
+//     for len(q) != 0 {
+//         qSize := len(q)
+//         for qSize != 0 {
+//             dq := q[0]
+//             q = q[1:]
+//             qSize--
+//             if qSize == 0 {
+//                 // this was the last element in the level
+//                 out = append(out, dq.Val)
+//             }
+//             if dq.Left != nil {q = append(q, dq.Left)}
+//             if dq.Right != nil {q = append(q, dq.Right)}
+//         }
+//     }
+//     return out
+// }
