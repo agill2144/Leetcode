cfc1465ebe4bdfa33a0d4578c32c0e4d4ec96a06
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-26 22:32:50 -0800

    Time: 276 ms (42.86%) | Memory: 26.9 MB (28.57%) - LeetSync

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.go
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.go
@@ -1,59 +1,38 @@
-import (
-	"container/list"
-	"sort"
-)
-
 func minimumOperations(root *TreeNode) int {
-	queue := list.New()
-	queue.PushBack(root)
-
-	result := 0
-
-	for {
-		n := queue.Len()
-		if n == 0 {
-			break
-		}
-		arr := make([]int, 0, n)
-		for i := 0; i < n; i++ {
-			elem := queue.Front()
-            queue.Remove(elem)
-            e := elem.Value.(*TreeNode)
-
-			arr = append(arr, e.Val)
-			if e.Left != nil {
-				queue.PushBack(e.Left)
+	result, queue := 0, []*TreeNode{root}
+	for len(queue) > 0 {
+		size, nums := len(queue), make([]int, 0)
+		for i := 0; i < size; i++ {
+			t := queue[0]
+			queue = queue[1:]
+			nums = append(nums, t.Val)
+			if t.Left != nil {
+				queue = append(queue, t.Left)
 			}
-			if e.Right != nil {
-				queue.PushBack(e.Right)
+			if t.Right != nil {
+				queue = append(queue, t.Right)
 			}
 		}
-        qty := minPermutations(arr)
-        result += qty
+		result += cnt(nums)
 	}
-
 	return result
 }
 
-func minPermutations(arr []int) int {
-	sorted := make([]int, len(arr))
-	copy(sorted, arr)
+func cnt(nums []int) int {
+	sorted := make([]int, len(nums))
+	copy(sorted, nums)
 	sort.Ints(sorted)
-	idxMap := make(map[int]int, len(arr))
-	permutationsQty := 0
-
-	for i := 0; i < len(arr); i++ {
-		idxMap[arr[i]] = i
+	index := make(map[int]int)
+	for i, num := range nums {
+		index[num] = i
 	}
-
-	for i, elem := range arr {
-		if elem != sorted[i] {
-			index := idxMap[sorted[i]]
-			arr[i], arr[index] = arr[index], arr[i]
-			idxMap[arr[i]] = i
-			idxMap[arr[index]] = index
-			permutationsQty++
+	result := 0
+	for i, num := range nums {
+		if num != sorted[i] {
+			nums[i], nums[index[sorted[i]]] = nums[index[sorted[i]]], nums[i]
+			index[nums[i]], index[num] = i, index[sorted[i]]
+			result++
 		}
 	}
-	return permutationsQty
+	return result
 }
