0d4948be576e4d7d3c6154cef497e6bf939f0356
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-26 23:48:11 -0800

    Time: 0 ms (100.00%) | Memory: 4.5 MB (88.27%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func diameterOfBinaryTree(root *TreeNode) int {
+    maxDia := 0
+    var dfs func(r *TreeNode) int
+    dfs = func(r *TreeNode) int {
+        // base
+        if r == nil {return 0}
+
+        // logic
+        left := dfs(r.Left)
+        right := dfs(r.Right)
+        
+        maxDia = max(maxDia, max(left+right, max(left, right)))
+        return max(left,right)+1
+    }
+    dfs(root)
+    return maxDia
+}
