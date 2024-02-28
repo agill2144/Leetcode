118279d7253acea54e103abe40830d8299bb82f5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-02-27 16:26:00 -0800

    Time: 2 ms (63.16%) | Memory: 2.4 MB (75.44%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.go
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.go
@@ -6,6 +6,38 @@
  *     Right *TreeNode
  * }
  */
+
+/*
+    approach: validate bst min/max range pattern
+    - we know our start root ; preorder[0]
+    - left of root in a bst must be less than parent
+        - i.e at max left child value MUST be < parent
+        - i.e max = parent value
+        - and min = no restriction
+    - right of root in a base must be greater than parent
+        - i.e at min right child value MUST be > parent
+        - i.e min = parent value
+        - and max = no restriction
+    - use the min and max to identify whether to place this child or not
+    - recursively build left and right subtrees
+*/
+func bstFromPreorder(preorder []int) *TreeNode {
+    ptr := 0
+    var dfs func(minVal, maxVal int) *TreeNode
+    dfs = func(minVal, maxVal int) *TreeNode {
+        // base
+        if ptr == len(preorder) {return nil}
+        if preorder[ptr] <= minVal || preorder[ptr] >= maxVal {return nil}
+        
+        // logic
+        root := &TreeNode{Val: preorder[ptr]}
+        ptr++
+        root.Left = dfs(minVal, root.Val)
+        root.Right = dfs(root.Val, maxVal)
+        return root
+    }
+    return dfs(math.MinInt64, math.MaxInt64)
+}
  /*
     approach: brute force
     - assuming the preorder is valid for a BST
@@ -17,30 +49,30 @@
     time = o(n) + o(nlogn) + o(n) = o(nlogn)
     space = o(n)
  */
-func bstFromPreorder(preorder []int) *TreeNode {
-    inorder := make([]int, len(preorder))
-    copy(inorder, preorder)
-    sort.Ints(inorder)
-    inorderIdx := map[int]int{}
-    for i := 0; i < len(inorder); i++ {
-        inorderIdx[inorder[i]] = i
-    }
+// func bstFromPreorder(preorder []int) *TreeNode {
+//     inorder := make([]int, len(preorder))
+//     copy(inorder, preorder)
+//     sort.Ints(inorder)
+//     inorderIdx := map[int]int{}
+//     for i := 0; i < len(inorder); i++ {
+//         inorderIdx[inorder[i]] = i
+//     }
     
-    prePtr := 0
-    var dfs func(left, right int) *TreeNode
-    dfs = func(left, right int) *TreeNode {
-        // base
-        if left > right {return nil}
+//     prePtr := 0
+//     var dfs func(left, right int) *TreeNode
+//     dfs = func(left, right int) *TreeNode {
+//         // base
+//         if left > right {return nil}
 
-        // logic
-        rootVal := preorder[prePtr]
-        prePtr++
-        root := &TreeNode{Val: rootVal}
-        rootIdx := inorderIdx[rootVal]
-        root.Left = dfs(left, rootIdx-1)
-        root.Right = dfs(rootIdx+1, right)
-        return root
-    }
-    return dfs(0, len(inorder)-1)
+//         // logic
+//         rootVal := preorder[prePtr]
+//         prePtr++
+//         root := &TreeNode{Val: rootVal}
+//         rootIdx := inorderIdx[rootVal]
+//         root.Left = dfs(left, rootIdx-1)
+//         root.Right = dfs(rootIdx+1, right)
+//         return root
+//     }
+//     return dfs(0, len(inorder)-1)
     
-}
+// }
