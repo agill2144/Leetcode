480bfeb4897bb8b3c821df3b7c8727148ce682a8
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-04 07:29:05 -0800

    Time: 2 ms (75.86%) | Memory: 2.7 MB (51.06%) - LeetSync

---
+++ 46-permutations/permutations.go
@@ -0,0 +1,31 @@
+func permute(nums []int) [][]int {
+    out := [][]int{}
+    var dfs func(path []int)
+    dfs = func(path []int) {
+        // base
+        if len(path) == len(nums) {
+            newP := make([]int, len(path))
+            copy(newP, path)
+            out = append(out, newP)
+            return
+        }
+
+        // logic
+        for i := 0; i < len(nums); i++ {
+            if !contains(path, nums[i]) {
+                path = append(path, nums[i])
+                dfs(path)
+                path = path[:len(path)-1]
+            }
+        }
+    }
+    dfs(nil)
+    return out
+}
+
+func contains(list []int, target int) bool {
+    for _, ele := range list {
+        if ele == target {return true}
+    }
+    return false
+}
