48d181fda5953b02b990427be390107115f65538
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-04 07:49:25 -0800

    Time: 0 ms (100.00%) | Memory: 2.6 MB (99.33%) - LeetSync

--- 46-permutations/permutations.go
+++ 46-permutations/permutations.go
@@ -1,31 +1,53 @@
 func permute(nums []int) [][]int {
     out := [][]int{}
-    var dfs func(path []int)
-    dfs = func(path []int) {
+    var dfs func(start int)
+    dfs = func(start int) {
         // base
-        if len(path) == len(nums) {
-            newP := make([]int, len(path))
-            copy(newP, path)
-            out = append(out, newP)
+        if start == len(nums) {
+            path := make([]int, len(nums))
+            copy(path, nums)
+            out = append(out, path)
             return
         }
 
         // logic
-        for i := 0; i < len(nums); i++ {
-            if !contains(path, nums[i]) {
-                path = append(path, nums[i])
-                dfs(path)
-                path = path[:len(path)-1]
-            }
+        for i := start; i < len(nums); i++ {
+            nums[i], nums[start] = nums[start], nums[i]
+            dfs(start+1)
+            nums[i], nums[start] = nums[start], nums[i]
         }
     }
-    dfs(nil)
+    dfs(0)
     return out
 }
+// func permute(nums []int) [][]int {
+//     out := [][]int{}
+//     var dfs func(path []int)
+//     dfs = func(path []int) {
+//         // base
+//         if len(path) == len(nums) {
+//             newP := make([]int, len(path))
+//             copy(newP, path)
+//             out = append(out, newP)
+//             return
+//         }
 
-func contains(list []int, target int) bool {
-    for _, ele := range list {
-        if ele == target {return true}
-    }
-    return false
-}
+//         // logic
+//         for i := 0; i < len(nums); i++ {
+//             if !contains(path, nums[i]) {
+//                 path = append(path, nums[i])
+//                 dfs(path)
+//                 path = path[:len(path)-1]
+//             }
+//         }
+//     }
+//     dfs(nil)
+//     return out
+// }
+
+// func contains(list []int, target int) bool {
+//     for _, ele := range list {
+//         if ele == target {return true}
+//     }
+//     return false
+// }
