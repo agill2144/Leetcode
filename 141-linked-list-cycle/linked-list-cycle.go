7372e901d5614fee5700cfe4f453fd44383c6f1e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-05 18:09:20 -0800

    Time: 2 ms (96.59%) | Memory: 4.6 MB (83.30%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.go
+++ 141-linked-list-cycle/linked-list-cycle.go
@@ -5,13 +5,28 @@
  *     Next *ListNode
  * }
  */
+
+
 func hasCycle(head *ListNode) bool {
-    m := map[*ListNode]struct{}{}
-    curr := head
-    for curr != nil {
-        if _, ok := m[curr]; ok {return true}
-        m[curr] = struct{}{}
-        curr = curr.Next
+    slow := head
+    fast := head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+        if slow == fast {return true}
     }
     return false
 }
+
+ // time = o(n)
+ // space = o(n)
+// func hasCycle(head *ListNode) bool {
+//     m := map[*ListNode]struct{}{}
+//     curr := head
+//     for curr != nil {
+//         if _, ok := m[curr]; ok {return true}
+//         m[curr] = struct{}{}
+//         curr = curr.Next
+//     }
+//     return false
+// }
