4d2bc96b605d510e6e7934ee4bddb1fa804517c6
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-05 20:35:50 -0800

    Time: 23 ms (50.00%) | Memory: 1.9 MB (79.17%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.go
+++ 526-beautiful-arrangement/beautiful-arrangement.go
@@ -1,3 +1,17 @@
+/*
+    do this after permutations question
+
+    approach: explore all permutations    
+    - while building permutation in-place
+    - check if the newely swapped element works
+        - i.e element at start position works 
+        - if nums[start] % start + 1 || start+1 % nums[start]
+    - only then recurse
+    - finally backtrack ( undo the swap )
+
+    time = o(n!)
+    space = o(n)
+*/
 func countArrangement(n int) int {
     nums := []int{}
     for i := 0; i < n; i++ {nums = append(nums,i+1)}
