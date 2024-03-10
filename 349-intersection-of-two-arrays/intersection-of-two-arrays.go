4d518e2c232f5d1ee7dae5faaf90b3b05505b892
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-09 20:19:55 -0800

    Time: 3 ms (73.43%) | Memory: 2.9 MB (39.85%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.go
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.go
@@ -1,22 +1,61 @@
-// hash hash
 func intersection(nums1 []int, nums2 []int) []int {
     if len(nums2) < len(nums1) {return intersection(nums2, nums1)}
     // nums1 is always smaller
-    n1Set := map[int]struct{}{}
+
+    // time = o(n1)
+    // space = o(n1)
+    n1Map := map[int]bool{}
     for i := 0; i < len(nums1); i++ {
-        n1Set[nums1[i]] = struct{}{}
+        n1Map[nums1[i]] = false // false = not added to our output yet
     }
-    outSet := map[int]struct{}{}
+
+    // time = o(n2)
+    out := []int{}
     for i := 0; i < len(nums2); i++ {
         num := nums2[i]
-        if _, ok := outSet[num]; ok {continue}
-        if _, ok := n1Set[num]; ok {
-            outSet[num] = struct{}{}
+        if used, ok := n1Map[num]; ok && !used {
+            out = append(out, num)
+            n1Map[num] = true // used, added to output, cannot be re-used
         }
     }
-    out := []int{}
-    for k, _ := range outSet {
-        out = append(out, k)
-    }
     return out
+
+    // time = o(n1) + o(n2) + o(n2) = o(n1+n2)
+    // space = o(n1) + o(n2)
 }
+
+
+// hash hash
+// n1 = len(nums1) 
+// n2 = len(nums2)
+// func intersection(nums1 []int, nums2 []int) []int {
+//     if len(nums2) < len(nums1) {return intersection(nums2, nums1)}
+//     // nums1 is always smaller
+
+//     // time = o(n1)
+//     // space = o(n1)
+//     n1Set := map[int]struct{}{}
+//     for i := 0; i < len(nums1); i++ {
+//         n1Set[nums1[i]] = struct{}{}
+//     }
+
+//     // time = o(n2)
+//     // space = o(n2) ; worst case n1 == n2 ( identical elements )
+//     outSet := map[int]struct{}{}
+//     for i := 0; i < len(nums2); i++ {
+//         num := nums2[i]
+//         if _, ok := outSet[num]; ok {continue}
+//         if _, ok := n1Set[num]; ok {
+//             outSet[num] = struct{}{}
+//         }
+//     }
+//     out := []int{}
+//     // time = o(n2)
+//     for k, _ := range outSet {
+//         out = append(out, k)
+//     }
+//     return out
+
+//     // time = o(n1) + o(n2) + o(n2) = o(n1+n2)
+//     // space = o(n1) + o(n2)
+// }
