446ed0b9941a7ed5f02c41b96c33e66083eb0634
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-09 18:15:06 -0800

    Time: 1 ms (79.79%) | Memory: 2.1 MB (86.70%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.go
+++ 807-custom-sort-string/custom-sort-string.go
@@ -1,24 +1,49 @@
+/*
+    we want to sort s based on a custom "order"
+    therefore we should collect all chars from order string that also exist in "s"
+    because order matters, chars in $order must be placed first
+    
+    appraoch: using a hashset
+    - store chars of s in a set
+    - loop thru the order string to collect chars that should be placed first
+    - if the order[i] does exist in s set, take it and add it to our output
+        - delete this char from set
+        - so that we know for sure its used
+    - then loop over s string one last time, to collect remaining chars that did not exist in $order
+    - there is a possibility that not all chars of $s exist in $order,
+    - therefore we must also preserve the order of chars in $s that do not exist in $order
+    
+    s = len(s)
+    t = len(order)
+    
+    time = o(s) + o(t) + o(s)
+    space = o(s)
+*/
 func customSortString(order string, s string) string {
-    sFreq := map[byte]int{}
+    
+    // make the chars of $s searchable 
+    freqMap := map[byte]int{}
     for i := 0; i < len(s); i++ {
-        sFreq[s[i]]++
+        freqMap[s[i]]++
     }
-    ptr := 0
+    
     out := new(strings.Builder)
-    for ptr < len(order) {
-        char := order[ptr]
-        times := sFreq[char]
-        for times > 0 {
+    // collect the chars from order that also exist in $s
+    for i := 0; i < len(order); i++ {
+        char := order[i]
+        count := freqMap[char]
+        for count > 0 {
             out.WriteByte(char)
-            times--
-        } 
-        delete(sFreq, char)
-        ptr++
+            count--
+        }
+        delete(freqMap, char)
     }
-    for k, v := range sFreq {
-        for v > 0 {
-            out.WriteByte(k)
-            v--
+    
+    // collect the remaining left over chars from $s that did not exist in $order
+    for i := 0; i < len(s); i++ {
+        count := freqMap[s[i]]
+        if count > 0 {
+            out.WriteByte(s[i])
         }
     }
     return out.String()
