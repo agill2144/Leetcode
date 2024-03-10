aad30fd0d7d7ac3064da4bfe05cc70cf8ba782ad
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-09 20:22:15 -0800

    Time: 4 ms (44.47%) | Memory: 2.6 MB (100.00%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.go
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.go
@@ -1,28 +1,50 @@
+// sort sort
 func intersection(nums1 []int, nums2 []int) []int {
-    if len(nums2) < len(nums1) {return intersection(nums2, nums1)}
-    // nums1 is always smaller
-
-    // time = o(n1)
-    // space = o(n1)
-    n1Map := map[int]bool{}
-    for i := 0; i < len(nums1); i++ {
-        n1Map[nums1[i]] = false // false = not added to our output yet
-    }
-
-    // time = o(n2)
+    sort.Ints(nums1)
+    sort.Ints(nums2)
     out := []int{}
-    for i := 0; i < len(nums2); i++ {
-        num := nums2[i]
-        if used, ok := n1Map[num]; ok && !used {
-            out = append(out, num)
-            n1Map[num] = true // used, added to output, cannot be re-used
+    n1, n2 := 0, 0
+    for n1 < len(nums1) && n2 < len(nums2) {
+        if nums1[n1] == nums2[n2] {
+            out = append(out,nums1[n1])
+            n1++
+            n2++
+            for n1 < len(nums1) && n2 < len(nums2) && nums1[n1] == nums1[n1-1] {n1++}
+            for n1 < len(nums1) && n2 < len(nums2) && nums2[n2] == nums2[n2-1] {n2++}
+        } else if nums1[n1] < nums2[n2] {
+            n1++
+        } else {
+            n2++
         }
     }
     return out
-
-    // time = o(n1) + o(n2) + o(n2) = o(n1+n2)
-    // space = o(n1) + o(n2)
 }
+
+// func intersection(nums1 []int, nums2 []int) []int {
+//     if len(nums2) < len(nums1) {return intersection(nums2, nums1)}
+//     // nums1 is always smaller
+
+//     // time = o(n1)
+//     // space = o(n1)
+//     n1Map := map[int]bool{}
+//     for i := 0; i < len(nums1); i++ {
+//         n1Map[nums1[i]] = false // false = not added to our output yet
+//     }
+
+//     // time = o(n2)
+//     out := []int{}
+//     for i := 0; i < len(nums2); i++ {
+//         num := nums2[i]
+//         if used, ok := n1Map[num]; ok && !used {
+//             out = append(out, num)
+//             n1Map[num] = true // used, added to output, cannot be re-used
+//         }
+//     }
+//     return out
+
+//     // time = o(n1) + o(n2) + o(n2) = o(n1+n2)
+//     // space = o(n1) + o(n2)
+// }
 
 
 // hash hash
