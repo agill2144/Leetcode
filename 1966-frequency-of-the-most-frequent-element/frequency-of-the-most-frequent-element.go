ee5ad08c407b4fc970f73db68863716e4d2b4fff
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-11 19:44:22 -0400

    Time: 206 ms (25.64%) | Memory: 9.2 MB (33.33%) - LeetSync

---
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.go
@@ -0,0 +1,28 @@
+func maxFrequency(nums []int, k int) int {
+    sort.Ints(nums)
+    prefixSum := make([]int, len(nums))
+    prefixSum[0] = nums[0]
+    for i := 1; i < len(nums); i++ {
+        prefixSum[i] = prefixSum[i-1]+nums[i]
+    }
+    maxFreq := 0
+    for i := 0; i < len(nums); i++ {
+        left := 0
+        right := i
+        target := nums[i]
+        for left <= right {
+            mid := left + (right-left)/2
+            windowSize := i-mid+1
+            desiredSum := windowSize * target
+            actualSum := prefixSum[i]-prefixSum[mid] + nums[mid]
+            ops := desiredSum - actualSum
+            if ops <= k {
+                maxFreq = max(maxFreq, windowSize)
+                right = mid-1
+            } else {
+                left = mid+1
+            }
+        }
+    }
+    return maxFreq
+}
