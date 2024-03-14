34447c7f7797e1ed4bf55734ad96b3d553c90266
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-14 19:25:56 -0400

    Time: 14 ms (5.41%) | Memory: 3.3 MB (63.53%) - LeetSync

---
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.go
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeZeroSumSublists(head *ListNode) *ListNode {
+    dummy := &ListNode{Val: 0}
+    dummy.Next = head
+    curr := head
+    rSum := 0
+    sumToNode := map[int]*ListNode{0:dummy}
+    
+    for curr != nil {
+        next := curr.Next
+        rSum += curr.Val
+        prev, ok := sumToNode[rSum]
+        if ok {
+            start := prev.Next
+            for start != curr {
+                for s, n := range sumToNode {
+                    if n == start {
+                        delete(sumToNode, s)
+                        break
+                    }
+                }
+                start = start.Next
+            }
+            prev.Next = next
+            curr.Next = nil
+        } else {
+            sumToNode[rSum] = curr
+        }
+        curr = next
+    }
+    return dummy.Next
+}
