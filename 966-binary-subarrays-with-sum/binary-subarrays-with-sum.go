d7035ed7d80f9ca3e79f6a215a49ef0cdd41db23
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-14 19:06:14 -0400

    Time: 33 ms (35.29%) | Memory: 7.9 MB (17.65%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
@@ -1,11 +1,15 @@
 func numSubarraysWithSum(nums []int, goal int) int {
     count := 0
+    rSum := 0
+    sumCount := map[int]int{0:1}
     for i := 0; i < len(nums); i++ {
-        sum := 0
-        for j := i; j < len(nums); j++ {
-            sum += nums[j]
-            if sum == goal {count++}
+        rSum += nums[i]
+        diff := rSum - goal
+        val, ok := sumCount[diff]
+        if ok {
+            count += val
         }
+        sumCount[rSum]++
     }
     return count
 }
