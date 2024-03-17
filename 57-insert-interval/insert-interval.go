b3ad9c5f06bcb5c2d4f9e1a5e992916c437880d3
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-16 21:28:34 -0400

    Time: 12 ms (5.09%) | Memory: 6.5 MB (5.22%) - LeetSync

---
+++ 57-insert-interval/insert-interval.go
@@ -0,0 +1,45 @@
+func insert(intervals [][]int, newInterval []int) [][]int {
+    if len(intervals) == 0 {
+        return [][]int{newInterval}
+    }
+
+    sort.Slice(intervals, func(i,j int)bool{
+        return intervals[i][0] < intervals[j][0]
+    })
+    left := 0
+    right := len(intervals)-1
+    idx := -1
+    for left <= right {
+        mid := left + (right-left)/2
+        val := intervals[mid][0]
+        valOnLeft := val <= newInterval[0]
+        if valOnLeft {
+            idx = mid
+            left = mid+1
+        } else {
+            right = mid-1
+        }
+    }
+    idx++
+    // case1: no valid idx found
+    // case2: valid idx found
+
+    tmp := [][]int{}
+    i := 0
+    for i < idx {tmp = append(tmp, intervals[i]);i++}
+    tmp = append(tmp, newInterval)
+    for i < len(intervals) {tmp = append(tmp, intervals[i]); i++}
+    fmt.Println(idx,tmp)
+
+    // merge intervals code 
+    out := [][]int{}
+    for i := 0; i < len(tmp); i++ {
+        if len(out) != 0 && tmp[i][0] <= out[len(out)-1][1] {
+            out[len(out)-1][0] = min(tmp[i][0], out[len(out)-1][0])
+            out[len(out)-1][1] = max(tmp[i][1], out[len(out)-1][1])
+        } else {
+            out = append(out, tmp[i])
+        }
+    }
+    return out
+}
