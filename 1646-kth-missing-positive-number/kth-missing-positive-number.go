3fcd21bd9d876d3e19ec9f2c87aeb14f4f4e10c9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-18 22:38:28 -0700

    Time: 4 ms (65.88%) | Memory: 2.9 MB (32.94%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.go
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.go
@@ -1,3 +1,19 @@
+/*
+    approach: linear scan / two ptrs
+    - maintain a ptr to keep track of "potential" missing number
+    - another ptr in input arr
+    - if 2 pointers value are not same;
+        - it means we have ran into a missing number
+        - increment a missing counter by 1
+        - if our missing counter == k; we have seen exactly k missing elements
+            - meaning the current value our ptr is at, is the missing number
+        - otherwise, increment ptr by 1
+    - otherwise, we have same value, meaning nothing is missing
+        - move both ptrs by 1
+
+    time = o(n)
+    space = o(1)
+*/
 func findKthPositive(arr []int, k int) int {
     mc := 0
     ptr := 1
@@ -6,6 +22,8 @@ func findKthPositive(arr []int, k int) int {
         if ptr != arr[i] {
             mc++
             if mc == k {
+                // dry run this on board to understand better
+                // make k outside of arr, while arr is missing some numbers
                 return ptr+(k-mc)
             }
             ptr++
@@ -14,5 +32,8 @@ func findKthPositive(arr []int, k int) int {
             i++
         }
     }
+    // when nothing was missing in arr, our ptr value went ahead by 1, when we were at the last
+    // therefore the -1 at the end
+    // dry run on board to see better
     return ptr+(k-mc)-1
 }
