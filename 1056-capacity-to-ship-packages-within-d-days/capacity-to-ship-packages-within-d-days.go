1b82cac470f2fa0c99fc1ee2ee24c525183a7f17
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-18 22:14:29 -0700

    Time: 38 ms (72.78%) | Memory: 8.1 MB (6.51%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.go
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.go
@@ -2,31 +2,32 @@ func shipWithinDays(weights []int, days int) int {
     left := math.MinInt64
     right := 0
     for i := 0; i < len(weights); i++ {
+        if weights[i] > left {left = weights[i]}
         right += weights[i]
-        left = max(left, weights[i])
     }
     ans := -1
     for left <= right {
         mid := left + (right-left)/2
-        // if at max, we are allowed to ship $mid weights in a day
-        // how many days will it take to ship all packages
-        // remember; packages must be shipped in the same order as they are given in
-        runningWeight := 0
-        count := 1
-        for j := 0; j < len(weights); j++ {
-            runningWeight += weights[j]
-            if runningWeight > mid {
-                runningWeight = weights[j]
-                count++
+        // if atMax, we can take $mid weight per day
+        // how many days would it take to take all packages
+
+        daysCount := 1
+        rSum := 0
+        for i := 0; i < len(weights); i++ {
+            rSum += weights[i]
+            if rSum > mid {
+                daysCount++
+                rSum = weights[i]
             }
         }
 
-        // when does mid work?
-        // mid works, if we were able to ship within $days
-        if count <= days {
-            // save this as a potential ans
+        // did mid work?
+        // mid works if it took less or equal to days allowed
+        // to ship all packages
+        if daysCount <= days {
+            // save this as potential ans
             ans = mid
-            // keep searching left, since we want smallest such ans
+            // look left, since we are searching for smallest such ans
             right = mid-1
         } else {
             left = mid+1
@@ -34,3 +35,31 @@ func shipWithinDays(weights []int, days int) int {
     }
     return ans
 }
+
+// func shipWithinDays(weights []int, days int) int {
+//     start := math.MinInt64
+//     end := 0
+//     for i := 0; i < len(weights); i++ {
+//         if weights[i] > start {start = weights[i]}
+//         end += weights[i]
+//     }
+
+//     for i := start ; i <= end; i++ {
+//         capacity := i // atMax capacity, cannot exceed
+
+//         // how many days would it take if atMax our capacity is $capacity
+//         totalDays := 1
+//         rSum := 0
+//         for j := 0; j < len(weights); j++ {
+//             rSum += weights[j]
+//             if rSum > capacity {
+//                 totalDays++
+//                 rSum = weights[j]
+//             }
+//         }
+//         if totalDays <= days {
+//             return capacity
+//         }
+//     }
+//     return -1
+// }
