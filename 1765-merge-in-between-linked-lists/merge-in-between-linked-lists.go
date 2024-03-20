899b65dd040b86ea3933d4855748d5187ed05c76
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-19 21:30:49 -0700

    Time: 90 ms (81.51%) | Memory: 8.2 MB (18.49%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.go
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.go
@@ -15,20 +15,18 @@ func mergeInBetween(list1 *ListNode, a int, b int, list2 *ListNode) *ListNode {
     // its not count from 1, its count from 0 :face_palm
     idx := 0
     for curr != nil && (athNode == nil || bthNode == nil) {
-        if idx == a {
-            athNode = prev
-        }
-        if idx == b {
-            bthNode = curr.Next
-        }
+        next := curr.Next
+        // we are removing nodes from idx a to b ( including ath idx and bth idx)
+        // thats why we need a's prev, and b's next
+        if idx == a { athNode = prev }
+        if idx == b { bthNode = next }
         idx++
         prev = curr
         curr = curr.Next
     }
-    fmt.Println(athNode, bthNode)
-    tailOfList2 := list2
-    for tailOfList2.Next != nil {tailOfList2 = tailOfList2.Next}
+    tail := list2
+    for tail.Next != nil {tail = tail.Next}
     athNode.Next = list2
-    tailOfList2.Next = bthNode
+    tail.Next = bthNode
     return list1
 }
