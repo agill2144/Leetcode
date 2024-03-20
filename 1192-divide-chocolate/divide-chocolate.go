469aeac1aacd6e2f0acbde0379b8941362362808
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-20 14:00:37 -0700

    Time: 7 ms (95.00%) | Memory: 6.3 MB (60.00%) - LeetSync

--- 1192-divide-chocolate/divide-chocolate.go
+++ 1192-divide-chocolate/divide-chocolate.go
@@ -1,33 +1,70 @@
 func maximizeSweetness(sweetness []int, k int) int {
-    start := math.MaxInt64
-    end := 0
+    // min ans is maximized
+    left := 1
+    right := 0
     for i := 0; i < len(sweetness); i++ {
-        end += sweetness[i]
-        start = min(start, sweetness[i])
+        right += sweetness[i]
     }
     ans := -1
-    left := start
-    right := end
     for left <= right {
-        // atMin sweetness level to be met
+        // if a chunk at-min is allowed $mid sweetness
+        // how many chunks can we get ( all chunks must satisfy atMin sweetness level )
         mid := left + (right-left)/2
-        count := 0
+
+        subArrCount := 0
         sum := 0
-        for j := 0; j < len(sweetness); j++ {
-            sum += sweetness[j]
+        for i := 0; i < len(sweetness); i++ {
+            sum += sweetness[i]
+            // if atMin is met, cut the bar here
             if sum >= mid {
-                count++
+                subArrCount++
                 sum = 0
             }
         }
-        // when does mid not work ?
-        // when there are less than k+1 chunks
-        if count < k+1 {
-            right = mid-1
-        } else {
+
+        // did $mid work?
+        // $mid will only work if we had atleast k+1 chunks with $atMin sweetness level
+        // why k+1 ? because we have to include ourselves, i.e there are k+1 total people and not k..
+        if subArrCount >= k+1 {
+            // we have atleast k+1 chunks that meet $atMin sweetness level
+            // save this ans as potentianl ans
             ans = mid
+            // search right, because we want largest such ans
             left = mid+1
+        } else {
+            right = mid-1
         }
     }
     return ans
+
 }
+// func maximizeSweetness(sweetness []int, k int) int {
+//     // min ans is maximized
+//     start := 1
+//     end := 0
+//     for i := 0; i < len(sweetness); i++ {
+//         end += sweetness[i]
+//     }
+//     ans := -1
+//     for i := start; i <= end; i++ {
+//         atMin := i
+//         subarrayCount := 0
+//         sum := 0
+//         for j := 0; j < len(sweetness); j++ {
+//             sum += sweetness[j]
+//             if sum >= atMin {
+//                 subarrayCount++
+//                 sum = 0
+//             }
+//         }
+
+//         // its okay to have more than k+1 chunks, we can apparently throw away extra left overs :shrug
+//         // thats why >= k+1 and not == k+1
+//         if subarrayCount >= k+1 {
+//             ans = atMin
+//         } else {
+//             break
+//         }
+//     }
+//     return ans
+// }
