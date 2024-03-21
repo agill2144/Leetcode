a6f291693a70ae0f7692e7af7b24c4a5f7d77ef4
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-20 17:25:48 -0700

    Time: 0 ms (100.00%) | Memory: 2.7 MB (21.41%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.go
+++ 206-reverse-linked-list/reverse-linked-list.go
@@ -5,50 +5,23 @@
  *     Next *ListNode
  * }
  */
-// func reverseList(head *ListNode) *ListNode {
-//     curr := head
-//     var prev *ListNode
-//     for curr != nil {
-//         next := curr.Next
-//         curr.Next = prev
-//         prev = curr
-//         curr = next
-//     }
-//     return prev
-// }
 
-
- // dfs
-// func reverseList(head *ListNode) *ListNode {
-//     var dfs func(curr, prev *ListNode)
-//     var newHead *ListNode
-//     dfs = func(curr, prev *ListNode) {
-//         // base
-//         if curr == nil {
-//             newHead = prev
-//             return
-//         }
-
-//         // logic
-//         dfs(curr.Next, curr)
-//         curr.Next = prev
-//         if prev != nil {
-//             prev.Next = nil
-//         }
-//     }
-//     dfs(head, nil)
-//     return newHead
-// }
-
+/*
+    approach: classic
+    - point to prev
+    - prev at the end becomes the new 
+    time = o(n)
+    space = o(1)
+*/
 func reverseList(head *ListNode) *ListNode {
-    // base
     if head == nil || head.Next == nil {return head}
-
-    // logic
-    result := reverseList(head.Next)
-    head.Next.Next = head
-    head.Next = nil
-    
-    return result
-
+    curr := head
+    var prev *ListNode
+    for curr != nil {
+        next := curr.Next
+        curr.Next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
 }
