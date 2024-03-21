b674618013026cd026aab46b0c18e9e81ccbeba4
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-20 22:15:55 -0700

    Time: 3 ms (44.56%) | Memory: 3.7 MB (74.12%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.go
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.go
@@ -8,6 +8,43 @@
  */
 
 /*
+
+*/
+func copyRandomList(head *Node) *Node {
+    if head == nil {return head}
+    oldToNew := map[*Node]*Node{}
+    curr := head
+    out := &Node{Val: -1}
+    tail := out
+    for curr != nil {
+        next := curr.Next
+        
+        // does new node exists ?
+        // could happen if random node was created first
+        node, ok := oldToNew[curr]
+        if !ok {
+            node = &Node{Val: curr.Val}
+            oldToNew[curr] = node
+        }
+        tail.Next = node
+        tail = tail.Next
+
+        // does this node have a random ptr ?
+        if curr.Random != nil {
+            randomNode, ok := oldToNew[curr.Random]
+            if !ok {
+                randomNode = &Node{Val: curr.Random.Val}
+                oldToNew[curr.Random] = randomNode
+            }
+            node.Random = randomNode
+        }
+
+        curr = next
+    }
+    return out.Next
+}
+
+/*
     approach: use hashmap
     - while creating a new linkedlist
     - keep a mapping of old node to new node in a hashmap
@@ -18,29 +55,29 @@
     time = o(2n)
     space = o(n)
 */
-func copyRandomList(head *Node) *Node {
-    if head == nil  {return head}
-    oldToNew := map[*Node]*Node{}
-    out := &Node{Val: -1}
-    tail := out
-    curr := head
-    for curr != nil {
-        newNode := &Node{Val: curr.Val}
-        tail.Next = newNode
-        tail = tail.Next
-        oldToNew[curr] = newNode
-        curr = curr.Next
-    }
+// func copyRandomList(head *Node) *Node {
+//     if head == nil  {return head}
+//     oldToNew := map[*Node]*Node{}
+//     out := &Node{Val: -1}
+//     tail := out
+//     curr := head
+//     for curr != nil {
+//         newNode := &Node{Val: curr.Val}
+//         tail.Next = newNode
+//         tail = tail.Next
+//         oldToNew[curr] = newNode
+//         curr = curr.Next
+//     }
 
-    curr = head
-    for curr != nil {
-        if curr.Random != nil {
-            random := curr.Random
-            deepCp := oldToNew[curr]
-            deepCpRandom := oldToNew[random]
-            deepCp.Random = deepCpRandom
-        }
-        curr = curr.Next
-    }
-    return out.Next
-}
+//     curr = head
+//     for curr != nil {
+//         if curr.Random != nil {
+//             random := curr.Random
+//             deepCp := oldToNew[curr]
+//             deepCpRandom := oldToNew[random]
+//             deepCp.Random = deepCpRandom
+//         }
+//         curr = curr.Next
+//     }
+//     return out.Next
+// }
