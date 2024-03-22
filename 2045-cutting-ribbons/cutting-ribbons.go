c3de1084dfd6298b7cf074adb5405623a2c6945a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-21 20:42:15 -0700

    Time: 124 ms (100.00%) | Memory: 7.8 MB (100.00%) - LeetSync

--- 2045-cutting-ribbons/cutting-ribbons.go
+++ 2045-cutting-ribbons/cutting-ribbons.go
@@ -1,52 +1,91 @@
 func maxLength(ribbons []int, k int) int {
+    right := math.MinInt64
     left := 1
-    right := math.MaxInt64
     for i := 0; i < len(ribbons); i++ {
         right = max(right, ribbons[i])
     }
     ans := 0
     for left <= right {
-        mid := left + (right-left)/2
-
+        mid := left + (right-left) / 2
+        // if the answer to evaluate is mid ( size of ribbon is mid )
+        // how many ribbons of the same size can we get ?
         count := 0
-        for j := 0; j < len(ribbons); j++ {
-            count += (ribbons[j]/mid)
+        for i := 0; i < len(ribbons); i++ {
+            count += (ribbons[i]/mid)
         }
-        // when does mid work ?
-        // if we were able to collect atleast k ribbons
+        // when does mid work?
+        // if we were able to create atleast ( or more ) than k ribbons
+        // mid works
         if count >= k {
+            // save this as a potential ans
             ans = mid
-            // but keep searching right, since we want max such ans
+            // but keep searching right, since we want the highest such answe
             left = mid+1
         } else {
-            // our desired size is too big, search left and our desired size of a ribbon smaller
             right = mid-1
         }
     }
     return ans
 }
 
-// tle
+/*
+    approach: linear scan over a range of possible answers
+    - we have ribbons, of a specific size
+    - we can cut a ribbon into many pieces
+    - after cutting a ribbon, we will have some pieces of different lenghts ( depending on where we cut )
+    - we want to find out, the max size of a cut we can have which results into atleast k ribbons
+        - we can collect several k from same ribbon
+        - and we can also collect k from different ribbons
+    - we want the max such lenght
+    - so, then lets be greedy and start with highest possible length we can get
+        - i.e max size of a ribbon
+        - [9,7,5] = 9 ; k=3
+    - then at each element, we will ask, how many 9 sized ribbons can i get at this ith ribbob
+        - how many 9's make up this ith element
+        - ithElement/9
+        - 9/9 = 1 ( meaning we can have 1 ribbon this element of size 9 )
+        - 7/9 = 0 ( meaning we cannot get a ribbon of size 9 from this element )
+        - 5/9 = 0 ( meaning we cannot get a ribbon of size 9 from this element )
+        - we could only collect 1 ribbon of size 9 if our desired size was 9.
+        - we need atleast ( at min ) k such ribbons of desired size ;
+    - lets try 8
+        - 9/8 = 1 
+        - 7/8 = 0
+        - 5/8 = 0
+        - didnt work, could not collect k=3 ribbons of size 8
+    - lets try 7 ( results into 2 ; not equal to k )
+    - lets try 6 ( results into 2 ; not equal to k )
+    - lets try 5
+        - 9/5 = 1 ( we can get 1 ribbon out of 9 of size 5 )
+        - 7/5 = 1 ( we can get 1 ribbon out of 7 of size 5 )
+        - 5/5 = 1 ( we can get 1 ribbon out of 5 of size 5 )
+        - we got 3 total ribbons of size 5
+        - this meets our k constraint and is the max possible ans
+        - therefore we can return 5        
+    - we can keep going until size 1
+        - this is our last valid size as a potential answer
+
+    - the key takeway was;
+        - answer lies within a range
+        - that range is from 1 ( smallest possible answer ) till 9 ( max possible answer )
+
+    time = o( max(ribbons) * n )
+    space = o(1)
+*/
 // func maxLength(ribbons []int, k int) int {
+//     maxSize := math.MinInt64
 //     start := 1
-//     end := math.MinInt64
 //     for i := 0; i < len(ribbons); i++ {
-//         end = max(end, ribbons[i])
+//         maxSize = max(maxSize, ribbons[i])
 //     }
 
-//     for i := end; i >= start; i-- {
-//         size := i
+//     for i := maxSize; i >= start; i-- {
+//         desiredSize := i
 //         count := 0
-//         // fmt.Println("desired size: ", size)
 //         for j := 0; j < len(ribbons); j++ {
-//             // fmt.Println("we could collect ", ribbons[j]/size, " ribbons from ", ribbons[j])
-//             count += (ribbons[j]/size)
+//             count += (ribbons[j] / desiredSize)
 //         }
-//         // when does size work
-//         // when we have collected k ribbons
-//         if count >= k {
-//             return size
-//         }
+//         if count >= k {return desiredSize}
 //     }
 //     return 0
 // }
