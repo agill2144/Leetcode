b3610417409618565e6d55f5474f820fccd831d6
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-22 20:30:53 -0700

    Time: 68 ms (100.00%) | Memory: 8.6 MB (66.08%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.go
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.go
@@ -1,34 +1,31 @@
 func largestRectangleArea(heights []int) int {
-    type stackNode struct {
-        idx int
-        val int
-    }
+   
     nsl := make([]int, len(heights))
     nsr := make([]int, len(heights))
-    st := []stackNode{}
+    st := []int{}
     for i := 0; i < len(heights); i++ {
         h := heights[i]
-        for len(st) != 0 && st[len(st)-1].val >= h {
+        for len(st) != 0 && heights[st[len(st)-1]] >= h {
             st = st[:len(st)-1]
         }
         if len(st) != 0 {
-            nsl[i] = st[len(st)-1].idx+1
+            nsl[i] = st[len(st)-1]+1
         }
-        st = append(st, stackNode{i,h})
+        st = append(st, i)
     }
 
-    st = []stackNode{}
+    st = []int{}
     for i := len(heights)-1; i >= 0; i-- {
         h := heights[i]
-        for len(st) != 0 && st[len(st)-1].val >= h {
+        for len(st) != 0 && heights[st[len(st)-1]] >= h {
             st = st[:len(st)-1]
         }
         if len(st) != 0 {
-            nsr[i] = st[len(st)-1].idx-1
+            nsr[i] = st[len(st)-1]-1
         } else {
             nsr[i] = len(heights)-1
         }
-        st = append(st, stackNode{i, h})
+        st = append(st, i)
     }
     ans := math.MinInt64
     for i := 0; i < len(heights); i++ {
