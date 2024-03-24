bfd4b4e48ee5af1627623162297c96465593ea06
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-24 15:21:12 -0700

    Time: 1022 ms (11.49%) | Memory: 45 MB (91.73%) - LeetSync

---
+++ 127-word-ladder/word-ladder.java
@@ -0,0 +1,59 @@
+class Solution {
+    Set<String> visited = new HashSet<>();
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        //start word can not be end word
+        if(beginWord == null || beginWord.length() == 0 || endWord == null || endWord.length() == 0) return 0;
+
+        Queue<String> q = new LinkedList<>();
+        int size = 0;
+        int tseq = 1;
+        q.add(beginWord); //hit
+        visited.add(beginWord);
+       
+        /*
+            q = [a, b]
+            visited = {a, b}
+        */
+
+        while(!q.isEmpty()){
+            size = q.size();
+            while(size>0){
+                String parent = q.poll();
+                boolean exist = false;
+                for(int i=0; i<wordList.size(); i++){
+                    String child = wordList.get(i);
+                    if(!visited.contains(child) && compareWord(parent, child)) {
+                        visited.add(child); //hot
+                        q.add(child);
+                        if(child.equals(endWord)){
+                            return tseq+1;
+                        }
+                    }
+                }
+                size --;
+            }
+            //level ended
+            tseq++;
+            // set.removeAll(); //Emptying Visited set at every level (need to revisit)
+        }
+        return 0;
+    }
+
+    private boolean compareWord(String beginWord, String currWord){ //hit hot emptySet
+        int diff = 0;
+
+        if(beginWord.length() != currWord.length() )
+            return false;
+        
+        int len = beginWord.length();
+        for(int i=0; i<len; i++ ){
+            if( beginWord.charAt(i) != currWord.charAt(i) ){
+                diff++;
+
+                if(diff > 1)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
