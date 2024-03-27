ac9d04a61c869993c24f35cd32fef4b6e35b66a1
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-26 21:59:50 -0700

    Time: 3 ms (67.32%) | Memory: 2.7 MB (22.44%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.go
+++ 74-search-a-2d-matrix/search-a-2d-matrix.go
@@ -1,10 +1,48 @@
+/*
+    approach: binary search within each row
+    - loop over all the rows
+    - if our target lies in a ith row 
+        - then binary search in this row ( n elements )
+        - therefore; logn
+    
+    total time = o(m) + logn
+    space = o(1)
+*/
 func searchMatrix(matrix [][]int, target int) bool {
     m := len(matrix)
     n := len(matrix[0])
     for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
-            if matrix[i][j] == target {return true}
+        if target >= matrix[i][0] && target <= matrix[i][n-1] {
+            left := 0
+            right := n-1
+            // vanilla binary search on a 1D array
+            for left <= right {
+                mid := left + (right-left)/2
+                if matrix[i][mid] == target {return true}
+                if target > matrix[i][mid] {
+                    left = mid+1
+                } else {
+                    right = mid-1
+                }
+            }
         }
     }
     return false
 }
+/*
+    approach: brute force
+    - linear scan the entire matrix
+
+    time = o(mn)
+    space = o(1)
+*/
+// func searchMatrix(matrix [][]int, target int) bool {
+//     m := len(matrix)
+//     n := len(matrix[0])
+//     for i := 0; i < m; i++ {
+//         for j := 0; j < n; j++ {
+//             if matrix[i][j] == target {return true}
+//         }
+//     }
+//     return false
+// }
