9ccfde75233319c83348765ad82636052f4bc073
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-31 18:58:20 -0700

    Time: 110 ms (100.00%) | Memory: 10 MB (28.95%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.go
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.go
@@ -1,3 +1,37 @@
+/*
+    - similar to finding peak element in a 1D array
+    - a cell is a peak element, if its bigger than its 4 adj neighbors
+    - in the 1d array, we used binary search to find the peak element
+    - how can we use binary search in this matrix to find peak element?
+    - we WILL keep the same intuition as finding peak in a 1D array
+    - if mid element is not peak, take binary search towards the higher element
+    
+    approach: binary search
+    - binary search on cols, randomly
+    - use binary search mid idx, to evaluate a col
+    - we want peak element from this col, and for each cell
+        - we have to check 4 adj cells to check if its peak
+    - instead of checking each cell, 
+    - our best bet is to only evaluate the max number present in this col
+    - which means, find the max element in this col
+    - finding and use max element from a col, takes care of 2 adj neighbors automatically
+        - top and bottom
+    - NOW this becomes a plain "finding peak in a 1D array"
+    - now we have a cell to check
+        - compare this cell with its left and right adj cells
+        - if its peak, return the row=maxElementIdx, and col=mid
+    - if a cell is not peak, pick a side / cell which has higher value COMPARED TO MID
+        - remember mid = col idx
+    - if right cell has higher value compared to mid cell, take search on the right side
+    - otherwise take the search on the left side
+
+    - we are doing a binary search on n cols
+        - o(logn)
+    - and at each cell, we look for a max element in m rows
+        - o(m)
+    time = o(logn * m)
+    space = o(1)
+*/
 func findPeakGrid(mat [][]int) []int {
     m := len(mat)
     n := len(mat[0])
