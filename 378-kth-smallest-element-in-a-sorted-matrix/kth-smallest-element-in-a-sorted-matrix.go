6dc143ed59a831a71e9926ee223e2187e6d9f9ca
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-31 20:13:30 -0700

    Time: 14 ms (90.66%) | Memory: 6.7 MB (75.82%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.go
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.go
@@ -1,28 +1,37 @@
+/*
+    approach: binary search on answers
+    - we know the smallest element is at 0,0
+    - we know the largest element is at m-1,n-1
+    - and the numbers within the matrix are within this min,max range ( guranteed )
+    - so we have a range of numbers
+    - ranges are sorted, therefore we can use binary search
+    - how can mid be our answer?
+        - if there are >= k elements to the left of mid number
+        
+*/
 func kthSmallest(matrix [][]int, k int) int {
     n := len(matrix)
     left := matrix[0][0]
     right := matrix[n-1][n-1]
     for left <= right {
         mid := left + (right-left)/2
-        count := countOnLeft(matrix, mid)
+        count := count(matrix, mid)
         if count < k {
             left = mid+1
         } else {
             right = mid-1
         }
     }
-    return right
-    
+    return left
 }
 
-
-func countOnLeft(matrix [][]int, target int) int {
+func count(matrix [][]int, target int) int {
     n := len(matrix)
     r := n-1
     c := 0
     total := 0
     for r >= 0 && c < n {
-        if matrix[r][c] < target {
+        if matrix[r][c] <= target {
             total += r+1
             c++
         } else {
