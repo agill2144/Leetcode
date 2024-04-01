fe7d264d2b1493f9a832d1084c473b5d4db62641
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-03-31 19:30:28 -0700

    Time: 100 ms (100.00%) | Memory: 13.6 MB (66.67%) - LeetSync

--- 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
+++ 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
@@ -1,10 +1,9 @@
-/*
-*/
 func matrixMedian(grid [][]int) int {
     m := len(grid)
     n := len(grid[0])
     total := m*n
-    half := total/2
+    median := total/2
+
     left := 0
     right := 1000002
     for left <= right {
@@ -13,14 +12,15 @@ func matrixMedian(grid [][]int) int {
         for i := 0; i < m; i++ {
             count += countOnLeft(grid[i], mid)
         }
-        if count <= half {
-            left = mid+1
-        } else {
+        if count > median {
             right = mid-1
+        } else {
+            left = mid+1
         }
     }
-    return left
+    return right
 }
+
 
 func countOnLeft(arr []int, target int) int {
     left := 0
@@ -29,7 +29,7 @@ func countOnLeft(arr []int, target int) int {
     idx := -1
     for left <= right {
         mid := left + (right-left)/2
-        if arr[mid] <= target {
+        if arr[mid] < target {
             idx = mid
             left = mid+1
         } else {
@@ -38,24 +38,3 @@ func countOnLeft(arr []int, target int) int {
     }
     return idx+1
 }
-/*
-    approach: brute force
-    - create a 1d array
-    - sort it
-    - return the middle
-    time = o(mn) + o(mn log mn)
-    space = o(mn)
-*/
-// func matrixMedian(grid [][]int) int {
-//     merged := []int{}
-//     m := len(grid)
-//     n := len(grid[0])
-//     mid := (m*n)/2
-//     for i := 0; i < m; i++ {
-//         for j := 0; j < n; j++ {
-//             merged = append(merged, grid[i][j])
-//         }
-//     }
-//     sort.Ints(merged)
-//     return merged[mid]
-// }
