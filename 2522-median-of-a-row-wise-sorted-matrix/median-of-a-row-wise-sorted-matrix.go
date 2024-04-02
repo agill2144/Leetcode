a114b01c27f4d417c3e3f7ed75a193d6b9124a04
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-01 22:07:58 -0700

    Time: 89 ms (100.00%) | Memory: 10.3 MB (100.00%) - LeetSync

--- 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
+++ 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
@@ -6,17 +6,18 @@ func matrixMedian(grid [][]int) int {
 
     left := 1
     right := 1000000
+    ans := 0
     for left <= right {
         mid := left + (right-left)/2
         count := countOnLeft(grid, mid)
-        if count <= half {
-            left = mid+1
-        } else {
+        if count > half {
+            ans = mid
             right = mid-1
+        } else {
+            left = mid+1
         }
-
     }
-    return left
+    return ans
 }
 
 
