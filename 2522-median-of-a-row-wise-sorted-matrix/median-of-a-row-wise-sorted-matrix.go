c226434d3bafa855f77608bf3267c038c454b5e1
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-02 21:16:46 -0700

    Time: 94 ms (72.73%) | Memory: 9.4 MB (90.91%) - LeetSync

--- 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
+++ 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
@@ -1,38 +1,57 @@
 func matrixMedian(grid [][]int) int {
     m := len(grid)
     n := len(grid[0])
+    left := 1 // given in constraint, the smallest possible cell value is 1
+    right := 1000000000 // and largest possible cell value is 10^9 
     total := m*n
     half := total/2
-
-    left := 1
-    right := 1000000
-    ans := 0
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
+        // is mid our median?
+        // mid can be our median ONLY if it has more than half elements on its left side 
+        // i.e number of elements <= mid are > half
+        // half = totalElements/2
+        // why > half ?, shouldn't this be == half ?
+        // no, because we are using <= operator to count on left of mid ( INCLUDING MID ITSELF )
+        // if we say that there are supposed to be half elements on left of median
+        // if we INCLUDED median in the count, then it will ALWAYS BE > half
+        // why are we including counting of median ?
+        // because there are dupes!
         count := countOnLeft(grid, mid)
         if count > half {
+            // we are looking at a potential median, save it and search left
             ans = mid
             right = mid-1
         } else {
+            // this means, number of elements <= mid are not even half!
+            // this cannot be median for sure, our median / middle is somewhere on the right
+            // therefore search right
             left = mid+1
         }
     }
     return ans
 }
 
-
+// time = o(m * logn)
 func countOnLeft(grid [][]int, target int) int {
+    count := 0
     m := len(grid)
     n := len(grid[0])
-    count := 0
     for i := 0; i < m; i++ {
+        // because each row is sorted, we need to find the closest/last idx to target
+        // from the left side of target
+        //                           closestIdx
+        // |----------------------------------|--|----------------------|
+        //                                       Target
+        // how will idx tell us the count ?
+        // count = idx+1
         idx := -1
         left := 0
         right := n-1
         for left <= right {
             mid := left + (right-left)/2
-            midVal := grid[i][mid]
-            if midVal <= target {
+            if grid[i][mid] <= target {
                 idx = mid
                 left = mid+1
             } else {
