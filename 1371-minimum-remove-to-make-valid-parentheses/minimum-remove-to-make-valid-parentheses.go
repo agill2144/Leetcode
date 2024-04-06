fa6ff143d167f53b29f79a4f78d88a96e183da85
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-05 18:54:17 -0700

    Time: 16 ms (46.60%) | Memory: 7.5 MB (42.48%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
@@ -0,0 +1,57 @@
+type stNode struct {
+    val byte
+    idx int
+}
+
+func minRemoveToMakeValid(s string) string {
+    invalidIdxSet := map[int]struct{}{}
+    st := []stNode{}
+    validOpenings := map[byte]byte{
+        ')' : '(',
+        '}' : '{',
+        ']' : '[',
+    }
+
+    for i := 0; i < len(s); i++ {
+        char := s[i]
+        if char == '[' || char == '(' || char == '{' {
+            st = append(st, stNode{char,i})
+        } else if char == ']' || char == ')' || char == '}' {
+            
+            // when is a closing paran invalid? 
+
+            // 1. when there is no corresponding open paran
+            if len(st) == 0 {
+                // invalid closing paran
+                invalidIdxSet[i] = struct{}{}
+                continue
+            }
+
+            // 2. last open paran is not the corresponding paran
+            top := st[len(st)-1]
+            lastOpen := top.val
+            validOpen := validOpenings[char]
+            if lastOpen != validOpen {
+                invalidIdxSet[i] = struct{}{}
+                continue
+            }
+
+            st = st[:len(st)-1]
+        }
+    }
+
+    // or 3. there are open parans in the st have not closed
+    for len(st) != 0 {
+        top := st[len(st)-1]
+        st = st[:len(st)-1]
+        invalidIdxSet[top.idx] = struct{}{}
+    }
+
+    out := new(strings.Builder)
+    for i := 0; i < len(s); i++ {
+        _, ok := invalidIdxSet[i]
+        if ok {continue}
+        out.WriteByte(s[i])
+    }
+    return out.String()
+}
