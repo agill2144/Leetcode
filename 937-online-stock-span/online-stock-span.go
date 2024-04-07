18b26c7f9b1c38f161403af3e3a28156047dedfa
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-07 14:43:01 -0700

    Time: 147 ms (78.88%) | Memory: 8.4 MB (43.48%) - LeetSync

--- 937-online-stock-span/online-stock-span.go
+++ 937-online-stock-span/online-stock-span.go
@@ -12,28 +12,72 @@
     - 6 + 1(current day) = 7
 
     time = o(n) for each Next() call
+
+
+    approach: monotonic stack
+    - we need to look back at history ( stack hint )
+    - if we had nested iterations, our inner loop start pos
+    - depends on outter i's position ( ngr , ngl )
+    - we need to go back until we run into a price > current price
+        - next greater on left ( stack problem hint )
+    - therefore we will apply classic NGL stack template
+    - we need to find the number of days between current day and the day when ngl was pushed
+    - therefore, whenever we push an item to stack, we will push a current day ( idx ) to it
+    - we will track the current day as idx as a global var
+    - when we need to find ngl
+        - thats easy, remove all elements that are smaller than current price from top of stack
+    - when we reach a ngl element
+        - it will have idx ( i.e day ) as part of it
+        - then finding the number of days should be easy;
+        - newIdx - ngl idx
+    - wont popping elements from stack mess up their idx ?
+        - no, because those idx are constant, they represent day
+        - meaning, price was. $x on day $idx
+        - so even if we delete items from stack, the ngl will show correct day it was pushed on to stack
+    - this way, each Next() call may not have to go thru ALL n days, it will be less on average compared to brute force
+
+    time = o(n)
 */
+type stackNode struct {
+    val int
+    idx int
+}
+
 type StockSpanner struct {
-    prices []int    
+    st []stackNode
+    idx int
 }
 
 
 func Constructor() StockSpanner {
     return StockSpanner{
-        prices: []int{},
+        st: []stackNode{},
     }    
 }
 
 
 func (this *StockSpanner) Next(price int) int {
-    this.prices = append(this.prices, price)
-    lastIdx := len(this.prices)-1
-    i := lastIdx
-    for i >= 0 {
-        if this.prices[i] > price {break}
-        i--
+    // clean up / remove prices <= current price
+    // we want ngl ( next greater on left )
+    // therefore currPrice will remove anyone/all that are <= currPrice
+    for len(this.st) != 0 && this.st[len(this.st)-1].val <= price {
+        this.st = this.st[:len(this.st)-1]
     }
-    return lastIdx-i
+
+    days := 0
+    if len(this.st) != 0 {
+        // top is ngl, we want to use its idx/day
+        days = this.idx - this.st[len(this.st)-1].idx
+    } else {
+        // if stack is empty, 
+        // and there were 3 elements [1,2,3]
+        // thats 3 days + current new day = 4
+        days = this.idx+1
+    }
+    // push current pice
+    this.st = append(this.st, stackNode{price, this.idx})
+    this.idx++
+    return days
 }
 
 
