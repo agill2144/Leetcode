12008a453b817c98cf1ae37a3f929b4bdbf14a70
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-07 14:58:21 -0700

    Time: 120 ms (91.23%) | Memory: 9.3 MB (71.43%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.go
+++ 739-daily-temperatures/daily-temperatures.go
@@ -1,16 +1,15 @@
-// nge pattern ( next greater on right )
+// ngr ( next greater on right )
 func dailyTemperatures(temperatures []int) []int {
     st := []int{}
     n := len(temperatures)
     out := make([]int, n)
     for i := n-1; i >= 0; i-- {
         curr := temperatures[i]
-        for len(st) != 0 && curr >= temperatures[st[len(st)-1]] {
+        for len(st) != 0 && temperatures[st[len(st)-1]] <= curr {
             st = st[:len(st)-1]
         }
-        if len(st) > 0 {
-            topIdx := st[len(st)-1]
-            out[i] = topIdx-i
+        if len(st) != 0 {
+            out[i] = st[len(st)-1] - i
         }
         st = append(st, i)
     }
