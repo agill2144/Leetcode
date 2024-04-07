9b02d3cba12c26f150a73b365a07af332a172c10
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-07 14:27:30 -0700

    Time: 343 ms (9.94%) | Memory: 7.4 MB (99.38%) - LeetSync

--- 937-online-stock-span/online-stock-span.go
+++ 937-online-stock-span/online-stock-span.go
@@ -1,36 +1,39 @@
-type stackNode struct {
-    idx int
-    val int
-}
+/*
+    approach: brute force
+    - for each price being added
+    - we need to tell how many CONSECUTIVE days, price is < current price
+    - we need to go back as far as we can
+    - until we run into a price > current day price
+    - example; current day = 85
+    - prev days = [100,65,75,82,81,76,70]
+    - go back until ^
+    - we stop at idx 0,
+    - therefore number of days price was less than or equal to the price of curr day is
+    - 6 + 1(current day) = 7
+
+    time = o(n) for each Next() call
+*/
 type StockSpanner struct {
-    idx int
-    st []stackNode
+    prices []int    
 }
 
 
 func Constructor() StockSpanner {
     return StockSpanner{
-        idx: 0,
-        st: []stackNode{},
-    }
+        prices: []int{},
+    }    
 }
 
 
 func (this *StockSpanner) Next(price int) int {
-    // ngl
-    for len(this.st) != 0 && this.st[len(this.st)-1].val <= price {
-        this.st = this.st[:len(this.st)-1]
+    this.prices = append(this.prices, price)
+    lastIdx := len(this.prices)-1
+    i := lastIdx
+    for i >= 0 {
+        if this.prices[i] > price {break}
+        i--
     }
-    ans := 0
-    if len(this.st) != 0 {
-        ans = this.idx - this.st[len(this.st)-1].idx        
-    } else {
-        
-        ans = this.idx+1 
-    } 
-    this.st = append(this.st, stackNode{this.idx,price})
-    this.idx++
-    return ans
+    return lastIdx-i
 }
 
 
