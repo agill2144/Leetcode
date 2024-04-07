66eae8583bf70ec8a7db8e15333e08730169d7f0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-06 21:04:14 -0700

    Time: 133 ms (98.79%) | Memory: 7.4 MB (100.00%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.go
@@ -0,0 +1,38 @@
+type stackNode struct {
+    idx int
+    val int
+}
+type StockSpanner struct {
+    idx int
+    st []stackNode
+}
+
+
+func Constructor() StockSpanner {
+    return StockSpanner{
+        idx: 0,
+        st: []stackNode{},
+    }
+}
+
+
+func (this *StockSpanner) Next(price int) int {
+    // ngl
+    for len(this.st) != 0 && this.st[len(this.st)-1].val <= price {
+        this.st = this.st[:len(this.st)-1]
+    }
+    ans := this.idx+1
+    if len(this.st) != 0 {
+        ans = this.idx - this.st[len(this.st)-1].idx        
+    }
+    this.st = append(this.st, stackNode{this.idx,price})
+    this.idx++
+    return ans
+}
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Next(price);
+ */
