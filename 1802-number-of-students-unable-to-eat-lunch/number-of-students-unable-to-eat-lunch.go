16ed5e7083c048a937898a7eb605a4a782fb0d15
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-07 21:29:21 -0700

    Time: 2 ms (43.04%) | Memory: 2.5 MB (6.33%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.go
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.go
@@ -1,4 +1,6 @@
 func countStudents(students []int, sandwiches []int) int {
+    st := []int{}
+    for i := 0; i < len(sandwiches); i++ {st = append(st, sandwiches[i])}
     // count to keep track of how many we could not feed
     count := 0
     // when count == len of sandwiches, it means all students went around the circle
@@ -9,11 +11,11 @@ func countStudents(students []int, sandwiches []int) int {
     // if count becomes 3, it means we save 3 consec students which would not take the top sandwich
     // and if we only had 3 sandwiches in total and all 3 students did not take it, 
     // it means we ran a full cycle of the queue without anyone making dents in the stack
-    for count != len(sandwiches) {
+    for count != len(st) {
         pref := students[0]
         students = students[1:]
-        if pref == sandwiches[0] {
-            sandwiches = sandwiches[1:]
+        if pref == st[0] {
+            st = st[1:]
             count = 0
         } else {
             students = append(students, pref)
