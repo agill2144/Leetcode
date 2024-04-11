de19b6d40b2ae3b4cac1d3ca653f5735fcb3c6ae
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-10 18:02:01 -0700

    Time: 0 ms (100.00%) | Memory: 5.3 MB (58.14%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.go
@@ -0,0 +1,25 @@
+func removeKdigits(num string, k int) string {
+    if k == len(num) {return "0"}
+    st := []byte{}
+    for i := 0; i < len(num); i++ {
+        curr := num[i]
+        for len(st) != 0 && k > 0 && st[len(st)-1] > curr {
+            st = st[:len(st)-1]
+            k--
+        }
+        if len(st) == 0 && curr == '0' {continue}
+        st = append(st, curr)
+    }
+    for (k != 0 && len(st) != 0) {
+        st = st[:len(st)-1]
+        k--
+    }
+    out := new(strings.Builder)
+    for i := 0; i < len(st); i++ {
+        if i == 0 && st[i] == '0' {continue}
+        out.WriteByte(st[i])
+    }
+    outStr := out.String()
+    if outStr == "" {outStr = "0"}
+    return outStr
+}
