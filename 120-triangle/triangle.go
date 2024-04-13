ac335f82c431a567a29d1bd44f14de0c33304988
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-12 18:39:57 -0700

    Time: 4 ms (80.44%) | Memory: 3.1 MB (52.37%) - LeetSync

---
+++ 120-triangle/triangle.go
@@ -0,0 +1,26 @@
+func minimumTotal(triangle [][]int) int {
+    dirs := [][]int{{-1,0},{-1,-1}}
+    m := len(triangle)
+    if m == 1 {return triangle[0][0]}
+    out := math.MaxInt64
+    for i := 1; i < m; i++ {
+        prevRowLen := len(triangle[i-1])
+        for j := 0; j < len(triangle[i]); j++ {
+            // look up and get min val
+            minVal := math.MaxInt64
+            for _, dir := range dirs {
+                nr := i+dir[0]
+                nc := j+dir[1]
+                if nr >= 0 && nc >= 0 && nc < prevRowLen {
+                    minVal = min(minVal, triangle[nr][nc])
+                }
+            }
+            triangle[i][j] += minVal
+
+            if i == m-1 {
+                out = min(out, triangle[i][j])
+            }
+        }
+    }
+    return out
+}
