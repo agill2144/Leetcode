9c7a66dcfd8746b6a07c3b44787d050d0eb001de
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-14 14:17:38 -0700

    Time: 280 ms (83.50%) | Memory: 21.9 MB (32.66%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.go
@@ -0,0 +1,97 @@
+type MedianFinder struct {
+    left *maxHeap
+    right *minHeap
+}
+
+
+func Constructor() MedianFinder {
+    return MedianFinder{
+        left: &maxHeap{items: []int{}},
+        right: &minHeap{items: []int{}},
+    }
+}
+
+
+func (this *MedianFinder) AddNum(num int)  {
+    heap.Push(this.left, num)
+    
+    // ensure left top <= right top
+    if this.right.Len() > 0 {
+        if this.left.items[0] > this.right.items[0] {
+            heap.Push(this.right, heap.Pop(this.left))
+        }
+    }
+
+    // ensure its balanced, so that the median is at the top of surface of both heaps
+    // otherwise median could get burried somewhere deep inside a heap
+    if this.left.Len() - this.right.Len() > 1 {
+        heap.Push(this.right,heap.Pop(this.left))
+    } else if this.right.Len() - this.left.Len() > 1 {
+        heap.Push(this.left,heap.Pop(this.right))
+    }
+}
+
+
+func (this *MedianFinder) FindMedian() float64 {
+    if this.right.Len() == 0 {return float64(this.left.items[0])}
+    totalSize := this.left.Len() + this.right.Len()
+    leftTop := float64(this.left.items[0])
+    rightTop := float64(this.right.items[0])
+    if totalSize % 2 == 0 {
+        return (leftTop+rightTop)/2.0                
+    }
+    if this.left.Len() > this.right.Len() {return leftTop}
+    return rightTop
+}
+
+
+type minHeap struct {
+	items []int
+}
+func (m *minHeap) Less(i, j int) bool {
+	return m.items[i] < m.items[j]
+}
+func (m *minHeap) Swap(i, j int) {
+	m.items[i], m.items[j] = m.items[j], m.items[i]
+}
+func (m *minHeap) Len() int {
+	return len(m.items)
+}
+func (m *minHeap) Push(x interface{}) {
+	m.items = append(m.items, x.(int))
+}
+func (m *minHeap) Pop() interface{} {
+	out := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return out
+}
+
+
+type maxHeap struct {
+	items []int
+}
+func (m *maxHeap) Less(i, j int) bool {
+	return m.items[i] > m.items[j]
+}
+func (m *maxHeap) Swap(i, j int) {
+	m.items[i], m.items[j] = m.items[j], m.items[i]
+}
+func (m *maxHeap) Len() int {
+	return len(m.items)
+}
+func (m *maxHeap) Push(x interface{}) {
+	m.items = append(m.items, x.(int))
+}
+func (m *maxHeap) Pop() interface{} {
+	out := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return out
+}
+
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddNum(num);
+ * param_2 := obj.FindMedian();
+ */
