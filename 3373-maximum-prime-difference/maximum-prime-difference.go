895e38f9be5a4c0e0cc6014bf41a8174b402b21b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-13 21:15:55 -0700

    Time: 102 ms (100.00%) | Memory: 15.9 MB (100.00%) - LeetSync

--- 3373-maximum-prime-difference/maximum-prime-difference.go
+++ 3373-maximum-prime-difference/maximum-prime-difference.go
@@ -6,7 +6,7 @@ func maximumPrimeDifference(nums []int) int {
     primes := make([]bool, end+1)
     for i := 0; i < len(primes); i++ {primes[i] = true}
     primes[0], primes[1] = false, false
-    for i := 2; i < len(primes); i++ {
+    for i := 2; i*i < len(primes); i++ {
         if primes[i] {
             for j := i+i; j < len(primes); j+=i {
                 primes[j] =  false
