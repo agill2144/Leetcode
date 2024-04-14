4f8d0bf29241f71f4abb8252cfb461a03f25fd94
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-14 14:31:27 -0700

    Time: 6 ms (58.00%) | Memory: 5.4 MB (46.35%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.go
+++ 98-validate-binary-search-tree/validate-binary-search-tree.go
@@ -7,18 +7,16 @@
  * }
  */
 func isValidBST(root *TreeNode) bool {
-    var prev *TreeNode
-    var dfs func(r *TreeNode) bool
-    dfs = func(r *TreeNode) bool {
+    var dfs func(r *TreeNode, min, max int) bool
+    dfs = func(r *TreeNode, min, max int) bool {
         // base
         if r == nil {return true}
 
         // logic
-        lok := dfs(r.Left)
-        if !lok {return false}
-        if prev != nil && prev.Val >= r.Val {return false}
-        prev = r
-        return dfs(r.Right)
+        if r.Val <= min || r.Val >= max {return false}
+        if !dfs(r.Left, min, r.Val) {return false}
+        if !dfs(r.Right, r.Val, max) {return false}
+        return true
     }
-    return dfs(root)
+    return dfs(root, math.MinInt64, math.MaxInt64)
 }
