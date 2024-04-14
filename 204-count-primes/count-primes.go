92673368820171f532856564af07bc75dfe68382
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-13 21:57:16 -0700

    Time: 80 ms (47.00%) | Memory: 13.2 MB (36.41%) - LeetSync

--- 204-count-primes/count-primes.go
+++ 204-count-primes/count-primes.go
@@ -34,9 +34,7 @@ func countPrimes(n int) int {
     if n <= 1 {return 0}
     primes := make([]bool, n)
     for i := 0; i < len(primes); i++ {primes[i] = true}
-    primes[0] = false
-    primes[1] = false
-    for i := 2; i < len(primes); i++ {
+    for i := 2; i*i < len(primes); i++ {
         if primes[i] {
             for j := i*i; j < len(primes); j+=i {
                 primes[j] = false
