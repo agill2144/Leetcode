c02a12a627d6ae46a69f9d02b2ede4b8e8a40de7
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-14 23:55:50 -0700

    Time: 80 ms (88.05%) | Memory: 8.9 MB (59.27%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.go
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.go
@@ -1,37 +1,69 @@
 func largestRectangleArea(heights []int) int {
-   
-    nsl := make([]int, len(heights))
-    nsr := make([]int, len(heights))
-    st := []int{}
-    for i := 0; i < len(heights); i++ {
-        h := heights[i]
-        for len(st) != 0 && heights[st[len(st)-1]] >= h {
-            st = st[:len(st)-1]
-        }
-        if len(st) != 0 {
-            nsl[i] = st[len(st)-1]+1
-        }
-        st = append(st, i)
-    }
+    n := len(heights)
+    if n == 0 {return 0}
+    if n == 1 {return heights[0]}
 
-    st = []int{}
-    for i := len(heights)-1; i >= 0; i-- {
-        h := heights[i]
-        for len(st) != 0 && heights[st[len(st)-1]] >= h {
+    maxArea := math.MinInt64
+    st := []int{} // index
+    for i := 0; i <= n; i++ {
+        for len(st) != 0 && (i == n || heights[i] < heights[st[len(st)-1]]) {
+            // curr height is the nsr for the ele that is on top of stack
+            // what about nsl ? its at the bottom of top of st
+            height := heights[st[len(st)-1]]
             st = st[:len(st)-1]
-        }
-        if len(st) != 0 {
-            nsr[i] = st[len(st)-1]-1
-        } else {
-            nsr[i] = len(heights)-1
+            nsr := i
+            width := nsr
+            if len(st) != 0 {
+                width = nsr-st[len(st)-1]-1
+            }
+            maxArea = max(maxArea, height*width)
         }
         st = append(st, i)
     }
-    ans := math.MinInt64
-    for i := 0; i < len(heights); i++ {
-        winSize := nsr[i] - nsl[i] + 1
-        area := heights[i] * winSize
-        ans = max(area, ans)
-    }
-    return ans
+    return maxArea
 }
+// func largestRectangleArea(heights []int) int {
+//     nsl := nsl(heights)
+//     nsr := nsr(heights)
+//     ans := math.MinInt64
+//     for i := 0; i < len(heights); i++ {
+//         winSize := nsr[i] - nsl[i] + 1
+//         area := heights[i] * winSize
+//         ans = max(area, ans)
+//     }
+//     return ans
+// }
+
+// func nsr(arr []int) []int {
+//     out := make([]int, len(arr))
+//     st := []int{}
+//     for i := len(arr)-1; i >= 0; i-- {
+//         h := arr[i]
+//         for len(st) != 0 && arr[st[len(st)-1]] >= h {
+//             st = st[:len(st)-1]
+//         }
+//         if len(st) != 0 {
+//             out[i] = st[len(st)-1]-1
+//         } else {
+//             out[i] = len(arr)-1
+//         }
+//         st = append(st, i)
+//     }
+//     return out
+// }
+
+// func nsl(arr []int) []int {
+//     out := make([]int, len(arr))
+//     st := []int{}
+//     for i := 0; i < len(arr); i++ {
+//         h := arr[i]
+//         for len(st) != 0 && arr[st[len(st)-1]] >= h {
+//             st = st[:len(st)-1]
+//         }
+//         if len(st) != 0 {
+//             out[i] = st[len(st)-1]+1
+//         }
+//         st = append(st, i)
+//     }
+//     return out
+// }
