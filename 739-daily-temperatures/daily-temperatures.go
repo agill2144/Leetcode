ce0e783f0fb4a6c107f27363eefae9e606830180
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-15 21:48:44 -0700

    Time: 142 ms (43.91%) | Memory: 11.6 MB (39.27%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.go
+++ 739-daily-temperatures/daily-temperatures.go
@@ -1,7 +1,22 @@
-// ngr ( next greater on right )
+/*
+    approach: classic NGR
+    - next greater element to right
+    - start from right hand side
+    - and for each ith element, we want its next greater element
+    - if we want next element to be accessible easily, we can store them in the stack
+    - then each ith element will look at top of the st;
+        - remove every element that is <= ith element
+        - if top is > ith element, we found ith's NGR
+    - because we want distances between 2 indicies
+        - we will store index in our st
+        - these index's are just reference to each element in temperatures array
+
+    time = o(n)
+    space = o(n) 
+*/
 func dailyTemperatures(temperatures []int) []int {
-    st := []int{}
     n := len(temperatures)
+    st := []int{}
     out := make([]int, n)
     for i := n-1; i >= 0; i-- {
         curr := temperatures[i]
@@ -9,7 +24,7 @@ func dailyTemperatures(temperatures []int) []int {
             st = st[:len(st)-1]
         }
         if len(st) != 0 {
-            out[i] = st[len(st)-1] - i
+            out[i] = st[len(st)-1]-i
         }
         st = append(st, i)
     }
