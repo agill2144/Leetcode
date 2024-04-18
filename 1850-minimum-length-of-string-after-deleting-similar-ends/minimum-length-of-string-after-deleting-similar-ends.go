4437d76fc21f35f5714c6bd57a9e6dd21bd5e484
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-17 22:47:51 -0700

    Time: 9 ms (49.29%) | Memory: 6.5 MB (43.59%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.go
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.go
@@ -1,15 +1,30 @@
 func minimumLength(s string) int {
+    n := len(s)
     left := 0
-    right := len(s)-1
+    right := n-1
+
     for left < right {
-        if s[left] == s[right] {
+        leftChar := s[left]
+        rightChar := s[right]
+        if leftChar == rightChar {
+            // we can remove as many contagious occurences of the same char
+            // move left as far in as possible
+            // as long as new left == leftChar
             left++
+            for left < right && s[left] == leftChar {
+                left++
+            }
+
+            // move right as far back as possible
+            // as long as new right == rightChar
             right--
-            for left <= right && s[left] == s[left-1] {left++}
-            for left <= right && s[right] == s[right+1] {right--}
+            for left < right && s[right] == rightChar {
+                right--
+            }
         } else {
             break
         }
     }
     return right-left+1
+
 }
