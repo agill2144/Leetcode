805053b91495e800b0aac293510b96ed46dff696
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-17 23:03:08 -0700

    Time: 377 ms (7.14%) | Memory: 13.1 MB (14.29%) - LeetSync

---
+++ 2664-maximize-greatness-of-an-array/maximize-greatness-of-an-array.go
@@ -0,0 +1,39 @@
+func maximizeGreatness(nums []int) int {
+    n := len(nums)
+    dupe := make([]int, n)
+    copy(dupe, nums)
+    sort.Ints(dupe)
+    lastIdx := map[int]int{}
+    for i := 0; i < n; i++ {
+        lastIdx[dupe[i]] = i
+    }
+    used := make([]bool, n)
+    count := 0
+    for i := 0; i < n; i++ {
+        num := nums[i]
+        idx := lastIdx[num]+1
+        for idx < n && used[idx] {
+            idx++
+        }
+        if idx < n {used[idx] = true; count++}
+    }
+    return count
+}
+
+/*
+
+    
+    [1,3,5,2,1,3,1]
+     2 5 0 3 3 0 i
+    [1,1,1,2,3,3,5]
+                 i
+    [F,F,F,T,T,T,T]
+
+
+    count = 3
+    [1,2,3,4]
+     2 3 4
+    [1,2,3,4]
+             i        
+    [F,T,T,T]
+*/
