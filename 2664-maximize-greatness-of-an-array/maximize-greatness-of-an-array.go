37791d06646749aad14d7eaa8f64a45584926340
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-17 23:09:10 -0700

    Time: 71 ms (85.71%) | Memory: 8.1 MB (78.57%) - LeetSync

--- 2664-maximize-greatness-of-an-array/maximize-greatness-of-an-array.go
+++ 2664-maximize-greatness-of-an-array/maximize-greatness-of-an-array.go
@@ -1,24 +1,48 @@
 func maximizeGreatness(nums []int) int {
     n := len(nums)
-    dupe := make([]int, n)
-    copy(dupe, nums)
-    sort.Ints(dupe)
-    lastIdx := map[int]int{}
-    for i := 0; i < n; i++ {
-        lastIdx[dupe[i]] = i
-    }
-    used := make([]bool, n)
-    count := 0
-    for i := 0; i < n; i++ {
-        num := nums[i]
-        idx := lastIdx[num]+1
-        for idx < n && used[idx] {
-            idx++
+    sort.Ints(nums)
+
+    slow, fast, count := 0, 1, 0
+    for fast < n {
+        if nums[fast] > nums[slow] {
+            count++
+            slow++
+            fast++
+        } else {
+            fast++
         }
-        if idx < n {used[idx] = true; count++}
     }
     return count
 }
+
+/*
+    [1,1,1,2,3,3,5]
+           LR
+    count = 3
+*/
+
+// time = o(n) + o(nlogn) + o(n) + o(n*n)
+// func maximizeGreatness(nums []int) int {
+//     n := len(nums)
+//     dupe := make([]int, n)
+//     copy(dupe, nums)
+//     sort.Ints(dupe)
+//     lastIdx := map[int]int{}
+//     for i := 0; i < n; i++ {
+//         lastIdx[dupe[i]] = i
+//     }
+//     used := make([]bool, n)
+//     count := 0
+//     for i := 0; i < n; i++ {
+//         num := nums[i]
+//         idx := lastIdx[num]+1
+//         for idx < n && used[idx] {
+//             idx++
+//         }
+//         if idx < n {used[idx] = true; count++}
+//     }
+//     return count
+// }
 
 /*
 
