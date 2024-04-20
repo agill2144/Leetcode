678fb13d8bc9a34c7c1dcaeae63a9dba9d6c0dad
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-20 11:25:23 -0700

    Time: 122 ms (7.14%) | Memory: 10.8 MB (7.14%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.go
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.go
@@ -1,24 +1,50 @@
 func minDominoRotations(tops []int, bottoms []int) int {
-    n := len(tops)
-    freq := map[int]int{}
-    target := -1
-    for i := 0; i < n; i++ {
-        t, b := tops[i], bottoms[i]
-        freq[t]++
-        if freq[t] >= n { target = t; break }
-        if t == b {continue}
-        freq[b]++
-        if freq[b] >= n { target = b; break }
-    }
-    // fmt.Println(target, freq)
-    if target == -1 {return -1}
-    if tops[0] != target && bottoms[0] != target {return -1}
+    if len(tops) != len(bottoms) {return -1}
 
-    tCount, bCount := 0, 0
-    for i := 0; i < n; i++ {
-        t, b := tops[i], bottoms[i]
-        if t != target {tCount++}
-        if b != target {bCount++}
+    var minRotations func(target int) int
+    minRotations = func(target int) int {
+        tCount, bCount := 0, 0
+        n := len(tops)
+        for i := 0; i < n; i++ {
+            t,b := tops[i], bottoms[i]
+            // when t and b is not target, this is impossible
+            if t != target && b != target {return -1}
+            // when t == b == target; no rotations required on either side
+            if t == b && t == target {continue}
+            // when target is sitting at the bottom, rotate for tops
+            if t != target && b == target {tCount++}
+            // when target is sitting at the top, rotate for bottom
+            if b != target && t == target {bCount++}
+        }
+        return min(tCount, bCount)
     }
-    return min(tCount, bCount)
+    t1Count := minRotations(tops[0])
+    if t1Count == -1 {
+        return minRotations(bottoms[0])
+    }
+    return t1Count
 }
+
+// func minDominoRotations(tops []int, bottoms []int) int {
+//     n := len(tops)
+//     freq := map[int]int{}
+//     target := -1
+//     for i := 0; i < n; i++ {
+//         t, b := tops[i], bottoms[i]
+//         freq[t]++
+//         if freq[t] >= n { target = t; break }
+//         if t == b {continue}
+//         freq[b]++
+//         if freq[b] >= n { target = b; break }
+//     }
+//     if target == -1 {return -1}
+//     if tops[0] != target && bottoms[0] != target {return -1}
+
+//     tCount, bCount := 0, 0
+//     for i := 0; i < n; i++ {
+//         t, b := tops[i], bottoms[i]
+//         if t != target {tCount++}
+//         if b != target {bCount++}
+//     }
+//     return min(tCount, bCount)
+// }
