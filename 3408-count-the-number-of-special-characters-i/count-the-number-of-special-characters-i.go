7f5ec023d9c95e07bee58acf7f69d9fdf7301506
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-20 22:20:35 -0700

    Time: 0 ms (100.00%) | Memory: 2.4 MB (100.00%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.go
@@ -0,0 +1,32 @@
+func numberOfSpecialChars(word string) int {
+    lowers := make([]bool, 26)
+    uppers := make([]bool, 26)
+    seen := map[byte]struct{}{}
+    count := 0
+    for i := 0; i < len(word); i++ {
+        char := word[i]
+        if unicode.IsUpper(rune(char)) {
+            // look for lower
+            lower := char+32
+            upper := char
+            _, ok := seen[lower]
+            if ok && !lowers[int(lower-'a')] && !uppers[int(upper-'A')] {
+                lowers[int(lower-'a')] = true
+                uppers[int(upper-'A')] = true
+                count++
+            }
+        } else if unicode.IsLower(rune(char)) {
+            // look for upper
+            lower := char
+            upper := char-32
+            _, ok := seen[upper]
+            if ok && !lowers[int(lower-'a')] && !uppers[int(upper-'A')] {
+                lowers[int(lower-'a')] = true
+                uppers[int(upper-'A')] = true
+                count++
+            }
+        }
+        seen[char] = struct{}{}
+    }
+    return count
+}
