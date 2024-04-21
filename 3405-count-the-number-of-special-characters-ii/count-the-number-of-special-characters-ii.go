7f007611acb734e3c433f3d917b0ab85eda6bd7c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-20 21:17:38 -0700

    Time: 280 ms (100.00%) | Memory: 9.3 MB (100.00%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.go
@@ -0,0 +1,29 @@
+func numberOfSpecialChars(word string) int {
+    idxMap := map[byte][]int{}
+    for i := 0; i < len(word); i++ {
+        idxMap[word[i]] = append(idxMap[word[i]], i)
+    }
+    ansSet := map[string]struct{}{}
+    for i := 0; i < len(word); i++ {
+        char := word[i]
+        if unicode.IsUpper(rune(char)) {continue}
+        
+        // char is lower, look for its upper pair
+        lowerCase := char
+        // lowercase "a" ascii value = 97
+        // uppercase "A" ascii value = 65
+        // the difference is 32
+        // therefore if we have a lowercase
+        // and we want its upper case ascii value
+        // lowercaseAscII - 32
+        upperCase := lowerCase-32
+        lastLowerIdx := len(idxMap[lowerCase])-1
+        if len(idxMap[upperCase]) == 0 {continue}
+        
+        // every lowercase occurrence appears before the first uppercase occurrence
+        if idxMap[lowerCase][lastLowerIdx] > idxMap[upperCase][0] {continue}
+        pair := fmt.Sprintf("%v-%v", lowerCase, upperCase)
+        ansSet[pair] = struct{}{}
+    }
+    return len(ansSet)
+}
