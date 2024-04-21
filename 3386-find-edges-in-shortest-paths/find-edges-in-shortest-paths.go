189bbbd0ea2d188c6ba3425ff53ad5d34ddae615
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-20 21:23:21 -0700

    Time: 652 ms (100.00%) | Memory: 45.8 MB (100.00%) - LeetSync

--- 3386-find-edges-in-shortest-paths/find-edges-in-shortest-paths.go
+++ 3386-find-edges-in-shortest-paths/find-edges-in-shortest-paths.go
@@ -29,16 +29,12 @@ func findAnswer(n int, edges [][]int) []bool {
                 out = make([]bool, len(edges))
                 splitPath := strings.Split(currPath, "-")
                 for i := 0; i < len(splitPath); i++ {
-                    // is this an edge in our input ?
                     if i+1 > len(splitPath)-1 {continue}
+                    // is this an edge in our input ?
                     pair := fmt.Sprintf("%v-%v", splitPath[i], splitPath[i+1])
-                    // pair2 := fmt.Sprintf("%v-%v", splitPath[i+1], splitPath[i])
                     if idx, ok := edgeIdx[pair]; ok {
                         out[idx] = true
                     }
-                    // if idx, ok := edgeIdx[pair2]; ok {
-                    //     out[idx] = true
-                    // }
                 }
             } else if currDist == shortestDistToDest {
                 splitPath := strings.Split(currPath, "-")
@@ -46,13 +42,10 @@ func findAnswer(n int, edges [][]int) []bool {
                     // is this an edge in our input ?
                     if i+1 == len(splitPath) {continue}
                     pair := fmt.Sprintf("%v-%v", splitPath[i], splitPath[i+1])
-                    pair2 := fmt.Sprintf("%v-%v", splitPath[i+1], splitPath[i])
                     if idx, ok := edgeIdx[pair]; ok {
                         out[idx] = true
                     }
-                    if idx, ok := edgeIdx[pair2]; ok {
-                        out[idx] = true
-                    }
+                    
                 }            }
         }
         
