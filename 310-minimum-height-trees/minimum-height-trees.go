57198bae1e189191659fc386a1855d191b661651
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-22 23:04:45 -0700

    Time: 80 ms (59.05%) | Memory: 9.4 MB (52.38%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.go
+++ 310-minimum-height-trees/minimum-height-trees.go
@@ -59,3 +59,52 @@ func findMinHeightTrees(n int, edges [][]int) []int {
     }
     return nil
 }
+
+
+// brute force: make each node a root and check its max height
+// then find the min height
+// then find nodes with that height, those nodes are our answers
+// func findMinHeightTrees(n int, edges [][]int) []int {
+//     adjList := map[int][]int{}
+//     heights := map[int]int{}
+//     for i := 0; i < len(edges); i++ {
+//         u,v := edges[i][0], edges[i][1]
+//         adjList[u] = append(adjList[u], v)
+//         adjList[v] = append(adjList[v], u)
+//         heights[u] = 0
+//         heights[v] = 0
+//     }
+
+//     visited := make([]bool, n)
+//     var dfs func(r, node, prev, height int)
+//     dfs = func(r, node, prev, height int) {
+//         // base
+//         if visited[node] {return}        
+
+//         // logic
+//         visited[node] = true
+//         heights[r] = max(heights[r], height)
+//         for _, nei := range adjList[node] {
+//             if node == prev {continue}
+//             dfs(r, nei, node, height+1)
+//         }
+//     }
+
+//     for i := 0; i < n; i++ {
+//         dfs(i, i, -1, 0)
+//         visited = make([]bool, n)
+//     }
+
+//     minH := math.MaxInt64
+//     for _, h := range heights {
+//         minH = min(minH, h)
+//     }
+
+//     out := []int{}
+//     for node, h := range heights {
+//         if h == minH {
+//             out = append(out, node)
+//         }
+//     }
+//     return out
+// }
