dd25284fd136d8a5855ff47c5cd55a2f1a570802
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-22 22:00:45 -0700

    Time: 83 ms (55.24%) | Memory: 15.9 MB (5.71%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.go
+++ 310-minimum-height-trees/minimum-height-trees.go
@@ -1,9 +1,10 @@
 func findMinHeightTrees(n int, edges [][]int) []int {
-    if n <= 2 {
+    if n < 2 {
         out := []int{}
         for i := 0; i < n; i++ {out = append(out, i)}
         return out
     }
+
     adjList := map[int][]int{}
     degrees := make([]int, n)
     for i := 0; i < len(edges); i++ {
@@ -13,27 +14,42 @@ func findMinHeightTrees(n int, edges [][]int) []int {
         degrees[u]++
         degrees[v]++
     }
-    // find the leaves
-    // leaves are ones who have exactly 1 edge
+
+    // find the leaves 
+    // leaves are nodes with only 1 edge
+    // leaves can never be our MHT roots because they are at the far bottom
+    // going from root to leaf will have a max height and we want min height
+    // therefore leaves will never be our answer
+    // now the idea becomes, cut the leaves and process next layer of leaves
+    // once a layer of leaves have been processed, find the parent/adj neighbors
+    // once those layer of leaves have been cut, their adj neis edge count drops too
+    // therefore the adj nei degrees drop by 1
+    // while we drop them, we can check whether this adj nei is now a leaf ? (degrees[nei] == 1)
+    // enqueue it if it is.
+    // when we do know we have answer ?
+    // when the number of nodes left to be processed are <= 2 
+    // there will never be more than 2 nodes as our answer
+    // draw it out ; 
+    // if there is only 1 node in the input, its the answer 
+    // if there are 2 nodes in the input, both nodes max depth will be 1, therefore both will be our answers
+    // if there are 3 nodes in the input, only 1 node's max depth will be the smallest, that node will be our answer
+    // therefore its either 1 node or 2 nodes at max
     leaves := []int{}
-    leftNodes := n
     for i := 0; i < len(degrees); i++ {
         if degrees[i] == 1 {
             leaves = append(leaves, i)
-            degrees[i] = 0
         }
     }
-    // process the leaves using multi source bfs
+    toBeProcessed := n
     for len(leaves) != 0 {
-        if leftNodes <= 2 {return leaves}
         qSize := len(leaves)
+        if toBeProcessed <= 2 {return leaves}
         for qSize != 0 {
             dq := leaves[0]
-            leaves = leaves[1:]            
-            leftNodes--
+            leaves = leaves[1:]
+            toBeProcessed--
             for _, nei := range adjList[dq] {
                 degrees[nei]--
-                // find new adj leaves
                 if degrees[nei] == 1 {
                     leaves = append(leaves, nei)
                 }
