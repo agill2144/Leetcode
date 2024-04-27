980a62b4d190c932c361cdf1b59b2e07fc1a4338
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-26 17:22:21 -0700

    Time: 25 ms (70.37%) | Memory: 6.7 MB (44.44%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.go
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.go
@@ -1,25 +1,71 @@
+func minFallingPathSum(grid [][]int) int {
+    smallest := math.MaxInt64
+    smallestIdx := -1
+    secondSmallest := math.MaxInt64
+
+    nSmallest := math.MaxInt64
+    nSmallestIdx := -1
+    nSecondSmallest := math.MaxInt64
+
+    ans := math.MaxInt64
+    m := len(grid)
+    n := len(grid[0])
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if i != 0 {
+                if j == smallestIdx {
+                    grid[i][j] += secondSmallest
+                } else {
+                    grid[i][j] += smallest
+                }
+            }
+            
+            val := grid[i][j]
+            if val < nSmallest {
+                nSecondSmallest = nSmallest
+                nSmallest = val
+                nSmallestIdx = j
+            }
+            if val < nSecondSmallest &&  j != nSmallestIdx {
+                nSecondSmallest = val
+            }
+
+        }
+        smallest = nSmallest
+        smallestIdx = nSmallestIdx
+        secondSmallest = nSecondSmallest
+        nSmallest = math.MaxInt64
+        nSmallestIdx = -1
+        nSecondSmallest = math.MaxInt64
+    }
+    for j := 0; j < n; j++ {
+        ans = min(ans, grid[m-1][j])
+    }    
+    return ans
+}
+
 // like min path falling sum 1
 // but instead of only 2 choices per cell
 // we have n choices per cell
 // time: o(n^3)
 // space = o(1)
-func minFallingPathSum(grid [][]int) int {
-    m := len(grid)
-    n := len(grid[0])
-    ans := math.MaxInt64
+// func minFallingPathSum(grid [][]int) int {
+//     m := len(grid)
+//     n := len(grid[0])
+//     ans := math.MaxInt64
     
-    for i := 1; i < m; i++ {
-        for j := 0; j < n; j++ {        
-            minSum := math.MaxInt64
-            for k := 0; k < n; k++ {
-                if k == j {continue}
-                minSum = min(minSum, grid[i-1][k] + grid[i][j])
-            }
-            grid[i][j] = minSum
-        }
-    }
-    for j := 0; j < n; j++ {
-        ans = min(ans, grid[m-1][j])
-    }
-    return ans
-}
+//     for i := 1; i < m; i++ {
+//         for j := 0; j < n; j++ {        
+//             minSum := math.MaxInt64
+//             for k := 0; k < n; k++ {
+//                 if k == j {continue}
+//                 minSum = min(minSum, grid[i-1][k] + grid[i][j])
+//             }
+//             grid[i][j] = minSum
+//         }
+//     }
+//     for j := 0; j < n; j++ {
+//         ans = min(ans, grid[m-1][j])
+//     }
+//     return ans
+// }
