863803fe0b63cb22dfe521485614b94d88aa4daa
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-04-28 15:29:59 -0700

    Time: 77 ms (88.40%) | Memory: 11.2 MB (40.11%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.go
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.go
@@ -1,50 +1,43 @@
 func largestRectangleArea(heights []int) int {
-    nsr := nsr(heights)
-    nsl := nsl(heights)
+    st := []int{}
     maxArea := 0
-    for i := 0; i < len(heights); i++ {
-        sr := nsr[i]
-        sl := nsl[i]
-        height := heights[i]
-        width := sr-sl-1
+    n := len(heights)
+    // top of st is processed
+    // if ith element is the nsr of top
+
+    for i := 0; i < n; i++ {
+        curr := heights[i]
+        for len(st) != 0 && curr < heights[st[len(st)-1]] {
+            top := st[len(st)-1]
+            st = st[:len(st)-1]
+            height := heights[top]
+            nsr := i
+            nsl := -1
+            if len(st) != 0 {
+                nsl = st[len(st)-1]
+            }
+            width := nsr-nsl-1
+            area := height*width
+            maxArea = max(maxArea, area)
+        }
+        st = append(st, i)
+    }
+    // edge case when processing top of st;
+    // we have items left in the stack, when would this happen ?
+    // when none of the ith values were a NSR of top
+    // this means, nsr of top element does not exist, it means we can use the entire len
+    for len(st) != 0 {
+        top := st[len(st)-1]
+        st = st[:len(st)-1]
+        height := heights[top]
+        nsr := n
+        nsl := -1
+        if len(st) != 0 {
+            nsl = st[len(st)-1]
+        }
+        width := nsr-nsl-1
         area := height*width
         maxArea = max(maxArea, area)
     }
     return maxArea
-}
-
-func nsr(nums []int) []int{
-    n := len(nums)
-    out := make([]int, n)
-    st := []int{}
-    for i := n-1; i >= 0; i-- {
-        out[i] = n
-        curr := nums[i]
-        for len(st) != 0 && nums[st[len(st)-1]] >= curr {
-            st = st[:len(st)-1]
-        }
-        if len(st) != 0 {
-            out[i] = st[len(st)-1]
-        }
-        st = append(st, i)
-    }
-    return out
-}
-
-func nsl(nums []int) []int{
-    n := len(nums)
-    out := make([]int, n)
-    st := []int{}
-    for i := 0; i < n ; i++ {
-        out[i] = -1
-        curr := nums[i]
-        for len(st) != 0 && nums[st[len(st)-1]] >= curr {
-            st = st[:len(st)-1]
-        }
-        if len(st) != 0 {
-            out[i] = st[len(st)-1]
-        }
-        st = append(st, i)
-    }
-    return out
 }
