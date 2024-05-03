d55b99dcbd5ebf93689695b08a7c4d64ca4afdf5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-02 21:38:08 -0700

    Time: 1 ms (78.95%) | Memory: 2.2 MB (75.44%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.go
@@ -0,0 +1,26 @@
+func compareVersion(version1 string, version2 string) int {
+    v1 := 0; v1Rev := 0
+    v2 := 0; v2Rev := 0
+
+    for v1 < len(version1) || v2 < len(version2) {
+        v1Rev = 0
+        v2Rev = 0
+
+        for v1 < len(version1) && version1[v1] != '.' {
+            v1Rev = v1Rev * 10 + int(version1[v1]-'0')
+            v1++
+        }
+
+        for v2 < len(version2) && version2[v2] != '.' {
+            v2Rev = v2Rev * 10 + int(version2[v2]-'0')
+            v2++
+        }
+        if v1Rev < v2Rev {
+            return -1
+        } else if v1Rev > v2Rev {
+            return 1
+        }
+        v1++;v2++
+    }
+    return 0
+}
