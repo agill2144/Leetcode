d41a62b1eb9c6e61f80bcad75396a7e337b893f4
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-02 21:38:39 -0700

    Time: 0 ms (100.00%) | Memory: 2.1 MB (75.44%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.go
+++ 165-compare-version-numbers/compare-version-numbers.go
@@ -5,22 +5,21 @@ func compareVersion(version1 string, version2 string) int {
     for v1 < len(version1) || v2 < len(version2) {
         v1Rev = 0
         v2Rev = 0
-
         for v1 < len(version1) && version1[v1] != '.' {
             v1Rev = v1Rev * 10 + int(version1[v1]-'0')
             v1++
         }
-
+        v1++ // escape the dot
         for v2 < len(version2) && version2[v2] != '.' {
             v2Rev = v2Rev * 10 + int(version2[v2]-'0')
             v2++
         }
+        v2++ // escape the dot
         if v1Rev < v2Rev {
             return -1
         } else if v1Rev > v2Rev {
             return 1
         }
-        v1++;v2++
     }
     return 0
 }
