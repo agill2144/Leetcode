c189e43fda108b2fac42c3657b55fc6189189e26
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-04 11:03:04 -0700

    Time: 1 ms (68.75%) | Memory: 2.3 MB (62.50%) - LeetSync

--- 1080-camelcase-matching/camelcase-matching.go
+++ 1080-camelcase-matching/camelcase-matching.go
@@ -5,24 +5,35 @@ func camelMatch(queries []string, pattern string) []bool {
         // everything in pattern must match, we can only insert chars into pattern
         res := true
         for p < len(pattern) && w < len(word) && res {
+            // if both chars are upper case, they both must match!
             if word[w] <= 'Z' && pattern[p] <= 'Z' {
                 if pattern[p] != word[w] {res = false}
                 w++
                 p++
             } else if word[w] != pattern[p] {
+                // if both are not uppercase, one of them is , or none of them are
+                // we can only move w ptr forward, until word char matches pattern char
+                // "everything in pattern must match, we can only insert chars into pattern"
+                // therefore move only w ptr forward in hopes for a match
                 w++
             } else {
+                // the happy case, when both chars do match
                 w++
                 p++
             }
         }
+        // "everything in pattern must match, we can only insert chars into pattern"
+        // if pattern still has chars left, aint no way this is a match
         if p < len(pattern) && res { res = false }
+
         // if word be bigger than pattern and pattern matched everything this far
         // ensure word does not have upper cases
+        // because we are only allowed to insert lower case chars into pattern
         for w < len(word) && res {
             if word[w] <= 'Z' { res = false }
             w++
         }
+
         out[idx] = res
     }
     return out
