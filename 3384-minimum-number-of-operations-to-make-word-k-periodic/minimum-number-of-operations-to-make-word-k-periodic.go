4ffb5018619b65bfd1707b084a9c457895627f81
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-05 15:41:44 -0700

    Time: 29 ms (100.00%) | Memory: 7 MB (100.00%) - LeetSync

--- 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.go
+++ 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.go
@@ -6,6 +6,18 @@ func minimumOperationsToMakeKPeriodic(word string, k int) int {
         kSizeFreq[subStr]++
         maxFreq = max(maxFreq, kSizeFreq[subStr])
     }
-    
+    // the word is divided into k size chunks
+    // for example if word size is 10, and k = 2
+    // then the word is divided into 5 chunks
+    // and out of those 5 chunks, we already have maxFreq number of chunks in our desired state 
+    // then whatever number of chunks are left, will need to replaced, i.e thats the number of operations needed
+    // for example; word size = 10, k = 2
+    // how many chunks do we have ? 10/2 = 5
+    // A substr of size k that is repeating the most often is 2
+    // that is, out of 5 chunks, there are 2 identical substr chunks
+    // that wont need to be replaced
+    // so now out of 5, we already have 2 chunks good to go
+    // we are left with 3, each of the chunks need to be replaced
+    // thats the number of operations required
     return len(word)/k - maxFreq
 }
