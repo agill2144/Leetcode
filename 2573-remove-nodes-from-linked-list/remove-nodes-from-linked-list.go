5b4a69bc4adb6fba9aafdedea2fa0c25ecd6a295
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-05 18:07:16 -0700

    Time: 227 ms (44.44%) | Memory: 13.2 MB (63.49%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// ngr, process top pattern
+func removeNodes(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {return head}
+    st := []*ListNode{}
+    curr := head
+    for curr != nil {
+        next := curr.Next
+        for len(st) != 0 && curr.Val > st[len(st)-1].Val {
+            top := st[len(st)-1]
+            st = st[:len(st)-1]
+            top.Next = nil
+            if len(st) != 0 {
+                st[len(st)-1].Next = curr
+            }
+        }
+        st = append(st, curr)
+        curr = next
+    }
+    return st[0]
+}
