e7960d82f250de4cd16f5bb045400a04fa1fd77a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-07 22:37:25 -0700

    Time: 1 ms (85.96%) | Memory: 2.3 MB (71.93%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.go
@@ -0,0 +1,30 @@
+func wordBreak(s string, wordDict []string) []string {
+    set := map[string]struct{}{}
+    for i := 0; i < len(wordDict); i++ { set[wordDict[i]] = struct{}{} }
+
+    out := []string{}
+    var dfs func(start int, sent []string)
+    dfs = func(start int, sent []string) {
+        // base
+        if start == len(s) {
+            out = append(out, strings.Join(sent, " "))
+            return
+        }
+
+
+        // logic
+        for i := start; i < len(s); i++ {
+            subStr := string(s[start:i+1])
+            if _, ok := set[subStr]; ok {
+                // action
+                sent = append(sent, subStr)
+                // recurse
+                dfs(i+1, sent)
+                // backtrack
+                sent = sent[:len(sent)-1]
+            }
+        }
+    }
+    dfs(0, []string{})
+    return out
+}
