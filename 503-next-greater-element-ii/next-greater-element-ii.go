2998d01726a78ffec6c19394b6e30604704c1107
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-12 20:50:42 -0700

    Time: 31 ms (26.67%) | Memory: 7.4 MB (34.00%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.go
+++ 503-next-greater-element-ii/next-greater-element-ii.go
@@ -1,38 +1,16 @@
-// ngr on a circular array
-// processing top of st instead of processing ith element
-/*
-    Am i(curr val) your(top of st) next greater element?
-    - yes
-        - process me(top of the st)
-        - and keep processing the top as long as you(i) can
-    - no
-        - if this ith element is from the 2nd loop ( 2nd pass on circular arr )
-            - do not push to stack
-        - otherwise push currVal and its idx to the stack
-            - the items in the stack are the ones that will be processed
-            - each ith element gets processed later, and this element is not in stack yet
-            - therefore to process this element, it must be in the stack
-            - and some later/next element will process it
-    time = o(2n)
-    space = o(n)
-*/
 func nextGreaterElements(nums []int) []int {
     n := len(nums)
     out := make([]int, n)
     for i := 0; i < n; i++ {out[i] = -1}
-    st := [][]int{} // [ [val, idx] ]
+    st := []int{}
     for i := 0; i < 2*n; i++ {
         curr := nums[i%n]
-        for len(st) != 0 && curr > st[len(st)-1][0] {
+        for len(st) != 0 && curr > nums[st[len(st)-1]] {
             top := st[len(st)-1]
-            topIdx := top[1]
             st = st[:len(st)-1]
-            out[topIdx] = curr
+            out[top] = curr
         }
-        // only push elements from the first iteration
-        if i < n {
-            st = append(st, []int{curr, i})
-        }
+        st = append(st, i%n)
     }
     return out
 }
