2eab85041fc042ba6c800ffe1d6043776c9d4593
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-12 20:42:31 -0700

    Time: 3 ms (74.41%) | Memory: 3.6 MB (34.56%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.go
+++ 496-next-greater-element-i/next-greater-element-i.go
@@ -1,27 +1,38 @@
+/*
+    approach: next-greater-element
+    - find the NGR of all elements in nums2
+    - and keep saving each elements from nums2 in a idx map
+    - then using a second pass, loop over nums1
+    - find its idx in nums2 idxMap
+    - then using that idx, take the value we have computed in ngr
+
+    time = o(n2) + o(n1)
+    space = o(n2)
+*/
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
-    // find ngr ( next greater to right ) for all elements in nums2
-    // keeping saving nums2 element to idx 
-    n2Idx := map[int]int{}
-    ngr := make([]int, len(nums2))
+    n1 := len(nums1)
+    n2 := len(nums2)
+    n2IdxMap := map[int]int{}
+    ngr := make([]int, n2)
     st := []int{}
-    for i := len(nums2)-1; i >= 0; i-- {
-        for len(st) != 0 && st[len(st)-1] <= nums2[i] {
+    for i := 0; i < n2; i++ {
+        ngr[i] = -1
+        n2IdxMap[nums2[i]] = i
+
+        curr := nums2[i]
+        for len(st) != 0 && curr > nums2[st[len(st)-1]] {
+            top := st[len(st)-1]
             st = st[:len(st)-1]
+            ngr[top] = curr
         }
-        if len(st) != 0 {
-            ngr[i] = st[len(st)-1]
-        } else {
-            ngr[i] = -1
-        }
-        st = append(st, nums2[i])
-        n2Idx[nums2[i]] = i
+        st = append(st, i)
+
     }
-    out := []int{}
-    for i := 0; i < len(nums1); i++ {
-        num := nums1[i]
-        idxInNums2 := n2Idx[num]
-        ngrVal := ngr[idxInNums2]
-        out = append(out, ngrVal)
+    out := make([]int, n1)
+    for i := 0; i < n1; i++ {
+        idx := n2IdxMap[nums1[i]]
+        out[i] = ngr[idx]
     }
     return out
+
 }
