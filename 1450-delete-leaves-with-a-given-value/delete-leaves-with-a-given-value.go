5e454ef0a50ec882cf1a3a6555b97514dfb47d64
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-16 22:46:21 -0700

    Time: 2 ms (74.51%) | Memory: 5 MB (94.12%) - LeetSync

---
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.go
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func removeLeafNodes(root *TreeNode, target int) *TreeNode {
+    var dfs func(r *TreeNode) 
+    dfs = func(r *TreeNode) {
+        // base
+        if r == nil {return}
+        // logic
+        dfs(r.Left)
+        dfs(r.Right)
+
+        if isLeaf(r.Left) && r.Left.Val == target {
+            r.Left = nil
+        }
+
+        if isLeaf(r.Right) && r.Right.Val == target {
+            r.Right = nil
+        }
+    }
+    dfs(root)
+    if root.Val == target && isLeaf(root) {
+        return nil
+    }
+    return root
+}
+
+func isLeaf(r *TreeNode) bool {
+    if r == nil {return false}
+    return r.Left == nil && r.Right == nil
+}
