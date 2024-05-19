8a74f35a6f8314f61cc3d51e21ac6232e81ec905
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-19 14:40:12 -0700

    Time: 12 ms (98.62%) | Memory: 6.8 MB (82.76%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.go
+++ 503-next-greater-element-ii/next-greater-element-ii.go
@@ -1,41 +1,18 @@
-/*
-    approach: next greater element ; process top first, process ith later
-    - this is a classic ngr question, but we have a circular array
-    - we can let the outter loop run 2*n
-        - inorder to get the correct ith index, we will use i%n
-    - we can use process top element ngr approach
-        - curr element = ith element
-        - "am i(curr) your(top) ngr?"
-        - if yes, pop the top, process it
-            - out[top] will be current value since its the ngr of top
-        - keep doing this until we have elements in stack and until curr is still ngr of new top each time
-    - finally, push the ith idx to stack, some future ith element will process it
-
-    - question, should we push the circular elements again into the stack ?
-        - it works, if you do, nothing breaks
-        - however its not necessary because we have already processed the circular items
-        - how do we detect whether this ith element is already processed or not ? whether to push into stack or not ?
-        - if i > n, it means, that this ith element is from the circular array, we do not have to push it as its already processed
-
-    time = o(2n)
-    space = o(n)
-*/
-
 func nextGreaterElements(nums []int) []int {
     n := len(nums)
+    st := []int{} // indices
     out := make([]int, n)
     for i := 0; i < n; i++ {out[i] = -1}
-    st := []int{}
-    for i := 0; i < 2*n; i++ {
+
+    for i := 0; i < 2 * n; i++ {
         curr := nums[i%n]
         for len(st) != 0 && curr > nums[st[len(st)-1]] {
+            // process top
             top := st[len(st)-1]
             st = st[:len(st)-1]
             out[top] = curr
         }
-        if i < n{
-            st = append(st, i)
-        }
+        st = append(st,i%n)
     }
     return out
 }
