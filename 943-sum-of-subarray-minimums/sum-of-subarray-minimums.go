7f1c318c1c60a04cbbb10b3632f90231ad45a256
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-22 23:59:31 -0700

    Time: 41 ms (72.88%) | Memory: 6.9 MB (88.14%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.go
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.go
@@ -1,33 +1,108 @@
+/*
+    for each subarray we need a minimum
+    instead of forming each subarray
+    find out the number of subarrays if ith ele was a min ele
+
+    approach:
+    - [0,3,1,2,4]
+    - when i is at val 1
+    - i stays minimum till idx 0 on the left hand side
+        - this means, all subarrays ending at i(val 1) from left hand sides are
+        - [1], [3,1], [0,3,1]
+        - total 3 subarrys on left hand side of i
+    - i stays minimum till last idx on the right hand side
+        - this means, all subarrays starting with i(val 1) in right hand side are
+        - [1], [1,2], [1,2,4]
+    - this far, we have counted subarrays ending at i and subarrays starting from i
+    - but i could be a middle element too, and those subarrays are
+        - [0,3,1,2], [0,3,1,2,4], [3,1,2], [3,1,2,4]
+    - now we need sum of min in these subarrays
+    - well we already were evaluating for ith element(val 1) 
+    - meaning if we have n subarrays where 1 is the min
+    - what is the sum of all those n subarrays min
+    - n subarrays, each has a min value of 1
+    - total sum = n * minVal = n * 1
+    - in this case our n is; 9
+    - n = [0,3,1],[3,1],[1],[1,2],[1,2,4],[0,3,1,2],[0,3,1,2,4],[3,1,2],[3,1,2,4]
+    - therefore total sum where 1 is the min element is 9*1 = 9
+    - now repeat the rest for each ith element
+
+    - [0,3,1,2,4]
+
+    - to figure out the number of subarrays where i is the min element
+    - this is NSR and NSL
+    - NSL of val 1 will be at idx -1
+    - how many elements/subarrays on left if NSL is at idx -1 ?
+        - idx - NSL
+        - 2-(-1) = 3
+    - NSR of val 1 will be at idx 5
+    - how many elements/subarrays on right if NSR is at idx 5 ?
+        - NSR - idx
+        - 5-2 = 3
+    - total subarrays = 3*3 = 9
+    
+    - For NSR, NSL ; we can use our basic "process top NSR/NSL via stack" template
+
+*/
 func sumSubarrayMins(arr []int) int {
-    // nsr and nsl ( processing top elements )
+    st := []int{} // indices
     mod := 1000000007
     n := len(arr)
-    totalSum := 0
-    st := []int{} // will hold indicies
+    sum := 0
     for i := 0; i < n; i++ {
-        for len(st) != 0 && arr[i] <= arr[st[len(st)-1]] {
-            idx := st[len(st)-1]
+        curr := arr[i]
+        for len(st) != 0 && curr < arr[st[len(st)-1]] {
+            top := st[len(st)-1]
             st = st[:len(st)-1]
-            countOnRight := i-idx
+            nsr := i
             nsl := -1
-            if len(st) != 0 {nsl = st[len(st)-1]}
-            countOnLeft := idx-nsl
-            totalSubArrs := countOnLeft * countOnRight
-            totalSum += ((arr[idx] * totalSubArrs) % mod)
+            if len(st) > 0 {nsl = st[len(st)-1]}
+            leftCount := top-nsl
+            rightCount := nsr-top
+            totalSum := (leftCount*rightCount) * arr[top]
+            sum = (sum + totalSum) % mod
         }
-        st = append(st,i)
+        st = append(st, i)
     }
+
+    // edge case for nsr processing top
+    // if no ith element was nsr of top
+    // this means, all elements were in increasing order
+    // this means, nsr is len(arr)
+    // process top with nsr set to len(arr) ; i.e n
     for len(st) != 0 {
-        idx := st[len(st)-1]
+        top := st[len(st)-1]
         st = st[:len(st)-1]
-        countOnRight := n-idx
+        nsr := n
         nsl := -1
-        if len(st)!= 0 {
-            nsl = st[len(st)-1]
-        }
-        countOnLeft := idx-nsl
-        totalSubArrs := countOnLeft * countOnRight
-        totalSum = (totalSum+(arr[idx] * totalSubArrs)) % mod
+        if len(st) > 0 {nsl = st[len(st)-1]}
+        leftCount := top-nsl
+        rightCount := nsr-top
+        totalSum := (leftCount*rightCount) * arr[top]
+        sum = (sum + totalSum) % mod
     }
-    return totalSum
+    return sum
 }
+
+/*
+    brute force
+    approach: create all possible subarrays
+    - create all subarrays
+    - keep track of min in that subarray
+    - keep adding min to the total sum
+
+    time = o(n^2)
+    space = o(1)
+*/
+// func sumSubarrayMins(arr []int) int {
+//     mod := 1000000007
+//     total := 0
+//     for i := 0; i < len(arr); i++ {
+//         minSoFar := arr[i]
+//         for j := i; j < len(arr); j++ {
+//             minSoFar = min(minSoFar, arr[j])
+//             total = (total+minSoFar) % mod
+//         }
+//     }
+//     return total
+// }
