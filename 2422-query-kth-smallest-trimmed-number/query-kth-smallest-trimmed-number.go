0d4be767664b3ceeb38c34b01b991d17d15d7e35
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-25 14:42:04 -0700

    Time: 219 ms (33.33%) | Memory: 8.2 MB (47.62%) - LeetSync

--- 2422-query-kth-smallest-trimmed-number/query-kth-smallest-trimmed-number.go
+++ 2422-query-kth-smallest-trimmed-number/query-kth-smallest-trimmed-number.go
@@ -1,54 +1,55 @@
-
 func smallestTrimmedNumbers(nums []string, queries [][]int) []int {
     n := len(nums)
+    q := len(queries)
     out := []int{}
+    for i := 0; i < q; i++ {
 
-    for _, query := range queries {
-        kth := query[0]
+        query := queries[i]
         keepLast := query[1]
-        mh := &maxHeap{}
-        heap.Init(mh)
+        kth := query[0]
+        mx := &maxHeap{items: []*mxNode{}}
 
         for j := 0; j < n; j++ {
             num := nums[j]
             subStr := num[len(num)-keepLast:]
-            heap.Push(mh, &item{value: subStr, index: j})
-            if mh.Len() > kth {
-                heap.Pop(mh)
+            // cant do this, apparently value goes out of bound 
+            // panic: strconv.Atoi: parsing "9288488870527604910029": value out of range
+            // subStrInt, _ := strconv.Atoi(subStr)
+            heap.Push(mx, &mxNode{val: subStr, idx: j})
+            if mx.Len() > kth {
+                heap.Pop(mx)
             }
         }
-
-        result := heap.Pop(mh).(*item)
-        out = append(out, result.index)
+        out = append(out, mx.items[0].idx)
     }
-
     return out
 }
 
-type item struct {
-    value string
-    index int
+type mxNode struct {
+    val string
+    idx int
+}
+type maxHeap struct {
+	items []*mxNode
 }
 
-type maxHeap []*item
-
-func (m maxHeap) Len() int           { return len(m) }
-func (m maxHeap) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
-func (m maxHeap) Less(i, j int) bool {
-    if m[i].value == m[j].value {
-        return m[i].index > m[j].index
+func (m *maxHeap) Less(i, j int) bool {
+	if m.items[i].val == m.items[j].val {
+        return m.items[i].idx > m.items[j].idx
     }
-    return m[i].value > m[j].value
+    return m.items[i].val > m.items[j].val
 }
-
+func (m *maxHeap) Swap(i, j int) {
+	m.items[i], m.items[j] = m.items[j], m.items[i]
+}
+func (m *maxHeap) Len() int {
+	return len(m.items)
+}
 func (m *maxHeap) Push(x interface{}) {
-    *m = append(*m, x.(*item))
+	m.items = append(m.items, x.(*mxNode))
 }
-
 func (m *maxHeap) Pop() interface{} {
-    old := *m
-    n := len(old)
-    x := old[n-1]
-    *m = old[0 : n-1]
-    return x
+	out := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return out
 }
