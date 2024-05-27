e0410b2ddce25231d0b7d3117f89799066d4de0c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-05-27 13:26:02 -0700

    Time: 0 ms (100.00%) | Memory: 2.6 MB (58.06%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.go
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.go
@@ -1,3 +1,31 @@
+func specialArray(nums []int) int {
+    left := 1
+    right := math.MinInt64
+    freqMap := map[int]int{}
+    for i := 0; i < len(nums); i++ {
+        freqMap[nums[i]]++
+        right = max(right, nums[i])
+    }
+
+    for left <= right {
+        mid := left + (right-left)/2
+        // count >= mid
+        count := 0
+        for k, v := range freqMap {
+            if k >= mid {
+                count+=v
+            }
+        }
+        if count == mid {return mid}
+        if count < mid {
+            right = mid-1
+        } else {
+            left = mid+1
+        }
+    }
+    return -1
+}
+// binary search on ans pattern
 // because we are looping over a range in our naive approach
 // ranges are sorted, therefore binary search
 // mid works if count == x
@@ -5,29 +33,28 @@
 // so if count < x; it means there are less number >= x
 // we need to increase our count, which means we need a smaller x value
 // therefore right = mid-1
-func specialArray(nums []int) int {
-    sort.Ints(nums)
-    left := 1
-    right := nums[len(nums)-1]
-    for left <= right {
-        mid := left + (right-left)/2
-        j := 0
-        for j < len(nums)  {
-            if nums[j] < mid {j++; continue}
-            break
-        }
-        count := len(nums)-j
-        if count == mid {return mid}
-        if count < mid {
-            right = mid-1
-        } else {
-            left = mid+1
-        }
-
-    }
-    return -1
-
-}
+// time = o(nlogn) + o(log(maxNum) * n)
+// func specialArray(nums []int) int {
+//     sort.Ints(nums)
+//     left := 1
+//     right := nums[len(nums)-1]
+//     for left <= right {
+//         mid := left + (right-left)/2
+//         j := 0
+//         for j < len(nums) {
+//             if nums[j] < mid {j++; continue}
+//             break
+//         }
+//         count := len(nums)-j
+//         if count == mid {return mid}
+//         if count < mid {
+//             right = mid-1
+//         } else {
+//             left = mid+1
+//         }
+//     }
+//     return -1
+// }
 // ans lies in a range; 1 to max number
 // eval each ans
 // time = o(nlogn) + o(maxNum * n)
