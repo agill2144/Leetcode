924b4453c14fe41d7122ab88ebd686a90a834307
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-01 14:42:00 -0700

    Time: 9 ms (58.88%) | Memory: 6.7 MB (41.25%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.go
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.go
@@ -7,8 +7,8 @@
  * }
  */
 func diameterOfBinaryTree(root *TreeNode) int {
-    ans := 0
     var dfs func(r *TreeNode) int
+    maxDepth := 0
     dfs = func(r *TreeNode) int {
         // base
         if r == nil {return 0}
@@ -16,9 +16,10 @@ func diameterOfBinaryTree(root *TreeNode) int {
         // logic
         left := dfs(r.Left)
         right := dfs(r.Right)
-        ans = max(ans, left+right)
-        return max(left, right)+1
+        maxDepth = max(maxDepth, left+right)
+        return max(left,right)+1
+
     }
     dfs(root)
-    return ans
+    return maxDepth
 }
