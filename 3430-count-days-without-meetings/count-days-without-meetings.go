7ca137123cc6283140210bb509f1712d29f88fe8
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-01 21:49:01 -0700

    Time: 240 ms (100.00%) | Memory: 18.4 MB (100.00%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.go
@@ -0,0 +1,32 @@
+func countDays(days int, meetings [][]int) int {
+    sort.Slice(meetings, func(i, j int)bool{return meetings[i][0] < meetings[j][0]})
+    merged := [][]int{}
+    for i := 0; i < len(meetings); i++ {
+        start, end := meetings[i][0], meetings[i][1]
+        if len(merged) == 0 {
+            merged = append(merged, meetings[i])
+        } else {
+            prev := merged[len(merged)-1]
+            prevStart, prevEnd := prev[0],prev[1]
+            if start <= prevEnd {
+                merged = merged[:len(merged)-1]
+                newStart := min(prevStart, start)
+                newEnd := max(end, prevEnd)
+                merged = append(merged, []int{newStart, newEnd})
+            } else {
+                merged = append(merged, meetings[i])
+            }
+        }
+    }
+    totalMeetingDays := 0
+    for i := 0; i < len(merged); i++ {
+        start, end := merged[i][0], merged[i][1]
+        totalMeetingDays += (end-start+1)
+    }
+    return days-totalMeetingDays
+}
+
+func abs(x int) int {
+    if x < 0 {return x *-1}
+    return x
+}
