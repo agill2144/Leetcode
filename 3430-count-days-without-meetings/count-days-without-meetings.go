dc458177b48d146fd9cbd0b2cf5b4e5d3f70c607
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-01 21:49:27 -0700

    Time: 233 ms (100.00%) | Memory: 17.8 MB (100.00%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.go
+++ 3430-count-days-without-meetings/count-days-without-meetings.go
@@ -10,7 +10,7 @@ func countDays(days int, meetings [][]int) int {
             prevStart, prevEnd := prev[0],prev[1]
             if start <= prevEnd {
                 merged = merged[:len(merged)-1]
-                newStart := min(prevStart, start)
+                newStart := prevStart
                 newEnd := max(end, prevEnd)
                 merged = append(merged, []int{newStart, newEnd})
             } else {
