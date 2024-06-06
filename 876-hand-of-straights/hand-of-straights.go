2c361ca9699d06aab63a5394679db2a914eead22
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-06 10:09:20 -0700

    Time: 37 ms (64.86%) | Memory: 7.5 MB (36.49%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.go
+++ 876-hand-of-straights/hand-of-straights.go
@@ -1,65 +1,94 @@
+
 func isNStraightHand(hand []int, groupSize int) bool {
-    n := len(hand)
-    if n % groupSize != 0 {return false}
-    mh := &minHeap{items: []int{}}
-    freq := map[int]int{}
-    for i := 0; i < len(hand); i++ {
-        if _, ok := freq[hand[i]]; !ok {
-            heap.Push(mh, hand[i])
-        }
-        freq[hand[i]]++
-    }
+	if len(hand) == 0 || len(hand)%groupSize != 0 {
+		return false
+	}
 
-    grpSize := 0 
-    lastGrpNum := -1
-    pb := []int{} 
-    for mh.Len() != 0 {
-        popped := heap.Pop(mh).(int)
-        if grpSize == 0 {
-            lastGrpNum = popped
-            grpSize = 1
-        } else {
-            if lastGrpNum+1 != popped {return false}
-            lastGrpNum = popped
-            grpSize++
-        }
+	sort.Ints(hand)
 
-        freq[popped]--
-        if freq[popped] > 0 {
-            pb = append(pb, popped)
-        }
+	cardCount := make(map[int]int)
+	for _, card := range hand {
+		cardCount[card]++
+	}
 
-        if grpSize == groupSize {
-            grpSize = 0
-            for k := 0; k < len(pb); k++ {heap.Push(mh, pb[k])}
-            pb = []int{}
-        }
-    }
+	for _, card := range hand {
+		if cardCount[card] == 0 {
+			continue
+		}
 
-    return grpSize == 0 && mh.Len() == 0
-}
+		for i := 0; i < groupSize; i++ {
+			if cardCount[card+i] == 0 {
+				return false
+			}
+			cardCount[card+i]--
+		}
+	}
 
-type minHeap struct {
-	items []int
-}
+	return true
 
-func (m *minHeap) Less(i, j int) bool {
-	return m.items[i] < m.items[j]
 }
-func (m *minHeap) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-func (m *minHeap) Len() int {
-	return len(m.items)
-}
-func (m *minHeap) Push(x interface{}) {
-	m.items = append(m.items, x.(int))
-}
-func (m *minHeap) Pop() interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
-}
+// func isNStraightHand(hand []int, groupSize int) bool {
+//     n := len(hand)
+//     if n % groupSize != 0 {return false}
+//     mh := &minHeap{items: []int{}}
+//     freq := map[int]int{}
+//     for i := 0; i < len(hand); i++ {
+//         if _, ok := freq[hand[i]]; !ok {
+//             heap.Push(mh, hand[i])
+//         }
+//         freq[hand[i]]++
+//     }
+
+//     grpSize := 0 
+//     lastGrpNum := -1
+//     pb := []int{} 
+//     for mh.Len() != 0 {
+//         popped := heap.Pop(mh).(int)
+//         if grpSize == 0 {
+//             lastGrpNum = popped
+//             grpSize = 1
+//         } else {
+//             if lastGrpNum+1 != popped {return false}
+//             lastGrpNum = popped
+//             grpSize++
+//         }
+
+//         freq[popped]--
+//         if freq[popped] > 0 {
+//             pb = append(pb, popped)
+//         }
+
+//         if grpSize == groupSize {
+//             grpSize = 0
+//             for k := 0; k < len(pb); k++ {heap.Push(mh, pb[k])}
+//             pb = []int{}
+//         }
+//     }
+
+//     return grpSize == 0 && mh.Len() == 0
+// }
+
+// type minHeap struct {
+// 	items []int
+// }
+
+// func (m *minHeap) Less(i, j int) bool {
+// 	return m.items[i] < m.items[j]
+// }
+// func (m *minHeap) Swap(i, j int) {
+// 	m.items[i], m.items[j] = m.items[j], m.items[i]
+// }
+// func (m *minHeap) Len() int {
+// 	return len(m.items)
+// }
+// func (m *minHeap) Push(x interface{}) {
+// 	m.items = append(m.items, x.(int))
+// }
+// func (m *minHeap) Pop() interface{} {
+// 	out := m.items[len(m.items)-1]
+// 	m.items = m.items[:len(m.items)-1]
+// 	return out
+// }
 
 
 // slow linear approach using bucket sort
