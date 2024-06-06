bd481a92a0fac4f47a01155c98fe2e0e30a6bd7a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-06 12:59:39 -0700

    Time: 0 ms (100.00%) | Memory: 4.3 MB (41.07%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.go
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.go
@@ -23,8 +23,16 @@ func evalRPN(tokens []string) int {
             st = st[:len(st)-2]
             st = append(st, first/second)
         } else {
-            nInt, _ := strconv.Atoi(currToken)
-            st = append(st, nInt)
+            curr := 0
+            isNegative := false
+            for k := 0; k < len(currToken); k++ {
+                if k == 0 && currToken[k] == '-' {isNegative = true; continue}
+                curr = curr * 10 + int(currToken[k]-'0')
+            }
+            if isNegative {
+                curr *= -1
+            }
+            st = append(st, curr)
         }
     }
     return st[len(st)-1]
