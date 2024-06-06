d8744bce7a29d09114eaca93ab5802e53bbffb60
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-06 12:45:56 -0700

    Time: 1 ms (75.29%) | Memory: 2.3 MB (71.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.go
+++ 20-valid-parentheses/valid-parentheses.go
@@ -1,22 +1,17 @@
 func isValid(s string) bool {
-    validOpens := map[byte]byte{
-        ')': '(', ']': '[','}':'{',
+    valid := map[byte]byte{
+        '}':'{',']':'[',')':'(',
     }
     st := []byte{}
     for i := 0; i < len(s); i++ {
-        char := s[i]
-        if char == '(' || char == '[' || char == '{' {
-            st = append(st, char)
+        if s[i] == '(' || s[i] == '{' || s[i] == '[' {
+            st = append(st, s[i])
         } else {
-            if len(st) == 0 {
-                return false
-            }
-            if validOpens[char] != st[len(st)-1] {
-                return false
-            }
+            if len(st) == 0 {return false}
+            validOpen := valid[s[i]]
+            if st[len(st)-1] != validOpen {return false}
             st = st[:len(st)-1]
         }
     }
-    if len(st) != 0 {return false}
-    return true
+    return len(st) == 0 
 }
