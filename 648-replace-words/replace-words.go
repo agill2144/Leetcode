246e177039d2cf71ca2c97b5f2278a5ae128ffd3
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-06 17:46:48 -0700

    Time: 173 ms (13.89%) | Memory: 21 MB (47.22%) - LeetSync

--- 648-replace-words/replace-words.go
+++ 648-replace-words/replace-words.go
@@ -1,6 +1,9 @@
+// with trie
 func replaceWords(dictionary []string, sentence string) string {
-    set := map[string]struct{}{}
-    for i := 0; i < len(dictionary); i++ {set[dictionary[i]] = struct{}{}}
+    root := &trieNode{childs: [26]*trieNode{}}
+    for i := 0; i < len(dictionary); i++ {
+        root.insert(dictionary[i])
+    }
     out := new(strings.Builder)
     words := strings.Split(sentence, " ")
     for i := 0; i < len(words); i++ {
@@ -8,7 +11,7 @@ func replaceWords(dictionary []string, sentence string) string {
         found := false
         for j := 0; j < len(word); j++ {
             subStr := word[:j+1]
-            if _, ok := set[subStr]; ok {
+            if root.search(subStr) {
                 found = true
                 out.WriteString(subStr)
                 break
@@ -19,3 +22,55 @@ func replaceWords(dictionary []string, sentence string) string {
     }
     return out.String()
 }
+
+type trieNode struct {
+	isEnd  bool
+	childs [26]*trieNode
+}
+
+func (r *trieNode) insert(word string) {
+	curr := r
+	for i := 0; i < len(word); i++ {
+		idx := int(word[i] - 'a')
+		if curr.childs[idx] == nil {
+			curr.childs[idx] = &trieNode{childs: [26]*trieNode{}}
+		}
+		curr = curr.childs[idx]
+	}
+	curr.isEnd = true
+}
+
+func (r *trieNode) search(word string) bool {
+	curr := r
+	for i := 0; i < len(word); i++ {
+		idx := int(word[i] - 'a')
+		if curr.childs[idx] == nil {
+			return false
+		}
+		curr = curr.childs[idx]
+	}
+	return curr.isEnd
+}
+
+// using hashset and without trie
+// func replaceWords(dictionary []string, sentence string) string {
+//     set := map[string]struct{}{}
+//     for i := 0; i < len(dictionary); i++ {set[dictionary[i]] = struct{}{}}
+//     out := new(strings.Builder)
+//     words := strings.Split(sentence, " ")
+//     for i := 0; i < len(words); i++ {
+//         word := words[i]
+//         found := false
+//         for j := 0; j < len(word); j++ {
+//             subStr := word[:j+1]
+//             if _, ok := set[subStr]; ok {
+//                 found = true
+//                 out.WriteString(subStr)
+//                 break
+//             }
+//         }
+//         if !found {out.WriteString(word)}
+//         if i != len(words)-1 {out.WriteString(" ")}
+//     }
+//     return out.String()
+// }
