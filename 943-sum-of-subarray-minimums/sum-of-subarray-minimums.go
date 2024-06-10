662484f4ba3b5ad05780d578c337d3a2ede10caa
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-09 22:58:51 -0700

    Time: 36 ms (95.08%) | Memory: 7.1 MB (70.49%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.go
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.go
@@ -1,5 +1,49 @@
 /*
     approach: nsr + nsl ( process top ) pattern
+    - instead of creating each possible subarray
+    - we will focus on evaluating each elements contribution to our total final sum
+    - each element gets added to our total sum based on how many subarrays it stays as minimum value in
+    - example; [2,6,7,3,8,9,1]
+    - lets say we are evaluating 3's contribution
+    - 3 stays minimum in this window of the array [6,7,3,8,9]
+    - how many subarrays are there where 3 is included? 
+        - from left side; [6,7,3] [7,3], [3] 
+        - from right side; [3,8], [3,8,9]
+        - across; [6,7,3,8], [6,7,3,8,9], [7,3,8], [7,3,8,9]
+        - total; 9 subarrays
+    - if we figure out that there are 9 subarrays where 3 is the min in ALL 9 subarrays
+    - what is 3's contribution ? 3*9 = 27 ( we need to add 3, 9 times; i.e 3*9)
+
+    - but how can we form/count all of the above possible subarrays which has 3 ?
+        - to count number of subarrays between left and i which includes ith value
+            - i-left+1 ; i.e window size
+            - i = 3; left = 1; leftCount = 3-1+1 = 3 subarrays on left side
+        - to count number of subarrays between right and i which includes ith value
+            - right-i+1; i.e window size
+            - i = 3; right = 5; rightCount = 5-3+1 = 3 subarrays on right side
+        - to count acorss; thats just leftCount*rightCount
+        - therefore total count = leftCount*rightCount
+
+    - now how do we find these left and right ptr for each ith value?
+        - 3 was the min value in a window as long as no smaller value came in
+        - [2,6,7,3,8,9,1]
+        - on left; we stopped at idx 0 because its smaller than 3
+        - THIS IS NSL ; we are looking for NSL for each ith element
+        - same on the right side; 
+        - on right; we stopped at last idx, because its smaller than 3
+        - THIS IS NSR; we are looking for NSR for each ith element    
+    - NSR and NSL can be implemented in 1 pass using "process top" 
+        - am i(ith val) your(top-of-stack) nsr ? ; nsl is next top of stack
+    - edge cases
+        1. stack may have elements left after our intial pass
+            - do we need to do something with elements that are still in stack?
+            - yes, because we were processing nsr, and if there are elements in stack
+            - for example; [1,2,3,4]
+            - it means we did not find a ith element that was a nsr of a previous element
+            - therefore these elements range/window can be the whole array
+            - therefore nsr for each of the remaining element is n (size of array)
+    time = o(n)
+    space = o(n)
 */
 func sumSubarrayMins(arr []int) int {
     mod := 1000000007
