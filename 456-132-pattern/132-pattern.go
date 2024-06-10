44822930eff11cce3b8b74d8f5e5f26947393b4c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-09 20:44:25 -0700

    Time: 81 ms (39.53%) | Memory: 14.9 MB (53.49%) - LeetSync

--- 456-132-pattern/132-pattern.go
+++ 456-132-pattern/132-pattern.go
@@ -3,23 +3,18 @@ func find132pattern(nums []int) bool {
 	if n < 3 {
 		return false
 	}
+    st := []int{}
+    nums3 := math.MinInt64 // k
+    for i := n-1; i >= 0; i-- {
+        if nums[i] < nums3 {
+            return true
+        }
 
-	// Stack to keep track of potential nums[j] and nums[k]
-	stack := []int{}
-	// Variable to keep track of nums[k] in 132 pattern
-	k := -1 << 63 // This will be the smallest value initially
-
-	for i := n - 1; i >= 0; i-- {
-		// If we find nums[i] < nums[k], we have a valid 132 pattern
-		if nums[i] < k {
-			return true
-		}
-		// Update k and stack with potential nums[j] values
-		for len(stack) > 0 && nums[i] > stack[len(stack)-1] {
-			k = stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-		}
-		stack = append(stack, nums[i])
-	}
-	return false
+        for len(st) != 0 && nums[i] > st[len(st)-1] {
+            nums3 = st[len(st)-1]
+            st = st[:len(st)-1]
+        }
+        st = append(st, nums[i])
+    }
+    return false
 }
