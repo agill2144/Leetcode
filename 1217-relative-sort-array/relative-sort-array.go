961b8693059536fa58d0d3982de9f65b013d1d18
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-10 20:29:49 -0700

    Time: 0 ms (100.00%) | Memory: 2.5 MB (89.74%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.go
+++ 1217-relative-sort-array/relative-sort-array.go
@@ -1,7 +1,9 @@
+// bucket sort; the range of numbers are small
 func relativeSortArray(arr1 []int, arr2 []int) []int {
     bucket := make([]int, 1001)
     for i := 0; i < len(arr1); i++ {bucket[arr1[i]]++}
     out := []int{}
+
     for i := 0; i < len(arr2); i++ {
         val := arr2[i]
         for bucket[val] != 0 {
