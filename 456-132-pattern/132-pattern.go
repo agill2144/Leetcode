c400c5e6c1e5ea6fcba22e8b96d96b0f269fa603
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-16 23:48:19 -0700

    Time: 67 ms (90.20%) | Memory: 17.3 MB (27.45%) - LeetSync

--- 456-132-pattern/132-pattern.go
+++ 456-132-pattern/132-pattern.go
@@ -1,13 +1,26 @@
 func find132pattern(nums []int) bool {
-    st := []int{} // indices, acting as nums[j]
+    st := []int{} // indices, viable numsK candidates
     n := len(nums)
     numsK := math.MinInt64
     for i := n-1; i >= 0; i-- {
+        // once we have numsK, numsJ becomes irrelevant
+        // because numsJ was the largest value once discovered and processed/setted numsK val
+        // therefore nums[k] < nums[j] is already satisfied is numsK is set
+        // if this number is < numsK, it means we have found nums[i] < nums[k]
+        // return true
         if nums[i] < numsK {return true}
+
+        // the iterator ptr is acting as our numsJ val
+        /// numsJ is supposed to the largest and its supposed to set numsK value
+        // as soon as we run into a ele > top of stack(potential numsK vals), we have found nums[k] < nums[j] (and j < k)
+        // process the stack until nums[i] > top of stack, and each popped element is a better numsK val
+        // therefore each popped element becomes numsK ( overriden each time )
         for len(st) != 0 && nums[i] > nums[st[len(st)-1]] {
             numsK = nums[st[len(st)-1]]
             st = st[:len(st)-1]
         }
+
+        // push this value as potential numsK val
         st = append(st, i)
     }
     return false
