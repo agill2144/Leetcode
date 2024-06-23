93a79e20e067c8c6989197260d2c20a73e839eb4
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-23 16:33:01 -0700

    Time: 0 ms (100.00%) | Memory: 2.7 MB (100.00%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.go
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.go
@@ -6,6 +6,16 @@ func minimumAverage(nums []int) float64 {
     for left < right {
         avg := (float64(nums[left]) + float64(nums[right])) / 2.0
         minAvg = min(minAvg, avg)
+        // which ptr to move away from ?
+        // if we move away from left ( smaller val )
+        // the sum will be bigger, therefore avg will be bigger
+        // if we move away from right ( smaller val )
+        // the sum will be smaller, therefore avg will be smaller
+        // so move away from right ?
+        // HOWEVER!
+        // the question states; "Remove the smallest element, minElement, and the largest element maxElement, from nums"
+        // so once we have seen/used left and right vals, we can no longer use them
+        // therefore moving away from both!
         left++
         right--
     }
