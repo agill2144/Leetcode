6e1c0a003d09fa4ae0fd05ce2ca5b6045f764c9f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-23 21:34:38 -0700

    Time: 1 ms (76.03%) | Memory: 2.3 MB (6.28%) - LeetSync

--- 75-sort-colors/sort-colors.go
+++ 75-sort-colors/sort-colors.go
@@ -1,27 +1,44 @@
 func sortColors(nums []int)  {
-    zeros := 0
-    ones := 0
-    twos := 0
-    for i := 0; i < len(nums); i++ {
-        if nums[i] == 0 {
-            zeros++
-        } else if nums[i] == 1 {
-            ones++
-        } else {
-            twos++
-        }
-    }
-    for i := 0; i < len(nums); i++ {
-        if zeros > 0 {
-            nums[i] = 0
-            zeros--
-        } else if ones > 0 {
-            nums[i] = 1
-            ones--
-        } else {
-            nums[i] = 2
+    zeros, twos := 0, len(nums)-1
+    i := 0
+    for i <= twos {
+        if nums[i] == 2 {
+            nums[i], nums[twos] = nums[twos], nums[i]
             twos--
+        } else if nums[i] == 0 {
+            nums[i], nums[zeros] = nums[zeros], nums[i]
+            zeros++
+            if i < zeros {i++}
+        } else {
+            i++
         }
     }
-    
 }
+
+// func sortColors(nums []int)  {
+//     zeros := 0
+//     ones := 0
+//     twos := 0
+//     for i := 0; i < len(nums); i++ {
+//         if nums[i] == 0 {
+//             zeros++
+//         } else if nums[i] == 1 {
+//             ones++
+//         } else {
+//             twos++
+//         }
+//     }
+//     for i := 0; i < len(nums); i++ {
+//         if zeros > 0 {
+//             nums[i] = 0
+//             zeros--
+//         } else if ones > 0 {
+//             nums[i] = 1
+//             ones--
+//         } else {
+//             nums[i] = 2
+//             twos--
+//         }
+//     }
+    
+// }
