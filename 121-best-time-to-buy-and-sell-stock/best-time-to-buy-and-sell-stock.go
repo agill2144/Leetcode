57305fde3ec24c6c80d7e9bc63f4f84d9f58b0ba
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-23 22:26:02 -0700

    Time: 106 ms (35.79%) | Memory: 9.3 MB (28.62%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.go
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.go
@@ -1,16 +1,27 @@
 func maxProfit(prices []int) int {
     n := len(prices)
-    suffix := make([]int, n)
-    for i := n-2; i >= 0; i-- {
-        next := prices[i+1]
-        maxSoFar := suffix[i+1]
-        suffix[i] = max(maxSoFar, next)        
-    }
+    buy := prices[0]
     maxProfit := 0
-    for i := 0; i < len(prices); i++ {
-        buy := prices[i]
-        sell := suffix[i]
+    for i := 1; i < n; i++ {
+        sell := prices[i]
         maxProfit = max(maxProfit, sell-buy)
+        buy = min(buy, sell)        
     }
     return maxProfit
 }
+// func maxProfit(prices []int) int {
+//     n := len(prices)
+//     suffix := make([]int, n)
+//     for i := n-2; i >= 0; i-- {
+//         next := prices[i+1]
+//         maxSoFar := suffix[i+1]
+//         suffix[i] = max(maxSoFar, next)        
+//     }
+//     maxProfit := 0
+//     for i := 0; i < len(prices); i++ {
+//         buy := prices[i]
+//         sell := suffix[i]
+//         maxProfit = max(maxProfit, sell-buy)
+//     }
+//     return maxProfit
+// }
