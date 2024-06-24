a2a74dc6baa185f43f13b8e29145c855eabbf161
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-23 21:29:26 -0700

    Time: 3 ms (90.33%) | Memory: 4.2 MB (30.80%) - LeetSync

---
+++ 1-two-sum/two-sum.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+    idxMap := map[int]int{}
+    for i := 0; i < len(nums); i++ {
+        diff := target-nums[i]
+        idx, ok := idxMap[diff]
+        if ok {
+            return []int{idx, i}
+        }
+        idxMap[nums[i]] = i
+    }
+    return nil
+}
