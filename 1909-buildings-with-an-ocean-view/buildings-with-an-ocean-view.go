af62dfe6867dd8a2481633c7785562f9ab2b97ef
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-24 22:07:55 -0700

    Time: 109 ms (10.78%) | Memory: 16.9 MB (5.99%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.go
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.go
@@ -1,27 +1,48 @@
 func findBuildings(heights []int) []int {
-    nextGreater := nextGreaterOnRight(heights)
-    out := []int{}
-    for i := 0; i < len(nextGreater); i++ {
-        if nextGreater[i] == -1 {out = append(out, i)}
+    n := len(heights)
+    tallestSoFar := heights[n-1]
+    out := []int{n-1}
+    for i := n-2; i >= 0; i-- {
+        if heights[i] > tallestSoFar {
+            out = append(out, i)
+        }
+        tallestSoFar = max(tallestSoFar, heights[i])
+    }
+    for i := 0; i < len(out)/2; i++ {
+        out[i], out[len(out)-1-i] = out[len(out)-1-i], out[i]
     }
     return out
 }
 
-func nextGreaterOnRight(nums []int) []int {
-    // process top
-    st := []int{} //idx
-    n := len(nums)
-    out := make([]int, n)
-    for i := 0; i < n; i++ {
-        out[i] = -1
-    }
-    for i := 0; i < n; i++ {
-        for len(st) != 0 && nums[i] >= nums[st[len(st)-1]] {
-            top := st[len(st)-1]
-            st = st[:len(st)-1]
-            out[top] = i
-        }
-        st = append(st, i)
-    }
-    return out
-}
+// next greater on right ( >= on right )
+// if an ith building has a ngr, then this building cannot see the ocean
+// if an ith building does not have a ngr, it can see the ocean
+// time = o(2n) = o(n)
+// space = o(n) for st
+// func findBuildings(heights []int) []int {
+//     nextGreater := nextGreaterOnRight(heights)
+//     out := []int{}
+//     for i := 0; i < len(nextGreater); i++ {
+//         if nextGreater[i] == -1 {out = append(out, i)}
+//     }
+//     return out
+// }
+
+// func nextGreaterOnRight(nums []int) []int {
+//     // process top
+//     st := []int{} //idx
+//     n := len(nums)
+//     out := make([]int, n)
+//     for i := 0; i < n; i++ {
+//         out[i] = -1
+//     }
+//     for i := 0; i < n; i++ {
+//         for len(st) != 0 && nums[i] >= nums[st[len(st)-1]] {
+//             top := st[len(st)-1]
+//             st = st[:len(st)-1]
+//             out[top] = i
+//         }
+//         st = append(st, i)
+//     }
+//     return out
+// }
