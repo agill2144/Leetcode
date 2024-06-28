6b1d7f95c8d3975f338d709f23f046a0819a6198
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-06-27 23:17:27 -0700

    Time: 101 ms (90.00%) | Memory: 8.6 MB (60.00%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.go
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.go
@@ -1,44 +1,70 @@
+import ("math")
 func minCost(colors string, neededTime []int) int {
+    count := 0
+    min := math.MaxInt64
+    second := math.MaxInt64
     totalTime := 0
-    n := len(colors)
-    pq := &minHeap{items: []int{}}
-    i := 0
-    for i < n-1 {
-        for i < n-1 && colors[i] == colors[i+1] {
-            heap.Push(pq, neededTime[i])
-            i++
+    for i := 0; i < len(colors); i++ {
+        if i == 0 {
+            min = neededTime[i]
+            count = 1
+            continue
         }
-        heap.Push(pq, neededTime[i])        
-        
-        for pq.Len() > 1 {
-            minTime := heap.Pop(pq).(int)
-            totalTime += minTime            
+
+        if colors[i] == colors[i-1] {
+            count++
+            if neededTime[i] < min {
+                second = min
+                min = neededTime[i]
+            } else if neededTime[i] < second {
+                second = neededTime[i]
+            }
+
+            if count == 2 {
+                count = 1
+                totalTime += min
+                min = second
+                second = math.MaxInt64
+            }
+        } else {
+            count = 1
+            min = neededTime[i]
+            second = math.MaxInt64
         }
-        heap.Pop(pq)
-        i++   
     }
     return totalTime
 }
 
-
-type minHeap struct {
-	items []int
-}
-
-func (m *minHeap) Less(i, j int) bool {
-	return m.items[i] < m.items[j]
-}
-func (m *minHeap) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-func (m *minHeap) Len() int {
-	return len(m.items)
-}
-func (m *minHeap) Push(x interface{}) {
-	m.items = append(m.items, x.(int))
-}
-func (m *minHeap) Pop() interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
-}
+// simulate using a stack
+// time = o(n)
+// space = o(n)
+// func minCost(colors string, neededTime []int) int {
+//     type stNode struct {
+//         color byte
+//         time int
+//     }
+//     st := []stNode{}
+//     totalTime := 0
+//     for i := 0; i < len(colors); i++ {
+//         currColor := colors[i]
+//         currTime := neededTime[i]
+//         pushCurr := true
+//         for len(st) != 0 && currColor == st[len(st)-1].color {
+//             // drop the one that takes least amount of time
+//             top := st[len(st)-1]
+//             topTime := top.time
+//             if currTime < topTime {
+//                 pushCurr = false
+//                 totalTime += currTime
+//                 break
+//             } else {
+//                 st = st[:len(st)-1]
+//                 totalTime += topTime
+//             }
+//         }
+//         if pushCurr {
+//             st = append(st, stNode{color: currColor, time: currTime})
+//         }
+//     }
+//     return totalTime
+// }
