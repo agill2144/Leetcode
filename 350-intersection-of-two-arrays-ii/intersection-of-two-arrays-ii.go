aaa9773d8ee72e39aa934f4ee1c22634f4c7f52d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-01 18:54:23 -0700

    Time: 3 ms (68.73%) | Memory: 2.8 MB (72.78%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
@@ -1,14 +1,35 @@
 func intersect(nums1 []int, nums2 []int) []int {
-    n1Freq := map[int]int{}
-    for i := 0; i < len(nums1); i++ {
-        n1Freq[nums1[i]]++
-    }
+    sort.Ints(nums1)
+    sort.Ints(nums2)
+    n1, n2 := 0, 0
     out := []int{}
-    for i := 0; i < len(nums2); i++ {
-        if val := n1Freq[nums2[i]]; val > 0 {
-            out = append(out, nums2[i])
-            n1Freq[nums2[i]]--
+    for n1 < len(nums1) && n2 < len(nums2) {
+        n1Val := nums1[n1]
+        n2Val := nums2[n2]
+        if n1Val == n2Val {
+            out = append(out, n1Val)
+            n1++
+            n2++
+        } else if n1Val < n2Val {
+            n1++
+        } else {
+            n2++
         }
     }
     return out
 }
+
+// func intersect(nums1 []int, nums2 []int) []int {
+//     n1Freq := map[int]int{}
+//     for i := 0; i < len(nums1); i++ {
+//         n1Freq[nums1[i]]++
+//     }
+//     out := []int{}
+//     for i := 0; i < len(nums2); i++ {
+//         if val := n1Freq[nums2[i]]; val > 0 {
+//             out = append(out, nums2[i])
+//             n1Freq[nums2[i]]--
+//         }
+//     }
+//     return out
+// }
