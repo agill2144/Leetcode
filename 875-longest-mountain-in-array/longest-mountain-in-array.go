e0135458725c2acbb78d20fc9c7601e68bec024d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-01 20:57:52 -0700

    Time: 9 ms (90.00%) | Memory: 6.5 MB (50.00%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.go
+++ 875-longest-mountain-in-array/longest-mountain-in-array.go
@@ -3,13 +3,14 @@ func longestMountain(arr []int) int {
     i := 0
     maxDist := 0
     for i < len(arr) {
-        
         // walk up the mountain
         start := i
         for i+1 < len(arr) && arr[i+1] > arr[i] {
             i++
         }
-        if start == i {i++; continue}
+        // next value is smaller, therefore being a new start point over again
+        // this means, start ptr we assumed as a good start point is not a good start point
+        if i == start {i++; continue}
 
         peakIdx := i
         // i at some peak val from left hand side
@@ -18,8 +19,18 @@ func longestMountain(arr []int) int {
         for i+1 < len(arr) && arr[i] > arr[i+1] {
             i++
         }
-        if i == peakIdx {i++; continue}
-
+        
+        // if peakIdx a valid peakIdx ?
+        // peakIdx valid if i ended up > peakIdx
+        // or i != peakIdx
+        // but if i == peakIdx, it means value next to peakIdx is not smaller, 
+        // therefore this is not a good peak idx
+        // start all over again
+        if i == peakIdx {
+            i++
+            continue
+        }
+        // now we have a valid mountain, take it size and save it if better than last size
         dist := i-start+1
         maxDist = max(maxDist, dist)
     }
