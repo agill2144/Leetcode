5062a999548b57e8cb601c3e546952a311356d42
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-07 15:55:05 -0700

    Time: 23 ms (45.83%) | Memory: 6.7 MB (82.64%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.go
+++ 503-next-greater-element-ii/next-greater-element-ii.go
@@ -1,19 +1,37 @@
-// processing ith element also works
-// just run the loop 2 times
+// processing top pattern ( ideal pattern )
 func nextGreaterElements(nums []int) []int {
     n := len(nums)
-    st := []int{} // vals    
+    st := []int{} // idx
     out := make([]int, n)
     for i := 0; i < n; i++ {out[i] = -1}
-
-    for i := (n-1)*2; i >= 0 ; i-- {
-        for len(st) != 0 && st[len(st)-1] <= nums[i%n] {
+    for i := 0; i < 2*n; i++ {
+        sanitizedI := i%n
+        for len(st) != 0 && nums[sanitizedI] > nums[st[len(st)-1]] {
+            top := st[len(st)-1]
             st = st[:len(st)-1]
+            out[top] = nums[sanitizedI]
         }
-        if len(st) != 0 {
-            out[i%n] = st[len(st)-1]
-        }
-        st = append(st, nums[i%n])
+        st = append(st, sanitizedI)
     }
     return out
 }
+
+// processing ith element also works
+// just run the loop 2 times
+// func nextGreaterElements(nums []int) []int {
+//     n := len(nums)
+//     st := []int{} // vals    
+//     out := make([]int, n)
+//     for i := 0; i < n; i++ {out[i] = -1}
+
+//     for i := (n-1)*2; i >= 0 ; i-- {
+//         for len(st) != 0 && st[len(st)-1] <= nums[i%n] {
+//             st = st[:len(st)-1]
+//         }
+//         if len(st) != 0 {
+//             out[i%n] = st[len(st)-1]
+//         }
+//         st = append(st, nums[i%n])
+//     }
+//     return out
+// }
