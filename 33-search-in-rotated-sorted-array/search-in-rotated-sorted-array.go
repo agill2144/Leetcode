11f0596a5b8450aeea3abb695a590a01ec2f68c6
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-06 21:34:01 -0700

    Time: 0 ms (100.00%) | Memory: 2.7 MB (72.50%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
@@ -1,42 +1,35 @@
-/*
-    The main gist:
-    - find sorted half, check if target is within sorted half
-
-    approach: binary search
-    - if mid is our target, return
-    - find sorted half
-    - if target is within sorted half, search in that area
-    - otherwise search in the other half
-
-    time = o(logn)
-    space = o(1)
-*/
+// rotated sorted array pattern #1: find the sorted half from mid's perspective
+// then check if target is in sorted half ; if yes take the search to that side
+// if no; take the search to the other side
 func search(nums []int, target int) int {
+    n := len(nums)
     left := 0
-    right := len(nums)-1
-
+    right := n-1
     for left <= right {
         mid := left + (right-left)/2
-        if nums[mid] == target {return mid}
-
-        // if left is sorted
-        if nums[left] <= nums[mid] {
-            // target is within left side ?
+        if nums[mid] == target {
+            return mid
+        }
+        
+        // is left sorted?
+        if nums[mid] >= nums[left] {
+            // left sorted; is target in left side ?
             if target >= nums[left] && target <= nums[mid] {
-                // search this side
+                // yes it is, take search to left
                 right = mid-1
             } else {
-                // search other side
+                // no its not, take search to right
                 left = mid+1
             }
-        } else { // right side is sorted
-            // target within right side?
+        } else {
+            // right sorted; is target in right side?
             if target >= nums[mid] && target <= nums[right] {
+                // yes it is, take the search to right side
                 left = mid+1
             } else {
+                // no its not, take the search on left side
                 right = mid-1
             }
-            
         }
     }
     return -1
