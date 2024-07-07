c54b20d7133e05f0dda8fe98b006669075f20c18
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-07 11:02:22 -0700

    Time: 3 ms (28.70%) | Memory: 2.6 MB (30.67%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -1,37 +1,27 @@
 /*
-    The main gist
-    - min element is always in unsorted half of the array
-
-    approach: binary search
-    - if left element <= right element, we are looking at a sorted array
-        - therefore we return smallest element ( i.e left element )
-    - mid is min if its smallest amongst its immediate neighbors
-    - find the unsorted half, and take the search there
-
-    time = o(logn)
-    space = o(1)
+    rotated sorted array pattern #2
+    - find the sorted half
+    - min always lies in the unsorted half
+        - unless curr window is already sorted
 */
 func findMin(nums []int) int {
+    n := len(nums)
+    if n == 1 {return nums[0]}
     left := 0
-    right := len(nums)-1
-
+    right := n-1
     for left <= right {
-        // are we looking at a sorted half ?
-        if nums[left] <= nums[right] {
-            // then return smallest element ( i.e left element )
+        mid := left + (right-left)/2
+        if nums[left] < nums[right] {
             return nums[left]
         }
-
-        mid := left + (right - left) / 2
-        // is mid smallest ? 
-        if (mid == 0 || nums[mid] < nums[mid-1]) && (mid == len(nums)-1 || nums[mid] < nums[mid+1]) {
+        
+        // when is mid our answer ?
+        // when mid element is smaller than 2 adj neighbors
+        if (mid == 0 || nums[mid] < nums[mid-1]) && (mid == n-1 || nums[mid] < nums[mid+1]) {
             return nums[mid]
         }
 
-        // left side is sorted ( when compared to mid )
         if nums[left] <= nums[mid] {
-            // which means right is unsorted half
-            // since min is always is in unsorted half, take search to right side
             left = mid+1
         } else {
             right = mid-1
