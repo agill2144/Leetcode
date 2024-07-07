f36a2cafe1bdd7efc019ff08032a2bfc87999865
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-07 15:18:09 -0700

    Time: 0 ms (100.00%) | Memory: 3.7 MB (11.92%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.go
+++ 496-next-greater-element-i/next-greater-element-i.go
@@ -1,38 +1,27 @@
-/*
-    approach: next-greater-element
-    - find the NGR of all elements in nums2
-    - and keep saving each elements from nums2 in a idx map
-    - then using a second pass, loop over nums1
-    - find its idx in nums2 idxMap
-    - then using that idx, take the value we have computed in ngr
-
-    time = o(n2) + o(n1)
-    space = o(n2)
-*/
+// brute force: nested iterations
+// find ngr of nums2 first, while creating idx map
+// then loop over nums1, look for its idx in map and find the ngr
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
     n1 := len(nums1)
     n2 := len(nums2)
-    n2IdxMap := map[int]int{}
+    st := []int{} // holds values
+    idxMap := map[int]int{}
     ngr := make([]int, n2)
-    st := []int{}
-    for i := 0; i < n2; i++ {
-        ngr[i] = -1
-        n2IdxMap[nums2[i]] = i
-
+    for i := n2-1; i >= 0; i-- {
         curr := nums2[i]
-        for len(st) != 0 && curr > nums2[st[len(st)-1]] {
-            top := st[len(st)-1]
+        ngr[i] = -1
+        idxMap[curr] = i
+        for len(st) != 0 && st[len(st)-1] < curr {
             st = st[:len(st)-1]
-            ngr[top] = curr
         }
-        st = append(st, i)
-
+        if len(st) != 0 {
+            ngr[i] = st[len(st)-1]
+        }
+        st = append(st, curr)
     }
     out := make([]int, n1)
     for i := 0; i < n1; i++ {
-        idx := n2IdxMap[nums1[i]]
-        out[i] = ngr[idx]
-    }
+        out[i] = ngr[idxMap[nums1[i]]]
+    }    
     return out
-
 }
