841c3a9dd3af51f853b5b7b331a7fbfb76a66db7
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-07 15:53:09 -0700

    Time: 20 ms (68.75%) | Memory: 7.2 MB (38.89%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.go
+++ 503-next-greater-element-ii/next-greater-element-ii.go
@@ -1,18 +1,19 @@
+// processing ith element also works
+// just run the loop 2 times
 func nextGreaterElements(nums []int) []int {
     n := len(nums)
-    st := []int{} // indices
+    st := []int{} // vals    
     out := make([]int, n)
     for i := 0; i < n; i++ {out[i] = -1}
 
-    for i := 0; i < 2 * n; i++ {
-        curr := nums[i%n]
-        for len(st) != 0 && curr > nums[st[len(st)-1]] {
-            // process top
-            top := st[len(st)-1]
+    for i := (n-1)*2; i >= 0 ; i-- {
+        for len(st) != 0 && st[len(st)-1] <= nums[i%n] {
             st = st[:len(st)-1]
-            out[top] = curr
         }
-        st = append(st,i%n)
+        if len(st) != 0 {
+            out[i%n] = st[len(st)-1]
+        }
+        st = append(st, nums[i%n])
     }
     return out
 }
