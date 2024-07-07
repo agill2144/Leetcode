62ca3b9e7b24ed484995b63b28efa4df5c6b2d42
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-06 21:18:30 -0700

    Time: 3 ms (70.50%) | Memory: 3.1 MB (6.02%) - LeetSync

--- 35-search-insert-position/search-insert-position.go
+++ 35-search-insert-position/search-insert-position.go
@@ -1,33 +1,22 @@
+// find and save; left most on right side of target
+// |----|--|--|--|---------|------------|----|
+//               target   leftMost
 func searchInsert(nums []int, target int) int {
-    if nums == nil || len(nums) == 0 {return 0}
     n := len(nums)
-    if target <= nums[0] {return 0}
-    if target >= nums[n-1] {
-        if nums[n-1]==target {return n-1} 
-        return n
-    }
+    if n == 0 {return 0}
+    if nums[len(nums)-1] < target {return n}
     left := 0
     right := n-1
-    idx := -1
-
-    // find the next closest value to target
-    // i.e leftMost on the right side of target
-    // ----------|--------|--------
-    //         target.  leftmost on right side of target 
+    leftMostIdx := -1
     for left <= right {
         mid := left + (right-left)/2
         if nums[mid] >= target {
-            // found a value on the right side of target
-            // save it as a potential ans
-            // but we may have overshot, try to find a even closer value!
-            // therefore continue searching on left hand side
-            // HOWEVER if mid == target, return its idx position
             if nums[mid] == target {return mid}
-            idx = mid
+            leftMostIdx = mid
             right = mid-1
         } else {
             left = mid+1
         }
     }
-    return idx
+    return leftMostIdx
 }
