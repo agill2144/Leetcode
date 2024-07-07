a4cce4994187bada0fa2a6890be7a3388c9a00dd
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-07 11:59:54 -0700

    Time: 5 ms (66.51%) | Memory: 2.8 MB (17.58%) - LeetSync

--- 162-find-peak-element/find-peak-element.go
+++ 162-find-peak-element/find-peak-element.go
@@ -7,9 +7,9 @@ func findPeakElement(nums []int) int {
         mid := left + (right-left)/2
         if (mid == 0 || nums[mid] > nums[mid-1]) && (mid == n-1 || nums[mid] > nums[mid+1]) {
             return mid
-        } else if (mid == n-1 || nums[mid+1] > nums[mid]) {
+        } else if nums[mid+1] > nums[mid] {
             left = mid+1
-        } else {
+        } else { 
             right = mid-1
         }
     }
