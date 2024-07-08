e003c8770db0537f925e2d74e4dfa2a10a92445a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-07 19:21:07 -0700

    Time: 11 ms (40.14%) | Memory: 6.3 MB (20.50%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.go
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -1,20 +1,23 @@
+/*
+
+*/
 func trap(height []int) int {
     n := len(height)
-    maxLeft := make([]int, n)
-    maxRight := make([]int, n)
-    for i := 0; i < n; i++ {
+    left := make([]int, n)
+    right := make([]int, n)
+    for i := 0; i < len(height); i++ {
         if i == 0 || i == n-1 {continue}
-        maxLeft[i] = max(maxLeft[i-1], height[i-1])
-        maxRight[n-1-i] = max(maxRight[n-i], height[n-i])
+        maxLeftSoFar := max(left[i-1], height[i-1])
+        maxRightSoFar := max(right[n-1-i+1], height[n-1-i+1])
+        left[i] = maxLeftSoFar
+        right[n-1-i] = maxRightSoFar
     }
     total := 0
-    for i := 0; i < len(height); i++ {
-        ml := maxLeft[i]
-        mr := maxRight[i]
-        smallest := min(ml,mr)
-        trapped := smallest-height[i]
-        if trapped > 0 {
-            total += trapped
+    for i := 0; i < n; i++ {
+        leftH, rightH := left[i],right[i]
+        canBeTrappedAboveIthBuilding := min(leftH, rightH)-height[i]
+        if canBeTrappedAboveIthBuilding > 0 {
+            total += canBeTrappedAboveIthBuilding
         }
     }
     return total
