691e5adb8f5bad7c744f315c56db83a2bb481509
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-07 17:15:18 -0700

    Time: 6 ms (100.00%) | Memory: 7 MB (100.00%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.go
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.go
@@ -13,12 +13,16 @@ func validStrings(n int) []string {
         for i := 0; i < len(choices); i++ {
             char := choices[i]
             path += string(char)
-            if len(path) >= 2 {
+            if len(path) < 2 {
+                // too small to validate subtring
+                dfs(path)
+            } else {
+                // only proceed with this path, IF we have 1 in either of the last 2 positions
+                // we do not need to validate the entire substr/path each time,
+                // as long as we have a 1 in either last or 2nd last position, we can continue with this path
                 if path[len(path)-1] == '1' || path[len(path)-2] == '1' {
                     dfs(path)
                 }
-            } else {
-                dfs(path)
             }
             path = path[:len(path)-1]
         }
