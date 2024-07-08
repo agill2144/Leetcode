564e9da654b71b685ff235760d824e98cf7e4ecd
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-07 20:05:35 -0700

    Time: 3 ms (98.80%) | Memory: 6.1 MB (28.86%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.go
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -1,11 +1,34 @@
 /*
+    approach: prefix / suffix
+    - for each building;
+    - find how much can be trapped above a building
+    - we can trap water above a building if;
+        - there exists a left building > curr building height
+        - there exists a right buidling > curr building height
+            __          __
+            ||    __    ||
+            || __ || __ ||
+        - we can trap water above building whose height is 0
+            - can trap 2 units of water ; min(leftBuilding, rightBuilding)-currHeight
+        - we can trap water above building whose height is 1
+            - can trap 1 unit of water ; min(leftBuilding, rightBuilding)-currHeight
+        - we can trap water above building whose height is 0
+            - can trap 2 units of water ; min(leftBuilding, rightBuilding)-currHeight
+        - leftBuilding and rightBuilding are the max heights ( max vals ) excluding current building
+        - i.e max val from left and max val from right
+        - i.e max prefix from left and max suffix from right
+    - once left and right prefixes
+    - we can figure out how much can be trapped above each ith building
+        - total += min(left[i], right[i]) - height[i]
 
+    time = o(2n) = o(n)
+    space = o(2n) = o(n)
 */
 func trap(height []int) int {
     n := len(height)
     left := make([]int, n)
     right := make([]int, n)
-    for i := 0; i < len(height); i++ {
+    for i := 0; i < n; i++ {
         if i == 0 || i == n-1 {continue}
         maxLeftSoFar := max(left[i-1], height[i-1])
         maxRightSoFar := max(right[n-1-i+1], height[n-1-i+1])
