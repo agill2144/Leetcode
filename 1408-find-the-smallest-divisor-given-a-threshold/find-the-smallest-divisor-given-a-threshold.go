96ca44fac2a32d92078c9483675392ac65030063
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-08 18:12:59 -0700

    Time: 18 ms (87.95%) | Memory: 6.8 MB (80.72%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.go
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.go
@@ -1,23 +1,45 @@
 func smallestDivisor(nums []int, threshold int) int {
-    left := 1
-    right := math.MinInt64
-    for i := 0; i < len(nums); i++ { right = max(right, nums[i]) }
+    start := 1
+    end := math.MinInt64
+    for i := 0; i < len(nums); i++ {
+        end = max(end, nums[i])
+    }
+    left := start
+    right := end
     ans := -1
     for left <= right {
-        mid := left + (right-left)/2
+        divisor := left + (right-left)/2
         sum := 0
         for j := 0; j < len(nums); j++ {
-            sum = sum + (int(math.Ceil(float64(nums[j]) / float64(mid))))
+            q := int(math.Ceil(float64(nums[j]) / float64(divisor)))
+            sum += q
         }
 
-        // when does mid not work?
         if sum > threshold {
-            left = mid+1
+            left = divisor+1
         } else {
-            ans = mid
-            right = mid-1
+            ans = divisor
+            right = divisor-1
         }
-
     }
     return ans
 }
+
+
+// func smallestDivisor(nums []int, threshold int) int {
+//     start := 1
+//     end := math.MinInt64
+//     for i := 0; i < len(nums); i++ {
+//         end = max(end, nums[i])
+//     }
+//     for i := start; i <= end; i++ {
+//         divisor := i
+//         sum := 0
+//         for j := 0; j < len(nums); j++ {
+//             q := int(math.Ceil(float64(nums[j]) / float64(divisor)))
+//             sum += q
+//         }
+//         if sum <= threshold {return divisor}
+//     }
+//     return -1
+// }
