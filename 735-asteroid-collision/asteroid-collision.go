8f4593fbe70d57334af02ddfb468bc7044d6b286
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-11 20:59:34 -0700

    Time: 6 ms (71.47%) | Memory: 4.9 MB (66.67%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.go
+++ 735-asteroid-collision/asteroid-collision.go
@@ -1,39 +1,30 @@
 func asteroidCollision(asteroids []int) []int {
     st := []int{}
-    for i := 0; i < len(asteroids); i++ {
-        curr := abs(asteroids[i])
-        push := true
-        for len(st) != 0 && st[len(st)-1] > 0 && asteroids[i] < 0 {
+    n := len(asteroids)
+    for i := 0; i < n; i++ {
+        curr := asteroids[i]
+        pushCurr := true
+        for len(st) != 0 && st[len(st)-1] > 0 && curr < 0 {
             top := st[len(st)-1]
             st = st[:len(st)-1]
-
-
-            // when both are same size,
-            // both are destroyed, nothing is pushed to st
-            if top == curr {
-                push = false
+            // top == abs(curr)
+            if top == abs(curr) {
+                pushCurr = false
                 break
-            }
-
-            // when top of st is bigger
-            // curr is not pushed to st
-            // top is pushed back
-            if top > curr {
+            } else if top > abs(curr) {
+                pushCurr = false
                 st = append(st, top)
-                push = false
                 break
             }
-
-            // when top is smaller than curr
-            // continue removing
         }
-
-        if push {st = append(st, asteroids[i])}
+        if pushCurr {
+            st = append(st, curr)
+        }
     }
     return st
 }
 
 func abs(x int) int {
-    if x < 0 {return x*-1}
+    if x < 0 {return x *-1 }
     return x
 }
