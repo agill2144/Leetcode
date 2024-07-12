fd53e43a524c997788c98c9dc2191c54d10575f9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-11 20:24:47 -0700

    Time: 113 ms (63.35%) | Memory: 8.7 MB (50.63%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.go
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.go
@@ -1,31 +1,30 @@
 func maxDistance(position []int, m int) int {
-    // min ans is max
-    // atMin = mid
-    // and look for max such mid value
-
+    // min ans is maximized ; "minimum magnetic force between any two balls is maximum"
     sort.Ints(position)
     left := 1
-    right := position[len(position)-1]  - position[0]
-    ans := 0
+    right := position[len(position)-1]
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
         
-
+        // mid is our at-min val
+        // how many balls can be placed if at-min the force is mid
         count := 1
-        lastPlaced := position[0]
-        for i := 0; i < len(position); i++ {
-            diff := position[i]-lastPlaced
-            if diff >= mid {
-                lastPlaced = position[i]
+        lastPlaced := 0
+        for i := 1; i < len(position); i++ {
+            currPos := position[i]
+            lastPos := position[lastPlaced]
+            if currPos - lastPos >= mid {
                 count++
+                lastPlaced = i
             }
         }
-
-        if count < m {
-            right = mid-1
-        } else {
+        
+        if count >= m {
             ans = mid
             left = mid+1
+        } else {
+            right = mid-1
         }
     }
     return ans
