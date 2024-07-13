3a869a703513683c51544090e7173f4fe5ac4d05
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-13 15:06:54 -0700

    Time: 6 ms (100.00%) | Memory: 6.6 MB (68.46%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
@@ -1,28 +1,17 @@
-/*
-    "row are sorted in ascending from left to right"
-    "column are sorted in ascending from top to bottom"
-
-    approach: staircase search
-    - pick a corner ( bottom left or top right )
-    - and check if its our target
-    - if not , we know a definitive direction to proceed into to look for our target
-    - keep doing this while we are still inbound
-
-    time = o(m+n)
-    space = o(1)   
-*/
 func searchMatrix(matrix [][]int, target int) bool {
     m := len(matrix)
     n := len(matrix[0])
-    r, c := m-1, 0
-
+    r := m-1
+    c := 0
     for r >= 0 && c < n {
-        if matrix[r][c] == target {return true}
+        if matrix[r][c] == target {
+            return true
+        }
         if target > matrix[r][c] {
             c++
         } else {
             r--
         }
     }
-    return false
+    return false     
 }
