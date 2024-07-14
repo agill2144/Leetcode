f3264fcdda9094fd471c4474f671899f1b98b9cd
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-14 14:51:04 -0700

    Time: 32 ms (54.68%) | Memory: 7.5 MB (44.19%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.go
+++ 658-find-k-closest-elements/find-k-closest-elements.go
@@ -1,3 +1,26 @@
+/*
+    approach: naive binary search
+    - find left most idx on right side of x
+    - use 2 ptrs to collect elements
+        - because we searched for leftMost idx on right side of x
+        - ptrs will be idx and idx-1
+        - calc dist at each ptr
+        - if both dists are same, pick the value of left ptr
+        - otherwise pick the smaller value and move the smaller value ptr away 
+        - keep doing this while ptrs are inbound and we have not collected k elements
+        - when this loop breaks out, its possible that one of the ptr went out of bount
+        - and we still have not collected all k elements
+        - therefore start another loop for each ptr thats inbound
+            - while ptr is inbound and we do not have k elements yet
+                - process this ptr
+
+    time = o(logn) + o(k) + o(klogk)
+    but its possible at that k == n ; therefore
+    time = o(logn) + o(n) + o(nlogn) = o(nlogn) worst case 
+
+    space = o(1) unless the underlying sorting algo takes o(n) space (merge sort or heap sort for example)
+        
+*/
 func findClosestElements(arr []int, k int, x int) []int {
     n := len(arr)
     if x < arr[0] {
@@ -5,6 +28,8 @@ func findClosestElements(arr []int, k int, x int) []int {
     } else if x > arr[len(arr)-1] {
         return arr[n-k:n]
     }
+
+    // time = o(logn)
     // find the left most on right side of x
     left := 0
     right := n-1
@@ -19,15 +44,16 @@ func findClosestElements(arr []int, k int, x int) []int {
             left = mid+1
         }
     }
-    fmt.Println(idx, arr[idx])
 
     p1 := idx // go right
     p2 := idx-1 // go left
     out := []int{}
+    // if k is same size as n ( worst case )
+    // we moved out ptrs to far ends ( 0 and n-1 )
+    // therefore time = o(k) ; or ; o(n) worst case
     for len(out) != k && p1 < n && p2 >= 0 {
         p1Dist := abs(arr[p1]-x)
         p2Dist := abs(arr[p2]-x)
-
         if p1Dist < p2Dist {
             out = append(out, arr[p1])
             p1++
@@ -38,8 +64,9 @@ func findClosestElements(arr []int, k int, x int) []int {
     }
     for len(out) != k && p1 < n{out = append(out, arr[p1]); p1++}
     for len(out) != k && p2 >= 0 {out = append(out, arr[p2]); p2--}
-    sort.Ints(out)
 
+    // time = o(klogk)
+    sort.Ints(out)
     return out
 }
 
