3d842179975c2492a54ab9c631802d9085b36a83
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-14 14:29:18 -0700

    Time: 58 ms (15.36%) | Memory: 7.5 MB (44.19%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.go
@@ -0,0 +1,60 @@
+/*
+    approach: heap
+    - find k closest
+    - heap vibes
+    - closest = smallest = max heap
+        - we will keep only k elements
+        - sort by dist
+        - if two distances are same, sort by value
+*/
+func findClosestElements(arr []int, k int, x int) []int {
+    mx := &maxHeap{items: []*heapNode{}}
+    for i := 0; i < len(arr); i++ {
+        val := arr[i]
+        dist := abs(val-x)
+        heap.Push(mx, &heapNode{dist:dist, val:val})
+        if mx.Len() > k {
+            heap.Pop(mx)
+        }
+    }
+    out := []int{}
+    for mx.Len() != 0 {
+        out = append(out, heap.Pop(mx).(*heapNode).val)
+    }
+    sort.Ints(out)
+    return out
+}
+
+func abs(x int) int {
+    if x < 0 {return x * -1}
+    return x
+}
+
+type heapNode struct {
+    dist int
+    val int
+}
+type maxHeap struct {
+	items []*heapNode
+}
+
+func (m *maxHeap) Less(i, j int) bool {
+	if m.items[i].dist == m.items[j].dist {
+        return m.items[i].val > m.items[j].val
+    }
+    return m.items[i].dist > m.items[j].dist
+}
+func (m *maxHeap) Swap(i, j int) {
+	m.items[i], m.items[j] = m.items[j], m.items[i]
+}
+func (m *maxHeap) Len() int {
+	return len(m.items)
+}
+func (m *maxHeap) Push(x interface{}) {
+	m.items = append(m.items, x.(*heapNode))
+}
+func (m *maxHeap) Pop() interface{} {
+	out := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return out
+}
