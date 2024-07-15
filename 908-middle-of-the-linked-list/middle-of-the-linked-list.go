87963673ca36e3f473b90d18cc9a789d3332d4df
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-15 00:21:40 -0700

    Time: 0 ms (100.00%) | Memory: 2.3 MB (62.75%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.go
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.go
@@ -5,6 +5,18 @@
  *     Next *ListNode
  * }
  */
+/*
+    approach: slow and fast ptrs
+    - if there are n nodes in the LL
+    - and we have 2 ptrs; slow moving by 1 speed and fast moving by 2x speed
+    - fast ptr will reach end in n/2 time
+    - if fast ptr is moving at 2x speed, and fast has reached end
+    - then slow which was moving at half of fast's speed
+    - then slow will be at the middle of the LL
+
+    time = o(n)
+    space = o(1)
+*/
 func middleNode(head *ListNode) *ListNode {
     slow, fast := head, head
     for fast != nil && fast.Next != nil {
