ee1b8e81e40db517761e1aafbe9018b0b33c62b5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-15 00:26:31 -0700

    Time: 0 ms (100.00%) | Memory: 3.1 MB (6.06%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.go
+++ 206-reverse-linked-list/reverse-linked-list.go
@@ -6,6 +6,22 @@
  * }
  */
 
+
+// recursive
+func reverseList(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {return head}
+    var dfs func(curr *ListNode, prev *ListNode) *ListNode
+    dfs = func(curr *ListNode, prev *ListNode) *ListNode {
+        // base
+        if curr == nil {return prev}
+        // logic
+        next := curr.Next
+        curr.Next = prev
+        return dfs(next, curr)
+    }
+    return dfs(head, nil)
+}
+
 /*
     approach: classic
     - point to prev
@@ -13,15 +29,15 @@
     time = o(n)
     space = o(1)
 */
-func reverseList(head *ListNode) *ListNode {
-    if head == nil || head.Next == nil {return head}
-    curr := head
-    var prev *ListNode
-    for curr != nil {
-        next := curr.Next
-        curr.Next = prev
-        prev = curr
-        curr = next
-    }
-    return prev
-}
+// func reverseList(head *ListNode) *ListNode {
+//     if head == nil || head.Next == nil {return head}
+//     curr := head
+//     var prev *ListNode
+//     for curr != nil {
+//         next := curr.Next
+//         curr.Next = prev
+//         prev = curr
+//         curr = next
+//     }
+//     return prev
+// }
