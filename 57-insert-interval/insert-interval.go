087217b15dfd21cb2352cfdd7ff4172a2cacae6e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-15 19:52:27 -0700

    Time: 3 ms (93.45%) | Memory: 4.3 MB (52.29%) - LeetSync

--- 57-insert-interval/insert-interval.go
+++ 57-insert-interval/insert-interval.go
@@ -1,38 +1,41 @@
 func insert(intervals [][]int, newInterval []int) [][]int {
-    if len(intervals) == 0 {
-        return [][]int{newInterval}
-    }
-
-    left := 0
-    right := len(intervals)-1
-    idx := -1
-    for left <= right {
-        mid := left + (right-left)/2
-        val := intervals[mid][0]
-        valOnLeft := val <= newInterval[0]
-        if valOnLeft {
-            idx = mid
-            left = mid+1
-        } else {
-            right = mid-1
-        }
-    }
-    idx++
-
-    tmp := [][]int{}
-    i := 0
-    for i < idx {tmp = append(tmp, intervals[i]);i++}
-    tmp = append(tmp, newInterval)
-    for i < len(intervals) {tmp = append(tmp, intervals[i]); i++}
-
-    // merge intervals code 
+    n := len(intervals)
+    if n == 0 {return [][]int{newInterval}}
     out := [][]int{}
-    for i := 0; i < len(tmp); i++ {
-        if len(out) != 0 && tmp[i][0] <= out[len(out)-1][1] {
-            out[len(out)-1][0] = min(tmp[i][0], out[len(out)-1][0])
-            out[len(out)-1][1] = max(tmp[i][1], out[len(out)-1][1])
+    used := false
+    i := 0
+    if intervals[0][0] < newInterval[0] {
+        out = append(out, intervals[0])
+        i=1
+    } else {
+        out = append(out, newInterval)
+        used = true
+    }
+    for i < n || !used {
+        start := math.MaxInt64
+        end := math.MaxInt64
+        if i < n {start = intervals[i][0]; end = intervals[i][1]}
+        newStart := newInterval[0]
+        newEnd := newInterval[1]
+        lastStart := out[len(out)-1][0]
+        lastEnd := out[len(out)-1][1]
+
+        if newStart < start && !used {
+            if newStart <= lastEnd {
+                out[len(out)-1][0] = min(lastStart, newStart)
+                out[len(out)-1][1] = max(lastEnd, newEnd)
+            } else {
+                out = append(out, newInterval)
+            }
+            used = true
         } else {
-            out = append(out, tmp[i])
+            if start <= lastEnd {
+                out[len(out)-1][0] = min(lastStart, start)
+                out[len(out)-1][1] = max(lastEnd, end)
+            } else {
+                out = append(out, intervals[i])   
+            }
+            i++
         }
     }
     return out
