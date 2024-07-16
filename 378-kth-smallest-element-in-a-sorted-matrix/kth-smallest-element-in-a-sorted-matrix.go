cd68817fa37380df308a78d007f62766c070d837
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-15 19:06:16 -0700

    Time: 16 ms (82.46%) | Memory: 6.6 MB (88.60%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.go
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.go
@@ -5,8 +5,16 @@ func kthSmallest(matrix [][]int, k int) int {
     left := matrix[0][0]
     right := matrix[m-1][n-1]
     for left <= right {
+
         mid := left + (right-left)/2
         count := countLessThanOrEqualTo(matrix, mid)
+        // is mid our kth smallest ?
+        // only if count on left of mid >= k elements
+        // why > ?
+        // because if mid is our kth smallest
+        // then we have included this kth element in our counting (because count uses <= operator )
+        // therefore if mid is our kth element, its count will ALWAYS be >= k elements on left
+        // therefore save this potential answer and continue searching on left
         if count >= k {
             ans = mid
             right = mid-1
