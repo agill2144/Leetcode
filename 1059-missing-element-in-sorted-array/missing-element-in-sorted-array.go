c4cd8c7d11b05fc9e3d633385f869367a7ed997e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-18 23:19:04 -0700

    Time: 33 ms (50.00%) | Memory: 7.3 MB (24.14%) - LeetSync

--- 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.go
+++ 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.go
@@ -1,21 +1,29 @@
 func missingElement(nums []int, k int) int {
     offSet := nums[0]
+    n := len(nums)
     left := 0
-    right := len(nums)-1
+    right := n-1
     for left <= right {
         mid := left + (right-left)/2
-        current := nums[mid]
-        correct := mid+offSet
-        missing := current-correct
-        if missing < k {
-            left = mid+1
-        } else {
+        correctVal := mid+offSet
+        missing := nums[mid]-correctVal
+        if missing >= k {
             right = mid-1
+        } else {
+            left = mid+1
         }
     }
-
-    rightVal := nums[right]
-    correctRightVal := right+offSet
-    missingOnLeft := rightVal - correctRightVal
-    return rightVal + (k-missingOnLeft)
+    missing := nums[right]-(right+offSet)
+    // if at right ptr, we are missing x values
+    // and lets assume x is a +ve value and is < k
+    // this means on left of rightVal, there are x values missing
+    // which means, the missing value is going to be k-x steps ahead of rightVal
+    // for example; rightVal = 10; missing on left = 3; k = 5
+    // if there are 3 values missing on left of 10, its obvious that we need to simply go 2 steps ahead of 10
+    // why ? we need 5th missing, 3 are missing on left of 10, that leaves us with 2 ( 5-3 ; i.e k-missing )
+    // then missing kth value is 10+2 = 12; rightVal + (k-missing) 
+    // thats the intuition here
+    // if there are x missing on left of right, how many more steps ahead of right val is our missing value ?
+    // however many ever is remaining after taking out x from k
+    return nums[right] + (k-missing)
 }
