de355433db94434a3474381bad4517b48f451f19
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-20 00:22:40 -0700

    Time: 235 ms (39.27%) | Memory: 30.7 MB (5.01%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.go
+++ 239-sliding-window-maximum/sliding-window-maximum.go
@@ -1,19 +1,16 @@
 func maxSlidingWindow(nums []int, k int) []int {
-    dq := []int{} // idx
-    out := []int{}
+    dq := []int{}
     left := 0
+    out := []int{}
     for i := 0; i < len(nums); i++ {
-        curr := nums[i]
-        for len(dq) != 0 && curr > nums[dq[len(dq)-1]] {
+        for len(dq) != 0 && nums[i] > nums[dq[len(dq)-1]] {
             dq = dq[:len(dq)-1]
         }
         dq = append(dq, i)
         if i-left+1 == k {
             out = append(out, nums[dq[0]])
             left++
-            if len(dq) != 0 && dq[0] < left {
-                dq = dq[1:]
-            }
+            if dq[0] < left {dq = dq[1:]}
         }
     }
     return out
