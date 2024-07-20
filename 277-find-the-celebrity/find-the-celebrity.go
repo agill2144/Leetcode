619372170f1fd7aaecaf2a6dfe1c0404e5c07d12
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-20 15:06:37 -0700

    Time: 439 ms (22.50%) | Memory: 7 MB (15.00%) - LeetSync

--- 277-find-the-celebrity/find-the-celebrity.go
+++ 277-find-the-celebrity/find-the-celebrity.go
@@ -8,10 +8,12 @@ func solution(knows func(a int, b int) bool) func(n int) int {
         outdegrees := make([]int, n)
         for i := 0; i < n; i++ {
             // if a knows b; this is a->b ( a outdegrees++ ; b indegress++ )
+            // its possible that a does not b; but b knows a or both is true
+            // therefore we have to check in both direction
+            // if b knows a; this is b->a (a indegrees++; b outdegrees++)
             for j := i; j < n; j++ {
                 if i == j {continue}
-                a := i
-                b := j
+                a := i; b := j
                 if knows(a,b) {
                     indegrees[b]++
                     outdegrees[a]++
