90e8813987d7c4329fbad20ec13c426bb46b0985
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-19 22:40:39 -0700

    Time: 160 ms (96.66%) | Memory: 14.1 MB (50.69%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.go
@@ -0,0 +1,20 @@
+func maxSlidingWindow(nums []int, k int) []int {
+    dq := []int{} // idx
+    out := []int{}
+    left := 0
+    for i := 0; i < len(nums); i++ {
+        curr := nums[i]
+        for len(dq) != 0 && curr > nums[dq[len(dq)-1]] {
+            dq = dq[:len(dq)-1]
+        }
+        dq = append(dq, i)
+        if i-left+1 == k {
+            out = append(out, nums[dq[0]])
+            left++
+            for len(dq) != 0 && dq[0] < left {
+                dq = dq[1:]
+            }
+        }
+    }
+    return out
+}
