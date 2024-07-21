4ac9c7d4ddc21fe2f853518b7eff19368512eab1
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-21 16:19:49 -0700

    Time: 0 ms (100.00%) | Memory: 3.4 MB (57.79%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.go
+++ 328-odd-even-linked-list/odd-even-linked-list.go
@@ -5,6 +5,20 @@
  *     Next *ListNode
  * }
  */
+
+/*
+    - split the even LL into a new dummy chain
+    - in the main loop; focus on grouping odd nodes into curr ptr
+    - while the even nodes will be appended to dummy head
+    - at the end of the loop we will have splitted the LL into 2
+    - head = has collected all the odd index position nodes
+    - dummy = has colleced all the even index position nodes
+    - our initial curr ptr will be sitting at the tail of orignal LL (ones with all odd idx position nodes)
+    - simply connect the 2 linkedlists again!
+    - oddTail.next(curr ptr; odd nodes) = dummy.next( even nodes )
+    time = o(n)
+    space = o(1); dummyNode is constant; we reused all the nodes, created no new nodes
+*/
 func oddEvenList(head *ListNode) *ListNode {
     if head == nil || head.Next == nil {return head}
     curr := head
