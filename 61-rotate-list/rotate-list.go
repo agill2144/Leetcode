a48e9feb5118321acd0bef00001be959487db772
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-23 22:02:15 -0700

    Time: 0 ms (100.00%) | Memory: 2.8 MB (11.32%) - LeetSync

--- 61-rotate-list/rotate-list.go
+++ 61-rotate-list/rotate-list.go
@@ -6,50 +6,75 @@
  * }
  */
 
-// apply the same reversing strategy from: https://leetcode.com/problems/rotate-array/
 func rotateRight(head *ListNode, k int) *ListNode {
-    if head == nil || head.Next == nil || k == 0  {return head}
-    size := 0
+    if head == nil || head.Next == nil || k == 0 {return head}
+    // ogHead := head
+    size := 1
     curr := head
-    for curr != nil {
+    for curr != nil && curr.Next != nil {
         size++
         curr = curr.Next
     }
     k %= size
-    if k == 0 {return head}
-        
-    // reverse the whole LL
-    head = reverse(head)
-    
-    // split the LL into 2 chunks ( k nodes, and remaining nodes )
-    i := 1
+    if k == size || k == 0 {return head}
+    curr.Next = head
+    var prev *ListNode
+    count := 0
     curr = head
-    for i < k {
-        i++
+    for count != size-k {
+        prev = curr
         curr = curr.Next
+        count++
     }
-    firstChunkHead := head
-    firstChunkTail := head
-    secondChunkHead := curr.Next
-    curr.Next = nil
-    
-    // reverse the first chunk
-    firstChunkHead = reverse(firstChunkHead)
-    
-    // reverse the second chunk
-    secondChunkHead = reverse(secondChunkHead)
-    firstChunkTail.Next = secondChunkHead
-    return firstChunkHead
+    prev.Next = nil
+    return curr
+
 }
 
-func reverse(head *ListNode) *ListNode {
-    var prev *ListNode
-    curr := head
-    for curr != nil {
-        next := curr.Next
-        curr.Next = prev
-        prev = curr
-        curr = next
-    }
-    return prev
-}
+// rotate array intuition from: https://leetcode.com/problems/rotate-array/
+// func rotateRight(head *ListNode, k int) *ListNode {
+//     if head == nil || head.Next == nil || k == 0  {return head}
+//     size := 0
+//     curr := head
+//     for curr != nil {
+//         size++
+//         curr = curr.Next
+//     }
+//     k %= size
+//     if k == 0 {return head}
+        
+//     // 1. reverse the whole LL
+//     head = reverse(head)
+    
+//     // 2. split the LL into 2 chunks ( k nodes, and remaining nodes )
+//     i := 1
+//     curr = head
+//     for i != k {
+//         i++
+//         curr = curr.Next
+//     }
+//     firstChunkHead := head
+//     firstChunkTail := head
+//     secondChunkHead := curr.Next
+//     curr.Next = nil
+    
+//     // 3. reverse the first chunk
+//     firstChunkHead = reverse(firstChunkHead)
+    
+//     // 4. reverse the second chunk
+//     secondChunkHead = reverse(secondChunkHead)
+//     firstChunkTail.Next = secondChunkHead
+//     return firstChunkHead
+// }
+
+// func reverse(head *ListNode) *ListNode {
+//     var prev *ListNode
+//     curr := head
+//     for curr != nil {
+//         next := curr.Next
+//         curr.Next = prev
+//         prev = curr
+//         curr = next
+//     }
+//     return prev
+// }
