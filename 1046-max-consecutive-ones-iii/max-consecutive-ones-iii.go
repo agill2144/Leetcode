b447711effb5baceff36bb0ac7c3b67cbd2bfaa0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-23 23:25:41 -0700

    Time: 49 ms (59.89%) | Memory: 7.2 MB (55.08%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.go
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.go
@@ -1,16 +1,18 @@
-// we are allowed to have atleast k zeros in our window
 func longestOnes(nums []int, k int) int {
-    maxSize := 0
-    n := len(nums)
     left := 0
     zeros := 0
-    for i := 0; i < n; i++ {
-        if nums[i] == 0 {zeros++}
-        for zeros > k {
-            if nums[left] == 0 {zeros--}
+    maxWindow := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] == 0 {
+            zeros++
+        }
+        if zeros > k {
+            if nums[left] == 0 {
+                zeros--
+            }
             left++
         }
-        maxSize = max(maxSize, i-left+1)
+        maxWindow = max(maxWindow, i-left+1)
     }
-    return maxSize
+    return maxWindow
 }
