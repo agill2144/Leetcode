b4cbc5db543512b878d92f95e3fb8c8af3c3c896
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-24 19:06:02 -0700

    Time: 3 ms (62.35%) | Memory: 2.7 MB (73.88%) - LeetSync

--- 61-rotate-list/rotate-list.go
+++ 61-rotate-list/rotate-list.go
@@ -5,74 +5,25 @@
  *     Next *ListNode
  * }
  */
-
 func rotateRight(head *ListNode, k int) *ListNode {
     if head == nil || head.Next == nil || k == 0 {return head}
     size := 1
     curr := head
-    for curr != nil && curr.Next != nil {
-        size++
+    for curr.Next != nil {
         curr = curr.Next
+        size++
     }
     k %= size
-    if k == size || k == 0 {return head}
+    if k == 0 {return head}
     curr.Next = head
+    dist := size-k
     count := 1
     curr = head
-    for count != size-k {
-        curr = curr.Next
+    for count < dist {
         count++
+        curr = curr.Next
     }
     head = curr.Next
     curr.Next = nil
     return head
-
 }
-
-// rotate array intuition from: https://leetcode.com/problems/rotate-array/
-// func rotateRight(head *ListNode, k int) *ListNode {
-//     if head == nil || head.Next == nil || k == 0  {return head}
-//     size := 0
-//     curr := head
-//     for curr != nil {
-//         size++
-//         curr = curr.Next
-//     }
-//     k %= size
-//     if k == 0 {return head}
-        
-//     // 1. reverse the whole LL
-//     head = reverse(head)
-    
-//     // 2. split the LL into 2 chunks ( k nodes, and remaining nodes )
-//     i := 1
-//     curr = head
-//     for i != k {
-//         i++
-//         curr = curr.Next
-//     }
-//     firstChunkHead := head
-//     firstChunkTail := head
-//     secondChunkHead := curr.Next
-//     curr.Next = nil
-    
-//     // 3. reverse the first chunk
-//     firstChunkHead = reverse(firstChunkHead)
-    
-//     // 4. reverse the second chunk
-//     secondChunkHead = reverse(secondChunkHead)
-//     firstChunkTail.Next = secondChunkHead
-//     return firstChunkHead
-// }
-
-// func reverse(head *ListNode) *ListNode {
-//     var prev *ListNode
-//     curr := head
-//     for curr != nil {
-//         next := curr.Next
-//         curr.Next = prev
-//         prev = curr
-//         curr = next
-//     }
-//     return prev
-// }
