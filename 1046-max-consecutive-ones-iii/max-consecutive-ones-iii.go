2ebddd0f6f57679a16b18fccfbf1ebf51d0f64fc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-24 21:41:22 -0700

    Time: 52 ms (44.20%) | Memory: 7.2 MB (48.70%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.go
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.go
@@ -1,4 +1,33 @@
 /*
+    sliding window nested loop iterations:
+    - can we make left ptr take bigger jumps
+    - yes, we would have maintain of a list of zeros idx positions
+*/
+func longestOnes(nums []int, k int) int {
+    zerosQ := []int{}
+    maxWindow := 0
+    left := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] == 0  {
+            zerosQ = append(zerosQ, i)
+        }
+        if len(zerosQ) > k {
+            // invalid window, escape the earliest 0 we saw
+            // i.e first zero we saw ( hence using queue, FIFO )
+            earliest := zerosQ[0]
+            if left <= earliest {
+                left = earliest + 1
+            }
+            zerosQ = zerosQ[1:]
+        }
+
+        // now we have a valid window
+        maxWindow = max(maxWindow, i-left+1)
+    }
+    return maxWindow
+}
+
+/*
     brute force:
     - form all possible subarrays
     - while allowing at most k zeros
@@ -17,23 +46,23 @@
     time = o(2n) = o(n)
     space = o(1)
 */
-func longestOnes(nums []int, k int) int {
-    n := len(nums)
-    maxWindow := 0
-    left := 0
-    zeros := 0
-    for i := 0; i < n; i++ {
-        if nums[i] == 0 {
-            zeros++
-        }
-        for zeros > k {
-            if nums[left] == 0 {
-                zeros--
-            }
-            left++
-        }
+// func longestOnes(nums []int, k int) int {
+//     n := len(nums)
+//     maxWindow := 0
+//     left := 0
+//     zeros := 0
+//     for i := 0; i < n; i++ {
+//         if nums[i] == 0 {
+//             zeros++
+//         }
+//         for zeros > k {
+//             if nums[left] == 0 {
+//                 zeros--
+//             }
+//             left++
+//         }
 
-        maxWindow = max(maxWindow, i-left+1)
-    }
-    return maxWindow
-}
+//         maxWindow = max(maxWindow, i-left+1)
+//     }
+//     return maxWindow
+// }
