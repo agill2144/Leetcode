c2b12b886d22c448df91b4b83924aaa4ce28fec4
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-24 21:45:14 -0700

    Time: 44 ms (86.63%) | Memory: 8.5 MB (16.92%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.go
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.go
@@ -1,31 +1,66 @@
 /*
     sliding window nested loop iterations:
-    - can we make left ptr take bigger jumps
-    - yes, we would have maintain of a list of zeros idx positions
+    - can we use a non-shrinking window?
+    - if window is invalid, oh well
+        - if left ptr is at a 0, reduce zeros counter
+        - move left 
+    - if our window is valid, take its size and save it as needed
+    time = o(n)
+    space = o(1)
 */
 func longestOnes(nums []int, k int) int {
-    zerosQ := []int{}
+    n := len(nums)
     maxWindow := 0
+    zeros := 0
     left := 0
-    for i := 0; i < len(nums); i++ {
-        if nums[i] == 0  {
-            zerosQ = append(zerosQ, i)
+    for i := 0; i < n; i++ {
+        if nums[i] == 0 {
+            zeros++
         }
-        if len(zerosQ) > k {
-            // invalid window, escape the earliest 0 we saw
-            // i.e first zero we saw ( hence using queue, FIFO )
-            earliest := zerosQ[0]
-            if left <= earliest {
-                left = earliest + 1
-            }
-            zerosQ = zerosQ[1:]
+        if zeros > k {
+            // invalid window
+            if nums[left] == 0 {zeros--}
+            left++
+        } else {
+            // valid window
+            maxWindow = max(maxWindow, i-left+1)
         }
-
-        // now we have a valid window
-        maxWindow = max(maxWindow, i-left+1)
     }
     return maxWindow
 }
+/*
+    sliding window nested loop iterations:
+    - can we make left ptr take bigger jumps
+    - yes, we would have maintain of a list of zeros idx positions
+    - we cannot use a idx map because once we escape a earliest seen zero, what about its next position?
+    - hence we need a list of idx positions 
+
+    time = o(n)
+    space = o(n) ; we are using a queue to store idx positions of zeros
+*/
+// func longestOnes(nums []int, k int) int {
+//     zerosQ := []int{}
+//     maxWindow := 0
+//     left := 0
+//     for i := 0; i < len(nums); i++ {
+//         if nums[i] == 0  {
+//             zerosQ = append(zerosQ, i)
+//         }
+//         if len(zerosQ) > k {
+//             // invalid window, escape the earliest 0 we saw
+//             // i.e first zero we saw ( hence using queue, FIFO )
+//             earliest := zerosQ[0]
+//             if left <= earliest {
+//                 left = earliest + 1
+//             }
+//             zerosQ = zerosQ[1:]
+//         }
+
+//         // now we have a valid window
+//         maxWindow = max(maxWindow, i-left+1)
+//     }
+//     return maxWindow
+// }
 
 /*
     brute force:
