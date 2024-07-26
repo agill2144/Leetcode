70af4e68b04f6363e0de1b574d886d39c2f12f05
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-25 20:47:49 -0700

    Time: 100 ms (60.67%) | Memory: 8.1 MB (52.00%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.go
+++ 940-fruit-into-baskets/fruit-into-baskets.go
@@ -5,18 +5,36 @@ func totalFruit(fruits []int) int {
     left := 0
     for i := 0; i < n; i++ {
         idx[fruits[i]] = i
-        if len(idx) > 2 {
-            // escape the earliest idx
-            leftMostIdx := n+1
-            for _, v := range idx {
-                leftMostIdx = min(leftMostIdx, v)
+        if len(idx) <= 2 {
+            maxWindow = max(maxWindow, i-left+1)
+        } else {
+            if val := idx[fruits[left]]; val <= left {
+                delete(idx, fruits[left])
             }
-            if left <= leftMostIdx {
-                left = leftMostIdx+1
-            }
-            delete(idx, fruits[leftMostIdx])
+            left++
         }
-        maxWindow = max(maxWindow, i-left+1)
     }
     return maxWindow
 }
+// func totalFruit(fruits []int) int {
+//     idx := map[int]int{}
+//     n := len(fruits)
+//     maxWindow := 0
+//     left := 0
+//     for i := 0; i < n; i++ {
+//         idx[fruits[i]] = i
+//         if len(idx) > 2 {
+//             // escape the earliest idx
+//             leftMostIdx := n+1
+//             for _, v := range idx {
+//                 leftMostIdx = min(leftMostIdx, v)
+//             }
+//             if left <= leftMostIdx {
+//                 left = leftMostIdx+1
+//             }
+//             delete(idx, fruits[leftMostIdx])
+//         }
+//         maxWindow = max(maxWindow, i-left+1)
+//     }
+//     return maxWindow
+// }
