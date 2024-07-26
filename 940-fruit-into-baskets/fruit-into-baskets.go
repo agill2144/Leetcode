726b767122813b6123721b4ef25a14d3dca9d97c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-25 22:18:06 -0700

    Time: 162 ms (5.33%) | Memory: 9.4 MB (26.00%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.go
+++ 940-fruit-into-baskets/fruit-into-baskets.go
@@ -1,40 +1,40 @@
+/*
+    we have 2 baskets
+    and this means we can only have 2 types of fruits
+    and the fruits selected must be consecutive
+
+    so at max we can have 2 different types of fruits
+    so at max we can have 2 distinct elements
+    and we have to find longest subarray with atmost 2 distinct elements
+
+    approach: classic sliding window
+    - maintain a window ( freq )
+    - if our window is valid, make it valid
+        - move left ptr forward 1-by-1 while updating our window state
+    - once we have a valid window, save its size as needed
+
+    time = o(2n) = o(n)
+    space = o(3) = o(1)
+*/
 func totalFruit(fruits []int) int {
-    idx := map[int]int{}
-    n := len(fruits)
-    maxWindow := 0
+    freq := map[int]int{}    
     left := 0
-    for i := 0; i < n; i++ {
-        idx[fruits[i]] = i
-        if len(idx) <= 2 {
-            maxWindow = max(maxWindow, i-left+1)
-        } else {
-            if val := idx[fruits[left]]; val <= left {
-                delete(idx, fruits[left])
+    maxWindow := 0
+    for i := 0; i < len(fruits); i++ {
+        freq[fruits[i]]++
+
+        // update window if its invalid
+        for len(freq) > 2 {
+            leftEle := fruits[left]
+            freq[leftEle]--
+            if freq[leftEle] == 0 {
+                delete(freq, leftEle)
             }
             left++
         }
+     
+        // now we have a valid window
+        maxWindow = max(maxWindow, i-left+1)
     }
     return maxWindow
 }
-// func totalFruit(fruits []int) int {
-//     idx := map[int]int{}
-//     n := len(fruits)
-//     maxWindow := 0
-//     left := 0
-//     for i := 0; i < n; i++ {
-//         idx[fruits[i]] = i
-//         if len(idx) > 2 {
-//             // escape the earliest idx
-//             leftMostIdx := n+1
-//             for _, v := range idx {
-//                 leftMostIdx = min(leftMostIdx, v)
-//             }
-//             if left <= leftMostIdx {
-//                 left = leftMostIdx+1
-//             }
-//             delete(idx, fruits[leftMostIdx])
-//         }
-//         maxWindow = max(maxWindow, i-left+1)
-//     }
-//     return maxWindow
-// }
