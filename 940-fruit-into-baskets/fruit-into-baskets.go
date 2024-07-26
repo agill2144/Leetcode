3a0932c1df6aa8ef1719cde00e272ad843b30ebf
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-25 22:32:56 -0700

    Time: 96 ms (72.67%) | Memory: 7.1 MB (98.67%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.go
+++ 940-fruit-into-baskets/fruit-into-baskets.go
@@ -1,34 +1,84 @@
-/*
-
-*/
 func totalFruit(fruits []int) int {
     left := 0
+    freq := map[int]int{}
     maxWindow := 0
-    idx := map[int]int{}
     for i := 0; i < len(fruits); i++ {
-        idx[fruits[i]] = i
-        if len(idx) > 2 {
+        freq[fruits[i]]++
+        if len(freq) > 2 {
             // invalid window
-            // to make it valid, escape the earliest idx to keep window size large
-            minIdx := len(fruits)+1
-            minIdxFruit := -1
-            for k, v := range idx {
-                if v < minIdx {
-                    minIdx = v
-                    minIdxFruit = k
-                }
+
+            // use non-shrinking window
+            // reduce left fruit count from our window
+            leftFruit := fruits[left]
+            freq[leftFruit]--
+            // if left fruit count is now 0, it means we have 
+            // fully removed this left fruit from our window
+            // therefore we can remove it from our window state.
+            // thereby making the next iteration window valid
+            if freq[leftFruit] == 0 {
+                delete(freq, leftFruit)
             }
-            // make sure left ptr has escaped the minIdx to make window valid
-            if left <= minIdx {left = minIdx+1}
-            // this fruit has left our window state
-            // therefore can be removed
-            delete(idx, minIdxFruit)
+            left++
+        } else {
+            // valid window
+            maxWindow = max(maxWindow, i-left+1)
         }
-        // now our window is valid
-        maxWindow = max(maxWindow, i-left+1)
     }
     return maxWindow
 }
+
+/*
+    nested sliding window optimization
+    - can we make left ptr take bigger jumps instead of going 1-by-1 ?
+    - yes
+    - once we have a invalid window, it means we have more than 2 fruit types
+    - therefore we need to escape/remove a fruit
+    - we want to remove the earliest idx fruit so that we can keep a bigger window
+    - therefore use a idx map to keep track of idx position of a fruit
+        - if a fruit is already tracked, it gets overriden
+        - and thats fine because if we have 3 fruits of type x
+        - and we must escape x, then we have to all 3 x's
+        - therefore the overriden value of x idx will be its last seen idx
+    - move left to earliest idx + 1
+    - remove this fruit from our window
+    - now we have a valid window
+    - save its size as needed
+
+    time = o(n*3) 
+    - the *3 because when window becomes invalid, we loop over idx map trying to find the earliest idx to remove from window
+    - this map will never exceed 3
+    - therefore o(n*3)
+    - which is just o(n)
+    space = o(3) = o(1)
+*/
+// func totalFruit(fruits []int) int {
+//     left := 0
+//     maxWindow := 0
+//     idx := map[int]int{}
+//     for i := 0; i < len(fruits); i++ {
+//         idx[fruits[i]] = i
+//         if len(idx) > 2 {
+//             // invalid window
+//             // to make it valid, escape the earliest idx to keep window size large
+//             minIdx := len(fruits)+1
+//             minIdxFruit := -1
+//             for k, v := range idx {
+//                 if v < minIdx {
+//                     minIdx = v
+//                     minIdxFruit = k
+//                 }
+//             }
+//             // make sure left ptr has escaped the minIdx to make window valid
+//             if left <= minIdx {left = minIdx+1}
+//             // this fruit has left our window state
+//             // therefore can be removed
+//             delete(idx, minIdxFruit)
+//         }
+//         // now our window is valid
+//         maxWindow = max(maxWindow, i-left+1)
+//     }
+//     return maxWindow
+// }
 
 /*
     we have 2 baskets
