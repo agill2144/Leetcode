06c5249c8542ef894254e901a9915065ae1d9ebb
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-07-31 22:32:13 -0700

    Time: 4 ms (82.74%) | Memory: 2.7 MB (95.94%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.go
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.go
@@ -9,6 +9,7 @@ func characterReplacement(s string, k int) int {
         nonRepeating := (i-left+1)-maxRepeating
         if nonRepeating > k {
             freq[s[left]]--
+            if freq[s[left]] == 0 {delete(freq, s[left])}
             left++
         } else {
             maxWin = max(maxWin, i-left+1)
