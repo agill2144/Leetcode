836a98d8ef3d42271ba842a8fff025f31f4ccdc5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-01 22:58:27 -0700

    Time: 85 ms (72.04%) | Memory: 7.3 MB (77.92%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.go
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.go
@@ -4,15 +4,24 @@
 // similar approach as runningSum pattern;
 // https://leetcode.com/problems/subarray-sum-equals-k/
 func numberOfSubarrays(nums []int, k int) int {
-    oddCounts := map[int]int{0:1}
-    res := 0
-    runningCount := 0
+    return count(nums, k) - count(nums, k-1)
+}
+
+func count(nums []int, k int) int {
+    left := 0
+    c := 0
+    oddCount := 0
     for i := 0; i < len(nums); i++ {
         if nums[i] % 2 != 0 {
-            runningCount++
+            oddCount++
         }
-        res += oddCounts[runningCount-k]
-        oddCounts[runningCount]++
+        for oddCount > k {
+            if nums[left] % 2 != 0 {
+                oddCount--
+            }
+            left++
+        }
+        c += (i-left+1)
     }
-    return res
+    return c
 }
