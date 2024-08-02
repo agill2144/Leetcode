c916b3b766ead873aa541829d27ad76e91b7e6da
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-01 21:53:44 -0700

    Time: 10 ms (63.03%) | Memory: 2.8 MB (37.48%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.go
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.go
@@ -7,15 +7,35 @@ func characterReplacement(s string, k int) int {
         freq[s[i]]++
         maxRep = max(maxRep, freq[s[i]])
         nonRepeating := (i-left+1) - maxRep
-        for nonRepeating > k {
+        if nonRepeating <= k {
+            maxWin = max(maxWin, i-left+1)
+        } else {
             freq[s[left]]--
-            if freq[s[left]] == 0 {delete(freq, s[left])}
-            maxRep = 0
-            for _, v := range freq {maxRep = max(maxRep, v)}
             left++
-            nonRepeating = (i-left+1) - maxRep            
         }
-        maxWin = max(maxWin, i-left+1)        
     }
     return maxWin
+
 }
+
+// func characterReplacement(s string, k int) int {
+//     freq := map[byte]int{}
+//     maxRep := 0
+//     left := 0
+//     maxWin := 0
+//     for i := 0; i < len(s); i++ {
+//         freq[s[i]]++
+//         maxRep = max(maxRep, freq[s[i]])
+//         nonRepeating := (i-left+1) - maxRep
+//         for nonRepeating > k {
+//             freq[s[left]]--
+//             if freq[s[left]] == 0 {delete(freq, s[left])}
+//             maxRep = 0
+//             for _, v := range freq {maxRep = max(maxRep, v)}
+//             left++
+//             nonRepeating = (i-left+1) - maxRep            
+//         }
+//         maxWin = max(maxWin, i-left+1)
+//     }
+//     return maxWin
+// }
