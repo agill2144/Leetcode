30e174d1f5ed0e0cfc54001f472de0e2ea52e1de
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-01 22:43:36 -0700

    Time: 24 ms (67.83%) | Memory: 7.1 MB (53.04%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
@@ -1,16 +1,33 @@
-// duplicate of: https://leetcode.com/problems/subarray-sum-equals-k/
 func numSubarraysWithSum(nums []int, goal int) int {
-    count := 0
-    rSum := 0
-    sumCount := map[int]int{0:1}
-    for i := 0; i < len(nums); i++ {
-        rSum += nums[i]
-        diff := rSum - goal
-        val, ok := sumCount[diff]
-        if ok {
-            count += val
-        }
-        sumCount[rSum]++
-    }
-    return count
+    if goal < 0 {return 0}
+    return count(nums, goal) - count(nums, goal-1)
 }
+
+func count(nums []int, goal int) int {
+    c := 0
+    left := 0
+    sum := 0
+    for i := 0; i < len(nums); i++ {
+        sum += nums[i]
+        for sum > goal && left <= i {
+            sum -= nums[left]
+            left++
+        }
+        c += (i-left+1)
+    }
+    return c
+}
+
+// duplicate of: https://leetcode.com/problems/subarray-sum-equals-k/
+// func numSubarraysWithSum(nums []int, goal int) int {
+//     count := 0
+//     rSum := 0
+//     sumFreq := map[int]int{0:1}
+//     for i := 0; i < len(nums); i++ {
+//         rSum += nums[i]
+//         diff := rSum - goal
+//         count += sumFreq[diff]
+//         sumFreq[rSum]++
+//     }
+//     return count
+// }
