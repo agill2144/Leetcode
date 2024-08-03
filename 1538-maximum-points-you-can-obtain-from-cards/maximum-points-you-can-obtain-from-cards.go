886a002a57d6851e9e141519216c88b3683b7127
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-02 20:38:19 -0700

    Time: 50 ms (15.53%) | Memory: 9.1 MB (16.51%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.go
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.go
@@ -1,20 +1,17 @@
 func maxScore(cardPoints []int, k int) int {
-    n := len(cardPoints)
     total := 0
-    for i := 0; i < n; i++ {
-        total += cardPoints[i]
-    }
-    if k >= n {return total}
-    left := 0 
-    size := n-k
+    n := len(cardPoints)
+    for i := 0; i < n; i++ {total += cardPoints[i]}
+    if k >= n {return total }
+    
+    invertedWindowSize := n-k
     rSum := 0
+    left := 0
     maxPoints := 0
-
     for i := 0; i < n; i++ {
         rSum += cardPoints[i]
-        if i-left+1 == size {
-            ansSum := total-rSum
-            maxPoints = max(maxPoints, ansSum)
+        if i-left+1 == invertedWindowSize {
+            maxPoints = max(maxPoints, total - rSum)
             rSum -= cardPoints[left]
             left++
         }
