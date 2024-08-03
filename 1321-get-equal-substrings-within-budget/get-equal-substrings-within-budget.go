4b480614a1dc6883f1ca375eaca6461aef6e7c27
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-03 14:18:21 -0700

    Time: 3 ms (74.14%) | Memory: 3 MB (86.21%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.go
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.go
@@ -1,26 +1,18 @@
-// n = len(s) or len(t); does not matter which one because they are both same size
-// time = o(n)
-// space = o(1)
 func equalSubstring(s string, t string, maxCost int) int {
-    l1, l2 := 0, 0
+    maxWin := 0
+    left := 0
     cost := 0
-    ans := 0
     for i := 0; i < len(s); i++ {
-        sChar := s[i]
-        tChar := t[i]
-        diff := abs(int(sChar-'a')-int(tChar-'a'))
+        diff := abs(int(s[i]-'a')-int(t[i]-'a'))
         cost += diff
         for cost > maxCost {
-            sLeftChar := s[l1]
-            tLeftChar := t[l2]
-            leftCharDiff := abs(int(sLeftChar-'a')-int(tLeftChar-'a'))
-            cost -= leftCharDiff
-            l1++
-            l2++
+            leftCost := abs(int(s[left]-'a')-int(t[left]-'a'))
+            cost -= leftCost
+            left++
         }
-        ans = max(ans, i-l1+1)
+        maxWin = max(maxWin, i-left+1)
     }
-    return ans
+    return maxWin
 }
 
 func abs(x int) int {
