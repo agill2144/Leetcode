eed165e6177d29f374cf47a5992c00dae418f168
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-03 14:44:07 -0700

    Time: 128 ms (34.69%) | Memory: 6.7 MB (89.80%) - LeetSync

--- 2890-count-substrings-without-repeating-character/count-substrings-without-repeating-character.go
+++ 2890-count-substrings-without-repeating-character/count-substrings-without-repeating-character.go
@@ -1,18 +1,33 @@
 func numberOfSpecialSubstrings(s string) int {
-    freq := map[byte]int{}
+    idx := map[byte]int{}
     count := 0
     left := 0
     for i := 0; i < len(s); i++ {
-        _, ok := freq[s[i]]
-        for ok {
-            leftChar := s[left]
-            freq[leftChar]--
-            if freq[leftChar] == 0 {delete(freq, leftChar)}
-            left++
-            _, ok = freq[s[i]]
+        lastSeen, ok := idx[s[i]]
+        if ok && left <= lastSeen {
+            left = lastSeen+1
         }
-        freq[s[i]]++
+        idx[s[i]] = i
         count += (i-left+1)
     }
     return count
 }
+
+// func numberOfSpecialSubstrings(s string) int {
+//     freq := map[byte]int{}
+//     count := 0
+//     left := 0
+//     for i := 0; i < len(s); i++ {
+//         _, ok := freq[s[i]]
+//         for ok {
+//             leftChar := s[left]
+//             freq[leftChar]--
+//             if freq[leftChar] == 0 {delete(freq, leftChar)}
+//             left++
+//             _, ok = freq[s[i]]
+//         }
+//         freq[s[i]]++
+//         count += (i-left+1)
+//     }
+//     return count
+// }
