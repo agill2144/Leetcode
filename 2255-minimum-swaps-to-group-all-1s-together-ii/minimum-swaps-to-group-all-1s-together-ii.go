0b90c9a3275932dcb14c105c4d47f705285222c9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-03 22:34:56 -0700

    Time: 78 ms (66.06%) | Memory: 7.8 MB (78.79%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.go
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.go
@@ -1,36 +1,20 @@
-// pattern: let sliding window wrap around
-// dont use the right(i) ptr to stop the loop
-// if right(i) ptr reaches the end, wrap it around ( reset back to 0 )
-// use the left ptr to stop the loop
-// once left ptr reaches the end idx, it means we have used endIdx + [0:i] 
-// i.e we have used to wrap our window back to the front
-
 func minSwaps(nums []int) int {
-    k := 0
-    for i := 0; i < len(nums); i++ {
-        if nums[i] == 1 {
-            k++
-        }
-    }
-    if k == 0 {return 0}
-
-    left := 0
     ones := 0
-    n := len(nums)
+    for i := 0; i < len(nums); i++ {
+        if nums[i] == 1 {ones++}
+    }
+    if ones == 0 {return 0}
+    left := 0
     swaps := math.MaxInt64
+    oneCount := 0
     i := 0
-    for left < n {
-        ii := i % n
-        if nums[ii] == 1 {
-            ones++
-        }
-        size := i-left+1
-        if size == k {
-            nonOnes := size - ones
-            swaps = min(swaps, nonOnes)
-            if nums[left] == 1 {
-                ones--
-            }
+    for left < len(nums) {
+        ii := i % len(nums)
+        if nums[ii] == 1 {oneCount++}
+        if i-left+1 == ones {
+            zeros := (i-left+1)-oneCount
+            swaps = min(swaps, zeros)
+            if nums[left] == 1 {oneCount--}
             left++
         }
         i++
