1ab9f0ea7b068a747e61585a6db4975336c7e31f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-03 19:03:35 -0700

    Time: 86 ms (7.69%) | Memory: 18.9 MB (7.69%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.go
@@ -0,0 +1,17 @@
+func rangeSum(nums []int, n int, left int, right int) int {
+    sums := []int{}
+    for i := 0; i < n; i++ {
+        s := 0
+        for j := i; j < n; j++ {
+            s+=nums[j]
+            sums = append(sums, s)
+        }
+    }
+    sort.Ints(sums)
+    ans := 0
+    mod := 1000000007
+    for i := left-1; i < right; i++ {
+        ans = (ans + sums[i]) % mod
+    }
+    return ans
+}
