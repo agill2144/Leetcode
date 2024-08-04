d5302d33db2f4e6c122427c328144f85df0a4dd7
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-04 16:45:30 -0700

    Time: 37 ms (62.96%) | Memory: 6.3 MB (62.96%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.go
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.go
@@ -1,38 +1,26 @@
-// we are not count number of substrings that match the rule
-// we are counting out of all substring that did match the rule, return the size
-// of that 1 substring the occured the most
-// { "aat": 11, "aa": 10 "bb": 10, "cba" : 92 }
-// we return size of cba, since its the most frequently occuring substr that satifies all rules
 func maxFreq(s string, maxLetters int, minSize int, maxSize int) int {
-    freqMap := map[byte]int{}
-    maxCount := 0
     left := 0
-    wordSizeCounts := map[string]int{}
+    maxSubStrFreq := 0
+    subStrFreq := map[string]int{}
+    charFreq := map[byte]int{}
     for i := 0; i < len(s); i++ {
-        char := s[i]
-        freqMap[char]++
-
-        for len(freqMap) > maxLetters {
+        charFreq[s[i]]++
+        for len(charFreq) > maxLetters {
             leftChar := s[left]
-            freqMap[leftChar]--
-            if val := freqMap[leftChar]; val == 0 {
-                delete(freqMap, leftChar)
-            }
+            charFreq[leftChar]--
+            if charFreq[leftChar] == 0 {delete(charFreq, leftChar)}
             left++
         }
         
         for i-left+1 >= minSize && i-left+1 <= maxSize {
-            word := s[left:i+1]
-            wordSizeCounts[word]++
-            if val := wordSizeCounts[word]; val > maxCount {maxCount = val}
-
+            subStr := s[left:i+1]
+            subStrFreq[subStr]++
+            maxSubStrFreq = max(maxSubStrFreq, subStrFreq[subStr])
             leftChar := s[left]
-            freqMap[leftChar]--
-            if val := freqMap[leftChar]; val == 0 {
-                delete(freqMap, leftChar)
-            }
-            left++            
+            charFreq[leftChar]--
+            if charFreq[leftChar] == 0 {delete(charFreq, leftChar)}
+            left++
         }
     }
-    return maxCount
+    return maxSubStrFreq
 }
