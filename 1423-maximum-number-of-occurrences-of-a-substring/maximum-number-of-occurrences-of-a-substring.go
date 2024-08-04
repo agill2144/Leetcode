8518fb17a125139c87ac554ecea6aad80a74b671
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-04 16:47:42 -0700

    Time: 41 ms (40.74%) | Memory: 6.3 MB (62.96%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.go
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.go
@@ -5,21 +5,21 @@ func maxFreq(s string, maxLetters int, minSize int, maxSize int) int {
     charFreq := map[byte]int{}
     for i := 0; i < len(s); i++ {
         charFreq[s[i]]++
-        for len(charFreq) > maxLetters {
+        if len(charFreq) > maxLetters {
             leftChar := s[left]
             charFreq[leftChar]--
             if charFreq[leftChar] == 0 {delete(charFreq, leftChar)}
             left++
-        }
-        
-        for i-left+1 >= minSize && i-left+1 <= maxSize {
-            subStr := s[left:i+1]
-            subStrFreq[subStr]++
-            maxSubStrFreq = max(maxSubStrFreq, subStrFreq[subStr])
-            leftChar := s[left]
-            charFreq[leftChar]--
-            if charFreq[leftChar] == 0 {delete(charFreq, leftChar)}
-            left++
+        } else {        
+            for i-left+1 >= minSize && i-left+1 <= maxSize {
+                subStr := s[left:i+1]
+                subStrFreq[subStr]++
+                maxSubStrFreq = max(maxSubStrFreq, subStrFreq[subStr])
+                leftChar := s[left]
+                charFreq[leftChar]--
+                if charFreq[leftChar] == 0 {delete(charFreq, leftChar)}
+                left++
+            }
         }
     }
     return maxSubStrFreq
