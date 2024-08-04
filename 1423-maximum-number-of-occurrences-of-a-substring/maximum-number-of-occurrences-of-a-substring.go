7d6eaf398dfda80a0ff1160cbe43efbd0dbe3291
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-04 16:50:44 -0700

    Time: 41 ms (40.74%) | Memory: 6.1 MB (100.00%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.go
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.go
@@ -11,7 +11,6 @@ func maxFreq(s string, maxLetters int, minSize int, maxSize int) int {
             if charFreq[leftChar] == 0 {delete(charFreq, leftChar)}
             left++
         } else {
-        
             for i-left+1 >= minSize && i-left+1 <= maxSize {
                 subStr := s[left:i+1]
                 subStrFreq[subStr]++
@@ -21,8 +20,44 @@ func maxFreq(s string, maxLetters int, minSize int, maxSize int) int {
                 if charFreq[leftChar] == 0 {delete(charFreq, leftChar)}
                 left++
             }
-
         }
     }
     return maxSubStrFreq
 }
+
+/*
+    classic sliding window
+    - make sure the window is valid first!
+    - then form all substrings within the valid window
+        - take the first string of the window
+        - now shrink the window from left side
+        - again check if its valid ( winSize >= minSize and winSize <= maxSize )
+        - form the next string in the window
+        - then repeat..
+*/
+// func maxFreq(s string, maxLetters int, minSize int, maxSize int) int {
+//     left := 0
+//     maxSubStrFreq := 0
+//     subStrFreq := map[string]int{}
+//     charFreq := map[byte]int{}
+//     for i := 0; i < len(s); i++ {
+//         charFreq[s[i]]++
+//         for len(charFreq) > maxLetters {
+//             leftChar := s[left]
+//             charFreq[leftChar]--
+//             if charFreq[leftChar] == 0 {delete(charFreq, leftChar)}
+//             left++
+//         }
+        
+//         for i-left+1 >= minSize && i-left+1 <= maxSize {
+//             subStr := s[left:i+1]
+//             subStrFreq[subStr]++
+//             maxSubStrFreq = max(maxSubStrFreq, subStrFreq[subStr])
+//             leftChar := s[left]
+//             charFreq[leftChar]--
+//             if charFreq[leftChar] == 0 {delete(charFreq, leftChar)}
+//             left++
+//         }
+//     }
+//     return maxSubStrFreq
+// }
