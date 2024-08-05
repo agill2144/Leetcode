c8907b050172ea6f1bcf9afa5324fe569b0d11f9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-05 00:09:17 -0700

    Time: 47 ms (11.48%) | Memory: 4 MB (14.75%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.go
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.go
@@ -1,21 +1,46 @@
 func numberOfSubstrings(s string) int {
-    a := 0
-    b := 0
-    c := 0
+    return countLessThanOrEqualTo(s, 3) - countLessThanOrEqualTo(s, 2)
+}
+
+func countLessThanOrEqualTo(s string, k int) int {
     left := 0
-    n := len(s)
     count := 0
-    for i := 0; i < n; i++ {
-        if s[i] == 'a' {a++}
-        if s[i] == 'b' {b++}
-        if s[i] == 'c' {c++}
-        for a > 0 && b > 0 && c > 0 {
-            count += (n-1-i+1)
-            if s[left] == 'a' {a--}
-            if s[left] == 'b' {b--}
-            if s[left] == 'c' {c--}
+    freq := map[byte]int{}
+    for i := 0; i < len(s); i++ {
+        freq[s[i]]++
+        for len(freq) > k {
+            leftChar := s[left]
+            freq[leftChar]--
+            if freq[leftChar] == 0 {delete(freq, leftChar)}
             left++
         }
+        count += (i-left+1)
     }
     return count
 }
+
+// func numberOfSubstrings(s string) int {
+//     a := 0
+//     b := 0
+//     c := 0
+//     left := 0
+//     n := len(s)
+//     count := 0
+//     for i := 0; i < n; i++ {
+//         if s[i] == 'a' {a++}
+//         if s[i] == 'b' {b++}
+//         if s[i] == 'c' {c++}
+//         for a > 0 && b > 0 && c > 0 {
+//             // ending at idx i , we have a valid substr
+//             // it means, anything else we add from right of i, will not make this invalid
+//             // therefore we find the end position of a valid window
+//             // and then do a count number of substrings we 
+//             count += (n-1-i+1)
+//             if s[left] == 'a' {a--}
+//             if s[left] == 'b' {b--}
+//             if s[left] == 'c' {c--}
+//             left++
+//         }
+//     }
+//     return count
+// }
