6ee330ca7d54347122e68d11c40f2f5372e45d39
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-04 22:17:16 -0700

    Time: 6 ms (78.69%) | Memory: 3.9 MB (83.61%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.go
@@ -0,0 +1,21 @@
+func numberOfSubstrings(s string) int {
+    a := 0
+    b := 0
+    c := 0
+    left := 0
+    n := len(s)
+    count := 0
+    for i := 0; i < n; i++ {
+        if s[i] == 'a' {a++}
+        if s[i] == 'b' {b++}
+        if s[i] == 'c' {c++}
+        for a >= 1 && b >= 1 && c >= 1 {
+            count += (n-1-i+1)
+            if s[left] == 'a' {a--}
+            if s[left] == 'b' {b--}
+            if s[left] == 'c' {c--}
+            left++
+        }
+    }
+    return count
+}
