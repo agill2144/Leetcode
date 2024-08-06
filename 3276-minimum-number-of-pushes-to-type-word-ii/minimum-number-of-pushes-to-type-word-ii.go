c5f6f49e04cdc13a5d17a5d05859a87264bfb2ae
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-05 17:44:02 -0700

    Time: 115 ms (29.17%) | Memory: 6.8 MB (75.00%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.go
@@ -0,0 +1,29 @@
+type pair struct {
+    char byte
+    freq int
+}
+
+func minimumPushes(word string) int {
+    freq := map[byte]int{}
+    for i := 0; i < len(word); i++ {freq[word[i]]++}
+    freqArr := []pair{}
+    for k, v := range freq {freqArr = append(freqArr, pair{k, v})}
+    sort.Slice(freqArr, func(i, j int)bool{
+        return freqArr[i].freq > freqArr[j].freq
+    })
+    
+    keyPad := map[int][]byte{}
+    digit := 2
+    charToDigit := map[byte][]int{} // {$char: [$digit, $idxInDigitArr]}
+    total := 0
+    for i := 0; i < len(freqArr); i++ {
+        if digit > 9 {digit = 2}
+        keyPad[digit] = append(keyPad[digit], freqArr[i].char)
+        charToDigit[freqArr[i].char] = []int{digit, len(keyPad[digit])-1}
+        times := len(keyPad[digit])
+        total += (freqArr[i].freq * times)
+        digit++
+    }
+
+    return total
+}
