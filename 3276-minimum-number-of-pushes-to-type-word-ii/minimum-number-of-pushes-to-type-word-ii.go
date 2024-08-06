770f41959ca243ef2d5e5a87e611865681900466
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-05 17:54:45 -0700

    Time: 115 ms (29.17%) | Memory: 6.7 MB (95.83%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.go
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.go
@@ -3,9 +3,27 @@ type pair struct {
     freq int
 }
 
+/*
+    greedy, assign the highest freq char 
+    to be the first char on an empty key in the keypad
+    
+    word = "accccccccccccbbb"
+    because we are allowed to remap any char to any key
+    we should make sure that c and b are the first char on a key
+    so that we dont have to press a key more than once to get all occurences of c and b
+    therefore highest repeating char is assigned to an empty key if possible
+
+    w = len(word)
+    
+    time;
+    o(w) + o(26 log 26) + o(26) = o(w)
+    
+    space;
+    o(26) + o(26) + o(9 digits in keyPad) = o(1)
+*/
 func minimumPushes(word string) int {
     freq := map[byte]int{}
-    for i := 0; i < len(word); i++ {freq[word[i]]++}
+    for i := 0; i < len(word); i++ {freq[word[i]]++} 
     freqArr := []pair{}
     for k, v := range freq {freqArr = append(freqArr, pair{k, v})}
     sort.Slice(freqArr, func(i, j int)bool{
