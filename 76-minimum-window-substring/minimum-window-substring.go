eaaf3af82b2fcf5ac637b36ec590828864d9529f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-06 22:18:32 -0700

    Time: 40 ms (18.64%) | Memory: 3.5 MB (35.05%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.go
@@ -0,0 +1,34 @@
+func minWindow(s string, t string) string {
+    if len(t) > len(s) {return ""}
+    tMap := map[byte]int{}
+    for i := 0; i < len(t); i++ {tMap[t[i]]++}
+    fm := 0
+    minWin := len(s)+1
+    minStr := ""
+    left := 0
+    freq := map[byte]int{}
+    for i := 0; i < len(s); i++ {
+        freq[s[i]]++
+        _, ok := tMap[s[i]]
+        if ok {
+            tMap[s[i]]--
+            if tMap[s[i]] == 0 {fm++}
+        }
+        for fm == len(tMap) {
+            if i-left+1 < minWin {
+                minWin = i-left+1
+                minStr = s[left:i+1]
+            }
+            minWin = min(minWin, i-left+1)
+            leftChar := s[left]
+            freq[leftChar]++
+            if _, ok := tMap[leftChar]; ok {
+                tMap[leftChar]++
+                if tMap[leftChar] == 1 {fm--}
+            }
+            left++
+        }
+
+    }
+    return minStr
+}
