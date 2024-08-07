f4f2bb3777f78bfa45997285329ab338d465e73e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-06 22:22:43 -0700

    Time: 9 ms (49.37%) | Memory: 2.7 MB (77.22%) - LeetSync

--- 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.go
+++ 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.go
@@ -1,13 +1,11 @@
-
 func lengthOfLongestSubstringKDistinct(s string, k int) int {
-    // we are allowed at have <= k uniq chars in a window
-    freq := map[byte]int{}
     left := 0
+    freq := map[byte]int{}
     maxWin := 0
     for i := 0; i < len(s); i++ {
         freq[s[i]]++
         if len(freq) <= k {
-            maxWin = max(maxWin, i-left+1)
+            maxWin = max(maxWin, i-left+1)            
         } else {
             freq[s[left]]--
             if freq[s[left]] == 0 {delete(freq, s[left])}
@@ -16,19 +14,3 @@ func lengthOfLongestSubstringKDistinct(s string, k int) int {
     }
     return maxWin
 }
-// func lengthOfLongestSubstringKDistinct(s string, k int) int {
-//     // we are allowed at have <= k uniq chars in a window
-//     freq := map[byte]int{}
-//     left := 0
-//     maxWin := 0
-//     for i := 0; i < len(s); i++ {
-//         freq[s[i]]++
-//         for len(freq) > k {
-//             freq[s[left]]--
-//             if freq[s[left]] == 0 {delete(freq, s[left])}
-//             left++
-//         }
-//         maxWin = max(maxWin, i-left+1)
-//     }
-//     return maxWin
-// }
