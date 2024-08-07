fc6d2a862d3331dfd18647dbf7f60d95fac40ff5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-07 16:30:25 -0700

    Time: 97 ms (30.00%) | Memory: 19.9 MB (60.00%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.go
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.go
@@ -1,28 +1,29 @@
 func vowelStrings(words []string, queries [][]int) []int {
-    vowels := make([]bool, len(words))
     prefixCounts := make([]int, len(words))
     for i := 0; i < len(words); i++ {
-        vowels[i] = isVowelWord(words[i])
-        prevCount := 0
-        if i - 1 >= 0 {prevCount = prefixCounts[i-1]}
-        prefixCounts[i] = prevCount
-        if vowels[i] {prefixCounts[i]++}
+        if isVowel(words[i]) {
+            count := 1
+            if i-1 >= 0 {count += prefixCounts[i-1]}
+            prefixCounts[i] = count
+        } else {
+            if i-1 >= 0 {prefixCounts[i] = prefixCounts[i-1]}            
+        }
     }
-    out := []int{}
+    out := make([]int, len(queries))
     for i := 0; i < len(queries); i++ {
         start := queries[i][0]
         end := queries[i][1]
         x := prefixCounts[end]
         y := 0
-        if start-1 >= 0 {y = prefixCounts[start-1]}
-        out = append(out, x-y)
+        if start - 1 >= 0 { y = prefixCounts[start-1] }
+        out[i] = x-y
     }
     return out
 }
 
-func isVowelWord(word string) bool {
-    vowels := map[byte]bool{
-        'a': true, 'e': true, 'i':true, 'o':true, 'u':true,
-    }
-    return vowels[word[0]] && vowels[word[len(word)-1]]
+func isVowel(word string) bool {
+    char1 := word[0]
+    char2 := word[len(word)-1]
+    vowels := map[byte]bool{'a':true,'e':true,'i':true,'o':true,'u':true}
+    return vowels[char1] && vowels[char2]
 }
