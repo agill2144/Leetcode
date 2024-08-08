225d3512484a9e7765cefdaca5b1bdb627628286
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-07 22:06:39 -0700

    Time: 161 ms (42.86%) | Memory: 9.1 MB (48.98%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.go
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.go
@@ -1,34 +1,21 @@
-// this is not a medium ... 
-// this is borderline hard .. wtf
-// https://www.youtube.com/watch?v=iOqH_JnXIOQ
-// time = o(nlogn) + o(n * logn)
 func maxFrequency(nums []int, k int) int {
     sort.Ints(nums)
-    prefixSum := make([]int, len(nums))
-    prefixSum[0] = nums[0]
-    for i := 1; i < len(nums); i++ {
-        prefixSum[i] = prefixSum[i-1]+nums[i]
-    }
-    maxFreq := 0
-    for i := 0; i < len(nums); i++ {
-        left := 0
-        right := i
-        target := nums[i]
-        for left <= right {
-            mid := left + (right-left)/2
-            windowSize := i-mid+1
-            desiredSum := windowSize * target
-            leftSumToRemove := 0
-            if mid-1 >= 0 {leftSumToRemove = prefixSum[mid-1]}
-            actualSum := prefixSum[i]-leftSumToRemove
-            ops := desiredSum - actualSum
-            if ops <= k {
-                maxFreq = max(maxFreq, windowSize)
-                right = mid-1
-            } else {
-                left = mid+1
-            }
+    n := len(nums)
+    maxWin := 1
+    right := n-1
+    rSum := nums[n-1]
+    for i := n-2; i >= 0; i-- {
+        size := right-i+1
+        val := nums[right]
+        desiredSum := val * size
+        rSum += nums[i]
+        if desiredSum - rSum <= k {
+            maxWin = max(maxWin, size)
+            continue
+        } else {
+            rSum -= nums[right]
+            right--
         }
     }
-    return maxFreq
+    return maxWin
 }
