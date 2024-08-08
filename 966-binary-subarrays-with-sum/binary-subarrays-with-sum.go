96e41e5b48ee9dea3e75bcbbfb13d1c3cb5dec36
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-07 20:57:01 -0700

    Time: 33 ms (44.60%) | Memory: 7.1 MB (54.68%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
@@ -1,40 +1,13 @@
+// prefixSum / runningSum approach
 func numSubarraysWithSum(nums []int, goal int) int {
-    if goal < 0 {return 0}    
-    return countLessThanOrEqualTo(nums, goal) - countLessThanOrEqualTo(nums, goal-1) 
-}
-
-func countLessThanOrEqualTo(nums []int, target int) int {
+    freq := map[int]int{0:1}
+    rSum := 0
     count := 0
-    left := 0
-    sum := 0
     for i := 0; i < len(nums); i++ {
-        sum += nums[i]
-        for sum > target && left <= i {
-            sum -= nums[left]
-            left++
-        }
-        count += (i-left+1)
+        rSum += nums[i]
+        toRemove := rSum - goal
+        count += freq[toRemove]
+        freq[rSum]++
     }
     return count
 }
-
-/*
-    approach: runningSum pattern
-    - num of subarrays + some mathematical = runningSum
-    - identical to https://leetcode.com/problems/subarray-sum-equals-k/description/
-    time = o(n)
-    space = o(n)
-*/
-
-// func numSubarraysWithSum(nums []int, goal int) int {
-//     sumCount := map[int]int{0:1}
-//     rSum := 0
-//     count := 0
-//     for i := 0; i < len(nums); i++ {
-//         rSum += nums[i]
-//         diff := rSum-goal
-//         count += sumCount[diff]
-//         sumCount[rSum]++
-//     }
-//     return count
-// }
