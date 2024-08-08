80c38e60be81f39781839bd17102b1d7e7116d5d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-07 21:00:21 -0700

    Time: 20 ms (83.45%) | Memory: 6.8 MB (69.06%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
@@ -1,13 +1,34 @@
-// prefixSum / runningSum approach
+// sliding window atMost k pattern
 func numSubarraysWithSum(nums []int, goal int) int {
-    freq := map[int]int{0:1}
-    rSum := 0
+    if goal < 0 {return 0}
+    return countAtMost(nums, goal) - countAtMost(nums, goal-1)
+}
+
+func countAtMost(nums []int, k int)int {
     count := 0
+    left := 0
+    rSum := 0
     for i := 0; i < len(nums); i++ {
         rSum += nums[i]
-        toRemove := rSum - goal
-        count += freq[toRemove]
-        freq[rSum]++
+        for rSum > k && left <= i {
+            rSum -= nums[left]
+            left++
+        }
+        count += (i-left+1)
     }
     return count
 }
+
+// prefixSum / runningSum approach
+// func numSubarraysWithSum(nums []int, goal int) int {
+//     freq := map[int]int{0:1}
+//     rSum := 0
+//     count := 0
+//     for i := 0; i < len(nums); i++ {
+//         rSum += nums[i]
+//         toRemove := rSum - goal
+//         count += freq[toRemove]
+//         freq[rSum]++
+//     }
+//     return count
+// }
