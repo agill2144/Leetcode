6e034d24260a0cbd33ce423380c0f657e0a214e5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-07 22:17:52 -0700

    Time: 218 ms (72.00%) | Memory: 13.6 MB (72.00%) - LeetSync

---
+++ 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.go
@@ -0,0 +1,15 @@
+// hash hash = brain turning into a potato
+func findLonely(nums []int) []int {
+    freq := map[int]int{}
+    for i := 0; i < len(nums); i++ {
+        freq[nums[i]]++
+    }
+    out := []int{}
+    for k, v := range freq {
+        if v > 1 {continue}
+        if freq[k-1] == 0 && freq[k+1] == 0{
+            out = append(out, k)
+        }
+    }
+    return out
+}
