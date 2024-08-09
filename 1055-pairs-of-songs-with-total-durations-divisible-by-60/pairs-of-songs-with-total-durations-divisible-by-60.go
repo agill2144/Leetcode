542bcdd5ac03a115dacb20cf4fb661bf8a75ea5e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-08 21:09:07 -0700

    Time: 20 ms (96.55%) | Memory: 6.9 MB (63.79%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.go
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.go
@@ -1,11 +1,16 @@
 func numPairsDivisibleBy60(time []int) int {
     rem := map[int]int{}
     count := 0
+    /*
+        (a+b) % 60
+        is same as
+        a%60 + b%60
+
+    */
     for i := 0; i < len(time); i++ {
         r := time[i] % 60
-        target := 60-r
-        if r == 0 {target = 0}
-        count += rem[target]
+        
+        count += rem[(60-r)%60]
         rem[r]++
     }
     return count
