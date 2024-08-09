c4b43a1cff3faddc61d1431b3b3ee1821d4407d8
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-08 18:45:01 -0700

    Time: 2 ms (56.25%) | Memory: 2.3 MB (100.00%) - LeetSync

---
+++ 870-magic-squares-in-grid/magic-squares-in-grid.go
@@ -0,0 +1,63 @@
+// what a stupid problem to waste time on ... fuck you google
+func numMagicSquaresInside(grid [][]int) int {
+    m := len(grid)
+    n := len(grid[0])
+    if m < 3 || n < 3 {return 0}
+    count := 0
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if i+2 < m && j+2 < n {
+                if isValid(i, j, grid) {
+                    count++
+                }
+            }
+        }
+    }
+    return count
+}
+
+
+func isValid(r, c int, grid [][]int) bool {
+    rowSum := -1 // not yet identified
+    colSum := -1 // not yet identified
+    set := map[int]struct{}{}
+    for i := r; i <= r+2; i++ {
+        rSum := 0
+        for j := c; j <= c+2; j++ {
+            if _, ok := set[grid[i][j]]; ok {return false}
+            if grid[i][j] < 1 || grid[i][j] > 9 {return false}
+            rSum += grid[i][j]
+            set[grid[i][j]] = struct{}{}
+        }
+        if rowSum == -1 {rowSum = rSum}
+        if rSum != rowSum {return false}
+    }
+
+    for j := c; j <= c+2; j++ {
+        cSum := 0
+        for i := r; i <= r+2; i++ {
+            cSum += grid[i][j]
+        }      
+        if colSum == -1 {colSum = cSum}
+        if cSum != colSum {return false}
+        if colSum != rowSum {return false}  
+    }
+
+    lrSum := 0
+    lrr := r; lrc := c
+    for lrr <= r+2 && lrc <= c+2 {
+        lrSum += grid[lrr][lrc]
+        lrr++
+        lrc++
+    }
+
+    rlSum := 0
+    rlr := r; rlc := c+2
+    for rlr <= r+2 && rlc <= c+2 {
+        rlSum += grid[rlr][rlc]
+        rlr++
+        rlc--
+    }
+    fmt.Println(r, c, lrSum, rlSum, colSum, rowSum)
+    return lrSum == rlSum && rlSum == rowSum && rowSum == colSum
+}
