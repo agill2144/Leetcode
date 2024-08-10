9ae46dba30b1c6170eab8b6f4e728b8569a50ab8
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-09 19:30:46 -0700

    Time: 93 ms (52.38%) | Memory: 7.4 MB (88.10%) - LeetSync

--- 890-lemonade-change/lemonade-change.go
+++ 890-lemonade-change/lemonade-change.go
@@ -1,23 +1,15 @@
 func lemonadeChange(bills []int) bool {
     fives := 0
     tens := 0
-    twentys := 0
-
     for i := 0; i < len(bills); i++ {
-        curr := bills[i]
-        diff := curr-5
-        if diff == 5 {
+        currBill := bills[i]
+        if currBill == 5 {
+            fives++
+        } else if currBill == 10 {
             if fives == 0 {return false}
+            tens++
             fives--
-        } else if diff == 10 {
-            if tens >= 1 {
-                tens--
-            } else if fives >= 2 {
-                fives-=2
-            } else {
-                return false
-            }
-        } else if diff == 15 {
+        } else if currBill == 20 {
             if tens >= 1 && fives >= 1 {
                 tens--
                 fives--
@@ -27,10 +19,6 @@ func lemonadeChange(bills []int) bool {
                 return false
             }
         }
-
-        if curr == 5 {fives++}
-        if curr == 10 {tens++}
-        if curr == 20 {twentys++}
     }
     return true
 }
