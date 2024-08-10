0f56ce6b34b53d359fbe2723483dc21bb786f4a9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-10 16:47:21 -0700

    Time: 317 ms (16.81%) | Memory: 7.3 MB (19.88%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.go
+++ 5-longest-palindromic-substring/longest-palindromic-substring.go
@@ -1,33 +1,40 @@
 func longestPalindrome(s string) string {
-    memo := make([][]*bool, 1001) // 0 = false, 1 = true
-    for i := 0; i < len(memo); i++ {
-        memo[i] = make([]*bool, 1001)
+    n := len(s)
+    memo := make([][]bool, n)
+    for i := range memo {
+        memo[i] = make([]bool, n)
     }
+
     var dfs func(i, j int) bool
     dfs = func(i, j int) bool {
-        if i >= j {return true}
-        
-        if memo[i][j] != nil {
-            return *memo[i][j]
+        if i >= j {
+            return true
         }
 
-        if s[i] == s[j] {
-            memo[i][j] = toPtrBool(dfs(i+1, j-1))
-        } else {
-            memo[i][j] = toPtrBool(false)
+        if memo[i][j] {
+            return true
         }
-        return *memo[i][j]
+
+        if s[i] == s[j] && dfs(i+1, j-1) {
+            memo[i][j] = true
+            return true
+        }
+
+        return false
     }
+
     maxSize := 0
     maxStr := ""
-    for i := 0; i < len(s); i++ {
-        for j := i; j < len(s); j++ {
+    for i := 0; i < n; i++ {
+        for j := i; j < n; j++ {
             if dfs(i, j) {
-                if j-i+1 > maxSize {maxSize = j-i+1; maxStr = s[i:j+1]}
+                if j-i+1 > maxSize {
+                    maxSize = j-i+1
+                    maxStr = s[i:j+1]
+                }
             }
         }
     }
+
     return maxStr
 }
-
-func toPtrBool(x bool) *bool {return &x}
