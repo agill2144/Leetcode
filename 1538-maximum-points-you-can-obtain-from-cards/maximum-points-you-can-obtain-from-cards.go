7e02d5b839a7592ee32ec3e6a30e70a0c4ff791d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-11 14:45:23 -0700

    Time: 42 ms (54.87%) | Memory: 7.5 MB (89.38%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.go
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.go
@@ -1,44 +1,18 @@
-// wrapping / circular winow : flowing from end and wrapping around from start
 func maxScore(cardPoints []int, k int) int {
     n := len(cardPoints)
-    i := n-k
     left := n-k
+    rSum := 0
+    i := left
     maxPoints := 0
-    sum := 0
-    // <= n because we could not pick any cards from the back, and pick all from the front
-    // when when left == n; handle idx out of bound
-    // hence the if check in the middle
     for left <= n {
-        sum += cardPoints[i % n]
+        rSum += cardPoints[i%n]
         if i-left+1 == k {
-            maxPoints = max(maxPoints, sum)
+            maxPoints = max(maxPoints, rSum)
             if left == n {break}
-            sum -= cardPoints[left]
+            rSum -= cardPoints[left]
             left++
         }
-        i++        
+        i++
     }
     return maxPoints
 }
-
-// inverted window
-// func maxScore(cardPoints []int, k int) int {
-//     total := 0
-//     n := len(cardPoints)
-//     for i := 0; i < n; i++ {total += cardPoints[i]}
-//     if k >= n {return total }
-
-//     invertedWindowSize := n-k
-//     rSum := 0
-//     left := 0
-//     maxPoints := 0
-//     for i := 0; i < n; i++ {
-//         rSum += cardPoints[i]
-//         if i-left+1 == invertedWindowSize {
-//             maxPoints = max(maxPoints, total - rSum)
-//             rSum -= cardPoints[left]
-//             left++
-//         }
-//     }
-//     return maxPoints
-// }
