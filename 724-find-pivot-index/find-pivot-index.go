092cfa76687d3b9da605d17c9b9070929a06df33
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-11 16:16:26 -0700

    Time: 25 ms (14.33%) | Memory: 7.9 MB (7.44%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.go
+++ 724-find-pivot-index/find-pivot-index.go
@@ -1,15 +1,16 @@
 func pivotIndex(nums []int) int {
-    prefixSum := make([]int, len(nums))   
+    prefixSum := []int{}
     rSum := 0
     for i := 0; i < len(nums); i++ {
         rSum += nums[i]
-        prefixSum[i] = rSum
+        prefixSum = append(prefixSum, rSum)
     }
-    for i := 0; i < len(prefixSum); i++ {
-        leftSum := 0
-        if i-1 >= 0 {leftSum = prefixSum[i-1]}
-        rightSum := prefixSum[len(prefixSum)-1] - prefixSum[i]
-        if leftSum == rightSum {return i}
+    for i := 0; i < len(nums); i++ {
+        total := prefixSum[len(prefixSum)-1]
+        left := 0
+        if i-1 >= 0 {left = prefixSum[i-1]}
+        right :=  total-prefixSum[i]
+        if left == right {return i}     
     }
     return -1
 }
