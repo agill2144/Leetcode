738a005462803f4ad809666c60f31aa99f6a0127
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-11 20:15:07 -0700

    Time: 49 ms (8.96%) | Memory: 4 MB (10.45%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.go
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.go
@@ -1,21 +1,14 @@
 func numberOfSubstrings(s string) int {
-    a := 0
-    b := 0
-    c := 0
+    freq := map[byte]int{}
     left := 0
     count := 0
     n := len(s)
     for i := 0; i < n; i++ {
-        if s[i] == 'a' {a++}
-        if s[i] == 'b' {b++}
-        if s[i] == 'c' {c++}
-        for a > 0 && b > 0 && c > 0 {
+        freq[s[i]]++
+        for freq['a'] > 0 && freq['b'] > 0 && freq['c'] > 0 {
             count += ((n-1)-i+1)
-            leftChar := s[left]
-            if leftChar == 'a' {a--}
-            if leftChar == 'b' {b--}
-            if leftChar == 'c' {c--}
-            left++            
+            freq[s[left]]--
+            left++
         }
     }
     return count
