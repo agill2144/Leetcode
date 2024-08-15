908e423287aee76b760050ae71786b281dc9f465
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-14 21:30:39 -0700

    Time: 123 ms (98.90%) | Memory: 10.3 MB (32.97%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.go
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.go
@@ -2,21 +2,21 @@ func maxFrequency(nums []int, k int) int {
     sort.Ints(nums)
     n := len(nums)
     right := n-1
-    rSum := nums[n-1]
-    maxFreq := 1
+    maxWin := 1
+    rSum := nums[right]
     for i := n-2; i >= 0; i-- {
         curr := nums[i]
-        matchTo := nums[right]
-        size := right-i+1
-        totalSum := matchTo * size
         rSum += curr
-        diff := totalSum - rSum 
+        size := right-i+1
+        rightVal := nums[right]
+        desiredSum := rightVal * size
+        diff := desiredSum - rSum
         if diff <= k {
-            maxFreq = max(maxFreq, size)
-            continue
+            maxWin = max(maxWin, size)
+        } else {
+            rSum -= nums[right]
+            right--
         }
-        rSum -= nums[right]
-        right--
     }
-    return maxFreq
+    return maxWin
 }
