f169d0f038006dbd16cc5aca97b6f3f9ee3cdf46
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-18 16:35:58 -0700

    Time: 7 ms (89.83%) | Memory: 6.1 MB (32.36%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.go
+++ 45-jump-game-ii/jump-game-ii.go
@@ -1,16 +1,16 @@
 func jump(nums []int) int {
-    n := len(nums)
-    if n <= 1 {return 0}
     left := 0
     right := 0
+    n := len(nums)
+    if n <= 1 {return 0}
     jumps := 0
-    for left < n && right < n {
-        farthest := -1
+    for right < n-1 {
+        farthestIdx := -1
         for i := left; i <= right; i++ {
-            farthest = max(farthest,i+nums[i])
+            farthestIdx = max(farthestIdx, i+nums[i])
         }
         left = right+1
-        right = farthest
+        right = farthestIdx
         jumps++
         if right >= n-1 {break}
     }
