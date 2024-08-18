558a697ab432a5902f3f7c0477be1b35f87c0032
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-18 16:27:55 -0700

    Time: 52 ms (47.04%) | Memory: 7.1 MB (79.66%) - LeetSync

--- 55-jump-game/jump-game.go
+++ 55-jump-game/jump-game.go
@@ -1,42 +1,19 @@
-// top down memo
+/*
+    approach: greedy
+    - from an ith position, keep track of how far you can jump
+    - and then go to next ith position, and update farthest idx as needed
+    - keep doing this as long as i <= farthest idx
+
+    time = o(n)
+    space = o(1)
+*/
 func canJump(nums []int) bool {
-    var dfs func(start int) bool
-    memo := map[int]*bool{}
-    dfs = func(start int) bool {
-        // base
-        if start >= len(nums)-1 {return true}
-
-        // logic
-
-        // when we are at node with value 0
-        // its impossible to reach end from here
-        if nums[start] == 0 { return false }
-
-        for k := nums[start]; k >= 1 ; k-- {
-            if memo[start+k] == nil {
-                // we dont have an answer for start+k
-                // compute it and save it
-                memo[start+k] = toPtrBool(dfs(start+k))
-                // exit early if we were able to reach
-                if *memo[start+k] {return true}
-            } else {
-                // if we do have an answer saved for start+k
-                // return whatever that saved ans is
-                return *memo[start+k]
-            }
-        }
-        return false
+    farthestIdx := 0
+    i := 0
+    n := len(nums)
+    for i < n && i <= farthestIdx {
+        farthestIdx = max(farthestIdx, i+nums[i])
+        i++
     }
-    return dfs(0)
+    return farthestIdx >= n-1
 }
-func toPtrBool(b bool) *bool {return &b }
-// func canJump(nums []int) bool {
-//     farthestIdx := 0
-//     i := 0
-//     n := len(nums)
-//     for i < n && i <= farthestIdx {
-//         farthestIdx = max(farthestIdx, i+nums[i])
-//         i++
-//     }
-//     return farthestIdx >= n-1
-// }
