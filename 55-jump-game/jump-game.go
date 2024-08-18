1a84468c4606f808d3808bef95bc6609532f684d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-17 21:59:02 -0700

    Time: 88 ms (26.38%) | Memory: 12.9 MB (5.25%) - LeetSync

--- 55-jump-game/jump-game.go
+++ 55-jump-game/jump-game.go
@@ -1,10 +1,36 @@
 func canJump(nums []int) bool {
-    farthestIdx := 0
-    i := 0
-    n := len(nums)
-    for i < n && i <= farthestIdx {
-        farthestIdx = max(farthestIdx, i+nums[i])
-        i++
+    var dfs func(start int) bool
+    memo := map[int]*bool{}
+    dfs = func(start int) bool {
+        // base
+        if start >= len(nums)-1 {return true}
+
+        // logic
+        val2, ok2 := memo[start]
+        if ok2 {return *val2}
+        if nums[start] == 0 { return false }
+
+        for k := nums[start]; k >= 1 ; k-- {
+            val, ok := memo[start+k]
+            if !ok {
+                memo[start+k] = toPtrBool(dfs(start+k))
+                if *memo[start+k] {return true} 
+            } else {
+                return *val
+            }
+        }
+        return false
     }
-    return farthestIdx >= n-1
+    return dfs(0)
 }
+func toPtrBool(b bool) *bool {return &b }
+// func canJump(nums []int) bool {
+//     farthestIdx := 0
+//     i := 0
+//     n := len(nums)
+//     for i < n && i <= farthestIdx {
+//         farthestIdx = max(farthestIdx, i+nums[i])
+//         i++
+//     }
+//     return farthestIdx >= n-1
+// }
