e6cbbc2785078f2fa65a021d598a0619d6994464
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-18 20:26:28 -0700

    Time: 8 ms (9.89%) | Memory: 5.4 MB (5.44%) - LeetSync

--- 57-insert-interval/insert-interval.go
+++ 57-insert-interval/insert-interval.go
@@ -1,42 +1,49 @@
 func insert(intervals [][]int, newInterval []int) [][]int {
     n := len(intervals)
     if n == 0 {return [][]int{newInterval}}
+    idx := binarySearch(intervals, newInterval[0])
+    
+    tmp := [][]int{}
+    i := 0
+    for i < idx {tmp = append(tmp, intervals[i]);i++}
+    tmp = append(tmp, newInterval)
+    for i < len(intervals) {tmp = append(tmp, intervals[i]); i++}
+    
     out := [][]int{}
-    out = append(out, intervals[0])
-    i := 1
-    used := false
-
-    if newInterval[0] < intervals[0][0] {
-        out[len(out)-1] = newInterval
-        used = true
-        i = 0
-    }
-    for i < n || !used {
-        start := math.MaxInt64
-        end := math.MaxInt64
-        if i < n {start = intervals[i][0]; end = intervals[i][1]}
-        newStart := newInterval[0]
-        newEnd := newInterval[1]
-        lastStart := out[len(out)-1][0]
-        lastEnd := out[len(out)-1][1]
-
-        if newStart < start && !used {
-            if newStart <= lastEnd {
-                out[len(out)-1][0] = min(lastStart, newStart)
-                out[len(out)-1][1] = max(lastEnd, newEnd)
-            } else {
-                out = append(out, newInterval)
-            }
-            used = true
+    for i := 0; i < len(tmp); i++ {
+        if len(out) != 0 && tmp[i][0] <= out[len(out)-1][1] {
+            out[len(out)-1][0] = min(tmp[i][0], out[len(out)-1][0])
+            out[len(out)-1][1] = max(tmp[i][1], out[len(out)-1][1])
         } else {
-            if start <= lastEnd {
-                out[len(out)-1][0] = min(lastStart, start)
-                out[len(out)-1][1] = max(lastEnd, end)
-            } else {
-                out = append(out, intervals[i])   
-            }
-            i++
+            out = append(out, tmp[i])
         }
     }
     return out
 }
+
+func binarySearch(intervals [][]int, target int) int {
+    // 1,3,6,9,10 ; target 2 -> return idx 1 ( rightmost number on left side of target )
+    // 6,7,8,9; target 2 -> return idx 0
+    // 1,2,3; target 5 -> return 2
+
+    left := 0
+    right := len(intervals)-1
+    ans := -1
+    for left <= right {
+        mid := left + (right-left)/2
+        start := intervals[mid][0]
+        if start <= target {
+            ans = mid
+            // keep searching right because we want the rightmost value on the left side of target
+            left = mid+1
+        }  else {
+            right = mid-1
+        }
+    }
+
+    // why +1 ?
+    // because with above binary search, we found the rightmost idx on left side target
+    // but we need to insert the new interval just after this idx position
+    // therefore +1 ; to indicate that this is the idx position to insert at
+    return ans+1
+}  
