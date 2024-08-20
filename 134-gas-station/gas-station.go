d4014bb4734e95f06296f10b9d0b128173e8d723
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-19 20:27:21 -0700

    Time: 122 ms (68.06%) | Memory: 8.5 MB (86.36%) - LeetSync

--- 134-gas-station/gas-station.go
+++ 134-gas-station/gas-station.go
@@ -1,4 +1,16 @@
 // like kadanes max subarray sum algorithm
+// "start a chain / subarray "
+// "if a chain / subarray goes negative, start a fresh one"
+// similarly, evaluate starting at EACH gas station
+// starting from 0th
+// fuel up, travel and remove travel dist (cost[i])
+// if we run out of gas ( current gas goes negative )
+// it means that the idx we started from is not good
+// therefore start a new evaluation from current idx
+// what happens if the ans is one of the gas station we went over when our current gas had not went negative yet
+// this is why we will run a 2*n loop, because there is gauranteed to be 1 answer or no answer
+// time = o(2n)
+// space = o(1)
 func canCompleteCircuit(gas []int, cost []int) int {
     curr := 0
     n := len(gas)
