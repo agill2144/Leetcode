e49d8d3e5961c67de672eeee2005dc99fb23d54a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-19 20:23:12 -0700

    Time: 110 ms (94.42%) | Memory: 8.5 MB (86.36%) - LeetSync

--- 134-gas-station/gas-station.go
+++ 134-gas-station/gas-station.go
@@ -1,3 +1,4 @@
+// like kadanes max subarray sum algorithm
 func canCompleteCircuit(gas []int, cost []int) int {
     curr := 0
     n := len(gas)
@@ -11,12 +12,17 @@ func canCompleteCircuit(gas []int, cost []int) int {
         curr -= cost[i%n]
         
         // if we are out (< 0), we reset
+        // just like max sum subarray: "it means our subarray sum went negative"
+        // there is no point in continuing in that subarray
+        // start a new one!
         if curr < 0 {
             curr = 0
             count = 0
             start = (i+1)%n
         } else {
             count++
+            // if we were able to reach all n stations, then we can call it success!
+            // the start of the subarray/chain is the correct gas station
             if count == n {return start}
         }
     }
