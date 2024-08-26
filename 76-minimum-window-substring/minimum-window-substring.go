587072a5f5a5140da0387d173d9e5052491a4763
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-25 22:42:35 -0700

    Time: 30 ms (39.81%) | Memory: 3.4 MB (68.04%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.go
+++ 76-minimum-window-substring/minimum-window-substring.go
@@ -1,31 +1,28 @@
 func minWindow(s string, t string) string {
-    if len(t) > len(s) {return ""}
     tMap := map[byte]int{}
-    for i := 0; i < len(t); i++ {tMap[t[i]]++}
-    fm := 0
-    minWin := len(s)+1
-    minStr := ""
+    for i := 0; i < len(t); i++ {
+        tMap[t[i]]++
+    }
+    fm := len(tMap)
+    ans := ""
     left := 0
     for i := 0; i < len(s); i++ {
         _, ok := tMap[s[i]]
         if ok {
             tMap[s[i]]--
-            if tMap[s[i]] == 0 {fm++}
-        }
-        for fm == len(tMap) {
-            if i-left+1 < minWin {
-                minWin = i-left+1
-                minStr = s[left:i+1]
+            if tMap[s[i]] == 0 {fm--}
+            for fm == 0 {
+                if ans == "" || i-left+1 < len(ans) {
+                    ans = s[left:i+1]
+                }
+                _, ok2 := tMap[s[left]]
+                if ok2 {
+                    tMap[s[left]]++
+                    if tMap[s[left]] == 1{fm++}
+                }
+                left++
             }
-            minWin = min(minWin, i-left+1)
-            leftChar := s[left]
-            if _, ok := tMap[leftChar]; ok {
-                tMap[leftChar]++
-                if tMap[leftChar] == 1 {fm--}
-            }
-            left++
         }
-
     }
-    return minStr
+    return ans
 }
