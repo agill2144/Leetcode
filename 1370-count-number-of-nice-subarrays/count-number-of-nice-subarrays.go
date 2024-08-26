ff5e3d5185c059291a776432107b30075e01d2b3
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-25 22:08:27 -0700

    Time: 132 ms (18.56%) | Memory: 10.6 MB (19.59%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.go
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.go
@@ -1,12 +1,14 @@
 func numberOfSubarrays(nums []int, k int) int {
-    oddCount := 0
     oddCounts := map[int]int{0:1}
-    total := 0   
+    rCount := 0
+    total := 0
     for i := 0; i < len(nums); i++ {
-        if nums[i] % 2 != 0 {oddCount++}
-        remove := oddCount-k
-        total += oddCounts[remove]
-        oddCounts[oddCount]++
+        if nums[i] % 2 != 0 {
+            rCount++
+        }
+        diff := rCount - k
+        total += oddCounts[diff]
+        oddCounts[rCount]++
     }
     return total
 }
