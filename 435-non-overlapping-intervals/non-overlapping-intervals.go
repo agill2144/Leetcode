c980350e923486ce3d30640591a8953d05145aeb
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-26 21:04:43 -0700

    Time: 188 ms (56.29%) | Memory: 17.7 MB (28.14%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.go
+++ 435-non-overlapping-intervals/non-overlapping-intervals.go
@@ -1,38 +1,14 @@
 func eraseOverlapIntervals(intervals [][]int) int {
-    sort.Slice(intervals, func(i, j int)bool{
-        return intervals[i][0] < intervals[j][0]
+    sort.Slice(intervals, func(i, j int)bool {
+        return intervals[i][1] < intervals[j][1]
     })
-    /*
-        [
-            [1,12]
-            [1,3]
-            [2,3]
-            [3,4]
-        ]
-    */
-    removedCount := 0
-    prev := 0 // idx of prev/last interval we decided to keep
-    for i := 1; i < len(intervals); i++ {
-        start, end := intervals[i][0], intervals[i][1]
-        pEnd := intervals[prev][1]
-        if start < pEnd {
-            // we need to either remove curr or prev interval
-            // therefore increment our counter thats tracking num of intervals removed
-            removedCount++
-
-            // remove the one that ends the later ( end time is further way)
-            // [1,12] , [1,3] = in this one, we want to keep [1,3]
-            // therefore prevIdx = i , 
-            // otherwise we remove current interval, and than prev stays where it is 
-            if end < pEnd {
-                // current one ends before prev one
-                // therefore remove prev
-                prev = i
-            }
-        } else {
-            // no overlap, prev becomes current
-            prev = i
+    lastEnd := math.MinInt64
+    count := 0 // count taken
+    for i := 0; i < len(intervals); i++ {
+        if lastEnd == math.MinInt64 || intervals[i][0] >= lastEnd{
+            lastEnd = intervals[i][1]
+            count++
         }
     }
-    return removedCount
+    return len(intervals)-count
 }
