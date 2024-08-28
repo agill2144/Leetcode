1aef6de3821a87eefee4b0b6cda37ecd59346baa
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-27 21:08:33 -0700

    Time: 129 ms (25.93%) | Memory: 7 MB (48.15%) - LeetSync

--- 2890-count-substrings-without-repeating-character/count-substrings-without-repeating-character.go
+++ 2890-count-substrings-without-repeating-character/count-substrings-without-repeating-character.go
@@ -1,13 +1,11 @@
 func numberOfSpecialSubstrings(s string) int {
-    idx := map[byte]int{}
     count := 0
+    idx := map[byte]int{}
     left := 0
     for i := 0; i < len(s); i++ {
-        lastSeenAt, ok := idx[s[i]]
-        if ok {
-            if left <= lastSeenAt {
-                left = lastSeenAt+1
-            }
+        val, ok := idx[s[i]]
+        if ok && left <= val {
+            left = val+1
         }
         idx[s[i]] = i
         count += (i-left+1)
