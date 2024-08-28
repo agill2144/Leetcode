3bd8efea258dbd97835a6efa366d6856898b7234
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-08-27 21:21:23 -0700

    Time: 0 ms (100.00%) | Memory: 3.1 MB (17.65%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.go
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.go
@@ -1,39 +1,20 @@
-
 func equalSubstring(s string, t string, maxCost int) int {
-    maxWin := 0
-    left := 0
     rCost := 0
-    for i := 0; i < len(s); i++ {
-        diff := abs(int(s[i]-'a') - int(t[i]-'a'))
-        rCost += diff
-        if rCost <= maxCost {
-            maxWin = max(maxWin, i-left+1)
-        } else {
-            leftCost := abs(int(s[left]-'a') - int(t[left]-'a'))
-            rCost -= leftCost
+    left := 0
+    maxWin := 0
+    n := len(s)
+    for i := 0; i < n; i++ {
+        rCost += abs(int(s[i]-'a') - int(t[i]-'a'))
+        if rCost > maxCost {
+            rCost -= abs(int(s[left]-'a') - int(t[left]-'a'))
             left++
+        } else {
+            maxWin = max(maxWin, i-left+1)
         }
     } 
     return maxWin
 }
-
-// func equalSubstring(s string, t string, maxCost int) int {
-//     maxWin := 0
-//     left := 0
-//     rCost := 0
-//     for i := 0; i < len(s); i++ {
-//         diff := abs(int(s[i]-'a') - int(t[i]-'a'))
-//         rCost += diff
-//         for rCost > maxCost {
-//             leftCost := abs(int(s[left]-'a') - int(t[left]-'a'))
-//             rCost -= leftCost
-//             left++
-//         }
-//         maxWin = max(maxWin, i-left+1)
-//     } 
-//     return maxWin
-// }
 func abs(x int) int {
-    if x < 0 {return x * -1}
+    if x < 0 {return x*-1 }
     return x
 }
