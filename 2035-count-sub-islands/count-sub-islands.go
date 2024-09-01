fc7a591d37e67e659b00858c43989ee4e6007e67
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-01 13:46:22 -0700

    Time: 241 ms (57.67%) | Memory: 13.7 MB (69.71%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.go
+++ 2035-count-sub-islands/count-sub-islands.go
@@ -1,29 +1,18 @@
-// we want to know whether islands in grid2 are also islands in grid1
-// use dfs to find connected lands in grid2
-// and keep making sure that grid1 at specific r,c is also a land
-// if a pariticular land does not exist in grid1, then turn some flag that indicates
-// this specific connected-land does not exist in grid1, therefore not a subisland
-// therefore only count when that state/flag says so
-// time = o(mn)
-// space = o(1)
 func countSubIslands(grid1 [][]int, grid2 [][]int) int {
-    m := len(grid1)
-    n := len(grid1[0])    
     dirs := [][]int{
         {1,0},{-1,0},
         {0,1},{0,-1},
     }
-    
-    isSubIsland := true
-    var dfs func(r, c int) 
+    m := len(grid1)
+    n := len(grid1[0])
+    isSub := true
+    var dfs func(r, c int)
     dfs = func(r, c int) {
         // base
         if r < 0 || r == m || c < 0 || c == n || grid2[r][c] != 1 {return}
 
         // logic
-        if grid1[r][c] != 1 {
-            isSubIsland = false
-        }
+        if grid1[r][c] != 1 {isSub = false}
         grid1[r][c] = 0
         grid2[r][c] = 0
         for _, dir := range dirs {
@@ -35,8 +24,8 @@ func countSubIslands(grid1 [][]int, grid2 [][]int) int {
         for j := 0; j < n; j++ {
             if grid2[i][j] == 1 {
                 dfs(i, j)
-                if isSubIsland {count++}
-                isSubIsland = true
+                if isSub {count++}
+                isSub = true
             }
         }
     }
