8811efc358da0b5bb764500b810ec8bb6e65a666
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-02 13:27:04 -0700

    Time: 189 ms (55.29%) | Memory: 19.9 MB (7.06%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.go
+++ 435-non-overlapping-intervals/non-overlapping-intervals.go
@@ -2,18 +2,23 @@ func eraseOverlapIntervals(intervals [][]int) int {
     sort.Slice(intervals, func(i, j int)bool{
         return intervals[i][0] < intervals[j][0]
     })
-    eraseCount := 0
+    taken := 1
     lastEnd := intervals[0][1]
     for i := 1; i < len(intervals); i++ {
         start, end := intervals[i][0], intervals[i][1]
         if start < lastEnd {
             // overlap, meaning we need to drop this or prev meeting (i.e erase one of them )
+            // always evaluate the golden thumb rule: "can we take/keep the earliest ending time"
+            // yes, take the one that ends the earliest
+            // so that 1 big interval like [1,10] ( prev ) does not stop us from picking [1,2][2,3][3,4]
+            // the question becomes "should we take [1,10] or [1,2] ? given that the rest of intervals are  [2,3][3,4]" 
             lastEnd = min(lastEnd, end)
-            eraseCount++
         } else {
             // no overlap, nothing to drop, nothing to erase
             lastEnd = end
+            taken++
         }
     }
-    return eraseCount
+    // erased meeting = n - taken
+    return len(intervals) - taken
 }
