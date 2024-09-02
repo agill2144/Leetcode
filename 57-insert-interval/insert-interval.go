442ccf4133301f9ee021149fa743212af1190c64
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-02 14:12:39 -0700

    Time: 7 ms (57.89%) | Memory: 4.7 MB (8.17%) - LeetSync

--- 57-insert-interval/insert-interval.go
+++ 57-insert-interval/insert-interval.go
@@ -1,39 +1,43 @@
 func insert(intervals [][]int, newInterval []int) [][]int {
     if len(intervals) == 0 {return [][]int{newInterval}}
-    insertIdx := binarySearch(intervals, newInterval[0])
+    idx := search(intervals, newInterval[0])
     tmp := [][]int{}
-    tmp = append(tmp, intervals[:insertIdx]...)
+    tmp = append(tmp, intervals[:idx]...)
     tmp = append(tmp, newInterval)
-    tmp = append(tmp, intervals[insertIdx:]...)
-    out := [][]int{tmp[0]}
+    tmp = append(tmp, intervals[idx:]...)
+    merged := [][]int{tmp[0]}
     for i := 1; i < len(tmp); i++ {
         start, end := tmp[i][0], tmp[i][1]
-        prevEnd := out[len(out)-1][1]
+        prevStart, prevEnd := merged[len(merged)-1][0],merged[len(merged)-1][1]
         if start <= prevEnd {
-            out[len(out)-1][1] = max(prevEnd, end)
+            merged[len(merged)-1][0] = min(start, prevStart)
+            merged[len(merged)-1][1] = max(end, prevEnd)
         } else {
-            out = append(out, tmp[i])
+            merged = append(merged, tmp[i])
         }
     }
-    return out
+    return merged
+
 }
 
-func binarySearch(intervals [][]int, target int) int {
+func search(intervals [][]int, target int) int {
     if target < intervals[0][0] {return 0}
     if target > intervals[len(intervals)-1][0] {return len(intervals)}
-    // right most value on left side of target
     left := 0
     right := len(intervals)-1
+    // find right most val on left side of target
     ans := 0
     for left <= right {
-        mid := left + (right-left)/2
+        mid := left+(right-left)/2
         val := intervals[mid][0]
-        if val > target {
-            right = mid-1
-        } else {
+        if val <= target {
             ans = mid
-            left = mid+1
+            left = mid+1 // since we want the right most value on left side of target
+        } else {
+            right = mid-1
         }
     }
+    // why +1 ?
+    // because this function is supposed to return the insert idx position
     return ans+1
 }
