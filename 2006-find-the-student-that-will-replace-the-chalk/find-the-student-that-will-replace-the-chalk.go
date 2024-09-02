ab67730479fe244ff733dea305c5fcb016bcde25
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-01 17:42:44 -0700

    Time: 89 ms (93.10%) | Memory: 9.3 MB (37.93%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.go
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.go
@@ -1,14 +1,26 @@
 func chalkReplacer(chalk []int, k int) int {
     total := 0
     for i := 0; i < len(chalk); i++ {total += chalk[i]; if total > k {break}}
+    // we could simulate taking a round trip in a for loop
+    // or, we can do it using mod operator
+    // if k = 22 and total = 11;
+    // it means, we can take 2 full trips from idx 0 to n-1
+    // so now we can use a for loop to remove total from k ( while k >= total { k-= total })
+    // then we can use a idx ptr to figure out which idx position we will end up at
+    // otherwise, we can use mod operator
+    // if k = 24 and total = 11
+    // then k % total ; 24 % 11; leaves a remainder of 2 
+    // with this mod operator; we have essentially ran the trip from idx 0 to n-1 2 times ( becuase quotient will also be 2 )
+    // leaving us with remaining k value ( i.e the remainder ) = 2
+    // now we can use a idx ptr to simulate 
     k %= total
-    idx := 0
     n := len(chalk)
-    for k >= chalk[idx%n] {
-        if k >= chalk[idx%n] {
-            k -= chalk[idx%n]
-            idx++
+    for i := 0; i < n; i++ {
+        if k >= chalk[i] {
+            k -= chalk[i]
+        } else {
+            return i
         }
     }
-    return idx%n
+    return -1
 }
