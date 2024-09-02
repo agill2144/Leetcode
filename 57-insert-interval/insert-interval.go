da4854c2850b7eb01448811a8167670d0f84c0d0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-02 14:18:00 -0700

    Time: 3 ms (91.27%) | Memory: 4.8 MB (8.17%) - LeetSync

--- 57-insert-interval/insert-interval.go
+++ 57-insert-interval/insert-interval.go
@@ -1,10 +1,33 @@
+/*
+    approach: 
+    - search, insert, merge
+    - given sorted intervals, use binary search to find the correct insert idx position
+        - "find right most val on left side of target"
+    - create a tmp array, and insert newInterval as well as old ones
+        - there may be overalap
+    - now finally, merge overlapping intervals
+        - overlapping in this question is; start <= prevEnd time
+    
+    time;
+    - search = o(logn)
+    - insert = o(n)
+    - merge = o(n)
+    total time = o(logn) + o(2n)
+
+    space = o(n) for tmp array
+*/
 func insert(intervals [][]int, newInterval []int) [][]int {
     if len(intervals) == 0 {return [][]int{newInterval}}
     idx := search(intervals, newInterval[0])
+    // space = o(n)
     tmp := [][]int{}
     tmp = append(tmp, intervals[:idx]...)
     tmp = append(tmp, newInterval)
     tmp = append(tmp, intervals[idx:]...)
+
+    // space = o(n) but will be o(1) if output array is not counted
+    // some people count it, some dont, i dont
+    // therefore o(1) space for this merged array
     merged := [][]int{tmp[0]}
     for i := 1; i < len(tmp); i++ {
         start, end := tmp[i][0], tmp[i][1]
@@ -20,6 +43,9 @@ func insert(intervals [][]int, newInterval []int) [][]int {
 
 }
 
+
+// time = o(logn)
+// space = o(1)
 func search(intervals [][]int, target int) int {
     if target < intervals[0][0] {return 0}
     if target > intervals[len(intervals)-1][0] {return len(intervals)}
