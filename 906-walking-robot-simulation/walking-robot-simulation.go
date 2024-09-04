c559ef26bfe7c25f05bfe14b05582312f75deedd
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-04 00:10:17 -0700

    Time: 38 ms (84.62%) | Memory: 7.3 MB (38.46%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.go
+++ 906-walking-robot-simulation/walking-robot-simulation.go
@@ -4,24 +4,19 @@ func robotSim(commands []int, obstacles [][]int) int {
         r,c := obstacles[i][0], obstacles[i][1]
         obSet[[2]int{r,c}] = true
     }
-    dirMap := map[[2]int]map[int][2]int{
-        [2]int{0,1}: map[int][2]int{-2:[2]int{-1,0}, -1:[2]int{1,0}},
-        [2]int{0,-1}: map[int][2]int{-2:[2]int{1,0}, -1:[2]int{-1,0}},
-        [2]int{-1,0}: map[int][2]int{-2:[2]int{0,-1}, -1:[2]int{0,1}},
-        [2]int{1,0}: map[int][2]int{-2:[2]int{0,1}, -1:[2]int{0,-1}},
-    }
-    currDir := [2]int{0,1} // looking right 
+    // clockwise starting from east
+    dirs := [][]int{{0,1},{1,0},{0,-1},{-1,0}} // east -> south -> west -> north
+    ptr := 0 // facing east
     maxDist := 0
     r := 0
     c := 0
     for i := 0; i < len(commands); i++ {
         cmd := commands[i]
         if cmd == -1 || cmd == -2 {
-            currDir = dirMap[currDir][cmd]
+            if cmd == -1 {ptr++} else { ptr-- }
+            if ptr == -1 {ptr = len(dirs)-1} else if ptr == len(dirs) {ptr = 0}
         } else {
-            // fmt.Println("currDir: ", currDir)
-            // fmt.Println("curr pos: ", r,c)
-            // fmt.Println("steps: ", commands[i])
+            currDir := dirs[ptr]
             nr := r
             nc := c
             stepsTaken := 0
@@ -30,13 +25,19 @@ func robotSim(commands []int, obstacles [][]int) int {
                 nr += currDir[0]
                 nc += currDir[1]
                 stepsTaken++
-                if obSet[[2]int{nr,nc}] { nr -= currDir[0]; nc -= currDir[1]; break }
+                if obSet[[2]int{nr,nc}] { 
+                    // ran into an obstacle
+                    // cannot be in a cell that has obstacle
+                    // therefore take 1 step back in the curr dir we are in
+                    nr -= currDir[0]; 
+                    nc -= currDir[1]; 
+                    break
+                }
                 dist := int(math.Pow(float64(nr), 2.0)) + int(math.Pow(float64(nc), 2.0))
                 maxDist = max(maxDist, dist)
             }
             r = nr
             c = nc
-            // fmt.Println("post pos: ", r, c)
         }
     }
     return maxDist
