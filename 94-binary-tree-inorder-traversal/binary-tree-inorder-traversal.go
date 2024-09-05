555c3e4ee67a43e0e0bd3bf55cc13c3978fa9238
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-04 19:55:32 -0700

    Time: 1 ms (75.22%) | Memory: 2.3 MB (75.44%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+    out := []int{}
+    var dfs func(r *TreeNode)
+    dfs = func(r *TreeNode) {
+        // base
+        if r == nil {return}
+
+        // logic
+        dfs(r.Left)
+        out = append(out, r.Val)
+        dfs(r.Right)
+    }
+    dfs(root)
+    return out
+}
