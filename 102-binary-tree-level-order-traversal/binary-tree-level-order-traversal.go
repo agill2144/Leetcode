f4c3ae48272422ae8029bb233bacf34b1934c6c2
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-04 20:01:12 -0700

    Time: 0 ms (100.00%) | Memory: 4.1 MB (14.46%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.go
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.go
@@ -7,27 +7,50 @@
  * }
  */
 
-// level order using bfs
+// level order using dfs
+// maintain level as dfs arg
+// and use the level as a idx in output array
 func levelOrder(root *TreeNode) [][]int {
     out := [][]int{}
-    if root == nil {return out}
-    q := []*TreeNode{root}    
-    for len(q) != 0 {
-        qSize := len(q)
-        level := []int{}
-        for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            level = append(level, dq.Val)
-            if dq.Left != nil {
-                q = append(q, dq.Left)
-            }
-            if dq.Right != nil {
-                q = append(q, dq.Right)
-            }
-            qSize--
+    var dfs func(r *TreeNode, level int)
+    dfs = func(r *TreeNode, level int) {
+        // base
+        if r == nil {return}
+
+        // logic
+        if len(out) == level {
+            out = append(out, []int{})
         }
-        out = append(out, level)
+        out[level] = append(out[level], r.Val)
+        dfs(r.Left, level+1)
+        dfs(r.Right, level+1)
     }
+    dfs(root, 0)
     return out
 }
+
+
+// level order using bfs
+// func levelOrder(root *TreeNode) [][]int {
+//     out := [][]int{}
+//     if root == nil {return out}
+//     q := []*TreeNode{root}    
+//     for len(q) != 0 {
+//         qSize := len(q)
+//         level := []int{}
+//         for qSize != 0 {
+//             dq := q[0]
+//             q = q[1:]
+//             level = append(level, dq.Val)
+//             if dq.Left != nil {
+//                 q = append(q, dq.Left)
+//             }
+//             if dq.Right != nil {
+//                 q = append(q, dq.Right)
+//             }
+//             qSize--
+//         }
+//         out = append(out, level)
+//     }
+//     return out
+// }
