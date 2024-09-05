e497b26e54bbf6ffa9838c909f110a52766218fa
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-04 20:04:35 -0700

    Time: 1 ms (75.17%) | Memory: 2.3 MB (6.99%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.go
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.go
@@ -6,17 +6,35 @@
  *     Right *TreeNode
  * }
  */
+// iterative
 func preorderTraversal(root *TreeNode) []int {
+    st := []*TreeNode{}
     out := []int{}
-    var dfs func(r *TreeNode)
-    dfs = func(r *TreeNode) {
-        // base
-        if r == nil {return}
-        // logic
-        out = append(out, r.Val)
-        dfs(r.Left)
-        dfs(r.Right)
+    for root != nil || len(st) != 0 {
+        for root != nil {
+            out = append(out, root.Val)
+            st = append(st, root)
+            root = root.Left
+        }
+        root = st[len(st)-1]
+        st = st[:len(st)-1]
+        root = root.Right
     }
-    dfs(root)
     return out
 }
+
+// dfs
+// func preorderTraversal(root *TreeNode) []int {
+//     out := []int{}
+//     var dfs func(r *TreeNode)
+//     dfs = func(r *TreeNode) {
+//         // base
+//         if r == nil {return}
+//         // logic
+//         out = append(out, r.Val)
+//         dfs(r.Left)
+//         dfs(r.Right)
+//     }
+//     dfs(root)
+//     return out
+// }
