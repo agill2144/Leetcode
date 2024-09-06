75899940af9928f975b99e534f448a9b637da73f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-05 21:01:52 -0700

    Time: 4 ms (6.88%) | Memory: 3.1 MB (73.33%) - LeetSync

--- 35-search-insert-position/search-insert-position.go
+++ 35-search-insert-position/search-insert-position.go
@@ -1,46 +1,20 @@
-// use left ptr as our ans tracker
-// if target == mid; return mid; we are done
-// if target < mid; go left
-// otherwise go right
-// the answer idx will be left ptr
 func searchInsert(nums []int, target int) int {
-    n := len(nums)
-    if n == 0 {return 0}
-    if nums[len(nums)-1] < target {return n}
+    if len(nums) == 0 {return 0}  
+    if target < nums[0] {return 0}
+    if target > nums[len(nums)-1] {return len(nums)}
+    // find the right most on left side of target
+    ans := -1
     left := 0
-    right := n-1
+    right := len(nums)-1
     for left <= right {
         mid := left + (right-left)/2
-        if nums[mid] == target {return mid}
-        if target < nums[mid] {
-            right = mid-1
-        } else {
+        if nums[mid] <= target {
+            if nums[mid] == target {return mid}
+            ans = mid
             left = mid+1
+        } else {
+            right = mid-1
         }
     }
-    return left
+    return ans+1
 }
-
-
-// find and save; left most on right side of target
-// |----|--|--|--|---------|------------|----|
-//               target   leftMost
-// func searchInsert(nums []int, target int) int {
-//     n := len(nums)
-//     if n == 0 {return 0}
-//     if nums[len(nums)-1] < target {return n}
-//     left := 0
-//     right := n-1
-//     leftMostIdx := -1
-//     for left <= right {
-//         mid := left + (right-left)/2
-//         if nums[mid] >= target {
-//             if nums[mid] == target {return mid}
-//             leftMostIdx = mid
-//             right = mid-1
-//         } else {
-//             left = mid+1
-//         }
-//     }
-//     return leftMostIdx
-// }
