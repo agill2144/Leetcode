010270c978750edecf14d8e43b8df086bd256584
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-07 20:21:43 -0700

    Time: 7 ms (44.75%) | Memory: 5.6 MB (9.36%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.go
+++ 110-balanced-binary-tree/balanced-binary-tree.go
@@ -6,7 +6,19 @@
  *     Right *TreeNode
  * }
  */
+/*
+
+A height-balanced binary tree is a binary tree in which 
+the depth of the two subtrees of every node never 
+differs by more than one.
+
+approach: bottom up dfs ( height )
+- once left and right child has returned their heights to parent
+- parent will decide if so far, childs under me are balanced or not
+- if yes, take the max height + 1 ( height logic )
+*/
 func isBalanced(root *TreeNode) bool {
+
     var dfs func(r *TreeNode) (int, bool)
     dfs = func(r *TreeNode) (int, bool) {
         // base
@@ -15,16 +27,15 @@ func isBalanced(root *TreeNode) bool {
         // logic
         left, lok := dfs(r.Left)
         right, rok := dfs(r.Right)
-        if !lok || !rok {return 0, false}
-        if abs(right-left) > 1 {return 0, false}
-
+        if !lok || !rok {return -1, false}
+        if abs(right-left) > 1 {return -1, false}
         return max(left, right)+1, true
-    }
-    _, ok := dfs(root)
+    } 
+    _,  ok := dfs(root)
     return ok
 }
 
 func abs(x int) int {
-    if x < 0 {return x *-1}
+    if x < 0 {return x*-1}
     return x
 }
