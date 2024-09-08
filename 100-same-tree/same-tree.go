a307b8b6a0f3adbca0d1ff34dcbbd458abbbc79b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-07 22:16:12 -0700

    Time: 1 ms (74.44%) | Memory: 2.3 MB (69.63%) - LeetSync

--- 100-same-tree/same-tree.go
+++ 100-same-tree/same-tree.go
@@ -9,6 +9,5 @@
 func isSameTree(p *TreeNode, q *TreeNode) bool {
     if p == nil && q == nil {return true}
     if p == nil || q == nil {return false}
-    if p.Val != q.Val {return false}
-    return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+    return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right) 
 }
