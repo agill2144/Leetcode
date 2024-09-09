5cae99fe3fceee8a70fe690bec8b08ee746f662f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-09 15:53:03 -0700

    Time: 4 ms (68.42%) | Memory: 3.2 MB (70.18%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
@@ -4,6 +4,7 @@ func findMin(nums []int) int {
     ans := math.MaxInt64
     for left <= right {
         mid := left + (right-left)/2
+        // when we cannot decide which side to take search on
         if nums[mid] == nums[right] && nums[right] == nums[left] {
             // all numbers are equal
             // save mid if mid is a better answer
@@ -13,15 +14,20 @@ func findMin(nums []int) int {
             ans = min(nums[mid], ans)
             left++
             right--
+        // left side is sorted
         } else if nums[left] <= nums[mid] {
             // left sorted
             // mid is bigger than left
             // save left if left is a better answer
+            // and take search to right side;
+            // min is always on unsorted half, since there can be dupes, we saved left, if left was our min val 
             ans = min(ans, nums[left])
             left = mid+1
         } else  {
             // right sorted ( right is bigger than mid )
             // save if mid is a better answer
+            // and take search to left side;
+            // min is always on unsorted half, since there can be dupes, we saved mid, if mid was our min val 
             ans = min(ans, nums[mid])
             right = mid-1
         }
