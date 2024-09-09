33eaef9a9605252b689b56d048be2260a7ab5e4f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-08 23:07:07 -0700

    Time: 49 ms (50.15%) | Memory: 7 MB (77.65%) - LeetSync

--- 55-jump-game/jump-game.go
+++ 55-jump-game/jump-game.go
@@ -1,40 +1,10 @@
-/*
-    approach: greedy
-    - from an ith position, keep track of how far you can jump
-    - and then go to next ith position, and update farthest idx as needed
-    - keep doing this as long as i <= farthest idx
-
-    time = o(n)
-    space = o(1)
-
-    what were we greedy about?
-    - as we kept iterating, we kept track of farthest reachable idx
-    - we kept spamming the farthest jump path we can reach (greedily spamming single path to reach an answer)    
-*/
 func canJump(nums []int) bool {
     farthestIdx := 0
     i := 0
-    for i < len(nums) && i <= farthestIdx {
-        nextIdx := nums[i] + i
-        farthestIdx = max(farthestIdx, nextIdx)
+    for i <= farthestIdx && i < len(nums) {
+        farthestIdx = max(farthestIdx, nums[i]+i)
         if farthestIdx >= len(nums)-1 {return true}
         i++
     }
     return false
-    
 }
-
-// func canJump(nums []int) bool {
-//     var dfs func(idx int) bool
-//     dfs = func(idx int) bool {
-//         // base
-//         if idx >= len(nums)-1 {return true}
-
-//         // logic; from current idx, take all possible jumps
-//         for k := 1; k <= nums[idx]; k++ {
-//             if dfs(idx+k) {return true}
-//         }
-//         return false
-//     }   
-//     return dfs(0)
-// }
