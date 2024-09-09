9c2cdad4e21a67fef069115533e7b4dd9035a6d5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-08 23:15:52 -0700

    Time: 85 ms (32.42%) | Memory: 9.4 MB (28.17%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.go
+++ 53-maximum-subarray/maximum-subarray.go
@@ -1,6 +1,8 @@
 func maxSubArray(nums []int) int {
-    maxSum := math.MinInt64
+    // kadanes greedy algo, try to evaluate each idx as a new starting point
+    // start a new eval if rSum < 0 
     rSum := 0
+    maxSum := math.MinInt64
     for i := 0; i < len(nums); i++ {
         rSum += nums[i]
         maxSum = max(maxSum, rSum)
