51032acf6305067971ba6f760dcdbec33e7285b8
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-08 23:16:32 -0700

    Time: 85 ms (32.42%) | Memory: 8 MB (82.70%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.go
+++ 53-maximum-subarray/maximum-subarray.go
@@ -1,6 +1,7 @@
 func maxSubArray(nums []int) int {
     // kadanes greedy algo, try to evaluate each idx as a new starting point
     // start a new eval if rSum < 0 
+    // same logic can be applied to gas station problem too ( eval whether a gas station works, drop and start new if it does not work in the middle .. )
     rSum := 0
     maxSum := math.MinInt64
     for i := 0; i < len(nums); i++ {
