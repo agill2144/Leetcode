cfbe14099662b8c874d92f5f6c0146ac1d47e9cd
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-09 15:51:18 -0700

    Time: 4 ms (68.42%) | Memory: 3.1 MB (70.18%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
@@ -5,13 +5,23 @@ func findMin(nums []int) int {
     for left <= right {
         mid := left + (right-left)/2
         if nums[mid] == nums[right] && nums[right] == nums[left] {
+            // all numbers are equal
+            // save mid if mid is a better answer
+            // move away from both since all values are same
+            // it means we cannot decide which side to move away from
+            // therefore shrink our space and re-evaluate
             ans = min(nums[mid], ans)
             left++
             right--
         } else if nums[left] <= nums[mid] {
+            // left sorted
+            // mid is bigger than left
+            // save left if left is a better answer
             ans = min(ans, nums[left])
             left = mid+1
         } else  {
+            // right sorted ( right is bigger than mid )
+            // save if mid is a better answer
             ans = min(ans, nums[mid])
             right = mid-1
         }
