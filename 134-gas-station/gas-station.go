5a2ca21d2702874811b171758152255cc815d333
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-09 21:54:03 -0700

    Time: 111 ms (76.16%) | Memory: 9 MB (58.53%) - LeetSync

--- 134-gas-station/gas-station.go
+++ 134-gas-station/gas-station.go
@@ -18,23 +18,22 @@ func canCompleteCircuit(gas []int, cost []int) int {
 
     idx := 0
     tank := 0
-    count := 0
-    for i := 0; i < 2*n; i++ {
+    i := 0
+    for i < 2*n {
         // fill up
         tank += gas[i%n]
         // reduce the travel cost
         tank -= cost[i%n]
         // can we proceed ?
         if tank >= 0 {
-            // yes, we have fuel to go to next
-            count++
-            if count == n {return idx}
+            i++
+            if i%n == idx {return idx}
         } else {
             // no we cannot, stop, drop everything
             // and start new eval from next gas station
+            i++
             tank = 0
-            count = 0
-            idx = (i+1)%n
+            idx = i%n
         }
     }
     return -1
