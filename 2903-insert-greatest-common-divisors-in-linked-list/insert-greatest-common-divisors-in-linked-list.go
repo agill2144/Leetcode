44deb56e3289987298563ded558c11c0e51a0807
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-09 17:20:28 -0700

    Time: 10 ms (74.22%) | Memory: 6.3 MB (64.06%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.go
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.go
@@ -23,16 +23,21 @@ func insertGreatestCommonDivisors(head *ListNode) *ListNode {
     return head
 }
 
+// greatest common divisior is highest such x that can divide both a and b evenly
+// meaning a/x and b/x leaves remainder as 0
 func gcd(a, b int) int {
     // base
     if a == b {return a}
 
     // logic
     if a > b {
+        // early exit
         // eg; a = 10; b = 5
         if a % b == 0 {return b}
         return gcd(a-b, b)
     } else {
+        // early exit
+        // eg; a = 5; b = 10
         if b % a == 0 {return a}
         return gcd(a, b-a)
     }
