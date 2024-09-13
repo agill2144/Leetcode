1fec91e2850111491d9076aecb69ee6faea38fa6
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-12 22:06:23 -0700

    Time: 65 ms (81.91%) | Memory: 7 MB (63.83%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.go
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.go
@@ -1,15 +1,16 @@
 func subarraysWithKDistinct(nums []int, k int) int {
-    if k == 0 {return 0}
-    return atMostK(nums, k) - atMostK(nums, k-1)    
+    if k <= 0 {return 0}
+    return atMostK(nums, k) - atMostK(nums, k-1)
 }
 
 func atMostK(nums []int, k int) int {
-    count := 0
-    left := 0
+    n := len(nums)
     freq := map[int]int{}
-    for i := 0; i < len(nums); i++ {
+    left := 0
+    count := 0
+    for i := 0; i < n; i++ {
         freq[nums[i]]++
-        for len(freq) > k {
+        for len(freq) > k && left <= i {
             freq[nums[left]]--
             if freq[nums[left]] == 0 {delete(freq, nums[left])}
             left++
