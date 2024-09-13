75d504e861957087f1af42c8ab619b5dcd4eafa3
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-12 22:33:49 -0700

    Time: 56 ms (44.26%) | Memory: 4.7 MB (37.70%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.go
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.go
@@ -1,6 +1,6 @@
 func lengthOfLongestSubstringTwoDistinct(s string) int {
-    freq := map[byte]int{}
     left := 0
+    freq := map[byte]int{}
     maxWin := 0
     for i := 0; i < len(s); i++ {
         freq[s[i]]++
@@ -8,24 +8,9 @@ func lengthOfLongestSubstringTwoDistinct(s string) int {
             maxWin = max(maxWin, i-left+1)
         } else {
             freq[s[left]]--
-            if freq[s[left]] == 0 {delete(freq, s[left])}
+            if freq[s[left]] == 0{delete(freq, s[left])}
             left++
         }
     }
     return maxWin
 }
-// func lengthOfLongestSubstringTwoDistinct(s string) int {
-//     freq := map[byte]int{}
-//     left := 0
-//     maxWin := 0
-//     for i := 0; i < len(s); i++ {
-//         freq[s[i]]++
-//         for len(freq) > 2 {
-//             freq[s[left]]--
-//             if freq[s[left]] == 0 {delete(freq, s[left])}
-//             left++
-//         }
-//         maxWin = max(maxWin, i-left+1)
-//     }
-//     return maxWin
-// }
