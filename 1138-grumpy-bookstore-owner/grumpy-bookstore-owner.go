4ccc92c8e00e5c7bf5362d494c4e423e075674ac
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-12 21:31:39 -0700

    Time: 26 ms (53.85%) | Memory: 7 MB (30.77%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.go
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.go
@@ -1,27 +1,26 @@
-/*
-    1. we already have a set of satisfied customers when store owner is not grumpy - therefore count them first
-    2. next, we care about finding a window of size $minutes where we have max customers that are unsatisfied
-        - i.e customer count where store owner is grumpy in a $minute size window
-        - because this window can be satisfied.
-        - if there many such windows, we care about window that gives us the most amount of customers
-            when the store owner was grumpy
-*/
 func maxSatisfied(customers []int, grumpy []int, minutes int) int {
-    satisfied := 0
-    for i := 0; i < len(customers); i++ {
-        if grumpy[i] == 0 {satisfied += customers[i]}
+
+    n := len(customers)
+    total := 0
+    for i := 0; i < n; i++ {
+        if grumpy[i] == 0 {total += customers[i]}
     }
 
     maxCount := 0
     left := 0
-    count := 0 // count of customers when store owner is grumpy
-    for i := 0; i < len(customers); i++ {
-        if grumpy[i] ==  1 {count+=customers[i]}
+    count := 0
+    for i := 0; i < n; i++ {
+        if grumpy[i] == 1 {
+            count += customers[i]
+        }
         if i-left+1 == minutes {
             maxCount = max(maxCount, count)
-            if grumpy[left] == 1 {count -= customers[left]}
+            if grumpy[left] == 1 {
+                count-=customers[left]
+            }
             left++
         }
     }
-    return maxCount + satisfied
+    return total + maxCount
+
 }
