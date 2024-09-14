6808a9136c35a0bad839a8d1f20fc67b8d57f0c5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-14 13:13:52 -0700

    Time: 22 ms (81.78%) | Memory: 8.1 MB (24.12%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.go
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.go
@@ -1,15 +1,17 @@
 func minSubArrayLen(target int, nums []int) int {
-    rSum := 0
     left := 0
-    minWin := len(nums)+1
+    sum := 0
+    minSize := len(nums)+1
+
     for i := 0; i < len(nums); i++ {
-        rSum += nums[i]
-        for rSum >= target && left <= i {
-            minWin = min(minWin, i-left+1)
-            rSum -= nums[left]
+        sum += nums[i]
+        for sum >= target {
+            minSize = min(minSize, i-left+1)
+            sum -= nums[left]
             left++
         }
     }
-    if minWin == len(nums)+1 {return 0}
-    return minWin
+
+    if minSize == len(nums)+1 {return 0}
+    return minSize
 }
