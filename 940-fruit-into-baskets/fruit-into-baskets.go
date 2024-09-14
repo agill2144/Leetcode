41678267f2d68566c5d91c46bb20e641431743ef
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-14 10:58:47 -0700

    Time: 99 ms (47.83%) | Memory: 7.4 MB (80.19%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.go
+++ 940-fruit-into-baskets/fruit-into-baskets.go
@@ -1,28 +1,16 @@
 func totalFruit(fruits []int) int {
-    idx := map[int]int{}
-    left := 0
+    freq := map[int]int{}
     maxWin := 0
+    left := 0
     for i := 0; i < len(fruits); i++ {
-        idx[fruits[i]] = i
-        if len(idx) > 2 {
-            // invalid window
-            // to make it valid, escape the earliest idx to keep window size large
-            minIdx := len(fruits)+1
-            minIdxFruit := -1
-            for k, v := range idx {
-                if v < minIdx {
-                    minIdx = v
-                    minIdxFruit = k
-                }
-            }
-            // make sure left ptr has escaped the minIdx to make window valid
-            left = minIdx+1
-            // this fruit has left our window state
-            // therefore can be removed
-            delete(idx, minIdxFruit)
+        freq[fruits[i]]++
+        if len(freq) <= 2 {
+            maxWin = max(maxWin, i-left+1)
+        } else {
+            freq[fruits[left]]--
+            if freq[fruits[left]] == 0 {delete(freq, fruits[left])}
+            left++
         }
-        // now valid
-        maxWin = max(maxWin, i-left+1)
     }
     return maxWin
 }
