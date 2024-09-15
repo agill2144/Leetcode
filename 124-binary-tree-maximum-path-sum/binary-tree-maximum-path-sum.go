03d331933901bf4fd186cae19db4fc0d05dcc89a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-15 12:57:56 -0700

    Time: 13 ms (64.47%) | Memory: 8.5 MB (31.98%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
@@ -6,32 +6,23 @@
  *     Right *TreeNode
  * }
  */
+
+// bottom up dfs
 func maxPathSum(root *TreeNode) int {
+    if root == nil {return 0}
     maxSum := math.MinInt64
     var dfs func(r *TreeNode) int
     dfs = func(r *TreeNode) int {
         // base
         if r == nil {return 0}
-
         // logic
         left := dfs(r.Left)
         right := dfs(r.Right)
-        // 4 possible sums
-        //1. curr node + leftSum + rightSum
-        leftRightAndCurr := left+right+r.Val
-        //2. curr node + leftSum
-        leftAndCurr := left+r.Val
-        //3. curr node + rightSum
-        rightAndCurr := right+r.Val
-        // 4. curr node on its own
-        // take the max sum out of the above 4 possibilites
-        maxAtThisNode := max(leftRightAndCurr, max(leftAndCurr, max(rightAndCurr, r.Val)))
-        // save the maxSum 
-        maxSum = max(maxSum, maxAtThisNode)
-        // return a valid path back to parent, 
-        // a valid path going back to parent will never contain all nodes ( curr+left+right )
-        // it will either be curr node || curr+left || curr+right
-        return max(r.Val, max(leftAndCurr,rightAndCurr))
+        total := left+right+r.Val
+        leftSum := left + r.Val
+        rightSum := right + r.Val
+        maxSum = max(maxSum, max(total, max(leftSum, max(rightSum, r.Val))))
+        return max(leftSum, max(rightSum, r.Val))
     }
     dfs(root)
     return maxSum
