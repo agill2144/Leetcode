bef9993ca629b90aeebd1e9a09fa5ac44feb8876
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-14 20:45:13 -0700

    Time: 3 ms (23.94%) | Memory: 2.8 MB (7.75%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.go
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.go
@@ -6,50 +6,34 @@
  *     Right *TreeNode
  * }
  */
- /*
-    why we cant use dfs while maintain nodes in a col ( i.e width ) ?
-    - draw it out with 4 levels of tree and see the problem
-    - the order matters, top nodes in a col should be placed first
-    - with pre-order dfs, top nodes will not always be placed first
-        - because pre-order dfs will go depth first in a path
-        - reaching the bottom node of a width
-        - thereby placing a leaf node first 
-    - therefore inorder to respect top nodes to be placed first, and then bottom nodes
-    - this is why we need bfs
-    - level order, top nodes are processed first and then bottom nodes
-    - to group together nodes per col, we can save them in a map { $colNum: [nodes..] }
-     
- */
 func verticalOrder(root *TreeNode) [][]int {
-	if root == nil {
-		return nil
-	}
-	type qNode struct {
-		node  *TreeNode
-		width int
-	}
-	q := []*qNode{{root, 0}}
-	minW := math.MaxInt64
-	maxW := math.MinInt64
-	widthToNodes := map[int][]int{}
-	for len(q) != 0 {
-		dq := q[0]
-		q = q[1:]
-		cn := dq.node
-		cw := dq.width
-		widthToNodes[cw] = append(widthToNodes[cw], cn.Val)
-		minW = min(minW, cw)
-		maxW = max(maxW, cw)
-		if cn.Left != nil {
-			q = append(q, &qNode{cn.Left, cw - 1})
-		}
-		if cn.Right != nil {
-			q = append(q, &qNode{cn.Right, cw + 1})
-		}
-	}
-	out := [][]int{}
-	for i := minW; i <= maxW; i++ {
-		out = append(out, widthToNodes[i])
-	}
-	return out
+    if root == nil {return nil}
+    widthToNodes := map[int][]int{}
+    type qNode struct {
+        node *TreeNode
+        width int
+    }
+    minWidth := math.MaxInt64
+    maxWidth := math.MinInt64
+    q := []*qNode{&qNode{root,0}}
+    for len(q) != 0 {
+        dq := q[0]
+        q = q[1:]
+        cn := dq.node
+        cw := dq.width
+        minWidth = min(minWidth, cw)
+        maxWidth = max(maxWidth, cw)
+        widthToNodes[cw] = append(widthToNodes[cw], cn.Val)
+        if cn.Left != nil {
+            q = append(q, &qNode{cn.Left,cw-1})
+        }
+        if cn.Right != nil {
+            q = append(q, &qNode{cn.Right,cw+1})
+        }
+    }
+    out := [][]int{}
+    for i := minWidth; i <= maxWidth; i++ {
+        out = append(out, widthToNodes[i])
+    }
+    return out
 }
