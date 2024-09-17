d07eff1cd01bc18bc6158093c60f51bb53abd9ef
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-16 21:24:09 -0700

    Time: 0 ms (100.00%) | Memory: 2.8 MB (97.40%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.go
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.go
@@ -12,11 +12,23 @@ func findKthPositive(arr []int, k int) int {
             right = mid-1
         }
     }
+
+    // from right ptr, we want to figure out how many are missing on left of right
+    // if right ptr went negative, it means num of missing on left of mid >= k
+    // hence right ptr moved to left ( take binary search to left hand side )
+    // however its possible right ptr goes negative
+    // it means number of missing elements on left of right val should 0
+    // since numbers start from 1, we can set right val to 1 ( if right ptr goes negative )
+    // which will result into "count missing on left of right ptr = 0"
     rightVal := 1
-    if right >= 0 {
-        rightVal = arr[right]        
+    if right >=  0 {
+        rightVal = arr[right]
     }
-    diff := (rightVal-offSet)-right
-    return rightVal + (k-diff)
+    missingOnLeft := (rightVal-1) - right
+    // we are missing $missingOnLeft
+    // remove those from k
+    k -= missingOnLeft
+    // now walk forward from right val 
+    return rightVal + k
     
 }
