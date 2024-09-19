652397673edd9439adb0cf9d3258119812a732b5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-18 20:19:39 -0700

    Time: 107 ms (5.94%) | Memory: 9.9 MB (72.88%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.go
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.go
@@ -1,45 +1,35 @@
 func restoreMatrix(rowSum []int, colSum []int) [][]int {
     m := len(rowSum)
     n := len(colSum)
-    matrix := make([][]int, m)
     currColSum := 0
+    mat := make([][]int, m)
     for i := 0; i < m; i++ {
-        matrix[i] = make([]int, n)
-        matrix[i][0] = rowSum[i]
+        mat[i] = make([]int, n)
+        mat[i][0] = rowSum[i]
         currColSum += rowSum[i]
     }
-    r := 0
     c := 0
     for c < n-1 {
-        desiredColSum := colSum[c]
-        // if this is what we are removing from curr col
-        // and moving these value to next col ( next col is all 0 )
-        // this also means, that the next col sum will be what we will remove from this col        
-        toRemove := abs(currColSum-desiredColSum)
-        // therefore nextCol sum == the sum we will remove from current col
-        nextColSum := abs(currColSum-colSum[c])
-
-        // go thru the current col, and start moving vals to the right
+        r := 0
+        desiredSum := colSum[c]
+        toRemove := abs(currColSum-desiredSum)
+        nextColSum := toRemove
         for r < m {
-            if matrix[r][c] <= toRemove {
-                // we can remove all
-                toRemove -= matrix[r][c]
-                matrix[r][c+1] = matrix[r][c]
-                matrix[r][c] = 0
+            if mat[r][c] <= toRemove {
+                mat[r][c+1] = mat[r][c]
+                toRemove -= mat[r][c]
+                mat[r][c] = 0
             } else {
-                // we dont need all
-                // eg: matrix val = 15, to remove = 2
-                matrix[r][c] -= toRemove
-                matrix[r][c+1] = toRemove
+                mat[r][c+1] = toRemove
+                mat[r][c] -= toRemove
                 toRemove = 0
             }
             r++
         }
         c++
-        r = 0
         currColSum = nextColSum
     }
-    return matrix
+    return mat
 }
 
 func abs(x int) int {
