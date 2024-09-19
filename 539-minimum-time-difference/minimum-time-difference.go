dbefdc3e2c1401f47752a0c8e2fd66b65bfd030a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-18 20:38:10 -0700

    Time: 4 ms (78.21%) | Memory: 6.3 MB (12.51%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.go
+++ 539-minimum-time-difference/minimum-time-difference.go
@@ -1,3 +1,7 @@
+// convert to minutes
+// sort
+// find smallest minutes differnce
+
 func findMinDifference(timePoints []string) int {
     mins := []int{}
     for i := 0; i < len(timePoints); i++ {
@@ -7,6 +11,15 @@ func findMinDifference(timePoints []string) int {
         mins = append(mins, (hh*60)+mm)
     }
     sort.Ints(mins)
+    // smallest diff ( adj numbers or 0 and n-1 element)
+    // since time is circule (i.e after 23:59 -> 00:00 )
+    // the 0th minutes needs to be relative to the n-1th minutes element
+    // 00:00 -> 0
+    // 23:50 -> 1439
+    // if we did 1439-0 = 1439
+    // but really ... 00:00 to minutes should be 1400
+    // therefore we need to make sure that 0th time is after midnight
+    // therefore minutes[0] + 24hr*60min 
     minDiff := (mins[0]+(24*60)) - mins[len(mins)-1]
     for i := 1; i < len(mins); i++ {
         minDiff = min(minDiff, mins[i]-mins[i-1])
