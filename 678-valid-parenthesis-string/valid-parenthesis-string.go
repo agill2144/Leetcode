11481415058bc248f721254b53bb4c57311083f5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-18 21:05:00 -0700

    Time: 1 ms (75.00%) | Memory: 2.1 MB (100.00%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.go
+++ 678-valid-parenthesis-string/valid-parenthesis-string.go
@@ -1,34 +1,32 @@
 func checkValidString(s string) bool {
     asteriks := []int{}
-    open := []int{}
+    opens := []int{}
     for i := 0; i < len(s); i++ {
-        if s[i] == '*' {
+        if s[i] == '(' {
+            opens = append(opens,i)
+        } else if s[i] == '*' {
             asteriks = append(asteriks, i)
-        } else if s[i] == '(' {
-            open = append(open, i)
         } else {
-            if len(open) > 0 {
-                open = open[:len(open)-1]
-            } else if len(asteriks) > 0 {
-                // use asteriks to place a missing open param
+            if len(opens) > 0 {
+                opens = opens[:len(opens)-1]
+            }else if len(asteriks) > 0{
+                // use previously seen asterik as a open paran
                 asteriks = asteriks[:len(asteriks)-1]
             } else {
                 return false
+
             }
         }
     }
-
-    // if we still have open parans that did not close
-    // we need asteriks to appear after open paran idx
-    for len(open) != 0 && len(asteriks) != 0 {
-        if asteriks[len(asteriks)-1] > open[len(open)-1] {
-            // asterik appears after open paran
-            // therefore this asterik can be used as a closing paran
+    for len(opens) != 0 && len(asteriks) != 0 {
+        // we only have opens left, therefore we need to use asteriks as closing
+        // therefore asteriks SHOULD HAVE CAME AFTER open paran
+        if opens[len(opens)-1] < asteriks[len(asteriks)-1] {
             asteriks = asteriks[:len(asteriks)-1]
-            open = open[:len(open)-1]
-        } else {
-            return false
+            opens = opens[:len(opens)-1]
+            continue
         }
+        break
     }
-    return len(open) == 0
+    return len(opens) == 0 
 }
