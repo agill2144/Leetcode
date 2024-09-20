c98c21842dd67c3cdf76ab48c149cd15520ce03b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-19 20:23:34 -0700

    Time: 2 ms (72.41%) | Memory: 2.9 MB (71.26%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.go
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.go
@@ -6,52 +6,44 @@
  *     Right *TreeNode
  * }
  */
+// nodes at the same level AND AT THE SAME COL/WIDTH need to be sorted
 func verticalTraversal(root *TreeNode) [][]int {
     if root == nil {return nil}
-    widthToNodes := map[int][]int{}
+    colToNodes := map[int][]int{}
+    minCol := math.MaxInt64
+    maxCol := math.MinInt64
     type qNode struct {
         node *TreeNode
-        width int
+        col int // width
     }
-    minWidth := math.MaxInt64
-    maxWidth := math.MinInt64
-    level := 0
     q := []*qNode{&qNode{root,0}}
     for len(q) != 0 {
+        levelColToNodes := map[int][]int{}
         qSize := len(q)
-        tmp := map[int][]int{} // width to nodes in this $level
         for qSize != 0 {
             dq := q[0]
             q = q[1:]
             cn := dq.node
-            cw := dq.width
-            tmp[cw] = append(tmp[cw], cn.Val)
+            cc := dq.col
+            levelColToNodes[cc] = append(levelColToNodes[cc], cn.Val)
             if cn.Left != nil {
-                q = append(q, &qNode{cn.Left, cw-1})
+                q = append(q, &qNode{cn.Left, cc-1})
             }
             if cn.Right != nil {
-                q = append(q, &qNode{cn.Right, cw+1})
+                q = append(q, &qNode{cn.Right, cc+1})
             }
-            minWidth = min(minWidth, cw)
-            maxWidth = max(maxWidth, cw)
+            minCol=min(minCol, cc)
+            maxCol=max(maxCol, cc)            
             qSize--
         }
-        // nodes on a specific level at the same width, need to be sorted by their values
-        // we have level specific nodes in the tmp map
-        // we need to sort if nodes are on the same width
-        // nodes in tmp map are stored by each node's width
-        // therefore we can loop over the tmp map ( which is level specific withToNodes map )
-        // and sort the list of nodes
-        for w, items := range tmp {
-            sort.Ints(items)
-            // then we can push the sorted width of nodes from this level to global widthToNodes map
-            widthToNodes[w] = append(widthToNodes[w], items...)
+        for col, nodes := range levelColToNodes {
+            sort.Ints(nodes)
+            colToNodes[col] = append(colToNodes[col], nodes...)
         }
-        level++
     }
     out := [][]int{}
-    for i := minWidth; i <= maxWidth; i++ {
-        out = append(out, widthToNodes[i])
+    for i := minCol; i <= maxCol; i++ {
+        out = append(out, colToNodes[i])
     }
     return out
 }
