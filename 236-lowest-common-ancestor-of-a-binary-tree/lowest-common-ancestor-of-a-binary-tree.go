93008fc1b1728f7b9dd0692cbf395964322a5e22
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-19 21:40:10 -0700

    Time: 10 ms (53.18%) | Memory: 7.4 MB (53.67%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    var dfs func(r *TreeNode) *TreeNode
+    dfs = func(r *TreeNode) *TreeNode {
+        // base
+        if r == nil {return nil}
+
+        // logic
+        if r == p || r == q {return r}
+        left := dfs(r.Left)
+        right := dfs(r.Right)
+        if left != nil && right != nil {return r}
+        if left != nil {return left}
+        return right
+    }
+    return dfs(root)
+}
