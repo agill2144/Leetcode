05bb01a8196887f26dd046db22a98999451a67c9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-19 19:54:16 -0700

    Time: 2 ms (50.10%) | Memory: 2.7 MB (69.11%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.go
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.go
@@ -6,43 +6,40 @@
  *     Right *TreeNode
  * }
  */
-
-// level order using bfs with pre-processing of a level
-// time = o(2n) = o(n)
-// space = o(2n) (queue & levelNodes ) = o(n) 
 func zigzagLevelOrder(root *TreeNode) [][]int {
     if root == nil {return nil}
-    out := [][]int{}
-    q := []*TreeNode{root}
-    level := 1
-    for len(q) != 0 {
-        // pre-process and get the nodes in the order we care about
-        // based on the current level
-        // if level is odd; we want left -> right
-        // if level is even; we want right -> left
-        qSize := len(q)
-        dir := 1
-        ptr := 0
-        if level % 2 == 0 {
-            dir = -1
-            ptr = qSize-1
+    st1 := []*TreeNode{root} // left->right
+    st2 := []*TreeNode{} // right->left
+    out := [][]int{}    
+    for len(st1) != 0 || len(st2) != 0 {
+        level := []int{}
+        for len(st1) != 0 {
+            top := st1[len(st1)-1]
+            st1 = st1[:len(st1)-1]
+            level = append(level, top.Val)
+            if top.Left != nil {
+                st2 = append(st2, top.Left)
+            }
+            if top.Right != nil {
+                st2 = append(st2, top.Right)
+            }
         }
-        levelNodes := []int{}
-        for qSize != 0 {
-            levelNodes = append(levelNodes, q[ptr].Val)
-            ptr += dir
-            qSize--
+        out = append(out, level)
+        level = []int{}
+        for len(st2) != 0 {
+            top := st2[len(st2)-1]
+            st2 = st2[:len(st2)-1]
+            level = append(level, top.Val)
+            if top.Right != nil {
+                st1 = append(st1, top.Right)
+            }
+            if top.Left != nil {
+                st1 = append(st1, top.Left)
+            }
         }
-        out = append(out, levelNodes)
-        qSize = len(q)
-        for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            if dq.Left != nil {q = append(q, dq.Left)}
-            if dq.Right != nil {q = append(q, dq.Right)}
-            qSize--
+        if len(level) > 0 {
+            out = append(out, level)
         }
-        level++
     }
     return out
 }
