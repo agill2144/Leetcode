3d277f43ab1408c7775fea071b4681a962287030
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-21 11:40:44 -0700

    Time: 15 ms (51.37%) | Memory: 8.3 MB (44.67%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
@@ -6,23 +6,19 @@
  *     Right *TreeNode
  * }
  */
-
-// bottom up dfs
 func maxPathSum(root *TreeNode) int {
-    if root == nil {return 0}
     maxSum := math.MinInt64
     var dfs func(r *TreeNode) int
     dfs = func(r *TreeNode) int {
         // base
         if r == nil {return 0}
+
         // logic
         left := dfs(r.Left)
         right := dfs(r.Right)
-        total := left+right+r.Val
-        leftSum := left + r.Val
-        rightSum := right + r.Val
-        maxSum = max(maxSum, max(total, max(leftSum, max(rightSum, r.Val))))
-        return max(leftSum, max(rightSum, r.Val))
+        total := left + right + r.Val
+        maxSum = max(maxSum, max(r.Val+left, max(r.Val+right, max(r.Val, total))))
+        return max(r.Val, max(r.Val+left, r.Val+right))
     }
     dfs(root)
     return maxSum
