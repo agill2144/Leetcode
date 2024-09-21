e281c4a02e5fd3187d7ce55e3769acd195a948c0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-21 13:26:59 -0700

    Time: 14 ms (62.59%) | Memory: 7.1 MB (95.28%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.go
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.go
@@ -6,19 +6,56 @@
  *     Right *TreeNode
  * }
  */
-// brute force
 func countNodes(root *TreeNode) int {
+    var getH func(r *TreeNode) (int, int)
+    getH = func(r *TreeNode) (int,int) {
+        l := r
+        lh := 1
+        for l.Left != nil {
+            l = l.Left
+            lh++
+        }
+        rh := 1
+        for r.Right != nil {
+            r = r.Right
+            rh++
+        }
+        return lh, rh
+    }
     count := 0
     var dfs func(r *TreeNode)
-    dfs = func(r *TreeNode){
+    dfs = func(r *TreeNode) {
         // base
-        if r == nil {return}
+        if r == nil {
+            return
+        }
 
-        //logic
-        count++
-        dfs(r.Left)
-        dfs(r.Right)
+        // logic
+        lh, rh := getH(r)
+        if lh == rh {
+            count += ( int(math.Pow(2.0, float64(lh))) - 1)
+        } else {
+            count++
+            dfs(r.Left)
+            dfs(r.Right)
+        }
     }
     dfs(root)
     return count
 }
+// brute force
+// func countNodes(root *TreeNode) int {
+//     count := 0
+//     var dfs func(r *TreeNode)
+//     dfs = func(r *TreeNode){
+//         // base
+//         if r == nil {return}
+
+//         //logic
+//         count++
+//         dfs(r.Left)
+//         dfs(r.Right)
+//     }
+//     dfs(root)
+//     return count
+// }
