15c05a1aefea72a4274daa02cbfc41aa101d0783
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-22 16:08:42 -0700

    Time: 5 ms (51.85%) | Memory: 4.7 MB (33.33%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.go
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.go
@@ -7,37 +7,28 @@
  * }
  */
 func widthOfBinaryTree(root *TreeNode) int {
-    if root == nil {return 0}
     type qNode struct {
         node *TreeNode
         idx int
     }
-    maxWidth := 1
-    q := []*qNode{&qNode{root, 1}}
+    q := []*qNode{&qNode{root,1}}
+    maxW := -1
     for len(q) != 0 {
         qSize := len(q)
-
-        // pre-level processing
-        // we have a level
-        // find the max width on this level
-        firstIdx := q[0].idx
-        lastIdx := q[qSize-1].idx
-        maxWidth = max(maxWidth, lastIdx-firstIdx+1)
-
+        start, end := q[0].idx, q[qSize-1].idx
+        maxW = max(maxW, end-start+1)
         for qSize != 0 {
             dq := q[0]
             q = q[1:]
             cn := dq.node
-            ci := dq.idx
             if cn.Left != nil {
-                q = append(q, &qNode{cn.Left, 2*ci})
+                q = append(q, &qNode{cn.Left, 2*dq.idx})
             }
             if cn.Right != nil {
-                q = append(q, &qNode{cn.Right, (2*ci)+1})
+                q = append(q, &qNode{cn.Right, 2*dq.idx+1})
             }
             qSize--
         }
-
     }
-    return maxWidth
+    return maxW
 }
