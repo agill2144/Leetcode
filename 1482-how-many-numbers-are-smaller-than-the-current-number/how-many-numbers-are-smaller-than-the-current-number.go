b1f91015dec1076b012ebba0256c273c678c0751
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-22 00:18:13 -0700

    Time: 4 ms (71.97%) | Memory: 3.3 MB (32.48%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.go
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.go
@@ -1,34 +1,25 @@
 func smallerNumbersThanCurrent(nums []int) []int {
     start := math.MaxInt64
     end := math.MinInt64
-    n := len(nums)
-    for i := 0; i < n ; i++ {
+    for i := 0; i < len(nums); i++ {
         start = min(start, nums[i])
         end = max(end, nums[i])
     }
-    
     bucket := make([]int, end+1)
-    for i := 0; i < n ; i++ {
+    for i := 0; i < len(nums); i++ {
         bucket[nums[i]]++
     }
-    
-    countMap := map[int]int{}
-    prev := -1
-    out := make([]int, n)
-    for i := 0; i < len(bucket); i++ {
-        if bucket[i] == 0 { continue }
-        prevCountInCountMap := 0
-        prevCountInBucket := 0
-        if prev != -1 {
-            prevCountInCountMap = countMap[prev]
-            prevCountInBucket = bucket[prev]
-        }
-        countMap[i] = prevCountInBucket + prevCountInCountMap
-        prev = i
+    prefix := make([]int, end+1)
+    for i := start; i < len(bucket); i++ {
+        prevCount := 0
+        prevPrefixCount := 0
+        if i-1 >= 0 {prevCount = bucket[i-1]; prevPrefixCount=prefix[i-1]}
+        prefix[i]= prevCount+prevPrefixCount
+
     }
-    
-    for i := 0; i < n; i++ {
-        out[i] = countMap[nums[i]]
+    out := make([]int, len(nums))
+    for i := 0; i < len(nums); i++ {
+        out[i] = prefix[nums[i]]
     }
     return out
 }
