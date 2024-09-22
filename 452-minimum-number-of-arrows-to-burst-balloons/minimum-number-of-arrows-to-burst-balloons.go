17c1ad2bfacd88bb035e807f6d8e2d8eea175b2d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-22 14:41:31 -0700

    Time: 196 ms (57.94%) | Memory: 14.4 MB (85.59%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.go
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.go
@@ -1,15 +1,16 @@
 func findMinArrowShots(points [][]int) int {
     sort.Slice(points, func(i, j int)bool{
-        return points[i][1] < points[j][1]
+        return points[i][0] < points[j][0]
     })
     count := 1
     lastEnd := points[0][1]
     for i := 0; i < len(points); i++ {
         start, end := points[i][0], points[i][1]
+        // is it overlapping?
         if start <= lastEnd {
-            // continue with same arrow
-            continue
+            lastEnd = min(lastEnd, end)
         } else {
+            // no overlap, 
             // need a new arrow
             count++
             lastEnd = end
