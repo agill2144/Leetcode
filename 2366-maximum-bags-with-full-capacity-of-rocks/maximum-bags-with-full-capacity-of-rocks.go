e9e0ef45e4fdd3afc1143383112c99069d522fdf
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-22 00:32:39 -0700

    Time: 170 ms (8.70%) | Memory: 13.9 MB (8.70%) - LeetSync

--- 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.go
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.go
@@ -1,22 +1,13 @@
 func maximumBags(capacity []int, rocks []int, additionalRocks int) int {
-    n := len(capacity)
-    diffs := make([]int, n)
-    for i := 0; i < n; i++ {
-        diffs[i] = capacity[i] - rocks[i]
+    diffs := []int{}
+    for i := 0; i < len(capacity); i++ {
+        diffs = append(diffs, capacity[i]-rocks[i])
     }
     sort.Ints(diffs)
-    fulls := 0
+    count := 0
     for i := 0; i < len(diffs); i++ {
-        if diffs[i] == 0 {
-            fulls++
-            continue
-        }
-        needed := diffs[i]
-        if additionalRocks >= needed {
-            additionalRocks -= needed
-            fulls++
-        }
-        if additionalRocks == 0 {break}
+        if diffs[i] == 0 {count++; continue}
+        if additionalRocks >= diffs[i] {count++; additionalRocks-= diffs[i]}
     }
-    return fulls
+    return count
 }
