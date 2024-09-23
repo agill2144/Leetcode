7cce53d57dd8269ca9e9bbc3bc4073a97e05a2c2
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-22 18:26:43 -0700

    Time: 13 ms (65.40%) | Memory: 6.2 MB (71.80%) - LeetSync

--- 135-candy/candy.go
+++ 135-candy/candy.go
@@ -1,19 +1,32 @@
 func candy(ratings []int) int {
-    candies := make([]int, len(ratings))
-    for i := 0; i < len(ratings); i++ {
-        candies[i] = 1
-        if i > 0 && ratings[i] > ratings[i-1] {
-            candies[i] = candies[i-1]+1
-        }
+    n := len(ratings)
+    total := n // start by givig 1 to each child
+    i := 1
+    for i < n {
+        for i < n && ratings[i] == ratings[i-1] {i++; continue}
+        peak := 0
+        for i < n && ratings[i] > ratings[i-1] {peak++; total += peak; i++}
+        dip := 0
+        for i < n && ratings[i] < ratings[i-1] {dip++; total += dip; i++}
+        total -= min(peak, dip)
     }
-    total := candies[len(candies)-1]
-    for i := len(ratings)-2; i >= 0; i-- {
-        if ratings[i] > ratings[i+1] && candies[i] <= candies[i+1] {
-            candies[i] = candies[i+1]+1
-        }
-        total += candies[i]
-    }
-
-
     return total
 }
+
+// func candy(ratings []int) int {
+//     candies := make([]int, len(ratings))
+//     for i := 0; i < len(ratings); i++ {
+//         candies[i] = 1
+//         if i > 0 && ratings[i] > ratings[i-1] {
+//             candies[i] = candies[i-1]+1
+//         }
+//     }
+//     total := candies[len(candies)-1]
+//     for i := len(ratings)-2; i >= 0; i-- {
+//         if ratings[i] > ratings[i+1] && candies[i] <= candies[i+1] {
+//             candies[i] = candies[i+1]+1
+//         }
+//         total += candies[i]
+//     }
+//     return total
+// }
