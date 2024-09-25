a63a346827e3e1148691619e0bbdb96629c70e0b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-24 17:25:11 -0700

    Time: 17 ms (34.24%) | Memory: 7.5 MB (42.37%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.go
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.go
@@ -7,18 +7,63 @@
  * }
  */
 
-// brute force, count using any traversal
+// use height property of a complete full binary tree
+// num of nodes in a complete full binary tree = 2^h - 1
+// h = height of this tree
+// however this 2^h-1 is only true when the tree is full and complete
+// i.e left height == right height
+// if its unbalanced, recurse and check left and right subtree + add current node
 func countNodes(root *TreeNode) int {
+    if root == nil {return 0}
+    getH := func(r *TreeNode) (int, int) { 
+        l := r
+        lh := 1
+        for l.Left != nil {
+            l = l.Left
+            lh++
+        }
+        rh := 1
+        for r.Right != nil {
+            r = r.Right
+            rh++
+        }
+        return lh, rh
+    }
     count := 0
     var dfs func(r *TreeNode)
     dfs = func(r *TreeNode) {
         // base
         if r == nil {return}
+
         // logic
-        count++
-        dfs(r.Left)
-        dfs(r.Right)
+        lh, rh := getH(r)
+        if lh == rh {
+            count += (int(math.Pow(2.0, float64(lh))) - 1)
+        } else {
+            // count current node
+            count++
+            // + count nodes of left subtree
+            dfs(r.Left)
+            // + count nodes of right subtree
+            dfs(r.Right)
+        }
     }
     dfs(root)
     return count
 }
+
+// brute force, count using any traversal
+// func countNodes(root *TreeNode) int {
+//     count := 0
+//     var dfs func(r *TreeNode)
+//     dfs = func(r *TreeNode) {
+//         // base
+//         if r == nil {return}
+//         // logic
+//         count++
+//         dfs(r.Left)
+//         dfs(r.Right)
+//     }
+//     dfs(root)
+//     return count
+// }
