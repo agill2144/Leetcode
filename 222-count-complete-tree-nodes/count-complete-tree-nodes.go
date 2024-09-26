0777b78f5a12225a4ec9c4aa33bc97dc23a88d1c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-25 20:28:31 -0700

    Time: 14 ms (62.50%) | Memory: 7.9 MB (13.67%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.go
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.go
@@ -6,57 +6,36 @@
  *     Right *TreeNode
  * }
  */
-
-// use height property of a complete full binary tree
-// num of nodes in a complete full binary tree = 2^h - 1
-// h = height of this tree
-// however this 2^h-1 is only true when the tree is full and complete
-// i.e left height == right height
-// if its unbalanced, recurse and check left and right subtree + add current node
+ // number of nodes in a complete full binary tree = 2^h - 1
+ // complete full = leftHeight == rightHeight
 func countNodes(root *TreeNode) int {
     if root == nil {return 0}
-    getH := func(r *TreeNode) (int, int) { 
+    getH := func(r *TreeNode) (int, int) {
+        lh := 0
         l := r
-        lh := 1
-        for l.Left != nil {
-            l = l.Left
+        for l != nil {
             lh++
+            l = l.Left
         }
-        rh := 1
-        for r.Right != nil {
-            r = r.Right
+        rh := 0
+        for r != nil {
             rh++
+            r = r.Right
         }
         return lh, rh
     }
     var dfs func(r *TreeNode) int
-    dfs = func(r *TreeNode) int{
+    dfs = func(r *TreeNode) int {
         // base
         if r == nil {return 0}
 
         // logic
         lh, rh := getH(r)
         if lh == rh {
-            return int(math.Pow(2.0, float64(lh))) - 1
+            return int(math.Pow(2.0, float64(lh)))-1
         } else {
             return 1 + dfs(r.Left) + dfs(r.Right)
         }
     }
     return dfs(root)
 }
-
-// brute force, count using any traversal
-// func countNodes(root *TreeNode) int {
-//     count := 0
-//     var dfs func(r *TreeNode)
-//     dfs = func(r *TreeNode) {
-//         // base
-//         if r == nil {return}
-//         // logic
-//         count++
-//         dfs(r.Left)
-//         dfs(r.Right)
-//     }
-//     dfs(root)
-//     return count
-// }
