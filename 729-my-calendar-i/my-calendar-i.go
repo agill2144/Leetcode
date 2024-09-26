afc9c7ff76b9404d0005f8b5f5f236a642abdb2c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-25 19:53:32 -0700

    Time: 101 ms (20.25%) | Memory: 8.2 MB (26.58%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.go
+++ 729-my-calendar-i/my-calendar-i.go
@@ -1,89 +1,26 @@
 type MyCalendar struct {
-    cal [][]int
+    times [][]int
 }
 
 
 func Constructor() MyCalendar {
-    return MyCalendar{cal: [][]int{}}
+    return MyCalendar{times: [][]int{}}
 }
 
 
-// everytime this func is invoked
-// o(logn) + o(logn) + o(n)
-// o(n) time
-// space: o(1)
 func (this *MyCalendar) Book(start int, end int) bool {
-    if len(this.cal) == 0 {
-        this.cal = append(this.cal, []int{start, end})
+    if len(this.times) == 0 {
+        this.times = append(this.times, []int{start, end})
         return true
     }
-    // o(logn)
-    prevIdx := this.searchLeft(start)
-    if prevIdx == -1 {prevIdx = 0}
-    prev := this.cal[prevIdx]
-    if start < prev[1] && end > prev[0] {return false}
-    // if end > prev[0] && start < prev[1]  {return false}
-    
-
-    // o(logn)
-    nextIdx := this.searchRight(start)
-    if nextIdx == -1 {nextIdx = 0}
-    next := this.cal[nextIdx]
-    if start < next[1] && end > next[0] {return false}
-    // if end > next[0] && start < next[1]  {return false}
-    
-    // o(n)
-    this.insert(start, end)
-    return true 
-}
-
-func (this *MyCalendar) insert(start, end int) {  
-    curr := []int{start, end}
-    this.cal = append(this.cal, curr)
-    for i := len(this.cal)-1; i > 0; i-- {
-        prev := this.cal[i-1]
-        if curr[0] < prev[0] {
-            this.cal[i], this.cal[i-1] = this.cal[i-1], this.cal[i]
-        }
+    for i := 0; i < len(this.times); i++ {
+        s, e := this.times[i][0], this.times[i][1]
+        // start = 33
+        // s = 49
+        if start < e && s < end {return false}
     }
-}
-
-func (this *MyCalendar) searchLeft(target int) int {
-    left := 0
-    right := len(this.cal)-1
-    ans := -1
-    for left <= right {
-        mid := left + (right-left)/2
-        if this.cal[mid][0] <= target {
-            if this.cal[mid][0] == target {
-                return mid
-            }
-            ans = mid
-            left = mid+1
-        } else {
-            right = mid-1
-        }
-    }
-    return ans
-}
-
-func (this *MyCalendar) searchRight(target int) int {
-    left := 0
-    right := len(this.cal)-1
-    ans := -1
-    for left <= right {
-        mid := left + (right-left)/2
-        if this.cal[mid][0] >= target {
-            if this.cal[mid][0] == target {
-                return mid
-            }
-            ans = mid
-            right = mid-1
-        } else {
-            left = mid+1
-        }
-    }
-    return ans
+    this.times = append(this.times, []int{start, end})
+    return true
 }
 
 
