2a9ada5a32edea6a3a92c8b2d6c9cba06d011d20
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-28 16:49:47 -0700

    Time: 356 ms (18.42%) | Memory: 29 MB (7.89%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.go
@@ -0,0 +1,19 @@
+func findWinners(matches [][]int) [][]int {
+    scores := map[int][]int{} // { id : [wins, loss] }
+    for i := 0; i < len(matches); i++ {
+        wPlayer := matches[i][0]
+        lPlayer := matches[i][1]
+        if _, ok := scores[wPlayer]; !ok {scores[wPlayer] = make([]int, 2)}
+        if _, ok := scores[lPlayer]; !ok {scores[lPlayer] = make([]int, 2)}
+        scores[wPlayer][0]++
+        scores[lPlayer][1]++
+    }
+    out := make([][]int, 2)
+    for player, counts := range scores {
+        wins, losses := counts[0], counts[1]
+        if losses == 1 {out[1] = append(out[1], player)}
+        if losses == 0 && wins+losses > 0 {out[0] = append(out[0], player)}
+    }
+    for _, l := range out {sort.Ints(l)}
+    return out
+}
