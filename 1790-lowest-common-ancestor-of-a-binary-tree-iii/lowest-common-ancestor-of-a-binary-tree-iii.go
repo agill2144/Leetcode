1f9d343d6c2a5a4a22091887c00c091e2af84542
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-28 13:45:59 -0700

    Time: 0 ms (100.00%) | Memory: 6.5 MB (66.42%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
@@ -8,6 +8,10 @@
  * }
  */
 
+// p = number of nodes from root to p
+// q = number of nodes from root to q
+// time = o(p) + o(q)
+// space = o(p)
 func lowestCommonAncestor(p *Node, q *Node) *Node {
     pPathNodes := map[*Node]bool{}
     for p != nil {
