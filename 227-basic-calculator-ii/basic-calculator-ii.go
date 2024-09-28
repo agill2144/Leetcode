b6ebcf4f30f93e026ddb1f3fceefbaa0e879eb62
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-28 12:55:08 -0700

    Time: 7 ms (65.75%) | Memory: 8.4 MB (43.38%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.go
+++ 227-basic-calculator-ii/basic-calculator-ii.go
@@ -1,68 +1,39 @@
-// maintain last contribution to help undo math when 
-// operator being processed in * or /
 func calculate(s string) int {
-    lastContr := 0
+    st := []int{}
     curr := 0
-    total := 0
-    var operator byte = '+'
+    var lastOp byte = '+'
     for i := 0; i < len(s); i++ {
         char := s[i]
         if char >= '0' && char <= '9' {
             curr = curr * 10 + int(char-'0')
         }
-        if char == '+' || char == '-' || char == '*' || char == '/' || i == len(s)-1 {
-            if operator == '+' {
-                total += curr
-                lastContr = +curr
-            } else if operator == '-' {
-                total -= curr
-                lastContr = -curr
-            } else if operator == '*' {
-                total = total - lastContr + (lastContr*curr)
-                lastContr = lastContr*curr
-            } else if operator == '/' {
-                total = total - lastContr + (lastContr/curr)
-                lastContr = lastContr/curr
+        if i == len(s)-1 || char == '*' || char == '/' || char == '-' || char == '+' {
+            if lastOp == '*' {
+                top := 1
+                if len(st) > 0 {
+                    top = st[len(st)-1]
+                }
+                top *= curr
+                st[len(st)-1] = top
+            } else if lastOp == '/' {
+                top := 1
+                if len(st) > 0 {
+                    top = st[len(st)-1]
+                }
+                st[len(st)-1] = top/curr
+            } else if lastOp == '+' {
+                st = append(st, curr)
+            } else if lastOp == '-' {
+                st = append(st, -curr)
             }
+            lastOp = char
             curr = 0
-            operator = char
         }
+    }
+    total := 0
+    for len(st) != 0 {
+        total += st[len(st)-1]
+        st = st[:len(st)-1]
     }
     return total
 }
-// func calculate(s string) int {
-//     st := []int{}
-//     digit := 0
-//     var operator byte = '+'
-//     for i := 0; i < len(s); i++ {
-//         char := s[i]
-//         if s[i] >= '0' && s[i] <= '9' {
-//             charInt := int(char-'0')
-//             digit = digit * 10 + charInt
-//         }
-//         if char == '+' || char == '-' || char == '*' || char == '/' || i == len(s)-1 {
-//             if operator == '+' {
-//                 st = append(st, digit)
-//             } else if operator == '-' {
-//                 st = append(st, -digit)
-//             } else if operator == '*' {
-//                 top := st[len(st)-1]
-//                 res := top*digit
-//                 st[len(st)-1] = res
-//             } else if operator == '/' {
-//                 top := st[len(st)-1]
-//                 res := top/digit
-//                 st[len(st)-1] = res
-//             }
-//             digit = 0
-//             operator = char
-//         }
-//     }
-//     res := 0
-//     for len(st) != 0 {
-//         top := st[len(st)-1]
-//         st = st[:len(st)-1]
-//         res += top
-//     }
-//     return res
-// }
