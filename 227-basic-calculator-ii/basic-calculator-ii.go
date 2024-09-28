499cb1fc4f5f4e9a337abf85a8764917e4c5c1fb
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-28 13:02:47 -0700

    Time: 5 ms (80.82%) | Memory: 4.2 MB (84.02%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.go
+++ 227-basic-calculator-ii/basic-calculator-ii.go
@@ -1,39 +1,36 @@
 func calculate(s string) int {
-    st := []int{}
+    res := 0
     curr := 0
     var lastOp byte = '+'
+    prevContr := 0
     for i := 0; i < len(s); i++ {
         char := s[i]
         if char >= '0' && char <= '9' {
             curr = curr * 10 + int(char-'0')
         }
-        if i == len(s)-1 || char == '*' || char == '/' || char == '-' || char == '+' {
-            if lastOp == '*' {
-                top := 1
-                if len(st) > 0 {
-                    top = st[len(st)-1]
-                }
-                top *= curr
-                st[len(st)-1] = top
-            } else if lastOp == '/' {
-                top := 1
-                if len(st) > 0 {
-                    top = st[len(st)-1]
-                }
-                st[len(st)-1] = top/curr
-            } else if lastOp == '+' {
-                st = append(st, curr)
+        if i == len(s)-1 || char == '*' || char == '-' || char == '+' || char == '/' {
+            // we ran into a new operator
+            // process lastOp 
+            if lastOp == '+' {
+                res += curr
+                prevContr = +curr
             } else if lastOp == '-' {
-                st = append(st, -curr)
+                res -= curr
+                prevContr = -curr
+            } else if lastOp == '*' {
+                // remove prevContr + process multiplication first ( i.e; prev number * curr )
+                // and then add back to result
+                // hence res - prevContr + (prevContr + curr)
+                res = res - prevContr + (prevContr * curr)
+                prevContr = prevContr*curr
+            } else if lastOp == '/' {
+                // same as multiplication
+                res = res - prevContr + (prevContr / curr)
+                prevContr = prevContr/curr
             }
             lastOp = char
             curr = 0
         }
     }
-    total := 0
-    for len(st) != 0 {
-        total += st[len(st)-1]
-        st = st[:len(st)-1]
-    }
-    return total
+    return res
 }
