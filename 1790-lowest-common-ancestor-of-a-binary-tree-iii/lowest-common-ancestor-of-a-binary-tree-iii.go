715e7e1b6120a26206507cf4ac348d7038ee796c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-28 16:07:44 -0700

    Time: 0 ms (100.00%) | Memory: 7 MB (13.43%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
@@ -8,7 +8,30 @@
  * }
  */
 
-// no space solu
+/*
+    approach: hash hash, brain turning into a potato
+    - create a map of nodes from p to root
+        - for p != nil { add p to map, p = p.parent }
+    - now we can traverse from q to root
+        - and check if q has been in the map 
+        - if yes, we found the lowest common node thats common between p and q
+    time = o(p + q)
+    space = o(p)
+
+    approach: no space, using heights
+    - 1st find root node ( take the traversal from p or q until we reach the root node )
+    - then find heights from root to p and h ( pHeight and qHeight )
+    - bring p or q at the same level ( if pHeight > qHeight , bring p to the same height as q )
+        - vice-verse 
+    - now that our p and q ptrs are at the same level / height
+    - walk up both ptrs until they both meet
+    - once both ptrs are on the same node, thats our LCA
+    
+    time = o(p) + o(n) + o(p+q)
+    space = o(1) if we dont count recursion space 
+*/
+
+// no space solution
 func lowestCommonAncestor(p *Node, q *Node) *Node {    
     root := p
     for root.Parent != nil {root = root.Parent}
