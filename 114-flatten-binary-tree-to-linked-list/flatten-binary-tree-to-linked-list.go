f7c7762a9d0adf6cd368907fcd5a6be579a059f7
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-28 20:31:25 -0700

    Time: 0 ms (100.00%) | Memory: 3.2 MB (20.81%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.go
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.go
@@ -6,37 +6,37 @@
  *     Right *TreeNode
  * }
  */
-func flatten(root *TreeNode)  {
-    var prev *TreeNode
-    var dfs func(r *TreeNode)
-    dfs = func(r *TreeNode) {
-        // base
-        if r == nil {return}
+// func flatten(root *TreeNode)  {
+//     var prev *TreeNode
+//     var dfs func(r *TreeNode)
+//     dfs = func(r *TreeNode) {
+//         // base
+//         if r == nil {return}
 
-        // logic
-        dfs(r.Right)
-        dfs(r.Left)
-        r.Right = prev
-        r.Left = nil
-        prev = r
+//         // logic
+//         dfs(r.Right)
+//         dfs(r.Left)
+//         r.Right = prev
+//         r.Left = nil
+//         prev = r
+//     }
+//     dfs(root)
+    
+// }
+func flatten(root *TreeNode)  {
+    if root == nil {return}
+    curr := root
+    for curr != nil {
+        if curr.Left == nil {
+            curr = curr.Right
+        } else {
+            tmp := curr.Left
+            for tmp.Right != nil {tmp = tmp.Right}
+            tmp.Right = curr.Right
+            curr.Right = curr.Left
+            curr.Left = nil
+            curr = curr.Right
+        }
     }
-    dfs(root)
     
 }
-// func flatten(root *TreeNode)  {
-//     if root == nil {return}
-//     curr := root
-//     for curr != nil {
-//         if curr.Left == nil {
-//             curr = curr.Right
-//         } else {
-//             tmp := curr.Left
-//             for tmp.Right != nil {
-//                 tmp = tmp.Right
-//             }
-//             tmp.Right = curr.Right
-//             curr.Right = curr.Left
-//             curr.Left = nil
-//         }
-//     }
-// }
