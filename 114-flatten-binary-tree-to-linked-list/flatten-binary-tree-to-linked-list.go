0acd8cec45cfffd3e37c37a342ae86af77b550e8
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-28 20:23:16 -0700

    Time: 0 ms (100.00%) | Memory: 3.1 MB (73.51%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.go
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.go
@@ -7,19 +7,36 @@
  * }
  */
 func flatten(root *TreeNode)  {
-    if root == nil {return}
-    curr := root
-    for curr != nil {
-        if curr.Left == nil {
-            curr = curr.Right
-        } else {
-            tmp := curr.Left
-            for tmp.Right != nil {
-                tmp = tmp.Right
-            }
-            tmp.Right = curr.Right
-            curr.Right = curr.Left
-            curr.Left = nil
-        }
+    var prev *TreeNode
+    var dfs func(r *TreeNode)
+    dfs = func(r *TreeNode) {
+        // base
+        if r == nil {return}
+
+        // logic
+        dfs(r.Right)
+        dfs(r.Left)
+        r.Right = prev
+        r.Left = nil
+        prev = r
     }
+    dfs(root)
+    
 }
+// func flatten(root *TreeNode)  {
+//     if root == nil {return}
+//     curr := root
+//     for curr != nil {
+//         if curr.Left == nil {
+//             curr = curr.Right
+//         } else {
+//             tmp := curr.Left
+//             for tmp.Right != nil {
+//                 tmp = tmp.Right
+//             }
+//             tmp.Right = curr.Right
+//             curr.Right = curr.Left
+//             curr.Left = nil
+//         }
+//     }
+// }
