de54acbb3ed9b8be63ac7e8d87bee0086c54d377
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-28 17:00:18 -0700

    Time: 375 ms (17.54%) | Memory: 28.8 MB (8.77%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.go
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.go
@@ -1,19 +1,41 @@
 func findWinners(matches [][]int) [][]int {
-    scores := map[int][]int{} // { id : [wins, loss] }
+    // each idx == playerID
+    scores := make([][]int, 100001)
     for i := 0; i < len(matches); i++ {
         wPlayer := matches[i][0]
         lPlayer := matches[i][1]
-        if _, ok := scores[wPlayer]; !ok {scores[wPlayer] = make([]int, 2)}
-        if _, ok := scores[lPlayer]; !ok {scores[lPlayer] = make([]int, 2)}
+        if scores[wPlayer] == nil {scores[wPlayer] = make([]int, 2)}
+        if scores[lPlayer] == nil {scores[lPlayer] = make([]int, 2)}
         scores[wPlayer][0]++
         scores[lPlayer][1]++
     }
     out := make([][]int, 2)
-    for player, counts := range scores {
-        losses := counts[1]
-        if losses == 0 {out[0] = append(out[0], player)}
-        if losses == 1 {out[1] = append(out[1], player)}
+    for i := 0; i < len(scores); i++ {
+        if scores[i] != nil {
+            losses := scores[i][1]
+            if losses == 0 {out[0] = append(out[0],i)}
+            if losses == 1 {out[1] = append(out[1],i)}
+        }
     }
-    for _, l := range out {sort.Ints(l)}
     return out
+
 }
+// func findWinners(matches [][]int) [][]int {
+//     scores := map[int][]int{} // { id : [wins, loss] }
+//     for i := 0; i < len(matches); i++ {
+//         wPlayer := matches[i][0]
+//         lPlayer := matches[i][1]
+//         if _, ok := scores[wPlayer]; !ok {scores[wPlayer] = make([]int, 2)}
+//         if _, ok := scores[lPlayer]; !ok {scores[lPlayer] = make([]int, 2)}
+//         scores[wPlayer][0]++
+//         scores[lPlayer][1]++
+//     }
+//     out := make([][]int, 2)
+//     for player, counts := range scores {
+//         losses := counts[1]
+//         if losses == 0 {out[0] = append(out[0], player)}
+//         if losses == 1 {out[1] = append(out[1], player)}
+//     }
+//     for _, l := range out {sort.Ints(l)}
+//     return out
+// }
