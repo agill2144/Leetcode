90de36660b6d8a51cd28ed7e58356459c7619fc2
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-09-29 19:20:01 -0700

    Time: 3 ms (58.20%) | Memory: 3.2 MB (22.22%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.go
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.go
@@ -6,6 +6,22 @@
  *     Right *TreeNode
  * }
  */
+/*
+    using morris traversal
+    - take the right most on left side of curr ( call this tmp )
+    - tmp.Right will always connect to curr.Right
+        - therefore; tmp.Right = curr.Right
+        - but not we lost curr.Right, we dont need it
+        - because we will keep moving our curr ptr to right
+    - once tmp.Right is connected to curr.Right
+    - bring the entire curr.Left to curr.Right
+        - i.e curr.Right = curr.Left
+        - and curr.Left = nil
+    - finally, move curr to curr.Right
+
+    time = o(n)
+    space = o(1)
+*/
 func flatten(root *TreeNode)  {
     if root == nil {return}
     curr := root
