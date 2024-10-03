49c6df0db46d119fa2de6c045e505ad5e667602c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-03 11:11:08 -0700

    Time: 82 ms (70.37%) | Memory: 12.4 MB (62.96%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.go
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.go
@@ -3,14 +3,14 @@ func minSubarray(nums []int, p int) int {
     for i := 0; i < len(nums); i++ {
         total += nums[i]
     }
-    k := total % p
-    if k == 0 {return 0}
+    rem := total % p
+    if rem == 0 {return 0}
     minWin := len(nums)
     rSum := 0
     idx := map[int]int{0:-1}
     for i := 0; i < len(nums); i++ {
         rSum = (rSum + nums[i]) % p
-        diff := (rSum - k + p) % p 
+        diff := (rSum - rem + p) % p 
         if left, ok := idx[diff]; ok {
             if i-left < minWin {minWin = i-left}
         }
