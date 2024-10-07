26489ca85590894d5bae8e84c9e1ba9b9c3beac7
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-06 19:33:02 -0700

    Time: 12 ms (88.28%) | Memory: 7.4 MB (35.68%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.go
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.go
@@ -9,12 +9,11 @@
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	for root != nil {
-        if p == root || q == root || (p.Val < root.Val && q.Val > root.Val) || (p.Val > root.Val && q.Val < root.Val) {return root}
-        if p.Val < root.Val && q.Val < root.Val {
-            root = root.Left
-        } else {
-            root = root.Right
-        }
+        if (p.Val < root.Val && q.Val > root.Val) ||
+          (p.Val > root.Val && q.Val < root.Val) ||   
+         (root.Val == p.Val || root.Val == q.Val) {return root}
+        if p.Val < root.Val && q.Val < root.Val {root = root.Left}
+        if p.Val > root.Val && q.Val > root.Val {root = root.Right}
     }
     return nil
 }
