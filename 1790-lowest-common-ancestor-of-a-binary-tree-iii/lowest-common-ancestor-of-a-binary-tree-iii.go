f179de3611a21a4b77328f743f1116e77f0fd35c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-06 18:43:06 -0700

    Time: 5 ms (40.14%) | Memory: 6.5 MB (66.20%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
@@ -8,50 +8,29 @@
  * }
  */
 
-/*
-    approach: hash hash, brain turning into a potato
-    - create a map of nodes from p to root
-        - for p != nil { add p to map, p = p.parent }
-    - now we can traverse from q to root
-        - and check if q has been in the map 
-        - if yes, we found the lowest common node thats common between p and q
-    time = o(p + q)
-    space = o(p)
-
-    approach: no space, using heights
-    - 1st find root node ( take the traversal from p or q until we reach the root node )
-    - then find heights from root to p and h ( pHeight and qHeight )
-    - bring p or q at the same level ( if pHeight > qHeight , bring p to the same height as q )
-        - vice-verse 
-    - now that our p and q ptrs are at the same level / height
-    - walk up both ptrs until they both meet
-    - once both ptrs are on the same node, thats our LCA
-    
-    time = o(p) + o(n) + o(p+q)
-    space = o(1) if we dont count recursion space 
-*/
-
-// no space solution
-func lowestCommonAncestor(p *Node, q *Node) *Node {    
-    root := p
-    for root.Parent != nil {root = root.Parent}
-    pH := -1
-    qH := -1
-    var getH func(r *Node, h int)
-    getH = func(r *Node, h int) {
-        // base
-        if r == nil {return}
-        if pH != -1 && qH != -1 {return }
-
-        // logic
-        if r == p {pH = h}
-        if r == q {qH = h}
-        getH(r.Left, h+1)
-        getH(r.Right, h+1)
+func lowestCommonAncestor(p *Node, q *Node) *Node {
+    if p == nil || q == nil {return nil}
+    getH := func(t *Node) int {
+        ch := 0
+        for t != nil {
+            t = t.Parent
+            ch++
+        }
+        return ch 
     }
-    getH(root, 0)
-    for pH > qH && p != nil {p = p.Parent; pH--}
-    for qH > pH && q != nil {q = q.Parent; qH--}
-    for p != q { p = p.Parent; q = q.Parent }
+    pH := getH(p)
+    qH := getH(q)
+    for qH > pH { 
+        q = q.Parent
+        qH--
+    }
+    for pH > qH {
+        p = p.Parent
+        pH--
+    }
+    for p != q {
+        p = p.Parent
+        q = q.Parent
+    }
     return p
 }
