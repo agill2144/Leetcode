41fbf6fec4e0170ba13eebf3a331cfd989c701fc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-06 21:13:33 -0700

    Time: 3 ms (67.90%) | Memory: 5.8 MB (6.17%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.go
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.go
@@ -12,29 +12,26 @@
  // right child = 2*i+1
  // start i = 1
 func widthOfBinaryTree(root *TreeNode) int {
-    if root == nil {return 0}
-    type qNode struct {
-        idx int
-        node *TreeNode
-    }   
-    q := []*qNode{&qNode{1,root}}
-    maxW := 0
-    for len(q) != 0 {
-        qSize := len(q)
-        startIdx := q[0].idx
-        endIdx := q[qSize-1].idx
+    idx := map[int][]int{} // { $level: [minIdx, maxIdx]}
+    maxLevel := 0
+    var dfs func(r *TreeNode, currIdx int, level int)
+    dfs = func(r *TreeNode, currIdx, level int) {
+        // base
+        if r == nil {return}
+
+        // logic
+        maxLevel = max(maxLevel, level)
+        if _, ok := idx[level]; !ok {idx[level] = []int{math.MaxInt64, math.MinInt64}}
+        idx[level][0] = min(idx[level][0], currIdx)
+        idx[level][1] = max(idx[level][1], currIdx)
+        dfs(r.Left, 2*currIdx, level+1)
+        dfs(r.Right, 2*currIdx+1, level+1)
+    }
+    dfs(root, 1, 0)
+    maxW := -1
+    for i := maxLevel; i >= 0; i-- {
+        startIdx, endIdx := idx[i][0], idx[i][1]
         maxW = max(maxW, endIdx-startIdx+1)
-        for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            if dq.node.Left != nil {
-                q = append(q, &qNode{2*dq.idx, dq.node.Left})
-            }
-            if dq.node.Right != nil {
-                q = append(q, &qNode{2*dq.idx+1, dq.node.Right})
-            }
-            qSize--
-        }
     }
     return maxW
 }
