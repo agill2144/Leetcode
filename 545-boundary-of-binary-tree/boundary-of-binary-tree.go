03d61e71498b361f7e9d273a9f929d14608a0b00
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-06 19:19:44 -0700

    Time: 5 ms (62.50%) | Memory: 5.3 MB (15.63%) - LeetSync

--- 545-boundary-of-binary-tree/boundary-of-binary-tree.go
+++ 545-boundary-of-binary-tree/boundary-of-binary-tree.go
@@ -8,45 +8,43 @@
  */
 func boundaryOfBinaryTree(root *TreeNode) []int {
     if root == nil {return nil}
+    isLeaf := func(r *TreeNode) bool {
+        return r.Left == nil && r.Right == nil
+    }
     out := []int{root.Val}
-    curr := root.Left
-    for curr != nil {
-        if curr.Left == nil && curr.Right == nil {break}
-        out = append(out,curr.Val)
-        if curr.Left != nil {
-            curr = curr.Left
-        } else {
-            curr = curr.Right
+    leftView := func(r *TreeNode) {
+        for r != nil && !isLeaf(r) {
+            out = append(out, r.Val)
+            if r.Left != nil {r =r.Left} else {r = r.Right}
         }
     }
-
-    // collect leaves from left to right
+    leftView(root.Left)
+    // collect leaves
     var leaves func(r *TreeNode)
     leaves = func(r *TreeNode) {
         // base
         if r == nil {return}
+        
         // logic
-        if r.Left == nil && r.Right == nil && r != root {out = append(out, r.Val); return}
+        if isLeaf(r) && r != root {
+            out = append(out, r.Val)
+        }
         leaves(r.Left)
         leaves(r.Right)
     }
     leaves(root)
 
-    // collect right side ( make note of ptrs )
-    // because we have to reverse the right side
-    rightStartPtr := len(out)
-    curr = root.Right
-    for curr != nil {
-        // we have reached the leaf node
-        if curr.Left == nil && curr.Right == nil {break}
-        out = append(out,curr.Val)
-        if curr.Right != nil {curr = curr.Right} else {curr = curr.Left}
+    // collect right
+    right := []int{}
+    rightView := func(r *TreeNode) {
+        for r != nil && !isLeaf(r) {
+            right = append(right, r.Val)
+            if r.Right != nil {r = r.Right } else {r = r.Left}
+        }
     }
-    end := len(out)-1
-    for rightStartPtr < end {
-        out[rightStartPtr], out[end] =  out[end],out[rightStartPtr]
-        rightStartPtr++
-        end--
+    rightView(root.Right)
+    for i := len(right)-1; i >= 0; i-- {
+        out = append(out, right[i])
     }
     return out
 }
