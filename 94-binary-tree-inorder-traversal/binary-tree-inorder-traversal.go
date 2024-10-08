072157407104d24b8fdb713933dee841ad638638
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-07 20:59:22 -0700

    Time: 0 ms (100.00%) | Memory: 2.3 MB (36.51%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
@@ -6,34 +6,36 @@
  *     Right *TreeNode
  * }
  */
+
+ // morris, using threaded tree
 func inorderTraversal(root *TreeNode) []int {
     if root == nil {return nil}
-    out := []int{}
     curr := root
+    out := []int{}
+    // inorder is left root right
     for curr != nil {
         if curr.Left == nil {
-            // left is nil
-            // process root ( left(nil) -> root(we are here) -> right(next step) )
+            // process root
             out = append(out, curr.Val)
+            // go right
             curr = curr.Right
         } else {
+            // connect the thread so we can come back to this node
+            // from the last node of this subtree
             tmp := curr.Left
             for tmp.Right != nil && tmp.Right != curr {
                 tmp = tmp.Right
             }
             if tmp.Right == nil {
-                // create the thread to curr
                 tmp.Right = curr
                 // go left
                 curr = curr.Left
             } else {
-                // tmp Right is curr, i.e this is a thread we created
-                // i.e from left, we came back to parent / root ( left(done) -> root(we are here) -> right(next step) )
-                
-                // disconnect tmp -> curr
-                tmp.Right = nil
-                // process curr ( i.e root )
+                // done with left subtree
+                // process root
                 out = append(out, curr.Val)
+                // disconnect the thread
+                tmp.Right = nil
                 // go right
                 curr = curr.Right
             }
