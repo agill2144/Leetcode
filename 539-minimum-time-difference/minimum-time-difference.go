e51619c442613493571c54ff8074c2653f51d8ee
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-09 16:50:14 -0700

    Time: 4 ms (78.07%) | Memory: 6.5 MB (5.90%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.go
+++ 539-minimum-time-difference/minimum-time-difference.go
@@ -1,38 +1,20 @@
-// convert to minutes
-// sort
-// find smallest minutes differnce
-// n = len(timePoints)
-// time = o(n) + o(nlogn) + o(n)
-// space = o(n) + o(n)
 func findMinDifference(timePoints []string) int {
-    mins := []int{}
+    // mins in 1 hour = 60
+    // eg; 2hr = 120 mins
+    minutes := []int{}
     for i := 0; i < len(timePoints); i++ {
         t := strings.Split(timePoints[i], ":")
-        hh, _ := strconv.Atoi(t[0])
-        mm, _ := strconv.Atoi(t[1])
-        mins = append(mins, (hh*60)+mm)
+        hh, mm := t[0], t[1]
+        hhInt, _ := strconv.Atoi(hh)
+        mmInt, _ := strconv.Atoi(mm)
+        m := (hhInt * 60) + mmInt
+        if m == 0 {m = 1440}
+        minutes = append(minutes, m)
     }
-    sort.Ints(mins)
-    // smallest diff ( adj numbers or 0 and n-1 element)
-    // since time is circule (i.e after 23:59 -> 00:00 )
-    // the 0th minutes needs to be relative to the n-1th minutes element
-    // 00:00 -> 0
-    // 23:50 -> 1439
-    // if we did 1439-0 = 1439
-    // but really ... 00:00 to minutes should be 1400
-    // therefore we need to make sure that 0th time is after midnight
-    // therefore minutes[0] + 24hr*60min 
-    minDiff := (mins[0]+(24*60)) - mins[len(mins)-1]
-    for i := 1; i < len(mins); i++ {
-        minDiff = min(minDiff, mins[i]-mins[i-1])
+    sort.Ints(minutes)
+    smallestDiff := minutes[0]+(24*60) - minutes[len(minutes)-1]
+    for i := 1; i < len(minutes); i++ {
+        smallestDiff = min(smallestDiff, minutes[i]-minutes[i-1])
     }
-    return minDiff
+    return smallestDiff
 }
-
-/*
-    1hr = 60 mins
-    2hr = 120 mins ( 2 * 60 )
-    5hr = 5*60 = 300 mins
-
-    05:34 = (5hr*60) + 34 = 300 + 34 = 334 
-*/
