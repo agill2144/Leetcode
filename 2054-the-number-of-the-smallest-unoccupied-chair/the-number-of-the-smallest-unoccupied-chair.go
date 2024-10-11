6f9c04239e3af63d921e50e3864f087c1ee6c255
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-10 21:20:09 -0700

    Time: 107 ms (90.00%) | Memory: 7.1 MB (70.00%) - LeetSync

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.go
@@ -0,0 +1,69 @@
+func smallestChair(times [][]int, targetFriend int) int {
+    targetFriendTime := times[targetFriend]
+    sort.Slice(times, func(i, j int)bool {
+        return times[i][0] < times[j][0]
+    })
+    chairs := &chairMinHeap{items: []int{}}
+    for i := 0; i < len(times); i++ {heap.Push(chairs, i)}
+    mn := &minHeap{items: [][]int{}}
+    for i := 0; i < len(times); i++ {
+        arrive, depart := times[i][0], times[i][1]
+        for mn.Len() > 0 && arrive >= mn.items[0][1] {
+            heap.Push(chairs, heap.Pop(mn).([]int)[0])
+        }
+        cid := heap.Pop(chairs).(int)
+        heap.Push(mn, []int{cid, depart})
+        if arrive == targetFriendTime[0] {return cid}
+    }
+    return -1
+
+}
+
+type chairMinHeap struct {
+	items []int
+}
+
+func (m *chairMinHeap) Less(i, j int) bool {
+	return m.items[i] < m.items[j]
+}
+func (m *chairMinHeap) Swap(i, j int) {
+	m.items[i], m.items[j] = m.items[j], m.items[i]
+}
+func (m *chairMinHeap) Len() int {
+	return len(m.items)
+}
+func (m *chairMinHeap) Push(x interface{}) {
+	m.items = append(m.items, x.(int))
+}
+func (m *chairMinHeap) Pop() interface{} {
+	out := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return out
+}
+
+
+
+type minHeap struct {
+	items [][]int // [[chairIdx, endTime]]
+}
+
+func (m *minHeap) Less(i, j int) bool {
+	if m.items[i][1] == m.items[j][1] {
+        return m.items[i][0] < m.items[j][0]
+    }
+    return m.items[i][1] < m.items[j][1]
+}
+func (m *minHeap) Swap(i, j int) {
+	m.items[i], m.items[j] = m.items[j], m.items[i]
+}
+func (m *minHeap) Len() int {
+	return len(m.items)
+}
+func (m *minHeap) Push(x interface{}) {
+	m.items = append(m.items, x.([]int))
+}
+func (m *minHeap) Pop() interface{} {
+	out := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return out
+}
