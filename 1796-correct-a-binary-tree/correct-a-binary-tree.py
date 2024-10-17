02032c1ea67df123cac54697c1c5375970fc3d6c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-16 20:07:29 -0700

    Time: 104 ms (34.59%) | Memory: 22 MB (75.47%) - LeetSync

---
+++ 1796-correct-a-binary-tree/correct-a-binary-tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def correctBinaryTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+
+        q = [[root, root]] # <node, parent>
+        while q:
+            q_size = len(q)
+            processed_level_nodes = set()
+            for _ in range(q_size):
+                dq = q.pop(0)
+                curr_node = dq[0]
+                parent = dq[1]
+                if curr_node.right:
+                    if curr_node.right in processed_level_nodes:
+                        if parent.left and parent.left == curr_node:
+                            parent.left = None
+                        elif parent.right and parent.right == curr_node:
+                            parent.right = None
+                        continue
+                    q.append([curr_node.right, curr_node])
+                if curr_node.left:
+                    q.append([curr_node.left, curr_node])
+                processed_level_nodes.add(curr_node)
+        return root
