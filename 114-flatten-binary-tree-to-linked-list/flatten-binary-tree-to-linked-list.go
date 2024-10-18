dd53a8b141080373c857a558ff0e76b63d911c2e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-17 22:43:01 -0700

    Time: 0 ms (100.00%) | Memory: 4.7 MB (5.99%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.go
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.go
@@ -6,26 +6,49 @@
  *     Right *TreeNode
  * }
  */
-func flatten(root *TreeNode)  {
-    var dfs func(r *TreeNode) *TreeNode
-    dfs = func(r *TreeNode) *TreeNode {
-        // base
-        if r == nil {return nil}
 
-        // logic
-        lt := dfs(r.Left)
-        rt := dfs(r.Right)
-        lh := r.Left
-        rh := r.Right
-        if lh != nil {
-            r.Right = lh
-            lt.Right = rh
-            r.Left = nil
+func flatten(root *TreeNode) {
+    if root == nil {return}
+    curr := root
+    for curr != nil {
+        if curr.Left == nil {
+            curr = curr.Right
+        } else {
+            // find the right-tail of left child
+            tail := curr.Left
+            for tail.Right != nil {
+                tail = tail.Right
+            }
+            // connect tail to curr right
+            tail.Right = curr.Right
+            // bring curr's left child to its right side
+            curr.Right = curr.Left
+            curr.Left = nil
+            curr = curr.Right
         }
-        if rt != nil {return rt}
-        if lt != nil {return lt}
-        return r
     }
-    dfs(root)
-
 }
+
+// func flatten(root *TreeNode)  {
+//     var dfs func(r *TreeNode) *TreeNode
+//     dfs = func(r *TreeNode) *TreeNode {
+//         // base
+//         if r == nil {return nil}
+
+//         // logic
+//         lt := dfs(r.Left)
+//         rt := dfs(r.Right)
+//         lh := r.Left
+//         rh := r.Right
+//         if lh != nil {
+//             r.Right = lh
+//             lt.Right = rh
+//             r.Left = nil
+//         }
+//         if rt != nil {return rt}
+//         if lt != nil {return lt}
+//         return r
+//     }
+//     dfs(root)
+
+// }
