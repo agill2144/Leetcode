100a7e7b6093773713e8104bda8c1c521bb3e4e3
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-17 21:14:12 -0700

    Time: 0 ms (100.00%) | Memory: 9.3 MB (8.09%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.go
+++ 975-range-sum-of-bst/range-sum-of-bst.go
@@ -7,18 +7,17 @@
  * }
  */
 func rangeSumBST(root *TreeNode, low int, high int) int {
-    sum := 0
+    total := 0
     var dfs func(r *TreeNode)
     dfs = func(r *TreeNode) {
         // base
-        if r == nil {return}        
+        if r == nil {return}
+
         // logic
-        if r.Val >= low && r.Val <= high {
-            sum += r.Val
-        }
-        dfs(r.Left)
-        dfs(r.Right)
+        if r.Val >= low && r.Val <= high {total += r.Val}
+        if r.Val >= low {dfs(r.Left)}
+        if r.Val <= high {dfs(r.Right)}
     }
     dfs(root)
-    return sum
+    return total
 }
