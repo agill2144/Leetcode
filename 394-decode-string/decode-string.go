a8ae6b97c0ebe35291fe06cf9ae8b8c79caf06fd
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-10-23 19:43:00 -0700

    Time: 0 ms (100.00%) | Memory: 3.9 MB (6.91%) - LeetSync

--- 394-decode-string/decode-string.go
+++ 394-decode-string/decode-string.go
@@ -1,73 +1,33 @@
 func decodeString(s string) string {
-    ptr := 0
-    var dfs func(str string) string
-    dfs = func(str string) string{
-        curr := new(strings.Builder)
-        n := 0
-        for ptr < len(str) {
-            char := s[ptr]
-            ptr++
-            if char >= '0' && char <= '9' {
-                n = n * 10 + int(char-'0')
-            } else if char >= 'a' && char <= 'z' {
-                curr.WriteByte(char)
-            } else {
-                if char == '[' {
-                    res := dfs(s)
-                    decoded := new(strings.Builder)
-                    for n > 0 {
-                        decoded.WriteString(res)
-                        n--
-                    }
-                    curr.WriteString(decoded.String())
-                    n = 0
-                } else if char == ']' {
-
-                    return curr.String()
-                }
+    strs := []*strings.Builder{}
+    nums := []int{}
+    curr := new(strings.Builder)
+    currNum := 0
+    for i := 0; i < len(s); i++ {
+        char := s[i]
+        if char >= '0' && char <= '9' {
+            currNum = currNum * 10 + int(char-'0')
+        } else if char >= 'a' && char <= 'z' {
+            curr.WriteByte(char)
+        } else if char == '[' {
+            nums = append(nums, currNum)
+            strs = append(strs, curr)
+            curr = new(strings.Builder)
+            currNum = 0
+        } else if char == ']' {
+            // repeat curr string $top num times
+            k := nums[len(nums)-1]
+            nums = nums[:len(nums)-1]
+            decoded := new(strings.Builder)
+            for j := 0; j < k; j++ {
+                decoded.WriteString(curr.String())
             }
+            // concat $top-of-strs + decoded
+            curr = new(strings.Builder)
+            curr.WriteString(strs[len(strs)-1].String())
+            curr.WriteString(decoded.String())
+            strs = strs[:len(strs)-1]
         }
-        return curr.String()
-
     }
-    return dfs(s)
+    return curr.String()
 }
-
-// func decodeString(s string) string {
-//     strSt := []*strings.Builder{}
-//     numSt := []int{}
-//     curr := new(strings.Builder)
-//     n := 0
-//     for i := 0; i < len(s); i++ {
-//         char := s[i]
-//         if char >= '0' && char <= '9' {
-//             n = n * 10 + int(char-'0')
-//         } else if char >= 'a' && char <= 'z' {
-//             curr.WriteByte(char)
-//         } else {
-//             if char == '[' {
-//                 strSt = append(strSt, curr)
-//                 numSt = append(numSt, n)
-//                 curr = new(strings.Builder)
-//                 n = 0
-//             } else if char == ']' {
-//                 // repeat curr string k times
-//                 // k = top of num st
-//                 k := numSt[len(numSt)-1]
-//                 numSt = numSt[:len(numSt)-1]
-//                 decoded := new(strings.Builder)
-//                 for k > 0 {
-//                     decoded.WriteString(curr.String())
-//                     k--
-//                 }
-//                 // concatnate $parent + $decoded
-//                 // parent = top of strSt
-//                 parent := strSt[len(strSt)-1]
-//                 strSt = strSt[:len(strSt)-1]
-//                 parent.WriteString(decoded.String())
-//                 curr = parent
-//             }
-//         }
-//     }
-//     return curr.String()
-// }
