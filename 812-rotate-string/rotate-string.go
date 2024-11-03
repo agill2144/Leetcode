4d675c06122e5d0177b6a5ea4d59fdc69e8a1ff1
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-03 14:54:46 -0800

    Time: 0 ms (100.00%) | Memory: 3.8 MB (35.58%) - LeetSync

--- 812-rotate-string/rotate-string.go
+++ 812-rotate-string/rotate-string.go
@@ -1,21 +1,27 @@
 func rotateString(s string, goal string) bool {
     if len(s) != len(goal) {return false}
-    starts := []int{}
-    for i := 0; i < len(goal); i++ {
-        if s[i] == goal[i] {starts = append(starts, i)}
-    }
-
-    for k := 0; k < len(goal); k++ {
-        sPtr := k
-        g := len(goal)
-        ok := true
-        for i := 0; i < len(s); i++ {
-            if s[i] != goal[sPtr%g] {ok = false; break}
-            sPtr++
-        }
-        if ok {return true}
-    }
-    return false
+    ss := s+s
+    return strings.Contains(ss, goal)
 }
+// find all possible start idxs from goal
+// func rotateString(s string, goal string) bool {
+//     if len(s) != len(goal) {return false}
+//     starts := []int{}
+//     for i := 0; i < len(goal); i++ {
+//         if s[i] == goal[i] {starts = append(starts, i)}
+//     }
 
-// aacde cdeaa
+//     for k := 0; k < len(goal); k++ {
+//         sPtr := k
+//         g := len(goal)
+//         ok := true
+//         for i := 0; i < len(s); i++ {
+//             if s[i] != goal[sPtr%g] {ok = false; break}
+//             sPtr++
+//         }
+//         if ok {return true}
+//     }
+//     return false
+// }
+
+// aacde-aacde cdeaa
