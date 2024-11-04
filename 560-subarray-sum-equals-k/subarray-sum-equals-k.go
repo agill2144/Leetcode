64b0dd093ad3fecd52e3f6c468058ed6daf3e0b4
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-04 11:21:05 -0800

    Time: 14 ms (84.67%) | Memory: 9.4 MB (11.53%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.go
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.go
@@ -1,17 +1,12 @@
-// sliding window temptations
-// but sliding window only works when window sum is either increasing or descreasing
-// its always monotonic in nature
-// with negatives, the window sum will be everywhere, hence we cannot use sliding window
-// runningSum pattern.
 func subarraySum(nums []int, k int) int {
-    sum := 0
     freq := map[int]int{0:1}
-    count := 0
+    rSum := 0
+    total := 0
     for i := 0; i < len(nums); i++ {
-        sum += nums[i]
-        diff := sum-k
-        count += freq[diff]
-        freq[sum]++
+        rSum += nums[i]
+        diff := rSum - k
+        total += freq[diff]
+        freq[rSum]++
     }
-    return count
+    return total
 }
