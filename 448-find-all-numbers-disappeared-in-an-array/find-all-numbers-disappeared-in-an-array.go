71f124c7c292b12a47fc61d22d1f877ac5298916
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-06 10:06:04 -0800

    Time: 25 ms (14.91%) | Memory: 9 MB (22.17%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.go
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.go
@@ -1,17 +1,10 @@
 func findDisappearedNumbers(nums []int) []int {
-    for i := 0; i < len(nums); i++ {
-        idx := abs(nums[i])-1
-        if nums[idx] < 0 {continue}
-        nums[idx] *= -1
-    }
+    set := map[int]bool{}
+    for i := 0; i < len(nums); i++ {set[nums[i]] = true}
+    exp := 1
     out := []int{}
-    for i := 0; i < len(nums); i++ {
-        if nums[i] > 0 {out = append(out, i+1)}
+    for i := exp; i <= len(nums); i++ {
+        if !set[i] {out = append(out, i)}
     }
     return out
-}
-
-func abs(x int) int {
-    if x < 0 {return x *-1}
-    return x
 }
