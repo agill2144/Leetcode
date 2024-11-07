b17c5fb74363e94c0664031ed4771e696dedc8f6
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-07 07:58:26 -0800

    Time: 5 ms (50.00%) | Memory: 6.2 MB (25.00%) - LeetSync

--- 1534-minimum-number-of-frogs-croaking/minimum-number-of-frogs-croaking.go
+++ 1534-minimum-number-of-frogs-croaking/minimum-number-of-frogs-croaking.go
@@ -1,28 +1,35 @@
 func minNumberOfFrogs(croakOfFrogs string) int {
-    active := 0
-    count := 0
-    c := 0; r := 0; o := 0; a := 0; k := 0;
+    c,r,o,a,k := 0,0,0,0,0
+    maxCount := 0
     for i := 0; i < len(croakOfFrogs); i++ {
         char := croakOfFrogs[i]
         if char == 'c' {
             c++
-            active++
-            count = max(count, active)
+            if c > maxCount {maxCount = c}
         } else if char == 'r' {
             r++
+            // c-r-o-a-k
+            // how can there be more r's than c's ?
+            // its impossible and out-of-order...
             if r > c {return -1}
         } else if char == 'o' {
             o++
             if o > r {return -1}
         } else if char == 'a' {
             a++
-            if a > o {return -1}
+            if a > o {return -1 }
         } else if char == 'k' {
             k++
             if k > a {return -1}
-            active--
+            k--; a--; o--; r--; c--;
+        } else {
+            // we ran into a char thats not part of "croak"
+            return -1
         }
     }
-    if c != k {return -1}
-    return count
+    // if the string was perfectly balanced
+    // then all chars in "croak" should have been finished
+    // nothing should be > 0 or < 0 - for a perfect valid input
+    if c != 0 || r != 0 || o != 0 || a != 0 || k != 0 {return -1}
+    return maxCount
 }
