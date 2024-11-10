889af6c1cb947fd0570785930dc1edff909a9261
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-10 10:14:34 -0800

    Time: 0 ms (100.00%) | Memory: 6.6 MB (32.58%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.go
+++ 113-path-sum-ii/path-sum-ii.go
@@ -6,31 +6,25 @@
  *     Right *TreeNode
  * }
  */
-func pathSum(root *TreeNode, targetSum int) [][]int {
-    out := [][]int{}    
+func pathSum(root *TreeNode, target int) [][]int {
+    out := [][]int{}
     var dfs func(r *TreeNode, sum int, path []int)
     dfs = func(r *TreeNode, sum int, path []int) {
         // base
         if r == nil {return}
 
         // logic
-        // add node to path
-        path = append(path, r.Val)
-        // add node value to sum
         sum += r.Val
-
-        // we have updated sum, now
-        // check whether this node is a leaf
-        // if yes, and the sum cumulated so far == target
-        // make a copy of the path, save the copy to output
+        path = append(path, r.Val)
         if r.Left == nil && r.Right == nil {
-            if sum == targetSum {
+            if sum == target {
                 newP := make([]int, len(path))
                 copy(newP, path)
                 out = append(out, newP)
             }
+            path = path[:len(path)-1]
+            return
         }
-
         dfs(r.Left, sum, path)
         dfs(r.Right, sum, path)
         path = path[:len(path)-1]
