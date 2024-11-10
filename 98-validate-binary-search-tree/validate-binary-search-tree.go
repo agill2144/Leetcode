7e6ebdb6e1d9eb1473390ab7a8a817ed665b016e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-10 09:32:05 -0800

    Time: 0 ms (100.00%) | Memory: 7.1 MB (28.23%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.go
+++ 98-validate-binary-search-tree/validate-binary-search-tree.go
@@ -8,20 +8,34 @@
  */
  // inorder on a BST = sorted order
 func isValidBST(root *TreeNode) bool {
-    var prev *TreeNode
-    var dfs func(r *TreeNode) bool
-    dfs = func(r *TreeNode) bool {
+    var dfs func(r *TreeNode, min, max int) bool
+    dfs = func(r *TreeNode, min, max int) bool {
         // base
         if r == nil {return true}
 
         // logic
-        left := dfs(r.Left)
-        if !left {return false}
-        if prev != nil {
-            if prev.Val >= r.Val {return false}
-        }
-        prev = r
-        return dfs(r.Right)
+        if r.Val <= min || r.Val >= max {return false}
+        if !dfs(r.Left, min, r.Val) {return false}
+        if !dfs(r.Right, r.Val, max) {return false}
+        return true
     }
-    return dfs(root)
+    return dfs(root, math.MinInt64, math.MaxInt64)
 }
+// func isValidBST(root *TreeNode) bool {
+//     var prev *TreeNode
+//     var dfs func(r *TreeNode) bool
+//     dfs = func(r *TreeNode) bool {
+//         // base
+//         if r == nil {return true}
+
+//         // logic
+//         left := dfs(r.Left)
+//         if !left {return false}
+//         if prev != nil {
+//             if prev.Val >= r.Val {return false}
+//         }
+//         prev = r
+//         return dfs(r.Right)
+//     }
+//     return dfs(root)
+// }
