516ac69c8d9a22a79489d4e802513b838daefc41
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-10 09:11:25 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (26.38%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.go
@@ -0,0 +1,17 @@
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+    ptr := len(nums1)-1
+    n1 := m-1
+    n2 := n-1
+    for n1 >= 0 && n2 >= 0 {
+        if nums1[n1] > nums2[n2] {
+            nums1[ptr] = nums1[n1]
+            n1--
+        } else {
+            nums1[ptr] = nums2[n2]
+            n2--
+        }
+        ptr--
+    }
+    for n1 >= 0 {nums1[ptr] = nums1[n1];n1--; ptr--}
+    for n2 >= 0 {nums1[ptr] = nums2[n2];n2--; ptr--}
+}
