048e7282fa1a0e07ebe583e56205e3651e23e30f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-10 21:58:33 -0800

    Time: 0 ms (100.00%) | Memory: 7.7 MB (38.42%) - LeetSync

--- 268-missing-number/missing-number.go
+++ 268-missing-number/missing-number.go
@@ -1,18 +1,7 @@
-/*
-    if you know the numbers are from 0 to n
-    then we can calculate the sum of all numbers from 0 to n using a formula
-    - (n(n+1))/2
-    
-    the above gives total EXPECTED sum
-    now we can calc the acutal sum and return the diff
-    - the diff is the missing number
-*/
 func missingNumber(nums []int) int {
     n := len(nums)
-    expected := (n * (n+1))/2
-    actual := 0
-    for i := 0; i < len(nums); i++ {
-        actual += nums[i]
-    }
-    return expected-actual
+    expectedSum := n*(n+1)/2
+    actualSum := 0
+    for i := 0; i < len(nums); i++ {actualSum += nums[i]}
+    return expectedSum - actualSum    
 }
