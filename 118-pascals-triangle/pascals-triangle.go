9d61728ec09b6a0d14c5ce9ee981ab85156945f6
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-11 10:55:17 -0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (14.31%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.go
+++ 118-pascals-triangle/pascals-triangle.go
@@ -1,16 +1,13 @@
 func generate(numRows int) [][]int {
-    // num of cols in a row i is i+1
-    // col[0] and col[n-1] is reserved by 1
-    // in-between col vals are out[i-1][j] + out[i-1][j-1]
-    out := make([][]int, numRows)
-    for i := 0; i < len(out); i++ {
+    matrix := make([][]int, numRows)
+    for i := 0; i < numRows; i++ {
         n := i+1
-        out[i] = make([]int, n)
-        out[i][0] = 1
-        out[i][n-1] = 1
+        matrix[i] = make([]int, n)
+        matrix[i][0] = 1
+        matrix[i][n-1] = 1
         for j := 1; j < n-1; j++ {
-            out[i][j] = out[i-1][j] + out[i-1][j-1]
+            matrix[i][j] = matrix[i-1][j] + matrix[i-1][j-1]
         }
     }
-    return out
+    return matrix
 }
