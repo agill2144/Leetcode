239316a259ec16d2b64afdb2eb6a35e096cb7965
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-10 22:00:31 -0800

    Time: 0 ms (100.00%) | Memory: 9 MB (31.63%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.go
+++ 485-max-consecutive-ones/max-consecutive-ones.go
@@ -1,8 +1,8 @@
 func findMaxConsecutiveOnes(nums []int) int {
-    maxCount := 0
     count := 0
+    maxCount := 0
     for i := 0; i < len(nums); i++ {
-        if nums[i] == 1 {count++} else {count=0}
+        if nums[i] == 1 {count++} else {count = 0}
         maxCount = max(maxCount, count)
     }
     return maxCount
