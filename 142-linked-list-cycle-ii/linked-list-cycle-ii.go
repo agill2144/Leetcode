42bcb342f62cd0702b55d6a951a4995908a0c0ad
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-11 13:17:00 -0800

    Time: 0 ms (100.00%) | Memory: 5.3 MB (30.49%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.go
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.go
@@ -6,12 +6,25 @@
  * }
  */
 func detectCycle(head *ListNode) *ListNode {
-    set := map[*ListNode]bool{}
-    curr := head
-    for curr != nil {
-        if set[curr] {return curr}
-        set[curr] = true
-        curr = curr.Next
+    slow := head
+    fast := head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+        if slow == fast {break}
     }
-    return nil
+    if fast == nil || fast.Next == nil {return nil}
+    p1 := head
+    for p1 != slow {p1 = p1.Next; slow = slow.Next}
+    return p1
 }
+// func detectCycle(head *ListNode) *ListNode {
+//     set := map[*ListNode]bool{}
+//     curr := head
+//     for curr != nil {
+//         if set[curr] {return curr}
+//         set[curr] = true
+//         curr = curr.Next
+//     }
+//     return nil
+// }
