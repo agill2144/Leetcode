701bb06eb905b31a4d5697c980ecb031740f47c3
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-11 15:10:56 -0800

    Time: 0 ms (100.00%) | Memory: 5.5 MB (79.59%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.go
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.go
@@ -1,22 +1,44 @@
 func findPairs(nums []int, k int) int {
-    set := map[[2]int]struct{}{}
-    idx := map[int]int{}
-    for i := 0; i < len(nums); i++ {
-        c1 := nums[i]-k
-        _, ok := idx[c1]
-        if ok {
-            tmp := [2]int{nums[i], c1}
-            if c1 < nums[i] {tmp = [2]int{c1, nums[i]}}
-            set[tmp] = struct{}{}
+    sort.Ints(nums)
+    count := 0
+    slow := 0
+    fast := 1
+    for slow < len(nums) && fast < len(nums) {
+        if slow == fast {fast++; continue}
+        diff := nums[fast] - nums[slow]
+        if diff == k {
+            count++
+            slow++
+            for slow < fast && nums[slow] == nums[slow-1] {slow++}
+            fast++
+            for fast < len(nums) && nums[fast] == nums[fast-1] {fast++}
+        } else if diff > k {
+            slow++
+        } else {
+            fast++
         }
-        c2 := nums[i]+k
-        _, ok = idx[c2]
-        if ok {
-            tmp := [2]int{nums[i], c2}
-            if c2 < nums[i] {tmp = [2]int{c2, nums[i]}}
-            set[tmp] = struct{}{}
-        }
-        idx[nums[i]] = i
     }
-    return len(set)
+    return count
 }
+// func findPairs(nums []int, k int) int {
+//     set := map[[2]int]struct{}{}
+//     idx := map[int]int{}
+//     for i := 0; i < len(nums); i++ {
+//         c1 := nums[i]-k
+//         _, ok := idx[c1]
+//         if ok {
+//             tmp := [2]int{nums[i], c1}
+//             if c1 < nums[i] {tmp = [2]int{c1, nums[i]}}
+//             set[tmp] = struct{}{}
+//         }
+//         c2 := nums[i]+k
+//         _, ok = idx[c2]
+//         if ok {
+//             tmp := [2]int{nums[i], c2}
+//             if c2 < nums[i] {tmp = [2]int{c2, nums[i]}}
+//             set[tmp] = struct{}{}
+//         }
+//         idx[nums[i]] = i
+//     }
+//     return len(set)
+// }
