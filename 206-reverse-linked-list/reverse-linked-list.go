ee9f8ddc152329654ee51f29daf242c09a969d18
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-11 13:35:48 -0800

    Time: 0 ms (100.00%) | Memory: 4.6 MB (6.09%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.go
+++ 206-reverse-linked-list/reverse-linked-list.go
@@ -6,14 +6,33 @@
  * }
  */
 func reverseList(head *ListNode) *ListNode {
-    if head == nil || head.Next == nil {return head}
-    curr := head
-    var prev *ListNode
-    for curr != nil {
-        next := curr.Next
-        curr.Next = prev
-        prev = curr
-        curr = next
+    var dfs func(curr *ListNode) *ListNode
+    dfs = func(curr *ListNode) *ListNode {
+        // base
+        if curr == nil {return nil}
+
+        // logic
+        newHead := dfs(curr.Next)
+        if curr.Next != nil {
+            curr.Next.Next = curr
+            curr.Next = nil
+        } else {
+            return curr
+        }
+        return newHead
     }
-    return prev
+    return dfs(head)
 }
+
+// func reverseList(head *ListNode) *ListNode {
+//     if head == nil || head.Next == nil {return head}
+//     curr := head
+//     var prev *ListNode
+//     for curr != nil {
+//         next := curr.Next
+//         curr.Next = prev
+//         prev = curr
+//         curr = next
+//     }
+//     return prev
+// }
