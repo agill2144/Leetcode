741634be3cb8c88b7f617d63793d2e5d0fd9beda
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-12 09:57:49 -0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (7.49%) - LeetSync

--- 48-rotate-image/rotate-image.go
+++ 48-rotate-image/rotate-image.go
@@ -1,19 +1,15 @@
-// transpose and reverse
 func rotate(matrix [][]int)  {
     n := len(matrix)
+    // 1. transpose
     for i := 0; i < n; i++ {
         for j := i+1; j < n; j++ {
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         }
     }
-
+    // 2. reverse each row
     for i := 0; i < n; i++ {
-        left := 0
-        right := n-1
-        for left < right {
-            matrix[i][left], matrix[i][right] = matrix[i][right], matrix[i][left]
-            left++
-            right--
+        for j := 0; j < n/2; j++ {
+            matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
         }
     }
     
