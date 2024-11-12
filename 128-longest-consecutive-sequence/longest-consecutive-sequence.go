37126b6bb0bedf136ff43e2f5e4a10f4815481de
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-12 10:07:33 -0800

    Time: 1812 ms (20.67%) | Memory: 12.3 MB (22.32%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.go
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.go
@@ -1,23 +1,18 @@
 func longestConsecutive(nums []int) int {
-    set := map[int]struct{}{}
+    set := map[int]bool{}
     for i := 0; i < len(nums); i++ {
-        set[nums[i]] = struct{}{}
+        set[nums[i]] = true
     }
-    longest := 0
-    for val, _ := range set {
-
-        _, prevExists := set[val-1]
-        if prevExists {continue}
-
-        _, ok := set[val]
-        start := val
+    maxLen := 0
+    for i := 0; i < len(nums); i++ {
+        if set[nums[i]-1] {continue}
+        k := nums[i]
         size := 0
-        for ok {
+        for set[k] {
             size++
-            longest = max(longest, size)
-            start++
-            _, ok = set[start]
+            k++
         }
+        maxLen = max(maxLen, size)
     }
-    return longest
+    return maxLen
 }
