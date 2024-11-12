388470854e46bad779a480770f149f6b73010659
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-12 09:21:15 -0800

    Time: 19 ms (65.25%) | Memory: 8.8 MB (31.68%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.go
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.go
@@ -4,6 +4,13 @@ func subarraySum(nums []int, k int) int {
     total := 0
     for i := 0; i < len(nums); i++ {
         rSum += nums[i]
+        // when sum from idx 0 to i is k
+        // we do not have to remove anything from this rSum to make it equal to k
+        // well, technically, we have to remove 0 from rSum to make it equal to k
+        // but we do not have 0 as rSum in our freq map, therefore handling of this case
+        // we have 2 options;
+        // 1. add 0 sum to freq at the very beginning {0:1}
+        // 2. handle when rSum == k without adding 0 sum in the freq map
         if rSum == k {total++}
         total += freq[rSum - k]
         freq[rSum]++
