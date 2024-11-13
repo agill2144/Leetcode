c35d7a45eae74bd125fff1208fafcc3953df5c2c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-13 13:01:40 -0800

    Time: 0 ms (100.00%) | Memory: 5.2 MB (38.46%) - LeetSync

--- 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.go
+++ 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.go
@@ -1,13 +1,22 @@
 func calculateTax(brackets [][]int, income int) float64 {
     // when there is no income, there is nothing to tax on
     if income == 0 {return 0.0}
-    taxableAmount := min(brackets[0][0], income)
-    var taxes float64 =  float64(taxableAmount) * (float64(brackets[0][1])/100.0)
-    income -= taxableAmount
-    for i := 1; i < len(brackets) && income > 0; i++ {
-        taxableAmount = min(income, brackets[i][0]-brackets[i-1][0])
-        taxes += float64(taxableAmount) * (float64(brackets[i][1])/100.0)
-        income -= taxableAmount
+    taxes := 0.0
+    for i := 0; i < len(brackets) && income > 0; i++ {
+        /*
+            - we have to evaluate each tax bracket ( while our income > 0 )
+            - taxRate = difference between current taxRate - prevTaxRate
+            - taxRate = 3 and income could be 7
+            - then tax is calculated using min value between the two * taxPercent in this bracket
+            - taxedAmount = min(taxRate, income) * (bracket[i][1]/100)
+            - then this taxedAmount is removed from income
+            - then we go to next tax bracket, and repeat the same things
+        */
+        taxRate := brackets[i][0]
+        if i > 0 {taxRate = brackets[i][0] - brackets[i-1][0]}
+        taxRate = min(taxRate, income)
+        taxes += (float64(taxRate) * (float64(brackets[i][1])/100.0))
+        income -= taxRate
     }
     return taxes
 }
