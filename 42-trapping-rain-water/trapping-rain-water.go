3733311983a7f556ea86b58382a93eacc8372182
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-13 09:32:13 -0800

    Time: 0 ms (100.00%) | Memory: 8.1 MB (15.31%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.go
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -1,60 +1,14 @@
 func trap(height []int) int {
     n := len(height)
-    leftWall := height[0]
-    rightWall := height[n-1]
-    left, right := 0, n-1
+    maxL := make([]int, n)
+    maxR := make([]int, n)
+    for i := 1; i < n; i++ {maxL[i] = max(maxL[i-1], height[i-1])}
+    for i := n-2; i >= 0; i-- {maxR[i] = max(maxR[i+1], height[i+1])}
     total := 0
-    for left < right {
-        if leftWall <= rightWall {
-            // we have a rightWall that is >= leftWall
-            // therefore we can process the left building
-            if leftWall >= height[left]{
-                // we can only process left build
-                // if leftWall height >= left build
-                // why not just > , why >= ?
-                // well 1. we have picked a side to process becuase leftWall is smaller, 
-                // and rightWall provides us a guarantee that we can trap something on left, we have enough height on right to trap
-                // 2. if building height == leftWall, this building cannot trap any water, even tho we have something on right side bigger to hold
-                // from overflowing on right side
-                // but we have nothing on left side from overflowing
-                // therefore still process this left building even if height == leftWall
-                // therefore >= instead of just > 
-                trap := leftWall-height[left]
-                total += trap
-                left++
-            } else {
-                leftWall = height[left]
-            }
-        } else {
-            // we have a leftWall > rightWall
-            // therefore we can process the right building
-            if rightWall >= height[right] {
-                trap := rightWall-height[right]
-                total += trap
-                right--
-            } else {
-                rightWall = height[right]
-            }
-        }
+    for i := 1; i < n-1; i++ {
+        units := min(maxL[i], maxR[i]) - height[i]
+        if units > 0 {total += units}
     }
     return total
 }
 
-// func trap(height []int) int {
-//     n := len(height)
-//     maxL := make([]int, n)
-//     maxR := make([]int, n)
-//     for i := 1; i < n; i++ {
-//         maxL[i] = max(maxL[i-1], height[i-1])
-//         maxR[n-1-i] = max(maxR[n-1-i+1], height[n-1-i+1])
-//     }
-
-//     out := 0
-//     for i := 1; i <= n-2; i++ {
-//         left := maxL[i]
-//         right := maxR[i]
-//         trap := min(left, right) - height[i]
-//         if trap > 0 {out += trap}
-//     }
-//     return out
-// }
