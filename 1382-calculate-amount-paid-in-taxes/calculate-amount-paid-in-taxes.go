ea93a0397f967c4fe915f8cdb893a1ad72c4cba0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-13 12:07:32 -0800

    Time: 0 ms (100.00%) | Memory: 5.3 MB (38.46%) - LeetSync

--- 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.go
+++ 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.go
@@ -1,18 +1,14 @@
 func calculateTax(brackets [][]int, income int) float64 {
     // when there is no income, there is nothing to tax on
     if income == 0 {return 0.0}
-    taxableAmount := min(float64(brackets[0][0]), float64(income))
-    var taxes float64 =  taxableAmount * (float64(brackets[0][1])/100.0)
-    income -= int(taxableAmount)
+    taxableAmount := min(brackets[0][0], income)
+    var taxes float64 =  float64(taxableAmount) * (float64(brackets[0][1])/100.0)
+    income -= taxableAmount
     for i := 1; i < len(brackets); i++ {
-        taxableAmount = min(float64(income), float64(brackets[i][0])-float64(brackets[i-1][0]))
-        taxes += taxableAmount * (float64(brackets[i][1])/100.0)
-        income -= int(taxableAmount)
+        taxableAmount = min(income, brackets[i][0]-brackets[i-1][0])
+        taxes += float64(taxableAmount) * (float64(brackets[i][1])/100.0)
+        income -= taxableAmount
     }
     return taxes
 }
 
-func min(x, y float64) float64 {
-    if x < y {return x}
-    return y
-}
