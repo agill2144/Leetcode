440e627cc413f63e4af7233984b18bc69f5e8218
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-13 13:41:11 -0800

    Time: 0 ms (100.00%) | Memory: 6.5 MB (27.66%) - LeetSync

--- 243-shortest-word-distance/shortest-word-distance.go
+++ 243-shortest-word-distance/shortest-word-distance.go
@@ -1,23 +1,35 @@
 func shortestDistance(wordsDict []string, word1 string, word2 string) int {
-    idxs := map[string][]int{}
-    for i := 0; i < len(wordsDict); i++ {
-        idxs[wordsDict[i]] = append(idxs[wordsDict[i]],i)
-    }
-    p1 := 0
-    p2 := 0
     dist := len(wordsDict)
-    for p1 < len(idxs[word1]) && p2 < len(idxs[word2]) {
-        idx1 := idxs[word1][p1]
-        idx2 := idxs[word2][p2]
-        dist = min(dist, abs(idx2-idx1))
-        if idx1 < idx2 {
-            p1++
-        } else {
-            p2++
+    w1 := -1; w2 := -1
+    for i := 0; i < len(wordsDict); i++ {
+        if wordsDict[i] == word1 {w1 = i}
+        if wordsDict[i] == word2 {w2 = i}
+        if w1 != -1 && w2 != -1 {
+            dist = min(dist, abs(w2-w1))
         }
     }
     return dist
 }
+// func shortestDistance(wordsDict []string, word1 string, word2 string) int {
+//     idxs := map[string][]int{}
+//     for i := 0; i < len(wordsDict); i++ {
+//         idxs[wordsDict[i]] = append(idxs[wordsDict[i]],i)
+//     }
+//     p1 := 0
+//     p2 := 0
+//     dist := len(wordsDict)
+//     for p1 < len(idxs[word1]) && p2 < len(idxs[word2]) {
+//         idx1 := idxs[word1][p1]
+//         idx2 := idxs[word2][p2]
+//         dist = min(dist, abs(idx2-idx1))
+//         if idx1 < idx2 {
+//             p1++
+//         } else {
+//             p2++
+//         }
+//     }
+//     return dist
+// }
 
 func abs(x int) int {
     if x < 0 {return x *-1}
