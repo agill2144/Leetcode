74856b13156bf6c4e0d9099df4c9728ac44a3056
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-13 12:03:26 -0800

    Time: 0 ms (100.00%) | Memory: 5.3 MB (38.46%) - LeetSync

--- 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.go
+++ 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.go
@@ -1,10 +1,11 @@
 func calculateTax(brackets [][]int, income int) float64 {
     // when there is no income, there is nothing to tax on
     if income == 0 {return 0.0}
-    var taxes float64 = min(float64(brackets[0][0]), float64(income)) * (float64(brackets[0][1])/100.0)
-    income -= int(min(float64(brackets[0][0]), float64(income)))
+    taxableAmount := min(float64(brackets[0][0]), float64(income))
+    var taxes float64 =  taxableAmount * (float64(brackets[0][1])/100.0)
+    income -= int(taxableAmount)
     for i := 1; i < len(brackets); i++ {
-        taxableAmount := min(float64(income), float64(brackets[i][0])-float64(brackets[i-1][0]))
+        taxableAmount = min(float64(income), float64(brackets[i][0])-float64(brackets[i-1][0]))
         taxes += taxableAmount * (float64(brackets[i][1])/100.0)
         income -= int(taxableAmount)
     }
