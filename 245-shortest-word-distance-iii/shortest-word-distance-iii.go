4702ce5408a0e16ffcd2457610251c69438df9f6
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-13 14:02:28 -0800

    Time: 5 ms (66.67%) | Memory: 14.1 MB (22.22%) - LeetSync

--- 245-shortest-word-distance-iii/shortest-word-distance-iii.go
+++ 245-shortest-word-distance-iii/shortest-word-distance-iii.go
@@ -5,9 +5,21 @@ func shortestWordDistance(wordsDict []string, word1 string, word2 string) int {
     for i := 0; i < len(wordsDict); i++ {
         if wordsDict[i] == word1 {
             if same {
-                p2 = p1
+                // if word1 == word2
+                // promote current p1 value to p2
+                // and p1 becomes i
+                // word1, word2 = makes
+                // when "makes" shows up the first time
+                // p2 = -1; p1 = $firsIdx
+                // when "makes" shows up the second time
+                // p2 = $firstIdx; p1 = second idx
+                // when "makes" shows up the third time
+                // p2 = $secondIdx; p1 = third idx
+                p2 = p1 
                 p1 = i
-            } else {p1 = i}          
+            } else {
+                p1 = i
+            }          
         } else if wordsDict[i] == word2 {
             p2 = i
         }
