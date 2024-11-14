b67d6adaed6e792f7d0a68c2d02ff569471479e1
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-14 10:52:12 -0800

    Time: 5 ms (80.00%) | Memory: 10.1 MB (15.38%) - LeetSync

--- 244-shortest-word-distance-ii/shortest-word-distance-ii.go
+++ 244-shortest-word-distance-ii/shortest-word-distance-ii.go
@@ -1,32 +1,33 @@
 type WordDistance struct {
-    wordsDict []string    
+    idxs map[string][]int
 }
 
 
 func Constructor(wordsDict []string) WordDistance {
-    return WordDistance{wordsDict}    
+    idxs := map[string][]int{}
+    for i := 0; i < len(wordsDict); i++ {idxs[wordsDict[i]] = append(idxs[wordsDict[i]], i)}
+    return WordDistance{idxs}    
 }
 
 
 func (this *WordDistance) Shortest(word1 string, word2 string) int {
-    w1, w2 := -1, -1
-    dist := len(this.wordsDict)
-    for idx, word := range this.wordsDict {
-        if word == word1 {w1 = idx}
-        if word == word2 {w2 = idx}
-        if w1 != -1 && w2 != -1 {
-            dist = min(dist, abs(w2-w1))
-            if dist == 1 {break}
-        }
+    w1, w2 := 0, 0
+    dist := math.MaxInt64
+    for w1 < len(this.idxs[word1]) && w2 < len(this.idxs[word2]) {
+        w1Idx := this.idxs[word1][w1]
+        w2Idx := this.idxs[word2][w2]
+        dist = min(dist, abs(w2Idx-w1Idx))
+        if w1Idx < w2Idx {w1++} else {w2++}
     }
-    return dist    
+    return dist
 }
-
 
 func abs(x int) int {
-    if x < 0 {return x * -1} 
+    if x < 0 {return x*-1}
     return x
 }
+
+
 /**
  * Your WordDistance object will be instantiated and called as such:
  * obj := Constructor(wordsDict);
