08f5f192c4a877e6eebbbf07a828bd0344343b04
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-14 10:57:26 -0800

    Time: 51 ms (33.33%) | Memory: 15.9 MB (11.11%) - LeetSync

--- 245-shortest-word-distance-iii/shortest-word-distance-iii.go
+++ 245-shortest-word-distance-iii/shortest-word-distance-iii.go
@@ -1,34 +1,24 @@
 func shortestWordDistance(wordsDict []string, word1 string, word2 string) int {
-    p1 := -1; p2 := -1
+    idxs := map[string][]int{}
+    for idx, word := range wordsDict {
+        idxs[word] = append(idxs[word], idx)
+    }
     same := word1 == word2
-    dist := len(wordsDict)
-    for i := 0; i < len(wordsDict); i++ {
-        if wordsDict[i] == word1 {
-            if same {
-                // if word1 == word2
-                // promote current p1 value to p2
-                // and p1 becomes i
-                // word1, word2 = makes
-                // when "makes" shows up the first time
-                // p2 = -1; p1 = $firsIdx
-                // when "makes" shows up the second time
-                // p2 = $firstIdx; p1 = second idx
-                // when "makes" shows up the third time
-                // p2 = $secondIdx; p1 = third idx
-                p2 = p1 
-                p1 = i
-            } else {
-                p1 = i
-            }          
-        } else if wordsDict[i] == word2 {
-            p2 = i
+    w1 := 0; w2 := 0
+    if same {w2++}
+    dist := math.MaxInt64
+    for w1 < len(idxs[word1]) && w2 < len(idxs[word2]) {
+        w1Idx := idxs[word1][w1]
+        w2Idx := idxs[word2][w2]
+        dist = min(dist, abs(w2Idx-w1Idx))
+        if same {w1++; w2++} else {
+            if w1Idx < w2Idx {w1++} else {w2++}
         }
-        if p1 != -1 && p2 != -1 {dist = min(dist, abs(p2-p1))}
     }
     return dist
 }
 
 func abs(x int) int {
-    if x < 0 {return x *-1}
+    if x < 0 {return x * -1} 
     return x
 }
