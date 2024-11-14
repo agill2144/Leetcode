ebcfb9c81f6c923f30421c86d53bd866b9dcd7a1
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-14 14:12:10 -0800

    Time: 0 ms (100.00%) | Memory: 5.1 MB (48.54%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.go
+++ 152-maximum-product-subarray/maximum-product-subarray.go
@@ -1,20 +1,14 @@
 func maxProduct(nums []int) int {
-    maxProd := -100000.0
-    var rp float64 = 1.0
-    var rrp float64 = 1.0
-    for i := 0; i < len(nums); i++ {
-        rp *= float64(nums[i])
-        rrp *= float64(nums[len(nums)-1-i])
-        maxProd = max(maxProd, max(rp, rrp))
-        if rp == 0.0 {rp = 1.0}
-        if rrp == 0.0 {rrp = 1.0}
+    ans := math.MinInt64
+    left := 1
+    right := 1
+    n := len(nums)
+    for i := 0; i < n; i++ {
+        left *= nums[i]
+        right *= nums[n-1-i]
+        ans = max(ans, max(left, right))
+        if left == 0 {left = 1}
+        if right == 0 {right = 1}
     }
-    return int(maxProd)
-}
-
-
-
-func max(x, y float64) float64 {
-    if x > y {return x}
-    return y
+    return ans
 }
