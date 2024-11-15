ae2594750e43aeb02fcd0829d3197f84aee08590
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-15 15:53:50 -0800

    Time: 4 ms (97.92%) | Memory: 10.5 MB (36.26%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.go
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.go
@@ -1,120 +1,74 @@
-// optimization on earlier binary search;
-// we dont have to start on day 1;
-// what if all the values were [6,6,6,6,6,7]
-// then there is no way we would be able to make any bouquets until day 6
-// i.e smallest/earliest day
-// therefore update left to smallest/earliest day
-// time: o( (log$maxDay-$minDay+1) * n)
-// sapce = o(1)
 func minDays(bloomDay []int, m int, k int) int {
-    n := len(bloomDay)
-
-    left := math.MaxInt64 // earliest possible day we can make m bouquets
-    right := math.MinInt64 // last possible day we can make m bouquets
-    for i := 0; i < n; i++ { 
-        left = min(left, bloomDay[i])
-        right = max(right, bloomDay[i])
-    }
-
+    left := 1
+    right := -1
+    for i := 0; i < len(bloomDay); i++ { right = max(right, bloomDay[i]) }
     ans := -1
     for left <= right {
-        // day to evaluate
         mid := left + (right-left)/2
-        bCount := 0
-        fCount := 0
-        for i := 0; i < n; i++ {
+        // how many bouquets can be made if today was $mid day
+        flowers := 0
+        bouquets := 0
+        for i := 0; i < len(bloomDay); i++ {
             if bloomDay[i] <= mid {
-                fCount++
-                if fCount == k {
-                    bCount++
-                    fCount = 0
+                flowers++
+                if flowers == k {
+                    bouquets++
+                    flowers = 0
                 }
             } else {
-                fCount = 0
+                flowers = 0
             }
         }
 
         // when does mid not work?
-        // mid does not work if bCount < m
-        if bCount < m {
-            left = mid+1
+        // when number of bouquets made were < required m
+        if bouquets < m {
+            // we are too early, wait for next set of days
+            // take search to right
+            left = mid+1            
         } else {
-            // mid works when we were able to make atleast m bouquets ( if not more )
-            // save this as potential answer, and continue searching left because we can smallest such day
             ans = mid
             right = mid-1
         }
     }
     return ans
+
 }
-
-
-// time: o(logMaxDay * n)
-// space = o(1)
 // func minDays(bloomDay []int, m int, k int) int {
-//     n := len(bloomDay)
+//     /*
+//         - we need to make m bouquets
+//         - for 1 bouquets, we need k flowers
+//         - these k flowers must be adjacent flowers
+//         - we are given what day a flower blooms, only then we can pick
+//         - we want to find what day ( smallest possible day ) we can make m bouquets
+//     */
 
-//     left := 1 // earliest possible day we can make m bouquets
-//     right := 0 // last possible day we can make m bouquets
-//     for i := 0; i < n; i++ { right = max(right, bloomDay[i]) }
-
-//     ans := -1
-//     for left <= right {
-//         // day to evaluate
-//         mid := left + (right-left)/2
-//         bCount := 0
-//         fCount := 0
-//         for i := 0; i < n; i++ {
-//             if bloomDay[i] <= mid {
-//                 fCount++
-//                 if fCount == k {
-//                     bCount++
-//                     fCount = 0
-//                 }
-//             } else {
-//                 fCount = 0
-//             }
-//         }
-
-//         // when does mid not work?
-//         // mid does not work if bCount < m
-//         if bCount < m {
-//             left = mid+1
-//         } else {
-//             // mid works when we were able to make atleast m bouquets ( if not more )
-//             // save this as potential answer, and continue searching left because we can smallest such day
-//             ans = mid
-//             right = mid-1
-//         }
+//     // we can start from the last possible day and keep going back from that day
+//     // we know on the last day, everything is usable, therefore for sure we can make m bouquets
+//     // so lets start from that day, and save the day that worked as our answer
+//     // then day--, until a day that no longer works
+//     // time = o(maxDay * n)
+//     day := -1
+//     // time = o(n)
+//     for i := 0; i < len(bloomDay); i++ {
+//         day = max(day, bloomDay[i])
 //     }
-//     return ans
-// }
-
-// time = o(n) + o(maxDay * n)
-// space = o(1)
-// func minDays(bloomDay []int, m int, k int) int {
-//     n := len(bloomDay)
-//     day := 1
-//     lastPossibleDay := 0
-//     for i := 0; i < n; i++ {
-//         lastPossibleDay = max(lastPossibleDay, bloomDay[i])
-//     }
-//     for day <= lastPossibleDay {
-//         bCount := 0
-//         fCount := 0
-//         for i := 0; i < n; i++ {
+//     res := -1
+//     // time = o(maxDay * n)
+//     for day >= 1 {
+//         flowers := 0
+//         b := 0
+//         for i := 0; i < len(bloomDay); i++ {
 //             if bloomDay[i] <= day {
-//                 fCount++
-//                 if fCount == k {
-//                     bCount++
-//                     fCount = 0
-//                     if bCount == m {return day}
-//                 }
+//                 flowers++
+//                 if flowers == k {b++; flowers = 0}
 //             } else {
-//                 fCount = 0
+//                 flowers = 0
 //             }
 //         }
-//         day++
+//         if b >= m {res = day; day--; continue}
+//         break
 //     }
-//     return -1
+//     return res
+    
 // }
