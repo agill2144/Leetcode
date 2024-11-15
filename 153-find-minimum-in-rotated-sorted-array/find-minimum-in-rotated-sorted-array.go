2739f2c99272d10dc9311d83e52f4b2e414321b5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-15 10:51:34 -0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (34.25%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -1,31 +1,6 @@
-/*
-    rotated sorted array pattern #2
-    - find the sorted half
-    - min always lies in the unsorted half
-        - unless curr window is already sorted
-*/
 func findMin(nums []int) int {
-    n := len(nums)
-    if n == 1 {return nums[0]}
-    left := 0
-    right := n-1
-    for left <= right {
-        mid := left + (right-left)/2
-        if nums[left] < nums[right] {
-            return nums[left]
-        }
-        
-        // when is mid our answer ?
-        // when mid element is smaller than 2 adj neighbors
-        if (mid == 0 || nums[mid] < nums[mid-1]) && (mid == n-1 || nums[mid] < nums[mid+1]) {
-            return nums[mid]
-        }
-
-        if nums[left] <= nums[mid] {
-            left = mid+1
-        } else {
-            right = mid-1
-        }
+    for i := 1; i < len(nums); i++ {
+        if nums[i] < nums[i-1] {return nums[i]}
     }
-    return -1
+    return nums[0]
 }
