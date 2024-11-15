781ded7107922fd6e0b61428dffb0ee8c787d83b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-15 12:38:27 -0800

    Time: 4 ms (33.53%) | Memory: 3.9 MB (25.44%) - LeetSync

--- 69-sqrtx/sqrtx.go
+++ 69-sqrtx/sqrtx.go
@@ -2,7 +2,7 @@ func mySqrt(x int) int {
     // right most on left side of x
     // x = 8; right most is 2 because 3*3 is 9 and thats > x
     left := 1
-    right := x
+    right := (x/2)+1
     ans := 0
     for left <= right {
         mid := left + (right-left)/2
