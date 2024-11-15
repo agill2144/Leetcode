58c93fb97a41b27557e7bfe236194235672962f0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-15 11:26:10 -0800

    Time: 0 ms (100.00%) | Memory: 4.9 MB (9.68%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
@@ -1,3 +1,23 @@
+func findMin(nums []int) int {
+    n := len(nums)
+    left := 0
+    right := n-1
+    ans := math.MaxInt64
+    for left <= right {
+        mid := left + (right-left)/2
+        if nums[mid] == nums[left] && nums[mid] == nums[right] {
+            ans = min(ans, nums[mid])
+            left++; right--
+        }else if nums[left] <= nums[mid] {
+            ans = min(ans, nums[left])
+            left = mid+1
+        } else {
+            ans = min(ans, nums[mid])
+            right = mid-1
+        }
+    }
+    return ans
+}
 /*
     approach: linear scan
     - as soon as asc order breaks
@@ -5,9 +25,9 @@
     time = o(n)
     space = o(1)
 */
-func findMin(nums []int) int {
-    for i := 1; i < len(nums); i++ {
-        if nums[i] < nums[i-1] {return nums[i]}
-    }
-    return nums[0]
-}
+// func findMin(nums []int) int {
+//     for i := 1; i < len(nums); i++ {
+//         if nums[i] < nums[i-1] {return nums[i]}
+//     }
+//     return nums[0]
+// }
