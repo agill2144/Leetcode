972355228741df65406df10567f6ec31f9741d9f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-15 19:47:19 -0800

    Time: 5 ms (6.25%) | Memory: 8.5 MB (7.14%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.go
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.go
@@ -1,3 +1,27 @@
+/*
+    approach: sliding window with dq maintaining next greater
+    - monotonically increasing dq storing elements in asc order
+    - top of dq will always be max 
+    - how do we check if our current window is sorted and elements are consec?
+    - thats what dq is literally telling us
+    - if we pushed elements into dq , they would be increasing and and in consec order
+    - therefore when we have a window of size k
+    - and number of elements in our dq == k, then top of dq is our answer
+    - when sliding win from left, if left idx == dq[0], drop dq[0]
+    - what happens when current element is not sorted and or not in consec order?
+    - we would traditionally not push such element into our mono increasing dq
+    - however, it also no longer makes sense to keep any element in the dq anymore
+    - because elements in dq must be consec and contagious
+    - if we ran into a element that breaks the consec and sorted nature, we have to drop the entire dq
+    - and form a new one!
+    - so when current element is not consec / sorted when compared with top of dq
+    - instead of just not pushing to dq, we will drop all elements from current dq
+    - and start forming a new consec chain starting from curr element
+    - therefore reset dq with just current element
+
+    time = o(n)
+    space = o(n)
+*/
 func resultsArray(nums []int, k int) []int {
     dq := []int{0} // idx
     n := len(nums)
