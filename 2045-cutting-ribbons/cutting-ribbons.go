6716d2cd8bf1733e399044da9903f4451c8970b3
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-16 10:49:52 -0800

    Time: 24 ms (62.50%) | Memory: 10.6 MB (20.00%) - LeetSync

--- 2045-cutting-ribbons/cutting-ribbons.go
+++ 2045-cutting-ribbons/cutting-ribbons.go
@@ -1,25 +1,38 @@
 func maxLength(ribbons []int, k int) int {
     left := 1
-    right := 0
-    for i := 0; i < len(ribbons); i++ {
-        right = max(right, ribbons[i])
-    }
-
+    right := -1
+    for i := 0; i < len(ribbons); i++ { right = max(right, ribbons[i]) }
     ans := 0
     for left <= right {
         mid := left + (right-left)/2
-        cutSize := mid
         count := 0
-        for i := 0 ; i < len(ribbons); i++ {
-            count += int(math.Floor(float64(ribbons[i])/float64(cutSize)))
+        for j := 0; j < len(ribbons); j++ {
+            count += (ribbons[j]/mid)
         }
-
-        if count < k {
-            right = mid-1
-        } else {
-            ans = cutSize
+        if count >= k {
+            ans = mid
             left = mid+1
+        } else {
+            right = mid-1
         }
     }
     return ans
 }
+
+// func maxLength(ribbons []int, k int) int {
+//     start := 1
+//     end := -1
+//     for i := 0; i < len(ribbons); i++ {
+//         end = max(end, ribbons[i])
+//     }
+//     ans := 0
+//     for i := start ; i <= end; i++ {
+//         count := 0
+//         for j := 0; j < len(ribbons); j++ {
+//             count += (ribbons[j]/i)
+//         }
+//         if count >= k {ans = i; continue}
+//         break
+//     }
+//     return ans
+// }
