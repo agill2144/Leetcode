6daafe8c7d26bfa8ba99ce7519bbb66140471eff
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-16 10:15:28 -0800

    Time: 5 ms (65.57%) | Memory: 8.7 MB (15.97%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.go
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.go
@@ -1,65 +1,29 @@
 func shipWithinDays(weights []int, days int) int {
-    left := math.MinInt64
+    left := 0
     right := 0
     for i := 0; i < len(weights); i++ {
-        right += weights[i]
         left = max(left, weights[i])
+        right += weights[i]
     }
-
-    // binary search on answers ( weight is what we are looking for )
     ans := -1
     for left <= right {
-
         mid := left + (right-left)/2
         rSum := 0
-        numDays := 1
-        for j := 0; j < len(weights); j++ {
-            rSum += weights[j]
+        day := 1
+        for i := 0; i < len(weights); i++ {
+            rSum += weights[i]
             if rSum > mid {
-                numDays++
-                rSum = weights[j]
+                rSum = weights[i]
+                day++
             }
         }
-
-        // when does mid as capacity for the ship not work ?
-        // when it took more than $days to ship all packages
-        if numDays > days {
-            // increase the weight
-            left = mid+1
-        } else {
-            // save this as potential ans
+        if day <= days {
             ans = mid
-            // keep searching left since we want smallest such ans
             right = mid-1
+        } else {
+            left = mid+1
         }
     }
     return ans
+
 }
-
-// time = o((sum-max+1) * n)
-// space = o(1)
-// func shipWithinDays(weights []int, days int) int {
-//     start := math.MinInt64
-//     end := 0
-//     for i := 0; i < len(weights); i++ {
-//         end += weights[i]
-//         start = max(start, weights[i])
-//     }
-
-//     for i := start; i <= end; i++ {
-
-//         cap := i
-//         rSum := 0
-//         numDays := 1
-//         for j := 0; j < len(weights); j++ {
-//             rSum += weights[j]
-//             if rSum > cap {
-//                 numDays++
-//                 rSum = weights[j]
-//             }
-//         }
-
-//         if numDays <= days {return cap}
-//     }
-//     return -1
-// }
