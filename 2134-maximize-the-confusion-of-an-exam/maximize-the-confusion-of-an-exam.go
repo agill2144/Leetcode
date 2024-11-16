5a9e828b33319cfac24483b0a3860def5eaa2554
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-15 16:06:53 -0800

    Time: 2 ms (85.71%) | Memory: 7.1 MB (57.58%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.go
@@ -0,0 +1,24 @@
+func maxConsecutiveAnswers(answerKey string, k int) int {
+    ts := 0
+    fs := 0
+    maxCount := 0
+    left := 0
+    maxSize := 0
+    for i := 0; i < len(answerKey); i++ {
+        if answerKey[i] == 'T' {ts++}
+        if answerKey[i] == 'F' {fs++}
+        maxCount = max(maxCount, max(ts, fs))
+        size := i-left+1
+        smallerCount := size-maxCount
+        for smallerCount > k {
+            if answerKey[left] == 'T' {ts--}
+            if answerKey[left] == 'F' {fs--}
+            maxCount = max(maxCount, max(ts, fs))
+            left++
+            size = i-left+1
+            smallerCount = size-maxCount                                
+        }
+        maxSize = max(maxSize, i-left+1)
+    }
+    return maxSize
+}
