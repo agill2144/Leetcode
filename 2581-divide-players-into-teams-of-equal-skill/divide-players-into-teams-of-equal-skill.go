f001c7fd94cd308370e438cf5998608856105b78
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-18 13:49:14 -0800

    Time: 13 ms (9.52%) | Memory: 9.6 MB (13.04%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.go
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.go
@@ -1,23 +1,20 @@
 func dividePlayers(skill []int) int64 {
     n := len(skill)
-    total := 0
+    teams := n/2
+    sum := 0
     freq := map[int]int{}
-    for i := 0; i < len(skill); i++ {
-        total += skill[i]
+    for i := 0; i < n; i++ {
+        sum += skill[i]
         freq[skill[i]]++
     }
-    numTeams := n/2
-    if total % numTeams != 0 {return int64(-1)}
-    var totalChem int64 = 0
-    skillPerTeam := total / numTeams
-    for i := 0; i < n; i++ {
+    if sum % teams != 0 {return int64(-1)}
+    skillPerTeam := sum / teams
+    var total int64 = 0
+    for i := 0; i < len(skill); i++ {
         diff := skillPerTeam - skill[i]
-        c := freq[diff]
-        if c == 0 {return -1}
-        freq[diff]--
-        if c == 1 {delete(freq, diff)}
-        totalChem += (int64(skill[i]) * int64(diff) )
+        if freq[diff] == 0 {return int64(-1)}
+        freq[diff]--; if freq[diff] == 0 {delete(freq, diff)}
+        total += (int64(skill[i]) * int64(diff))
     }
-
-    return totalChem/2
+    return total/2
 }
