498b0e62921e17da45f69274e7a908b67f829d9d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-18 13:15:25 -0800

    Time: 240 ms (100.00%) | Memory: 11.7 MB (100.00%) - LeetSync

--- 2702-maximum-number-of-integers-to-choose-from-a-range-ii/maximum-number-of-integers-to-choose-from-a-range-ii.go
+++ 2702-maximum-number-of-integers-to-choose-from-a-range-ii/maximum-number-of-integers-to-choose-from-a-range-ii.go
@@ -7,6 +7,15 @@ func maxCount(banned []int, n int, maxSum int64) int {
     for left <= right {
         mid := left + (right-left)/2
         sum := int64(mid * (mid+1)/2)
+        // why this count ? 
+        // we want to know number of numbers we were able to pick
+        // we dont want the the mid number itself
+        // sum = sum of all number from 1 to mid
+        // now we need to remove banned numbers from the sum
+        // therefore we are removing the number of digits that made the sum from 1 to mid
+        // to keep track of num of numbers that are part of the sum , we use count
+        // then we loop over the banned set to remove numbers from the sum that are banned 
+        // in doing this, number of digits also go down, therefore count--
         count := mid
         for k,_ := range set {if k <= int64(mid) {sum -= k; count--}}
         if  sum <= maxSum {
