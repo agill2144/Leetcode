ee199026df27cf049167ca69a303c3ca5c2088b6
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-17 19:31:15 -0800

    Time: 0 ms (100.00%) | Memory: 3.9 MB (6.39%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,22 +1,24 @@
 func myPow(x float64, n int) float64 {
+    // x = base
+    // n = exp
     if n < 0 {
         x = 1/x
         n *= -1
     }
-
     var dfs func(base float64, exp int) float64
     dfs = func(base float64, exp int) float64 {
         // base
-        if exp == 1 {return base}
-        if exp == 0 {return 1.0}
+        if exp <= 1 {
+            if exp == 1 {return base}
+            return 1.0
+        }
 
         // logic
         res := dfs(base, exp/2)
         res *= res
-        if exp % 2 != 0 {
-            res *= base
-        }
+        if exp % 2 != 0 {res *= base}
         return res
     }
     return dfs(x, n)
+    
 }
