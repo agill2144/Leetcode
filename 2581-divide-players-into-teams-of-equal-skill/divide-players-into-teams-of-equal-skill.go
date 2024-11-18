db57dd17fb352058d49756483ffede50b2bc0e9f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-18 13:53:13 -0800

    Time: 21 ms (7.14%) | Memory: 9.3 MB (16.39%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.go
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.go
@@ -2,7 +2,7 @@ func dividePlayers(skill []int) int64 {
     n := len(skill)
     teams := n/2
     sum := 0
-    freq := make([]int, 2002)
+    freq := map[int]int{}
     for i := 0; i < n; i++ {
         sum += skill[i]
         freq[skill[i]]++
@@ -13,8 +13,13 @@ func dividePlayers(skill []int) int64 {
     for i := 0; i < len(skill); i++ {
         diff := skillPerTeam - skill[i]
         if freq[diff] == 0 {return int64(-1)}
-        freq[diff]--
+        freq[diff]--; if freq[diff] == 0 {delete(freq, diff)}
         total += (int64(skill[i]) * int64(diff))
     }
+    // why the divide by 2 here?
+    // because every compliment search in the 2nd pass is added twice
+    // skill = 6
+    // and we have a pair 2+4, then 4+2 will come up too
+    // therefore dupes, therefore each compliment is added twice, therefore removing by dividing the total by 2
     return total/2
 }
