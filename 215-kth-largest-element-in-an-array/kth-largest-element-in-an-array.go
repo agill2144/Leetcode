c215ec24bbdc8cb26976d6c6f956ba7c173aea8e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-19 10:11:18 -0800

    Time: 23 ms (69.02%) | Memory: 10.4 MB (26.44%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
@@ -1,32 +1,12 @@
-// bucket sort / counting sort
-// use map as a bucket instead of an array
-// why ? we have negative values, and we cannot easily plot/map a negative value to a idx in an array
-// therefore using map
-// time = o(n) + o(n) + o(max-min+1) 
-// space = o(n)
 func findKthLargest(nums []int, k int) int {
     n := len(nums)
-    if k > n {return -1}
-
-    start := math.MaxInt64
-    end := math.MinInt64
-    for i := 0; i < n; i++ {
-        start = min(start, nums[i])
-        end = max(end, nums[i])
-    }
-
-    bucket := map[int]int{}
-    for i := 0; i < n; i++ {
-        bucket[nums[i]]++
-    }
-
-    idx := -1
-    targetIdx := n-k
-    for i := start; i <= end; i++ {
-        count, ok := bucket[i]
-        if !ok {continue}
-        idx += count
-        if idx >= targetIdx {return i}
-    }
-    return -1
+    sort.Ints(nums)
+    return nums[n-k]
 }
+
+
+/*
+    k = 4
+
+    1,2,2,3,3,4,5,5,6
+*/
