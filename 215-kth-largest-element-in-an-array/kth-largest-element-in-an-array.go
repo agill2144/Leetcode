172bc16d2ca2647a2a26b592c71758675a9c334d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-19 10:39:31 -0800

    Time: 48 ms (30.98%) | Memory: 12.3 MB (10.00%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
@@ -1,12 +1,32 @@
 func findKthLargest(nums []int, k int) int {
     n := len(nums)
-    sort.Ints(nums)
-    return nums[n-k]
+    mn := &minHeap{items: []int{}}
+    for i := 0; i < n; i++ {
+        heap.Push(mn, nums[i])
+        if mn.Len() > k {heap.Pop(mn)}
+    }
+    return mn.items[0]
 }
 
 
-/*
-    k = 4
+type minHeap struct {
+	items []int
+}
 
-    1,2,2,3,3,4,5,5,6
-*/
+func (m *minHeap) Less(i, j int) bool {
+	return m.items[i] < m.items[j]
+}
+func (m *minHeap) Swap(i, j int) {
+	m.items[i], m.items[j] = m.items[j], m.items[i]
+}
+func (m *minHeap) Len() int {
+	return len(m.items)
+}
+func (m *minHeap) Push(x interface{}) {
+	m.items = append(m.items, x.(int))
+}
+func (m *minHeap) Pop() interface{} {
+	out := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return out
+}
