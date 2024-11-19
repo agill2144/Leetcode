b5371601534ecce04ec20dfe35a492b36bcff800
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-18 16:38:40 -0800

    Time: 68 ms (5.13%) | Memory: 17.9 MB (5.55%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.go
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.go
@@ -6,20 +6,21 @@ func maximumSubarraySum(nums []int, k int) int64 {
     for i := 0; i < len(nums); i++ {
         sum += int64(nums[i])
         freq[nums[i]]++
-        for len(freq) < i-left+1 {
-            sum -= int64(nums[left])
-            freq[nums[left]]--
-            if freq[nums[left]] == 0 {delete(freq, nums[left])}
-            left++
-        }
         if i-left+1 == k {
-            maxSum = max(maxSum, sum)
-            sum -= int64(nums[left])
-            freq[nums[left]]--
-            if freq[nums[left]] == 0 {delete(freq, nums[left])}
-            left++
-
+            if len(freq) == k {
+                maxSum = max(maxSum, sum)
+                sum -= int64(nums[left])
+                freq[nums[left]]--
+                if freq[nums[left]] == 0 {delete(freq, nums[left])}
+                left++
+            } else {
+                sum -= int64(nums[left])
+                freq[nums[left]]--
+                if freq[nums[left]] == 0 {delete(freq, nums[left])}
+                left++
+            }   
         }
+        
     }
     return maxSum
 }
