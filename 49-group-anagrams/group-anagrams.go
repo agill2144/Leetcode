c59577345f665db3fa88e71b66834f4c38e08448
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-18 19:20:06 -0800

    Time: 12 ms (49.64%) | Memory: 9 MB (54.78%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,20 +1,14 @@
 func groupAnagrams(strs []string) [][]string {
-    groups := map[float64][]string{}
+    groups := map[string][]string{}
     for i := 0; i < len(strs); i++ {
-        hashVal := hash(strs[i])
-        groups[hashVal] = append(groups[hashVal], strs[i])
+        word := strings.Split(strs[i], "")
+        sort.Strings(word)
+        sortStr := strings.Join(word,"")
+        groups[sortStr] = append(groups[sortStr], strs[i])
     }
     out := [][]string{}
-    for _, v := range groups { out = append(out, v)}
-    return out
-}
-
-func hash(key string) float64 {
-    primes := []float64{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101}
-    prod := 1.0
-    for i := 0; i < len(key); i++ {
-        idx := int(key[i] - 'a')
-        prod *= primes[idx]
+    for _, v := range groups {
+        out = append(out, v)
     }
-    return prod
+    return out
 }
