f159ce4ce9c2be42a0d3595796a5c4325557f49d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-20 11:29:50 -0800

    Time: 0 ms (100.00%) | Memory: 3.8 MB (51.24%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.go
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.go
@@ -1,33 +1,16 @@
-/*
-    approach: 2 pass
-    - because the array was sorted at one point, then rotated
-    - we need to find the breaking point
-    - that is, at what idx, does the asc order break from left to right
-    - we need to find the first such idx
-    - once we found the breaking idx, then a simple loop around the whole array
-    - starting from breaking idx, should be in asc order
-    - example; [3,4,5,1,2]
-    -                 ^ breaking idx
-    - now everything after that breaking idx ( wrapped around the whole array )
-    - should be in asc / sorted order
-    time = o(n)
-    space = o(1)
-*/
 func check(nums []int) bool {
-    // find the breaking idx
+    n := len(nums)
+    if n <= 1 {return true}
     idx := -1
-    for i := 0; i < len(nums); i++ {
+    for i := 0; i < n; i++ {
         if i == 0 {continue}
         if nums[i] < nums[i-1] {idx = i; break}
     }
     if idx == -1 {return true}
-    n := len(nums)
-    // run a loop wrapping around the whole array
-    // and validate sorted order
     start := idx+1
     end := idx
-    for start % n != end {
-        if nums[(start-1)%n] <= nums[start%n] {start++} else {break}
+    for (start%n) != end {
+        if nums[start%n] >=  nums[(start-1)%n] {start++} else {return false}
     }
-    return (start%n) == end
+    return true
 }
