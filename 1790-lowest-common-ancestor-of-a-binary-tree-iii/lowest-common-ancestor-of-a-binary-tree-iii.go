0b65d6e25b3a9bd055be7274ac9ba13689457ad0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-19 20:35:48 -0800

    Time: 2 ms (63.10%) | Memory: 8.3 MB (19.19%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
@@ -7,26 +7,37 @@
  *     Parent *Node
  * }
  */
-
 func lowestCommonAncestor(p *Node, q *Node) *Node {
-    getH := func(r *Node) int {
-        h := 0
-        for r != nil {
-            r = r.Parent
-            h++
-        }
-        return h
+    if p == nil || q == nil {return nil}
+    p1 := p; p2 := q
+    for p1 != p2 {
+        if p1 != nil { p1 = p1.Parent } else { p1 = q }
+        if p2 != nil { p2 = p2.Parent } else { p2 = p }
     }
-    ph := getH(p)
-    qh := getH(q)
-    for ph > qh {p = p.Parent; ph--}
-    for qh > ph {q = q.Parent; qh--}
-    for p != q {
-        p = p.Parent
-        q = q.Parent
-    }
-    return p
+    return p1
 }
+
+// func lowestCommonAncestor(p *Node, q *Node) *Node {
+//     getH := func(r *Node) int {
+//         h := 0
+//         for r != nil {
+//             r = r.Parent
+//             h++
+//         }
+//         return h
+//     }
+//     // time = o(h) + o(h) + o(h)
+//     // space = o(1)
+//     ph := getH(p)
+//     qh := getH(q)
+//     for ph > qh {p = p.Parent; ph--}
+//     for qh > ph {q = q.Parent; qh--}
+//     for p != q {
+//         p = p.Parent
+//         q = q.Parent
+//     }
+//     return p
+// }
 
 // func lowestCommonAncestor(p *Node, q *Node) *Node {
 //     if p == nil || q == nil {return nil}
