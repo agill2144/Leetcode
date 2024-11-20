d00c869683c8a7c63794307bd058f564f771ef7b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-20 12:59:26 -0800

    Time: 0 ms (100.00%) | Memory: 4.4 MB (46.78%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.go
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.go
@@ -1,45 +1,19 @@
-func findKthPositive(arr []int, k int) int {
-    if k < arr[0] {return k}
+func findKthPositive(nums []int, k int) int {
+    n := len(nums)
     left := 0
-    right := len(arr)-1
+    right := n-1
     for left <= right {
         mid := left + (right-left)/2
-        cIdx := arr[mid]-1
-        missing := cIdx - mid
-        if missing >= k {
-            right = mid-1
-        } else {
+        cIdx := nums[mid]-1
+        missOnLeft := cIdx - mid
+        if missOnLeft < k {
             left = mid+1
+        } else {
+            right = mid-1
         }
     }
-    // we have already handled if missing number is to the left of the array
-    // k < arr[0]
-    // now this means, the missing number is either in the middle or to the right side of array
-    missing := (arr[right]-1)-right
-    rem := k-missing
-    return arr[right]+rem
+    if right < 0 {return k}
+    missOnLeft := (nums[right]-1) - right
+    k -= missOnLeft
+    return nums[right] + k
 }
-
-
-
-/*
-    approach: linear scan with 2 ptrs
-    time = o(n)
-    space = o(1)
-*/
-// func findKthPositive(arr []int, k int) int {
-//     ptr := 1
-//     ms := 0
-//     i := 0
-//     for i < len(arr) {
-//         if arr[i] == ptr {
-//             i++; ptr++
-//         } else {
-//             ms++
-//             if ms == k {return ptr}
-//             ptr++
-//         }
-//     }
-//     rem := k-ms
-//     return ptr+rem-1
-// }
