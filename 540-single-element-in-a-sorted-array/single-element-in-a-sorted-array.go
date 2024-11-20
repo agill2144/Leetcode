a722c0771e74797fc3c8e4021140e094fb993793
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-20 12:50:42 -0800

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.72%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.go
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.go
@@ -1,19 +1,18 @@
 func singleNonDuplicate(nums []int) int {
-    n := len(nums)
     left := 0
-    right := n-1
+    right := len(nums)-1
     for left <= right {
         mid := left + (right-left)/2
-        if (mid == 0 || nums[mid] != nums[mid-1]) && (mid == n-1 || nums[mid] != nums[mid+1]) {return nums[mid]}
+        if (mid == 0 || nums[mid] != nums[mid-1]) && (mid == len(nums)-1 || nums[mid] != nums[mid+1]) {
+            return nums[mid]
+        }
         if mid % 2 == 0 {
-            // mid = even idx
-            if (mid == n-1 || nums[mid] == nums[mid+1]) {
+            if mid == len(nums)-1 || nums[mid] == nums[mid+1] {
                 left = mid+1
             } else {
                 right = mid-1
             }
         } else {
-            // mid = odd idx
             if (mid == 0 || nums[mid] == nums[mid-1]) {
                 left = mid+1
             } else {
@@ -23,9 +22,3 @@ func singleNonDuplicate(nums []int) int {
     }
     return -1
 }
-
-/*
-     0 1 2 3 4  5   6 
-    [3,3,7,7,10,10,11]
-
-*/
