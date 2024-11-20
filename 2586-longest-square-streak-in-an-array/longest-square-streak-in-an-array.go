a16275dd6915345ac65263351a77dabc26d4941d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-20 13:12:42 -0800

    Time: 56 ms (54.64%) | Memory: 11.7 MB (68.57%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.go
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.go
@@ -1,53 +1,20 @@
 func longestSquareStreak(nums []int) int {
-    sort.Ints(nums)
-    maxStreak := 0
-    dirty := map[int]bool{}
-    for i := 0; i < len(nums); i++ {
-        curr := nums[i]*nums[i]
-        count := 1
-        for search(nums, i, curr)  && !dirty[curr] {
-            dirty[curr] = true
-            count++
-            curr *= curr
+    set := map[int]bool{}
+    n := len(nums)
+    maxN := math.MinInt64
+    for i := 0; i < n; i++ {set[nums[i]] = true; maxN = max(maxN, nums[i])}
+    maxSize := 0
+    for i := 0; i < n; i++ {
+        curr := nums[i]
+        start := curr
+        size := 1
+        for set[start*start] && start*start <= maxN {
+            start *= start
+            size++
         }
-        maxStreak = max(maxStreak, count)
+        maxSize = max(maxSize, size)
     }
-    if maxStreak == 1 {return -1}
-    return maxStreak
-}
-func search(nums []int, left, target int) bool {
-    right := len(nums)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        if nums[mid] == target {return true}
-        if target > nums[mid] {
-            left = mid+1
-        } else {
-            right = mid-1
-        }
-    }
-    return false
+    if maxSize == 1 {maxSize = -1}
+    return maxSize
 }
 
-// func longestSquareStreak(nums []int) int {
-//     set := map[int]bool{}
-//     start := math.MaxInt64
-//     end := math.MinInt64
-//     for i := 0; i < len(nums); i++ {
-//         set[nums[i]] = true
-//         start = min(start, nums[i])
-//         end = max(end, nums[i])
-//     }
-//     maxStreak := 1
-//     for i := 0; i < len(nums); i++ {
-//         curr := nums[i]
-//         count := 0
-//         for set[curr] && curr < int(math.Pow(10.0, 5)) {
-//             curr = curr * curr
-//             count++
-//         }
-//         maxStreak = max(maxStreak, count)
-//     }
-//     if maxStreak == 1 {return -1}
-//     return maxStreak
-// }
