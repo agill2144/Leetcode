05a2c136052e1616aa6f970cd0c4d631f296a256
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-21 12:07:30 -0800

    Time: 3 ms (56.67%) | Memory: 7.8 MB (45.23%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
@@ -7,61 +7,23 @@
  *     Parent *Node
  * }
  */
+
 func lowestCommonAncestor(p *Node, q *Node) *Node {
-    if p == nil || q == nil {return nil}
-    p1 := p; p2 := q
-    for p1 != p2 {
-        if p1 != nil { p1 = p1.Parent } else { p1 = q }
-        if p2 != nil { p2 = p2.Parent } else { p2 = p }
+    getH := func(r *Node) int {
+        h := 0
+        for r != nil {
+            h++
+            r = r.Parent
+        }
+        return h
     }
-    return p1
+    ph := getH(p)
+    qh := getH(q)
+    for ph > qh {p = p.Parent; ph--}
+    for qh > ph {q = q.Parent; qh--}
+    for p != q {
+        p = p.Parent
+        q = q.Parent
+    }
+    return p
 }
-
-// func lowestCommonAncestor(p *Node, q *Node) *Node {
-//     getH := func(r *Node) int {
-//         h := 0
-//         for r != nil {
-//             r = r.Parent
-//             h++
-//         }
-//         return h
-//     }
-//     // time = o(h) + o(h) + o(h)
-//     // space = o(1)
-//     ph := getH(p)
-//     qh := getH(q)
-//     for ph > qh {p = p.Parent; ph--}
-//     for qh > ph {q = q.Parent; qh--}
-//     for p != q {
-//         p = p.Parent
-//         q = q.Parent
-//     }
-//     return p
-// }
-
-// func lowestCommonAncestor(p *Node, q *Node) *Node {
-//     if p == nil || q == nil {return nil}
-//     pPaths := []*Node{}
-//     qPaths := []*Node{}
-//     // h = height of tree
-//     // p and q are some leaf nodes
-//     // p = o(h)
-//     // q = o(q)
-//     // ph = height from p to root
-//     // qh = height from q to root
-//     // time = o(ph) + o(qh) + o(ph+qh)
-//     // space = o(ph) + o(qh)
-//     for p != nil {pPaths = append(pPaths, p); p = p.Parent}
-//     for q != nil {qPaths = append(qPaths, q); q = q.Parent}
-//     p1, p2 := len(pPaths)-1, len(qPaths)-1
-//     var out *Node
-//     for p1 >= 0 && p2 >= 0 {
-//         if pPaths[p1] != qPaths[p2] {
-//             break
-//         }
-//         out = pPaths[p1]
-//         p1--
-//         p2--
-//     }
-//     return out
-// }
