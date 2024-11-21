f8f4e7f418f1b73623ca59ab707ccb3e72924897
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-20 19:56:06 -0800

    Time: 93 ms (12.50%) | Memory: 22.2 MB (7.14%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.go
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.go
@@ -1,21 +1,22 @@
 func countUnguarded(m int, n int, guards [][]int, walls [][]int) int {
+    if len(guards) == 0 { return (m*n)-len(walls)  }
     wall := 1
     guard := 2
     visited := 3
-    if len(guards) == 0 {
-        return (m*n)-len(walls)
-    }
     matrix := make([][]int, m)
+    total := m*n
     for i := 0; i < m; i++ {matrix[i] = make([]int, n)}
     for i := 0; i < len(walls); i++ {
         r, c := walls[i][0], walls[i][1]
         matrix[r][c] = wall
+        total--
     }
     q := [][]int{}
     for i := 0; i < len(guards); i++ {
         r, c := guards[i][0], guards[i][1]
         matrix[r][c] = guard
         q = append(q, guards[i])
+        total--
     }
     dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
     for len(q) != 0 {
@@ -27,18 +28,14 @@ func countUnguarded(m int, n int, guards [][]int, walls [][]int) int {
             nr := cr + dir[0]
             nc := cc + dir[1]
             for nr >= 0 && nr < m && nc >= 0 && nc < n && (matrix[nr][nc] != guard && matrix[nr][nc] != wall) {
+                if matrix[nr][nc] != visited {total--}
                 matrix[nr][nc] = visited
                 nr += dir[0]
-                nc += dir[1]
+                nc += dir[1]                
             }       
         }
     }
-    count := 0
-    for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
-            if matrix[i][j] == 0 {count++}
-        }
-    }
+    
     /*
         wall := 1
         guard := 2
@@ -50,5 +47,5 @@ func countUnguarded(m int, n int, guards [][]int, walls [][]int) int {
         [3 3 1 2 3 3] 
         [3 3 0 3 0 0]
     */
-    return count
+    return total
 }
