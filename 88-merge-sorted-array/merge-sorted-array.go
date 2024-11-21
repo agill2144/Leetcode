7d83c34b6d7c68f1e1e8d4002506f79a4da52388
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-21 13:41:17 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (39.43%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.go
+++ 88-merge-sorted-array/merge-sorted-array.go
@@ -1,20 +1,18 @@
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-    merged := []int{}
-    n1 := 0
-    n2 := 0
-    for n1 < m && n2 < len(nums2) {
-        if nums1[n1] < nums2[n2] {
-            merged = append(merged, nums1[n1])
-            n1++
+    ptr := len(nums1)-1
+    n1 := m-1
+    n2 := n-1
+    for n1 >= 0 && n2 >= 0 {
+        if nums1[n1] > nums2[n2] {
+            nums1[ptr] = nums1[n1]
+            n1--
         } else {
-            merged = append(merged, nums2[n2])
-            n2++
+            nums1[ptr] = nums2[n2]
+            n2--
         }
+        ptr--
     }
-    for n1 < m {merged = append(merged, nums1[n1]); n1++}
-    for n2 < len(nums2) {merged = append(merged, nums2[n2]); n2++}
-    for i := 0; i < len(merged); i++ {
-        nums1[i] = merged[i]
-    }
+    for n1 >= 0 {nums1[ptr] = nums1[n1]; n1--; ptr--}
+    for n2 >= 0 {nums1[ptr] = nums2[n2]; n2--; ptr--}
     
 }
