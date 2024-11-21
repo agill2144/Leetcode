0b119b33be61d101c6eae1da021073d51a10b948
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-21 10:50:27 -0800

    Time: 0 ms (100.00%) | Memory: 3.7 MB (41.08%) - LeetSync

--- 670-maximum-swap/maximum-swap.go
+++ 670-maximum-swap/maximum-swap.go
@@ -1,31 +1,41 @@
-
+// bring the largest value in smallest unit to the largest unit
+// time = o(n)
+// space = o(n) for nums list + o(9) for idxs array = o(n)
 func maximumSwap(num int) int {
     if num <= 9 {return num}
     numStr := fmt.Sprintf("%v", num)
     nums := []int{}
     for i := 0; i < len(numStr); i++ {nums = append(nums, int(numStr[i]-'0'))}
-    bucket := make([]int, 10) // last seen idxs
-    for i := 0; i < len(bucket); i++ {bucket[i] = -1}
-    // 9991
-    // 1119
-    for i := 0; i < len(nums); i++ {bucket[nums[i]] = i}
+    // last seen idxs; 
+    // map each value to its idx ( val : idx ), and note down its actual idx position from input
+    // if a value shows up again, thats fine, we can override its idx again ( we want the last idx , right most idx )
+    // ex; 989
+    // idx of 9 will be 2 at the end
+    idxs := make([]int, 10) 
+    // pre-fill idxs with -1 ( as if they are not yet found )
+    for i := 0; i < len(idxs); i++ {idxs[i] = -1}
+    for i := 0; i < len(nums); i++ {idxs[nums[i]] = i}
     
-    // 2736
-    // i
-    // 0 1 2 3 4 5 6 7 8 9
-    //     0 2     3 1  
-    //               j
-    
-    done := false
-    for i := 0; i < len(nums) && !done; i++ {
-        for j := len(bucket)-1; j > nums[i]; j-- {
-            if bucket[j] != -1 && bucket[j] > i {
-                nums[bucket[j]], nums[i] = nums[i], nums[bucket[j]]
-                done = true
+    swapped := false
+    for i := 0; i < len(nums) && !swapped; i++ {
+        // for each digit on the higest unit ( from left to right )
+        // check for possible swaps
+        // if we value 2, 3,4, we want highest possible number to swap with
+        // highest possible num is 9
+        // therefore start the idx loop from the back of the array
+        // each idx is a value and the value in the idx array represents the last idx we've seen this value at in the input
+        for j := len(idxs)-1; j > nums[i]; j-- {
+            // if an idx does exist
+            // and its after the ith idx,
+            // then we can swap
+            if idxs[j] != -1 && idxs[j] > i {
+                nums[idxs[j]], nums[i] = nums[i], nums[idxs[j]]
+                swapped = true
                 break
             }
         }
     }
+    // re-construct the number back
     res := 0
     for i := 0; i < len(nums); i++ {
         res = (res * 10) + nums[i]
