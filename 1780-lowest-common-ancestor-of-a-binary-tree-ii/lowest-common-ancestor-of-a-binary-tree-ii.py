284b0ba5900d9a15bd482b3a6b0aefa9cc909283
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-21 12:02:06 -0800

    Time: 151 ms (29.93%) | Memory: 21.3 MB (50.32%) - LeetSync

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -7,11 +7,13 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        p_found = False
-        q_found = False
+        if not p or not q or not root:
+            return None
+        p_exists = False
+        q_exists = False
         def dfs(r: 'TreeNode') -> 'TreeNode':
-            nonlocal p_found
-            nonlocal q_found
+            nonlocal p_exists
+            nonlocal q_exists
             # base
             if not r:
                 return None
@@ -19,18 +21,16 @@ class Solution:
             # logic
             left = dfs(r.left)
             right = dfs(r.right)
-            if r is p or r is q:
-                if r == p:
-                    p_found = True
-                if r == q:
-                    q_found = True
+            if r == p:
+                p_exists = True
+                return r
+            if r == q:
+                q_exists = True
                 return r
             if left and right:
                 return r
-            if left:
-                return left
-            return right
+            return left if left else right
         res = dfs(root)
-        if p_found and q_found:
+        if p_exists and q_exists:
             return res
         return None
