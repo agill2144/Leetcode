35d0c98763f436d1fe460807e658fbf9842fd1db
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-21 11:04:20 -0800

    Time: 0 ms (100.00%) | Memory: 8.3 MB (53.28%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.go
+++ 680-valid-palindrome-ii/valid-palindrome-ii.go
@@ -1,18 +1,18 @@
 func validPalindrome(s string) bool {
-    if len(s) <= 2 {return true}
     left := 0
     right := len(s)-1
     for left < right {
         if s[left] != s[right] {
             return isValid(left+1, right, s) || isValid(left, right-1, s)
         }
-        left++;right--
+        left++
+        right--
     }
     return true
 }
 
-// xyabcbay
 func isValid(left, right int, s string) bool {
+    if right-left+1 <= 1 {return true}
     for left <= right {
         if s[left] != s[right] {return false}
         left++
