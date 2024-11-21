c9319ece083b6f3d9ca2b5f0063f6cac83971e60
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-21 11:06:42 -0800

    Time: 4 ms (88.44%) | Memory: 8.2 MB (63.79%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
@@ -1,65 +1,26 @@
 func minRemoveToMakeValid(s string) string {
-    res := new(strings.Builder)
-    open := 0
+    invalidIdxs := map[int]bool{}
+    st := []int{}
     for i := 0; i < len(s); i++ {
         if s[i] == '(' {
-            open++
-            res.WriteByte(s[i])
+            st = append(st, i)
         } else if s[i] == ')' {
-            if open > 0 {
-                open--
-                res.WriteByte(s[i])
+            if len(st) == 0 {
+                invalidIdxs[i] = true
+                continue
             }
-        } else {
-            res.WriteByte(s[i])
+            st = st[:len(st)-1]
         }
     }
-    s = res.String()
-    res.Reset()
-    close := 0
-    for i := len(s)-1; i >= 0; i-- {
-        if s[i] == ')' {
-            close++
-            res.WriteByte(s[i])
-        } else if s[i] == '(' {
-            if close > 0 {
-                close--
-                res.WriteByte(s[i])
-            }
-        } else {
-            res.WriteByte(s[i])
-        }
+    for len(st) != 0 {
+        top := st[len(st)-1]
+        st = st[:len(st)-1]
+        invalidIdxs[top] = true 
     }
-    s = res.String()
-    res.Reset()
-    for i := len(s)-1; i >= 0; i-- {
+    res := new(strings.Builder)
+    for i := 0; i < len(s); i++ {
+        if invalidIdxs[i] {continue}
         res.WriteByte(s[i])
     }
     return res.String()
 }
-// func minRemoveToMakeValid(s string) string {
-//     st := []int{} // idx of open parans
-//     invalid := map[int]bool{} // idxs of all invalid chars that should not be part of final string
-//     for i := 0; i < len(s); i++ {
-//         if s[i] == '(' {
-//             st = append(st, i)
-//         } else if s[i] == ')' {
-//             if len(st) == 0 {
-//                 invalid[i] = true
-//             } else {
-//                 st = st[:len(st)-1]
-//             }
-//         }
-//     }
-//     for len(st) != 0 {
-//         top := st[len(st)-1]
-//         st = st[:len(st)-1]
-//         invalid[top] = true
-//     }
-//     res := new(strings.Builder)
-//     for i := 0; i < len(s); i++ {
-//         if invalid[i] {continue}
-//         res.WriteByte(s[i])
-//     }
-//     return res.String()
-// }
