ea7268bb2306c60f9356c3c648414c4a0122a604
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-21 16:01:05 -0800

    Time: 0 ms (100.00%) | Memory: 4.4 MB (59.40%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.go
+++ 40-combination-sum-ii/combination-sum-ii.go
@@ -1,40 +1,68 @@
 func combinationSum2(candidates []int, target int) [][]int {
-    freq := map[int]int{}
-    for i := 0; i < len(candidates); i++ {
-        freq[candidates[i]]++
-    }
-    deduped := [][]int{}
-    for k, v := range freq {
-        deduped = append(deduped, []int{k,v})
-    }
+    sort.Ints(candidates)
     out := [][]int{}
-    var dfs func(start int, sum int, path []int)
-    dfs = func(start int, sum int, path []int) {
+    var dfs func(start, sum int, path []int)
+    dfs = func(start, sum int, path []int) {
         // base
         if sum > target {return}
 
-        // logic
         if sum == target {
             newL := make([]int, len(path))
             copy(newL, path)
             out = append(out, newL)
-            return
+            return 
         }
-        for i := start ; i < len(deduped); i++ {
-            if deduped[i][1] > 0 {
-                // action
-                path = append(path, deduped[i][0])
-                sum += deduped[i][0]
-                deduped[i][1]--
-                // recurse
-                dfs(i, sum, path)
-                // backtrack
-                deduped[i][1]++
-                sum -= deduped[i][0]
-                path = path[:len(path)-1]
-            }
+
+        // logic
+        for i := start; i < len(candidates); i++ {
+            if i > start && candidates[i] == candidates[i-1] {continue}
+            sum += candidates[i]
+            path = append(path, candidates[i])
+            dfs(i+1, sum, path)
+            path = path[:len(path)-1]
+            sum -= candidates[i]
         }
     }
     dfs(0,0, nil)
     return out
 }
+// func combinationSum2(candidates []int, target int) [][]int {
+//     freq := map[int]int{}
+//     for i := 0; i < len(candidates); i++ {
+//         freq[candidates[i]]++
+//     }
+//     deduped := [][]int{}
+//     for k, v := range freq {
+//         deduped = append(deduped, []int{k,v})
+//     }
+//     out := [][]int{}
+//     var dfs func(start int, sum int, path []int)
+//     dfs = func(start int, sum int, path []int) {
+//         // base
+//         if sum > target {return}
+
+//         // logic
+//         if sum == target {
+//             newL := make([]int, len(path))
+//             copy(newL, path)
+//             out = append(out, newL)
+//             return
+//         }
+//         for i := start ; i < len(deduped); i++ {
+//             if deduped[i][1] > 0 {
+//                 // action
+//                 path = append(path, deduped[i][0])
+//                 sum += deduped[i][0]
+//                 deduped[i][1]--
+//                 // recurse
+//                 dfs(i, sum, path)
+//                 // backtrack
+//                 deduped[i][1]++
+//                 sum -= deduped[i][0]
+//                 path = path[:len(path)-1]
+//             }
+//         }
+//     }
+//     dfs(0,0, nil)
+//     return out
+// }
