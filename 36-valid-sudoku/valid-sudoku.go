9af6801d376f3e5103f1b9a209a3b9bdfd49021f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-22 12:11:20 -0800

    Time: 2 ms (31.49%) | Memory: 5.4 MB (21.15%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.go
+++ 36-valid-sudoku/valid-sudoku.go
@@ -1,14 +1,15 @@
 func isValidSudoku(board [][]byte) bool {
     n := len(board)
     boxes := map[string]map[byte]bool{}
+    rows := map[int]map[byte]bool{}
+    cols := map[int]map[byte]bool{}
     for i := 0; i < n; i++ {
-        row := map[byte]bool{}
-        col := map[byte]bool{}
+        if rows[i] == nil {rows[i] = map[byte]bool{}}
         for j := 0; j < n; j++ {
             val := board[i][j]
             if val != '.'{
-                if row[val] {return false}
-                row[val] = true
+                if rows[i][val] {return false}
+                rows[i][val] = true
                 r, c := i/3, j/3
                 key := fmt.Sprintf("%v_%v", r, c)
                 if boxes[key] == nil {boxes[key] = map[byte]bool{}}
@@ -16,11 +17,11 @@ func isValidSudoku(board [][]byte) bool {
                 boxes[key][val] = true
 
             }
-
+            if cols[i] == nil {cols[i] = map[byte]bool{}}
             val = board[j][i]
             if val != '.' {
-                if col[val] {return false}
-                col[val] = true
+                if cols[i][val] {return false}
+                cols[i][val] = true
             }
         }
     }
