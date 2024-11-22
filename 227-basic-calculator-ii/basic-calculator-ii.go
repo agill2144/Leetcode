1547994d4959fe1754f32f8ac33aa9404a7a0570
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-21 21:53:05 -0800

    Time: 1 ms (83.22%) | Memory: 5.8 MB (75.86%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.go
+++ 227-basic-calculator-ii/basic-calculator-ii.go
@@ -1,30 +1,33 @@
 func calculate(s string) int {
-    total := 0
-    curr := 0
-    n := len(s)
-    lastContr := 0
-    var op byte = '+'
-    for i := 0; i < n; i++ {
-        if s[i] >= '0' && s[i] <= '9' {
-            curr = curr * 10 + int(s[i]-'0')
-        }
-        if s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || i == n-1 {
-            if op == '+' {
-                total += curr
-                lastContr = curr
-            } else if op == '-' {
-                total -= curr
-                lastContr = -curr
-            } else if op == '*'{
-                total = total - lastContr + (lastContr * curr)
-                lastContr = lastContr * curr
-            } else if op == '/' {
-                total = total - lastContr + (lastContr/curr)
-                lastContr = lastContr / curr
-            }
-            curr = 0
-            op = s[i]
-        }
-    }
-    return total
+	n := len(s)
+	curr := 0
+	var op byte = '+'
+	total := 0
+	lastContr := 0
+	for i := 0; i < n; i++ {
+		char := s[i]
+		if char >= '0' && char <= '9' {
+			curr = curr * 10 + int(char-'0')
+		}
+		if i == n-1 || char == '+' || char == '-' || char == '/' || char == '*' {
+			if op == '+' {
+				total += curr
+				lastContr = +curr
+			} else if op == '-' {
+				total -= curr
+				lastContr = -curr
+			} else if op == '*' {
+				multiRes := lastContr * curr
+				total = total - lastContr + multiRes
+				lastContr = multiRes
+			} else if op == '/' {
+				divRes := lastContr / curr
+				total = total - lastContr + divRes
+				lastContr = divRes
+			}
+			curr = 0
+			op = char
+		}
+	}
+	return total
 }
