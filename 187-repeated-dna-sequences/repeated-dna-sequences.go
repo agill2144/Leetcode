59dccafb4f1f634e2a4b1f7d155b35704e2074bb
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-22 12:00:36 -0800

    Time: 17 ms (78.74%) | Memory: 13.6 MB (26.51%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.go
+++ 187-repeated-dna-sequences/repeated-dna-sequences.go
@@ -1,15 +1,17 @@
 func findRepeatedDnaSequences(s string) []string {
+    out := []string{}
     freq := map[string]int{}
     left := 0
     for i := 0; i < len(s); i++ {
         if i-left+1 == 10 {
-            freq[s[left:i+1]]++
+            subStr := s[left:i+1]
+            freq[subStr]++
+            count := freq[subStr]
+            if count == 2 {
+                out = append(out, subStr)
+            }
             left++
         }
-    }
-    out := []string{}
-    for k, v := range freq {
-        if v >= 2 {out = append(out, k)}
     }
     return out
 }
