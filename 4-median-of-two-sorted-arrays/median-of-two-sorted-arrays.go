c76d54fd970fb37a4176105c28f7bf1d3eb501cc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-22 10:05:45 -0800

    Time: 1 ms (43.85%) | Memory: 6.5 MB (46.05%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
@@ -1,35 +1,42 @@
-// time = o(log min(n1, n2))
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-    n1 := len(nums1)
-    n2 := len(nums2)
-    if n2 < n1 {return findMedianSortedArrays(nums2, nums1)}
-    total := n1+n2
-    half := total/2
-    left := 0
-    right := n1
-    for left <= right {
-        mid := left + (right-left)/2
-        mid2 := half-mid
-        l1 := math.MinInt64
-        l2 := math.MinInt64
-        if mid-1 >= 0 {l1 = nums1[mid-1]}
-        if mid2-1 >= 0 {l2 = nums2[mid2-1]}
-        r1 := math.MaxInt64
-        r2 := math.MaxInt64
-        if mid < n1 {r1 = nums1[mid]}
-        if mid2 < n2 {r2 = nums2[mid2]}
-
-        if l1 <= r2 && l2 <= r1 {
-            if total % 2 == 0 {
-                return (float64(max(l1, l2)) + float64(min(r1, r2))) / 2.0
-            }
-            return float64(min(r1, r2))
-        }
-        if l2 > r1 {
-            left = mid+1
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {    
+    total := len(nums1) + len(nums2)
+    mid1 := (total-1)/2
+    ele1 := -1
+    mid2 := mid1+1
+    ele2 := -1
+    ptr := 0
+    n1 := 0
+    n2 := 0
+    for n1 < len(nums1) && n2 < len(nums2) {
+        n1Val := nums1[n1]
+        n2Val := nums2[n2]
+        if n1Val < n2Val {
+            if ptr == mid1 {ele1=n1Val}
+            if ptr == mid2 {ele2=n1Val}
+            n1++
         } else {
-            right = mid-1
+            if ptr == mid1 {ele1=n2Val}
+            if ptr == mid2 {ele2=n2Val}
+            n2++
         }
+        ptr++
     }
-    return 0.0
+    for n1 < len(nums1){
+        n1Val := nums1[n1]
+        if ptr == mid1 {ele1 = n1Val}
+        if ptr == mid2 {ele2 = n1Val}
+        ptr++
+        n1++
+    }
+    for n2 < len(nums2){
+        n2Val := nums2[n2]
+        if ptr == mid1 {ele1 = n2Val}
+        if ptr == mid2 {ele2 = n2Val}
+        ptr++
+        n2++
+    }
+    if total % 2 == 0 {
+        return (float64(ele1) + float64(ele2)) / 2.0
+    }
+    return float64(ele1)
 }
