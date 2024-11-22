ea2ab0308fc31c726b11defc68c5eb7602aae948
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-22 13:08:23 -0800

    Time: 37 ms (27.49%) | Memory: 3.8 MB (92.45%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.go
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.go
@@ -1,37 +1,20 @@
-// 1 <= s.length <= 16
-// i see small constraints, jump to the ultimate brute force; try and explore all paths
-// i.e dfs + backtracking
-// 2 choices at each char ; take it or not take it
-// n = len(s)
-// time = o(n * 2^n)
-// the extra n is when we create substr at each recursive node
-// space = o(n) = max depth of recursion tree
 func maxUniqueSplit(s string) int {
-    n := len(s)
-    set := map[string]bool{}
-    maxSplits := 0
-    var dfs func(start int)
-    // ababccc
-    dfs = func(start int) {
+    maxCount := 0
+    var dfs func(start int, path map[string]bool)
+    dfs = func(start int, path map[string]bool) {
         // base
-        if start == n {
-            maxSplits = max(maxSplits, len(set))
-            return
-        }
-
+        maxCount = max(maxCount, len(path))
+        if start == len(s) {return}
         // logic
-        for i := start; i < n; i++ {
+        for i := start; i < len(s); i++ {
             subStr := s[start:i+1]
-            if !set[subStr] {
-                // action
-                set[subStr] = true
-                // recurse
-                dfs(i+1)
-                // backtrack
-                delete(set, subStr)
+            if !path[subStr] {
+                path[subStr] = true
+                dfs(i+1, path)
+                delete(path, subStr)
             }
         }
     }
-    dfs(0)
-    return maxSplits
+    dfs(0, map[string]bool{})
+    return maxCount
 }
