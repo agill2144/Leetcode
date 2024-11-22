18357570bed558577f53e105fa6921ea7d508e13
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-22 11:05:19 -0800

    Time: 22 ms (33.33%) | Memory: 9 MB (16.39%) - LeetSync

--- 436-find-right-interval/find-right-interval.go
+++ 436-find-right-interval/find-right-interval.go
@@ -7,10 +7,16 @@ func findRightInterval(intervals [][]int) []int {
         return intervals[i][0] < intervals[j][0]
     })
     out := make([]int, len(intervals))
+    // "right interval for an interval i is an interval j such that startJ >= endI and startJ is minimized"
+    // find the left-most idx on right side of intervals[i][1]
     for i := 0; i < len(intervals); i++ {
         ogIdx := idx[[2]int{intervals[i][0], intervals[i][1]}]
         out[ogIdx] = -1
         endI := intervals[i][1]
+        // why include ith idx as part of our search?
+        // we are looking for right interval of i
+        // therefore should'nt this be i+1 ? i.e start from i+1
+        // no, because; "Note that i may equal j"
         left := i
         right := len(intervals)-1
         for left <= right {
