404f294252da4034c67135385c1a68c86715935b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-22 12:58:47 -0800

    Time: 32 ms (25.00%) | Memory: 3.8 MB (50.00%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.go
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.go
@@ -1,23 +1,25 @@
 func distributeCookies(cookies []int, k int) int {
     ans := math.MaxInt64
-    var dfs func(start int, childs []int, cookieIdx int, maxi int)
-    dfs = func(start int, childs []int, cookieIdx int, maxi int) {
+    childs := make([]int, k)
+    var dfs func(start int, cookieIdx int, maxi int)
+    dfs = func(start int, cookieIdx int, maxi int) {
         // base
         if cookieIdx == len(cookies) {
             ans = min(ans, maxi)
             return
         }
 
+
         // logic
         for i := start; i < len(childs); i++ {
             childs[i] += cookies[cookieIdx]
             newM := max(maxi, childs[i])
-            if newM < ans {
-                dfs(start, childs, cookieIdx+1, newM)
+            if childs[i] < ans {
+                dfs(start, cookieIdx+1, newM)
             }
             childs[i] -= cookies[cookieIdx]
         }
     }
-    dfs(0, make([]int, k),0, 0)
+    dfs(0,0,math.MinInt64)
     return ans
 }
