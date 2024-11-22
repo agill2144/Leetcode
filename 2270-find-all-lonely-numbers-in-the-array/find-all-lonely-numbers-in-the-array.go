57b65d15fd5989f9309309ad89e226d7011b8258
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-22 12:46:58 -0800

    Time: 112 ms (40.00%) | Memory: 21.9 MB (18.18%) - LeetSync

--- 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.go
+++ 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.go
@@ -1,28 +1,12 @@
 func findLonely(nums []int) []int {
-    sort.Ints(nums)
     out := []int{}
-    for i := 0; i < len(nums); i++ {
-        curr := nums[i]
-        prev := math.MinInt64+10
-        if i-1 >= 0 {prev = nums[i-1]}
-        next := math.MaxInt64-10
-        if i+1 < len(nums) {next = nums[i+1]}
-        if curr == prev || curr == next || curr-1 == prev || curr+1 == next {continue}
-        out = append(out,curr)
+    freq := map[int]int{}
+    for i := 0; i < len(nums); i++ {freq[nums[i]]++}
+    for key, val := range freq {
+        curr := key
+        prev := curr-1
+        next := curr+1
+        if freq[next] == 0 && freq[prev] == 0 && val == 1 {out = append(out, curr)}
     }
     return out
 }
-// func findLonely(nums []int) []int {
-//     freq := map[int]int{}
-//     for i := 0; i < len(nums); i++ {
-//         freq[nums[i]]++
-//     }
-//     out := []int{}
-//     for k, v := range freq {
-//         if v > 1 {continue}
-//         if freq[k-1] == 0 && freq[k+1] == 0 {
-//             out = append(out, k)
-//         }
-//     }
-//     return out
-// }
