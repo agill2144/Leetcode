fc0743038a3832b820cc08bd22363d3bbf1e10ad
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-22 11:04:53 -0800

    Time: 25 ms (33.33%) | Memory: 8.9 MB (22.95%) - LeetSync

--- 436-find-right-interval/find-right-interval.go
+++ 436-find-right-interval/find-right-interval.go
@@ -1,40 +1,27 @@
 func findRightInterval(intervals [][]int) []int {
-    out := make([]int, len(intervals))
     idx := map[[2]int]int{}
     for i := 0; i < len(intervals); i++ {
         idx[[2]int{intervals[i][0], intervals[i][1]}] = i
-        out[i] = -1
     }
-    sort.Slice(intervals, func(i, j int)bool {
+    sort.Slice(intervals, func(i, j int)bool{
         return intervals[i][0] < intervals[j][0]
     })
-
-    // "right interval for an interval i is an interval j such that startJ >= endI and startJ is minimized"
-    // find the left-most idx on right side of intervals[i][1]
-    out[len(out)-1] = -1
+    out := make([]int, len(intervals))
     for i := 0; i < len(intervals); i++ {
-        ithOrigIdx := idx[[2]int{intervals[i][0], intervals[i][1]}]
+        ogIdx := idx[[2]int{intervals[i][0], intervals[i][1]}]
+        out[ogIdx] = -1
         endI := intervals[i][1]
-        // why include ith idx as part of our search?
-        // we are looking for right interval of i
-        // therefore should'nt this be i+1 ? i.e start from i+1
-        // no, because; "Note that i may equal j"
         left := i
         right := len(intervals)-1
-        ans := -1
         for left <= right {
             mid := left + (right-left)/2
-            startJ :=  intervals[mid][0]
+            startJ := intervals[mid][0]
             if startJ >= endI {
-                ans = mid
+                out[ogIdx] = idx[[2]int{intervals[mid][0], intervals[mid][1]}]
                 right = mid-1
             } else {
                 left = mid+1
             }
-        }
-        if ans != -1 {
-            origIdx := idx[[2]int{intervals[ans][0], intervals[ans][1]}]
-            out[ithOrigIdx] = origIdx
         }
     }
     return out
