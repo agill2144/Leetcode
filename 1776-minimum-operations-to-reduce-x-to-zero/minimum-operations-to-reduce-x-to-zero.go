a42a0b1ababc0834be765066f3fc25c30f0c750e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-23 11:52:11 -0800

    Time: 6 ms (15.79%) | Memory: 10 MB (30.77%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.go
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.go
@@ -1,24 +1,24 @@
 func minOperations(nums []int, x int) int {
     n := len(nums)
-    sum := nums[n-1]
+    winSum := nums[n-1]
     i := n-1
-    for i >= 0 && sum < x {
-        sum += nums[i]
+    for i >= 0 && winSum < x {
+        winSum += nums[i]
         i--
     }
     if i == -1 {i = 0}
-    if sum < x {return -1}
-    sum = 0
+    if winSum < x {return -1}
+    winSum = 0
     left := i
     minWin := len(nums)+1
     for left <= n {
-        sum += nums[i%n]
-        for sum > x && left <= n {
-            sum -= nums[left%n]
+        winSum += nums[i%n]
+        for winSum > x && left <= n {
+            winSum -= nums[left%n]
             left++
         }
         if left > n {break}
-        if sum == x { minWin = min(minWin, i-left+1) }
+        if winSum == x { minWin = min(minWin, i-left+1) }
         i++
     }
     if minWin == len(nums)+1 {return -1}
