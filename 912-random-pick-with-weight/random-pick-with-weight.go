78a5f097d5333b6584e6efe60b917cc0a354790f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-23 13:58:22 -0800

    Time: 14 ms (72.80%) | Memory: 10.1 MB (20.53%) - LeetSync

---
+++ 912-random-pick-with-weight/random-pick-with-weight.go
@@ -0,0 +1,40 @@
+type Solution struct {
+    prefix []int
+}
+
+
+func Constructor(w []int) Solution {
+    prefix := make([]int, len(w))
+    for i := 0; i < len(w); i++ {
+        prefix[i] = w[i]
+        if i-1 >= 0 {prefix[i] += prefix[i-1]}
+    }
+    return Solution{prefix}
+}
+
+
+func (this *Solution) PickIndex() int {
+    n := len(this.prefix)
+    total := this.prefix[n-1]
+    r := rand.Intn(total)
+    left := 0
+    right := n-1
+    ans := 0
+    for left <= right {
+        mid := left + (right-left)/2
+        if r < this.prefix[mid] {
+            ans = mid
+            right = mid-1
+        } else {
+            left = mid+1
+        }
+    }
+    return ans
+}
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(w);
+ * param_1 := obj.PickIndex();
+ */
