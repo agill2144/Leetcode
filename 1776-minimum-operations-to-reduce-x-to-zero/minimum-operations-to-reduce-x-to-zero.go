8056430d2268373c6a0a36e8d6652b965098a75d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-23 11:45:00 -0800

    Time: 0 ms (100.00%) | Memory: 11.1 MB (26.92%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.go
@@ -0,0 +1,29 @@
+func minOperations(nums []int, x int) int {
+    n := len(nums)
+    if x < nums[0] && x < nums[n-1] {return -1}
+    sum := nums[n-1]
+    i := n-1
+    for i >= 0 && sum < x {
+        sum += nums[i]
+        i--
+    }
+    if i == -1 {i = 0}
+    if sum < x {return -1}
+    sum = 0
+    left := i
+    minWin := len(nums)+1
+    for left <= n {
+        sum += nums[i%n]
+        for sum > x &&  left <= n {
+            sum -= nums[left%n]
+            left++
+        }
+        if left > n {break}
+        if sum == x {
+            minWin = min(minWin, i-left+1)
+        }
+        i++
+    }
+    if minWin == len(nums)+1 {return -1}
+    return minWin
+}
