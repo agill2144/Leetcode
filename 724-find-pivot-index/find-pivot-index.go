874ba741e9f595ddfa56a56b3ca9c488bd0f6ba8
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-22 20:07:25 -0800

    Time: 0 ms (100.00%) | Memory: 7.8 MB (46.80%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.go
+++ 724-find-pivot-index/find-pivot-index.go
@@ -6,6 +6,11 @@ func pivotIndex(nums []int) int {
     rSum := 0
     for i := 0; i < len(nums); i++ {
         leftSum := rSum
+        // we want the sum from i+1 to n-1
+        // total has everything
+        // first remove leftSum from total ( this excludes ith element )
+        // then remove ith element too, because remember total sum is sum of all nums
+        // while leftSum is sum of nums from idx 0 to i-1 ( does not include i) 
         rightSum := total - leftSum - nums[i]
         if leftSum == rightSum {return i}
         rSum += nums[i]
