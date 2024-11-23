aa6b2afd841988e0cf0976d7a72fd11afb4ca778
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-22 19:38:55 -0800

    Time: 18 ms (68.31%) | Memory: 9.3 MB (22.71%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.go
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.go
@@ -1,15 +1,11 @@
 func subarraySum(nums []int, k int) int {
-    count := 0
-    freq := map[int]int{}
     rSum := 0
+    freq := map[int]int{0:1}
+    total := 0
     for i := 0; i < len(nums); i++ {
         rSum += nums[i]
-        if rSum == k {
-            count++
-        }
-        diff := rSum - k
-        count += freq[diff]
+        total += freq[rSum-k]
         freq[rSum]++
     }
-    return count
+    return total
 }
