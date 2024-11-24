a5f63f3706af891f09e8982a5cf28bf1766dcd16
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-23 22:13:02 -0800

    Time: 0 ms (100.00%) | Memory: 4.3 MB (43.29%) - LeetSync

--- 162-find-peak-element/find-peak-element.go
+++ 162-find-peak-element/find-peak-element.go
@@ -1,13 +1,31 @@
 func findPeakElement(nums []int) int {
-    // strictly greater than its neighbors
-    // not >= , strictly > 
-    for i := 0; i < len(nums); i++ {
-        prev := math.MinInt64
-        if i-1 >= 0 {prev = nums[i-1]}
-        curr := nums[i]
-        next := math.MinInt64
-        if i+1 < len(nums) {next = nums[i+1]}
-        if curr > prev && curr > next {return i}
+    n := len(nums)
+    left := 0
+    right := n-1
+    for left <= right {
+        mid := left + (right-left)/2
+        if (mid == 0 || nums[mid] > nums[mid-1]) && (mid == n-1 || nums[mid] > nums[mid+1]) {
+            return mid
+        }
+        if (mid == n-1 || nums[mid+1] > nums[mid]) {
+            left = mid+1
+        } else {
+            right = mid-1
+        }
     }
     return -1
 }
+// brute force: linear scan
+// func findPeakElement(nums []int) int {
+//     // strictly greater than its neighbors
+//     // not >= , strictly > 
+//     for i := 0; i < len(nums); i++ {
+//         prev := math.MinInt64
+//         if i-1 >= 0 {prev = nums[i-1]}
+//         curr := nums[i]
+//         next := math.MinInt64
+//         if i+1 < len(nums) {next = nums[i+1]}
+//         if curr > prev && curr > next {return i}
+//     }
+//     return -1
+// }
