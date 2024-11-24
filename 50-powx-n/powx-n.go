023f251e3536abf542a880c59c19c6f1dafc26c9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-24 00:29:58 -0800

    Time: 0 ms (100.00%) | Memory: 3.7 MB (50.80%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,23 +1,47 @@
 func myPow(x float64, n int) float64 {
-    // assuming the underlying system in a 64-bit system
-    // then converting a -2^31 (min int32) to positivie will not cause an int32 overflow
-    // if the underlying system is a 32 bit system, then handle the overflow using int64
-    // LC system is a 64 bit system, therefore making a min int32 to positive will not overflow
     if n < 0 {
         n *= -1
         x = 1/x
     }
-    // 2^8 = (2^2)^4
-    // x^n = (x^2)^n/2
-    var runningProd float64 = x
-    var res float64 = 1.0
-    for n != 0 {
-        if n % 2 != 0 {
-            res *= runningProd
-            n--
+    var dfs func(base float64, exp int) float64
+    dfs = func(base float64, exp int) float64 {
+        // base
+        if exp <= 1 {
+            if exp == 1 {return base}
+            return 1.0
         }
-        runningProd *= runningProd
-        n /= 2
+        // logic
+        res := dfs(base*base, exp/2)
+        if exp % 2 != 0 {
+            res *= base
+        }
+        return res
     }
-    return res
+    return dfs(x, n)
 }
+// func myPow(x float64, n int) float64 {
+//     // assuming the underlying system in a 64-bit system
+//     // then converting a -2^31 (min int32) to positivie will not cause an int32 overflow
+//     // if the underlying system is a 32 bit system, then handle the overflow using int64
+//     // LC system is a 64 bit system, therefore making a min int32 to positive will not overflow
+//     if n < 0 {
+//         n *= -1
+//         x = 1/x
+//     }
+//     // x^n = (x^2)^n/2
+//     // 2^8 = (2x2)^4
+//     // 4^4 = (4x4)^4/2 = 16^2
+//     // 16^2 = (16x16)^2/2 = 256^1
+//     // 256^1 = odd exp, save runningProd to res; res *= 256; n-- ; n=0
+//     var runningProd float64 = x
+//     var res float64 = 1.0
+//     for n != 0 {
+//         if n % 2 != 0 {
+//             res *= runningProd
+//             n--
+//         }
+//         runningProd *= runningProd
+//         n /= 2
+//     }
+//     return res
+// }
