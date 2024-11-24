66aaa5c57297225e203f67e6f26cac83e412fe04
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-23 20:42:06 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (53.85%) - LeetSync

--- 1192-divide-chocolate/divide-chocolate.go
+++ 1192-divide-chocolate/divide-chocolate.go
@@ -1,26 +1,20 @@
 func maximizeSweetness(sweetness []int, k int) int {
-    start := math.MaxInt64
-    end := 0
-    for i := 0; i < len(sweetness); i++ {
-        start = min(start, sweetness[i])
-        end += sweetness[i]
-    }
+    n := len(sweetness)
+    left := 1
+    right := 0
+    for i := 0; i < n; i++ {right += sweetness[i]}
     ans := 0
-    left := start
-    right := end
     for left <= right {
         mid := left + (right-left)/2
-        chunks := 0
         rSum := 0
-        for j := 0; j < len(sweetness); j++ {
+        chunks := 0
+        for j := 0; j < n; j++ {
             rSum += sweetness[j]
             if rSum >= mid {
                 chunks++
                 rSum = 0
             }
         }
-        // when does mid not work?
-        // when chunks < k+1
         if chunks < k+1 {
             right = mid-1
         } else {
