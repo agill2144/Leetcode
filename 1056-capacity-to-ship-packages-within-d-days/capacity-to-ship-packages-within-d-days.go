99e60df7892ef6249dda2927897bb8a2d831a811
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-23 20:55:59 -0800

    Time: 4 ms (75.90%) | Memory: 8.5 MB (41.06%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.go
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.go
@@ -1,29 +1,29 @@
 func shipWithinDays(weights []int, days int) int {
-    n := len(weights)
-    start := math.MinInt64
-    end := 0
-    for i := 0; i < n; i++ {
-        start = max(start, weights[i])
-        end += weights[i]
+    left := 0
+    right := 0
+    for i := 0; i < len(weights); i++ {
+        left = max(left, weights[i])
+        right += weights[i]
     }
-    ans := 0
-    for start <= end {
-        atMax := start + (end-start)/2
-        currW := 0
-        countDays := 1
-        for j := 0; j < n; j++ {
-            currW += weights[j]
-            if currW > atMax {
-                countDays++
-                currW = weights[j]
+    ans := -1
+    for left <= right {
+        mid := left + (right-left)/2
+        rSum := 0
+        day := 1
+        for i := 0; i < len(weights); i++ {
+            rSum += weights[i]
+            if rSum > mid {
+                rSum = weights[i]
+                day++
             }
         }
-        if countDays > days {
-            start = atMax+1
+        if day <= days {
+            ans = mid
+            right = mid-1
         } else {
-            ans = atMax
-            end = atMax-1
+            left = mid+1
         }
     }
     return ans
+
 }
