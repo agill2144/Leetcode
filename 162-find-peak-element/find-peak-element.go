ee4a621c9e311cbc1de30aa8675f8e92ddfacc75
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-23 22:11:32 -0800

    Time: 0 ms (100.00%) | Memory: 4.6 MB (6.25%) - LeetSync

--- 162-find-peak-element/find-peak-element.go
+++ 162-find-peak-element/find-peak-element.go
@@ -1,18 +1,13 @@
 func findPeakElement(nums []int) int {
-    n := len(nums)
-	left := 0
-	right := n-1
-	for left <= right {
-		mid := left + (right-left)/2
-		// is mid our ans?
-		// is mid our peek element?
-		if (mid == 0 || nums[mid] > nums[mid-1]) && (mid == n-1 || nums[mid] > nums[mid+1]) {
-			return mid
-		} else if nums[mid+1] > nums[mid] {
-			left = mid+1
-		} else {
-			right = mid-1
-		}
-	}
-	return -1
+    // strictly greater than its neighbors
+    // not >= , strictly > 
+    for i := 0; i < len(nums); i++ {
+        prev := math.MinInt64
+        if i-1 >= 0 {prev = nums[i-1]}
+        curr := nums[i]
+        next := math.MinInt64
+        if i+1 < len(nums) {next = nums[i+1]}
+        if curr > prev && curr > next {return i}
+    }
+    return -1
 }
