042f947be8cd0fa8c04ca60ef4ae4d7ee1b1e1bc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-23 23:34:17 -0800

    Time: 0 ms (100.00%) | Memory: 3.8 MB (33.51%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,18 +1,23 @@
 func myPow(x float64, n int) float64 {
+    // assuming the underlying system in a 64-bit system
+    // then converting a -2^31 (min int32) to positivie will not cause an int32 overflow
+    // if the underlying system is a 32 bit system, then handle the overflow using int64
+    // LC system is a 64 bit system, therefore making a min int32 to positive will not overflow
     if n < 0 {
-        x = 1/x
         n *= -1
+        x = 1/x
     }
+    // 2^8 = (2^2)^4
+    // x^n = (x^2)^n/2
+    var runningProd float64 = x
     var res float64 = 1.0
-    // x^8 = (x^2)^4
     for n != 0 {
-      if n % 2 != 0 {
-        res *= x
-        n--
-      }
-      if n <= 1 {break}
-      x = x*x
-      n = n/2
+        if n % 2 != 0 {
+            res *= runningProd
+            n--
+        }
+        runningProd *= runningProd
+        n /= 2
     }
     return res
 }
