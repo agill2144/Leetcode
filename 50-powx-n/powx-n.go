1831f0026164f7be5f232e68df1c8cb9d4653f1e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-24 23:26:14 -0800

    Time: 0 ms (100.00%) | Memory: 3.8 MB (34.29%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -37,26 +37,25 @@
         because PEMDAS
     time = o(logn)
     space = o(logn) ; recursive stack
+
+    approach: iterative binary exponentiation
+    time = o(logn)
+    space = o(1)
 */
 func myPow(x float64, n int) float64 {
     if n < 0 {
         n *= -1
         x = 1/x
     }
-    var dfs func(base float64, exp int) float64
-    dfs = func(base float64, exp int) float64 {
-        // base
-        if exp <= 1 {
-            if exp == 1 {return base}
-            return 1.0
+    var res float64 = 1.0
+    for n != 0 {
+        if n % 2 != 0 {
+            res *= x
+            n--
+        } else {
+            x *= x
+            n /= 2
         }
-
-        // logic
-        // #2; x^n = (x*x)^n/2
-        res := dfs(base*base, exp/2)
-        // handle extra term when exp is odd *AFTER* exponent computation is done
-        if exp % 2 != 0 {res *= base}
-        return res
     }
-    return dfs(x, n)
+    return res
 }
