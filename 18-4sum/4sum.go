7d686142d574e7b855611a38f1a94fc98a1513af
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-24 19:20:12 -0800

    Time: 3 ms (93.17%) | Memory: 4.4 MB (61.54%) - LeetSync

--- 18-4sum/4sum.go
+++ 18-4sum/4sum.go
@@ -4,37 +4,32 @@ func fourSum(nums []int, target int) [][]int {
     n := len(nums)
     
     for i := 0; i < n-3; i++ {
-        // Skip duplicates for i
-        if i > 0 && nums[i] == nums[i-1] {
-            continue
-        }
-        
+        if i > 0 && nums[i] == nums[i-1] { continue}
         for j := i+1; j < n-2; j++ {
-            // Skip duplicates for j
-            if j > i+1 && nums[j] == nums[j-1] {
-                continue
-            }
-            
-            // Use two pointers for the remaining two numbers
+            /*
+                [2,2,2,3,4....]
+                 i j
+                - we want the first j to get processed even if jth val is same as j-1th val
+                - because this is the first time we are making quads starting with [2,2,x,y]
+
+                [2,2,2,3,4....]
+                 i   j
+                - now we can skip this jth ptr pointing at the same value as prev value
+                - because continuing with this will create another quad starting with [2,2,x,y]
+                    that we had discovered in the last iteration ( above )
+
+                - therefore j > i+1 for j to skip curr position if curr position value is same as prev
+            */
+            if j > i+1 && nums[j] == nums[j-1] {continue}
             left, right := j+1, n-1
-            
             for left < right {
-                sum := nums[i] + nums[j] + nums[left] + nums[right]
-                
+                sum := nums[i] + nums[j] + nums[left] + nums[right] 
                 if sum == target {
-                    result = append(result, []int{nums[i], nums[j], nums[left], nums[right]})
-                    
-                    // Skip duplicates for left
-                    for left < right && nums[left] == nums[left+1] {
-                        left++
-                    }
-                    // Skip duplicates for right
-                    for left < right && nums[right] == nums[right-1] {
-                        right--
-                    }
-                    
+                    result = append(result, []int{nums[i], nums[j], nums[left], nums[right]})                    
                     left++
+                    for left < right && nums[left] == nums[left-1] {left++}
                     right--
+                    for left < right && nums[right] == nums[right+1] {right--}                    
                 } else if sum < target {
                     left++
                 } else {
