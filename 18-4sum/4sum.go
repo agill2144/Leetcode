f3a21526700b6ddaf514eb09b0e8ef59b479e5e7
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-24 18:48:12 -0800

    Time: 5 ms (81.23%) | Memory: 4.6 MB (41.86%) - LeetSync

--- 18-4sum/4sum.go
+++ 18-4sum/4sum.go
@@ -1,31 +1,48 @@
 func fourSum(nums []int, target int) [][]int {
     sort.Ints(nums)
+    result := [][]int{}
     n := len(nums)
-    out := [][]int{}
-    set := map[[4]int]bool{}
-    for i := 0; i < n; i++ {
-        for j := i+1; j < n; j++ {
-            left := j+1
-            right := n-1
+    
+    for i := 0; i < n-3; i++ {
+        // Skip duplicates for i
+        if i > 0 && nums[i] == nums[i-1] {
+            continue
+        }
+        
+        for j := i+1; j < n-2; j++ {
+            // Skip duplicates for j
+            if j > i+1 && nums[j] == nums[j-1] {
+                continue
+            }
+            
+            // Use two pointers for the remaining two numbers
+            left, right := j+1, n-1
+            
             for left < right {
                 sum := nums[i] + nums[j] + nums[left] + nums[right]
+                
                 if sum == target {
-                    tmp := [4]int{nums[i], nums[j], nums[left], nums[right]}
-                    if !set[tmp] {
-                        out = append(out, []int{nums[i], nums[j], nums[left], nums[right]})
-                        set[tmp] = true
+                    result = append(result, []int{nums[i], nums[j], nums[left], nums[right]})
+                    
+                    // Skip duplicates for left
+                    for left < right && nums[left] == nums[left+1] {
+                        left++
                     }
+                    // Skip duplicates for right
+                    for left < right && nums[right] == nums[right-1] {
+                        right--
+                    }
+                    
                     left++
-                    for left < right && nums[left] == nums[left-1] {left++}
                     right--
-                    for left < right && nums[right] == nums[right+1] {right--}
-                } else if sum > target {
-                    right--
+                } else if sum < target {
+                    left++
                 } else {
-                    left++
+                    right--
                 }
             }
         }
     }
-    return out
+    
+    return result
 }
