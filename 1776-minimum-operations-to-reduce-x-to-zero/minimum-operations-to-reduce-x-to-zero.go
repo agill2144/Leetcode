e54011dce998cc550df4e9425dd68449c0cdbc4c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-24 21:28:20 -0800

    Time: 2 ms (50.00%) | Memory: 9.9 MB (48.15%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.go
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.go
@@ -1,3 +1,31 @@
+/*
+    approach: inverted sliding window
+    - giving edgy sliding window vibes
+    - where a window wraps around as if the array was circular
+        - when you see this, try to think of inverted window
+        - inverted window items outside of a sliding window
+        - where the current window is not necessarily the answer
+        - but the elements outside of the window could be
+        - inverted sliding windows are specially helpful when a window needs to wrap around the array
+        - [x,x,x,x,x,x,x,x]
+               |_______|
+        - if the above is our current window, the elements outside are automatically wrapping!
+            - thereby creating the wrapping window outside of current regular sliding window
+        - [x,x,x,x,x,x,x,x]
+           |_____|
+        - [x,x,x,x,x,x,x,x]
+               |_____|
+        - [x,x,x,x,x,x,x,x]
+                   |_____|
+    - we need to reduce x to 0
+    - and these elements can only be picked from the outside edges of the array
+        - i.e elements that are part of the inverted window
+    - we can create a total sum of nums array
+    - and can remove x from total sum
+    - this means, if we creating a window with the total-x sum, we will have exactly x sum in the inverted window
+    time = o(n)
+    space = o(1)
+*/
 func minOperations(nums []int, x int) int {
     n := len(nums)
     k := 0
