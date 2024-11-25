d8b78533153db9191fc85a1ae50272e29762c564
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-25 15:48:15 -0800

    Time: 0 ms (100.00%) | Memory: 7.8 MB (22.22%) - LeetSync

---
+++ 979-di-string-match/di-string-match.go
@@ -0,0 +1,17 @@
+func diStringMatch(s string) []int {
+    n := len(s)
+    d := n
+    i := 0
+    out := []int{}
+    for j := 0; j < len(s); j++ {
+        if s[j] == 'I' {
+            out = append(out,i)
+            i++
+        } else {
+            out = append(out, d)
+            d--
+        }
+    }
+    if d >= 0 {out = append(out, d)} else { out = append(out, i)}
+    return out
+}
