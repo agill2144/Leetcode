411e4f0c05accd9a96212dfc55861c9ede7d832a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-24 20:36:21 -0800

    Time: 0 ms (100.00%) | Memory: 10.8 MB (66.67%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.go
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.go
@@ -1,25 +1,21 @@
 func findPeakGrid(mat [][]int) []int {
     m := len(mat)
     n := len(mat[0])
-    left := 0
-    right := m-1
-    for left <= right {
-        mid := left+(right-left)/2
-        j := -1
-        for i := 0; i < n; i++ {
-            if j == -1 || mat[mid][i] > mat[mid][j] {j=i}
-        }
-
-        topVal := math.MinInt64
-        if mid-1 >= 0 {topVal = mat[mid-1][j]}
-        bottomVal := math.MinInt64
-        if mid+1 < m {bottomVal = mat[mid+1][j]}
-        if mat[mid][j] > topVal && mat[mid][j] > bottomVal {return []int{mid, j}}
-
-        if topVal > bottomVal {
-            right = mid-1
-        } else {
-            left = mid+1
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            curr := mat[i][j]
+            left := math.MinInt64
+            if j-1 >= 0 {left = mat[i][j-1]}
+            right := math.MinInt64
+            if j+1 < n {right = mat[i][j+1]}
+            top := math.MinInt64
+            if i-1 >= 0 {top = mat[i-1][j]}
+            bottom := math.MinInt64
+            if i+1 < m {bottom = mat[i+1][j]}
+            if curr > left && curr > right &&
+                curr > top && curr > bottom {
+                    return []int{i, j}
+            }
         }
     }
     return nil
