7a0831cfadc20735b7a819f4e83fbdb213bb41c6
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-24 21:21:48 -0800

    Time: 4 ms (40.00%) | Memory: 9.5 MB (74.07%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.go
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.go
@@ -1,28 +1,21 @@
 func minOperations(nums []int, x int) int {
     n := len(nums)
-    left := n-1
-    sum := 0
-    for sum < x && left >= 0 {
-        sum += nums[left]
-        left--
-    }
-    if sum < x {return -1}
-    if left < 0 {left++}
-    sum = 0
-    i := left
-    minWin := math.MaxInt64
-    for left <= n {
-        sum += nums[i%n]
-        for sum > x && left <= n {
-            sum -= nums[left%n]
+    k := 0
+    for i := 0; i < n; i++ {k += nums[i]}
+    k -= x
+    left := 0
+    rSum := 0
+    minWin := len(nums)+1
+    for i := 0; i < n; i++ {
+        rSum += nums[i]
+        for rSum > k && left <= i {
+            rSum -= nums[left]
             left++
         }
-        if left > n {break}
-        if sum == x {
-            minWin = min(minWin, i-left+1)
+        if rSum == k {
+            minWin = min(minWin, n-(i-left+1))            
         }
-        i++
     }
-    if minWin == math.MaxInt64 {minWin = -1}
-    return minWin
+    if minWin == len(nums)+1 {minWin = -1}
+    return minWin 
 }
