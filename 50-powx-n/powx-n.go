d770889ad6cb7e6e5b2eabad2a9e746033b9068e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-24 23:25:05 -0800

    Time: 0 ms (100.00%) | Memory: 3.9 MB (34.29%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,40 +1,62 @@
+/*
+    x^n can be re-written in two ways
+    1. (x^n/2) * (x^n/2)
+    - where exponent computation is done FIRST
+    - because PEMDAS
+    - repeatedly squaring x and halving n
+
+    2. (x^2)^n/2
+    - repeatedly squaring x and halving n
+
+    In both cases, we have to perform n/2
+    which only works when exp is divisible by 2
+    
+    To handle odd exponents, we simply add another term of current base
+    1. (x^n/2) * (x^n/2) * x
+    - where exponent computation is done FIRST
+    - because PEMDAS
+    - the extra * x happens after the left handle is solved
+
+    2. ((x^2)^n/2) * x
+    - where the  brackets are solved first
+    - because PEMDAS
+    - the extra * x happens after the left handle is solved
+
+
+    approach: brute force
+    - run a loop 0 -> n times
+    - and repeatedly multiply base
+    time = o(n)
+    space = o(1)
+
+    approach: recursive binary exponentiation
+    - we can use either #1 or #2 form of binary exponentiation
+    - as long as we handle multiplying extra term when exp becomes odd
+    - and making sure that the extra base term
+        multiplication happens after exponent is computed,
+        because PEMDAS
+    time = o(logn)
+    space = o(logn) ; recursive stack
+*/
 func myPow(x float64, n int) float64 {
     if n < 0 {
         n *= -1
         x = 1/x
     }
-    var res float64 = 1.0
-    for n != 0 {
-        if n % 2 != 0 {
-            // save base to res to multiply left over term later.
-            // because PEMDAS - exponent first and then multiplcation
-            // we may still have exponent left ( n could be 2 for instance )
-            // therefore save current base as is into the result
-            res *= x
-            n--
-        } else {
-            x *= x
-            n /= 2
+    var dfs func(base float64, exp int) float64
+    dfs = func(base float64, exp int) float64 {
+        // base
+        if exp <= 1 {
+            if exp == 1 {return base}
+            return 1.0
         }
+
+        // logic
+        // #2; x^n = (x*x)^n/2
+        res := dfs(base*base, exp/2)
+        // handle extra term when exp is odd *AFTER* exponent computation is done
+        if exp % 2 != 0 {res *= base}
+        return res
     }
-    return res
+    return dfs(x, n)
 }
-// func myPow(x float64, n int) float64 {
-//     if n < 0 {
-//         n *= -1
-//         x = 1/x
-//     }
-//     var dfs func(base float64, exp int) float64
-//     dfs = func(base float64, exp int) float64 {
-//         // base
-//         if exp <= 1 {
-//             if exp == 1 {return base}
-//             return 1.0
-//         }
-//         // logic
-//         res := dfs(base*base, exp/2)
-//         if exp % 2 != 0 {res *= base}
-//         return res
-//     }
-//     return dfs(x, n)
-// }
