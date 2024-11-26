44cfc0902ae0a4417de5fa1fc6fd1fbec88b9f30
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-25 18:52:45 -0800

    Time: 0 ms (100.00%) | Memory: 4.1 MB (28.09%) - LeetSync

--- 78-subsets/subsets.go
+++ 78-subsets/subsets.go
@@ -1,36 +1,20 @@
 func subsets(nums []int) [][]int {
-    out := [][]int{{}}
-    for i := 0; i < len(nums); i++ {
-        // range will take a snapshot of output array , so modifying it while 
-        // looping over out will not change the initial snapshot of output array
-        // that range started looping over
-        for _, curr := range out {
-            newP := make([]int, len(curr))
-            copy(newP, curr)
-            newP = append(newP, nums[i])
-            out = append(out, newP)
+    out := [][]int{}
+    var dfs func(start int, path []int)
+    dfs = func(start int, path []int) {
+        // base
+        
+        newL := make([]int, len(path))
+        copy(newL, path)
+        out = append(out, newL)
+
+        // logic
+        for i := start; i < len(nums); i++ {
+            path = append(path, nums[i])
+            dfs(i+1, path)
+            path = path[:len(path)-1]
         }
     }
+    dfs(0, nil)
     return out
 }
-
-
-// func subsets(nums []int) [][]int {
-//     out := [][]int{}
-//     var dfs func(start int, path []int)
-//     dfs = func(start int, path []int) {
-//         // base
-//         newP := make([]int, len(path))
-//         copy(newP, path)
-//         out = append(out, newP)
-
-//         // logic
-//         for i := start; i < len(nums); i++ {
-//             path = append(path, nums[i])
-//             dfs(i+1, path)
-//             path = path[:len(path)-1]
-//         }
-//     }
-//     dfs(0, nil)
-//     return out
-// }
