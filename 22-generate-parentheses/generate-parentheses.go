d2088faa4748544e1708e327da5b8cae8ca6585b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-25 16:46:56 -0800

    Time: 0 ms (100.00%) | Memory: 4.4 MB (57.60%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.go
@@ -0,0 +1,18 @@
+func generateParenthesis(n int) []string {
+    out := []string{}
+    var dfs func(o, c int, path string)
+    dfs = func(o, c int, path string) {
+        // base
+        if o == 0 && c == 0 {out = append(out, path); return}
+
+        // logic
+        if o > 0 {
+            dfs(o-1, c, path + "(")
+        }
+        if c > o {
+            dfs(o, c-1, path + ")")
+        }
+    } 
+    dfs(n, n, "")
+    return out
+}
