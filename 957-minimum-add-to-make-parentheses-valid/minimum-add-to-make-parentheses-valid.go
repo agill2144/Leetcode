469978ce93005fcdee3a6707d65c73bfff0e188a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-26 20:55:32 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (5.12%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.go
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.go
@@ -1,12 +1,13 @@
 func minAddToMakeValid(s string) int {
-    count := 0 // open paran count ( like stack )
-    closeCount := 0 // close paran showed up when there were no open ones
-    for i := 0 ; i < len(s); i++ {
+    extraClosing := 0
+    open := 0
+    for i := 0; i < len(s); i++ {
         if s[i] == '(' {
-            count++
-        } else {
-            if count > 0 {count--} else {closeCount++}
+            open++
+        } else if s[i] == ')' {
+            if open == 0 {extraClosing++; continue}
+            open--
         }
     }
-    return count + closeCount
+    return open + extraClosing
 }
