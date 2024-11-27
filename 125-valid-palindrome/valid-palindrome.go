9bfc0f793868b425acb58cabad58ef2ba8067eae
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-26 20:22:03 -0800

    Time: 5 ms (30.40%) | Memory: 5.2 MB (33.53%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.go
+++ 125-valid-palindrome/valid-palindrome.go
@@ -1,26 +1,19 @@
 func isPalindrome(s string) bool {
-    var dfs func(left, right int)bool
-    dfs = func(left, right int)bool {
-        // base
-        if left > right {return true}
-
-        // logic
-        leftChar := s[left]
-        rightChar := s[right]
-        if !isAlpha(leftChar) {
-            return dfs(left+1, right)
-        }
-        if !isAlpha(rightChar) {
-            return dfs(left, right-1)
-        }
-        leftStr := strings.ToLower(string(leftChar))
-        rightStr := strings.ToLower(string(rightChar))
-        fmt.Println(leftStr, rightStr)
-        return leftStr == rightStr && dfs(left+1, right-1)
+    t := new(strings.Builder)
+    for i := 0; i < len(s); i++ {
+        if (s[i] >= '0' && s[i] <= '9') || 
+            (s[i] >= 'a' && s[i] <= 'z') ||
+            (s[i] >= 'A' && s[i] <= 'Z') {
+                t.WriteString(strings.ToLower(string((s[i]))))
+            }
     }
-    return dfs(0, len(s)-1)
-}
-
-func isAlpha(c byte)bool {
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')
+    tStr := t.String()
+    l := 0
+    r := len(tStr)-1
+    for l < r {
+        if tStr[l] != tStr[r] {return false}
+        l++
+        r--
+    }
+    return true
 }
