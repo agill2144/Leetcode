aefa4abe3014fd2a889f39af91c8e3a403932255
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-26 20:16:09 -0800

    Time: 9 ms (84.78%) | Memory: 9.4 MB (28.74%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.go
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.go
@@ -2,30 +2,28 @@ func shortestPathBinaryMatrix(grid [][]int) int {
     m := len(grid)
     n := len(grid[0])
     if grid[0][0] == 1 || grid[m-1][n-1] == 1 {return -1}
-    level := 1
     dirs := [][]int{
         {1,0},{-1,0},
         {0,1},{0,-1},
         {1,-1},{1,1},
         {-1,-1},{-1,1},
     }
+    visited := -1    
     q := [][]int{{0,0}}
-    grid[0][0] = -1
+    grid[0][0] = visited
+    level := 1
     for len(q) != 0 {
         qSize := len(q)
         for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            cr := dq[0]
-            cc := dq[1]
+            dq := q[0]; q = q[1:]
+            cr, cc := dq[0], dq[1]
             if cr == m-1 && cc == n-1 {return level}
             for _, dir := range dirs {
                 nr := cr + dir[0]
                 nc := cc + dir[1]
                 if nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 0 {
-                    q = append(q, []int{nr, nc})
-                    grid[nr][nc] = -1
-                    if nr == m-1 && nc == n-1 {return level+1}
+                    grid[nr][nc] = visited
+                    q = append(q, []int{nr,nc})
                 }
             }
             qSize--
