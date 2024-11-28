6496a4817753ca0c18f1fae957ddbae9d2fc557b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-27 20:43:06 -0800

    Time: 7 ms (47.33%) | Memory: 6.8 MB (36.29%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.go
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -6,21 +6,43 @@
  * }
  */
 func mergeKLists(lists []*ListNode) *ListNode {
-    n := len(lists)
-    items := []int{}
-    for i := 0; i < n; i++ {
-        curr := lists[i]
-        for curr != nil {
-            items = append(items, curr.Val)
-            curr = curr.Next
-        }
+    mn := &minHeap{nodes: []*ListNode{}}
+    for i := 0; i < len(lists); i++ {
+        if lists[i] == nil {continue}
+        heap.Push(mn, lists[i])
     }
-    sort.Ints(items)
-    head := &ListNode{Val:0}
+    head := &ListNode{Val: 0}
     tail := head
-    for i := 0; i < len(items); i++ {
-        tail.Next = &ListNode{Val: items[i]}
+    for mn.Len() != 0 {
+        top := heap.Pop(mn).(*ListNode)
+        next := top.Next
+        top.Next = nil
+        tail.Next = top
         tail = tail.Next
+        if next != nil { heap.Push(mn, next) }
     }
     return head.Next
+}
+
+
+type minHeap struct {
+	nodes []*ListNode
+}
+
+func (m *minHeap) Less(i, j int) bool {
+	return m.nodes[i].Val < m.nodes[j].Val
+}
+func (m *minHeap) Swap(i, j int) {
+	m.nodes[i], m.nodes[j] = m.nodes[j], m.nodes[i]
+}
+func (m *minHeap) Len() int {
+	return len(m.nodes)
+}
+func (m *minHeap) Push(x interface{}) {
+	m.nodes = append(m.nodes, x.(*ListNode))
+}
+func (m *minHeap) Pop() interface{} {
+	out := m.nodes[len(m.nodes)-1]
+	m.nodes = m.nodes[:len(m.nodes)-1]
+	return out
 }
