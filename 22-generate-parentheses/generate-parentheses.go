cf009a8f3a37c320df81cb37b54450b92b613ec0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-27 19:56:56 -0800

    Time: 0 ms (100.00%) | Memory: 4.8 MB (14.23%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.go
+++ 22-generate-parentheses/generate-parentheses.go
@@ -3,20 +3,19 @@ func generateParenthesis(n int) []string {
     var dfs func(o, c int, path string)
     dfs = func(o, c int, path string) {
         // base
-        if o == 0 && c == 0 {out = append(out, path); return}
+        if o == 0 && c == 0 {
+            out = append(out, path)
+            return
+        }
 
         // logic
         if o > 0 {
             dfs(o-1, c, path + "(")
         }
-        // there must be open parans for us to close
-        // and thats only possible if open count was less than close count
-        // meaning we have more close parans left than open
-        // meaning there are open parans that have yet to be closed, therefore start closing them
         if c > o {
             dfs(o, c-1, path + ")")
         }
-    } 
-    dfs(n, n, "")
+    }
+    dfs(n,n,"")
     return out
 }
