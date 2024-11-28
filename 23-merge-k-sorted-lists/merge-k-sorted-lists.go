c1bde258198a13eebd77f1f1eaa0fb01383f7755
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-27 23:01:39 -0800

    Time: 0 ms (100.00%) | Memory: 6.4 MB (65.08%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.go
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -5,14 +5,13 @@
  *     Next *ListNode
  * }
  */
-
-
 func mergeKLists(lists []*ListNode) *ListNode {
-    if lists == nil || len(lists) == 0 {return nil}
+    if len(lists) == 0 {return nil}
     var dfs func(left, right int) *ListNode
-    dfs = func(left, right int) *ListNode {
+    dfs = func(left,right int) *ListNode {
         // base
         if left == right {return lists[left]}
+        if left > right {return nil}
 
         // logic
         mid := left + (right-left)/2
@@ -21,10 +20,13 @@ func mergeKLists(lists []*ListNode) *ListNode {
         return merge2Lists(l1, l2)
     }
     return dfs(0, len(lists)-1)
-
+    
 }
 
 func merge2Lists(l1, l2 *ListNode) *ListNode {
+    if l1 == nil && l2 == nil {return nil}
+    if l1 == nil && l2 != nil {return l2}
+    if l1 != nil && l2 == nil {return l1}
     head := &ListNode{Val: 0}
     tail := head
     for l1 != nil && l2 != nil {
@@ -50,44 +52,3 @@ func merge2Lists(l1, l2 *ListNode) *ListNode {
     }
     return head.Next
 }
-// func mergeKLists(lists []*ListNode) *ListNode {
-//     mn := &minHeap{nodes: []*ListNode{}}
-//     for i := 0; i < len(lists); i++ {
-//         if lists[i] == nil {continue}
-//         heap.Push(mn, lists[i])
-//     }
-//     head := &ListNode{Val: 0}
-//     tail := head
-//     for mn.Len() != 0 {
-//         top := heap.Pop(mn).(*ListNode)
-//         next := top.Next
-//         top.Next = nil
-//         tail.Next = top
-//         tail = tail.Next
-//         if next != nil { heap.Push(mn, next) }
-//     }
-//     return head.Next
-// }
-
-
-// type minHeap struct {
-// 	nodes []*ListNode
-// }
-
-// func (m *minHeap) Less(i, j int) bool {
-// 	return m.nodes[i].Val < m.nodes[j].Val
-// }
-// func (m *minHeap) Swap(i, j int) {
-// 	m.nodes[i], m.nodes[j] = m.nodes[j], m.nodes[i]
-// }
-// func (m *minHeap) Len() int {
-// 	return len(m.nodes)
-// }
-// func (m *minHeap) Push(x interface{}) {
-// 	m.nodes = append(m.nodes, x.(*ListNode))
-// }
-// func (m *minHeap) Pop() interface{} {
-// 	out := m.nodes[len(m.nodes)-1]
-// 	m.nodes = m.nodes[:len(m.nodes)-1]
-// 	return out
-// }
