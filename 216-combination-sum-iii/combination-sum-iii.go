e59a60223b31ebcd39b099816a9795f6700240dc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-27 19:04:36 -0800

    Time: 0 ms (100.00%) | Memory: 3.8 MB (30.65%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.go
+++ 216-combination-sum-iii/combination-sum-iii.go
@@ -1,25 +1,27 @@
 func combinationSum3(k int, n int) [][]int {
     out := [][]int{}
-    var dfs func(start, sum int, path []int)
-    dfs = func(start, sum int, path []int) {
+    var dfs func(start int, sum int, path []int)
+    dfs = func(start int, sum int, path []int) {
         // base
-        if sum > n || len(path) > k {return}
-
-        // logic
-        if sum == n && len(path) == k {
-            newL := make([]int, len(path))
-            copy(newL, path)
-            out = append(out, newL)
+        if len(path) == k {
+            if sum == n {
+                newL := make([]int, len(path))
+                copy(newL, path)
+                out = append(out, newL)
+            }
             return
         }
+        if sum > n {return}
+
+        // logic
         for i := start; i <= 9; i++ {
             sum += i
             path = append(path, i)
             dfs(i+1, sum, path)
             path = path[:len(path)-1]
             sum -= i
-        } 
+        }
     }
-    dfs(1, 0, nil)
+    dfs(1,0,nil)
     return out
 }
