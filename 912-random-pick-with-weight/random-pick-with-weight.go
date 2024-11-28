c6746ca5decd696c504684e46f237c53985be2b5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-27 18:25:59 -0800

    Time: 9 ms (94.74%) | Memory: 9.8 MB (35.91%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.go
+++ 912-random-pick-with-weight/random-pick-with-weight.go
@@ -1,36 +1,62 @@
+/*
+     0 1 2 3  
+    [2,1,4,3]
+
+    instead of prob of ith idx = i/4
+    its now based on weight
+
+    total weight = 10
+    instead of 4 choices, we now have to consider 10 idxs
+    0 -> 2/10
+    1 -> 1/10
+    2 -> 4/10
+    3 -> 3/10
+
+    [0,0,1,2,2,2,2,3,3,3]
+    prob -> 1/10
+
+    truncate the space    
+    [0(2), 1(1), 2(4), 3(3)]
+ 
+     0 1 2 3
+    [2,3,7,10]
+
+    idx 0 -> [0,2) -> 0,1
+    idx 1 -> [2,3) -> 2
+    idx 2 -> [3,7) -> 3,4,5,6
+    idx 3 -> [7,10) -> 7,8,9
+*/
 type Solution struct {
-    prefix []int
+    items []int    
 }
 
 
 func Constructor(w []int) Solution {
-    prefix := make([]int, len(w))
+    items := []int{}
     for i := 0; i < len(w); i++ {
-        prefix[i] = w[i]
-        if i-1 >= 0 {prefix[i] += prefix[i-1]}
+        items = append(items,w[i])
+        if i-1 >= 0{items[i] += items[i-1]}
     }
-    return Solution{prefix}
+    return Solution{items}
 }
 
 
-
 func (this *Solution) PickIndex() int {
-    n := len(this.prefix)
-    r := rand.Intn(this.prefix[n-1])
+    n := len(this.items)
+    r := rand.Intn(this.items[n-1])
     left := 0
     right := n-1
-    ans := 0
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
-        if this.prefix[mid] <= r {
-            left = mid+1
-        } else {
+        if this.items[mid] > r {
             ans = mid
             right = mid-1
+        } else {
+            left = mid+1
         }
     }
     return ans
-
 }
 
 
