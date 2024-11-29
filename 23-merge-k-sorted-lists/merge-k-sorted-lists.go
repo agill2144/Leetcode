b3807e46d022f965389ca0083ae572ea7f225dfc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-29 13:00:47 -0800

    Time: 4 ms (59.06%) | Memory: 6.6 MB (48.79%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.go
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -6,41 +6,46 @@
  * }
  */
 func mergeKLists(lists []*ListNode) *ListNode {
-    mn := &minHeap{items: []*ListNode{}}
-    for i := 0; i < len(lists); i++ {
-        if lists[i] == nil {continue}
-        heap.Push(mn, lists[i])
+    if len(lists) == 0 {return nil}
+    
+    var dfs func(left, right int) *ListNode
+    dfs = func(left, right int) *ListNode {
+        // base
+        if left > right {return nil}
+        if left == right {return lists[left]}
+
+        // logic
+        mid := left + (right-left)/2
+        l1 := dfs(left, mid)
+        l2 := dfs(mid+1, right)
+        return merge2Lists(l1,l2)        
     }
-    head := &ListNode{Val: 0}
+    return dfs(0, len(lists)-1)    
+}
+
+func merge2Lists(l1, l2 *ListNode) *ListNode {
+    head := &ListNode{Val:0}
     tail := head
-    for mn.Len() != 0 {
-        top := heap.Pop(mn).(*ListNode)
-        if top.Next != nil {heap.Push(mn, top.Next); top.Next = nil}
-        tail.Next = top
+    for l1 != nil && l2 != nil {
+        if l1.Val < l2.Val {
+            tail.Next = l1
+            tail = tail.Next
+            l1 = l1.Next
+        } else {
+            tail.Next = l2
+            tail = tail.Next
+            l2 = l2.Next
+        }
+    } 
+    for l1 != nil {
+        tail.Next = l1
         tail = tail.Next
+        l1 = l1.Next        
+    }
+    for l2 != nil {
+        tail.Next = l2
+        tail = tail.Next
+        l2 = l2.Next
     }
     return head.Next
-}
-
-
-type minHeap struct {
-	items []*ListNode
-}
-
-func (m *minHeap) Less(i, j int) bool {
-	return m.items[i].Val < m.items[j].Val
-}
-func (m *minHeap) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-func (m *minHeap) Len() int {
-	return len(m.items)
-}
-func (m *minHeap) Push(x interface{}) {
-	m.items = append(m.items, x.(*ListNode))
-}
-func (m *minHeap) Pop() interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
 }
