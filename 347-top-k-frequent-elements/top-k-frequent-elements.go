8ccf0bfe08fc1499538bf8b186ad34d85c38093f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-29 10:19:11 -0800

    Time: 1 ms (72.90%) | Memory: 7.9 MB (24.74%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -14,25 +14,3 @@ func topKFrequent(nums []int, k int) []int {
     }
     return out
 }
-
-type minHeap struct {
-	items [][]int // [ [val, freq] ]
-}
-
-func (m *minHeap) Less(i, j int) bool {
-	return m.items[i][1] < m.items[j][1]
-}
-func (m *minHeap) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-func (m *minHeap) Len() int {
-	return len(m.items)
-}
-func (m *minHeap) Push(x interface{}) {
-	m.items = append(m.items, x.([]int))
-}
-func (m *minHeap) Pop() interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
-}
