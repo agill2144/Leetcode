9cb7df192b2ef7ddc7322a34e4bf83f3347eb2c9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-28 19:46:30 -0800

    Time: 159 ms (73.33%) | Memory: 9.8 MB (73.81%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.go
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.go
@@ -1,35 +1,18 @@
 type SparseVector struct {
-    // [ [idx, val], [idx, val] ] - only contains items with non-zero values
-    items [][]int 
+    items []int
 }
 
 func Constructor(nums []int) SparseVector {
-    items := [][]int{}
-    for i := 0; i < len(nums); i++ {
-        if nums[i] == 0 {continue}
-        items = append(items, []int{i, nums[i]})
-    }
-    return SparseVector{items}
+    return SparseVector{nums}    
 }
 
 // Return the dotProduct of two sparse vectors
 func (this *SparseVector) dotProduct(vec SparseVector) int {
-    p1 := 0
-    p2 := 0
-    res := 0
-    for p1 < len(this.items) && p2 < len(vec.items) {
-        idx1 := this.items[p1][0]
-        idx2 := vec.items[p2][0]
-        if idx1 == idx2 {
-            res += (this.items[p1][1] * vec.items[p2][1])
-            p1++; p2++
-        } else if idx1 < idx2 {
-            p1++
-        } else {
-            p2++
-        }
+    sum := 0
+    for i := 0; i < len(this.items); i++ {
+        sum += (this.items[i] * vec.items[i])
     }
-    return res
+    return sum
 }
 
 /**
