7d93d27b83f889e4897a99e76b92d2123184f7bb
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-29 10:12:35 -0800

    Time: 2 ms (50.94%) | Memory: 8.4 MB (5.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,17 +1,37 @@
 func topKFrequent(nums []int, k int) []int {
     freq := map[int]int{}
     for i := 0; i < len(nums); i++ {freq[nums[i]]++}
-    sort.Slice(nums, func(i, j int)bool{
-        if freq[nums[i]] == freq[nums[j]] {
-            return nums[i] < nums[j]
-        }
-        return freq[nums[i]] < freq[nums[j]]
-    })
+
+    mn := &minHeap{items: [][]int{}}
+    for key, val := range freq {
+        heap.Push(mn, []int{key, val})
+        if mn.Len() > k {heap.Pop(mn)}
+    }
     out := []int{}
-    for i := len(nums)-1; i >= 0 && len(out) != k; i-- {
-        for i+1 < len(nums) && i >= 0 && nums[i] == nums[i+1] {i--;}
-        if i == -1 {break}
-        out = append(out, nums[i])
+    for mn.Len() != 0 {
+        out = append(out, heap.Pop(mn).([]int)[0])
     }
     return out
+}
+
+type minHeap struct {
+	items [][]int // [ [val, freq] ]
+}
+
+func (m *minHeap) Less(i, j int) bool {
+	return m.items[i][1] < m.items[j][1]
+}
+func (m *minHeap) Swap(i, j int) {
+	m.items[i], m.items[j] = m.items[j], m.items[i]
+}
+func (m *minHeap) Len() int {
+	return len(m.items)
+}
+func (m *minHeap) Push(x interface{}) {
+	m.items = append(m.items, x.([]int))
+}
+func (m *minHeap) Pop() interface{} {
+	out := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return out
 }
