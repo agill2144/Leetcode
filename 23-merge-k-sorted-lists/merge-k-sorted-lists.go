f3869c8461c4388cb107d04886aeac4e8b5a3938
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-29 12:51:16 -0800

    Time: 0 ms (100.00%) | Memory: 7.7 MB (13.57%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.go
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -6,40 +6,20 @@
  * }
  */
 func mergeKLists(lists []*ListNode) *ListNode {
-    if len(lists) == 0 {return nil}
-    pairIncr := 1
-    for pairIncr < len(lists) {
-        for i := 0; i + pairIncr < len(lists); i += (pairIncr*2) {
-            lists[i] = merge2Lists(lists[i], lists[i+pairIncr])
+    items := []int{}
+    for i := 0; i < len(lists); i++ {
+        curr := lists[i]
+        for curr != nil {
+            items = append(items, curr.Val)
+            curr = curr.Next
         }
-        pairIncr *= 2
     }
-    return lists[0]
-}
-
-func merge2Lists(l1, l2 *ListNode) *ListNode {
-    head := &ListNode{Val:0}
+    sort.Ints(items)
+    head := &ListNode{Val: 0}
     tail := head
-    for l1 != nil && l2 != nil {
-        if l1.Val < l2.Val {
-            tail.Next = l1
-            tail = tail.Next
-            l1 = l1.Next
-        } else {
-            tail.Next = l2
-            tail = tail.Next
-            l2 = l2.Next
-        }
-    } 
-    for l1 != nil {
-        tail.Next = l1
+    for i := 0; i < len(items); i++ {
+        tail.Next = &ListNode{Val: items[i]}
         tail = tail.Next
-        l1 = l1.Next        
-    }
-    for l2 != nil {
-        tail.Next = l2
-        tail = tail.Next
-        l2 = l2.Next
     }
     return head.Next
 }
