996b720fb43779d5e324e501e1bfbe63cb556531
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-29 20:59:22 -0800

    Time: 116 ms (12.50%) | Memory: 9.7 MB (26.90%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.go
@@ -0,0 +1,43 @@
+func kClosest(points [][]int, k int) [][]int {
+    type distNode struct {
+        dist float64
+        cord []int
+    }
+    dists := []distNode{}
+    n := len(points)
+    for i := 0; i < n; i++ {
+        dists = append(dists, distNode{
+            dist: calcDist(points[i][0], points[i][1]),
+            cord: points[i],
+        })
+    }
+    left := 0
+    right := len(dists)-1
+    targetIdx := k-1
+    for left <= right {
+        pivot := right
+        ns := left
+        for i := left; i < pivot; i++ {
+            if dists[i].dist <= dists[pivot].dist {
+                dists[i], dists[ns] = dists[ns], dists[i]
+                ns++
+            }
+        }
+        dists[ns], dists[pivot] = dists[pivot], dists[ns]
+        if ns == targetIdx {break}
+        if targetIdx > ns {
+            left = ns+1
+        } else {
+            right = ns-1
+        }
+    }
+    out := [][]int{}
+    for i := 0; i < k; i++ {
+        out = append(out, dists[i].cord)
+    }
+    return out
+}
+
+func calcDist(x, y int) float64 {
+    return math.Sqrt(float64((x*x) + (y*y)))
+}
