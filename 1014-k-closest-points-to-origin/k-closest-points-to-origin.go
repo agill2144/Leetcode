8889a9c6e5a9cfb2070e60a5e3792888d028428a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-30 14:45:33 -0800

    Time: 66 ms (21.52%) | Memory: 9.6 MB (27.31%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.go
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.go
@@ -2,25 +2,93 @@ type distNode struct {
     dist float64
     cord []int
 }
+// appraoch: quick select
+// we want k closest to origin
+// origin is 0,0 -> euclidean dist of origin is 0
+// i.e we want k smallest distances
+// using quick select, we can make k size partition on left
 func kClosest(points [][]int, k int) [][]int {
-    mx := &maxHeap{items: []*distNode{}}
+    distances := []*distNode{}
     for i := 0; i < len(points); i++ {
         node := &distNode{
             dist: calcDist(points[i][0],points[i][1]),
             cord: points[i],
         }
-        heap.Push(mx, node)
-        if mx.Len() > k {
-            heap.Pop(mx)
+        distances = append(distances, node)
+    }
+    left := 0
+    right := len(distances)-1
+    for left <= right {
+        pivot := right
+        nextSmaller := left
+        for i := left; i < pivot; i++ {
+            if distances[i].dist <= distances[pivot].dist {
+                distances[i], distances[nextSmaller] = distances[nextSmaller], distances[i]
+                nextSmaller++
+            }
+        }
+        distances[pivot], distances[nextSmaller] = distances[nextSmaller], distances[pivot]
+        if nextSmaller == k-1 {
+            break
+        } else if k-1 > nextSmaller {
+            left = nextSmaller+1
+        } else {
+            right = nextSmaller-1
         }
     }
+
     out := [][]int{}
-    for mx.Len() != 0 {
-        top := heap.Pop(mx).(*distNode)
-        out = append(out, top.cord)
+    for i := 0; i < k; i++ {
+        out = append(out, distances[i].cord)
     }
     return out
 }
+
+// approach: max heap
+// tc = o(n * logk) + o(k)
+// sc = o(k)
+// func kClosest(points [][]int, k int) [][]int {
+//     mx := &maxHeap{items: []*distNode{}}
+//     for i := 0; i < len(points); i++ {
+//         node := &distNode{
+//             dist: calcDist(points[i][0],points[i][1]),
+//             cord: points[i],
+//         }
+//         heap.Push(mx, node)
+//         if mx.Len() > k {
+//             heap.Pop(mx)
+//         }
+//     }
+//     out := [][]int{}
+//     for mx.Len() != 0 {
+//         top := heap.Pop(mx).(*distNode)
+//         out = append(out, top.cord)
+//     }
+//     return out
+// }
+// type maxHeap struct {
+// 	items []*distNode
+// }
+
+// func (m *maxHeap) Less(i, j int) bool {
+// 	return m.items[i].dist > m.items[j].dist
+// }
+// func (m *maxHeap) Swap(i, j int) {
+// 	m.items[i], m.items[j] = m.items[j], m.items[i]
+// }
+// func (m *maxHeap) Len() int {
+// 	return len(m.items)
+// }
+// func (m *maxHeap) Push(x interface{}) {
+// 	m.items = append(m.items, x.(*distNode))
+// }
+// func (m *maxHeap) Pop() interface{} {
+// 	out := m.items[len(m.items)-1]
+// 	m.items = m.items[:len(m.items)-1]
+// 	return out
+// }
+
+// approach: brute force / sort
 // tc = o(n) + o(nlogn) + o(k) = o(nlogn)
 // sc = o(n) + o(n) = o(n)
 // func kClosest(points [][]int, k int) [][]int {
@@ -54,27 +122,6 @@ func kClosest(points [][]int, k int) [][]int {
 //     return out
 // }
 
-type maxHeap struct {
-	items []*distNode
-}
-
-func (m *maxHeap) Less(i, j int) bool {
-	return m.items[i].dist > m.items[j].dist
-}
-func (m *maxHeap) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-func (m *maxHeap) Len() int {
-	return len(m.items)
-}
-func (m *maxHeap) Push(x interface{}) {
-	m.items = append(m.items, x.(*distNode))
-}
-func (m *maxHeap) Pop() interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
-}
 
 
 func calcDist(x, y int) float64 {
