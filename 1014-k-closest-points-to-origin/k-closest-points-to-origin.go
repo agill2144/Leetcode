7a63e60ccf6b1fd43804ef10102babbdd5d53c8a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-30 14:00:59 -0800

    Time: 31 ms (58.28%) | Memory: 9.3 MB (47.45%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.go
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.go
@@ -1,43 +1,82 @@
+type distNode struct {
+    dist float64
+    cord []int
+}
 func kClosest(points [][]int, k int) [][]int {
-    type distNode struct {
-        dist int
-        cord []int
-    }
-    dists := []distNode{}
-    n := len(points)
-    for i := 0; i < n; i++ {
-        dists = append(dists, distNode{
-            dist: calcDist(points[i][0], points[i][1]),
+    mx := &maxHeap{items: []*distNode{}}
+    for i := 0; i < len(points); i++ {
+        node := &distNode{
+            dist: calcDist(points[i][0],points[i][1]),
             cord: points[i],
-        })
-    }
-    left := 0
-    right := len(dists)-1
-    targetIdx := k-1
-    for left <= right {
-        pivot := right
-        ns := left
-        for i := left; i < pivot; i++ {
-            if dists[i].dist <= dists[pivot].dist {
-                dists[i], dists[ns] = dists[ns], dists[i]
-                ns++
-            }
         }
-        dists[ns], dists[pivot] = dists[pivot], dists[ns]
-        if ns == targetIdx {break}
-        if targetIdx > ns {
-            left = ns+1
-        } else {
-            right = ns-1
+        heap.Push(mx, node)
+        if mx.Len() > k {
+            heap.Pop(mx)
         }
     }
     out := [][]int{}
-    for i := 0; i < k; i++ {
-        out = append(out, dists[i].cord)
+    for mx.Len() != 0 {
+        top := heap.Pop(mx).(*distNode)
+        out = append(out, top.cord)
     }
     return out
 }
+// tc = o(n) + o(nlogn) + o(k) = o(nlogn)
+// sc = o(n) + o(n) = o(n)
+// func kClosest(points [][]int, k int) [][]int {
+//     type distNode struct {
+//         dist float64
+//         cord []int
+//     }
 
-func calcDist(x, y int) int {
-    return (x*x) + (y*y)
+//     // sc = o(n)
+//     distances := []*distNode{}
+
+//     // n = len(points)
+//     // tc = o(n)
+//     for i := 0; i < len(points); i++ {
+//         distances = append(distances, &distNode{
+//             dist: calcDist(points[i][0], points[i][1]),
+//             cord: points[i],
+//         })
+//     }
+
+//     // tc = o(nlogn)
+//     sort.Slice(distances, func(i, j int)bool{
+//         return distances[i].dist < distances[j].dist
+//     })
+
+//     out := [][]int{}
+//     //tc = o(k)
+//     for i := 0; i < k; i++ {
+//         out = append(out, distances[i].cord)
+//     }
+//     return out
+// }
+
+type maxHeap struct {
+	items []*distNode
+}
+
+func (m *maxHeap) Less(i, j int) bool {
+	return m.items[i].dist > m.items[j].dist
+}
+func (m *maxHeap) Swap(i, j int) {
+	m.items[i], m.items[j] = m.items[j], m.items[i]
+}
+func (m *maxHeap) Len() int {
+	return len(m.items)
+}
+func (m *maxHeap) Push(x interface{}) {
+	m.items = append(m.items, x.(*distNode))
+}
+func (m *maxHeap) Pop() interface{} {
+	out := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return out
+}
+
+
+func calcDist(x, y int) float64 {
+    return math.Sqrt(float64(x*x) + float64(y*y))
 }
