ac201cbda1c59e38eb0f8ab3f8a77599207af87d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-01 14:06:00 -0800

    Time: 0 ms (100.00%) | Memory: 8.4 MB (9.17%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.go
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -1,54 +1,18 @@
 func trap(height []int) int {
     n := len(height)
-    leftWall := 0
-    rightWall := n-1
-    left, right := 0, n-1
-    total := 0
-    for left < right {
-        if height[rightWall] >= height[leftWall] {
-            if height[leftWall] >= height[left] {
-                total += (height[leftWall]-height[left])
-                left++
-            } else {
-                leftWall = left
-            }
-        } else {
-            if height[rightWall] >= height[right] {
-                total += (height[rightWall]-height[right])
-                right--
-            } else {
-                rightWall = right
-            }
+    left := make([]int, n)
+    right := make([]int, n)
+    for i := 1; i < n-1; i++ {left[i] = max(left[i-1], height[i-1])}
+    for i := n-2; i >= 0; i-- { right[i] = max(right[i+1], height[i+1]) }
 
+    total := 0
+    for i := 1; i < n-1; i++ {
+        curr := height[i]
+        unit := min(left[i], right[i]) - curr
+        if unit > 0 {
+            total += unit
         }
     }
+
     return total
 }
-
-
-// func trap(height []int) int {
-//     n := len(height)
-//     // space = o(n)
-//     maxL := make([]int, n)
-//     // space = o(n)
-//     maxR := make([]int, n)
-//     // time = o(2n)
-//     for i := 1; i < n; i++ {maxL[i] = max(height[i-1], maxL[i-1])}
-//     for i := n-2; i >= 0; i-- {maxR[i] = max(height[i+1], maxR[i+1])}
-
-//     total := 0
-//     // time = o(n)
-//     // total = o(3n) or o(n)
-//     // space = o(2n) or o(n)
-//     for i := 1; i < n-1; i++ {
-//         h := height[i]
-//         // we can trap on top of this
-//         // if there is a height on left and right greater than itself
-//         lh := maxL[i]
-//         rh := maxR[i]
-//         if lh > h && rh > h {
-//             total += min(lh,rh)-h
-//         }
-//     }
-//     return total
-// }
