225cc2972275e70c57c69ebb9fda8fe3f48ee833
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-30 20:37:48 -0800

    Time: 0 ms (100.00%) | Memory: 6 MB (43.79%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.go
+++ 692-top-k-frequent-words/top-k-frequent-words.go
@@ -3,20 +3,48 @@ func topKFrequent(words []string, k int) []string {
     for i := 0; i < len(words); i++ {
         freq[words[i]]++
     }
-    mn := &minHeap{items: []*item{}}
-    for word, count := range freq {
-        heap.Push(mn, &item{word, count}) 
-        if mn.Len() > k {
-            heap.Pop(mn)
+    deduped := []string{}
+    for k, _ := range freq {deduped = append(deduped, k)}
+    targetIdx := len(deduped)-k  
+    left := 0
+    right := len(deduped)-1
+    for left <= right {
+        pivot := right
+        nsf := left
+        for i := left; i < pivot; i++ {
+            iVal := deduped[i]
+            pivotVal := deduped[pivot]
+            iFreq := freq[iVal]
+            pivotFreq := freq[pivotVal]
+            if iFreq == pivotFreq {
+                // fmt.Println("same freq! ", iVal, pivotVal, strings.Compare(iVal, pivotVal))
+                //The result will be 0 if a == b, -1 if a < b, and +1 if a > b.
+                if strings.Compare(iVal, pivotVal) == 1 {
+                    deduped[nsf], deduped[i] = deduped[i], deduped[nsf]
+                    nsf++
+                }
+            } else if iFreq < pivotFreq {
+                deduped[nsf], deduped[i] = deduped[i], deduped[nsf]
+                nsf++
+            }
+        }
+        deduped[nsf], deduped[pivot] = deduped[pivot], deduped[nsf]
+        if nsf == targetIdx {break}
+        if targetIdx > nsf {
+            left = nsf+1
+        } else {
+            right = nsf-1
         }
     }
-    out := make([]string, mn.Len())
-    ptr := len(out)-1
-    for mn.Len() != 0 {
-        out[ptr] = heap.Pop(mn).(*item).word
-        ptr--
-    }
-    
+    // fmt.Println(freq, deduped, deduped[targetIdx:])
+    out := deduped[targetIdx:]
+    sort.Slice(out, func(i, j int)bool{
+        if freq[out[i]] == freq[out[j]] {
+            //The result will be 0 if a == b, -1 if a < b, and +1 if a > b.
+            return strings.Compare(out[i], out[j]) == -1
+        }
+        return freq[out[i]] > freq[out[j]]
+    })
     return out
 }
 
