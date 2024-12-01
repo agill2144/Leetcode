5b130293e7a2837ad9e3dfd8a23b39da975212d2
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-30 20:52:13 -0800

    Time: 0 ms (100.00%) | Memory: 6.1 MB (31.36%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.go
+++ 692-top-k-frequent-words/top-k-frequent-words.go
@@ -4,7 +4,6 @@ func topKFrequent(words []string, k int) []string {
         freq[words[i]]++
     }
     deduped := []string{}
-    sort.Strings(deduped)
     for k, _ := range freq {deduped = append(deduped, k)}
     targetIdx := len(deduped)-k  
     left := 0
@@ -49,36 +48,4 @@ func topKFrequent(words []string, k int) []string {
         return freq[out[i]] > freq[out[j]]
     })
     return out
-}
-
-
-type item struct {
-    word string
-    freq int
-}
-
-type minHeap struct {
-	items []*item
-}
-
-func (m *minHeap) Less(i, j int) bool {
-    if m.items[i].freq == m.items[j].freq {
-        // The result will be 0 if a == b, -1 if a < b, and +1 if a > b.
-        return strings.Compare(m.items[i].word, m.items[j].word) == 1
-    }
-    return m.items[i].freq < m.items[j].freq
-}
-func (m *minHeap) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-func (m *minHeap) Len() int {
-	return len(m.items)
-}
-func (m *minHeap) Push(x interface{}) {
-	m.items = append(m.items, x.(*item))
-}
-func (m *minHeap) Pop() interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
 }
