05ee0bd7d0bba4dd87a9f5922868bb0b5ff2a58d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-30 23:07:34 -0800

    Time: 3 ms (58.62%) | Memory: 4.7 MB (14.05%) - LeetSync

--- 133-clone-graph/clone-graph.go
+++ 133-clone-graph/clone-graph.go
@@ -6,27 +6,20 @@
  * }
  */
 
-/*
-    approach: 2 pass with hashmap
-    - traverse the nodes and create copies
-    - save copy into a hashmap { orig: copy }
-    - then take a 2nd pass and create the edges
-
-*/
 func cloneGraph(node *Node) *Node {
+    if node == nil {return node}
     origToCp := map[*Node]*Node{}
     var dfs func(curr *Node)
     dfs = func(curr *Node) {
         // base
-        if curr == nil {return}
         if _, ok := origToCp[curr]; ok {return}
 
         // logic
-        cp := &Node{Val: curr.Val}
-        origToCp[curr] = cp
+        clone := &Node{Val: curr.Val}
+        origToCp[curr] = clone
         for _, nei := range curr.Neighbors {
             dfs(nei)
-            cp.Neighbors = append(cp.Neighbors, origToCp[nei])
+            clone.Neighbors = append(clone.Neighbors, origToCp[nei])
         }
     }
     dfs(node)
