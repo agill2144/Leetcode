8f572004360e5522da29b3f21e1fa13eb9c31133
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-30 23:14:39 -0800

    Time: 3 ms (58.62%) | Memory: 4.8 MB (14.05%) - LeetSync

--- 133-clone-graph/clone-graph.go
+++ 133-clone-graph/clone-graph.go
@@ -8,20 +8,19 @@
 
 func cloneGraph(node *Node) *Node {
     if node == nil {return node}
-    origToCp := map[*Node]*Node{}
-    var dfs func(curr *Node)
-    dfs = func(curr *Node) {
-        // base
-        if _, ok := origToCp[curr]; ok {return}
-
-        // logic
-        clone := &Node{Val: curr.Val}
-        origToCp[curr] = clone
-        for _, nei := range curr.Neighbors {
-            dfs(nei)
-            clone.Neighbors = append(clone.Neighbors, origToCp[nei])
+    origToCp := map[*Node]*Node{node: &Node{Val:node.Val}}
+    q := []*Node{node}
+    for len(q) != 0 {
+        dq := q[0]
+        q = q[1:]
+        for _, nei := range dq.Neighbors {
+            _, ok := origToCp[nei]
+            if !ok {
+                origToCp[nei] = &Node{Val: nei.Val}
+                q = append(q, nei)
+            }
+            origToCp[dq].Neighbors = append(origToCp[dq].Neighbors, origToCp[nei])
         }
     }
-    dfs(node)
     return origToCp[node]
 }
