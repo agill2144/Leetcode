b0e1c234953e141b91f802db9c19656fad6d4a67
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-11-30 16:29:37 -0800

    Time: 152 ms (13.30%) | Memory: 6.6 MB (49.42%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.go
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -6,24 +6,27 @@
  * }
  */
 func mergeKLists(lists []*ListNode) *ListNode {
-    if len(lists) == 0 {return nil}
-    iIncr := 2
-    pairIncr := 1
-    for pairIncr < len(lists) {
-        for i := 0; i + pairIncr < len(lists); i+=iIncr {
-            lists[i] = merge2Lists(lists[i], lists[i+pairIncr])
-        }
-        iIncr *= 2
-        pairIncr *= 2
+    k := len(lists)
+    if k == 0 {return nil}
+    head := lists[0]
+    for i := 1; i < len(lists); i++ {
+        head = merge2Lists(head, lists[i])
     }
-    return lists[0]
+    return head
 }
 
 func merge2Lists(l1, l2 *ListNode) *ListNode {
-    head := &ListNode{Val:0}
+    head := &ListNode{Val: 0}
     tail := head
-    for l1 != nil && l2 != nil {
-        if l1.Val < l2.Val {
+    for l1 != nil || l2 != nil {
+        l1Val := math.MaxInt64
+        if l1 != nil {l1Val = l1.Val}
+        l2Val := math.MaxInt64
+        if l2 != nil {l2Val = l2.Val}
+
+        if l1Val == math.MaxInt64 && l2Val == math.MaxInt64 {break}
+
+        if l1Val < l2Val {
             tail.Next = l1
             tail = tail.Next
             l1 = l1.Next
@@ -32,16 +35,6 @@ func merge2Lists(l1, l2 *ListNode) *ListNode {
             tail = tail.Next
             l2 = l2.Next
         }
-    } 
-    for l1 != nil {
-        tail.Next = l1
-        tail = tail.Next
-        l1 = l1.Next        
-    }
-    for l2 != nil {
-        tail.Next = l2
-        tail = tail.Next
-        l2 = l2.Next
     }
     return head.Next
 }
