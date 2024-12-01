7b3066ae1549332e9a834e47248501e9a9b3bcfc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-01 14:35:41 -0800

    Time: 0 ms (100.00%) | Memory: 5.5 MB (8.00%) - LeetSync

--- 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.go
+++ 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.go
@@ -3,9 +3,9 @@ func calculateTax(brackets [][]int, income int) float64 {
     for i := 0; i < len(brackets); i++ {
         upper, taxRate := brackets[i][0], float64(brackets[i][1])/100.0
         if i-1 >= 0 {upper = brackets[i][0]-brackets[i-1][0]}
-        taxableIncome := min(upper, income)
-        taxes += (float64(taxableIncome) * taxRate)
-        income -= taxableIncome
+        incomeToBeTaxed := min(upper, income)
+        taxes += float64(incomeToBeTaxed) * (taxRate)
+        income -= incomeToBeTaxed
     }
     return taxes
 }
