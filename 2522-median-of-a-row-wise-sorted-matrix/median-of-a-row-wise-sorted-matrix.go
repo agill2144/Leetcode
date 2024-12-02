4c192a5d7e00a3944ef2dc5903d5b51223f36ae0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-01 18:28:52 -0800

    Time: 2 ms (100.00%) | Memory: 12.1 MB (100.00%) - LeetSync

--- 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
+++ 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
@@ -1,40 +1,46 @@
-func matrixMedian(grid [][]int) int {
+func matrixMedian(matrix [][]int) int {
+    m := len(matrix)
+    n := len(matrix[0])
     left := 1
     right := 1000000
-    n := len(grid) * len(grid[0])
+    totalEle := m*n
+    minCount := (totalEle/2)+1
     ans := -1
     for left <= right {
         mid := left + (right-left)/2
-        count := countOnLeft(grid, mid)
-        if count >= (n/2)+1 {
+        count := countLessThanOrEqualTo(matrix, mid)
+        // count <= median value
+        // MUST ALWAYS BE midIdx+1 ( at the very least! )
+        // therefore if count < minCount
+        // search right
+        if count < minCount {
+            left = mid+1
+        } else {
             ans = mid
             right = mid-1
-        } else {
-            left = mid+1
         }
     }
     return ans
 }
 
-func countOnLeft(grid [][]int, target int) int {
-    m := len(grid)
-    n := len(grid[0])
-    count := 0
+func countLessThanOrEqualTo(matrix [][]int, target int) int {
+    total := 0
+    m := len(matrix)
+    n := len(matrix[0])
     for i := 0; i < m; i++ {
+        idx := -1
         left := 0
         right := n-1
-        // find the right most idx on left side of target ( target may exists, find the right most position)
-        idx := -1
         for left <= right {
             mid := left + (right-left)/2
-            if grid[i][mid] <= target {
+            if matrix[i][mid] <= target {
                 idx = mid
                 left = mid+1
             } else {
                 right = mid-1
             }
         }
-        count += idx+1
+        total += idx+1
     }
-    return count
+    return total
 }
