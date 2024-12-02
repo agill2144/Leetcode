b901872c4c5238e79c9d53fb7460f0d741050c73
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-01 19:12:50 -0800

    Time: 1 ms (50.00%) | Memory: 9.7 MB (100.00%) - LeetSync

--- 2369-maximum-sum-score-of-array/maximum-sum-score-of-array.go
+++ 2369-maximum-sum-score-of-array/maximum-sum-score-of-array.go
@@ -1,17 +1,14 @@
 func maximumSumScore(nums []int) int64 {
-    var ans int64 = math.MinInt64
-    var total int64
-    for i := 0; i < len(nums); i++ {total += int64(nums[i])}
-    var rSum int64
+    var total int64 = 0
+    for i := 0; i < len(nums); i++ {
+        total += int64(nums[i])
+    }
+    var maxSum int64 = math.MinInt64
+    var rSum int64 = 0
     for i := 0; i < len(nums); i++ {
         rSum += int64(nums[i])
-        rightSum := total - rSum + int64(nums[i])
-        ans = max(ans, max(rSum, rightSum))
+        var rightSum int64 = total-rSum + int64(nums[i])
+        maxSum = max(maxSum, max(rSum, rightSum))
     }
-    return ans
-}
-
-func max(x, y int64) int64 {
-    if x > y {return x}
-    return y
+    return maxSum
 }
