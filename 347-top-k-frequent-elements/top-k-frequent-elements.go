968905458ca8bd40abbfd2e9dbafe35e177ad240
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-03 19:05:57 -0800

    Time: 1 ms (73.46%) | Memory: 8.2 MB (11.78%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,37 +1,15 @@
 func topKFrequent(nums []int, k int) []int {
     freq := map[int]int{}
     for i := 0; i < len(nums); i++ {freq[nums[i]]++}
-    mn := &minHeap{items: [][]int{}}
+    bucket := make([][]int, len(nums)+1)
     for num, count := range freq {
-        heap.Push(mn, []int{num, count})
-        if mn.Len() > k {heap.Pop(mn)}
+        if bucket[count] == nil {bucket[count] = []int{}}
+        bucket[count] = append(bucket[count], num)
     }
     out := []int{}
-    for mn.Len() != 0 {
-        out = append(out, heap.Pop(mn).([]int)[0])
+    for i := len(bucket)-1; i >= 0 && len(out) != k; i-- {
+        out = append(out, bucket[i]...)
     }
     return out
 }
 
-
-type minHeap struct {
-	items [][]int // [[node, freq]]
-}
-
-func (m *minHeap) Less(i, j int) bool {
-	return m.items[i][1] < m.items[j][1]
-}
-func (m *minHeap) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-func (m *minHeap) Len() int {
-	return len(m.items)
-}
-func (m *minHeap) Push(x interface{}) {
-	m.items = append(m.items, x.([]int))
-}
-func (m *minHeap) Pop() interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
-}
