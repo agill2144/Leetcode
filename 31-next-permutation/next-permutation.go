53494b7d3874d5585083dc86cf1ba81923d5209f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-03 19:23:43 -0800

    Time: 0 ms (100.00%) | Memory: 4.4 MB (20.00%) - LeetSync

--- 31-next-permutation/next-permutation.go
+++ 31-next-permutation/next-permutation.go
@@ -1,27 +1,29 @@
 func nextPermutation(nums []int)  {
+    breachIdx := -1
     n := len(nums)
-	if n <= 1 {return}
-	breachIdx := -1
-	for i := n-2; i >= 0; i-- {
-		if nums[i] < nums[i+1] {
-			breachIdx = i
-			break
-		}
-	}
-	if breachIdx != -1 {
-		ns := -1 // idx
-		for i := n-1; i > breachIdx; i-- {
-			if nums[i] > nums[breachIdx] && (ns == -1 || nums[i] < nums[ns]) {
-				ns = i
-			}
-		}
-		nums[breachIdx], nums[ns] = nums[ns], nums[breachIdx]
-	}
-	left := breachIdx+1
-	right := n-1
-	for left < right {
-		nums[left], nums[right] = nums[right], nums[left]
-		left++
-		right--
-	}    
+    for i := n-1; i >= 0; i-- {
+        curr := nums[i]
+        next := curr
+        if i+1 < n {next = nums[i+1]}
+        if curr < next {breachIdx = i; break}
+    }
+
+    if breachIdx != -1 {
+        nextGreaterIdx := -1
+        for i := n-1; i > breachIdx; i-- {
+            if nums[i] > nums[breachIdx] && (nextGreaterIdx == -1 || nums[i] < nums[nextGreaterIdx]) {
+                nextGreaterIdx = i
+            }
+        }
+        nums[breachIdx], nums[nextGreaterIdx] = nums[nextGreaterIdx], nums[breachIdx]
+    }
+
+    left := breachIdx+1
+    right := n-1
+    for left < right {
+        nums[left], nums[right] = nums[right], nums[left]
+        left++
+        right--
+    }
+    
 }
