209890dc85b74017dd31f98fdb3ce81c7f464932
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-04 00:15:24 -0800

    Time: 317 ms (5.21%) | Memory: 21.3 MB (8.00%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.go
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.go
@@ -1,16 +1,24 @@
 type SparseVector struct {
-    items []int
+    idx map[int]int // {idxFromNums: valueAtThatIdx}
 }
 
 func Constructor(nums []int) SparseVector {
-    return SparseVector{items: nums}    
+    idx := map[int]int{}
+    for i := 0; i < len(nums); i++ {
+        idx[i] = nums[i]
+    }
+    return SparseVector{idx}
 }
 
 // Return the dotProduct of two sparse vectors
 func (this *SparseVector) dotProduct(vec SparseVector) int {
+    if len(vec.idx) < len(this.idx) {return vec.dotProduct(*this)}
     sum := 0
-    for i := 0; i < len(this.items); i++ {
-        sum += (this.items[i] * vec.items[i])
+    for i, val := range this.idx {
+        val2 := vec.idx[i]
+        if val2 != 0 {
+            sum += (val*val2)
+        }
     }
     return sum
 }
