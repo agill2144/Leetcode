ef5049323610e1c4142121cd4caaab2f41fde98c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-03 19:13:45 -0800

    Time: 6 ms (15.16%) | Memory: 8 MB (30.25%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,15 +1,28 @@
 func topKFrequent(nums []int, k int) []int {
     freq := map[int]int{}
     for i := 0; i < len(nums); i++ {freq[nums[i]]++}
-    bucket := make([][]int, len(nums)+1)
-    for num, count := range freq {
-        if bucket[count] == nil {bucket[count] = []int{}}
-        bucket[count] = append(bucket[count], num)
+    deduped := []int{}
+    for k, _ := range freq {deduped = append(deduped, k)}
+    left := 0
+    right := len(deduped)-1
+    targetIdx := len(deduped)-k
+    for left <= right {
+        pivot := right
+        nsf := left
+        for i := left; i < pivot; i++ {
+            if freq[deduped[i]] <= freq[deduped[pivot]] {
+                deduped[i], deduped[nsf] = deduped[nsf], deduped[i]
+                nsf++
+            }
+        }
+        deduped[nsf], deduped[pivot] = deduped[pivot], deduped[nsf]
+        if nsf == targetIdx {return deduped[nsf:]}
+        if targetIdx > nsf {
+            left = nsf+1
+        } else {
+            right = nsf-1
+        }
     }
-    out := []int{}
-    for i := len(bucket)-1; i >= 0 && len(out) != k; i-- {
-        out = append(out, bucket[i]...)
-    }
-    return out
+    return nil
 }
 
