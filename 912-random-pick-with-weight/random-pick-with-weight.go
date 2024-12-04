7c265dc18b80c9d9e2bda6098ff327acbd1d4d5f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-03 23:32:20 -0800

    Time: 15 ms (69.62%) | Memory: 9.2 MB (72.80%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.go
+++ 912-random-pick-with-weight/random-pick-with-weight.go
@@ -1,38 +1,32 @@
-
 type Solution struct {
-    // compressedIdxRanges represents the repetition of each index as ranges in a compact form.
-    compressedIdxRanges []int
+    compressedRanges []int
 }
 
 
 func Constructor(w []int) Solution {
-    compressedIdxRanges := []int{}
-    for i := 0; i < len(w); i++ {
-        compressedIdxRanges = append(compressedIdxRanges,w[i])
-        if i-1 >= 0 {compressedIdxRanges[i] += compressedIdxRanges[i-1]}
+    c := []int{w[0]}    
+    for i := 1; i < len(w); i++ {
+        c = append(c, w[i] + c[i-1])
     }
-    return Solution{compressedIdxRanges}
+    return Solution{c}
 }
 
 
 func (this *Solution) PickIndex() int {
-    n := len(this.compressedIdxRanges)
-    r := rand.Intn(this.compressedIdxRanges[n-1])
+    n := len(this.compressedRanges)
+    maxRange := this.compressedRanges[n-1]
+    r := rand.Intn(maxRange)
+    ans := -1
     left := 0
     right := n-1
-    ans := -1
-    // r is our target and we need to find 
-    // the range where our target lies in
-    // when is mid our potential ans?
-    // r = 4
-    // when mid value is 3 and mid+1 value is 7
-    // meaning range from mid is from [3 -> 7)
-    // from 3 to 7 excluding 7
-    // therefore 4 falls under this range, this is a potential answer, save it and search left
-    // because on the right side, the range will increase as the cumulative sum increases
+    // mid position tells us the ending of range at mid idx
+    // EXCLUDING mid value
+    // if mid idx(2) val is 3
+    // it means, the range at mid pos ends at 3( does not include 3)
+    // we want to a smallest such mid where r IN the range (not equal to, but r < mid range )
     for left <= right {
         mid := left + (right-left)/2
-        if r < this.compressedIdxRanges[mid]  {
+        if r < this.compressedRanges[mid] {
             ans = mid
             right = mid-1
         } else {
@@ -40,6 +34,7 @@ func (this *Solution) PickIndex() int {
         }
     }
     return ans
+    
 }
 
 
