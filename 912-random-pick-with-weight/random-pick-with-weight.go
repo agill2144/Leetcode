a45a3a2dbb99e5df9b9bef1394a16f802052f9d2
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-03 22:57:35 -0800

    Time: 18 ms (51.90%) | Memory: 11.6 MB (5.02%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.go
+++ 912-random-pick-with-weight/random-pick-with-weight.go
@@ -1,55 +1,38 @@
-/*
-     0 1 2 3  
-    [2,1,4,3]
 
-    instead of prob of ith idx = i/4
-    its now based on weight
-
-    total weight = 10
-    instead of 4 choices, we now have to consider 10 idxs
-    0 -> 2/10
-    1 -> 1/10
-    2 -> 4/10
-    3 -> 3/10
-
-    [0,0,1,2,2,2,2,3,3,3]
-    prob -> 1/10
-
-    truncate the space    
-    [0(2), 1(1), 2(4), 3(3)]
- 
-     0 1 2 3
-    [2,3,7,10]
-
-    idx 0 -> [0,2) -> 0,1
-    idx 1 -> [2,3) -> 2
-    idx 2 -> [3,7) -> 3,4,5,6
-    idx 3 -> [7,10) -> 7,8,9
-*/
 type Solution struct {
-    items []int    
+    // compressedIdxRanges represents the repetition of each index as ranges in a compact form.
+    compressedIdxRanges []int
 }
 
 
 func Constructor(w []int) Solution {
-    items := []int{}
+    compressedIdxRanges := []int{}
     for i := 0; i < len(w); i++ {
-        items = append(items,w[i])
-        if i-1 >= 0{items[i] += items[i-1]}
+        compressedIdxRanges = append(compressedIdxRanges,w[i])
+        if i-1 >= 0 {compressedIdxRanges[i] += compressedIdxRanges[i-1]}
     }
-    return Solution{items}
+    return Solution{compressedIdxRanges}
 }
 
 
 func (this *Solution) PickIndex() int {
-    n := len(this.items)
-    r := rand.Intn(this.items[n-1])
+    n := len(this.compressedIdxRanges)
+    r := rand.Intn(this.compressedIdxRanges[n-1])
     left := 0
     right := n-1
     ans := -1
+    // r is our target and we need to find 
+    // the range where our target lies in
+    // when is mid our potential ans?
+    // r = 4
+    // when mid value is 3 and mid+1 value is 7
+    // meaning range from mid is from [3 -> 7)
+    // from 3 to 7 excluding 7
+    // therefore 4 falls under this range, this is a potential answer, save it and search left
+    // because on the right side, the range will increase as the cumulative sum increases
     for left <= right {
         mid := left + (right-left)/2
-        if this.items[mid] > r {
+        if r < this.compressedIdxRanges[mid]  {
             ans = mid
             right = mid-1
         } else {
