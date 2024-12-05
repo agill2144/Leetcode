4ebee11ce829c961e3007e4cfba7349e53690061
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-04 19:31:58 -0800

    Time: 151 ms (13.09%) | Memory: 6.6 MB (65.60%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.go
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -5,84 +5,30 @@
  *     Next *ListNode
  * }
  */
-
 func mergeKLists(lists []*ListNode) *ListNode {
-    k := len(lists)
-    if k == 0 {return nil}
-    pairIncr := 1
-    iIncr := 2
-    for pairIncr < k {
-        for i := 0; i + pairIncr < k; i += iIncr {
-            lists[i] = merge2Lists(lists[i], lists[i+pairIncr])
-        }
-        pairIncr *= 2
-        iIncr *= 2
+    if len(lists) == 0 {return nil}
+    head := lists[0]
+    for i := 1; i < len(lists); i++ {
+        head = merge2Lists(head, lists[i])
     }
-    return lists[0]
+    return head
 }
-
-
-// approach: divide and conquer / merge sort ( recursive )
-// tc = o(N * logk)
-// sc = o(k) for recursive depth
-// func mergeKLists(lists []*ListNode) *ListNode {
-//     k := len(lists)
-//     if k == 0 {return nil}
-//     var dfs func(left, right int) *ListNode
-//     dfs = func(left, right int) *ListNode {
-//         // base
-//         if left == right {return lists[left]}
-//         if left > right {return nil}
-
-//         // logic
-//         mid := left + (right-left)/2
-//         l1 := dfs(left, mid)
-//         l2 := dfs(mid+1, right)
-//         return merge2Lists(l1, l2)
-//     }
-//     return dfs(0, k-1)
-// }
-
-
-
-
-
-// approach: brute force
-// merge lists from 1 to n-1 into lists[0]
-// return lists[0]
-// N = n*k ( i.e total number of nodes in the final list )
-// tc = k*N
-// sc = o(1)
-// func mergeKLists(lists []*ListNode) *ListNode {
-//     k := len(lists)
-//     if k == 0 {return nil}
-//     head := lists[0]
-//     for i := 1; i < len(lists); i++ { // k
-//         head = merge2Lists(head, lists[i]) // n
-//     }
-//     return head
-// }
 
 func merge2Lists(l1, l2 *ListNode) *ListNode {
     head := &ListNode{Val: 0}
     tail := head
-    for l1 != nil || l2 != nil {
-        l1Val := math.MaxInt64
-        if l1 != nil {l1Val = l1.Val}
-        l2Val := math.MaxInt64
-        if l2 != nil {l2Val = l2.Val}
-
-        if l1Val == math.MaxInt64 && l2Val == math.MaxInt64 {break}
-
-        if l1Val < l2Val {
+    for l1 != nil && l2 != nil {
+        if l1.Val < l2.Val {
             tail.Next = l1
-            tail = tail.Next
             l1 = l1.Next
+            tail = tail.Next
         } else {
             tail.Next = l2
             tail = tail.Next
-            l2 = l2.Next
+            l2 = l2.Next            
         }
     }
+    for l1 != nil {tail.Next=l1; l1 = l1.Next; tail=tail.Next}
+    for l2 != nil {tail.Next=l2; l2 = l2.Next; tail=tail.Next}
     return head.Next
 }
