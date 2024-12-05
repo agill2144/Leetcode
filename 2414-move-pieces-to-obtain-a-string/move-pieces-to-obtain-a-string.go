886337e05293c5047407d923947ee7b030653123
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-04 18:32:27 -0800

    Time: 6 ms (50.00%) | Memory: 8.4 MB (88.89%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.go
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.go
@@ -1,7 +1,7 @@
 func canChange(start string, target string) bool {
     s := 0
     t := 0
-    for t < len(target) || s < len(start) {
+    for t < len(target) && s < len(start) {
         for t < len(target) && target[t] == '_' {t++}
         for s < len(start) && start[s] == '_' {s++}
         if t == len(target) && s == len(start) {return true}
@@ -15,5 +15,9 @@ func canChange(start string, target string) bool {
         s++
         t++
     }
+    // if ptrs are still inbound, they have not failed yet
+    // just process them all the way till the end and make sure they are all empty spaces
+    for s < len(start) {if start[s] != '_'{return false}; s++}
+    for t < len(target) {if target[t] != '_'{return false}; t++}
     return true
 }
