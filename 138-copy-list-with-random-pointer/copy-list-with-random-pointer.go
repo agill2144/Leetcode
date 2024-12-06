b5c5882a2c34d7dd3c21b22f540e37277b6ec890
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-05 19:57:56 -0800

    Time: 3 ms (58.49%) | Memory: 5.4 MB (25.65%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.go
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.go
@@ -8,29 +8,62 @@
  */
 
 func copyRandomList(head *Node) *Node {
-    if head == nil {return nil}
-    m := map[*Node]*Node{}
-    dummy := &Node{Val:0}
-    tail := dummy
+    if head == nil {return head}
     curr := head
     for curr != nil {
         next := curr.Next
-        node := m[curr]
-        if node == nil {
-            node = &Node{Val: curr.Val}
-            m[curr] = node
-        }
-        tail.Next = node
-        tail = tail.Next
+        curr.Next = &Node{Val: curr.Val}
+        curr.Next.Next = next
+        curr = next
+    }
+
+    curr = head
+    for curr != nil {
+        next := curr.Next.Next
         if curr.Random != nil {
-            randCp := m[curr.Random]
-            if randCp == nil {
-                randCp = &Node{Val:curr.Random.Val}
-                m[curr.Random] = randCp
-            }
-            node.Random = randCp
+            curr.Next.Random = curr.Random.Next
         }
+        curr = next
+    }
+    dummy := &Node{Val: 0}
+    tail := dummy 
+    curr = head
+    for curr != nil {
+        next := curr.Next.Next
+        r := curr.Next
+        r.Next = nil
+        curr.Next = next
+        tail.Next = r
+        tail = tail.Next
         curr = next
     }
     return dummy.Next
 }
+
+// func copyRandomList(head *Node) *Node {
+//     if head == nil {return nil}
+//     m := map[*Node]*Node{}
+//     dummy := &Node{Val:0}
+//     tail := dummy
+//     curr := head
+//     for curr != nil {
+//         next := curr.Next
+//         node := m[curr]
+//         if node == nil {
+//             node = &Node{Val: curr.Val}
+//             m[curr] = node
+//         }
+//         tail.Next = node
+//         tail = tail.Next
+//         if curr.Random != nil {
+//             randCp := m[curr.Random]
+//             if randCp == nil {
+//                 randCp = &Node{Val:curr.Random.Val}
+//                 m[curr.Random] = randCp
+//             }
+//             node.Random = randCp
+//         }
+//         curr = next
+//     }
+//     return dummy.Next
+// }
