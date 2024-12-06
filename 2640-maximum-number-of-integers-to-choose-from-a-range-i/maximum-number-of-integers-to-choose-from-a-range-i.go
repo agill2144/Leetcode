ff8b1c5b3048e551fcfb1e2fd8a2f14e65d87511
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-05 18:57:33 -0800

    Time: 143 ms (25.00%) | Memory: 9.2 MB (80.00%) - LeetSync

---
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.go
@@ -0,0 +1,22 @@
+func maxCount(banned []int, n int, maxSum int) int {
+    set := map[int]bool{}
+    for i := 0; i < len(banned); i++ {set[banned[i]] = true}
+    left := 1
+    right := n
+    ans := 0
+    for left <= right {
+        mid := left + (right-left)/2
+        sum := mid*(mid+1)/2
+        count := mid
+        for k, _ := range set {
+            if k <= mid {sum -= k; count--}
+        }
+        if sum <= maxSum {
+            ans = count
+            left = mid+1
+        } else {
+            right = mid-1
+        }
+    }
+    return ans
+}
