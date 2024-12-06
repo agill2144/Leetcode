8e19c4e4511c55edf20ebf107881c4a91bcc639c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-05 18:58:55 -0800

    Time: 168 ms (25.00%) | Memory: 10.2 MB (60.00%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.go
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.go
@@ -1,9 +1,16 @@
+// pick a number from 1 to n
+// this is a range to select an answer for
+// ranges are sorted
+// binary search on answers
 func maxCount(banned []int, n int, maxSum int) int {
     set := map[int]bool{}
     for i := 0; i < len(banned); i++ {set[banned[i]] = true}
     left := 1
     right := n
     ans := 0
+    // m = len(banned)
+    // tc = o(logn * m)
+    // sc = o(m) for banned hashset
     for left <= right {
         mid := left + (right-left)/2
         sum := mid*(mid+1)/2
