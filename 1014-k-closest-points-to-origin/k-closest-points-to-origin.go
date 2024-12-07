16265ad34232811cbe508080a2916d178d1922f5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-06 20:14:57 -0800

    Time: 22 ms (74.05%) | Memory: 9.1 MB (69.91%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.go
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.go
@@ -1,26 +1,49 @@
 func kClosest(points [][]int, k int) [][]int {
-    type distNode struct {
-        dist float64
-        point []int
-    }
-    dists := []*distNode{}
+    mx := &maxHeap{items: []*distNode{}}
     for i := 0; i < len(points); i++ {
-        dists = append(dists, &distNode{
+        heap.Push(mx, &distNode{
             dist: calcDist(points[i][0], points[i][1]),
             point: points[i],
         })
+        if mx.Len() > k {
+            heap.Pop(mx)
+        }
     }
-    sort.Slice(dists, func(i, j int)bool{
-        return dists[i].dist < dists[j].dist
-    })
-
     out := [][]int{}
     for i := 0; i < k; i++ {
-        out = append(out, dists[i].point)
+        out = append(out, heap.Pop(mx).(*distNode).point)
     }
     return out
+
 }
 
-func calcDist(x, y int)float64 {
+func calcDist(x , y int) float64 {
     return math.Sqrt(float64(x*x)+float64(y*y))
+}
+
+type distNode struct {
+    dist float64
+    point []int
+}
+
+type maxHeap struct {
+	items []*distNode
+}
+
+func (m *maxHeap) Less(i, j int) bool {
+	return m.items[i].dist > m.items[j].dist
+}
+func (m *maxHeap) Swap(i, j int) {
+	m.items[i], m.items[j] = m.items[j], m.items[i]
+}
+func (m *maxHeap) Len() int {
+	return len(m.items)
+}
+func (m *maxHeap) Push(x interface{}) {
+	m.items = append(m.items, x.(*distNode))
+}
+func (m *maxHeap) Pop() interface{} {
+	out := m.items[len(m.items)-1]
+	m.items = m.items[:len(m.items)-1]
+	return out
 }
