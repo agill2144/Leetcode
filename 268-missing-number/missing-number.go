982d66afd424d6444ec90c60b1c0b4684338d3bd
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-07 12:13:34 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (53.63%) - LeetSync

--- 268-missing-number/missing-number.go
+++ 268-missing-number/missing-number.go
@@ -1,7 +1,6 @@
 func missingNumber(nums []int) int {
     n := len(nums)
-    expectedSum := n*(n+1)/2 
     sum := 0
     for i := 0; i < n; i++ {sum += nums[i]}
-    return expectedSum - sum
+    return (n*(n+1)/2) - sum
 }
