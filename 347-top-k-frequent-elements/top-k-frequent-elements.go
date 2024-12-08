8a05f8de721cc550079c6b8d1e5df76768a39e1b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-07 21:10:42 -0800

    Time: 4 ms (25.75%) | Memory: 7.6 MB (80.96%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,18 +1,15 @@
 func topKFrequent(nums []int, k int) []int {
     freq := map[int]int{}
-    for i := 0; i < len(nums); i++ {freq[nums[i]]++}
-    sort.Slice(nums, func(i, j int)bool{
-        iFreq := freq[nums[i]]
-        jFreq := freq[nums[j]]
-        if iFreq == jFreq {
-            return nums[i] < nums[j]
-        }
-        return iFreq < jFreq
-    })
+    maxFreq := 0
+    for i := 0; i < len(nums); i++ {freq[nums[i]]++; maxFreq = max(maxFreq, freq[nums[i]])}
+    bucket := make([][]int, maxFreq+1)
+    for num , count := range freq {
+        if bucket[count] == nil {bucket[count] = []int{}}
+        bucket[count] = append(bucket[count], num)
+    }
     out := []int{}
-    for i := len(nums)-1; i >= 0 && len(out) != k; i-- {
-        if len(out) != 0 && out[len(out)-1] == nums[i] {continue}
-        out = append(out, nums[i])
+    for i := len(bucket)-1; i >= 0 && len(out) != k; i-- {
+        out = append(out, bucket[i]...)
     }
     return out
 }
