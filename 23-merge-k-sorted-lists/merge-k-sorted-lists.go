8a34c13ceb75fe109e8647230a9fba73486b8091
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-08 11:07:19 -0800

    Time: 1 ms (74.50%) | Memory: 6.4 MB (79.20%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.go
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -7,33 +7,43 @@
  */
 func mergeKLists(lists []*ListNode) *ListNode {
     if len(lists) == 0 {return nil}
-    iIncr := 2
-    pairIncr := 1
-    for pairIncr < len(lists) {
-        for i := 0; i + pairIncr < len(lists); i+=iIncr {
-            lists[i] = merge2Lists(lists[i], lists[i+pairIncr])
-        }
-        iIncr *= 2
-        pairIncr *= 2
+    var dfs func(left, right int) *ListNode
+    dfs = func(left, right int) *ListNode {
+        // base
+        if left > right {return nil}
+        if left == right {return lists[left]}
+
+        // logic
+        mid := left + (right-left)/2
+        l1 := dfs(left, mid)
+        l2 := dfs(mid+1, right)
+        return merge2Lists(l1, l2)
     }
-    return lists[0]
+    return dfs(0, len(lists)-1)
 }
 
-func merge2Lists(l1, l2 *ListNode) *ListNode {
-    head := &ListNode{Val: 0}
+func merge2Lists(l1,l2 *ListNode) *ListNode {
+    head := &ListNode{Val:0}
     tail := head
     for l1 != nil && l2 != nil {
         if l1.Val < l2.Val {
             tail.Next = l1
             l1 = l1.Next
-            tail = tail.Next
         } else {
             tail.Next = l2
-            tail = tail.Next
-            l2 = l2.Next            
+            l2 = l2.Next
         }
+        tail = tail.Next
     }
-    for l1 != nil {tail.Next=l1; l1 = l1.Next; tail=tail.Next}
-    for l2 != nil {tail.Next=l2; l2 = l2.Next; tail=tail.Next}
+    for l1 != nil {
+        tail.Next = l1
+        l1 = l1.Next
+        tail = tail.Next
+    }
+    for l2 != nil {
+        tail.Next = l2
+        l2 = l2.Next
+        tail = tail.Next
+    }
     return head.Next
 }
