fe9b2f01d4ccf48719891d7194c539b421eeacbb
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-08 13:09:30 -0800

    Time: 3 ms (16.67%) | Memory: 8.3 MB (16.67%) - LeetSync

--- 1192-divide-chocolate/divide-chocolate.go
+++ 1192-divide-chocolate/divide-chocolate.go
@@ -1,24 +1,30 @@
 func maximizeSweetness(sweetness []int, k int) int {
-    n := len(sweetness)
     left := 1
     right := 0
-    for i := 0; i < n; i++ {right += sweetness[i]}
-    ans := 0
+    for i := 0; i < len(sweetness); i++ {
+        right += sweetness[i]
+    }
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
+        atMin := mid
+        count := 0
         rSum := 0
-        chunks := 0
-        for j := 0; j < n; j++ {
-            rSum += sweetness[j]
-            if rSum >= mid {
-                chunks++
+        for i := 0; i < len(sweetness); i++ {
+            rSum += sweetness[i]
+            if rSum >= atMin {
+                count++
                 rSum = 0
             }
         }
-        if chunks < k+1 {
-            right = mid-1
+        // when does atMin or mid not work?
+        // when we have < k+1 chunks
+        if count < k+1 {
+            // decrease per chunk sweetness level
+            right = mid-1            
         } else {
             ans = mid
+            // we want max such ans
             left = mid+1
         }
     }
