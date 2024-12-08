0590d05e8d83e58b57eddb76a743aefb8656f3ae
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-08 13:36:36 -0800

    Time: 0 ms (100.00%) | Memory: 7.1 MB (18.61%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.go
@@ -2,23 +2,22 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
     merged := []int{}
     totalSize := len(nums1) + len(nums2)
     n1, n2 := 0, 0
-    for n1 < len(nums1) && n2 < len(nums2) && len(merged) < (totalSize/2)+1 {
-        if nums1[n1] < nums2[n2] {
+    // N = totalSize of n1+n2
+    // n = (N/2)+1
+    // tc = o(n)
+    // sc = o(n)
+    for (n1 < len(nums1) || n2 < len(nums2)) && len(merged) < (totalSize/2)+1 {
+        n1Val := math.MaxInt64
+        n2Val := math.MaxInt64
+        if n1 < len(nums1) {n1Val = nums1[n1]}
+        if n2 < len(nums2) {n2Val = nums2[n2]}
+        if n1Val < n2Val {
             merged = append(merged, nums1[n1])
             n1++
         } else {
             merged = append(merged, nums2[n2])
             n2++
         }
-        if len(merged) > (totalSize/2)+1 {break}
-    }
-    for n1 < len(nums1) && len(merged) < (totalSize/2)+1 {
-        merged = append(merged, nums1[n1])
-        n1++
-    }
-    for n2 < len(nums2) && len(merged) < (totalSize/2)+1 {
-        merged = append(merged, nums2[n2])
-        n2++
     }
     if totalSize % 2 == 0 {
         return (float64(merged[len(merged)-1]) + float64(merged[len(merged)-2])) / 2.0
