a7348cf7a1cd22ffa5b6c7221d3060b6445bcdb5
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-07 22:47:36 -0800

    Time: 0 ms (100.00%) | Memory: 3.9 MB (11.52%) - LeetSync

--- 670-maximum-swap/maximum-swap.go
+++ 670-maximum-swap/maximum-swap.go
@@ -1,15 +1,17 @@
 func maximumSwap(num int) int {
-    numStr := fmt.Sprintf("%v", num)
-    nums := []int{}
-    for i := 0; i < len(numStr); i++ {nums = append(nums, int(numStr[i]-'0'))}
-    bucket := make([]int, 10)
-    for i := 0; i < len(bucket); i++ {bucket[i] = -1}
-    for i := 0; i < len(nums); i++ {bucket[nums[i]] = i}
+    idxs := make([]int, 10)
+    for i := 0; i < len(idxs); i++ {idxs[i] = -1}
+    nums := strings.Split(fmt.Sprintf("%v", num), "")
+    for i := 0; i < len(nums); i++ {
+        numInt, _  := strconv.Atoi(nums[i])
+        idxs[numInt] = i
+    }
     swapped := false
     for i := 0; i < len(nums) && !swapped; i++ {
-        for j := len(bucket)-1; j > nums[i]; j-- {
-            if bucket[j] != -1 && bucket[j] > i {
-                nums[i], nums[bucket[j]] = nums[bucket[j]], nums[i]
+        curr, _ := strconv.Atoi(nums[i])
+        for j := len(idxs)-1; j > curr; j-- {
+            if idxs[j] != -1 && idxs[j] > i && j > curr {
+                nums[i], nums[idxs[j]] = nums[idxs[j]], nums[i]
                 swapped = true
                 break
             }
@@ -17,16 +19,8 @@ func maximumSwap(num int) int {
     }
     out := 0
     for i := 0; i < len(nums); i++ {
-        out = out * 10 + nums[i]
+        n, _ := strconv.Atoi(nums[i])
+        out = out * 10 +n
     }
     return out
 }
-
-/*
-    2199
-
-    9129
-    9192
-
-*/
-
