7155216818f613a0f493aa3921ee6f14f9a852fc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-08 12:14:13 -0800

    Time: 0 ms (100.00%) | Memory: 4.8 MB (21.59%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.go
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.go
@@ -1,19 +1,16 @@
-func findKthPositive(nums []int, k int) int {
-    n := len(nums)
-    left := 0
-    right := n-1
-    for left <= right {
-        mid := left + (right-left)/2
-        cIdx := nums[mid]-1
-        missOnLeft := cIdx - mid
-        if missOnLeft < k {
-            left = mid+1
+func findKthPositive(arr []int, k int) int {
+    ptr := 1
+    ptr2 := 0
+    count := 0
+    for ptr2 < len(arr) {
+        if ptr != arr[ptr2] {
+            count++
+            if count == k {return ptr}
+            ptr++
         } else {
-            right = mid-1
+            ptr2++
+            ptr++
         }
     }
-    if right < 0 {return k}
-    missOnLeft := (nums[right]-1) - right
-    k -= missOnLeft
-    return nums[right] + k
+    return arr[len(arr)-1] + (k-count)
 }
