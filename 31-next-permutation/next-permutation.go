564dd4bb309a26cd8a99713358bbe43f9282c640
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-08 11:18:40 -0800

    Time: 0 ms (100.00%) | Memory: 4.4 MB (25.69%) - LeetSync

--- 31-next-permutation/next-permutation.go
+++ 31-next-permutation/next-permutation.go
@@ -1,18 +1,19 @@
 func nextPermutation(nums []int)  {
-    breachIdx := -1
     n := len(nums)
-    for i := n-1; i >= 0; i-- {
-        curr := nums[i]
-        next := curr
-        if i+1 < n {next = nums[i+1]}
-        if curr < next {breachIdx = i; break}
+    breachIdx := -1
+    for i := n-2; i >= 0; i-- {
+        if nums[i] < nums[i+1] {
+            breachIdx = i
+            break
+        }
     }
-
     if breachIdx != -1 {
         nextGreaterIdx := -1
         for i := n-1; i > breachIdx; i-- {
-            if nums[i] > nums[breachIdx] && (nextGreaterIdx == -1 || nums[i] < nums[nextGreaterIdx]) {
-                nextGreaterIdx = i
+            if nums[i] > nums[breachIdx] {
+                if nextGreaterIdx == -1 || nums[i] < nums[nextGreaterIdx] {
+                    nextGreaterIdx = i
+                }
             }
         }
         nums[breachIdx], nums[nextGreaterIdx] = nums[nextGreaterIdx], nums[breachIdx]
