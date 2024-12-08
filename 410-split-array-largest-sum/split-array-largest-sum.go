01c6f052f6c722c1bbf550ccfe0ad4fbc3625abd
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-08 13:23:36 -0800

    Time: 0 ms (100.00%) | Memory: 4.1 MB (35.90%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.go
+++ 410-split-array-largest-sum/split-array-largest-sum.go
@@ -1,5 +1,4 @@
 func splitArray(nums []int, k int) int {
-    // max is minimized
     left := -1
     right := 0
     for i := 0; i < len(nums); i++ {
@@ -9,11 +8,12 @@ func splitArray(nums []int, k int) int {
     ans := -1
     for left <= right {
         mid := left + (right-left)/2
+        atMax := mid
         count := 1
         rSum := 0
         for i := 0; i < len(nums); i++ {
             rSum += nums[i]
-            if rSum > mid {
+            if rSum > atMax {
                 count++
                 rSum = nums[i]
             }
