38c0f8b34acb6e37e33f34fe4015c72928c6092a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-08 21:43:07 -0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (66.85%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.go
+++ 1028-interval-list-intersections/interval-list-intersections.go
@@ -4,12 +4,21 @@ func intervalIntersection(firstList [][]int, secondList [][]int) [][]int {
     for a < len(firstList) && b < len(secondList) {
         aStart, aEnd := firstList[a][0], firstList[a][1]
         bStart, bEnd := secondList[b][0], secondList[b][1]
-        start := max(aStart, bStart)
-        end := min(aEnd, bEnd)
-        if start <= end {
-            out = append(out, []int{start, end})
+        // 1. if there is no intersection, move on
+        if aStart > bEnd {
+            // if a starts after b ends
+            b++
+        } else if bStart > aEnd {
+            // b starts after a ends
+            a++
+        } else {
+            // there is an intersection!
+            // we want the latest start time
+            // and we want the earlist end time
+            out = append(out, []int{max(aStart,bStart), min(aEnd, bEnd)})
+            if aEnd < bEnd {a++} else {b++}
         }
-        if aEnd < bEnd {a++} else {b++}
+
     }
     return out
 }
