93592a0e5dc89b1ee393b6f1878c6610848fea5a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-08 22:28:42 -0800

    Time: 0 ms (100.00%) | Memory: 4.1 MB (34.88%) - LeetSync

--- 65-valid-number/valid-number.go
+++ 65-valid-number/valid-number.go
@@ -6,11 +6,12 @@ func isNumber(s string) bool {
     // we can only have 1 exponent
     seenExp := false
     start := 0
-    // we can have a operator at the very beginning AND right after e||E
+    // we can have a operator at the very beginning AND right after e||E AND we cannot end number with operator
     if s[start] == '+' || s[start] == '-' {start = 1}
     for i := start; i < len(s); i++ {
         char := s[i]
         if char == '+' || char == '-' {
+            if i == len(s)-1 {return false}
             if i-1 >= 0 && (s[i-1] == 'e' || s[i-1] == 'E') && i != len(s)-1 {continue}
             return false
         } else if char == 'E' || char == 'e' {
