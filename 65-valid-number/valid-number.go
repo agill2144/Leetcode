06982ac9f550dcfe0c7986f5cd31c61760b995b8
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-08 22:33:02 -0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (34.88%) - LeetSync

--- 65-valid-number/valid-number.go
+++ 65-valid-number/valid-number.go
@@ -11,15 +11,20 @@ func isNumber(s string) bool {
     for i := start; i < len(s); i++ {
         char := s[i]
         if char == '+' || char == '-' {
+            // cannot have a operator at the end
             if i == len(s)-1 {return false}
             if i-1 >= 0 && s[i-1] != 'e' && s[i-1] != 'E' {return false}
         } else if char == 'E' || char == 'e' {
-            if !seenDigit || i == len(s)-1 {return false}
-            if seenExp {return false}
+            // we have seen an exp already or
+            // we have not seen a digit yet, cannot have a e without a digit first, or
+            // or this e is at the, invalid!
+            if seenExp || !seenDigit || i == len(s)-1 {return false}
             seenExp = true
         } else if char >= '0' && char <= '9' {
             seenDigit = true
         } else if char == '.' {
+            // cannot have more than 1 decimal OR
+            // cannot have a decimal be part of the exponent
             if seenDecimal || seenExp {return false}
             seenDecimal = true
         } else {
