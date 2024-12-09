2423d95c3cba09c208d3f4e8eed549e1a729ebbc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-08 22:37:44 -0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (27.91%) - LeetSync

--- 65-valid-number/valid-number.go
+++ 65-valid-number/valid-number.go
@@ -1,4 +1,18 @@
+/*
+    rules tldr
+    - we must have seen atleast 1 digit
+    - "+" or "-" are only allowed 2 times, and never allowed at the last position
+        - at the beginning
+        - right after "e" || "E"
+    - "e" and "E" is not allowed
+        - when we have not seen digit yet, cannot have a exponent without a base number
+        - this is at the last position
+        - we have already seen an e || E before
+
+
+*/
 func isNumber(s string) bool {
+    n := len(s)
     // we can only have 1 decimal ever
     seenDecimal := false
     // we must have seen 1 digit
@@ -8,17 +22,16 @@ func isNumber(s string) bool {
     start := 0
     // we can have a operator at the very beginning AND right after e||E AND we cannot end number with operator
     if s[start] == '+' || s[start] == '-' {start = 1}
-    for i := start; i < len(s); i++ {
+    for i := start; i < n; i++ {
         char := s[i]
         if char == '+' || char == '-' {
             // cannot have a operator at the end
-            if i == len(s)-1 {return false}
-            if i-1 >= 0 && s[i-1] != 'e' && s[i-1] != 'E' {return false}
+            if i == n-1 || (i-1 >= 0 && s[i-1] != 'e' && s[i-1] != 'E') {return false}
         } else if char == 'E' || char == 'e' {
             // we have seen an exp already or
             // we have not seen a digit yet, cannot have a e without a digit first, or
             // or this e is at the, invalid!
-            if seenExp || !seenDigit || i == len(s)-1 {return false}
+            if seenExp || !seenDigit || i == n-1 {return false}
             seenExp = true
         } else if char >= '0' && char <= '9' {
             seenDigit = true
