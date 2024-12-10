709650e9d123c830a255dd6213f82945d6e28b4f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-10 14:37:58 -0800

    Time: 4 ms (6.16%) | Memory: 8.7 MB (19.34%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.go
+++ 1028-interval-list-intersections/interval-list-intersections.go
@@ -1,39 +1,15 @@
 func intervalIntersection(firstList [][]int, secondList [][]int) [][]int {
+    a, b := 0, 0
     out := [][]int{}
-    a, b := 0,0
     for a < len(firstList) && b < len(secondList) {
-        start := max(firstList[a][0], secondList[b][0])
-        end := min(firstList[a][1], secondList[b][1])
-        if start <= end {
-            out = append(out, []int{start, end})
+        aStart, aEnd := firstList[a][0], firstList[a][1]
+        bStart, bEnd := secondList[b][0], secondList[b][1]
+        if aStart > bEnd || bStart > aEnd {
+            if aEnd < bEnd {a++} else {b++}
+        } else {
+            out = append(out, []int{max(aStart, bStart), min(aEnd, bEnd)})
+            if aEnd < bEnd {a++} else {b++}
         }
-        if firstList[a][1] < secondList[b][1] {a++} else {b++}
     }
     return out
 }
-// func intervalIntersection(firstList [][]int, secondList [][]int) [][]int {
-//     out := [][]int{}
-//     a, b := 0,0
-//     for a < len(firstList) && b < len(secondList) {
-//         aStart, aEnd := firstList[a][0], firstList[a][1]
-//         bStart, bEnd := secondList[b][0], secondList[b][1]
-//         // 1. if there is no intersection, move on
-//         if aStart > bEnd {
-//             // a starts after b
-//             // b is over, not usable, move away from b
-//             b++
-//         } else if bStart > aEnd {
-//             // b starts after a
-//             // a is over, not usable, move away from a
-//             a++
-//         } else {
-//             // there is an intersection!
-//             // we want the latest start time
-//             // and we want the earlist end time
-//             out = append(out, []int{max(aStart,bStart), min(aEnd, bEnd)})
-//             if aEnd < bEnd {a++} else {b++}
-//         }
-
-//     }
-//     return out
-// }
