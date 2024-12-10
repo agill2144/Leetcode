046c48497a7e99a46a99cd467f6198dcef07ee34
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-10 11:06:39 -0800

    Time: 0 ms (100.00%) | Memory: 4.4 MB (49.37%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.go
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.go
@@ -1,21 +1,17 @@
-// we need to check whether words are sorted accoriding to a custom character order
 func isAlienSorted(words []string, order string) bool {
-    if len(words) <= 1 {return true}
     idx := map[byte]int{}
     for i := 0; i < len(order); i++ {idx[order[i]] = i}
     for i := 1; i < len(words); i++ {
-        curr := words[i]
-        prev := words[i-1]
+        curr, prev := words[i], words[i-1]
         c, p := 0,0
         for c < len(curr) && p < len(prev) {
-            cChar := curr[c]
-            pChar := prev[p]
-            if cChar == pChar {p++; c++; continue}
-            cCharIdx := idx[cChar]
-            pCharIdx := idx[pChar]
-            if cCharIdx < pCharIdx {return false} 
+            if curr[c] == prev[c] {c++; p++; continue}
+            cIdx := idx[curr[c]]
+            pIdx := idx[prev[p]]
+            if pIdx > cIdx {return false}
             break
         }
+
         if p < len(prev) && c == len(curr) {return false}
     }
     return true
