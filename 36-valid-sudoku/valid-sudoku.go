b895997cd0e59a9f69d631aef06293d259e65787
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-10 12:11:45 -0800

    Time: 2 ms (32.07%) | Memory: 4.8 MB (49.75%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.go
+++ 36-valid-sudoku/valid-sudoku.go
@@ -1,8 +1,17 @@
 func isValidSudoku(board [][]byte) bool {
     n := len(board)
+    // tc = o(n^2)
+    // sc = o(1)
+
+    // there are only 9 possible keys
+    // and each key only has a array of size 10
+    // i.e constant space
+    // sc = o(1)
     boxSet := map[string][]bool{}
     for i := 0; i < n; i++ {
+        // constant space ; o(1)
         rowSet := make([]bool, n+1)
+        // constant space ; o(1)
         colSet := make([]bool, n+1)
         for j := 0; j < n; j++ {
             if board[i][j] != '.'{
@@ -10,7 +19,7 @@ func isValidSudoku(board [][]byte) bool {
                 if rowSet[rowVal] {return false}
                 rowSet[rowVal] = true
 
-                boxKey := fmt.Sprint("%v-%v", i/3,j/3)
+                boxKey := fmt.Sprintf("%v-%v", i/3,j/3)
                 if boxSet[boxKey] == nil {boxSet[boxKey] = make([]bool, n+1)}
                 if boxSet[boxKey][rowVal] {return false}
                 boxSet[boxKey][rowVal] = true
