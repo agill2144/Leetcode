11e6c8471e620964cd48aecc392d069e6bf77277
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-10 14:26:26 -0800

    Time: 0 ms (100.00%) | Memory: 4.1 MB (45.28%) - LeetSync

--- 65-valid-number/valid-number.go
+++ 65-valid-number/valid-number.go
@@ -1,38 +1,19 @@
-/*
-    rules tldr
-    - we must have seen atleast 1 digit
-    - "+" or "-" are only allowed 2 times, and never allowed at the last position
-        - at the beginning
-        - right after "e" || "E"
-        - so its not allowed when i == n-1 || prev char is not a e and not a E
-    - "e" and "E" is not allowed
-        - when we have not seen digit yet, cannot have a exponent without a base number
-        - this is at the last position
-        - we have already seen an e || E before
-    - "." is not allowed;
-        - when we have already seen a dot before
-        - when we have seen an exp already (dots / decimals are not allowed in exponents )
-
-*/
 func isNumber(s string) bool {
     n := len(s)
-    seenDigit := false
     seenExp := false
+    seenDigit := false
     seenDecimal := false
     start := 0
     if s[start] == '+' || s[start] == '-' {start++}
-    for i := start ; i < n; i++ {
+    for i := start; i < len(s); i++ {
         char := s[i]
-        if char == '+' || char == '-' {
-            if i == n-1 ||
-                (i-1 >= 0 && s[i-1] != 'e' && s[i-1] != 'E') {
-                    return false
-                }
-        } else if char == 'e' || char == 'E' {
-            if i == n-1 || seenExp || !seenDigit {return false}
+        if char == 'e' || char == 'E' {
+            if !seenDigit || i == n-1 || seenExp {return false}
             seenExp = true
+        } else if char == '+' || char == '-' {
+            if !seenDigit || i == n-1 || (s[i-1] != 'e' && s[i-1] != 'E'){return false}
         } else if char == '.' {
-            if seenExp || seenDecimal {return false}
+            if seenDecimal || seenExp {return false}
             seenDecimal = true
         } else if char >= '0' && char <= '9' {
             seenDigit = true
