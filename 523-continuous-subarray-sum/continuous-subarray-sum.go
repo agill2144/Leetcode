62f1afbef00a195439ce3d705eecf75329435301
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-10 12:30:01 -0800

    Time: 29 ms (88.24%) | Memory: 15.3 MB (24.32%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.go
+++ 523-continuous-subarray-sum/continuous-subarray-sum.go
@@ -1,16 +1,13 @@
 func checkSubarraySum(nums []int, k int) bool {
-    rems := map[int]int{0:-1}
+    remIdx := map[int]int{0:-1}
     rSum := 0
     for i := 0; i < len(nums); i++ {
-        rSum += nums[i]
-        r := rSum % k
-        if r < 0 { r += k}
-        left, ok := rems[r]
-        if ok {
-            if i-left >= 2 {return true}
-        } else {
-            rems[r] = i
+        rSum += nums[i]        
+        idx, ok := remIdx[rSum%k]
+        if ok && i-idx >= 2 {return true}
+        if !ok {
+            remIdx[rSum%k] = i
         }
-    }
+    }   
     return false
 }
