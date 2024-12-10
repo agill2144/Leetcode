3cd9142a151b8b0dfb7f7efaae43bdb9a6a45668
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-09 22:36:16 -0800

    Time: 37 ms (5.88%) | Memory: 9.8 MB (5.26%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.go
@@ -0,0 +1,28 @@
+func maximumLength(s string) int {
+    left := 1
+    right := len(s)-1
+    ans := -1
+    for left <= right {
+        mid := left + (right-left)/2
+        // mid is our len
+        // evaluate each substring of size mid
+        found := false
+        freq := map[string]int{}
+        for i := 0; i+mid < len(s)+1; i++ {
+            subStr := s[i:i+mid]
+            expected := ""
+            for k := i; k < i+mid; k++ {expected += string(s[i])}
+            if subStr == expected {
+                freq[subStr]++
+                if freq[subStr] == 3 {found = true; break}
+            }
+        }
+        if found {
+            ans = mid
+            left = mid+1   
+        } else {
+            right = mid-1
+        }
+    }
+    return ans
+}
