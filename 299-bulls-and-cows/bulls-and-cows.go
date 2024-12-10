aa38c42db16dcc410e128930e5d7b07f2bc35743
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-10 10:32:24 -0800

    Time: 0 ms (100.00%) | Memory: 4.1 MB (34.00%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.go
+++ 299-bulls-and-cows/bulls-and-cows.go
@@ -1,21 +1,39 @@
 func getHint(secret string, guess string) string {
     if len(secret) != len(guess) {return ""}
     bulls := 0
+    cows := 0
     freq := map[byte]int{}
     for i := 0; i < len(secret); i++ {
         if secret[i] == guess[i] {
             bulls++
         } else {
+            if freq[secret[i]] < 0 {cows++}
+            if freq[guess[i]] > 0 {cows++}
             freq[secret[i]]++
-        }
-    }
-    cows := 0
-    for i := 0; i < len(secret); i++ {
-        if secret[i] == guess[i] {continue}
-        if freq[guess[i]] != 0 {
             freq[guess[i]]--
-            cows++
         }
     }
     return fmt.Sprintf("%vA%vB", bulls, cows)
 }
+
+// func getHint(secret string, guess string) string {
+//     if len(secret) != len(guess) {return ""}
+//     bulls := 0
+//     freq := map[byte]int{}
+//     for i := 0; i < len(secret); i++ {
+//         if secret[i] == guess[i] {
+//             bulls++
+//         } else {
+//             freq[secret[i]]++
+//         }
+//     }
+//     cows := 0
+//     for i := 0; i < len(secret); i++ {
+//         if secret[i] == guess[i] {continue}
+//         if freq[guess[i]] != 0 {
+//             freq[guess[i]]--
+//             cows++
+//         }
+//     }
+//     return fmt.Sprintf("%vA%vB", bulls, cows)
+// }
