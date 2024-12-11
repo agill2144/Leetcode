c4530ebbf2135874ef9d0408f647690f20bb07e1
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-10 20:03:52 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (66.98%) - LeetSync

--- 65-valid-number/valid-number.go
+++ 65-valid-number/valid-number.go
@@ -1,20 +1,20 @@
 func isNumber(s string) bool {
-    n := len(s)
-    seenExp := false
     seenDigit := false
-    seenDecimal := false
+    seenExp := false
+    seenDec := false
+    n := len(s)
     start := 0
     if s[start] == '+' || s[start] == '-' {start++}
-    for i := start; i < len(s); i++ {
+    for i := start; i < n; i++ {
         char := s[i]
         if char == 'e' || char == 'E' {
-            if !seenDigit || i == n-1 || seenExp {return false}
+            if !seenDigit || seenExp || i == n-1 {return false}
             seenExp = true
         } else if char == '+' || char == '-' {
-            if !seenDigit || i == n-1 || (s[i-1] != 'e' && s[i-1] != 'E'){return false}
+            if (s[i-1] != 'e' && s[i-1] != 'E') || i == n-1 {return false}
         } else if char == '.' {
-            if seenDecimal || seenExp {return false}
-            seenDecimal = true
+            if seenDec || seenExp {return false}
+            seenDec = true
         } else if char >= '0' && char <= '9' {
             seenDigit = true
         } else {
