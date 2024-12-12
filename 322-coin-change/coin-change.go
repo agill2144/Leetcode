734b477a298d1bc9b15e0badda034a04bf286d89
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-11 19:58:07 -0800

    Time: 8 ms (93.33%) | Memory: 8.3 MB (74.58%) - LeetSync

--- 322-coin-change/coin-change.go
+++ 322-coin-change/coin-change.go
@@ -17,36 +17,38 @@
     tc = 2^n
     sc = o(n) for recursive stack
 
-    approach: 2D DP
-    - 
+    approach: Bottom up dp
+    - start with the smallest subproblem
+    - and look left for pre-solved subproblem
+    tc = o(m*n)
+    sc = o(m*n)
+
+    approach: bottom up dp, space optimized
+    - in 2d dp, we only looked up 1 row for an old value and looked left in the same row for an updated value
+    - we can use a 1D array
+    - each cell before being updated has the prev row value ( well visually )
+    - and to the left, in the same row, we get the updated value
+    tc = o(m*n)
+    sc = o(n)
 */
 func coinChange(coins []int, amount int) int {
     m := len(coins)+1
     n := amount+1
-    dp := make([][]int, m)
-    for i := 0; i < m; i++ {
-        dp[i] = make([]int, n)
-        if i == 0 {
-            for j := 1; j < n; j++ {
-                dp[i][j] = math.MaxInt64-1000
-            }
-        }
-    }
+    dp := make([]int, n)
+    for i := 0; i < n; i++ {dp[i] = math.MaxInt64-1000}
+    dp[0] = 0
+
     for i := 1; i < m; i++ { // coins
         for j := 1; j < n; j++ { // amount
             coin := coins[i-1]
             am := j
-            if coin > am {
-                // we do not have a choose case
-                // not choose case is 1 step up
-                dp[i][j] = dp[i-1][j]
-            } else {
-                // not choose = j-1
-                // choose = 1 + (dp[i][j-coinStepsBack])
-                dp[i][j] = min(dp[i-1][j], 1+dp[i][j-coin])
+            if coin <= am {
+                // not choose = j
+                // choose = 1 + (dp[j-coinStepsBack])
+                dp[j] = min(dp[j], 1+dp[j-coin])
             }
         }
     }
-    if dp[m-1][n-1] == math.MaxInt64-1000 {return -1}
-    return dp[m-1][n-1]
+    if dp[n-1] == math.MaxInt64-1000 {return -1}
+    return dp[n-1]
 }
