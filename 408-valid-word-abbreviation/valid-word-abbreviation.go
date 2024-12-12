5a0b8cf2baaa878ad1a1ac06cf0ef695e47e4c80
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-12 15:16:06 -0800

    Time: 0 ms (100.00%) | Memory: 4.1 MB (8.89%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.go
+++ 408-valid-word-abbreviation/valid-word-abbreviation.go
@@ -1,19 +1,20 @@
 func validWordAbbreviation(word string, abbr string) bool {
-    w, a, n := 0, 0, 0
+    a, w := 0, 0
+    n := 0
     for a < len(abbr) && w < len(word) {
         for a < len(abbr) && abbr[a] >= '0' && abbr[a] <= '9' {
             if abbr[a] == '0' && n == 0 {return false}
-            n = n*10 + int(abbr[a]-'0')
+            n = n * 10 + int(abbr[a]-'0')
             a++
         }
         w += n
         n = 0
-
         if a == len(abbr) || w == len(word) {break}
-        if abbr[a] != word[w] {return false}
-        a++; w++
+        if word[w] != abbr[a] {return false}
+        a++
+        w++
     }
     w += n
     n = 0
-    return a == len(abbr) && w == len(word) && n == 0
+    return n == 0 && w == len(word) && a == len(abbr)
 }
