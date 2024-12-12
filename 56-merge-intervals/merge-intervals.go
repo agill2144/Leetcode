88ce8a17a045a5a164f447e0bff71ebb4fdc4739
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-12 14:50:51 -0800

    Time: 1 ms (76.40%) | Memory: 8.3 MB (53.11%) - LeetSync

--- 56-merge-intervals/merge-intervals.go
+++ 56-merge-intervals/merge-intervals.go
@@ -1,16 +1,15 @@
 func merge(intervals [][]int) [][]int {
-    if len(intervals) == 0 {return nil}
-    sort.Slice(intervals, func(i, j int)bool {
+    sort.Slice(intervals, func(i, j int)bool{
         return intervals[i][0] < intervals[j][0]
     })
     out := [][]int{intervals[0]}
     for i := 1; i < len(intervals); i++ {
-        // when is it not overlapping ?
-        // when i starts after prev ends ( prev = last element in output array )
-        if intervals[i][0] > out[len(out)-1][1] {
-            out = append(out, intervals[i])
+        start, end := intervals[i][0], intervals[i][1]
+        prevEnd := out[len(out)-1][1]
+        if start <= prevEnd {
+            out[len(out)-1][1] = max(end, prevEnd)
         } else {
-            out[len(out)-1][1] = max(intervals[i][1], out[len(out)-1][1])
+            out = append(out, intervals[i])
         }
     }
     return out
