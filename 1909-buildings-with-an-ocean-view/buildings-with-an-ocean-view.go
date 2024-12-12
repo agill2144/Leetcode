f380ac431a224d5cdeafcedb68cc00c9c65f6fd7
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-11 18:35:55 -0800

    Time: 2 ms (67.80%) | Memory: 9.8 MB (86.36%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.go
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.go
@@ -1,31 +1,15 @@
-// if asked to start from left -> right
-// we keep track of building heights in a stack ( store idxs )
-// when an ith building, if this building blocks prev building (top of stack )
-// then remove prev building (top of stack) - and keep removing it until ith building keeps blocking ( while stack is !empty )
-// finally, add this building, it might get removed later on
 func findBuildings(heights []int) []int {
-    st := []int{} // idx
     n := len(heights)
-    for i := 0; i < n; i++ {
-        for len(st) != 0 && heights[i] >= heights[st[len(st)-1]] {
-            st = st[:len(st)-1]
+    out := []int{n-1}
+    maxH := heights[n-1]
+    for i := n-2; i >= 0; i-- {
+        if heights[i] > maxH {
+            out = append(out, i)
+            maxH = heights[i]
         }
-        st = append(st, i)
     }
-    return st
+    for i := 0; i < len(out)/2; i++ {
+        out[i], out[len(out)-1-i] = out[len(out)-1-i], out[i]
+    }
+    return out
 }
-// func findBuildings(heights []int) []int {
-//     n := len(heights)
-//     out := []int{n-1}
-//     highest := heights[n-1]
-//     for i := n-2; i >= 0; i-- {
-//         if heights[i] > highest {
-//             out = append(out, i)
-//             highest = heights[i]
-//         }
-//     }
-//     for i := 0; i < len(out)/2; i++ {
-//         out[i], out[len(out)-1-i] = out[len(out)-1-i], out[i]
-//     }
-//     return out
-// }
