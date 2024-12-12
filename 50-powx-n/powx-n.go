0c334fd99411e8e38a1cab5c64c5f5250dc9603a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-11 17:24:57 -0800

    Time: 0 ms (100.00%) | Memory: 4.1 MB (7.13%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,16 +1,21 @@
-// x^n = (x*x)^n/2
-// PEMDAS 
 func myPow(x float64, n int) float64 {
-    var ans float64 = 1.0
-    if n < 0 {n*=-1; x = 1/x}
-    for n != 0 {
-        if n % 2 == 0 {
-            x *= x
-            n /= 2
-        } else {
-            ans *= x
-            n--
-        }
+    if n < 0 {
+        x = 1/x
+        n *= -1
     }
-    return ans
+
+    var dfs func(base float64, exp int) float64
+    dfs = func(base float64, exp int) float64 {
+        // base
+        if exp <= 1 {
+            if exp == 1 {return base}
+            return 1.0
+        }
+
+        // logic
+        res := dfs(base*base, exp/2)
+        if exp % 2 != 0 {res *= base}
+        return res
+    }
+    return dfs(x, n)
 }
