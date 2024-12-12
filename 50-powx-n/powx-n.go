bff9c5213a69609d66e7f61d4febae5a53def9b1
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-11 17:28:12 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (30.61%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,7 +1,7 @@
 func myPow(x float64, n int) float64 {
     if n == 0 {return 1.0}
-    if n == 1 {return x}
-    if n == -1 {return 1/x}
+    // if n == 1 {return x}
+    // if n == -1 {return 1/x}
     if n < 0 {
         x = 1/x
         n *= -1
