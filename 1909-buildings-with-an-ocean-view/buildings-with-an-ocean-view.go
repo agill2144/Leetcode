7a1bb5b2da402418b63d08bb2f46a5385bc5b09d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-11 18:37:49 -0800

    Time: 2 ms (67.80%) | Memory: 10.9 MB (45.45%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.go
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.go
@@ -1,15 +1,26 @@
 func findBuildings(heights []int) []int {
     n := len(heights)
-    out := []int{n-1}
-    maxH := heights[n-1]
-    for i := n-2; i >= 0; i-- {
-        if heights[i] > maxH {
-            out = append(out, i)
-            maxH = heights[i]
+    st := []int{} // idx
+    for i := 0; i < n; i++ {
+        for len(st) != 0 && heights[i] >= heights[st[len(st)-1]] {
+            st = st[:len(st)-1]
         }
+        st = append(st,i)
     }
-    for i := 0; i < len(out)/2; i++ {
-        out[i], out[len(out)-1-i] = out[len(out)-1-i], out[i]
-    }
-    return out
+    return st
 }
+// func findBuildings(heights []int) []int {
+//     n := len(heights)
+//     out := []int{n-1}
+//     maxH := heights[n-1]
+//     for i := n-2; i >= 0; i-- {
+//         if heights[i] > maxH {
+//             out = append(out, i)
+//             maxH = heights[i]
+//         }
+//     }
+//     for i := 0; i < len(out)/2; i++ {
+//         out[i], out[len(out)-1-i] = out[len(out)-1-i], out[i]
+//     }
+//     return out
+// }
