b2d31654a8e4ed354a765f48bc457dc5c4cf4c71
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-11 22:24:07 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (28.57%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.go
+++ 807-custom-sort-string/custom-sort-string.go
@@ -1,16 +1,24 @@
 func customSortString(order string, s string) string {
+    // sc = o(1)
     sFreq := map[byte]int{}
+    // m = len(order)
+    // n = len(s)
+    // tc = o(n)    
     for i := 0; i < len(s); i++ {sFreq[s[i]]++}
     out := new(strings.Builder)
+    // tc = o(n+m)
     for i := 0; i < len(order); i++ {
         count := sFreq[order[i]]
         for count != 0 { out.WriteByte(order[i]); count--}
         delete(sFreq, order[i])
     }
+    // o(n)
     for i := 0; i < len(s); i++ {
         if sFreq[s[i]] > 0 {
             out.WriteByte(s[i])
         }
     }
+    // total tc = o(n) + o(n+m) + o(n) = o(n+m) + (2n)
+    // sc = o(1)
     return out.String()
 }
