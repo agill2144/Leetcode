f79923633172a83d3405941e459556fd9bd74b13
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-12 13:16:22 -0800

    Time: 148 ms (93.04%) | Memory: 9.7 MB (91.03%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.go
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.go
@@ -1,43 +1,18 @@
 type SparseVector struct {
-    items [][]int // [[val, idx], [val2, idx2]]
+    nums []int
 }
 
 func Constructor(nums []int) SparseVector {
-    items := [][]int{}
-    for i := 0; i < len(nums); i++{
-        if nums[i] == 0{continue}
-        items = append(items, []int{nums[i], i})
-    }
-    return SparseVector{items}
+    return SparseVector{nums}
 }
 
 // Return the dotProduct of two sparse vectors
 func (this *SparseVector) dotProduct(vec SparseVector) int {
-    if len(vec.items) < len(this.items) {return vec.dotProduct(*this)}
     sum := 0
-    for i := 0; i < len(this.items); i++ {
-        val, idx := this.items[i][0], this.items[i][1]
-        idx2 := search(vec.items, idx)
-        if idx2 != -1 {
-            sum += (val * vec.items[idx2][0])
-        }
+    for i := 0; i < len(this.nums); i++ {
+        sum += (this.nums[i] * vec.nums[i])
     }
     return sum
-}
-
-func search(items[][]int, targetIdx int) int {
-    left := 0
-    right := len(items)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        if items[mid][1] == targetIdx {return mid}
-        if targetIdx > items[mid][1] {
-            left = mid+1
-        } else {
-            right = mid-1
-        }
-    }
-    return -1
 }
 
 /**
