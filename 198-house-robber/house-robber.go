6e0e13287e9c7637a00ea3028cc1d3f825077755
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-11 20:54:00 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (31.52%) - LeetSync

--- 198-house-robber/house-robber.go
+++ 198-house-robber/house-robber.go
@@ -4,11 +4,26 @@ func rob(nums []int) int {
         if n == 1 {return nums[0]}
         return 0
     }
-    dp := make([]int, n)
-    dp[0] = nums[0]
-    dp[1] = max(nums[0], nums[1])
+    prev := nums[0]
+    curr := max(nums[1], nums[0])
     for i := 2; i < n; i++ {
-        dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+        newCurr := max(curr, nums[i]+prev)
+        prev = curr
+        curr = newCurr
     }
-    return dp[n-1]
+    return curr
 }
+// func rob(nums []int) int {
+//     n := len(nums)
+//     if n <= 1 {
+//         if n == 1 {return nums[0]}
+//         return 0
+//     }
+//     dp := make([]int, n)
+//     dp[0] = nums[0]
+//     dp[1] = max(nums[0], nums[1])
+//     for i := 2; i < n; i++ {
+//         dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+//     }
+//     return dp[n-1]
+// }
