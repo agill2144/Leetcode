70b2a1450c000929326935b8ddcb1363ca70d26c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-11 22:21:03 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (46.15%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.go
+++ 807-custom-sort-string/custom-sort-string.go
@@ -1,43 +1,16 @@
 func customSortString(order string, s string) string {
-    idx := map[string]int{}
-    for i := 0; i < len(order); i++ {idx[string(order[i])] = i}
-    strList := strings.Split(s, "")
-    sort.SliceStable(strList, func(i, j int)bool{
-        iIdx, ok := idx[strList[i]]
-        jIdx, ok2 := idx[strList[j]]
-        if ok && ok2 {
-            return iIdx < jIdx
+    sFreq := map[byte]int{}
+    for i := 0; i < len(s); i++ {sFreq[s[i]]++}
+    out := new(strings.Builder)
+    for i := 0; i < len(order); i++ {
+        count := sFreq[order[i]]
+        for count != 0 { out.WriteByte(order[i]); count--}
+        delete(sFreq, order[i])
+    }
+    for i := 0; i < len(s); i++ {
+        if sFreq[s[i]] > 0 {
+            out.WriteByte(s[i])
         }
-        if ok {return true}
-        if !ok && ok2 {return false}
-        return false
-    })
-    return strings.Join(strList, "")
+    }
+    return out.String()
 }
-// func customSortString(order string, s string) string {
-// // order and s consist of lowercase English letters.
-
-//     // o(1) space
-//     freq := map[byte]int{}
-//     // o(s) time
-//     for i := 0; i < len(s); i++ {freq[s[i]]++}
-
-//     // space = o(s)
-//     out := new(strings.Builder)
-
-//     // time = o(1 * s) = o(s)
-//     for i := 0; i < len(order); i++ {
-//         for freq[order[i]] > 0 {
-//             out.WriteByte(order[i])
-//             freq[order[i]]--
-//         }
-//     }
-//     // time = o(s)
-//     for i := 0; i < len(s); i++ {
-//         for freq[s[i]] > 0 {
-//             out.WriteByte(s[i])
-//             freq[s[i]]--
-//         }        
-//     }
-//     return out.String()
-// }
