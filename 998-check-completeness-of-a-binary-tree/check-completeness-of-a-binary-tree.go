2745ba96e6c2904100e7f0115c5d8305357c3968
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-12 22:03:18 -0800

    Time: 0 ms (100.00%) | Memory: 5 MB (37.78%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.go
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.go
@@ -7,19 +7,42 @@
  * }
  */
 func isCompleteTree(root *TreeNode) bool {
-    if root == nil {return true }
-    seenNil := false
-    q := []*TreeNode{root}
-    for len(q) != 0 {
-        dq := q[0]
-        q = q[1:]
-        if dq == nil {
-            seenNil = true
-        } else {
-            if seenNil {return false}
-            q = append(q, dq.Left)
-            q = append(q, dq.Right)
-        }
+    var count func(r *TreeNode) int
+    count = func(r *TreeNode) int {
+        // base
+        if r == nil {return 0}
+
+        // logic
+        return 1 + count(r.Left) + count(r.Right)
     }
-    return true
+    n := count(root)
+    var dfs func(r *TreeNode, idx int) bool
+    dfs = func(r *TreeNode, idx int) bool {
+        // base
+        if r == nil {return true}
+
+        // logic
+        if idx >= n {return false}
+        if !dfs(r.Left, 2*idx+1) {return false}
+        return dfs(r.Right, 2*idx+2)
+    }
+    return dfs(root, 0)
 }
+
+// func isCompleteTree(root *TreeNode) bool {
+//     if root == nil {return true }
+//     seenNil := false
+//     q := []*TreeNode{root}
+//     for len(q) != 0 {
+//         dq := q[0]
+//         q = q[1:]
+//         if dq == nil {
+//             seenNil = true
+//         } else {
+//             if seenNil {return false}
+//             q = append(q, dq.Left)
+//             q = append(q, dq.Right)
+//         }
+//     }
+//     return true
+// }
