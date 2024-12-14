422ee81ab88a729b860ff75562869f036e0b4cf7
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-14 11:41:31 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (85.62%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.go
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.go
@@ -10,19 +10,45 @@
 
 func connect(root *Node) *Node {
     if root == nil {return root}
-	q := []*Node{root}
-    for len(q) != 0 {
-        qSize := len(q)
-        for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            qSize--
-            if qSize > 0 && len(q) != 0 {
-                dq.Next = q[0]
+    r := root
+    for r != nil {
+        curr := r
+        for curr != nil {
+            if curr.Left != nil && curr.Right != nil {
+                curr.Left.Next = curr.Right
             }
-            if dq.Left != nil {q = append(q, dq.Left)}
-            if dq.Right != nil {q = append(q, dq.Right)}
+            if curr.Right != nil && curr.Next != nil {
+                if curr.Next.Left != nil  {
+                    curr.Right.Next = curr.Next.Left
+                } else if curr.Next.Right != nil {
+                    curr.Right.Next = curr.Next.Right
+                }
+            }
+            curr = curr.Next
+        }
+        if r.Left != nil {
+            r = r.Left
+        } else {
+            r = r.Right
         }
     }
     return root
 }
+// func connect(root *Node) *Node {
+//     if root == nil {return root}
+// 	q := []*Node{root}
+//     for len(q) != 0 {
+//         qSize := len(q)
+//         for qSize != 0 {
+//             dq := q[0]
+//             q = q[1:]
+//             qSize--
+//             if qSize > 0 && len(q) != 0 {
+//                 dq.Next = q[0]
+//             }
+//             if dq.Left != nil {q = append(q, dq.Left)}
+//             if dq.Right != nil {q = append(q, dq.Right)}
+//         }
+//     }
+//     return root
+// }
