8441c348a94580548dc248818c95a866fd99007e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-14 11:24:26 -0800

    Time: 6 ms (74.24%) | Memory: 8.9 MB (49.12%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
@@ -1,19 +1,39 @@
 func minRemoveToMakeValid(s string) string {
-    st := []int{} // idx
-    invalid := map[int]bool{}
+    out := new(strings.Builder)
+    count := 0
     for i := 0; i < len(s); i++ {
         if s[i] == '(' {
-            st = append(st, i)
+            count++
+            out.WriteByte(s[i])
         } else if s[i] == ')' {
-            if len(st) == 0 {invalid[i] = true; continue}
-            st = st[:len(st)-1]
+            if count > 0 {
+                count--
+                out.WriteByte(s[i])
+            }
+        } else {
+            out.WriteByte(s[i])
         }
     }
-    for i := 0; i < len(st); i++ {invalid[st[i]] = true}
-    out := new(strings.Builder)
-    for i := 0; i < len(s); i++ {
-        if invalid[i] {continue}
-        out.WriteByte(s[i])
+    outStr := out.String()
+    out.Reset()
+    count = 0
+    for i := len(outStr)-1; i >= 0; i-- {
+        if outStr[i] == '(' {
+            if count > 0 {
+                count--
+                out.WriteByte(outStr[i])
+            }
+        } else if outStr[i] == ')' {
+            count++ 
+            out.WriteByte(outStr[i])
+        } else {
+            out.WriteByte(outStr[i])
+        }
     }
+    outStr = out.String()
+    out.Reset()
+    for i := len(outStr)-1; i >= 0; i-- {
+        out.WriteByte(outStr[i])
+    } 
     return out.String()
 }
