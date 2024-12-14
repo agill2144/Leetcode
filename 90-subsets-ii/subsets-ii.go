243e90310954ffea495b1cdb472a9d40b48c7c38
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-14 14:46:17 -0800

    Time: 0 ms (100.00%) | Memory: 4.4 MB (63.57%) - LeetSync

--- 90-subsets-ii/subsets-ii.go
+++ 90-subsets-ii/subsets-ii.go
@@ -1,27 +1,29 @@
-// same approach as combination sum
-// tc = o(nlogn) + o(2^n * n)
-// sc = o(n) + o(n) + o(n)
 func subsetsWithDup(nums []int) [][]int {
     sort.Ints(nums)
     out := [][]int{}
     var dfs func(start int, path []int)
     dfs = func(start int, path []int) {
         // base
+
+        // logic
         newL := make([]int, len(path))
         copy(newL, path)
         out = append(out, newL)
 
-        // logic
         for i := start; i < len(nums); i++ {
-            // action
+            // action : choose this number
             path = append(path, nums[i])
             // recurse
             dfs(i+1, path)
-            // backtrack
+            // backtrack : remove this number
+            // as-in, do not choose this num
+            skip := path[len(path)-1]
             path = path[:len(path)-1]
-            // not-choose case; do not choose this number and also skip all instances of this number
-            for i+1 < len(nums) && nums[i] == nums[i+1] {i++; continue}
-        }
+            // if next num is same as the one we just removed, skip it!
+
+            for i+1 < len(nums) && nums[i+1] == skip {i++}
+
+        } 
     }
     dfs(0, nil)
     return out
