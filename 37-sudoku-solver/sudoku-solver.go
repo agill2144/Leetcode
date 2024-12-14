9a125a64fc5c110fa64ba4a69d369efe41e2e0a1
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-14 08:59:52 -0800

    Time: 44 ms (45.70%) | Memory: 4.1 MB (39.23%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.go
+++ 37-sudoku-solver/sudoku-solver.go
@@ -1,61 +1,48 @@
-func solveSudoku(board [][]byte) {
-    var rows, cols [9][10]bool
-    var boxes [3][3][10]bool
-
-    // First pass: mark existing numbers
-    for i := 0; i < 9; i++ {
-        for j := 0; j < 9; j++ {
+func solveSudoku(board [][]byte)  {
+    rows := [10][10]bool{}
+    cols := [10][10]bool{}
+    box :=  [3][3][10]bool{}
+    n := len(board)
+    for i := 0; i < n; i++ {
+        for j := 0; j < n; j++ {
             if board[i][j] != '.' {
-                num := int(board[i][j] - '0')
-                rows[i][num] = true
-                cols[j][num] = true
-                boxes[i/3][j/3][num] = true
+                idx := int(board[i][j]-'0')
+                rows[i][idx] = true
+                box[i/3][j/3][idx] = true
+            }
+            if board[j][i] != '.' {
+                idx := int(board[j][i]-'0')
+                cols[i][idx] = true
             }
         }
     }
+    var dfs func(r, c int) bool
+    dfs = func(r, c int) bool {
+        // base
+        if r == n {return true}
+        if c == n {return dfs(r+1, 0)}
+        if board[r][c] != '.' {return dfs(r,c+1)}
 
-    var solve func(int, int) bool
-    solve = func(r, c int) bool {
-        // Base case: solved the entire board
-        if r == 9 {
-            return true
-        }
+        // logic
+        for i := 1; i <= 9; i++ {
+            inRow := rows[r][i]
+            inCol := cols[c][i]
+            inBox := box[r/3][c/3][i]
+            canUse := !inRow && !inCol && !inBox
 
-        // Move to next row if column is full
-        if c == 9 {
-            return solve(r+1, 0)
-        }
-
-        // Skip filled cells
-        if board[r][c] != '.' {
-            return solve(r, c+1)
-        }
-
-        // Try placing numbers 1-9
-        for num := 1; num <= 9; num++ {
-            // Check if number can be placed
-            if !rows[r][num] && !cols[c][num] && !boxes[r/3][c/3][num] {
-                // Place the number
-                board[r][c] = byte(num) + '0'
-                rows[r][num] = true
-                cols[c][num] = true
-                boxes[r/3][c/3][num] = true
-
-                // Try solving the rest of the board
-                if solve(r, c+1) {
-                    return true
-                }
-
-                // Backtrack
+            if canUse {
+                rows[r][i] = true
+                cols[c][i] = true
+                box[r/3][c/3][i] = true
+                board[r][c] =  byte(i)+'0'
+                if dfs(r, c+1) {return true}
                 board[r][c] = '.'
-                rows[r][num] = false
-                cols[c][num] = false
-                boxes[r/3][c/3][num] = false
+                box[r/3][c/3][i] = false
+                cols[c][i] = false
+                rows[r][i] = false
             }
         }
-
         return false
     }
-
-    solve(0, 0)
+    dfs(0, 0)
 }
