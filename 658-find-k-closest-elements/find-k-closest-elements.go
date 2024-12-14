95d46f5ef93516b7d51559bae83c402d449ac34b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-14 07:33:46 -0800

    Time: 0 ms (100.00%) | Memory: 8.8 MB (71.67%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.go
+++ 658-find-k-closest-elements/find-k-closest-elements.go
@@ -1,53 +1,17 @@
 func findClosestElements(arr []int, k int, x int) []int {
     n := len(arr)
-    left := 0
-    right := n-k
-    for left <= right {
-        mid := left + (right-left)/2
-        startDist := x-arr[mid]
-        if mid+k == n {break}
-        endDist := arr[mid+k]-x
-        if endDist >= startDist {
-            right = mid-1
+    left, right := 0, n-1
+    for right-left+1 != k {
+        rightDist := abs(arr[right]-x)
+        leftDist := abs(arr[left]-x)
+        if leftDist <= rightDist {
+            right--
         } else {
-            left = mid+1
+            left++
         }
     }
     return arr[left:left+k]
 }
-/*
-    approach: 2 ptrs
-    - left ptr at 0
-    - right ptr at n-1
-    - calc dist of both
-    - move away from ptr whose dist is farther away
-    - what if dists are same?
-        - move away from right
-        - "|a - x| == |b - x| and a < b"
-        - a = left; b = right
-        - we want to keep a if a is smaller
-        - this is a sorted array, therefore a will always be smaller
-        - therefore we move from right ptr
-    time = o(n-k) + k
-    space = o(1)
-
-*/
-// func findClosestElements(arr []int, k int, x int) []int {
-//     n := len(arr)
-//     left := 0
-//     right := n-1
-//     // n-k
-//     for right-left+1 != k {
-//         leftDist := abs(arr[left]-x)
-//         rightDist := abs(arr[right]-x)
-//         if rightDist >= leftDist {
-//             right--
-//         } else {
-//             left++
-//         }
-//     }
-//     return arr[left:right+1]
-// }
 
 func abs(x int) int {
     if x < 0 {return x*-1}
