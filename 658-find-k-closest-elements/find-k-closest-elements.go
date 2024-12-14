f0f56b6cb2d61e7cfbc0e69fc78694c706d62541
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-14 07:39:30 -0800

    Time: 0 ms (100.00%) | Memory: 8.8 MB (71.67%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.go
+++ 658-find-k-closest-elements/find-k-closest-elements.go
@@ -1,19 +1,32 @@
 func findClosestElements(arr []int, k int, x int) []int {
     n := len(arr)
-    left, right := 0, n-1
-    for right-left+1 != k {
-        rightDist := abs(arr[right]-x)
-        leftDist := abs(arr[left]-x)
-        if leftDist <= rightDist {
-            right--
+    left, right := 0, n-k
+    for left <= right {
+        mid := left + (right-left)/2
+        if mid+k >= n {break}
+        startDist := x-arr[mid]
+        endDist := arr[mid+k]-x
+        if endDist >= startDist {
+            right = mid-1
         } else {
-            left++
+            left = mid+1
         }
     }
     return arr[left:left+k]
 }
-
-func abs(x int) int {
-    if x < 0 {return x*-1}
-    return x
-}
+// tc = o(n-k) + o(k)
+// sc = o(1)
+// func findClosestElements(arr []int, k int, x int) []int {
+//     n := len(arr)
+//     left, right := 0, n-1
+//     for right-left+1 != k {
+//         rightDist := abs(arr[right]-x)
+//         leftDist := abs(arr[left]-x)
+//         if leftDist <= rightDist {
+//             right--
+//         } else {
+//             left++
+//         }
+//     }
+//     return arr[left:left+k]
+// }
