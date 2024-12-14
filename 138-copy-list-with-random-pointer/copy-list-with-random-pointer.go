b52e52f7b020a7fd8a54222595cb7902d3c7ba8a
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-14 10:16:33 -0800

    Time: 0 ms (100.00%) | Memory: 5.3 MB (35.57%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.go
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.go
@@ -12,11 +12,11 @@ func copyRandomList(head *Node) *Node {
     curr := head
     for curr != nil {
         next := curr.Next
-        curr.Next = &Node{Val: curr.Val}
-        curr.Next.Next = next
+        newNode := &Node{Val:curr.Val}
+        curr.Next = newNode
+        newNode.Next = next
         curr = next
     }
-
     curr = head
     for curr != nil {
         next := curr.Next.Next
@@ -25,45 +25,15 @@ func copyRandomList(head *Node) *Node {
         }
         curr = next
     }
-    dummy := &Node{Val: 0}
-    tail := dummy 
+    dummy := &Node{Val:0}
+    tail := dummy
     curr = head
     for curr != nil {
         next := curr.Next.Next
-        r := curr.Next
-        r.Next = nil
-        curr.Next = next
-        tail.Next = r
+        tail.Next = curr.Next
         tail = tail.Next
+        curr.Next = next
         curr = next
     }
     return dummy.Next
 }
-
-// func copyRandomList(head *Node) *Node {
-//     if head == nil {return nil}
-//     m := map[*Node]*Node{}
-//     dummy := &Node{Val:0}
-//     tail := dummy
-//     curr := head
-//     for curr != nil {
-//         next := curr.Next
-//         node := m[curr]
-//         if node == nil {
-//             node = &Node{Val: curr.Val}
-//             m[curr] = node
-//         }
-//         tail.Next = node
-//         tail = tail.Next
-//         if curr.Random != nil {
-//             randCp := m[curr.Random]
-//             if randCp == nil {
-//                 randCp = &Node{Val:curr.Random.Val}
-//                 m[curr.Random] = randCp
-//             }
-//             node.Random = randCp
-//         }
-//         curr = next
-//     }
-//     return dummy.Next
-// }
