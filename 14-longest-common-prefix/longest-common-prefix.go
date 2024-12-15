c893e86724e9710f2e9f5ee36a97de1de820d041
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-14 19:53:46 -0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (61.20%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.go
+++ 14-longest-common-prefix/longest-common-prefix.go
@@ -1,21 +1,44 @@
 func longestCommonPrefix(strs []string) string {
-    n := len(strs)
-    if n <= 1 {
-        if n == 1 {return strs[0]}
-        return ""
-    }
-    sort.Strings(strs)
-    left := 0
-    right := 0
-    for left < len(strs[0]) && right < len(strs[n-1]) {
-        if strs[0][left] != strs[len(strs)-1][right] {
-            return strs[0][:left]
+    if len(strs) == 0 {return ""}
+    if len(strs) == 1 {return strs[0]}
+    
+    for i := 0; i < len(strs[0]); i++ {
+        for j := 1; j < len(strs) ; j++ {
+            if i == len(strs[j]) || strs[j][i] != strs[0][i] {return strs[0][:i]}
         }
-        left++
-        right++
     }
     return strs[0]
 }
+/*
+    approach: sort + 2 ptrs
+    - sorting groups common prefixes
+    - then take 2 ptrs to compare first str and last str
+        - compare char by char
+        - we have to compare 1st and last because
+        - we want to find the common prefix that is present in ALL strings
+    n = len(strs)
+    k = avg len of each word
+    time = o(nlogn) + o(k)
+    space = o(1)
+*/
+// func longestCommonPrefix(strs []string) string {
+//     n := len(strs)
+//     if n <= 1 {
+//         if n == 1 {return strs[0]}
+//         return ""
+//     }
+//     sort.Strings(strs)
+//     left := 0
+//     right := 0
+//     for left < len(strs[0]) && right < len(strs[n-1]) {
+//         if strs[0][left] != strs[len(strs)-1][right] {
+//             return strs[0][:left]
+//         }
+//         left++
+//         right++
+//     }
+//     return strs[0]
+// }
 /*
     approach: brute force
     num of words = n
