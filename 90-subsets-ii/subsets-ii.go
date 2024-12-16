71e0902dd37205e180d547fe2576a1331d1e79be
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-16 13:15:48 -0800

    Time: 0 ms (100.00%) | Memory: 4.5 MB (21.85%) - LeetSync

--- 90-subsets-ii/subsets-ii.go
+++ 90-subsets-ii/subsets-ii.go
@@ -1,10 +1,3 @@
-// o(numberOfOptionsPerNode ^ numOfOptions * extraWorkAtEachNode )
-// numberOfOptionsPerNode = 2; choose and not-choose
-// numOfOptions = n
-// extraWorkAtEachNode = o(n) ; copy path into a new list
-// tc = o(nlogn) +  o(n*2^n) 
-// 2 = number of options
-// sc = o(n) + o(n) = 2n = o(n)
 func subsetsWithDup(nums []int) [][]int {
     sort.Ints(nums)
     out := [][]int{}
@@ -16,21 +9,16 @@ func subsetsWithDup(nums []int) [][]int {
         newL := make([]int, len(path))
         copy(newL, path)
         out = append(out, newL)
-
         for i := start; i < len(nums); i++ {
-            // action : choose this number
+            // action
             path = append(path, nums[i])
             // recurse
             dfs(i+1, path)
-            // backtrack : remove this number
-            // as-in, do not choose this num
-            skip := path[len(path)-1]
+            // backtrack
             path = path[:len(path)-1]
-            // if next num is same as the one we just removed, skip it!
-
-            for i+1 < len(nums) && nums[i+1] == skip {i++}
-
-        } 
+            skipElement := nums[i]
+            for i+1 < len(nums) && nums[i+1] == skipElement {i++}
+        }
     }
     dfs(0, nil)
     return out
