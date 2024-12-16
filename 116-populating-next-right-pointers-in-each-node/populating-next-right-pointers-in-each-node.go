1a0c83971486e6a9777373bf347f73065afa9880
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-16 13:23:04 -0800

    Time: 7 ms (42.56%) | Memory: 7.8 MB (92.57%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.go
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.go
@@ -9,7 +9,7 @@
  */
 
 func connect(root *Node) *Node {
-    if root == nil {return root}
+	if root == nil {return root}
     r := root
     for r != nil {
         curr := r
@@ -18,7 +18,7 @@ func connect(root *Node) *Node {
                 curr.Left.Next = curr.Right
             }
             if curr.Right != nil && curr.Next != nil {
-                if curr.Next.Left != nil  {
+                if curr.Next.Left != nil {
                     curr.Right.Next = curr.Next.Left
                 } else if curr.Next.Right != nil {
                     curr.Right.Next = curr.Next.Right
@@ -26,29 +26,7 @@ func connect(root *Node) *Node {
             }
             curr = curr.Next
         }
-        if r.Left != nil {
-            r = r.Left
-        } else {
-            r = r.Right
-        }
+        r = r.Left
     }
     return root
 }
-// func connect(root *Node) *Node {
-//     if root == nil {return root}
-// 	q := []*Node{root}
-//     for len(q) != 0 {
-//         qSize := len(q)
-//         for qSize != 0 {
-//             dq := q[0]
-//             q = q[1:]
-//             qSize--
-//             if qSize > 0 && len(q) != 0 {
-//                 dq.Next = q[0]
-//             }
-//             if dq.Left != nil {q = append(q, dq.Left)}
-//             if dq.Right != nil {q = append(q, dq.Right)}
-//         }
-//     }
-//     return root
-// }
