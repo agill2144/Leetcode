6b8b0460312bc8053a47a6c6adec9c1e6cd679a2
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-16 13:09:42 -0800

    Time: 81 ms (19.95%) | Memory: 9.1 MB (73.30%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.go
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.go
@@ -1,49 +1,43 @@
-func kClosest(points [][]int, k int) [][]int {
-    mx := &maxHeap{items: []*distNode{}}
+func kClosest(points [][]int, k int) [][]int {    
+    type distNode struct {
+        dist float64
+        point []int
+    }
+    dists := []*distNode{}
     for i := 0; i < len(points); i++ {
-        heap.Push(mx, &distNode{
-            dist: calcDist(points[i][0], points[i][1]),
+        dists = append(dists, &distNode{
             point: points[i],
+            dist: calcDist(points[i][0], points[i][1]),
         })
-        if mx.Len() > k {
-            heap.Pop(mx)
+    }
+    targetIdx := k-1
+    left := 0
+    right := len(dists)-1
+    for left <= right {
+        ns := left
+        pivot := right
+        for i := left; i < pivot; i++ {
+            if dists[i].dist <= dists[pivot].dist {
+                dists[i], dists[ns] = dists[ns], dists[i]
+                ns++
+            }
+        }
+        dists[ns], dists[pivot] = dists[pivot], dists[ns]
+        if ns == targetIdx {
+            break
+        } else if targetIdx > ns {
+            left = ns+1
+        } else {
+            right = ns-1
         }
     }
     out := [][]int{}
     for i := 0; i < k; i++ {
-        out = append(out, heap.Pop(mx).(*distNode).point)
+        out =append(out, dists[i].point)
     }
     return out
-
 }
 
-func calcDist(x , y int) float64 {
-    return math.Sqrt(float64(x*x)+float64(y*y))
-}
-
-type distNode struct {
-    dist float64
-    point []int
-}
-
-type maxHeap struct {
-	items []*distNode
-}
-
-func (m *maxHeap) Less(i, j int) bool {
-	return m.items[i].dist > m.items[j].dist
-}
-func (m *maxHeap) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-func (m *maxHeap) Len() int {
-	return len(m.items)
-}
-func (m *maxHeap) Push(x interface{}) {
-	m.items = append(m.items, x.(*distNode))
-}
-func (m *maxHeap) Pop() interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
+func calcDist(x, y int) float64 {
+    return math.Sqrt(math.Pow(float64(x), 2.0)+math.Pow(float64(y),2))
 }
