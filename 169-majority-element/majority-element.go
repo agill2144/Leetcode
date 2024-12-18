78224d103d95fcbffd6e569fa3202a497124c275
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-17 23:23:45 -0800

    Time: 0 ms (100.00%) | Memory: 8.4 MB (57.22%) - LeetSync

--- 169-majority-element/majority-element.go
+++ 169-majority-element/majority-element.go
@@ -1,9 +1,30 @@
 func majorityElement(nums []int) int {
-    n := len(nums)
-    freq := map[int]int{}
-    for i := 0; i < n; i++ {
-        freq[nums[i]]++
-        if freq[nums[i]] > n/2 {return nums[i]}
+    count := 0
+    ele := -1
+    for i := 0; i < len(nums); i++ {
+        if count == 0 {
+            count = 1
+            ele = nums[i]
+        } else if nums[i] == ele{ 
+            count++
+        } else {
+            count--
+        }
     }
-    return -1
+    return ele
 }
+/*
+    approach: brute force
+    - freq map
+    tc = o(n)
+    sc = o(n)
+*/
+// func majorityElement(nums []int) int {
+//     n := len(nums)
+//     freq := map[int]int{}
+//     for i := 0; i < n; i++ {
+//         freq[nums[i]]++
+//         if freq[nums[i]] > n/2 {return nums[i]}
+//     }
+//     return -1
+// }
