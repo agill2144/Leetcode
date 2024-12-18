56f8200fbe2098c1c0bae3737fa950a4680e8624
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-18 14:02:05 -0800

    Time: 2 ms (62.78%) | Memory: 4.4 MB (62.78%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.go
+++ 518-coin-change-ii/coin-change-ii.go
@@ -2,21 +2,16 @@
 func change(amount int, coins []int) int {
     m := len(coins)+1
     n := amount+1
-    dp := make([][]int, m)
-    for i := 0; i < m; i++ {
-        dp[i] = make([]int, n)
-        dp[i][0] = 1
-    }
+    dp := make([]int, n)
+    dp[0] = 1
     for i := 1; i < m; i++ {
         for j := 1; j < n; j++ {
             coin := coins[i-1]
             am := j
-            if coin > am {
-                dp[i][j] = dp[i-1][j]
-            } else {
-                dp[i][j] = dp[i-1][j] + dp[i][j-coin] 
+            if coin <= am {
+                dp[j] = dp[j] + dp[j-coin] 
             }
         }
     }
-    return dp[m-1][n-1]
+    return dp[n-1]
 }
