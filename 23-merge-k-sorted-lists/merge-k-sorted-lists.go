ea6e7e9739760775dbda9f65d6a2dff1b5ebe643
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-19 14:02:39 -0800

    Time: 1733 ms (5.04%) | Memory: 16.4 MB (5.04%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.go
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -5,20 +5,34 @@
  *     Next *ListNode
  * }
  */
+
+
 func mergeKLists(lists []*ListNode) *ListNode {
-    merged := []int{}
-    for i := 0; i < len(lists); i++ {
-        curr := lists[i]
-        for curr != nil {
-            merged = append(merged, curr.Val)
-            curr = curr.Next
-        }
+    if len(lists) == 0 {return nil}
+    head := lists[0]
+    for i := 1; i < len(lists); i++ {
+        head = merge2Lists(head, lists[i])
     }
-    sort.Ints(merged)
+    return head
+}
+
+func merge2Lists(l1, l2 *ListNode) *ListNode {
+    if l1 == nil {return l2}
+    if l2 == nil {return l1}
     head := &ListNode{Val: 0}
     tail := head
-    for i := 0; i < len(merged); i++ {
-        tail.Next = &ListNode{Val: merged[i]}
+    for l1 != nil || l2 != nil {
+        l1Val := math.MaxInt64
+        if l1 != nil {l1Val = l1.Val}
+        l2Val := math.MaxInt64
+        if l2 != nil {l2Val = l2.Val}
+        if l1Val < l2Val {
+            tail.Next = &ListNode{Val: l1Val}
+            l1 = l1.Next
+        } else {
+            tail.Next = &ListNode{Val: l2Val}
+            l2 = l2.Next
+        }
         tail = tail.Next
     }
     return head.Next
