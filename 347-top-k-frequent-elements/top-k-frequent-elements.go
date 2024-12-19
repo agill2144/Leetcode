ac4e494536d09c598cb933a9e039d3eaf1d6feb2
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-19 14:27:16 -0800

    Time: 2 ms (51.57%) | Memory: 7.9 MB (44.65%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,18 +1,58 @@
+// sorting ? heaping ? bucket sorting ?
+// quick select may also work
 func topKFrequent(nums []int, k int) []int {
     freq := map[int]int{}
     for i := 0; i < len(nums); i++ {freq[nums[i]]++}
-    bucket := make([][]int, len(nums)+1)
-    for key, val := range freq {
-        bucket[val] = append(bucket[val], key)
-    }
-    out := []int{}
-    for i := len(bucket)-1; i >= 0 && len(out) != k; i-- {
-        if bucket[i] != nil {
-            out = append(out, bucket[i]...)
+    deduped := []int{}
+    for key, _ := range freq {deduped= append(deduped, key)}
+    targetIdx := len(deduped)-k
+    left := 0
+    right := len(deduped)-1
+    for left <= right {
+        nsf := left
+        pivot := right
+        for i := left; i < pivot; i++ {
+            if freq[deduped[i]] <= freq[deduped[pivot]] {
+                deduped[i], deduped[nsf] = deduped[nsf], deduped[i]
+                nsf++
+            }
+        }
+        deduped[pivot], deduped[nsf] = deduped[nsf], deduped[pivot]
+        if nsf == targetIdx {return deduped[targetIdx:]}
+        if targetIdx > nsf {
+            left = nsf+1
+        } else {
+            right = nsf-1
         }
     }
-    return out
+    return nil
 }
+// sorting ? heaping?
+// try bucket sort
+// we want to sort by freq
+// we can use bucket idxs are freq val
+// and map each element to idx based on its freq ( freq == idx in bucket )
+// because multiple elements could exist with same freq, 
+// each idx in bucket should be able to save multiple elements ( because idx in bucket == freq val )
+// therefore our bucket wont be a simple []int, it will be a [][]int
+// bucket size is also known
+// because we are sorting by freq, the largest possible idx we need in our bucket will be 
+// the largest freq val, and that value will never exceed the size of nums array
+// func topKFrequent(nums []int, k int) []int {
+//     freq := map[int]int{}
+//     for i := 0; i < len(nums); i++ {freq[nums[i]]++}
+//     bucket := make([][]int, len(nums)+1)
+//     for key, val := range freq {
+//         bucket[val] = append(bucket[val], key)
+//     }
+//     out := []int{}
+//     for i := len(bucket)-1; i >= 0 && len(out) != k; i-- {
+//         if bucket[i] != nil {
+//             out = append(out, bucket[i]...)
+//         }
+//     }
+//     return out
+// }
 
 // tc = o(n) + o(nlogk) + o(k)
 // sc = o(n) + o(k)
