88f73566185ddb7f17747b2dd1d0a23cb682ae48
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-19 14:11:07 -0800

    Time: 29 ms (5.74%) | Memory: 7.9 MB (44.65%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,31 +1,19 @@
 func topKFrequent(nums []int, k int) []int {
     freq := map[int]int{}
-    deduped := []int{}
-    for i := 0; i < len(nums); i++ {
-        if freq[nums[i]] == 0 {
-            deduped = append(deduped, nums[i])
+    for i := 0; i < len(nums); i++ {freq[nums[i]]++}
+    sort.Slice(nums, func(i, j int)bool{
+        if freq[nums[i]] == freq[nums[j]] {
+            return nums[i] < nums[j]
         }
-        freq[nums[i]]++
-    }
-    left := 0
-    right := len(deduped)-1
-    targetIdx := len(deduped)-k
-    for left <= right {
-        pivot := right
-        nsf := left
-        for i := left; i < pivot; i++ {
-            if freq[deduped[i]] <= freq[deduped[pivot]] {
-                deduped[nsf], deduped[i] = deduped[i], deduped[nsf]
-                nsf++ 
-            }
-        }
-        deduped[pivot], deduped[nsf] = deduped[nsf], deduped[pivot]
-        if nsf == targetIdx {return deduped[nsf:]}
-        if targetIdx < nsf {
-            right = nsf-1
-        } else {
-            left = nsf+1
+        return freq[nums[i]] < freq[nums[j]]
+    })
+    out := []int{}
+    for i := len(nums)-1; i >= 0 && len(out) != k; i-- {
+        lastNum := math.MinInt64
+        if len(out) != 0 {lastNum = out[len(out)-1]}
+        if nums[i] != lastNum {
+            out = append(out, nums[i])
         }
     }
-    return nil
+    return out
 }
