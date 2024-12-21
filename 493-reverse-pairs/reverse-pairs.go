030feaefcdef8e25a794fb2f47bbfdccfcddc307
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-21 12:23:56 -0800

    Time: 99 ms (36.67%) | Memory: 35 MB (6.67%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.go
+++ 493-reverse-pairs/reverse-pairs.go
@@ -3,23 +3,24 @@ func reversePairs(nums []int) int {
     var dfs func(left, right int)
     dfs = func(left, right int) {
         // base
-        if left >= right {return }
+        if left >= right {return}
+
 
         // logic
         mid := left + (right-left)/2
         dfs(left, mid)
         dfs(mid+1, right)
-        i, j := left, mid+1
+        i := left; j := mid+1
         for i <= mid && j <= right {
-            if nums[i] > 2 * nums[j] {
+            if nums[i] > 2*nums[j] {
                 count += (mid-i+1)
                 j++
             } else {
                 i++
             }
         }
+        i = left; j = mid+1
         merged := []int{}
-        i, j = left, mid+1
         for i <= mid && j <= right {
             if nums[i] < nums[j] {
                 merged = append(merged, nums[i])
@@ -29,14 +30,13 @@ func reversePairs(nums []int) int {
                 j++
             }
         }
-        for i <= mid {merged = append(merged, nums[i]); i++}
-        for j <= right {merged = append(merged, nums[j]); j++}
+        for i <= mid {merged=append(merged, nums[i]); i++}
+        for j <= right {merged=append(merged, nums[j]); j++}
         ptr := 0
         for i := left; i <= right; i++ {
             nums[i] = merged[ptr]
             ptr++
         }
-
     }
     dfs(0, len(nums)-1)
     return count
