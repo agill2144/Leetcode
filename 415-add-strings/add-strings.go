b163751933d0c69bbb4b8d74579b67c6a126ec5e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-21 11:45:20 -0800

    Time: 11 ms (25.12%) | Memory: 9 MB (34.60%) - LeetSync

--- 415-add-strings/add-strings.go
+++ 415-add-strings/add-strings.go
@@ -1,21 +1,19 @@
 func addStrings(num1 string, num2 string) string {
-    n1 := len(num1)-1
-    n2 := len(num2)-1
     carry := 0
-    tmp := []int{}
+    res := ""
+    n1, n2 := len(num1)-1, len(num2)-1
     for n1 >= 0 || n2 >= 0 {
         n1Val := 0
         n2Val := 0
         if n1 >= 0 {n1Val = int(num1[n1]-'0'); n1--}
         if n2 >= 0 {n2Val = int(num2[n2]-'0'); n2--}
         sum := n1Val + n2Val + carry
-        tmp = append(tmp, sum%10)
-        carry = sum / 10        
+        res = fmt.Sprintf("%v", sum%10) + res
+        carry = sum / 10
     }
-    if carry != 0 {tmp = append(tmp, carry)}
-    out := new(strings.Builder)
-    for i := len(tmp)-1; i >= 0; i-- {
-        out.WriteString(fmt.Sprintf("%v", tmp[i]))
+    if carry != 0 {
+        res = fmt.Sprintf("%v", carry) + res
+        carry = 0
     }
-    return out.String()
+    return res
 }
