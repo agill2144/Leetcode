c4ae40f7bf6b05e61ca22372021816836da42b16
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-21 15:34:05 -0800

    Time: 0 ms (100.00%) | Memory: 4.7 MB (42.45%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.go
+++ 101-symmetric-tree/symmetric-tree.go
@@ -8,10 +8,13 @@
  */
 func isSymmetric(root *TreeNode) bool {
     var dfs func(r1, r2 *TreeNode) bool
-    dfs = func(r1,r2 *TreeNode) bool {
+    dfs = func(r1, r2 *TreeNode) bool {
+        // base
         if r1 == nil && r2 == nil {return true}
+
+        // logic
         if r1 == nil || r2 == nil {return false}
-        return r1.Val == r2.Val && dfs(r1.Left, r2.Right) && dfs(r1.Right, r2.Left) 
+        return r1.Val == r2.Val && dfs(r1.Left, r2.Right) && dfs(r1.Right, r2.Left)
     }
     return dfs(root, root)
 }
