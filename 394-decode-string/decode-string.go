28261438e02065b8ff2e9d6f4c14ec43f7e1be82
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-21 13:59:28 -0800

    Time: 0 ms (100.00%) | Memory: 4.1 MB (24.13%) - LeetSync

--- 394-decode-string/decode-string.go
+++ 394-decode-string/decode-string.go
@@ -1,32 +1,32 @@
 func decodeString(s string) string {
-    strs := []*strings.Builder{}
-    nums := []int{}
+    st := []string{}
+    numSt := []int{}
     curr := new(strings.Builder)
-    currNum := 0
+    currN := 0
     for i := 0; i < len(s); i++ {
-        char := s[i]
-        if char >= '0' && char <= '9' {
-            currNum = currNum * 10 + int(char-'0')
-        } else if char >= 'a' && char <= 'z' {
-            curr.WriteByte(char)
-        } else if char == '[' {
-            nums = append(nums, currNum)
-            strs = append(strs, curr)
-            curr = new(strings.Builder)
-            currNum = 0
-        } else if char == ']' {
-            // repeat curr string $top num times
-            k := nums[len(nums)-1]
-            nums = nums[:len(nums)-1]
-            decoded := new(strings.Builder)
-            for j := 0; j < k; j++ {
-                decoded.WriteString(curr.String())
+        if s[i] >= '0' && s[i] <= '9' {
+            currN = currN * 10 + int(s[i]-'0')
+        } else if s[i] >= 'a' && s[i] <= 'z' {
+            curr.WriteByte(s[i])
+        } else if s[i] == '[' {
+            st = append(st, curr.String())
+            numSt = append(numSt, currN)
+            currN = 0
+            curr.Reset()             
+        } else if s[i] == ']' {
+            k := numSt[len(numSt)-1]
+            numSt = numSt[:len(numSt)-1]
+            decode := new(strings.Builder)
+            currStr := curr.String()
+            for k != 0 {
+                decode.WriteString(currStr)
+                k--
             }
-            // concat $top-of-strs + decoded
-            curr = new(strings.Builder)
-            curr.WriteString(strs[len(strs)-1].String())
-            curr.WriteString(decoded.String())
-            strs = strs[:len(strs)-1]
+            parent := st[len(st)-1]
+            st = st[:len(st)-1]
+            curr.Reset()
+            curr.WriteString(parent)
+            curr.WriteString(decode.String())
         }
     }
     return curr.String()
