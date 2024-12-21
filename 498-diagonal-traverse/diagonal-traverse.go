433f2c42c08bd63f1dc9e2ebd91cfcfee36a9c04
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-21 10:36:27 -0800

    Time: 0 ms (100.00%) | Memory: 9.2 MB (16.35%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.go
+++ 498-diagonal-traverse/diagonal-traverse.go
@@ -7,28 +7,26 @@ func findDiagonalOrder(mat [][]int) []int {
     for len(out) != m*n {
         out = append(out, mat[r][c])
         if up {
+            // when going up, we could hit
+            // top row edge, or right col edge
             if r == 0 || c == n-1 {
-                if c != n-1 {
-                    c++
-                } else {
-                    r++
-                }
+                // continue in next col if we are not yet at n-1 col
+                // when we are at n-1 col, start from next row, c++ will go out of bounds
+                if c != n-1 { c++ } else { r++ }
                 up = false
             } else {
-                r--
-                c++
+                // not hitting edges yet
+                // keep going up in this dir
+                r--; c++
             }
         } else {
             if c == 0 || r == m-1 {
-                if r != m-1 {
-                    r++
-                } else {
-                    c++
-                }
+                if r != m-1 { r++ } else { c++ }
                 up = true
             } else {
-                r++
-                c--
+                // not hitting edges yet
+                // keep going up in this dir
+                r++; c--
             }
         }
     }
