6c553f26b8f0532f3309a4a9d116f5549204f1ac
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-21 19:11:48 -0800

    Time: 2 ms (69.69%) | Memory: 8.7 MB (20.29%) - LeetSync

--- 207-course-schedule/course-schedule.go
+++ 207-course-schedule/course-schedule.go
@@ -1,60 +1,27 @@
-func canFinish(numCourses int, prerequisites [][]int) bool {
+func canFinish(numCourses int, pre [][]int) bool {
+    indegrees := make([]int, numCourses)
     adjList := map[int][]int{}
-    for i := 0; i < len(prerequisites); i++ {
-        a, b := prerequisites[i][0], prerequisites[i][1]
+    for i := 0; i < len(pre); i++ {
+        a, b := pre[i][0], pre[i][1]
+        // a depends on b
+        // b is independent / parent
+        // { independent -> [dependents...] }
+        indegrees[a]++
         adjList[b] = append(adjList[b], a)
     }
-    taken := 0
-    visited := make([]bool, numCourses)
-    var dfs func(node int, path []bool) bool
-    dfs = func(node int, path []bool) bool {
-        // base
-        if path[node] {return false}
-        if visited[node] {return true}
-
-        // logic
-        visited[node] = true
-        path[node] = true
-        taken++
-        for _, nei := range adjList[node] {
-            if !dfs(nei, path) {return false}
+    q := []int{}
+    for i := 0; i < len(indegrees); i++ {
+        if indegrees[i] == 0 {q = append(q, i)}
+    }
+    completed := 0
+    for len(q) != 0 {
+        dq := q[0]
+        q = q[1:]
+        completed++
+        for _, nei := range adjList[dq] {
+            indegrees[nei]--
+            if indegrees[nei] == 0 {q = append(q, nei)}
         }
-        path[node] = false
-        return true
     }
-    p := make([]bool, numCourses)
-    for i := 0; i < numCourses; i++ {
-        if !dfs(i, p) {return false}
-    }
-    return taken == numCourses
-
+    return completed == numCourses
 }
-// func canFinish(numCourses int, prerequisites [][]int) bool {
-//     adjList := map[int][]int{}
-//     indegrees := make([]int, numCourses)
-//     for i := 0; i < len(prerequisites); i++ {
-//         a, b := prerequisites[i][0], prerequisites[i][1]
-//         adjList[b] = append(adjList[b], a)
-//         indegrees[a]++
-//     }
-//     q := []int{}
-//     for i := 0; i < len(indegrees); i++ {
-//         if indegrees[i] == 0 {q = append(q, i)}
-//     }
-//     if len(q) == 0 {return false}
-//     taken := 0
-//     for len(q) != 0 {
-//         qSize := len(q)
-//         for qSize != 0 {
-//             dq := q[0]
-//             q = q[1:]
-//             taken++
-//             for _, nei := range adjList[dq] {
-//                 indegrees[nei]--
-//                 if indegrees[nei] == 0 {q = append(q, nei)}
-//             }
-//             qSize--
-//         }
-//     }
-//     return numCourses == taken
-// }
