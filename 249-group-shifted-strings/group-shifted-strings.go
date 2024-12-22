6f7bf719d805ec52f90fb3cb962407b50d58a023
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-21 21:01:08 -0800

    Time: 1 ms (22.22%) | Memory: 4.9 MB (38.89%) - LeetSync

---
+++ 249-group-shifted-strings/group-shifted-strings.go
@@ -0,0 +1,31 @@
+func groupStrings(strings []string) [][]string {
+    groups := map[string][]string{}
+    for i := 0; i < len(strings); i++ {
+        key := hash(strings[i])
+        groups[key] = append(groups[key], strings[i])
+    }
+    out := [][]string{}
+    for _, v := range groups {
+        out = append(out, v)
+    }
+    return out
+}
+
+
+func hash(word string) string {
+    if len(word) <= 1 {return "0"}
+    out := new(strings.Builder)
+    for i := 1; i < len(word); i++ {
+        curr := int(word[i]-'a')
+        prev := int(word[i-1]-'a')
+        diff := curr-prev
+        if diff < 0 {diff += 26}
+        out.WriteString(fmt.Sprintf("%v-", diff))
+    }
+    return out.String()
+} 
+
+func abs(x int) int {
+    if x < 0 {return x*-1}
+    return x
+}
