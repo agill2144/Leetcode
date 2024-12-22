9182916f719a827d2c9fd58639314a530d38b0a0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-21 16:12:58 -0800

    Time: 79 ms (50.00%) | Memory: 9.7 MB (30.56%) - LeetSync

---
+++ 282-expression-add-operators/expression-add-operators.go
@@ -0,0 +1,29 @@
+func addOperators(num string, target int) []string {
+    out := []string{}
+    var dfs func(start int, res int, prevContr int, exp string)
+    dfs = func(start int, res int, prevContr int, exp string) {
+        // base
+        if start == len(num) {
+            if res == target {
+                out = append(out, exp)
+            }
+            return
+        }
+
+        // logic
+        for i := start; i < len(num); i++ {
+            numStr := num[start:i+1]
+            if len(numStr) > 1 && numStr[0] == '0' {continue}
+            n, _ := strconv.Atoi(numStr)
+            if len(exp) == 0 {
+                dfs(i+1, n, n, numStr)
+            } else {
+                dfs(i+1, res+n, n, exp+"+"+numStr)
+                dfs(i+1, res-n, -n, exp+"-"+numStr)
+                dfs(i+1, (res-prevContr)+(prevContr*n), prevContr*n, exp+"*"+numStr)
+            }
+        }
+    }
+    dfs(0,0,0,"")
+    return out
+}
