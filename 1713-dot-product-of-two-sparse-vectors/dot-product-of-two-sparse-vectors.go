c743e0b8592373fbe86755cf0a4476723eba57a0
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-22 13:32:44 -0800

    Time: 148 ms (92.70%) | Memory: 9.9 MB (82.48%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.go
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.go
@@ -1,57 +1,29 @@
 type SparseVector struct {
-    nums [][]int // [ [val, idx] ]
+    idxs map[int]int
 }
 
 func Constructor(nums []int) SparseVector {
-    vals := [][]int{}
+    idxs := map[int]int{}
     for i := 0; i < len(nums); i++ {
-        if nums[i] != 0 {vals = append(vals, []int{nums[i], i})}
+        if nums[i] == 0 {continue}
+        idxs[i] = nums[i]
     }
-    return SparseVector{vals}
+    return SparseVector{idxs}
 }
 
 // Return the dotProduct of two sparse vectors
 func (this *SparseVector) dotProduct(vec SparseVector) int {
-    sum := 0
-    v1, v2 := 0, 0
-
-    for v1 < len(this.nums) && v2 < len(vec.nums) {
-        idx1, idx2 := this.nums[v1][1], vec.nums[v2][1]
-
-        if idx1 == idx2 {
-            sum += this.nums[v1][0] * vec.nums[v2][0]
-            v1++
-            v2++
-        } else if idx1 < idx2 {
-            // Move v1 using binary search
-            v1 = rightMostOnLeftSide(this.nums, v1, idx2)
-            if this.nums[v1][1] != idx2 {v1++; v2++}
-        } else {
-            // Move v2 using binary search
-            v2 = rightMostOnLeftSide(vec.nums, v2, idx1)
-            if vec.nums[v2][1] != idx1 {v1++;v2++}
+    if len(vec.idxs) < len(this.idxs) {
+        return vec.dotProduct(*this)
+    }
+    total := 0
+    for key, val := range this.idxs {
+        val2, ok := vec.idxs[key]
+        if ok {
+            total += (val * val2)
         }
     }
-
-    return sum
-}
-
-func rightMostOnLeftSide(nums [][]int, left int, target int) int {
-    right := len(nums) - 1
-    ans := left
-
-    for left <= right {
-        mid := left + (right-left)/2
-        if nums[mid][1] <=  target {
-            if nums[mid][1] == target {return mid}
-            ans = mid
-            left = mid+1
-        } else {
-            right = mid-1
-        }
-    }
-
-    return ans
+    return total
 }
 
 /**
