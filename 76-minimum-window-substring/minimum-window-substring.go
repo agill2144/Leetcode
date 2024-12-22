72d8cac7c655d50311c8663e3a57185226d30948
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-21 20:40:41 -0800

    Time: 25 ms (47.08%) | Memory: 5 MB (80.42%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.go
+++ 76-minimum-window-substring/minimum-window-substring.go
@@ -1,25 +1,24 @@
 func minWindow(s string, t string) string {
     freq := map[byte]int{}
     for i := 0; i < len(t); i++ {freq[t[i]]++}
-    fullMatch := 0
     left := 0
     res := ""
+    fullMatch := 0
     for i := 0; i < len(s); i++ {
-        if _, ok := freq[s[i]]; ok {
+        _, ok := freq[s[i]]
+        if ok {
             freq[s[i]]--
             if freq[s[i]] == 0 {fullMatch++}
-            for left <= i && fullMatch == len(freq) {
-                if res == "" || i-left+1 < len(res) {
-                    res = s[left:i+1]
-                }
-                if _, ok := freq[s[left]]; ok {
-                    freq[s[left]]++
-                    if freq[s[left]] == 1 {
-                        fullMatch--
-                    }
-                }
-                left++
+        }
+        for fullMatch == len(freq) {
+            subStr := s[left:i+1]
+            if res == "" || len(subStr) < len(res) {
+                res = subStr
             }
+            _, ok = freq[s[left]]
+            if ok {freq[s[left]]++}
+            if freq[s[left]] == 1 {fullMatch--}
+            left++
         }
     }
     return res
