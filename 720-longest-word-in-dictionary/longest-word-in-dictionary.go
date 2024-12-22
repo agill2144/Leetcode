9436e53ece89661279a47bb267a44a1e9bf937c4
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-21 18:26:04 -0800

    Time: 1 ms (93.48%) | Memory: 9.4 MB (10.87%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.go
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.go
@@ -1,102 +1,46 @@
 func longestWord(words []string) string {
-    root := &trieNode{childs: [26]*trieNode{}}
+    root := newTrieNode()
     for i := 0; i < len(words); i++ {
-        word := words[i]
-        root.insert(word)
+        root.insert(words[i])
     }
-    var dfs func(curr *trieNode) string
-    dfs = func(curr *trieNode) string {
+    res := ""
+    var dfs func(curr *trieNode, path string)
+    dfs = func(curr *trieNode, path string) {
         // base
+        if curr == nil {return}
 
         // logic
-        longest := ""
-        if curr.isEnd {
-            // trie node has each word saved
-            longest = curr.word
-        } 
-
+        if len(path) > len(res) {res = path}
         for i := 0; i < len(curr.childs); i++ {
-            if curr.childs[i] != nil && curr.childs[i].isEnd {
-                valueFromDFS := dfs(curr.childs[i])
-                if len(valueFromDFS) > len(longest) {
-                    longest = valueFromDFS
-                }
+            if curr.childs[i] != nil && 
+                curr.childs[i].isEnd {
+                    path += string(i+'a')
+                    dfs(curr.childs[i], path)
+                    path = path[:len(path)-1]
             }
         }
-        return longest
     }
-
-    return dfs(root)
+    dfs(root, "")
+    return res
 }
 
 
 type trieNode struct {
-    isEnd bool
     childs [26]*trieNode
-    word string
+    isEnd bool
 }
 
-func (t *trieNode) insert(word string) {
-    curr := t
+func newTrieNode() *trieNode{
+    return &trieNode{childs:[26]*trieNode{}}
+}
+
+func (r *trieNode) insert(word string)  {
+    curr := r
     for i := 0; i < len(word); i++ {
-        char := word[i]
-        idx := int(char-'a')
-        if curr.childs[idx] == nil {
-            curr.childs[idx] = &trieNode{childs: [26]*trieNode{}}
-        }
+        idx := int(word[i] - 'a')
+        if curr.childs[idx] == nil {curr.childs[idx] = newTrieNode()}
         curr = curr.childs[idx]
     }
     curr.isEnd = true
-    curr.word = word
 }
-
-// without saving word at each trie node
-// func longestWord(words []string) string {
-//     root := &trieNode{childs: [26]*trieNode{}}
-//     for i := 0; i < len(words); i++ {
-//         word := words[i]
-//         root.insert(word)
-//     }
-//     out := ""
-//     var dfs func(curr *trieNode, path []string)
-//     dfs = func(curr *trieNode, path []string) {
-//         // base
-//         pathStr := strings.Join(path, "")
-//         if path != nil && len(pathStr) > len(out) {
-//             out = pathStr
-//         }
-
-//         // logic
-//         for i := 0; i < len(curr.childs); i++ {
-//             if curr.childs[i] != nil && curr.childs[i].isEnd {
-//                 path = append(path, string(i+'a'))
-//                 dfs(curr.childs[i], path)
-//                 path = path[:len(path)-1]
-//             }
-//         }
-//     }
-
-//     dfs(root, nil)
-//     return out
-
-// }
-
-
-// type trieNode struct {
-//     isEnd bool
-//     childs [26]*trieNode
-// }
-
-// func (t *trieNode) insert(word string) {
-//     curr := t
-//     for i := 0; i < len(word); i++ {
-//         char := word[i]
-//         idx := int(char-'a')
-//         if curr.childs[idx] == nil {
-//             curr.childs[idx] = &trieNode{childs: [26]*trieNode{}}
-//         }
-//         curr = curr.childs[idx]
-//     }
-//     curr.isEnd = true
-// }
 
