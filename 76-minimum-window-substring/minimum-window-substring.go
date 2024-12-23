ad2fe0532c5c144f7baf8a80a1de5d76b2bfbadc
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-22 21:10:33 -0800

    Time: 26 ms (44.68%) | Memory: 5.1 MB (58.77%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.go
+++ 76-minimum-window-substring/minimum-window-substring.go
@@ -1,32 +1,22 @@
 func minWindow(s string, t string) string {
-    type pair struct {
-        char byte
-        idx int
-    }
-    filtered := []pair{}    
     freq := map[byte]int{}
     for i := 0; i < len(t); i++ {freq[t[i]]++}
-    for i := 0; i < len(s); i++ {
-        if _, ok := freq[s[i]]; ok {
-            filtered = append(filtered, pair{s[i], i})
-        }
-    }
-    fullMatch := 0
     left := 0
     start, end := -1,-1
-    for i := 0; i < len(filtered); i++ {
-        char := filtered[i].char
-        idx := filtered[i].idx
-        freq[char]--
-        if freq[char] == 0 {fullMatch++}
+    fullMatch := 0
+    for i := 0; i < len(s); i++ {
+        _, ok := freq[s[i]]
+        if ok {
+            freq[s[i]]--
+            if freq[s[i]] == 0 {fullMatch++}
+        }
         for fullMatch == len(freq) {
-            leftChar := filtered[left].char
-            leftIdx := filtered[left].idx
-            if start == -1 || idx-leftIdx+1 < end-start+1 {
-                start, end = leftIdx, idx
+            if start == -1 || i-left+1 < end-start+1 {
+                start = left; end = i
             }
-            freq[leftChar]++
-            if freq[leftChar] == 1 {fullMatch--}
+            _, ok = freq[s[left]]
+            if ok {freq[s[left]]++}
+            if freq[s[left]] == 1 {fullMatch--}
             left++
         }
     }
@@ -34,28 +24,34 @@ func minWindow(s string, t string) string {
     return s[start:end+1]
 }
 
+// brute force: o(t) + o(s^2 * t)
+// sc - o(1)
 // func minWindow(s string, t string) string {
-//     freq := map[byte]int{}
-//     for i := 0; i < len(t); i++ {freq[t[i]]++}
-//     left := 0
-//     res := ""
-//     fullMatch := 0
+//     tFreq := map[byte]int{}
+//     for i := 0; i < len(t); i++ {tFreq[t[i]]++}
+//     start, end := -1,-1
 //     for i := 0; i < len(s); i++ {
-//         _, ok := freq[s[i]]
-//         if ok {
-//             freq[s[i]]--
-//             if freq[s[i]] == 0 {fullMatch++}
-//         }
-//         for fullMatch == len(freq) {
-//             subStr := s[left:i+1]
-//             if res == "" || len(subStr) < len(res) {
-//                 res = subStr
+//         if tFreq[s[i]] > 0 {
+//             freq := map[byte]int{}
+//             for k, v := range tFreq {freq[k] = v}
+//             fullMatch := 0
+//             ss, ee := -1,-1
+//             for j := i; j < len(s); j++ {
+//                 _, ok := freq[s[j]]
+//                 if ok {
+//                     freq[s[j]]--
+//                     if freq[s[j]] == 0 {
+//                         fullMatch++
+//                     }
+//                 }
+//                 if fullMatch == len(freq) {
+//                     ss, ee = i, j
+//                     break
+//                 }
 //             }
-//             _, ok = freq[s[left]]
-//             if ok {freq[s[left]]++}
-//             if freq[s[left]] == 1 {fullMatch--}
-//             left++
+//             if ss != -1 && (start == -1 || ee-ss+1 < end-start+1) {start, end = ss, ee}
 //         }
 //     }
-//     return res
+//     if start == -1 {return ""}
+//     return s[start:end+1]
 // }
