8d3eb1c672ee4ef9899acca1d6edcb9d2f954188
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-22 17:28:13 -0800

    Time: 0 ms (100.00%) | Memory: 4.5 MB (87.38%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
@@ -1,14 +1,32 @@
 func intersect(nums1 []int, nums2 []int) []int {
-    freq := map[int]int{}
-    for i := 0; i < len(nums1); i++ {
-        freq[nums1[i]]++
-    }
+    if len(nums2) < len(nums1) {return intersect(nums2, nums1)}
+    sort.Ints(nums1)
+    sort.Ints(nums2)
+    // loop over smaller array and binary search on larger array
+    // nums1 is always smaller
     out := []int{}
-    for i := 0; i < len(nums2); i++ {
-        if freq[nums2[i]] > 0 {
-            out = append(out, nums2[i])
-            freq[nums2[i]]--
-        }
+    left := 0
+    for i := 0; i < len(nums1); i++ {
+        idx := search(nums2, left, nums1[i])
+        if idx != -1 {
+            out = append(out, nums1[i])
+            left = idx+1
+        }   
     }
     return out
+}
+
+func search(nums []int, left, target int) int {
+    ans := -1
+    right := len(nums)-1
+    for left <= right {
+        mid := left + (right-left)/2
+        if nums[mid] == target {
+            ans = mid
+            right = mid-1
+            continue
+        }
+        if target > nums[mid] {left=mid+1} else {right=mid-1}
+    }
+    return ans
 }
