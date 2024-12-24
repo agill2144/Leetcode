89ed147bc0bd197567cd53ed2bf4bab1a1d43b48
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-24 14:34:40 -0800

    Time: 0 ms (100.00%) | Memory: 8.1 MB (48.96%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.go
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -1,23 +1,50 @@
 func trap(height []int) int {
     n := len(height)
-    maxL := make([]int, n)
-    maxR := make([]int, n)
-    for i := 0; i < n; i++ {
-        if i != 0 {
-            maxL[i] = max(maxL[i-1], height[i-1])
-        }
-        idx := n-1-i
-        if idx != n-1 {
-            maxR[idx] = max(maxR[idx+1], height[idx+1])
-        }
-    }
+    lh := 0
+    rh := n-1
+    l := 0
+    r := n-1
     total := 0
-    for i := 0; i < n; i++ {
-        curr := height[i]
-        trap := min(maxL[i], maxR[i])-curr
-        if trap > 0 {
-            total += trap
+    for l < r {
+        if height[rh] >= height[lh] {
+            // process left side
+            if height[l] <= height[lh] {
+                total += (height[lh]-height[l])
+                l++
+            } else {
+                lh = l
+            }
+        } else {
+            if height[r] <= height[rh] {
+                total += (height[rh]-height[r])
+                r--
+            } else {
+                rh = r
+            }
         }
     }
     return total
 }
+// func trap(height []int) int {
+//     n := len(height)
+//     maxL := make([]int, n)
+//     maxR := make([]int, n)
+//     for i := 0; i < n; i++ {
+//         if i != 0 {
+//             maxL[i] = max(maxL[i-1], height[i-1])
+//         }
+//         idx := n-1-i
+//         if idx != n-1 {
+//             maxR[idx] = max(maxR[idx+1], height[idx+1])
+//         }
+//     }
+//     total := 0
+//     for i := 0; i < n; i++ {
+//         curr := height[i]
+//         trap := min(maxL[i], maxR[i])-curr
+//         if trap > 0 {
+//             total += trap
+//         }
+//     }
+//     return total
+// }
