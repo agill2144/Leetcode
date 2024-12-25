2b78595b05f08f9a5c1155a1cc1ca7e61a74a91f
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-24 16:05:00 -0800

    Time: 0 ms (100.00%) | Memory: 4.6 MB (78.25%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
@@ -1,15 +1,33 @@
 func intersect(nums1 []int, nums2 []int) []int {
-    if len(nums2) < len(nums2) {return intersect(nums2, nums1)} 
-    freq := map[int]int{}
-    for i := 0; i < len(nums1); i++ {
-        freq[nums1[i]]++
-    }
+    sort.Ints(nums1)
+    sort.Ints(nums2)
     out := []int{}
-    for i := 0; i < len(nums2); i++ {
-        if freq[nums2[i]] > 0 {
-            out = append(out, nums2[i])
-            freq[nums2[i]]--
+    n1 := 0; n2 := 0
+    for n1 < len(nums1) && n2 < len(nums2) {
+        if nums1[n1] == nums2[n2] {
+            out = append(out, nums1[n1])
+            n1++; n2++
+        } else if nums1[n1] < nums2[n2] {
+            n1++
+        } else {
+            n2++
         }
     }
     return out
 }
+// approach : put smaller array into freq map ( because we have to loop over both array regardless! )
+// func intersect(nums1 []int, nums2 []int) []int {
+//     if len(nums2) < len(nums2) {return intersect(nums2, nums1)} 
+//     freq := map[int]int{}
+//     for i := 0; i < len(nums1); i++ {
+//         freq[nums1[i]]++
+//     }
+//     out := []int{}
+//     for i := 0; i < len(nums2); i++ {
+//         if freq[nums2[i]] > 0 {
+//             out = append(out, nums2[i])
+//             freq[nums2[i]]--
+//         }
+//     }
+//     return out
+// }
