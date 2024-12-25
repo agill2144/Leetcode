aabcf36f0093246ec4ec73fbffb8f5dd39db8fb1
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-24 17:39:17 -0800

    Time: 19 ms (55.17%) | Memory: 10.2 MB (75.86%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.go
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.go
@@ -1,25 +1,27 @@
 func maximumCandies(candies []int, k int64) int {
-    total := 0
     left := 1
-    right := math.MinInt64
+    right := 0
+    total := 0
     for i := 0; i < len(candies); i++ {
-        right = max(right, candies[i])
         total += candies[i]
+        right = max(right, candies[i])
     }
-    if int64(total) < k {return 0}
-
+    if k > int64(total) {return 0}
     ans := 0
     for left <= right {
         mid := left + (right-left)/2
         count := 0
         for i := 0; i < len(candies); i++ {
-            count += int(math.Floor(float64(candies[i]) / float64(mid)))
+            count += (candies[i]/mid)
         }
+        // when does mid not work?
+        // when num of subpiles of equal sizes are < k
+        // if we reduce the pile size, we can more piles
         if int64(count) < k {
             right = mid-1
         } else {
             ans = mid
-            left = mid+1
+            left = mid+1 // we want max such ans
         }
     }
     return ans
