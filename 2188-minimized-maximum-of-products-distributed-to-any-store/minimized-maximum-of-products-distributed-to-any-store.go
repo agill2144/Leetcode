846eae6ee5717d50351486b7e14fe95ea3e2150b
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-24 16:28:19 -0800

    Time: 11 ms (95.42%) | Memory: 10.2 MB (42.46%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.go
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.go
@@ -1,19 +1,21 @@
 func minimizedMaximum(n int, quantities []int) int {
-    m := len(quantities)
-    if n < m {return -1}
     left := 1
     right := -1
-    for i := 0; i < m; i++ {right = max(right, quantities[i])}
+    for i := 0; i < len(quantities); i++ {
+        right= max(right, quantities[i])
+    }
     ans := -1
     for left <= right {
-        mid := left + (right-left)/2
-        count := 0
-        for i := 0; i < m; i++ {
-            count += int(math.Ceil( float64(quantities[i])/float64(mid)))
+        mid := left +(right-left)/2
+        countPerStore := mid
+        storesNeeded := 0
+        for j := 0; j < len(quantities); j++ {
+            storesNeeded += int(math.Ceil(float64(quantities[j])/float64(countPerStore)))
         }
         // when does mid not work?
-        // when count num of stores needed is more than num of stores we have
-        if count > n {
+        // when we need more stores than we have (n)
+        if storesNeeded > n {
+            // increase count per product type
             left = mid+1
         } else {
             ans = mid
