8145a9d9b9b2deded532a12cad2cadbb431dc685
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-25 09:04:00 -0800

    Time: 13 ms (97.38%) | Memory: 10.8 MB (70.25%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.go
@@ -0,0 +1,19 @@
+func minimumSize(nums []int, maxOperations int) int {
+    left := 1
+    right := slices.Max(nums)
+    ans := -1
+    for left <= right {
+        mid := left + (right-left)/2
+        ops := 0
+        for i := 0; i < len(nums); i++ {
+            ops += int(math.Ceil(float64(nums[i])/float64(mid)))-1
+        }
+        if ops > maxOperations {
+            left = mid+1
+        } else {
+            ans = mid
+            right = mid-1
+        }
+    }
+    return ans
+}
