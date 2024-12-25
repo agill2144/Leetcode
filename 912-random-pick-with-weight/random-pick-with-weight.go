0d6d2e57773161703481dc95a8aaad8e31500876
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-24 21:53:03 -0800

    Time: 10 ms (93.27%) | Memory: 9.8 MB (61.54%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.go
+++ 912-random-pick-with-weight/random-pick-with-weight.go
@@ -12,7 +12,13 @@ func Constructor(w []int) Solution {
     return Solution{compressedRanges}
 }
 
-
+// remember that values in compressed range indicate end idx of a range
+// if compressedRanges[1] =  4
+// it means, it covers until idx 4 from larger/expanded array
+// but excluding idx 4
+// therefore if r is < 4 (ex: 3), then yes this is potential ans, save it
+// compressedRanges[1] = 2 ; ( meaning i cover up to idx 2 but not inculding 2)
+// and if r = 3 or if r = 2; we are not in the range, therefore look right
 func (this *Solution) PickIndex() int {
     n := len(this.compressedRanges)
     total := this.compressedRanges[n-1]
