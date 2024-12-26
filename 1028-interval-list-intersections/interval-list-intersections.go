5c33249c6c74cdedf7892f8ddf76b697f5cd2f60
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-25 20:34:28 -0800

    Time: 0 ms (100.00%) | Memory: 8.9 MB (7.41%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.go
+++ 1028-interval-list-intersections/interval-list-intersections.go
@@ -4,14 +4,11 @@ func intervalIntersection(firstList [][]int, secondList [][]int) [][]int {
     for f < len(firstList) && s < len(secondList) {
         aStart, aEnd := firstList[f][0], firstList[f][1]
         bStart, bEnd := secondList[s][0], secondList[s][1]
-        
-        // there is no overlap when;
-        if aStart > bEnd || bStart > aEnd {
-            if aEnd < bEnd {f++} else {s++}
-        } else {
-            out = append(out, []int{max(aStart, bStart), min(aEnd, bEnd)})
-            if aEnd < bEnd {f++} else {s++}
+        start, end := max(aStart, bStart), min(aEnd, bEnd)
+        if start <= end {
+            out = append(out, []int{start, end})
         }
+        if aEnd < bEnd {f++} else {s++}
     }
     return out
 }
