26d67d386a6cb158ddc81d1f71a6328cca4f9b07
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-25 21:50:18 -0800

    Time: 7 ms (21.52%) | Memory: 10.5 MB (37.75%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.go
+++ 238-product-of-array-except-self/product-of-array-except-self.go
@@ -1,18 +1,32 @@
+/*
+    approach: brute force
+    - nested loop
+    tc = o(n^2)
+    sc = o(1)
+    
+    approach: prefix / suffix array
+    - get a prefix prod for each ith position ( excluding that position )
+    - get a suffix prod for each ith position ( excluding that position )
+    - now for each ith position prodExceptItself is prefix[i] * suffix[i]
+    tc = o(3n) = o(n)
+    sc = o(2n) = o(n)
+*/
 func productExceptSelf(nums []int) []int {
     n := len(nums)
-    left := make([]int, n)
-    left[0] = 1
+    prefix := make([]int, n)
+    prefix[0] = 1
     for i := 1; i < n; i++ {
-        leftRp := left[i-1]
-        prev := nums[i-1]
-        left[i] = prev * leftRp
+        prefix[i] = nums[i-1]*prefix[i-1]
     }
-    rrp := 1
+    suffix := make([]int, n)
+    suffix[n-1] = 1
     for i := n-2; i >= 0; i-- {
-        prev := nums[i+1]
-        rrp *= prev
-        left[i] *= rrp
+        suffix[i] = suffix[i+1] * nums[i+1]
     }
-    
-    return left
+
+    out := make([]int, n)
+    for i := 0; i < n; i++ {
+        out[i] = prefix[i] * suffix[i]
+    }
+    return out
 }
