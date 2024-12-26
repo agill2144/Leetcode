6a11593fd4c887a0836d9e45d4fc8caa6c11f942
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-25 19:53:34 -0800

    Time: 0 ms (100.00%) | Memory: 7.3 MB (48.82%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.go
+++ 229-majority-element-ii/majority-element-ii.go
@@ -3,7 +3,7 @@ func majorityElement(nums []int) []int {
     e1, e2 := math.MinInt64, math.MinInt64
     n := len(nums)
     for i := 0; i < n; i++ {
-        if c1 == 0 && e2 != nums[i] {
+        if c1 == 0 && nums[i] != e2 {
             c1 = 1
             e1 = nums[i]
         } else if c2 == 0 && nums[i] != e1 {
@@ -14,19 +14,17 @@ func majorityElement(nums []int) []int {
         } else if nums[i] == e2 {
             c2++
         } else {
-            c1--; c2--
+            c1--
+            c2--
         }
+    }
+    c1 = 0
+    c2 = 0
+    for i := 0; i < n; i++ {
+        if nums[i] == e1 {c1++}
+        if nums[i] == e2 {c2++}
     }
     out := []int{}
-    c1, c2 = 0, 0
-    for i := 0; i < n; i++ {
-        if nums[i] == e1 {
-            c1++
-        }
-        if nums[i] == e2 {
-            c2++
-        }
-    }
     if c1 > n/3 {out = append(out, e1)}
     if c2 > n/3 {out = append(out, e2)}
     return out
