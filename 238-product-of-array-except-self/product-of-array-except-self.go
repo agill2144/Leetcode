d87b89170803b14f6152153f06fc1e7c52b4c96d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-25 21:57:12 -0800

    Time: 1 ms (78.48%) | Memory: 9.7 MB (78.65%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.go
+++ 238-product-of-array-except-self/product-of-array-except-self.go
@@ -10,6 +10,22 @@
     - now for each ith position prodExceptItself is prefix[i] * suffix[i]
     tc = o(3n) = o(n)
     sc = o(2n) = o(n)
+
+    approach: prefix / suffix array
+    - instead of allocating both prefix and suffix array
+    - once we have prefix, we can re-use prefix array as suffix array
+    - now each ith element in the prefix array is the prod of i except itself
+    - we also wont need a separate output array
+    - HOWEVER! we cannot store suffix product right away in prefix array
+    - because each prefix[i] would now have the prod of prefix[i-1] * prefix[i+1]
+    - and now when i moves back 1 more position while processing suffix, the prefix[i+1] is NOT the suffix prod
+        - it is not the right running product
+        - prefix[i+1] is prod of i+1 except itself
+        - basically a pollution
+    - hence we need to split and maintain a suffix prod separately in a running suffix prod array
+        - call it "rrp" = right running prod    
+    tc = o(2n) = o(n)
+    sc = o(n)
 */
 func productExceptSelf(nums []int) []int {
     n := len(nums)
@@ -18,15 +34,10 @@ func productExceptSelf(nums []int) []int {
     for i := 1; i < n; i++ {
         prefix[i] = nums[i-1]*prefix[i-1]
     }
-    suffix := make([]int, n)
-    suffix[n-1] = 1
+    rrp := 1
     for i := n-2; i >= 0; i-- {
-        suffix[i] = suffix[i+1] * nums[i+1]
+        rrp *= nums[i+1]
+        prefix[i] *= rrp
     }
-
-    out := make([]int, n)
-    for i := 0; i < n; i++ {
-        out[i] = prefix[i] * suffix[i]
-    }
-    return out
+    return prefix
 }
