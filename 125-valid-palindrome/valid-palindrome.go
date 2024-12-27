2b30b7817dd0b3a3a2d78cec5cb29bbbddc6f71e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-26 18:22:41 -0800

    Time: 1 ms (53.28%) | Memory: 4.6 MB (68.99%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.go
+++ 125-valid-palindrome/valid-palindrome.go
@@ -2,11 +2,18 @@ func isPalindrome(s string) bool {
     left := 0
     right := len(s)-1
     for left < right {
-        for left < right && !isAlphaNumeric(s[left]) {left++}
-        if left == right {break}
-        for left < right && !isAlphaNumeric(s[right]) {right--}
-        if left == right {break}
-        if strings.ToLower(string(s[left])) != strings.ToLower(string(s[right])) {return false}
+        for left < right && !isAlphaNumeric(s[left]) {
+            left++
+        }
+        for left < right && !isAlphaNumeric(s[right]) {
+            right--
+        }
+        if left >= right {
+            return true
+        }
+        if strings.ToLower(string(s[left])) != strings.ToLower(string(s[right])) {
+            return false
+        }
         left++
         right--
     }
@@ -14,9 +21,10 @@ func isPalindrome(s string) bool {
 }
 
 func isAlphaNumeric(char byte) bool {
-    return (char >= '0' && char <= '9') || 
-            (char >= 'a' && char <= 'z') || 
-            (char >= 'A' && char <= 'Z')
+    return (char >= 'a' && char <= 'z') ||
+        (char >= 'A' && char <= 'Z') || 
+        (char >= '0' && char <= '9')
 }
-
-
+//                         R
+// A man, a plan, a canal: Panama
+//           L
