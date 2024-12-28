2269aa8041573db465c19295c6c2cbb12f3fa64e
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-28 14:08:19 -0800

    Time: 0 ms (100.00%) | Memory: 8.9 MB (33.02%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.go
+++ 498-diagonal-traverse/diagonal-traverse.go
@@ -1,32 +1,33 @@
-func findDiagonalOrder(mat [][]int) []int {
-    m := len(mat)
-    n := len(mat[0])
-    out := []int{}
-    r, c := 0, 0
+func findDiagonalOrder(matrix [][]int) []int {
+    m := len(matrix)
+    n := len(matrix[0])
+    r := 0
+    c := 0
     up := true
-    for len(out) != m*n {
-        out = append(out, mat[r][c])
+    out := []int{}
+    for len(out) < m*n {
+        out = append(out, matrix[r][c])
         if up {
-            // when going up, we could hit
-            // top row edge, or right col edge
-            if r == 0 || c == n-1 {
-                // continue in next col if we are not yet at n-1 col
-                // when we are at n-1 col, start from next row, c++ will go out of bounds
-                if c != n-1 { c++ } else { r++ }
+            if  c == n-1 {
+                r++
+                up = false
+            } else if r == 0 {
+                c++
                 up = false
             } else {
-                // not hitting edges yet
-                // keep going up in this dir
-                r--; c++
+                r--
+                c++
             }
         } else {
-            if c == 0 || r == m-1 {
-                if r != m-1 { r++ } else { c++ }
+            if r == m-1  {
+                c++
+                up = true
+            } else if c == 0 {
+                r++
                 up = true
             } else {
-                // not hitting edges yet
-                // keep going up in this dir
-                r++; c--
+                r++
+                c--
             }
         }
     }
