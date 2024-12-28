5b7f40061bb10c90fb6be52ac02a97d53f0f4795
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-28 12:05:30 -0800

    Time: 1 ms (64.66%) | Memory: 4 MB (76.82%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.go
+++ 1894-merge-strings-alternately/merge-strings-alternately.go
@@ -1,28 +1,20 @@
 func mergeAlternately(word1 string, word2 string) string {
     out := new(strings.Builder)
-    m := len(word1)
-    n := len(word2)
-    for i := 0; i < max(m, n); i++ {
-        if i < m {out.WriteByte(word1[i])}
-        if i < n {out.WriteByte(word2[i])}
+    w1, w2 := 0,0 
+    for w1 < len(word1) && w2 < len(word2) {
+        out.WriteByte(word1[w1])
+        w1++
+        out.WriteByte(word2[w2])
+        w2++
     }
+    for w1 < len(word1) {
+        out.WriteByte(word1[w1])
+        w1++
+    }
+    for w2 < len(word2) {
+        out.WriteByte(word2[w2])
+        w2++
+    }
+
     return out.String()
 }
-
-// func mergeAlternately(word1 string, word2 string) string {
-//     p1, p2 := 0, 0
-//     out := new(strings.Builder)
-//     for p1 < len(word1) || p2 < len(word2) {
-//         p1Val := ""
-//         p2Val := ""
-//         if p1 < len(word1) {p1Val = string(word1[p1]); p1++}
-//         if p2 < len(word2) {p2Val = string(word2[p2]); p2++}
-//         if p1Val != "" {
-//             out.WriteString(p1Val)
-//         }
-//         if p2Val != "" {
-//             out.WriteString(p2Val)
-//         }
-//     }
-//     return out.String()
-// }
