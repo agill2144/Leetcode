54caf42c959b1ba2d6171027c1a9f23100bbf03d
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-28 12:08:43 -0800

    Time: 0 ms (100.00%) | Memory: 4.5 MB (47.10%) - LeetSync

--- 90-subsets-ii/subsets-ii.go
+++ 90-subsets-ii/subsets-ii.go
@@ -4,20 +4,17 @@ func subsetsWithDup(nums []int) [][]int {
     var dfs func(start int, path []int)
     dfs = func(start int, path []int) {
         // base
+        
 
         // logic
         newL := make([]int, len(path))
         copy(newL, path)
         out = append(out, newL)
         for i := start; i < len(nums); i++ {
-            // action
             path = append(path, nums[i])
-            // recurse
             dfs(i+1, path)
-            // backtrack
             path = path[:len(path)-1]
-            skipElement := nums[i]
-            for i+1 < len(nums) && nums[i+1] == skipElement {i++}
+            for i+1 < len(nums) && nums[i+1] == nums[i] {i++}
         }
     }
     dfs(0, nil)
