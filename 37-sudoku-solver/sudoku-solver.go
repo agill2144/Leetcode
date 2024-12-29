19a531dba0070ff80b92398da0b9f739b2744114
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-28 21:25:57 -0800

    Time: 1336 ms (5.95%) | Memory: 8.7 MB (11.39%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.go
+++ 37-sudoku-solver/sudoku-solver.go
@@ -1,54 +1,82 @@
-// numOfChoicesPerNode^maxDepthOfRecursion * extraWorkAtEachRecursion
-// numOfChoicesPerNode = 9, but choices reduce by 1 each time we use a numer
-// maxDepthOfRecursion = 9
-// tc = (9!)^9 = constant = o(1)
-// sc = 9*9 = 81 = o(1)
 func solveSudoku(board [][]byte)  {
-    // using constant sized arrays is better instead of dynamic slices
-    rows := [10][10]bool{}
-    cols := [10][10]bool{}
-    box :=  [3][3][10]bool{}
     n := len(board)
+    rowSet := make([]*set, 9)
+    colSet := make([]*set, 9)
+    boxSet := map[string]*set{}
     for i := 0; i < n; i++ {
         for j := 0; j < n; j++ {
-            if board[i][j] != '.' {
-                idx := int(board[i][j]-'0')
-                rows[i][idx] = true
-                box[i/3][j/3][idx] = true
-            }
-            if board[j][i] != '.' {
-                idx := int(board[j][i]-'0')
-                cols[i][idx] = true
-            }
+            if rowSet[i] == nil {rowSet[i] = newSet()}
+            if colSet[i] == nil {colSet[i] = newSet()}
+            boxKey := fmt.Sprintf("%v-%v", i/3, j/3)
+            if boxSet[boxKey] == nil { boxSet[boxKey] = newSet() }
+            rowSet[i].add(board[i][j])
+            colSet[i].add(board[j][i])
+            boxSet[boxKey].add(board[i][j])
         }
     }
+    choices := []byte{'1','2','3','4','5','6','7','8','9'}
     var dfs func(r, c int) bool
-    dfs = func(r, c int) bool {
+    dfs = func(r, c int)bool {
         // base
-        if r == n {return true}
-        if c == n {return dfs(r+1, 0)}
-        if board[r][c] != '.' {return dfs(r,c+1)}
+        if r == n-1 && c == n {
+            return true
+        }
+        if c == n {
+            r++
+            c = 0
+        }
+
+        if board[r][c] != '.' {
+            return dfs(r, c+1)
+        }
+
 
         // logic
-        for i := 1; i <= 9; i++ {
-            inRow := rows[r][i]
-            inCol := cols[c][i]
-            inBox := box[r/3][c/3][i]
-            canUse := !inRow && !inCol && !inBox
-
-            if canUse {
-                rows[r][i] = true
-                cols[c][i] = true
-                box[r/3][c/3][i] = true
-                board[r][c] =  byte(i)+'0'
+        boxKey := fmt.Sprintf("%v-%v",r/3,c/3)
+        rowData := rowSet[r]
+        colData := colSet[c]
+        boxData := boxSet[boxKey]
+        for i := 0; i < len(choices); i++ {
+            choice := choices[i]
+            inRow := rowData.contains(choice)
+            inCol := colData.contains(choice)
+            inBox := boxData.contains(choice)
+            if !inRow && !inCol && !inBox {
+                board[r][c] = choice
+                rowData.add(choice)
+                colData.add(choice)
+                boxData.add(choice)
                 if dfs(r, c+1) {return true}
+                boxData.remove(choice)
+                colData.remove(choice)
+                rowData.remove(choice)
                 board[r][c] = '.'
-                box[r/3][c/3][i] = false
-                cols[c][i] = false
-                rows[r][i] = false
             }
         }
         return false
     }
-    dfs(0, 0)
+    dfs(0,0)
 }
+
+
+type set struct {
+    items map[byte]struct{}
+}
+
+func newSet() *set {
+    return &set{items: map[byte]struct{}{}}
+}
+
+func (s *set) add(key byte) {
+    s.items[key] = struct{}{}
+}
+
+func (s *set) remove(key byte) {
+    delete(s.items, key)
+}
+
+func (s *set) contains(key byte) bool {
+    _, ok := s.items[key]
+    return ok
+}
+
