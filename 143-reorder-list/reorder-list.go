74dc5ab3d7822b32222da33d3fbcb4d6368a603c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-29 14:20:26 -0800

    Time: 0 ms (100.00%) | Memory: 7.6 MB (84.89%) - LeetSync

--- 143-reorder-list/reorder-list.go
+++ 143-reorder-list/reorder-list.go
@@ -7,44 +7,41 @@
  */
 func reorderList(head *ListNode)  {
     if head == nil || head.Next == nil {return}
+    var prev *ListNode    
     slow := head
     fast := head
     for fast != nil && fast.Next != nil {
+        prev = slow
         slow = slow.Next
         fast = fast.Next.Next
     }
-    p1 := head
-    p2 := slow.Next
-    slow.Next = nil
-    p2 = reverse(p2)
-    // 1-2
-    // 5-4-3
-    var tail *ListNode
-    for p1 != nil && p2 != nil {
-        p1Next := p1.Next
-        p2Next := p2.Next
-        p1.Next = p2
-        p2.Next = p1Next
-        p1 = p1Next
-        p2 = p2Next
-    }
-    for p2 != nil {
-        tail.Next = p2
-        tail = tail.Next
-        p2 = p2.Next
-    }
-
-}
-
-func reverse(head *ListNode) *ListNode {
-    if head == nil || head.Next == nil {return head}
-    curr := head
-    var prev *ListNode
+    prev.Next = nil
+    head2 := slow
+    prev = nil
+    curr := head2
     for curr != nil {
         next := curr.Next
         curr.Next = prev
         prev = curr
         curr = next
     }
-    return prev
+    head2 = prev
+
+    dummy := &ListNode{Val:0}
+    tail := dummy
+    p1 := head
+    p2 := head2
+    for p1 != nil || p2 != nil {
+        if p1 != nil {
+            tail.Next = p1
+            tail = tail.Next
+            p1 = p1.Next
+        }
+        if p2 != nil {
+            tail.Next = p2
+            tail = tail.Next
+            p2 = p2.Next
+        }
+    }
+
 }
