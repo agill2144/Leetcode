29ee8b017d0bc3ccd3fcbee29de661ccdb17e30c
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-29 12:56:44 -0800

    Time: 3 ms (75.00%) | Memory: 9 MB (14.58%) - LeetSync

--- 382-linked-list-random-node/linked-list-random-node.go
+++ 382-linked-list-random-node/linked-list-random-node.go
@@ -5,65 +5,25 @@
  *     Next *ListNode
  * }
  */
-
-
-/*
-    approach: brute force
-    - save all node values in a list
-    - use rand.Intn to select a random idx from list
-        - each value has a equal probability of being selected    
-    - return the value at that idx position
-    tc = o(n)
-    sc = o(n)
-
-
-    Follow up:
-    Could you solve this efficiently without using extra space?
-    What if the linked list is extremely large and its length is unknown to you?
-    - reservoir sampling!
-
-    approach: reservoir sampling
-    - reservoir = bucket of some size
-        - we only care about 1 element
-        - therefore size is 1
-    - use counter to figure out whether current node 
-        could replace our reservoir element
-    - as counter goes up, probability of each element seen so far goes down
-    - most importantly probability of current element being selected goes down
-    - use rand.Intn(count) == 0 to check whether we can we can replace our reservoir elememt or not
-        - 0 is a constant number, it will always be part of rand.Intn(count)
-        - as rand.Intn(n) picks a number from [0, n)
-        - therefore if random number is 0, with the lower probability counter, we can replace our res with current element
-
-    tc = o(n)
-    sc = o(1)
-
-*/
-
-
 type Solution struct {
-    head *ListNode
+    items []int
 }
 
 
 func Constructor(head *ListNode) Solution {
-    
-    return Solution{head}    
+    curr := head
+    items := []int{}
+    for curr != nil {
+        items = append(items, curr.Val)
+        curr = curr.Next
+    }
+    return Solution{items}
 }
 
 
 func (this *Solution) GetRandom() int {
-    res := -1
-    count := 0
-    curr := this.head
-    for curr != nil {
-        count++
-        if rand.Intn(count) == count-1 {
-            res = curr.Val
-        }
-        curr = curr.Next
-    }
-    return res
+    r := rand.Intn(len(this.items))
+    return this.items[r]    
 }
 
 
