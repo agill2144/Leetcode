7324313339753e5e3a43274642414c0a0c7b59c3
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-28 20:07:05 -0800

    Time: 54 ms (97.44%) | Memory: 29.7 MB (16.67%) - LeetSync

--- 1249-snapshot-array/snapshot-array.go
+++ 1249-snapshot-array/snapshot-array.go
@@ -1,52 +1,37 @@
 type SnapshotArray struct {
-    items [][][]int
-    id int
+    snapId int
+    history [][][]int // [ [], [] ]
 }
 
 
 func Constructor(length int) SnapshotArray {
-    // its possible we take a bunch of snap() first before setting anything
-    // in that case, if we do not create all idx arrays right away
-    // those lists will be nil, but in this case , we want to return 0 ( default int value )
-    // technically, the correct return value should be -1 or something that indicates a value has not been stored yet
-    // 0 is a valid value and could be used as real value :shrug
-    // therefore pre-setting 0 values with id = 0
-    items := make([][][]int , length)
-    for i := 0; i < len(items); i++ {
-        items[i] = [][]int{{0,0}}
-    }
     return SnapshotArray{
-        items: items,
-        id: 0,
-    }
+        history: make([][][]int, length),
+    } 
 }
 
 
 func (this *SnapshotArray) Set(index int, val int)  {
-    items := this.items[index]
-    if items == nil { items = [][]int{}}
-    if len(items) > 0 && items[len(items)-1][1] == this.id {
-        items[len(items)-1][0] = val
-    } else {
-        items = append(items, []int{val, this.id})
+    pair := []int{val, this.snapId}
+    if this.history[index] == nil {
+        this.history[index] = [][]int{}
     }
-    this.items[index] = items
+    this.history[index] = append(this.history[index], pair)
 }
 
 
 func (this *SnapshotArray) Snap() int {
-    this.id++
-    return this.id-1
+    out := this.snapId
+    this.snapId++
+    return out
 }
 
 
 func (this *SnapshotArray) Get(index int, snap_id int) int {
-    // right most on left side of snap_id
-    items := this.items[index]
-
+    items := this.history[index]
     left := 0
     right := len(items)-1
-    ans := -1
+    ans := 0
     for left <= right {
         mid := left + (right-left)/2
         if items[mid][1] <= snap_id {
@@ -55,7 +40,7 @@ func (this *SnapshotArray) Get(index int, snap_id int) int {
         } else {
             right = mid-1
         }
-    }
+    }    
     return ans
 }
 
@@ -66,17 +51,4 @@ func (this *SnapshotArray) Get(index int, snap_id int) int {
  * obj.Set(index,val);
  * param_2 := obj.Snap();
  * param_3 := obj.Get(index,snap_id);
-
-["SnapshotArray","set","snap","snap","snap","get","snap","snap","get"]
-[[1],[0,15],[],[],[],[0,2],[],[],[0,0]]
-
-SnapshotArray(1) ; id = 0
-set(0,15)
-snap() id = 1
-snap() id = 2
-snap() id = 3
-get(0,2)
-snap() id = 4
-snap() id = 5
-get(0,0) 
  */
