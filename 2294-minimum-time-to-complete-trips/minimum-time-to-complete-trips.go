6c403ef41545200cea79d031bdafb963196c3a17
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-28 19:47:16 -0800

    Time: 57 ms (25.00%) | Memory: 10.9 MB (42.50%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.go
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.go
@@ -1,14 +1,14 @@
 func minimumTime(time []int, totalTrips int) int64 {
+    var ans int64 = -1
     left := 1
     right := slices.Max(time)*totalTrips
-    var ans int64 = -1
     for left <= right {
         mid := left + (right-left)/2
-        completed := 0
+        trips := 0
         for i := 0; i < len(time); i++ {
-            completed += (mid/time[i])
+            trips += (mid/time[i])
         }
-        if completed >= totalTrips {
+        if trips >= totalTrips {
             ans = int64(mid)
             right = mid-1
         } else {
