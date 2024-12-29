ba3fcab5145b629c3b40a75d61ef0148dfcea905
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-29 14:11:58 -0800

    Time: 0 ms (100.00%) | Memory: 4.6 MB (30.56%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.go
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.go
@@ -6,41 +6,35 @@
  * }
  */
 func splitListToParts(head *ListNode, k int) []*ListNode {
-    if k <= 1 {
-        return []*ListNode{head}
-    }
     n := 0
     curr := head
     for curr != nil {
         n++
         curr = curr.Next
     }
-    // If there are N nodes in the list, and k parts, 
-    // then every part has N/k elements, 
-    // except the first N%k parts have an extra one.
-    sizeOfEach := n/k
+    // we are creating k groups
+    // of size m each
+    m := n/k
+    r := n%k
     curr = head
-    out := []*ListNode{}
-    start := curr
-    size := 0
-    for curr != nil {
-        size++
-        next := curr.Next
-        if size < sizeOfEach || (len(out) < n%k && size < sizeOfEach+1) {
-            curr = next
-            continue
+    out := make([]*ListNode, k)
+    i := 0
+    for i < len(out) {
+        currSize := 0
+        h := curr
+        tail := curr
+        for currSize != m && curr != nil {
+            tail = curr
+            curr = curr.Next
+            currSize++
         }
-        
-        curr.Next = nil
-        out = append(out, start)
-        start = next
-        curr = next
-        size = 0
-    }
-    // its possible k > n
-    // then we need to fill output array to match k size with nil LL heads
-    for len(out) != k {
-        out = append(out, nil)
+        if r != 0 {
+            if curr != nil {tail = curr; curr=curr.Next}
+            r--
+        }
+        if tail != nil {tail.Next = nil}
+        out[i] = h
+        i++
     }
     return out
 }
