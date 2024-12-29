b31b250fdcdb451f504d5539c66a66c6948d9772
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-29 13:57:41 -0800

    Time: 1 ms (89.09%) | Memory: 11 MB (65.76%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.go
+++ 234-palindrome-linked-list/palindrome-linked-list.go
@@ -7,29 +7,26 @@
  */
 func isPalindrome(head *ListNode) bool {
     if head == nil || head.Next == nil {return true}
+    var prev *ListNode
     slow := head
     fast := head
-    var prev *ListNode
     for fast != nil && fast.Next != nil {
         prev = slow
         slow = slow.Next
         fast = fast.Next.Next
     }
+    // head2 is slow the ptr
+    head2 := reverse(slow)
     prev.Next = nil
-    h1 := head
-    h2 := slow
-    h2 = reverse(h2)
-    p1, p2 := h1, h2
-    for p1 != nil && p2 != nil {
-        if p1.Val != p2.Val {return false}
-        p1 = p1.Next
-        p2 = p2.Next
+    for head != nil && head2 != nil {
+        if head.Val != head2.Val {return false}
+        head = head.Next
+        head2 = head2.Next
     }
     return true
 }
 
 func reverse(head *ListNode) *ListNode {
-    if head == nil || head.Next == nil {return head}
     var prev *ListNode
     curr := head
     for curr != nil {
