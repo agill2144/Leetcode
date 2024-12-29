acd0f488889db26d931bba0c731f6b0a9dc54990
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-29 14:31:18 -0800

    Time: 29 ms (7.84%) | Memory: 8.2 MB (41.18%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.go
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.go
@@ -7,38 +7,24 @@
  */
 func insertGreatestCommonDivisors(head *ListNode) *ListNode {
     if head == nil || head.Next == nil {return head}
-    var prev *ListNode
     curr := head
     for curr != nil {
         next := curr.Next
-        if prev != nil {
-            // we have a,b
-            newNode := &ListNode{Val: gcd(prev.Val, curr.Val)}
-            prev.Next = newNode
-            newNode.Next = curr
+        if next != nil {
+            curr.Next = &ListNode{Val:gcd(curr.Val, next.Val)}
+            curr = curr.Next
+            curr.Next = next
         }
-        prev = curr
-        curr = next        
-    }    
+        curr = next
+    }
     return head
 }
 
-// greatest common divisior is highest such x that can divide both a and b evenly
-// meaning a/x and b/x leaves remainder as 0
 func gcd(a, b int) int {
-    // base
-    if a == b {return a}
-
-    // logic
-    if a > b {
-        // early exit
-        // eg; a = 10; b = 5
-        if a % b == 0 {return b}
-        return gcd(a-b, b)
-    } else {
-        // early exit
-        // eg; a = 5; b = 10
-        if b % a == 0 {return a}
-        return gcd(a, b-a)
+    res := min(a, b)
+    for res > 0 {
+        if a % res == 0 && b % res == 0 {return res}
+        res--
     }
+    return 0
 }
