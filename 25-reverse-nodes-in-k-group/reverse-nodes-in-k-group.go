5aafd3d2bc261f7b829696f8ee0767039f8b0cd9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-30 13:50:11 -0800

    Time: 0 ms (100.00%) | Memory: 5.5 MB (19.23%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.go
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.go
@@ -15,14 +15,22 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
         next := curr.Next
         if size == 1 {start = curr}
         if size == k {
+            // disconnect this group from next grp
             curr.Next = nil
+            // reverse this grp
             nh, nt := reverse(start)
+            // if there was a prev grp ( pt = prevTail )
+            // then connect pt with newHead (nh)
             if pt != nil {
                 pt.Next = nh
             } else {
+                // if there was no pt, it means this is 1st k size grp
+                // nh is the head node now
                 head = nh
             }
+            // connect this grp newTail with next node
             nt.Next = next
+            // move pt ptr to our newTail (nt)
             pt = nt
             size = 1
             curr = next
