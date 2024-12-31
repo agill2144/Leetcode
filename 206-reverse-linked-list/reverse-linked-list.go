236e0ae95a8d69c01ee7916d70f76128d11a12f9
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-30 17:37:24 -0800

    Time: 0 ms (100.00%) | Memory: 4.8 MB (7.39%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.go
+++ 206-reverse-linked-list/reverse-linked-list.go
@@ -10,9 +10,18 @@ func reverseList(head *ListNode) *ListNode {
     dfs = func(curr *ListNode) *ListNode {
         // base
         if curr == nil {return nil}
+
         // logic
         tail := dfs(curr.Next)
-        if curr.Next != nil {curr.Next.Next = curr; curr.Next = nil} else {return curr}
+        if curr.Next == nil {
+            // we are at the tail end of original LL
+            // the tail is the new head, therefore return it
+            return curr
+        } else {
+            // next node's next ptr should point to me!
+            curr.Next.Next = curr
+            curr.Next = nil
+        }
         return tail
     }
     return dfs(head)
