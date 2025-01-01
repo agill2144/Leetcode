98fc480aa697b081eff4a972e02bb5a1c27264ee
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-01 14:35:58 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (33.75%) - LeetSync

--- 143-reorder-list/reorder-list.go
+++ 143-reorder-list/reorder-list.go
@@ -7,7 +7,8 @@
  */
 func reorderList(head *ListNode)  {
     if head == nil || head.Next == nil {return}
-    var prev *ListNode    
+    // find middle
+    var prev *ListNode
     slow := head
     fast := head
     for fast != nil && fast.Next != nil {
@@ -15,33 +16,41 @@ func reorderList(head *ListNode)  {
         slow = slow.Next
         fast = fast.Next.Next
     }
+    // split into 2
     prev.Next = nil
-    head2 := slow
-    prev = nil
-    curr := head2
+    // reverse 2nd list
+    slow = reverse(slow)
+    // take 2 ptrs
+    p1 := head
+    p2 := slow
+    // collect node from left and then from right
+    // use sentinel node with a tail to collect nodes
+    // until both LL ptrs are done   
+    dummy := &ListNode{Val: 0}
+    tail := dummy
+    for p1 != nil || p2 != nil {
+        if p1 != nil {
+            tail.Next = p1
+            p1 = p1.Next
+            tail = tail.Next
+        }
+        if p2 != nil {
+            tail.Next = p2
+            p2 = p2.Next
+            tail = tail.Next
+        }
+    }
+}
+
+func reverse(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {return head}
+    curr := head
+    var prev *ListNode
     for curr != nil {
         next := curr.Next
         curr.Next = prev
         prev = curr
         curr = next
     }
-    head2 = prev
-
-    dummy := &ListNode{Val:0}
-    tail := dummy
-    p1 := head
-    p2 := head2
-    for p1 != nil || p2 != nil {
-        if p1 != nil {
-            tail.Next = p1
-            tail = tail.Next
-            p1 = p1.Next
-        }
-        if p2 != nil {
-            tail.Next = p2
-            tail = tail.Next
-            p2 = p2.Next
-        }
-    }
-    dummy.Next = nil
+    return prev
 }
