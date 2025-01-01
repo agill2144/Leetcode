c2c24381b04f08e55349de66bd1ea2fca82934bc
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-01 14:17:35 -0800

    Time: 0 ms (100.00%) | Memory: 3.8 MB (49.35%) - LeetSync

--- 670-maximum-swap/maximum-swap.go
+++ 670-maximum-swap/maximum-swap.go
@@ -1,82 +1,45 @@
+
 // bring the largest value in smallest unit to the largest unit
 // time = o(n)
 // space = o(n) for nums list + o(9) for idxs array = o(n)
 func maximumSwap(num int) int {
-    numStr := strings.Split(fmt.Sprintf("%v", num), "")
-    idxs := make([]int, 10)
+    if num <= 9 {return num}
+    numStr := fmt.Sprintf("%v", num)
+    nums := []int{}
+    for i := 0; i < len(numStr); i++ {nums = append(nums, int(numStr[i]-'0'))}
+    idxs := make([]int, 10) 
+    // pre-fill idxs with -1 ( as if they are not yet found )
     for i := 0; i < len(idxs); i++ {idxs[i] = -1}
     // store the last occurrence of each digit
-    for i := 0; i < len(numStr); i++ {
-        ii, _ := strconv.Atoi(numStr[i])
-        idxs[ii] = i
-    }
-    n := len(numStr)
-    found := false
-
-    // traverse the string to find the first digit that can be swapped with
-    for i := 0; i < n && !found; i++ {
-        iVal, _ := strconv.Atoi(numStr[i])
-        // start with largest possible digit
-        // and check if we have seen this digit before
-        // because we can only use digits that exist within the num input
-        for j := 9; j > iVal; j-- {
-            /// if we found a digit larger than ith val
-            // its idx must also be on the right of ith idx
-            // we want to "bring the largest value in smallest unit to the largest unit"
+    // map each value to its idx ( val : idx ), and note down its actual idx position from input
+    // if a value shows up again, thats fine, we can override its idx again ( we want the last idx , right most idx )
+    // ex; 989
+    // idx of 9 will be 2 at the end
+    for i := 0; i < len(nums); i++ {idxs[nums[i]] = i}
+    
+    swapped := false
+    for i := 0; i < len(nums) && !swapped; i++ {
+        // for each digit on the higest unit ( from left to right )
+        // check for possible swaps
+        // if we value 2, 3,4, we want highest possible number to swap with
+        // highest possible num is 9
+        // therefore start the idx loop from the back of the array
+        // each idx is a value and the value in the idx array represents the last idx we've seen this value at in the input
+        for j := len(idxs)-1; j > nums[i]; j-- {
+            // if an idx does exist
+            // and its after the ith idx,
+            // then we can swap
             if idxs[j] != -1 && idxs[j] > i {
-                // swap it, and break, because only 1 swap is allowed
-                maxIdx := idxs[j]
-                numStr[i], numStr[maxIdx] = numStr[maxIdx], numStr[i]
-                found = true
+                nums[idxs[j]], nums[i] = nums[i], nums[idxs[j]]
+                swapped = true
                 break
             }
         }
     }
-
-    // reconstruct the num from list back into int
-    out := 0
-    for i := 0; i < len(numStr); i++ {
-        val, _ := strconv.Atoi(numStr[i])
-        out = out*10+val
+    // re-construct the number back
+    res := 0
+    for i := 0; i < len(nums); i++ {
+        res = (res * 10) + nums[i]
     }
-    return out
-
+    return res
 }
-// func maximumSwap(num int) int {
-//     numStr := strings.Split(fmt.Sprintf("%v", num),"")
-//     n := len(numStr)
-//     maxIdxs := make([]int, n)
-//     maxIdxs[n-1] = n-1
-//     for i := n-2; i >= 0; i-- {
-//         currVal, _ := strconv.Atoi(numStr[i])
-//         maxSoFar, _ := strconv.Atoi(numStr[maxIdxs[i+1]])
-//         if currVal > maxSoFar {
-//             maxIdxs[i] = i
-//         } else {
-//             maxIdxs[i] = maxIdxs[i+1]
-//         }
-//     }
-
-//     for i := 0; i < n; i++ {
-//         maxIdx := maxIdxs[i]
-//         maxOnRight, _:= strconv.Atoi(numStr[maxIdx])
-//         currVal, _ := strconv.Atoi(numStr[i])
-//         if maxOnRight > currVal {
-//             numStr[maxIdx], numStr[i] = numStr[i], numStr[maxIdx]
-//             break
-//         }
-//     }
-//     out := 0
-//     for i := 0; i < len(numStr); i++ {
-//         val, _ := strconv.Atoi(numStr[i])
-//         out = out*10+val
-//     }
-//     return out
-// }
-
-/*
-    126546
-    555555
-
-    626541
-*/
