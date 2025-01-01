a2cf554da09df996e0446380889d594ce5386ec3
agill2144 <120353980+agill2144@users.noreply.github.com>
2024-12-31 16:35:27 -0800

    Time: 29 ms (62.35%) | Memory: 17 MB (68.23%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.go
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.go
@@ -1,49 +1,35 @@
 type NumMatrix struct {
-    prefixSum [][]int
+    matrix [][]int
 }
 
 
 func Constructor(matrix [][]int) NumMatrix {
     m := len(matrix)
     n := len(matrix[0])
-    prefixSum := make([][]int, m)
     for i := 0; i < m; i++ {
-        prefixSum[i] = make([]int, n)
         for j := 0; j < n; j++ {
-            prev := 0
-            if j-1 >= 0 {prev = prefixSum[i][j-1]}
-            above := 0
-            if i-1 >= 0 {above = prefixSum[i-1][j]}
-            overlap := 0
-            if i-1 >= 0 && j-1 >= 0 {overlap = prefixSum[i-1][j-1]}
-            // sum at i, j = left panel + top panel ( prev + above )
-            // left panel and top panel could have overlapping cells
-            // so they get added twice, therefore remvove the overlapping cells
-            // that got added twice
-            prefixSum[i][j] = matrix[i][j] + prev + above - overlap
+            left := 0
+            if j-1 >= 0 {left = matrix[i][j-1]}
+            top := 0
+            if i-1 >= 0 {top = matrix[i-1][j]}
+            overlapping := 0
+            if i-1 >= 0 && j-1 >= 0 {overlapping = matrix[i-1][j-1]}
+            val := left+top+matrix[i][j]-overlapping
+            matrix[i][j] = val
         }
-    }
-    return NumMatrix{prefixSum}
+    }    
+    return NumMatrix{matrix}
 }
 
 
 func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
-    // right corner sum 
-    total := this.prefixSum[row2][col2]
-    // remove left panel sum
-    if col1-1 >= 0 {
-        total -= this.prefixSum[row2][col1-1]
-    }
-    // remove top panel sum
-    if row1-1 >= 0 {
-        total -= this.prefixSum[row1-1][col2]
-    }
-    // add back the overlapping cells that got removed twice
-    if row1-1 >= 0 && col1-1 >= 0 {
-        total += this.prefixSum[row1-1][col1-1]
-    }
-    return total
-
+    topSum := 0
+    if row1-1 >= 0 {topSum = this.matrix[row1-1][col2]}
+    leftSum := 0
+    if col1-1 >= 0 {leftSum = this.matrix[row2][col1-1]}
+    overlapping := 0
+    if row1-1 >= 0 && col1-1 >= 0 {overlapping = this.matrix[row1-1][col1-1]}
+    return this.matrix[row2][col2] - topSum - leftSum + overlapping
 }
 
 
