b461f5c2300794adf784cb5979a2cd27f481b496
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-01 12:30:19 -0800

    Time: 0 ms (100.00%) | Memory: 8 MB (37.50%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
@@ -13,22 +13,43 @@
  *     Right *TreeNode
  * }
  */
+
+/*
+    approach: brute force with extra space
+    - traverse the LL and put the node values into a list
+    - then build a height balanced bst using the sorted list
+    - the root node will the mid element; given 2 idxs left and right 
+    - the left of this root will be elements from left -> mid-1
+    - the right of this root will be elements from mid+1 -> right
+
+    tc = o(2n)
+    sc = o(n)
+
+    approach: no extra list space
+    - we can get mid of linkedlist using slow and fast ptrs
+    - and then split the list into 2 halves; for creating left subtree and for creating right subtree
+
+*/
 func sortedListToBST(head *ListNode) *TreeNode {
     if head == nil {return nil}
-    nodes := []int{}
-    curr := head
-    for curr != nil {nodes = append(nodes, curr.Val); curr = curr.Next}
-    var dfs func(left, right int) *TreeNode
-    dfs = func(left, right int) *TreeNode {
+    var dfs func(left, right *ListNode) *TreeNode
+    dfs = func(left, right *ListNode) *TreeNode {
         // base
-        if left > right {return nil}
+        if left == right {return nil}
+
         // logic
-        mid := left + (right-left)/2
-        root := &TreeNode{Val: nodes[mid]}
-        root.Left = dfs(left, mid-1)
-        root.Right = dfs(mid+1, right)
+        slow := left
+        fast := left
+        for fast != right && fast.Next != right {
+            slow = slow.Next
+            fast = fast.Next.Next
+        }
+        // slow is at mid point
+        root := &TreeNode{Val: slow.Val}
+        root.Left = dfs(left, slow)
+        root.Right = dfs(slow.Next, right)
         return root
     }
-    return dfs(0, len(nodes)-1)
+    return dfs(head, nil)
     
 }
