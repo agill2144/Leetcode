1af48c96d9dd04d30e359e25b2bbb81bcf5076de
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-01 19:40:57 -0800

    Time: 14 ms (15.38%) | Memory: 22.4 MB (61.54%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.go
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.go
@@ -1,26 +1,25 @@
 func vowelStrings(words []string, queries [][]int) []int {
-    prefixCounts := make([]int, len(words))
-    for i := 0; i < len(words); i++ {
+    n := len(words)
+    vCount := make([]int, n)
+    for i := 0; i < n; i++ {
         prev := 0
-        if i-1 >= 0 {prev = prefixCounts[i-1]}
-        if meetsVowelCondition(words[i]) {            
-            prefixCounts[i] = prev+1
-        } else {
-            prefixCounts[i] = prev
-        }
+        if i-1 >= 0 {prev = vCount[i-1]}
+        vCount[i] = prev
+        if meetsVowel(words[i]) {vCount[i]++}
     }
-    out := []int{}
+    out := make([]int, len(queries))
     for i := 0; i < len(queries); i++ {
-        start, end := queries[i][0], queries[i][1]
-        total := prefixCounts[end]
-        if start - 1 >= 0 {total -= prefixCounts[start-1]}
-        out = append(out, total)
+        left, right := queries[i][0], queries[i][1]
+        count := vCount[right]
+        leftCount := 0
+        if left-1 >= 0 {leftCount = vCount[left-1]}
+        count -= leftCount
+        out[i] = count
     }
     return out
 }
 
-func meetsVowelCondition(word string) bool {
-    // words that start and end with a vowel
-    vs := map[byte]bool{'a':true,'e':true,'i':true,'o':true,'u':true}
-    return vs[word[0]] && vs[word[len(word)-1]]
+func meetsVowel(word string) bool {
+    v := map[byte]bool{'a':true,'e':true,'i':true,'o':true,'u':true}
+    return v[word[0]] && v[word[len(word)-1]]
 }
