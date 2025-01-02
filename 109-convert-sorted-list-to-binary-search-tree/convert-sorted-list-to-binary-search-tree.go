51aeb384053e927aed75cfbe81916901d753391c
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-01 21:24:53 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (44.32%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
@@ -13,45 +13,29 @@
  *     Right *TreeNode
  * }
  */
-
-/*
-    approach: brute force with extra space
-    - traverse the LL and put the node values into a list
-    - then build a height balanced bst using the sorted list
-    - the root node will the mid element; given 2 idxs left and right 
-    - the left of this root will be elements from left -> mid-1
-    - the right of this root will be elements from mid+1 -> right
-
-    tc = o(2n)
-    sc = o(n)
-
-    approach: no extra list space
-    - we can get mid of linkedlist using slow and fast ptrs
-    - and then split the list into 2 halves; for creating left subtree and for creating right subtree
-
-*/
 func sortedListToBST(head *ListNode) *TreeNode {
-    if head == nil {return nil}
     var dfs func(curr *ListNode) *TreeNode
     dfs = func(curr *ListNode) *TreeNode {
         // base
         if curr == nil {return nil}
-        if curr.Next == nil {return &TreeNode{Val: curr.Val}}
+        if curr.Next == nil {
+            return &TreeNode{Val: curr.Val}
+        }        
+
         // logic
+        prev := curr
         slow := curr
         fast := curr
-        var prev *ListNode
         for fast != nil && fast.Next != nil {
             prev = slow
             slow = slow.Next
             fast = fast.Next.Next
         }
-        root := &TreeNode{Val: slow.Val}
         prev.Next = nil
+        root := &TreeNode{Val:slow.Val}
         root.Left = dfs(curr)
         root.Right = dfs(slow.Next)
         return root
     }
     return dfs(head)
-    
 }
