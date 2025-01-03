d17575ede83d2688ee79fe4815215b8809f5f4c4
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-02 18:20:07 -0800

    Time: 33 ms (30.46%) | Memory: 8.6 MB (51.98%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.go
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.go
@@ -6,16 +6,21 @@
  * }
  */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-    aSize := 0
-    bSize := 0
-    curr := headA
-    for curr != nil {aSize++; curr=curr.Next}
-    curr = headB
-    for curr != nil {bSize++; curr=curr.Next}
-    for aSize > bSize {headA = headA.Next; aSize--}
-    for bSize > aSize {headB = headB.Next; bSize--}
-    for headA != nil && headB != nil {
-        if headA == headB  {return headA}
+    getLen := func(head *ListNode) int {
+        n := 0
+        curr := head
+        for curr != nil {
+            n++
+            curr = curr.Next
+        }
+        return n
+    }
+    aSize := getLen(headA)
+    bSize := getLen(headB)
+    for aSize > bSize {headA=headA.Next; aSize--}
+    for bSize > aSize {headB=headB.Next; bSize--}
+    for headA != nil && headB != nil{
+        if headA == headB {return headA}
         headA = headA.Next
         headB = headB.Next
     }
