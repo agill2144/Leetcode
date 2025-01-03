ab8bdacb808a9331424b9f31b30232d1de14ca0b
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-02 20:48:24 -0800

    Time: 24 ms (42.42%) | Memory: 10.1 MB (78.79%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.go
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.go
@@ -3,16 +3,19 @@ func maximumCandies(candies []int, k int64) int {
     right := slices.Max(candies)
     ans := 0
     for left <= right {
-        mid := left+(right-left)/2
-        piles := 0
+        // num candies allowed per pile
+        mid := left + (right-left)/2
+        pileCount := 0
         for i := 0; i < len(candies); i++ {
-            piles += (candies[i]/mid)
+            pileCount += (candies[i]/mid)
         }
-        if int64(piles) >= k {
+        // when does mid not work?
+        // when we could not create k piles ( one for each child )
+        if int64(pileCount) < k {
+            right = mid-1
+        } else {
             ans = mid
             left = mid+1
-        } else {
-            right = mid-1
         }
     }
     return ans
