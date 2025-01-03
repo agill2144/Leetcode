00cd27ccf9d4564e7f4734aebe01a86754806b12
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-02 18:27:12 -0800

    Time: 0 ms (100.00%) | Memory: 6.6 MB (51.14%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.go
+++ 735-asteroid-collision/asteroid-collision.go
@@ -1,21 +1,27 @@
-func asteroidCollision(asteroid []int) []int {
+func asteroidCollision(ast []int) []int {
+    n := len(ast)
     st := []int{}
-    for i := 0; i < len(asteroid); i++ {
+    for i := 0; i < n; i++ {
         pushCurr := true
-        for len(st) != 0 && asteroid[i] < 0 && st[len(st)-1] > 0 {
-            if abs(asteroid[i]) <= st[len(st)-1] {
-                if abs(asteroid[i]) == st[len(st)-1] {st = st[:len(st)-1]}
+        for len(st) != 0 && st[len(st)-1] > 0 && ast[i] < 0 {
+            if st[len(st)-1] == abs(ast[i]) {
+                st = st[:len(st)-1]
+                pushCurr = false
+                break
+            } else if st[len(st)-1] < abs(ast[i]) {
+                st = st[:len(st)-1]
+            } else if st[len(st)-1] > abs(ast[i]) {
                 pushCurr = false
                 break
             }
-            st = st[:len(st)-1]                
         }
         if pushCurr {
-            st = append(st, asteroid[i])
+            st = append(st, ast[i])
         }
     }
     return st
 }
+
 
 func abs(x int) int {
     if x < 0 {return x*-1}
