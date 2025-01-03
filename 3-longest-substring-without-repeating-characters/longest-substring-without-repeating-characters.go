8efd0386a007e86588bb9e42794292be4535dcc5
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-02 21:10:06 -0800

    Time: 3 ms (76.32%) | Memory: 5 MB (53.53%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -1,14 +1,13 @@
 func lengthOfLongestSubstring(s string) int {
     res := 0
     left := 0
-    freq := map[byte]int{}
+    idx := map[byte]int{}
     for i := 0; i < len(s); i++ {
-        freq[s[i]]++
-        for freq[s[i]] > 1 {
-            freq[s[left]]--
-            if freq[s[left]] == 0 {delete(freq, s[left])}
-            left++
+        val, ok := idx[s[i]]
+        if ok && left <= val {
+            left = val+1
         }
+        idx[s[i]] = i
         res = max(res, i-left+1)
     }
     return res
