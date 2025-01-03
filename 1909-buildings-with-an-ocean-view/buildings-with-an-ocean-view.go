4da32c6a814e23d6a3298c1a920a25ccac0cd26d
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-02 20:10:32 -0800

    Time: 3 ms (56.25%) | Memory: 10.3 MB (64.06%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.go
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.go
@@ -1,15 +1,10 @@
 func findBuildings(heights []int) []int {
-    n := len(heights)
-    maxOnRight := heights[n-1]
-    out := []int{n-1}
-    for i := n-2; i >= 0; i-- {
-        if heights[i] > maxOnRight {
-            out = append(out, i)
-            maxOnRight = heights[i]
+    st := []int{} // idxs
+    for i := 0; i < len(heights); i++ {
+        for len(st) != 0 && heights[i] >= heights[st[len(st)-1]] {
+            st = st[:len(st)-1]
         }
+        st = append(st, i)
     }
-    for i := 0; i < len(out)/2; i++ {
-        out[i], out[len(out)-1-i] = out[len(out)-1-i], out[i]
-    }
-    return out
+    return st
 }
