13a1cad2d6bef6798c1e0fc6bc36ed0e340b145c
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-03 15:15:20 -0800

    Time: 0 ms (100.00%) | Memory: 7 MB (20.69%) - LeetSync

--- 545-boundary-of-binary-tree/boundary-of-binary-tree.go
+++ 545-boundary-of-binary-tree/boundary-of-binary-tree.go
@@ -6,58 +6,57 @@
  *     Right *TreeNode
  * }
  */
+/*
+    0 = root ( curr node is root )
+    1 = left boundary node ( curr node is left boundary )
+    2 = right boundary node ( curr node is right boundary )
+    3 = middle node ( curr node is middle node )
+*/
 func boundaryOfBinaryTree(root *TreeNode) []int {
-    if root == nil {return nil}
-    out := []int{root.Val}
-    left := func(r *TreeNode) {
-        for r != nil {
-            if r.Left == nil && r.Right == nil {return}
-            out = append(out, r.Val)
-            if r.Left != nil {
-                r = r.Left
-            } else {
-                r = r.Right
-            }
-        }
-    }
-    left(root.Left)
-    // leaves from left to right
-    var leaves func(r *TreeNode)
-    leaves = func(r *TreeNode) {
+    left := []int{}
+    right := []int{}
+    leaves := []int{}
+    var dfs func(r *TreeNode, state int)
+    dfs = func(r *TreeNode, state int) {
         // base
         if r == nil {return}
 
         // logic
-        if r != root && r.Left == nil && r.Right == nil {
-            out = append(out, r.Val)
-            return
+        if state == 0 {
+            left = append(left, r.Val)
+        } else if state == 1 {
+            left = append(left, r.Val)
+        } else if state == 2 {
+            right = append(right, r.Val)
+        } else if r.Left == nil && r.Right == nil {
+            leaves = append(leaves, r.Val)
         }
-        leaves(r.Left)
-        leaves(r.Right)
+        dfs(r.Left, leftState(r,state))
+        dfs(r.Right, rightState(r,state))
     }
-    leaves(root)
+    dfs(root, 0)
+    left = append(left, leaves...)
+    for i := len(right)-1; i >= 0; i-- {
+        left = append(left, right[i])
+    }
+    return left
+}
 
-    // right side
-    // but we need it in reverse order
-    // therefore keeping track of leftPtr
-    leftPtr := len(out)
-    right := func(r *TreeNode) {
-        for r != nil {
-            if r.Left == nil && r.Right == nil {return}
-            out = append(out, r.Val)
-            if r.Right != nil {
-                r = r.Right
-            } else {
-                r = r.Left
-            }
-        }
+
+func leftState(r *TreeNode, curr int) int {
+    if curr == 0 || curr == 1 {
+        return 1
+    } else if curr == 2 {
+        if r.Right == nil {return 2}
     }
-    right(root.Right)
-    rightPtr := len(out)-1
-    for leftPtr < rightPtr {
-        out[leftPtr], out[rightPtr] = out[rightPtr], out[leftPtr]
-        leftPtr++
-        rightPtr--
+    return 3
+}
+
+func rightState(r *TreeNode, curr int) int {
+    if curr == 0 || curr == 2 {
+        return 2
+    } else if curr == 1 {
+        if r.Left == nil {return 1}
     }
-    return out
+    return 3
 }
