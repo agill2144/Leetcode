591160c5b54923202898480a8e76021b75e460d8
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-03 13:57:37 -0800

    Time: 0 ms (100.00%) | Memory: 7.1 MB (20.69%) - LeetSync

--- 545-boundary-of-binary-tree/boundary-of-binary-tree.go
+++ 545-boundary-of-binary-tree/boundary-of-binary-tree.go
@@ -7,44 +7,57 @@
  * }
  */
 func boundaryOfBinaryTree(root *TreeNode) []int {
+    if root == nil {return nil}
     out := []int{root.Val}
     left := func(r *TreeNode) {
         for r != nil {
-            if r.Left == nil && r.Right == nil {break}
+            if r.Left == nil && r.Right == nil {return}
             out = append(out, r.Val)
-            if r.Left != nil {r = r.Left} else {r = r.Right} 
+            if r.Left != nil {
+                r = r.Left
+            } else {
+                r = r.Right
+            }
         }
     }
     left(root.Left)
-    // leaves
-    var leaves func(r *TreeNode) 
+    // leaves from left to right
+    var leaves func(r *TreeNode)
     leaves = func(r *TreeNode) {
         // base
         if r == nil {return}
 
         // logic
+        if r != root && r.Left == nil && r.Right == nil {
+            out = append(out, r.Val)
+            return
+        }
         leaves(r.Left)
         leaves(r.Right)
-        if r.Left == nil && r.Right == nil && r != root {
-            out = append(out, r.Val)
-        }
     }
     leaves(root)
 
-    // right
-    startIdx := len(out)
+    // right side
+    // but we need it in reverse order
+    // therefore keeping track of leftPtr
+    leftPtr := len(out)
     right := func(r *TreeNode) {
         for r != nil {
-            if r.Left == nil && r.Right == nil {break}
+            if r.Left == nil && r.Right == nil {return}
             out = append(out, r.Val)
-            if r.Right != nil {r = r.Right} else {r = r.Left}            
+            if r.Right != nil {
+                r = r.Right
+            } else {
+                r = r.Left
+            }
         }
     }
     right(root.Right)
-    l, r := startIdx, len(out)-1
-    for l < r {
-        out[l], out[r] = out[r], out[l]
-        l++; r--
+    rightPtr := len(out)-1
+    for leftPtr < rightPtr {
+        out[leftPtr], out[rightPtr] = out[rightPtr], out[leftPtr]
+        leftPtr++
+        rightPtr--
     }
     return out
 }
