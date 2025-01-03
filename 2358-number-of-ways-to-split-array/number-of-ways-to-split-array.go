6d33b694984cbc74862575f9b24844a39ce79cf3
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-02 18:38:10 -0800

    Time: 0 ms (100.00%) | Memory: 11.5 MB (22.50%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.go
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.go
@@ -3,6 +3,9 @@ func waysToSplitArray(nums []int) int {
     for i := 0; i < len(nums); i++ {total += nums[i]}
     sum := 0
     count := 0
+    // why upto n-1 ? 
+    // because of "There is at least one element to the right of i"
+    // meaning, we must have 1 num in the right subarray/ right prefix sum
     for i := 0; i < len(nums)-1; i++ {
         sum += nums[i]
         rightSum := total-sum
