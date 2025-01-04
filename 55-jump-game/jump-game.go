3f6b1beafe8b0098da13d8688e61e530707575d5
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-04 13:13:17 -0800

    Time: 0 ms (100.00%) | Memory: 9.4 MB (34.04%) - LeetSync

--- 55-jump-game/jump-game.go
+++ 55-jump-game/jump-game.go
@@ -1,35 +1,53 @@
 func canJump(nums []int) bool {
     n := len(nums)
-    memo := map[int]*bool{}
-    var dfs func(ptr int)bool
-    dfs = func(ptr int)bool {
-        if ptr >= n-1 {return true}
-
-        // already solved idx
-        if memo[ptr] != nil {return *memo[ptr]}
-        jumpSize := nums[ptr]
-
-        // when jump size is 0, not possible to proceed,
-        // save false and return false
-        if jumpSize == 0 {
-            memo[ptr] = toBoolPtr(false)
-            return *memo[ptr]
-        }
-
-        var res bool
-        for i := jumpSize; i >= 1; i-- {
-            res = dfs(ptr+i)
-            // as soon as we get a true, break
-            if res {break}
-        }
-        // save the res
-        memo[ptr] = toBoolPtr(res)
-        // return the res
-        return *memo[ptr]
+    if n <= 1 {return true}
+    i := 0
+    right := 0
+    for i <= right {
+        newR := i+nums[i]
+        i++
+        right = max(right, newR)
+        if right >= n-1 {return true}
     }
-    return dfs(0)
+    return false
 }
+/*
+    approach: brute force dfs
+    - try all possible jump distances!
+        - but lets be a little greedy and take the highest possible jump first
+        - if jumpSize is 5
+        - instead of doing try jump 1, then 2, then 3... then 5
+        - lets start from 5 first
+        - we care about whether we can reach end or not
+    - start at pos 0
+    - then take all possible jumps, and explore each path depth first
+    - example : [2,2,0,1]
+    - we are idx 0 initially, so take 2 jumps and we end at idx 2 (val = 0)
+    - and now at idx 2, explore the same thing
+        - but since this is 0, we cannot go anywhere
+        - go back to prev recursion
+        - do we need to backtrack anything?
+        - no, the args are not references, so no
+    - and now we are back at idx 0, take the next smaller jump of size 1
+    - now we are idx 1, val = 2
+    - again, explore all possible jump dists but be greedy and start from highest
 
-func toBoolPtr(val bool) *bool {
-    return &val
-}
+    tc:
+    - numOfOptionsPerNode^totalNumOfOptions
+    - at each node we have x options
+    - x is the value of each node in the array
+    - tc = x^n
+*/
+// func canJump(nums []int) bool {
+//     n := len(nums)
+//     var dfs func(ptr int)bool
+//     dfs = func(ptr int)bool {
+//         if ptr >= n-1 {return true}
+//         jumpSize := nums[ptr]
+//         for i := 1; i <= jumpSize; i++ {
+//             if dfs(ptr+i) {return true}
+//         }
+//         return false
+//     }
+//     return dfs(0)
+// }
