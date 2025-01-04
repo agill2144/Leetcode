71b876860a4bf2e9050866c2f631ac9e0722fcae
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-03 21:17:57 -0800

    Time: 0 ms (100.00%) | Memory: 8.5 MB (56.36%) - LeetSync

---
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.go
@@ -0,0 +1,28 @@
+func sortArrayByParityII(nums []int) []int {
+    i := 0
+    n := len(nums)
+    e := 0
+    o := n-1
+    for i < n {
+        numIsEven := nums[i] % 2 == 0
+        idxIsEven := i % 2 == 0
+        numIsOdd := !numIsEven
+        idxIsOdd := !idxIsEven
+        if numIsEven {
+            if idxIsEven {
+                i++
+            } else {
+                nums[i], nums[e] = nums[e], nums[i]
+                e+=2
+            }
+        } else if numIsOdd {
+            if idxIsOdd {
+                i++
+            } else {
+                nums[i], nums[o] = nums[o], nums[i]
+                o -= 2
+            }
+        }
+    }
+    return nums
+}
