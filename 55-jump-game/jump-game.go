d4ffadfce3c6e41379f2b723483332e4aa521fd0
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-04 11:23:01 -0800

    Time: 1401 ms (5.04%) | Memory: 10.6 MB (12.87%) - LeetSync

--- 55-jump-game/jump-game.go
+++ 55-jump-game/jump-game.go
@@ -1,10 +1,35 @@
 func canJump(nums []int) bool {
-    farthestIdx := 0
-    i := 0
-    for i <= farthestIdx && i < len(nums) {
-        farthestIdx = max(farthestIdx, nums[i]+i)
-        if farthestIdx >= len(nums)-1 {return true}
-        i++
+    n := len(nums)
+    memo := map[int]*bool{}
+    var dfs func(ptr int)bool
+    dfs = func(ptr int)bool {
+        if ptr >= n-1 {return true}
+
+        // already solved idx
+        if memo[ptr] != nil {return *memo[ptr]}
+        jumpSize := nums[ptr]
+
+        // when jump size is 0, not possible to proceed,
+        // save false and return false
+        if jumpSize == 0 {
+            memo[ptr] = toBoolPtr(false)
+            return *memo[ptr]
+        }
+
+        var res bool
+        for i := jumpSize; i >= 1; i-- {
+            res = dfs(ptr+i)
+            // as soon as we get a true, break
+            if res {break}
+        }
+        // save the res
+        memo[ptr] = toBoolPtr(res)
+        // return the res
+        return *memo[ptr]
     }
-    return false
+    return dfs(0)
+}
+
+func toBoolPtr(val bool) *bool {
+    return &val
 }
