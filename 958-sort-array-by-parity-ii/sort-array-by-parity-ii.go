a52043b6ee7e355c44cc39dd8442c5c66380d168
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-03 21:55:45 -0800

    Time: 0 ms (100.00%) | Memory: 8.5 MB (56.36%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.go
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.go
@@ -2,26 +2,20 @@ func sortArrayByParityII(nums []int) []int {
     i := 0
     n := len(nums)
     e := 0
-    o := n-1
+    o := 1
     for i < n && e < n && o >= 0 {
         numIsEven := nums[i] % 2 == 0
         idxIsEven := i % 2 == 0
         numIsOdd := !numIsEven
         idxIsOdd := !idxIsEven
-        if numIsEven {
-            if idxIsEven {
-                i++
-            } else {
-                nums[i], nums[e] = nums[e], nums[i]
-                e+=2
-            }
-        } else if numIsOdd {
-            if idxIsOdd {
-                i++
-            } else {
-                nums[i], nums[o] = nums[o], nums[i]
-                o -= 2
-            }
+        if numIsEven && idxIsOdd {
+            nums[e], nums[i] = nums[i], nums[e]
+            e+=2
+        } else if numIsOdd &&  idxIsEven {
+            nums[o], nums[i] = nums[i], nums[o]
+            o+=2
+        } else {
+            i++
         }
     }
     return nums
