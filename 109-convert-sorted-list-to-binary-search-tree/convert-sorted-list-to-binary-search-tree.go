241b016b88da8020462ac5dd7fba475c2632f4b9
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-04 18:34:22 -0800

    Time: 3 ms (17.35%) | Memory: 8 MB (43.88%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
@@ -13,33 +13,15 @@
  *     Right *TreeNode
  * }
  */
-/*
-    approach: brute force with extra space
-    - traverse the LL and put the node values into a list
-    - then build a height balanced bst using the sorted list
-    - the root node will the mid element; given 2 idxs left and right 
-    - the left of this root will be elements from left -> mid-1
-    - the right of this root will be elements from mid+1 -> right
-
-    tc = o(2n)
-    sc = o(n)
-
-    approach: no extra list space
-    - we can get mid of linkedlist using slow and fast ptrs
-    - and then split the list into 2 halves; for creating left subtree and for creating right subtree
-
-*/
 func sortedListToBST(head *ListNode) *TreeNode {
     var dfs func(curr *ListNode) *TreeNode
     dfs = func(curr *ListNode) *TreeNode {
         // base
         if curr == nil {return nil}
-        if curr.Next == nil {
-            return &TreeNode{Val: curr.Val}
-        }        
+        if curr.Next == nil {return &TreeNode{Val: curr.Val}}
 
         // logic
-        prev := curr
+        var prev *ListNode
         slow := curr
         fast := curr
         for fast != nil && fast.Next != nil {
@@ -47,13 +29,7 @@ func sortedListToBST(head *ListNode) *TreeNode {
             slow = slow.Next
             fast = fast.Next.Next
         }
-        // slow ptr is at middle node
-        // therefore mid is our root node
-        root := &TreeNode{Val:slow.Val}
-        // cut the list into 2 halves
-        // left half is the left subtree (curr->prev)
-        // right half is right subtree (slow.Next->right)
-        // slow is already used! slow is the root node, therefore right half is after slow ptr
+        root := &TreeNode{Val: slow.Val}
         prev.Next = nil
         root.Left = dfs(curr)
         root.Right = dfs(slow.Next)
