a97d37273cda4d07ed65e45e9cb299f25eb7a0bd
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-04 18:25:37 -0800

    Time: 67 ms (90.70%) | Memory: 9 MB (83.72%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.go
+++ 282-expression-add-operators/expression-add-operators.go
@@ -1,35 +1,30 @@
-// numberOfOptionsPerNode ^ totalNumberOfOptions * extraWorkPerRecursion
-// numberOfOptionsPerNode = 4
-// totalNumberOfOptions = n
-// extraWorkPerRecursion = subStr creation takes n time
-// tc = 4^n * n
-// sc = n
 func addOperators(num string, target int) []string {
     out := []string{}
-    var dfs func(start int, total int, lastContr int, path string)
-    dfs = func(start int, total int, lastContr int, path string) {
+    var dfs func(start int, total int, lastContr int, expr string)
+    dfs = func(start, total, lastContr int, expr string) {
         // base
-        if start == len(num) {
-            if total == target {
-                out = append(out, path)
-            }
+        if start == len(num){
+            if total == target {out = append(out, expr)}
             return
         }
 
         // logic
         for i := start; i < len(num); i++ {
             subStr := num[start:i+1]
-            if len(subStr) > 1 && subStr[0] == '0' {continue}
+            if len(subStr) > 1 && subStr[0] == '0' {return}
             subStrInt, _ := strconv.Atoi(subStr)
-            if len(path) == 0 {
+            if expr == "" {
                 dfs(i+1, subStrInt, subStrInt, subStr)
             } else {
-                dfs(i+1, total + subStrInt, subStrInt, path + "+" + subStr)
-                dfs(i+1, total - subStrInt, -subStrInt, path + "-" + subStr)
-                dfs(i+1, total - lastContr + lastContr * subStrInt, lastContr*subStrInt, path + "*" + subStr)
+                // +
+                dfs(i+1, total+subStrInt, subStrInt, expr+"+"+subStr)
+                // -
+                dfs(i+1, total-subStrInt, -subStrInt, expr+"-"+subStr)
+                // *
+                dfs(i+1, total-lastContr+(lastContr*subStrInt), lastContr*subStrInt, expr+"*"+subStr)
             }
         }
     }
-    dfs(0,0,0,"")
+    dfs(0,0,0, "")
     return out
 }
