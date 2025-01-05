0a2ab6481b67fd63d73eedf697884700f10df453
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-05 13:54:10 -0800

    Time: 26 ms (27.45%) | Memory: 10.9 MB (13.73%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.go
+++ 912-random-pick-with-weight/random-pick-with-weight.go
@@ -1,39 +1,36 @@
 type Solution struct {
-    compressedRanges []int
+    cmpIdxs []int
 }
 
 
 func Constructor(w []int) Solution {
-    compressedRanges := make([]int, len(w))
+    sum := 0
+    cmpIdxs := []int{}    
     for i := 0; i < len(w); i++ {
-        compressedRanges[i] = w[i]
-        if i-1 >= 0 {compressedRanges[i] += compressedRanges[i-1]}
+        sum += w[i]
+        cmpIdxs = append(cmpIdxs, sum)
     }
-    return Solution{compressedRanges}
+    return Solution{cmpIdxs}
 }
 
-// remember that values in compressed range indicate end idx of a range
-// if compressedRanges[1] =  4
-// it means, it covers until idx 4 from larger/expanded array
-// but excluding idx 4
-// therefore if r is < 4 (ex: 3), then yes this is potential ans, save it
-// compressedRanges[1] = 2 ; ( meaning i cover up to idx 2 but not inculding 2)
-// and if r = 3 or if r = 2; we are not in the range, therefore look right
+
 func (this *Solution) PickIndex() int {
-    n := len(this.compressedRanges)
-    total := this.compressedRanges[n-1]
-    r := rand.Intn(total)
-    left := 0; right := n-1; ans := -1
+    n := len(this.cmpIdxs)
+    end := this.cmpIdxs[n-1]
+    r := rand.Intn(end)
+    left := 0
+    right := n-1
+    res := -1
     for left <= right {
         mid := left + (right-left)/2
-        if r < this.compressedRanges[mid] {
-            ans = mid
-            right = mid-1
-        } else {
+        if this.cmpIdxs[mid] <= r {
             left = mid+1
+        } else {
+            res = mid
+            right = mid-1
         }
-    }    
-    return ans
+    }
+    return res
 }
 
 
