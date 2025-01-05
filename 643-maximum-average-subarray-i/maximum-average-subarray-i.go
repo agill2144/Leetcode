b3fa823b0f5d0aa3330d28463085be46e6be64be
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-05 14:19:58 -0800

    Time: 0 ms (100.00%) | Memory: 10 MB (44.60%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.go
@@ -0,0 +1,15 @@
+func findMaxAverage(nums []int, k int) float64 {
+    res := -100001.1
+    left := 0
+    sum := 0
+    for i := 0; i < len(nums); i++ {
+        sum += nums[i]
+        if i-left+1 == k {
+            avg := float64(sum) / float64(k)
+            if avg > res {res = avg}
+            sum -= nums[left]
+            left++
+        }
+    }
+    return res
+}
