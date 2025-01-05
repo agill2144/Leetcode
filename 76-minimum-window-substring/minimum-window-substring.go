1b360d1dc64dda4532a5cd82a08dfb01b067bcb6
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-04 19:14:09 -0800

    Time: 44 ms (13.80%) | Memory: 10.6 MB (5.03%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.go
+++ 76-minimum-window-substring/minimum-window-substring.go
@@ -1,29 +1,41 @@
 func minWindow(s string, t string) string {
     tFreq := map[byte]int{}
     for i := 0; i < len(t); i++ {tFreq[t[i]]++}
-    fullMatch := 0
+    type char struct {
+        c byte
+        idx int
+    }
+    filteredS := []*char{}
+    for i := 0; i < len(s); i++ {
+        if _, ok := tFreq[s[i]]; ok {
+            filteredS = append(filteredS, &char{s[i], i})
+        }
+    }
     left := 0
     start, end := -1,-1
-    for i := 0; i < len(s); i++ {
-        _, ok := tFreq[s[i]]
-        if ok {
-            tFreq[s[i]]--
-            if tFreq[s[i]] == 0 {fullMatch++}
+    fullMatch := 0
+    for i := 0; i < len(filteredS); i++ {
+        c,idx := filteredS[i].c, filteredS[i].idx
+        tFreq[c]--
+        if tFreq[c] == 0 {
+            fullMatch++
         }
         for fullMatch == len(tFreq) {
-            if start == -1 || i-left+1 < end-start+1 {
-                start = left
-                end = i
+            leftChar := filteredS[left].c
+            leftIdx := filteredS[left].idx
+            if start == -1 || idx-leftIdx+1 < end-start+1 {
+                start = leftIdx
+                end = idx
             }
-            if _, ok := tFreq[s[left]]; ok {
-                tFreq[s[left]]++
-                if tFreq[s[left]] ==  1 {fullMatch--}
+            if _, ok := tFreq[leftChar]; ok {
+                tFreq[leftChar]++
+                if tFreq[leftChar] == 1 {
+                    fullMatch--
+                }
             }
             left++
         }
     }
-    if start == -1 {
-        return ""
-    }
+    if start == -1 {return ""}
     return s[start:end+1]
 }
