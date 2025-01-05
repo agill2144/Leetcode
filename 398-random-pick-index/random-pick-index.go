ffbf1d08dc6bdf0acfc5254d50778104cb082148
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-04 18:58:49 -0800

    Time: 1655 ms (5.13%) | Memory: 11.9 MB (24.36%) - LeetSync

--- 398-random-pick-index/random-pick-index.go
+++ 398-random-pick-index/random-pick-index.go
@@ -1,25 +1,25 @@
 type Solution struct {
-    nums []int
+    nums []int    
 }
 
 
 func Constructor(nums []int) Solution {
-    return Solution{nums}
+    return Solution{nums}    
 }
 
 
 func (this *Solution) Pick(target int) int {
-    idx := -1
     count := 0
+    res := -1
     for i := 0; i < len(this.nums); i++ {
         if this.nums[i] == target {
             count++
             if rand.IntN(count) == 0 {
-                idx = i
+                res = i
             }
         }
     }
-    return idx
+    return res
 }
 
 
