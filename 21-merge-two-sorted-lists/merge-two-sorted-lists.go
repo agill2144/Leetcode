fcdd493496a6d198a5381581e2d38962cf30c99c
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-04 20:34:18 -0800

    Time: 0 ms (100.00%) | Memory: 4.3 MB (63.06%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.go
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.go
@@ -5,26 +5,23 @@
  *     Next *ListNode
  * }
  */
-func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-dummy := &ListNode{Val: -1}
-	tail := dummy
-	for l1 != nil || l2 != nil {
-		l1Val := math.MaxInt64
-		l2Val := math.MaxInt64
-		if l1 != nil {
-			l1Val = l1.Val
-		}
-		if l2 != nil {
-			l2Val = l2.Val
-		}
-		if l1Val <= l2Val {
-			tail.Next = l1
-			tail = tail.Next
-			l1 = l1.Next
-		} else {
-			tail.Next = l2
-			tail = tail.Next
-			l2 = l2.Next
-		}
-	}
-	return dummy.Next}
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+    dummy := &ListNode{Val: 0}
+    tail := dummy
+    l1, l2 := list1, list2
+    for l1 != nil || l2 != nil {
+        l1Val := math.MaxInt64
+        l2Val := math.MaxInt64
+        if l1 != nil {l1Val = l1.Val}
+        if l2 != nil {l2Val = l2.Val}
+        if l1Val < l2Val {
+            tail.Next = l1
+            l1 = l1.Next
+        } else {
+            tail.Next = l2
+            l2 = l2.Next
+        }
+        tail = tail.Next
+    }
+    return dummy.Next
+}
