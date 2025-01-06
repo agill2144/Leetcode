f4e32010f7f3677ec112a5bb54209b0bc122aabf
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-05 23:36:48 -0800

    Time: 0 ms (100.00%) | Memory: 9.1 MB (17.24%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.go
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.go
@@ -13,27 +13,20 @@
  *     Right *TreeNode
  * }
  */
- // run dfs from each root node of the tree
 func isSubPath(head *ListNode, root *TreeNode) bool {
     if head == nil && root == nil {return true}
     if head == nil || root == nil {return false}
-    var dfs func(curr *ListNode, r *TreeNode) bool
-    dfs = func(curr *ListNode, r *TreeNode) bool {
+    var dfs func(r *TreeNode, curr *ListNode) bool
+    dfs = func(r *TreeNode, curr *ListNode) bool {
         // base
         if curr == nil {return true}
         if r == nil {return false}
 
         // logic
-        // ll matches r
-        if curr.Val == r.Val {
-            return dfs(curr.Next, r.Left) || dfs(curr.Next, r.Right)
+        if curr.Val == r.Val  {
+            return dfs(r.Left, curr.Next) || dfs(r.Right, curr.Next)
         }
         return false
     }
-    // start at root of tree or at child1 or at child2 or at child3 ... or at childN
-    // so if the match is sitting somewhere closer to the bottom of the tree
-    // lets say, 10 level deep, then we ran dfs on each of those childs ( 1-9 )
-    // then at the 10th child dfs returned true
-    // so we ran dfs on *each* child in worst case :) uff
-    return dfs(head, root) || isSubPath(head, root.Left) || isSubPath(head, root.Right)
+    return dfs(root, head) || isSubPath(head, root.Left) || isSubPath(head, root.Right)
 }
