4d7bde6a6a703e183476a98889d5773135957742
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-06 19:18:10 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (68.04%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
@@ -14,29 +14,58 @@
  * }
  */
 func sortedListToBST(head *ListNode) *TreeNode {
-    var dfs func(curr *ListNode) *TreeNode
-    dfs = func(curr *ListNode) *TreeNode {
+    n := size(head)
+    var dfs func(left, right int) *TreeNode
+    dfs = func(left, right int) *TreeNode {
         // base
-        if curr == nil {return nil}
-        if curr.Next == nil {
-            return &TreeNode{Val: curr.Val}
-        }
-
+        if left > right {return nil}
         // logic
-        var prev *ListNode
-        slow := curr
-        fast := curr
-        for fast != nil && fast.Next != nil {
-            prev = slow
-            slow = slow.Next
-            fast = fast.Next.Next
+        mid := left+(right-left)/2
+        leftNode := dfs(left, mid-1)
+        if head != nil {
+            root := &TreeNode{Val: head.Val}
+            head = head.Next
+            root.Left = leftNode
+            root.Right = dfs(mid+1, right)
+            return root
         }
-        prev.Next = nil
-        root := &TreeNode{Val: slow.Val}
-        root.Left = dfs(curr)
-        root.Right = dfs(slow.Next)
-        return root
+        return nil
     }
-    return dfs(head)
-
+    return dfs(0, n-1)
 }
+
+func size(head *ListNode) int {
+    n := 0
+    curr := head
+    for curr != nil {
+        n++
+        curr = curr.Next
+    }
+    return n
+}
+// func sortedListToBST(head *ListNode) *TreeNode {
+//     var dfs func(curr *ListNode) *TreeNode
+//     dfs = func(curr *ListNode) *TreeNode {
+//         // base
+//         if curr == nil {return nil}
+//         if curr.Next == nil {
+//             return &TreeNode{Val: curr.Val}
+//         }
+
+//         // logic
+//         var prev *ListNode
+//         slow := curr
+//         fast := curr
+//         for fast != nil && fast.Next != nil {
+//             prev = slow
+//             slow = slow.Next
+//             fast = fast.Next.Next
+//         }
+//         prev.Next = nil
+//         root := &TreeNode{Val: slow.Val}
+//         root.Left = dfs(curr)
+//         root.Right = dfs(slow.Next)
+//         return root
+//     }
+//     return dfs(head)
+// }
