b4931b6abb203213b2392f329188661595d7cacf
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-06 18:18:48 -0800

    Time: 0 ms (100.00%) | Memory: 7.1 MB (28.13%) - LeetSync

--- 545-boundary-of-binary-tree/boundary-of-binary-tree.go
+++ 545-boundary-of-binary-tree/boundary-of-binary-tree.go
@@ -43,68 +43,56 @@ approach: top-down / preorder recursion, 2 passes
 - reucrse right, with right child's ID ( parent will identify this value and pass down the value to the recursion )
 */
 
-/*
-    0 = root
-    1 = left
-    2 = right
-    3 = middle
-*/
-
 func boundaryOfBinaryTree(root *TreeNode) []int {
-    if root == nil {return nil}
-    left := []int{}
-    leaves := []int{}
-    right := []int{}
-    var dfs func(r *TreeNode, id int)
-    dfs = func(r *TreeNode, id int) {
+    out := []int{root.Val}
+    getLeftNodes := func(r *TreeNode) {
+        for r != nil {
+            if r.Left == nil && r.Right == nil {return}
+            out = append(out, r.Val)
+            if r.Left != nil {
+                r = r.Left
+            } else {
+                r = r.Right
+            }
+        }
+    }
+    getLeftNodes(root.Left)
+    var leaves func(r *TreeNode)
+    leaves = func(r *TreeNode) {
         // base
         if r == nil {return}
-        
+
         // logic
-        if r.Left == nil && r.Right == nil {
-            leaves = append(leaves, r.Val)
-        } else if id == 0 || id == 1 {
-            left = append(left, r.Val)
-        } else if id == 2 {
-            right = append(right, r.Val)
+        if r != root && r.Left == nil && r.Right == nil {
+            out = append(out, r.Val)
+            return
         }
-        dfs(r.Left, leftID(r,id))
-        dfs(r.Right, rightID(r,id))
+        leaves(r.Left)
+        leaves(r.Right)
     }
-    dfs(root, 0)
-    left = append(left, leaves...)
-    for i := len(right)-1; i >= 0; i-- {
-        left = append(left, right[i])
+    leaves(root)
+
+    start := len(out)
+    getRightNodes := func(r *TreeNode) {
+        for r != nil {
+            if r.Left == nil && r.Right == nil {return}
+            out = append(out, r.Val)
+            if r.Right != nil {
+                r = r.Right
+            } else {
+                r = r.Left
+            }
+        }
     }
-    return left
+    getRightNodes(root.Right)
+    end := len(out)-1
+    for start < end {
+        out[start], out[end] = out[end], out[start]
+        start++
+        end-- 
+    }
+
+    return out
 }
 
 
-
-func leftID(r *TreeNode, currID int) int {
-    if currID == 0 || currID == 1 {
-        // root's left will always be a left boundary node
-        // or 
-        // an current left boundary node's left will always be a left node!
-        return 1
-    } else if currID == 2 {
-        if r.Right == nil {
-            return 2
-        }
-    }
-    return 3
-}
-
-func rightID(r *TreeNode, currID int) int {
-    if currID == 0 || currID == 2{
-        // root's right will always be a right boundary node
-        // or 
-        // an current right boundary node's right will always be a right node!
-        return 2
-    } else if currID == 1 {
-        if r.Left == nil {
-            return 1
-        }
-    }
-    return 3
-} 
