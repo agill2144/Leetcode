37c55c048ed61cab9544df1d251d5e51156c0c4f
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-06 18:41:56 -0800

    Time: 2 ms (60.13%) | Memory: 3.9 MB (81.88%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.go
+++ 1894-merge-strings-alternately/merge-strings-alternately.go
@@ -1,8 +1,12 @@
 func mergeAlternately(word1 string, word2 string) string {
-    out := new(strings.Builder)
-    for i := 0; i < max(len(word1), len(word2)); i++ {
-        if i < len(word1) {out.WriteByte(word1[i])}
-        if i < len(word2) {out.WriteByte(word2[i])}
+    res := new(strings.Builder)
+    for i := 0; i < max(len(word1),len(word2)); i++ {
+        if i < len(word1) {
+            res.WriteByte(word1[i])
+        }
+        if i < len(word2) {
+            res.WriteByte(word2[i])
+        }
     }
-    return out.String()
+    return res.String()
 }
