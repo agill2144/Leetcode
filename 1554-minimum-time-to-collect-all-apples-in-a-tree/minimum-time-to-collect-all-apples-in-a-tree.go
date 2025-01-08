28fc725c46980d48280943aaa09aa3efc3c6f184
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-07 22:55:11 -0800

    Time: 38 ms (25.00%) | Memory: 22.1 MB (40.00%) - LeetSync

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.go
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.go
@@ -1,30 +1,25 @@
 func minTime(n int, edges [][]int, hasApple []bool) int {
     adjList := map[int][]int{}
     for i := 0; i < len(edges); i++ {
-        u,v := edges[i][0], edges[i][1]
-        adjList[u] = append(adjList[u], v)
-        adjList[v] = append(adjList[v], u)
+        a, b := edges[i][0], edges[i][1]
+        adjList[a] = append(adjList[a], b)
+        adjList[b] = append(adjList[b], a)
     }
-    var dfs func(node, prev int) (int)
-    dfs = func(node, prev int) (int) {
+    var dfs func(node, prev int) int
+    dfs = func(node, prev int) int {
         // base
 
         // logic
         total := 0
         for _, nei := range adjList[node] {
             if nei == prev {continue}
-            childTime := dfs(nei, node)
-            // we are standing at a parent and looking down
-            // curr node is a parent node
-            // and we are back from child node recursion
-            // now we ask, child has returned any time? ( meaning child had apples underneath itself )
-            // or the child itself has apples
-            // meaning a node does not check itself, it only looks down at its child and asks the question
-            if childTime != 0 || hasApple[nei] {
-                total += childTime + 2
+            cCount := dfs(nei, node)
+            if cCount > 0  || hasApple[nei] {
+                total += cCount + 2
             }
         }
         return total
     }
     return dfs(0,-1)
+    
 }
