43afee798bb3e3daef1cd5635cd1ea4cc1d10b6a
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-07 22:43:38 -0800

    Time: 0 ms (100.00%) | Memory: 4.3 MB (40.00%) - LeetSync

--- 851-goat-latin/goat-latin.go
+++ 851-goat-latin/goat-latin.go
@@ -1,26 +1,14 @@
 func toGoatLatin(sentence string) string {
-    /*
-        vowel
-        - append ma
-        - append a's suffix based on idx
-
-        consonant
-        - take substr; word[1:]
-        - add the 0th char to the end
-        - add ma
-        - append a's suffix based on idx
-    */
+    words := strings.Split(sentence, " ")
     res := new(strings.Builder)
     suffix := "a"
-    words := strings.Split(sentence, " ")
     for i := 0; i < len(words); i++ {
-        word := words[i]
-        if isVowel(word) {
-            res.WriteString(word)
+        if isVowel(words[i]) {
+            res.WriteString(words[i])
             res.WriteString("ma")
         } else {
-            res.WriteString(word[1:])
-            res.WriteByte(word[0])
+            res.WriteString(words[i][1:])
+            res.WriteByte(words[i][0])
             res.WriteString("ma")
         }
         res.WriteString(suffix)
@@ -28,8 +16,8 @@ func toGoatLatin(sentence string) string {
         if i != len(words)-1 {res.WriteString(" ")}
     }
     return res.String()
-}
 
+}
 func isVowel(s string) bool {
     set := map[byte]bool{
         'a':true,'e':true,'i':true,'o':true,'u':true,
