e02334626648a1468c4efccc932dbdf1e817ae61
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-08 19:34:03 -0800

    Time: 0 ms (100.00%) | Memory: 8.7 MB (27.71%) - LeetSync

--- 169-majority-element/majority-element.go
+++ 169-majority-element/majority-element.go
@@ -11,5 +11,8 @@ func majorityElement(nums []int) int {
             count--
         }
     }
-    return ele
+    count = 0
+    for i := 0; i < len(nums); i++ {if nums[i] == ele{count++}}
+    if count > len(nums)/2 {return ele}
+    return -1
 }
