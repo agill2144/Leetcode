874f78afbc3674b9b0311bdc508352aad94ca623
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-08 19:29:12 -0800

    Time: 1 ms (91.15%) | Memory: 10.8 MB (52.65%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.go
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.go
@@ -6,21 +6,6 @@
  *     Right *TreeNode
  * }
  */
-/*
-    approach: store snapshot in a list
-    - traverse the BST in constructor
-    - and store values in a list
-    - this is wrong because it does not handle changes to bst nodes as much as possible
-    - iterators are supposed to handle dynamic changes :) 
-
-    approach: controlled recursion
-    - put all left nodes in a stack
-    - such that top of the stack *ALWAYS* represents the next node in inorder traversal
-    - when a node is popped from stack, go to its right and store all the left childs again
-    - this way we have only stored left nodes
-    - allowing right nodes to change overtime as needed
-    - it does not handle FULL dynamic ability, but its tries to as much as possible
-*/
 type BSTIterator struct {
     st []*TreeNode
 }
@@ -28,27 +13,28 @@ type BSTIterator struct {
 
 func Constructor(root *TreeNode) BSTIterator {
     out := BSTIterator{st: []*TreeNode{}}
-    out.pushLeftChilds(root)
-    return out    
+    out.pushNodes(root)
+    return out
 }
 
-func (this *BSTIterator) pushLeftChilds(r *TreeNode) {
+func (this *BSTIterator) pushNodes(r *TreeNode) {
     for r != nil {
         this.st = append(this.st, r)
         r = r.Left
     }
 }
 
+
 func (this *BSTIterator) Next() int {
     top := this.st[len(this.st)-1]
     this.st = this.st[:len(this.st)-1]
-    this.pushLeftChilds(top.Right)
+    this.pushNodes(top.Right)
     return top.Val
 }
 
 
 func (this *BSTIterator) HasNext() bool {
-    return len(this.st) > 0    
+   return len(this.st) > 0 
 }
 
 
