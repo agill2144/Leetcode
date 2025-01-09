ca1dd5ec110d02a361ee34197488e32de99e5b36
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-08 22:48:16 -0800

    Time: 0 ms (100.00%) | Memory: 6.1 MB (54.92%) - LeetSync

--- 57-insert-interval/insert-interval.go
+++ 57-insert-interval/insert-interval.go
@@ -1,69 +1,31 @@
 /*
-    approach: 
-    - search, insert, merge
-    - given sorted intervals, use binary search to find the correct insert idx position
-        - "find right most val on left side of target"
-    - create a tmp array, and insert newInterval as well as old ones
-        - there may be overalap
-    - now finally, merge overlapping intervals
-        - overlapping in this question is; start <= prevEnd time
-    
-    time;
-    - search = o(logn)
-    - insert = o(n)
-    - merge = o(n)
-    total time = o(logn) + o(2n)
-
-    space = o(n) for tmp array
+    approach: brute force
+    - append
+    - sort
+    - merge overlapping
 */
 func insert(intervals [][]int, newInterval []int) [][]int {
-    if len(intervals) == 0 {return [][]int{newInterval}}
-    idx := search(intervals, newInterval[0])
-    // space = o(n)
-    tmp := [][]int{}
-    tmp = append(tmp, intervals[:idx]...)
-    tmp = append(tmp, newInterval)
-    tmp = append(tmp, intervals[idx:]...)
+    if newInterval == nil || len(newInterval) == 0 {return intervals}
+    
+    // append
+    intervals = append(intervals, newInterval)
+    
+    // sort
+    sort.Slice(intervals, func(i, j int)bool{
+        return intervals[i][0] < intervals[j][0]
+    })
 
-    // space = o(n) but will be o(1) if output array is not counted
-    // some people count it, some dont, i dont
-    // therefore o(1) space for this merged array
-    merged := [][]int{tmp[0]}
-    for i := 1; i < len(tmp); i++ {
-        start, end := tmp[i][0], tmp[i][1]
-        prevStart, prevEnd := merged[len(merged)-1][0],merged[len(merged)-1][1]
+    // merge overlapping
+    out := [][]int{intervals[0]}
+    for i := 1; i < len(intervals); i++ {
+        start, end := intervals[i][0], intervals[i][1]
+        prevEnd := out[len(out)-1][1]
         if start <= prevEnd {
-            merged[len(merged)-1][0] = min(start, prevStart)
-            merged[len(merged)-1][1] = max(end, prevEnd)
+            out[len(out)-1][0] = min(start, out[len(out)-1][0])
+            out[len(out)-1][1] = max(end, out[len(out)-1][1])
         } else {
-            merged = append(merged, tmp[i])
+            out = append(out, intervals[i])
         }
     }
-    return merged
-
-}
-
-
-// time = o(logn)
-// space = o(1)
-func search(intervals [][]int, target int) int {
-    if target < intervals[0][0] {return 0}
-    if target > intervals[len(intervals)-1][0] {return len(intervals)}
-    left := 0
-    right := len(intervals)-1
-    // find right most val on left side of target
-    ans := 0
-    for left <= right {
-        mid := left+(right-left)/2
-        val := intervals[mid][0]
-        if val <= target {
-            ans = mid
-            left = mid+1 // since we want the right most value on left side of target
-        } else {
-            right = mid-1
-        }
-    }
-    // why +1 ?
-    // because this function is supposed to return the insert idx position
-    return ans+1
+    return out
 }
