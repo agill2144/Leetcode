80cd36613d1ae24904e7e985f8bfb114c96b60bd
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-08 19:39:29 -0800

    Time: 9 ms (32.08%) | Memory: 4.8 MB (72.03%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -1,12 +1,30 @@
 func lengthOfLongestSubstring(s string) int {
     res := 0
+    if len(s) == 0 {return res}
+    freq := map[byte]int{}
+    left := 0
     for i := 0; i < len(s); i++ {
-        set := make([]bool, 256)
-        for j := i ; j < len(s); j++ {
-            if set[int(s[j])] {break}
-            set[int(s[j])] = true
-            res = max(res, j-i+1)            
+        freq[s[i]]++
+        for left <= i && freq[s[i]] > 1 {
+            freq[s[left]]--
+            if freq[s[left]] == 0 {delete(freq, s[left])}
+            left++            
         }
+        res = max(res, i-left+1)
     }
     return res
 }
+
+// LOL brute force getting accepted
+// func lengthOfLongestSubstring(s string) int {
+//     res := 0
+//     for i := 0; i < len(s); i++ {
+//         set := make([]bool, 256)
+//         for j := i ; j < len(s); j++ {
+//             if set[int(s[j])] {break}
+//             set[int(s[j])] = true
+//             res = max(res, j-i+1)            
+//         }
+//     }
+//     return res
+// }
