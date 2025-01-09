74945eb66271f5fbab722bd6798838d28ccad9ef
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-08 19:32:17 -0800

    Time: 2 ms (100.00%) | Memory: 5.8 MB (93.94%) - LeetSync

--- 865-robot-room-cleaner/robot-room-cleaner.go
+++ 865-robot-room-cleaner/robot-room-cleaner.go
@@ -18,30 +18,30 @@
  */
 
 func cleanRoom(robot *Robot) {
-    dirs := [][]int{{-1,0},{0,1},{1,0},{0,-1}}
     visited := map[[2]int]bool{}
-    var dfs func(r, c int, currDir int)
-    dfs = func(r, c int, currDir int) {
+    dirs := [][]int{{-1,0},{0,1},{1,0},{0,-1}}
+    var dfs func(r, c, currDir int)
+    dfs = func(r, c, currDir int) {
         // base
-        if visited[[2]int{r,c}] {return}
 
         // logic
         visited[[2]int{r,c}] = true
         robot.Clean()
         for i := 0; i < 4; i++ {
-            nr := r + dirs[currDir][0]
-            nc := c + dirs[currDir][1]
+            nr := r+dirs[currDir][0]
+            nc := c+dirs[currDir][1]
             if !visited[[2]int{nr,nc}] && robot.Move() {
-                dfs(nr, nc, currDir)
+                dfs(nr,nc, currDir)
                 robot.TurnRight()
                 robot.TurnRight()
                 robot.Move()
                 robot.TurnRight()
-                robot.TurnRight()                
+                robot.TurnRight()
             }
             robot.TurnRight()
-            currDir = (currDir + 1)%4
+            currDir = (currDir+1) % 4
         }
     }
     dfs(0,0,0)
+    
 }
