25a6cec9c09c929ebcf0c89c986d1e5d6e993252
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-08 18:34:58 -0800

    Time: 18 ms (84.85%) | Memory: 9.3 MB (93.22%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
@@ -1,18 +1,4 @@
 func findKthLargest(nums []int, k int) int {
-    n := len(nums)
-    start := math.MaxInt64
-    end := math.MinInt64
-    bucket := map[int]int{}
-    for i := 0; i < n; i++ {
-        start = min(start, nums[i])
-        end = max(end, nums[i])
-        bucket[nums[i]]++
-    }
-    idx := -1
-    for i := start; i <= end; i++ {
-        if bucket[i] == 0 {continue}
-        idx += bucket[i]
-        if idx >= n-k {return i}
-    }
-    return -1
+    sort.Ints(nums)
+    return nums[len(nums)-k]
 }
