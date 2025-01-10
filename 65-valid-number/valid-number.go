04cf4ffa350929edb93f9b1054920dcb1d6a59b7
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-10 13:47:39 -0800

    Time: 1 ms (30.09%) | Memory: 4.2 MB (40.71%) - LeetSync

--- 65-valid-number/valid-number.go
+++ 65-valid-number/valid-number.go
@@ -1,24 +1,24 @@
 func isNumber(s string) bool {
-    seenDigit := false
-    seenDecimal := false
     seenExp := false
+    seenDec := false
+    seenDig := false
     start := 0
-    n := len(s)
     if s[start] == '+' || s[start] == '-' {start++}
-    for i := start; i < len(s); i++ {
-        if s[i] == '+' || s[i] == '-' {
-            if i == n-1 || (s[i-1] != 'e' && s[i-1] != 'E') {return false}             
-        } else if s[i] == 'e' || s[i] == 'E' {
-            if !seenDigit || seenExp || i == n-1 {return false}
-            seenExp = true
+    n := len(s)
+    for i := start; i < n; i++ {
+        if s[i] >= '0' && s[i] <= '9' {
+            seenDig = true
         } else if s[i] == '.' {
-            if seenExp || seenDecimal {return false}
-            seenDecimal = true
-        } else if s[i] >= '0' && s[i] <= '9' {
-            seenDigit = true
+            if seenExp || seenDec {return false}
+            seenDec = true
+        } else if s[i] == 'e' || s[i] == 'E' {
+            if !seenDig || seenExp || i == n-1 {return false}
+            seenExp = true
+        } else if s[i] == '+' || s[i] == '-' {
+            if i == n-1 || (i-1 >= 0 && (s[i-1] !='e' && s[i-1] != 'E')) {return false}
         } else {
             return false
         }
     }
-    return seenDigit
+    return seenDig
 }
