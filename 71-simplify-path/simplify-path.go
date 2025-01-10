153f853d8e09eb04266b4b9ae7084b49137e7b5b
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-10 13:54:04 -0800

    Time: 0 ms (100.00%) | Memory: 5.1 MB (51.13%) - LeetSync

--- 71-simplify-path/simplify-path.go
+++ 71-simplify-path/simplify-path.go
@@ -2,22 +2,18 @@ func simplifyPath(path string) string {
     dirs := strings.Split(path, "/")
     st := []string{}
     for i := 0; i < len(dirs); i++ {
-        if dirs[i] == "." || dirs[i] == "" || dirs[i] == "/" {continue}
         if dirs[i] == ".." {
-            if len(st) != 0 {
-                st = st[:len(st)-1]
-            }    
-        } else  {
-            st = append(st, dirs[i])
+            if len(st) != 0 {st = st[:len(st)-1]}
+            continue
         }
+        if dirs[i] == "." || dirs[i] == "" {continue}
+        st = append(st, dirs[i])
     }
-    res := new(strings.Builder)
+    res := &strings.Builder{}
     res.WriteString("/")
     for i := 0; i < len(st); i++ {
         res.WriteString(st[i])
-        if i != len(st)-1 {
-            res.WriteString("/")
-        }
+        if i != len(st)-1 {res.WriteString("/")}
     }
     return res.String()
 }
