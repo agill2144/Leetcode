88e169b5747b950988ef9726328cc2db20f2ec86
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-10 17:15:45 -0800

    Time: 56 ms (6.25%) | Memory: 8.4 MB (12.50%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.go
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.go
@@ -2,12 +2,17 @@ func canConstruct(s string, k int) bool {
     if len(s) == k {return true}
     if len(s) < k {return false}
     set := map[byte]bool{}
+    // count 2 size strings
     count := 0
     for i := 0; i < len(s); i++ {
         if set[s[i]] {delete(set, s[i]); count++; continue}
         set[s[i]] = true
     }
-    left := len(set)
-    left -= count
-    return left + count <= k    
+    remaining := len(set)
+    // our count has 2 size strings with idential chars
+    // ex: aa, bb, cc
+    // therefore each of those 2 size strings can take 1 more char out of remaining
+    // hence the true remaining is whatever is left after taking $count chars out of set
+    remaining -= count
+    return remaining + count <= k
 }
