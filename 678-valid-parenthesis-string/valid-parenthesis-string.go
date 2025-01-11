f93da14dc22a6fdf856a80c99e5d1eee325267fb
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-11 12:45:44 -0800

    Time: 0 ms (100.00%) | Memory: 3.9 MB (61.90%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.go
+++ 678-valid-parenthesis-string/valid-parenthesis-string.go
@@ -1,32 +1,30 @@
 func checkValidString(s string) bool {
-    asteriks := []int{}
-    opens := []int{}
+    asteriks := []int{} // idxs of asteriks
+    st := []int{} // idxs of open parans
     for i := 0; i < len(s); i++ {
         if s[i] == '(' {
-            opens = append(opens,i)
+            st = append(st, i)
+        } else if s[i] == ')' {
+            if len(st) > 0 {
+                st = st[:len(st)-1]
+                continue
+            }
+            if len(asteriks) > 0 {
+                asteriks = asteriks[:len(asteriks)-1]
+                continue                
+            }
+            return false
         } else if s[i] == '*' {
             asteriks = append(asteriks, i)
-        } else {
-            if len(opens) > 0 {
-                opens = opens[:len(opens)-1]
-            }else if len(asteriks) > 0{
-                // use previously seen asterik as a open paran
-                asteriks = asteriks[:len(asteriks)-1]
-            } else {
-                return false
-
-            }
         }
     }
-    for len(opens) != 0 && len(asteriks) != 0 {
-        // we only have opens left, therefore we need to use asteriks as closing
-        // therefore asteriks SHOULD HAVE CAME AFTER open paran
-        if opens[len(opens)-1] < asteriks[len(asteriks)-1] {
+    for len(st) != 0 && len(asteriks) != 0 {
+        if asteriks[len(asteriks)-1] > st[len(st)-1] {
             asteriks = asteriks[:len(asteriks)-1]
-            opens = opens[:len(opens)-1]
+            st = st[:len(st)-1]
             continue
         }
         break
     }
-    return len(opens) == 0 
+    return len(st) == 0
 }
