0fd58308532c48d5ee9e23dc8103b00dae792e08
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-10 17:31:40 -0800

    Time: 0 ms (100.00%) | Memory: 8 MB (62.50%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.go
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.go
@@ -9,6 +9,16 @@ func canConstruct(s string, k int) bool {
     for i := 0; i < len(freq); i++ {
         if freq[i] % 2 != 0 {oddCount++}
     }
+    // oddCount are num of chars that need to stand on their own
+    // if oddCount = 5, there would be 5 single char palindrome
+    // remove k from oddCount, because we have to form k, 
+    // if k was 3, and oddCount was 5, then have 2 oddCount left that are EXTRA!
+    // therefore cannot construct
+    // oddCount could have been smaller like 1, and k = 3
+    // oddCount -= k ; 1 -= 3 = -2
+    // which is fine, why ?
+    // because the extra palindromes can still be formed 
+    // using even characters, or by splitting characters redundantly into smaller palindromes.
     oddCount -= k
     return oddCount <= 0
 }
