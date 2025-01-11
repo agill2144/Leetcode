babd6aa38cd06dca1a426ddde2c9aae937446667
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-10 17:24:16 -0800

    Time: 0 ms (100.00%) | Memory: 8.3 MB (12.50%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.go
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.go
@@ -1,18 +1,13 @@
 func canConstruct(s string, k int) bool {
     if len(s) == k {return true}
     if len(s) < k {return false}
-    set := map[byte]bool{}
-    // count 2 size strings
-    count := 0
+    freq := make([]int, 26)
     for i := 0; i < len(s); i++ {
-        if set[s[i]] {delete(set, s[i]); count++; continue}
-        set[s[i]] = true
+        freq[int(s[i]-'a')]++
     }
-    remaining := len(set)
-    // our count has 2 size strings with idential chars
-    // ex: aa, bb, cc
-    // therefore each of those 2 size strings can take 1 more char out of remaining
-    // hence the true remaining is whatever is left after taking $count chars out of set
-    remaining -= count
-    return remaining + count <= k
+    oddCount := 0
+    for i := 0; i < len(freq); i++ {
+        if freq[i] % 2 != 0 {oddCount++}
+    }
+    return oddCount <= k
 }
