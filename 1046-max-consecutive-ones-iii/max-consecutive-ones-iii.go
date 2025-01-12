93008c437c6391c8e8219962fd88cdefd27ed272
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-12 14:50:07 -0800

    Time: 0 ms (100.00%) | Memory: 9 MB (36.91%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.go
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.go
@@ -1,15 +1,16 @@
 func longestOnes(nums []int, k int) int {
+    zeros := 0
     maxWin := 0
     left := 0
-    zeroCount := 0
     for i := 0; i < len(nums); i++ {
-        if nums[i] == 0 {zeroCount++}
-        if zeroCount <= k {
-            maxWin = max(maxWin, i-left+1)
-        } else {
-            if nums[left] == 0 {zeroCount--}
+        if nums[i] == 0 {
+            zeros++
+        }
+        for zeros > k {
+            if nums[left] == 0 {zeros--}
             left++
         }
+        maxWin = max(maxWin, i-left+1)
     }
     return maxWin
 }
