fb922f380e78302f2f866262ed704be31b92fd17
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-11 20:53:27 -0800

    Time: 7 ms (91.46%) | Memory: 20.2 MB (6.71%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.go
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.go
@@ -1,14 +1,17 @@
 func getAverages(nums []int, k int) []int {
-    out := make([]int, len(nums))
+    n := len(nums)
     kk := k*2+1
+    out := make([]int, n)
+    for i := 0; i < len(out); i++ {out[i] = -1}
     left := 0
-    rSum := 0
-    for i := 0; i < len(nums); i++ {
-        out[i] = -1
-        rSum += nums[i]
+    sum := 0
+    for i := 0; i < n; i++ {
+        sum += nums[i]
         if i-left+1 == kk {
-            out[i-k] = rSum / kk
-            rSum -= nums[left]
+            idx := i-k
+            avg := sum / kk
+            out[idx] = avg
+            sum -= nums[left]
             left++
         }
     }
