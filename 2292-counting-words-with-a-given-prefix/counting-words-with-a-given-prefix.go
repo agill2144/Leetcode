a68f7f6e5297810dbf5ba05abaaf72d5603d09ba
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-11 22:42:18 -0800

    Time: 14 ms (4.11%) | Memory: 11.2 MB (11.71%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.go
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.go
@@ -1,7 +1,39 @@
 func prefixCount(words []string, pref string) int {
-    count := 0
+    root := newTrieNode()
     for i := 0; i < len(words); i++ {
-        if strings.HasPrefix(words[i], pref) {count++}
+        root.insert(words[i])
     }
-    return count
+    return root.prefixCount(pref)
+}
+
+type trieNode struct {
+    childs [26]*trieNode
+    isEnd bool
+    count int
+}
+
+func newTrieNode() *trieNode {
+    return &trieNode{childs:[26]*trieNode{}}
+}
+
+func (r *trieNode) insert(word string) {
+    curr := r
+    for i := 0; i < len(word); i++ {
+        if curr.childs[int(word[i]-'a')] == nil {
+            curr.childs[int(word[i]-'a')] = newTrieNode()
+        }
+        curr = curr.childs[int(word[i]-'a')]
+        curr.count++
+    }
+    curr.isEnd = true
+}
+
+func (r *trieNode) prefixCount(pref string) int {
+    curr := r
+    for i := 0; i < len(pref); i++ {
+        idx := int(pref[i]-'a')
+        if curr.childs[idx] == nil {return 0}
+        curr = curr.childs[idx]
+    }
+    return curr.count
 }
