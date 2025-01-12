f7f060eb5cc4c38723592bbb305432ebbdb4424b
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-11 20:27:03 -0800

    Time: 0 ms (100.00%) | Memory: 4.3 MB (54.29%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.go
+++ 269-alien-dictionary/alien-dictionary.go
@@ -1,49 +1,49 @@
 func alienOrder(words []string) string {
     adjList := map[byte][]byte{}
     for i := 0; i < len(words); i++ {
-        for k := 0; k < len(words[i]); k++ {
-            adjList[words[i][k]] = []byte{}
+        for j := 0; j < len(words[i]); j++ {
+            adjList[words[i][j]] = []byte{}
         }
     }
-
     for i := 0; i < len(words)-1; i++ {
         parent := words[i]
         child := words[i+1]
-        p, c := 0, 0
+        p, c := 0,0
         for p < len(parent) && c < len(child) {
-            if parent[p] != child[c] {
-                adjList[parent[p]] = append(adjList[parent[p]], child[c])
-                break
-            }
-            p++; c++
+            if parent[p] == child[c] {p++; c++; continue}
+            adjList[parent[p]] = append(adjList[parent[p]], child[c])
+            break
         }
         if c == len(child) && p < len(parent) {return ""}
     }
+    
     st := []byte{}
-    visited := make([]bool, 26)
-    var dfs func(node byte, path []bool) bool
-    dfs = func(node byte, path []bool) bool {
+    visited := make([]bool, 26)    
+    var dfs func(curr byte, path []bool) bool
+    dfs = func(curr byte, path []bool) bool {
         // base
-        if path[int(node-'a')] {return false}
-        if visited[int(node-'a')] {return true}
+        if path[int(curr-'a')] {return false}
+        if visited[int(curr-'a')] {return true}
 
         // logic
-        visited[int(node-'a')] = true
-        path[int(node-'a')] = true
-        for _, nei := range adjList[node] {
+        visited[int(curr-'a')] = true
+        path[int(curr-'a')] = true
+        for _, nei := range adjList[curr] {
             if !dfs(nei, path) {return false}
         }
-        st = append(st, node)
-        path[int(node-'a')] = false
+        path[int(curr-'a')] = false
+        st = append(st, curr)
         return true
     }
     p := make([]bool, 26)
     for k, _ := range adjList {
-        if !dfs(k, p){return ""}
+        if !dfs(k, p) {return ""}
     }
-    res := &strings.Builder{}
-    for i := len(st)-1; i >= 0; i-- {
-        res.WriteByte(st[i])
+    res := new(strings.Builder)
+    for len(st) != 0{
+        top := st[len(st)-1]
+        st = st[:len(st)-1]
+        res.WriteByte(top)
     }
     return res.String()
 }
