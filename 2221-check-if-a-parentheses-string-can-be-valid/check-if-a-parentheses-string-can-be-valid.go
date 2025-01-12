44d411293a40534d4a72b02cb7cbe5abf5e41d88
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-11 18:29:18 -0800

    Time: 13 ms (12.50%) | Memory: 9.5 MB (37.50%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.go
@@ -0,0 +1,28 @@
+func canBeValid(s string, locked string) bool {
+    flexible := []int{}
+    open := []int{}
+    for i := 0; i < len(s); i++ {
+        if locked[i] == '0' {
+            flexible = append(flexible, i)
+        } else if s[i] == '(' {
+            open = append(open, i)
+        } else if s[i] == ')' {
+            if len(open) > 0 {
+                open = open[:len(open)-1]
+            } else if len(flexible) > 0 {
+                flexible = flexible[:len(flexible)-1]
+            } else {
+                return false
+            }
+        }
+    }
+    for len(open) > 0 && len(flexible) > 0 {
+        if flexible[len(flexible)-1] > open[len(open)-1] {
+            flexible = flexible[:len(flexible)-1]
+            open = open[:len(open)-1]
+            continue
+        }
+        break
+    }
+    return len(open) == 0 && len(flexible) % 2 == 0
+}
