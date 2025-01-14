42d6688e9c4b9b12e16fa4a9fcbe61ac330fb2ef
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-13 18:50:47 -0800

    Time: 3 ms (83.33%) | Memory: 13.2 MB (13.64%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.go
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.go
@@ -1,12 +1,31 @@
 func numberOfSubarrays(nums []int, k int) int {
-    freq := map[int]int{0:1}
+    if k == 0 {return 0}
+    return atMostK(nums, k) - atMostK(nums, k-1)
+}
+
+func atMostK(nums []int, k int) int {
+    count := 0
     oddCount := 0
-    total := 0
+    left := 0
     for i := 0; i < len(nums); i++ {
         if nums[i] % 2 != 0 {oddCount++}
-        total += freq[oddCount-k]
-        freq[oddCount]++
+        for left <= i && oddCount > k {
+            if nums[left] % 2 != 0 {oddCount--}
+            left++
+        }
+        count += (i-left+1)
     }
-    return total
-
+    return count
 }
+// func numberOfSubarrays(nums []int, k int) int {
+//     freq := map[int]int{0:1}
+//     oddCount := 0
+//     total := 0
+//     for i := 0; i < len(nums); i++ {
+//         if nums[i] % 2 != 0 {oddCount++}
+//         total += freq[oddCount-k]
+//         freq[oddCount]++
+//     }
+//     return total
+
+// }
