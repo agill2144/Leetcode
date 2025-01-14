75c411990e86659cdce9c1cb38e03829123233fa
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-13 20:49:54 -0800

    Time: 0 ms (100.00%) | Memory: 3.8 MB (62.50%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.go
+++ 678-valid-parenthesis-string/valid-parenthesis-string.go
@@ -1,26 +1,47 @@
 func checkValidString(s string) bool {
-    asterik := []int{}
-    open := []int{}
-    for i := 0; i < len(s); i++ {
-        if s[i] == '*' {
-            asterik = append(asterik, i)
-        } else if s[i] == '(' {
-            open = append(open, i)
+    open := 0
+    close := 0
+    n := len(s)
+    for i := 0; i < n; i++ {
+        if s[i] == '(' || s[i] == '*' {
+            open++
         } else if s[i] == ')' {
-            if len(open) > 0 {
-                open = open[:len(open)-1]
-            } else if len(asterik) > 0 {
-                asterik = asterik[:len(asterik)-1]
-            } else {
-                return false
-            }
+            open--
+            if open < 0 {return false}
+        }
+
+        if s[n-1-i] == ')' || s[n-1-i] == '*' {
+            close++
+        } else if s[n-1-i] == '(' {
+            close--
+            if close < 0 {return false}
         }
     }
-    for len(open) > 0 && len(asterik) > 0 && 
-        asterik[len(asterik)-1] > open[len(open)-1] {
-            open = open[:len(open)-1]
-            asterik = asterik[:len(asterik)-1]
-    }
-    return len(open) == 0
-
+    return true
 }
+// func checkValidString(s string) bool {
+//     asterik := []int{}
+//     open := []int{}
+//     for i := 0; i < len(s); i++ {
+//         if s[i] == '*' {
+//             asterik = append(asterik, i)
+//         } else if s[i] == '(' {
+//             open = append(open, i)
+//         } else if s[i] == ')' {
+//             if len(open) > 0 {
+//                 open = open[:len(open)-1]
+//             } else if len(asterik) > 0 {
+//                 asterik = asterik[:len(asterik)-1]
+//             } else {
+//                 return false
+//             }
+//         }
+//     }
+//     for len(open) > 0 && len(asterik) > 0 && 
+//         asterik[len(asterik)-1] > open[len(open)-1] {
+//             open = open[:len(open)-1]
+//             asterik = asterik[:len(asterik)-1]
+//     }
+//     return len(open) == 0
+
+// }
