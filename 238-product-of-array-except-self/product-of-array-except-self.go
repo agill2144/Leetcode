55515a202eb3d175ae57e697d71f5e7719ede584
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-13 16:41:46 -0800

    Time: 3 ms (54.81%) | Memory: 9.7 MB (80.06%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.go
+++ 238-product-of-array-except-self/product-of-array-except-self.go
@@ -5,14 +5,10 @@ func productExceptSelf(nums []int) []int {
     for i := 1; i < n; i++ {
         leftProd[i] = nums[i-1]*leftProd[i-1]
     }
-    rightProd := make([]int, n)
-    rightProd[n-1] = 1
-    for i := n-2; i >= 0; i-- {
-        rightProd[i] = rightProd[i+1] * nums[i+1]
+    rrp := 1
+    for i := n-1; i >= 0; i-- {
+        leftProd[i] *= rrp
+        rrp *= nums[i]
     }
-    out := make([]int, n)
-    for i := 0; i < n; i++{
-        out[i] = leftProd[i] * rightProd[i]
-    }
-    return out
+    return leftProd
 }
