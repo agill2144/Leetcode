5d08292b34d90a7b1542fe92cd9d491e001d4cdc
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-13 18:46:23 -0800

    Time: 35 ms (48.48%) | Memory: 9.1 MB (83.33%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.go
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.go
@@ -1,35 +1,12 @@
 func numberOfSubarrays(nums []int, k int) int {
-    if k < 1 {return 0}
-    return atMostK(nums, k) - atMostK(nums, k-1)
-}
-
-func atMostK(nums []int, k int) int {
-    n := len(nums)
+    freq := map[int]int{0:1}
+    oddCount := 0
     total := 0
-    left := 0
-    rCount := 0
-    for i := 0; i < n; i++ {
-        if nums[i] % 2 != 0 {rCount++}
-        for rCount > k {
-            if nums[left] % 2 != 0 {rCount--}
-            left++
-        }
-        total += (i-left+1)
+    for i := 0; i < len(nums); i++ {
+        if nums[i] % 2 != 0 {oddCount++}
+        total += freq[oddCount-k]
+        freq[oddCount]++
     }
     return total
-}
 
-// running sum pattern:
-// time = o(n)
-// space = o(n)
-// func numberOfSubarrays(nums []int, k int) int {
-//     freq := map[int]int{0:1}
-//     rCount := 0
-//     total := 0
-//     for i := 0; i < len(nums); i++ {
-//         if nums[i] % 2 != 0 {rCount++}
-//         total += freq[rCount-k]
-//         freq[rCount]++
-//     }
-//     return total
-// }
+}
