4f72086b4375e65d412f0b165dc47e51d0459046
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-14 20:46:38 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (41.24%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.go
+++ 678-valid-parenthesis-string/valid-parenthesis-string.go
@@ -1,49 +1,23 @@
-// bhai... matlab... who tf comes up with this SHIT ???? 
-// FUCK YOU 
 func checkValidString(s string) bool {
-    open := 0
-    close := 0
     n := len(s)
+    if n == 0 {return true}
+    open := 0
     for i := 0; i < n; i++ {
         if s[i] == '(' || s[i] == '*' {
             open++
-        } else if s[i] == ')' {
+        } else {
             open--
             if open < 0 {return false}
         }
-
-        if s[n-1-i] == ')' || s[n-1-i] == '*' {
+    }
+    close := 0
+    for i := n-1; i >= 0; i-- {
+        if s[i] == ')' || s[i] == '*' {
             close++
-        } else if s[n-1-i] == '(' {
+        } else {
             close--
             if close < 0 {return false}
         }
     }
     return true
 }
-// func checkValidString(s string) bool {
-//     asterik := []int{}
-//     open := []int{}
-//     for i := 0; i < len(s); i++ {
-//         if s[i] == '*' {
-//             asterik = append(asterik, i)
-//         } else if s[i] == '(' {
-//             open = append(open, i)
-//         } else if s[i] == ')' {
-//             if len(open) > 0 {
-//                 open = open[:len(open)-1]
-//             } else if len(asterik) > 0 {
-//                 asterik = asterik[:len(asterik)-1]
-//             } else {
-//                 return false
-//             }
-//         }
-//     }
-//     for len(open) > 0 && len(asterik) > 0 && 
-//         asterik[len(asterik)-1] > open[len(open)-1] {
-//             open = open[:len(open)-1]
-//             asterik = asterik[:len(asterik)-1]
-//     }
-//     return len(open) == 0
-
-// }
