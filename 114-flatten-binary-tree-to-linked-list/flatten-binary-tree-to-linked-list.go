8b4b8f4fd92273ac110980f3c0d55330291fbef2
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-14 19:09:26 -0800

    Time: 0 ms (100.00%) | Memory: 4.9 MB (15.19%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.go
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.go
@@ -6,49 +6,25 @@
  *     Right *TreeNode
  * }
  */
-
-func flatten(root *TreeNode) {
-    if root == nil {return}
-    curr := root
-    for curr != nil {
-        if curr.Left == nil {
-            curr = curr.Right
-        } else {
-            // find the right-tail of left child
-            tail := curr.Left
-            for tail.Right != nil {
-                tail = tail.Right
-            }
-            // connect tail to curr right
-            tail.Right = curr.Right
-            // bring curr's left child to its right side
-            curr.Right = curr.Left
-            curr.Left = nil
-            curr = curr.Right
+func flatten(root *TreeNode)  {
+    var dfs func(r *TreeNode) *TreeNode
+    dfs = func(r *TreeNode) *TreeNode {
+        // base
+        if r == nil {return nil}        
+        // logic
+        rt := dfs(r.Right)
+        lt := dfs(r.Left)
+        rh := r.Right
+        lh := r.Left
+        if lh != nil {
+            r.Right = lh
+            r.Left = nil
+            lt.Right = rh
         }
+        if rt != nil {return rt}
+        if lt != nil {return lt}
+        return r
+        
     }
+    dfs(root)
 }
-
-// func flatten(root *TreeNode)  {
-//     var dfs func(r *TreeNode) *TreeNode
-//     dfs = func(r *TreeNode) *TreeNode {
-//         // base
-//         if r == nil {return nil}
-
-//         // logic
-//         lt := dfs(r.Left)
-//         rt := dfs(r.Right)
-//         lh := r.Left
-//         rh := r.Right
-//         if lh != nil {
-//             r.Right = lh
-//             lt.Right = rh
-//             r.Left = nil
-//         }
-//         if rt != nil {return rt}
-//         if lt != nil {return lt}
-//         return r
-//     }
-//     dfs(root)
-
-// }
