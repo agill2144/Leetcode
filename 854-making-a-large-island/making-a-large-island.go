6d9e577f0816245e4bcd4dabc2b541b16bc9e5f7
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-15 17:44:57 -0800

    Time: 129 ms (50.00%) | Memory: 12.9 MB (56.25%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.go
+++ 854-making-a-large-island/making-a-large-island.go
@@ -1,101 +1,56 @@
 func largestIsland(grid [][]int) int {
+    dirs := [][]int{
+        {-1,0},
+        {1,0},
+        {0,1},
+        {0,-1},
+    }
     n := len(grid)
-    dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
+    idToSize := map[int]int{}
     var dfs func(r, c, id int) int
     dfs = func(r, c, id int) int {
         // base
         if r < 0 || r == n || c < 0 || c == n || grid[r][c] != 1 {return 0}
 
         // logic
+        size := 1
         grid[r][c] = id
-        count := 1
         for _, dir := range dirs {
-            count += dfs(r+dir[0], c+dir[1], id)
+            size += dfs(r+dir[0],c+dir[1], id)
         }
-        return count
+        return size
     }
-
-    //1. pre-process connected components
-    // find connected lands and group them with an id
-    // each dfs on a land will return back its area
-    // save this area in hashmap {id: area}
-    // ** also mutate / update each cell with its id number **
     id := 2
-    idToArea := map[int]int{}
     for i := 0; i < n; i++ {
         for j := 0; j < n; j++ {
             if grid[i][j] == 1 {
-                idToArea[id] = dfs(i, j, id)
+                idToSize[id] = dfs(i,j,id)
                 id++
             }
         }
     }
-    maxArea := 0    
-    //2. for each 0 cell, find neighboring islands
-    // each cell value is updated to be an id instead of 1 and 0
+    res := 0
+    haveZero := false
     for i := 0; i < n; i++ {
         for j := 0; j < n; j++ {
             if grid[i][j] == 0 {
+                haveZero = true
                 uniqIDs := map[int]bool{}
                 for _, dir := range dirs {
                     nr := i+dir[0]
                     nc := j+dir[1]
-                    if nr < 0 || nr == n || nc < 0 || nc == n  {continue}
-                    if _, ok := idToArea[grid[nr][nc]]; ok {
+                    if nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] != 0 {
                         uniqIDs[grid[nr][nc]] = true
                     }
-                    total := 1
-                    for uid, _ := range uniqIDs {
-                        total += idToArea[uid]
-                    }
-                    maxArea = max(maxArea, total)
                 }
+                totalSize := 1
+                for id, _ := range uniqIDs {
+                    totalSize += idToSize[id]
+                }
+                res = max(res, totalSize)
             }
         }
     }
-
-    if maxArea == 0 {maxArea = n*n}
-    return maxArea
+    if !haveZero {res = n*n}
+    return res
 }
-
-// brute force
-// for every 0 cell, explore all islands around it
-// tc = o(n^4)
-// sc = o(n^2) + o(n^2)
-// func largestIsland(grid [][]int) int {
-//     n := len(grid)
-//     dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
-//     maxArea := 0
-//     var dfs func(r, c int, visited [][]bool) int
-//     dfs = func(r, c int, visited [][]bool) int{
-//         // base
-//         if r < 0 || r == n || c < 0 || c == n || visited[r][c] || grid[r][c] != 1 {return 0}
-
-//         // logic
-//         visited[r][c] = true
-//         count := 1
-//         for _, dir := range dirs {
-//             count += dfs(r+dir[0], c+dir[1], visited)
-//         }
-//         return count
-//     }
-//     for i := 0; i < n; i++ {
-//         for j := 0; j < n; j++ {
-//             if grid[i][j] == 0 {
-                
-//                 total := 1
-//                 visited := make([][]bool, n)
-//                 for ii := 0; ii < n; ii++ {visited[ii] = make([]bool, n)}
-//                 for _, dir := range dirs {
-//                     nr := i+dir[0]
-//                     nc := j+dir[1]
-//                     total += dfs(nr,nc,visited)
-//                 }
-                
-//                 maxArea = max(maxArea, total)
-//             }
-//         }
-//     }
-//     if maxArea == 0 {maxArea = n*n}
-//     return maxArea
-// }
