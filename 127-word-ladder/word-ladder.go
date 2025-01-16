686c789804b41e304a325cb4cbe210f05b74bc9a
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-15 19:22:42 -0800

    Time: 170 ms (48.80%) | Memory: 9.3 MB (42.81%) - LeetSync

--- 127-word-ladder/word-ladder.go
+++ 127-word-ladder/word-ladder.go
@@ -1,45 +1,43 @@
-func ladderLength(beginWord string, endWord string, words []string) int {
-    adjList := map[string][]string{}
-    words = append(words, beginWord)
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+    words := map[string]bool{}
     foundEnd := false
-    for i := 0; i < len(words); i++ {
-        if words[i] == endWord {foundEnd = true}
-        parent := words[i]
-        for j := 0; j < len(words); j++ {
-            if i == j {continue}
-            child := words[j]
-            p, c := 0,0
-            diff := 0
-            for p < len(parent) && c < len(child) {
-                if parent[p] != child[c] {diff++}
-                p++
-                c++ 
-            }
-            if diff <= 1 {
-                adjList[parent] = append(adjList[parent], child)
-            }
-        }
+    //o(n) where n is len words
+    // k = avg len of each word
+    for i := 0; i < len(wordList); i++ {
+        if wordList[i] == endWord{foundEnd = true}
+        words[wordList[i]] = true
     }
+    
     if !foundEnd {return 0}
-    level := 1
+    
     q := []string{beginWord}
+    levels := 1
     visited := map[string]bool{beginWord:true}
+    // o(n)
     for len(q) != 0 {
         qSize := len(q)
         for qSize != 0 {
             dq := q[0]
             q = q[1:]
-            if dq == endWord {return level}
-            for _, nei := range adjList[dq] {
-                if !visited[nei] {
-                    if nei == endWord {return level+1}
-                    visited[nei] = true
-                    q = append(q, nei)
+            if dq == endWord {return levels}
+            
+            // o(k)  
+            for i := 0; i < len(dq); i++ {
+                // o(26) ; i.e o(1)
+                for j := 0; j < 26; j++ {
+                    child := dq[:i] + string(j+'a') + dq[i+1:]
+                    if !visited[child] && words[child] {
+                        q = append(q, child)
+                        visited[child] = true
+                    }
                 }
             }
             qSize--
         }
-        level++
-    }    
+        levels++
+    }
+    // total time = o(n) + o(n * k^2)
+    // total space = o(n) wordSet, o(n) for queue 
+    
     return 0
 }
