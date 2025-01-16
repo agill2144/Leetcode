88d5450e7b8c44f8e2d0b9a376f34b082c3bbdb7
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-15 20:05:44 -0800

    Time: 41 ms (12.50%) | Memory: 22.2 MB (41.67%) - LeetSync

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.go
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.go
@@ -1,37 +1,26 @@
 func minTime(n int, edges [][]int, hasApple []bool) int {
     adjList := map[int][]int{}
     for i := 0; i < len(edges); i++ {
-        a, b := edges[i][0], edges[i][1]
-        adjList[a] = append(adjList[a], b)
-        adjList[b] = append(adjList[b], a)
+        u,v := edges[i][0], edges[i][1]
+        adjList[u] = append(adjList[u], v)
+        adjList[v] = append(adjList[v], u)
     }
     total := 0
     var dfs func(node, prev int) bool
-    dfs = func(node, prev int) bool{
+    dfs = func(node, prev int)bool{
         // base
 
         // logic
-        childHasApples := false
-        for _, nei := range adjList[node] {
-            if nei == prev {continue}
-            // ask child whether we need to travel on this edge?
-            if dfs(nei, node) {
-                // if child says yes, then add a travel time +2
+        childHasHasApples := false
+        for _, child := range adjList[node] {
+            if child == prev {continue}
+            if dfs(child, node) {
+                childHasHasApples = true
                 total += 2
-                // if even 1 child out of n said yes, then this node is also a child of some parent
-                // meaning, this node now also need to inform its parent that its parent need 
-                // to travel on this edge
-                // therefore update a flag to tell the parent we need to travel to me(curr node)
-                childHasApples = true
             }
         }
-        // if either had apples, then this node's parent needs to travel to it
-        // or if none of the child had apples, this node itself could have apples
-        // in either cases, we need to tell this node's parent node to add travel time 
-        // between parent to this node
-        if childHasApples || hasApple[node] {return true}
-        return false
+        return childHasHasApples || hasApple[node]
     }
-    dfs(0,-1)
+    dfs(0, -1)
     return total
 }
