3991aafcf79ae0505a8344464695dd784a522975
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-16 13:05:06 -0800

    Time: 1 ms (9.92%) | Memory: 6.1 MB (33.18%) - LeetSync

--- 57-insert-interval/insert-interval.go
+++ 57-insert-interval/insert-interval.go
@@ -1,22 +1,39 @@
 func insert(intervals [][]int, newInterval []int) [][]int {
-    if len(newInterval) == 0 {return intervals}
     if len(intervals) == 0 {return [][]int{newInterval}}
-    i := 0
-    n := len(intervals)
+    // move i ptr where this interval should be inserted at
     out := [][]int{}
-    for i < n && intervals[i][1] < newInterval[0] {
-        out = append(out, intervals[i])
-        i++
+    i := 0
+    for i < len(intervals) {
+        if intervals[i][0] <= newInterval[0] {
+            out = append(out, intervals[i])
+            i++
+            continue
+        }
+        break
     }
-    for i < n && intervals[i][0] <= newInterval[1] {
-        newInterval[0] = min(newInterval[0], intervals[i][0])
-        newInterval[1] = max(newInterval[1], intervals[i][1])
-        i++
+
+    // now insert the newInterval
+    // but could this new interval overlap with last interval inserted?
+    // yes
+    start, end := newInterval[0], newInterval[1]
+    if len(out) > 0 && start <= out[len(out)-1][1] {
+        out[len(out)-1][1] = max(out[len(out)-1][1],end)
+    } else {
+        out = append(out, newInterval)
     }
-    out = append(out, newInterval)
-    for i < n {
-        out = append(out, intervals[i])
+    // now add the rest of remaining intervals
+    // while merging overlaps
+    for i < len(intervals) {
+        start, end := intervals[i][0], intervals[i][1]
+        lastStart, lastEnd := out[len(out)-1][0], out[len(out)-1][1]
+        if start <= lastEnd {
+            out[len(out)-1][0] = min(lastStart, start)
+            out[len(out)-1][1] = max(lastEnd,end)
+        } else {
+            out = append(out, intervals[i])
+        }
         i++
     }
     return out
+
 }
