f1be134355a7fbac0c955ea99b6a9485757fc3ce
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-16 19:02:50 -0800

    Time: 0 ms (100.00%) | Memory: 4.1 MB (56.76%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.go
+++ 807-custom-sort-string/custom-sort-string.go
@@ -1,39 +1,20 @@
 func customSortString(order string, s string) string {
     idx := map[string]int{}
-    for i := 0; i < len(order); i++ {idx[string(order[i])] = i}
+    for i := 0; i < len(order); i++ {
+        idx[string(order[i])] = i
+    }
     chars := strings.Split(s, "")
-    sort.Slice(chars, func(i, j int)bool{
-        iIdx, ok := idx[chars[i]]
-        jIdx, ok2 := idx[chars[j]]
-        if ok && ok2 {
-            return iIdx < jIdx
+    sort.Slice(chars, func(i, j int)bool {
+        iidx, iok := idx[chars[i]]
+        jidx, jok := idx[chars[j]]
+        if iok && jok {
+            return iidx < jidx
+        } else if iok {
+            return true
+        } else if jok {
+            return false
         }
-        if ok {return true}
-        return false
+        return true
     })
     return strings.Join(chars, "")
 }
-// func customSortString(order string, s string) string {
-//     // sc = o(1)
-//     sFreq := map[byte]int{}
-//     // m = len(order)
-//     // n = len(s)
-//     // tc = o(n)    
-//     for i := 0; i < len(s); i++ {sFreq[s[i]]++}
-//     out := new(strings.Builder)
-//     // tc = o(n+m)
-//     for i := 0; i < len(order); i++ {
-//         count := sFreq[order[i]]
-//         for count != 0 { out.WriteByte(order[i]); count--}
-//         delete(sFreq, order[i])
-//     }
-//     // o(n)
-//     for i := 0; i < len(s); i++ {
-//         if sFreq[s[i]] > 0 {
-//             out.WriteByte(s[i])
-//         }
-//     }
-//     // total tc = o(n) + o(n+m) + o(n) = o(n+m) + (2n)
-//     // sc = o(1)
-//     return out.String()
-// }
