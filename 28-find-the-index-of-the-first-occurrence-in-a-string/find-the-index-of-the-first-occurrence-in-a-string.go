87bfb9e3ec64419beaf5d41100087833454618d3
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-16 21:23:37 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (39.51%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
@@ -0,0 +1,37 @@
+func strStr(s string, pattern string) int {
+    if len(pattern) > len(s) {return -1}
+    lps := calcLPS(pattern)
+    i := 0
+    j := 0
+    for i < len(s) {
+        if s[i] == pattern[j] {
+            i++
+            j++
+            if j == len(pattern) {return i-j}
+        } else {
+            if j-1 >= 0 {j = lps[j-1]} else {i++}
+        }
+    }
+    return -1
+}
+
+func calcLPS(s string) []int {
+    n := len(s)
+    lps := make([]int, n)
+    length := 0
+    i := 1
+    for i < n {
+        if s[i] == s[length] {
+            length++
+            lps[i] = length
+            i++
+        } else {
+            if length-1 >= 0 {
+                length = lps[length-1]
+            } else {
+                i++
+            }
+        }
+    }
+    return lps
+}
