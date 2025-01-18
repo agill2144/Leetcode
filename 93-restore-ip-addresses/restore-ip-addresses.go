93a1b9f562879ade13abac7871c4a8aaa05fa646
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-17 20:54:25 -0800

    Time: 0 ms (100.00%) | Memory: 4.3 MB (17.89%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.go
+++ 93-restore-ip-addresses/restore-ip-addresses.go
@@ -1,3 +1,9 @@
+// numOfOptionsPerNode^totalNumOfOptions * extraWorkPerRecursion
+// numOfOptionsPerNode = 3 ( substr size of 1,2,3)
+// totalNumOfOptions = 4 ( we never exceed 4 chunks )
+// extraWorkPerRecursion = o(4) always for strings.Join()
+// tc = o(3^4) = o(1)
+// sc = o(4) = o(1) ( recursion never exceeds depth of 4 )
 func restoreIpAddresses(s string) []string {
     out := []string{}
     var dfs func(start int, path []string)
@@ -14,7 +20,7 @@ func restoreIpAddresses(s string) []string {
         // logic
         for i := start; i < len(s); i++ {
             subStr := s[start:i+1]
-            if len(subStr) > 1 && subStr[0] == '0' {return}
+            if (len(subStr) > 1 && subStr[0] == '0') || len(subStr) > 3 {return}
             subStrInt, _ := strconv.Atoi(subStr)
             if subStrInt >= 0 && subStrInt <= 255 {
                 path = append(path, subStr)
