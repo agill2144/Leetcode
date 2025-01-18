c7c8fd572d263b7f7df645ee7ca81eae1cac97cd
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-17 18:21:51 -0800

    Time: 0 ms (100.00%) | Memory: 4.4 MB (63.25%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.go
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.go
@@ -6,30 +6,28 @@
  *     Right *TreeNode
  * }
  */
+type qNode struct {
+    node *TreeNode
+    col int
+}
+
 func verticalOrder(root *TreeNode) [][]int {
     if root == nil {return nil}
     colToNodes := map[int][]int{}
-    type qNode struct {
-        node *TreeNode
-        col int
-    }
     minCol := math.MaxInt64
     maxCol := math.MinInt64
     q := []*qNode{&qNode{root,0}}
     for len(q) != 0 {
         dq := q[0]
         q = q[1:]
-        currNode := dq.node
-        currCol := dq.col
-        minCol = min(minCol, currCol)
-        maxCol = max(maxCol, currCol)
-        if colToNodes[currCol] == nil {colToNodes[currCol] = []int{}}
-        colToNodes[currCol] = append(colToNodes[currCol], currNode.Val)
-        if currNode.Left != nil {
-            q = append(q, &qNode{currNode.Left, currCol-1})
+        minCol = min(minCol, dq.col)
+        maxCol = max(maxCol, dq.col)
+        colToNodes[dq.col] = append(colToNodes[dq.col], dq.node.Val)
+        if dq.node.Left != nil {
+            q = append(q, &qNode{dq.node.Left, dq.col-1})
         }
-        if currNode.Right != nil {
-            q = append(q, &qNode{currNode.Right, currCol+1})
+        if dq.node.Right != nil {
+            q = append(q, &qNode{dq.node.Right, dq.col+1})
         }
     }
     out := [][]int{}
