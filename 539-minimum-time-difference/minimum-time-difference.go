2dda1660b2757b7739695262abc273d7b396e9a1
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-18 11:06:33 -0800

    Time: 4 ms (36.36%) | Memory: 8.3 MB (9.09%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.go
+++ 539-minimum-time-difference/minimum-time-difference.go
@@ -1,20 +1,17 @@
 func findMinDifference(timePoints []string) int {
-    // mins in 1 hour = 60
-    // eg; 2hr = 120 mins
-    minutes := []int{}
+    mins := []int{}
     for i := 0; i < len(timePoints); i++ {
-        t := strings.Split(timePoints[i], ":")
-        hh, mm := t[0], t[1]
-        hhInt, _ := strconv.Atoi(hh)
-        mmInt, _ := strconv.Atoi(mm)
-        m := (hhInt * 60) + mmInt
+        hhmm := strings.Split(timePoints[i], ":")
+        hh, _ := strconv.Atoi(hhmm[0])
+        mm, _ := strconv.Atoi(hhmm[1])
+        m := (hh*60)+mm
         if m == 0 {m = 1440}
-        minutes = append(minutes, m)
+        mins = append(mins, m)
     }
-    sort.Ints(minutes)
-    smallestDiff := minutes[0]+(24*60) - minutes[len(minutes)-1]
-    for i := 1; i < len(minutes); i++ {
-        smallestDiff = min(smallestDiff, minutes[i]-minutes[i-1])
+    sort.Ints(mins)
+    minDiff := mins[0]+(24*60) - mins[len(mins)-1]
+    for i := 1; i < len(mins); i++ {
+        minDiff = min(minDiff, mins[i]-mins[i-1])
     }
-    return smallestDiff
+    return minDiff
 }
