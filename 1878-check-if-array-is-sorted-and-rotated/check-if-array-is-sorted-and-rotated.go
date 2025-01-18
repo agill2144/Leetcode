6ce90d4c78d6f7844f2f5741adc0b838eae331d3
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-18 11:02:00 -0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (7.27%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.go
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.go
@@ -1,39 +1,20 @@
-/*
-    approach: 2 pass
-    - find the ending and starting nodes
-    - ending = largest element
-    - starting = smallest element
-    - if its rotated, ending will show up right before starting
-    - meaning if nums[i] > nums[i+1]
-    - ith element is our ending node
-    - i+1th element is our starting node
-    - then take another from starting node until ending node
-    - to make sure all elements are sorted
-
-    tc = o(2n) = o(n)
-    sc = o(1)
-*/
 func check(nums []int) bool {
     n := len(nums)
     startIdx := -1
-    endIdx := -1
-    for i := 0; i+1 < n; i++ {
+    for i := 0; i+1 < len(nums); i++ {
         if nums[i] > nums[i+1] {
             startIdx = i+1
-            endIdx = i
             break
         }
     }
-    // already sorted, not rotated
-    // examples;
-    // [1,1,1,1,1,1]
-    // [1,2,2,2,2,5,5,6]
-    // [1]
     if startIdx == -1 {return true}
-
-    for startIdx%n != endIdx%n {
-        if nums[startIdx%n] > nums[(startIdx+1)%n] {return false}
+    endIdx := startIdx-1
+    if endIdx < 0 {endIdx = n-1}
+    for (startIdx%n) != (endIdx%n) {
+        if nums[startIdx%n] > nums[(startIdx+1)%n] {
+            return false
+        }
         startIdx++
     }
     return true
-}
+}   
