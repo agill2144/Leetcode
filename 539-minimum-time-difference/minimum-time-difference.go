c71f5f4aa07380029a8d5f190787d64ed3e21fcb
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-18 11:06:57 -0800

    Time: 4 ms (36.36%) | Memory: 8.3 MB (12.12%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.go
+++ 539-minimum-time-difference/minimum-time-difference.go
@@ -5,10 +5,10 @@ func findMinDifference(timePoints []string) int {
         hh, _ := strconv.Atoi(hhmm[0])
         mm, _ := strconv.Atoi(hhmm[1])
         m := (hh*60)+mm
-        if m == 0 {m = 1440}
         mins = append(mins, m)
     }
     sort.Ints(mins)
+    // its possible that
     minDiff := mins[0]+(24*60) - mins[len(mins)-1]
     for i := 1; i < len(mins); i++ {
         minDiff = min(minDiff, mins[i]-mins[i-1])
