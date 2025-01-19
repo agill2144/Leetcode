df1e0b0cb4e54f8fc3591c744f8efbd4ba2cb6a8
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-19 15:29:03 -0800

    Time: 15 ms (11.98%) | Memory: 13.1 MB (9.90%) - LeetSync

--- 415-add-strings/add-strings.go
+++ 415-add-strings/add-strings.go
@@ -1,14 +1,13 @@
 func addStrings(num1 string, num2 string) string {
     carry := 0
+    n1 := len(num1)-1
+    n2 := len(num2)-1
     res := ""
-    n1, n2 := len(num1)-1, len(num2)-1
     for n1 >= 0 || n2 >= 0 {
-        n1Val := 0
-        n2Val := 0
-        if n1 >= 0 {n1Val = int(num1[n1]-'0'); n1--}
-        if n2 >= 0 {n2Val = int(num2[n2]-'0'); n2--}
-        sum := n1Val + n2Val + carry
-        res = fmt.Sprintf("%v", sum%10) + res
+        sum := carry
+        if n1 >= 0 {sum += int(num1[n1]-'0'); n1--;}
+        if n2 >= 0 {sum += int(num2[n2]-'0'); n2--;}
+        res = fmt.Sprintf("%v", sum % 10) + res
         carry = sum / 10
     }
     if carry != 0 {
