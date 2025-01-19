7f990a32917448cf680416ce38150f965a0e1ac1
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-18 21:01:34 -0800

    Time: 38 ms (29.41%) | Memory: 4 MB (73.53%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.go
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.go
@@ -1,26 +1,22 @@
 func maxUniqueSplit(s string) int {
-    maxSplit := 0
+    maxCount := 0
     var dfs func(start int, path map[string]bool)
     dfs = func(start int, path map[string]bool) {
         // base
         if start == len(s) {
-            maxSplit = max(maxSplit, len(path))
+            maxCount = max(maxCount, len(path))
             return
         }
-
-        // short circuit 
-        if len(s)-start + len(path) <= maxSplit {return}
 
         // logic
         for i := start; i < len(s); i++ {
             subStr := s[start:i+1]
-            if !path[subStr] {
-                path[subStr] = true
-                dfs(i+1, path)
-                delete(path, subStr)
-            }
+            if path[subStr] {continue}
+            path[subStr] = true
+            dfs(i+1, path)
+            delete(path, subStr)
         }
     }
     dfs(0, map[string]bool{})
-    return maxSplit
+    return maxCount
 }
