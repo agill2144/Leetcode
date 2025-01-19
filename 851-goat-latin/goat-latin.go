cc7604e1e7d52d6696c35b1038d41d49db1166ae
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-18 19:35:44 -0800

    Time: 0 ms (100.00%) | Memory: 4.3 MB (44.74%) - LeetSync

--- 851-goat-latin/goat-latin.go
+++ 851-goat-latin/goat-latin.go
@@ -1,23 +1,22 @@
 func toGoatLatin(sentence string) string {
-    words := strings.Split(sentence, " ")
-    res := new(strings.Builder)
     suffix := "a"
+    res := new(strings.Builder)
+    words := strings.Split(sentence, " ")
     for i := 0; i < len(words); i++ {
         if isVowel(words[i]) {
             res.WriteString(words[i])
-            res.WriteString("ma")
         } else {
             res.WriteString(words[i][1:])
             res.WriteByte(words[i][0])
-            res.WriteString("ma")
         }
+        res.WriteString("ma")
         res.WriteString(suffix)
         suffix += "a"
         if i != len(words)-1 {res.WriteString(" ")}
     }
     return res.String()
-
 }
+
 func isVowel(s string) bool {
     set := map[byte]bool{
         'a':true,'e':true,'i':true,'o':true,'u':true,
