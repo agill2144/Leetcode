7d668e5eaa44c5ba933f67a667bf30811d1f927d
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-20 14:48:24 -0800

    Time: 5 ms (42.40%) | Memory: 8.4 MB (35.20%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.go
@@ -11,8 +11,8 @@
 func lowestCommonAncestor(p *Node, q *Node) *Node {
     p1, p2 := p, q
     for p1 != p2 {
-        if p1.Parent == nil {p1 = q} else {p1 = p1.Parent}
-        if p2.Parent == nil {p2 = p} else {p2 = p2.Parent}
+        if p1 == nil {p1 = q} else {p1 = p1.Parent}
+        if p2 == nil {p2 = p} else {p2 = p2.Parent}
     }
     return p1
 }
