894b777a9a7b72f5eec0e1a35b805c26e33b8bbc
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-19 16:52:17 -0800

    Time: 4 ms (35.71%) | Memory: 8.3 MB (9.52%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.go
+++ 539-minimum-time-difference/minimum-time-difference.go
@@ -7,6 +7,13 @@ func findMinDifference(timePoints []string) int {
         mins = append(mins, (hh*60)+mm)
     }
     sort.Ints(mins)
+    // edge case 
+    // mins[-1] = 23:50 (i.e 1430 mins)
+    // mins[0] = 02:00 (i.e 120 mins)
+    // the difference between these 2 is not 1430-120 = 1320 mins ..
+    // but really its (10 mins from 1430 -> 1440) + 120 mins
+    // we can get remaining mins left until 1440 is hit by; 1440-mins[-1]
+    // then we can simply add the mins at mins[0]
     minDiff := (1440-mins[len(mins)-1])+mins[0]
     for i := 1; i < len(mins); i++ {
         minDiff = min(minDiff, mins[i]-mins[i-1])
