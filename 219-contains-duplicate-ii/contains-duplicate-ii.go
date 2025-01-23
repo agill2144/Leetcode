adc79cb7e3504c46b8a4a791938b1b5a058734e3
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-22 21:47:02 -0800

    Time: 20 ms (86.67%) | Memory: 14.3 MB (38.63%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.go
+++ 219-contains-duplicate-ii/contains-duplicate-ii.go
@@ -1,10 +1,8 @@
 func containsNearbyDuplicate(nums []int, k int) bool {
     idx := map[int]int{}
     for i := 0; i < len(nums); i++ {
-        seenAt, ok := idx[nums[i]]
-        if ok && i-seenAt <= k {
-            return true
-        }
+        left, ok := idx[nums[i]]
+        if ok && i-left <= k {return true}
         idx[nums[i]] = i
     }
     return false
