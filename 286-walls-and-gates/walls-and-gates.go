adba3567dd0cb960f0653e88f9a29c14ea1286d9
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-22 21:25:36 -0800

    Time: 9 ms (56.32%) | Memory: 9.7 MB (48.28%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.go
+++ 286-walls-and-gates/walls-and-gates.go
@@ -1,44 +1,34 @@
-// multi-src bfs
-// start from gates and find/fill nearest rooms
-// nearest / shortest path without any custom weights = vanilla bfs
 func wallsAndGates(rooms [][]int)  {
+    empty := 2147483647
+    gate := 0
+    q := [][]int{}
     m := len(rooms)
     n := len(rooms[0])
-    emptyRoomID := 2147483647
-    haveEmptyRooms := false
-    q := [][]int{} // [r,c,dist]
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            if rooms[i][j] == 0 {
-                q = append(q, []int{i,j,0})
-            }
-            if rooms[i][j] == emptyRoomID {
-                haveEmptyRooms = true
+            if rooms[i][j] == gate {
+                q = append(q, []int{i,j})
             }
         }
     }
-    if len(q) == 0 {return}
-    if !haveEmptyRooms {return}
-    
-    dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
-    // from each gate, look for empty rooms
+    dirs := [][]int{
+        {-1,0},{1,0},
+        {0,-1},{0,1},
+    }
     for len(q) != 0 {
-        qSize := len(q)
-        for qSize != 0 {
-            dq := q[0]
-            q = q[1:]
-            cr := dq[0]
-            cc := dq[1]
-            cd := dq[2]
-            for _, dir := range dirs {
-                nr := cr + dir[0]
-                nc := cc + dir[1]
-                if nr >= 0 && nr < m && nc >= 0 && nc < n && rooms[nr][nc] == emptyRoomID {
-                    rooms[nr][nc] = cd+1
-                    q = append(q, []int{nr,nc,cd+1})
-                }
+        dq := q[0]
+        q = q[1:]
+        cr, cc := dq[0], dq[1]
+        cd := rooms[cr][cc]
+        for _, dir := range dirs {
+            nr := cr + dir[0]
+            nc := cc + dir[1]
+            nd := cd+1
+            if nr >= 0 && nr < m && nc >= 0 && nc < n && rooms[nr][nc] == empty {
+                q = append(q, []int{nr,nc})
+                rooms[nr][nc] = nd
             }
-            qSize--
         }
     }
+
 }
