2f4050a38d50c252965e8d934abc645a3f8c43ad
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-25 13:00:09 -0800

    Time: 6 ms (51.60%) | Memory: 8.6 MB (98.63%) - LeetSync

--- 455-assign-cookies/assign-cookies.go
+++ 455-assign-cookies/assign-cookies.go
@@ -1,20 +1,10 @@
-/*
-    approach: greedy
-    - try to meet bare min requirement for each child
-    time = o(g log g) + o(s log s) + o(max(s+g)) 
-    space = o(g) + o(s) == if underlying sort is using merge sort 
-*/
 func findContentChildren(g []int, s []int) int {
     sort.Ints(g)
     sort.Ints(s)
-    p1, p2 := 0, 0
-    for p1 < len(g) && p2 < len(s) {
-        if g[p1] <= s[p2] {
-            p1++
-            p2++
-        } else {
-            p2++
-        }
+    i, j := 0, 0
+    for i < len(g) && j < len(s) {
+        if s[j] >= g[i] {i++; j++; continue}
+        j++
     }
-    return p1
+    return i
 }
