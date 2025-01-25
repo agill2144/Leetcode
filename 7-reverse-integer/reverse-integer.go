93cedede428d8feaaae414cfc31d654d873ee405
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-24 19:58:48 -0800

    Time: 5 ms (18.31%) | Memory: 3.9 MB (78.50%) - LeetSync

--- 7-reverse-integer/reverse-integer.go
+++ 7-reverse-integer/reverse-integer.go
@@ -1,13 +1,17 @@
 func reverse(x int) int {
-    // left limit = -2147483648
+    // left limit = 2147483648
     left := -214748364
     // right limit = 2147483647
     right := 214748364
     rev := 0
+    // dont we need to handle explicitly ?
+    // no,
+    // in golang Remainder result has the same sign as the dividend
+    // so if the dividend is -ve, our % result will be -ve
     for x != 0 {
         lastDig := x % 10
-        if rev < left || (rev == left && lastDig > 8) {return 0}
-        if rev > right || (rev == right && lastDig > 7) {return 0}
+        if rev < left || (rev == left && lastDig >= 8) {return 0}
+        if rev > right || (rev == right && lastDig >= 7) {return 0}
         rev = rev * 10 + lastDig
         x /= 10
     }
