06b9c63efe7e31cc90d54c9d8bee64e274071a06
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-25 10:56:56 -0800

    Time: 0 ms (100.00%) | Memory: 9.7 MB (38.46%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.go
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.go
@@ -2,25 +2,23 @@ func minDominoRotations(tops []int, bottoms []int) int {
     desired := tops[0]
     tSwaps := 0
     bSwaps := 0
-    possible1 := true
-    res := math.MaxInt64
+    possible := true
     for i := 0; i < len(tops); i++ {
-        if tops[i] != desired && bottoms[i] != desired {possible1 = false; break}
+        if tops[i] != desired && bottoms[i] != desired {possible = false; break}
         if tops[i] != desired && bottoms[i] == desired {tSwaps++}
         if bottoms[i] != desired && tops[i] == desired {bSwaps++}
     }
-    if possible1 {res = min(tSwaps, bSwaps)}
+    if possible {return min(tSwaps, bSwaps)}
 
     desired = bottoms[0]
     tSwaps = 0
     bSwaps = 0
-    possible2 := true    
+    possible = true    
     for i := 0; i < len(tops); i++ {
-        if tops[i] != desired && bottoms[i] != desired {possible2 = false; break}
+        if tops[i] != desired && bottoms[i] != desired {possible = false; break}
         if tops[i] != desired && bottoms[i] == desired {tSwaps++}
         if bottoms[i] != desired && tops[i] == desired {bSwaps++}
     }
-    if !possible1 && !possible2 {return -1}
-    if possible2 {res = min(res, min(tSwaps, bSwaps))}
-    return res
+    if possible {return min(tSwaps, bSwaps)}
+    return -1
 }
