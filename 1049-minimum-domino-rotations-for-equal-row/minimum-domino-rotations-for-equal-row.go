f41805756b1b0a3d7582a8c84be9747b1320001d
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-25 10:55:36 -0800

    Time: 1 ms (69.23%) | Memory: 9.3 MB (53.85%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.go
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.go
@@ -1,57 +1,26 @@
-// time = o(n) + o(n)
-// space = o(1)
-func minDominoRotations(tops []int, bottoms []int) int {
-    if len(tops) != len(bottoms) {return -1}
+func minDominoRotations(tops []int, bottoms []int) int {    
+    desired := tops[0]
+    tSwaps := 0
+    bSwaps := 0
+    possible1 := true
+    res := math.MaxInt64
+    for i := 0; i < len(tops); i++ {
+        if tops[i] != desired && bottoms[i] != desired {possible1 = false; break}
+        if tops[i] != desired && bottoms[i] == desired {tSwaps++}
+        if bottoms[i] != desired && tops[i] == desired {bSwaps++}
+    }
+    if possible1 {res = min(tSwaps, bSwaps)}
 
-    var minRotations func(target int) int
-    minRotations = func(target int) int {
-        tCount, bCount := 0, 0
-        n := len(tops)
-        for i := 0; i < n; i++ {
-            t,b := tops[i], bottoms[i]
-            // when t and b is not target, this is impossible
-            if t != target && b != target {return -1}
-            // when t == b == target; no rotations required on either side
-            if t == b && t == target {continue}
-            // when target is sitting at the bottom, rotate for tops
-            if t != target && b == target {tCount++}
-            // when target is sitting at the top, rotate for bottom
-            if b != target && t == target {bCount++}
-        }
-        return min(tCount, bCount)
+    desired = bottoms[0]
+    tSwaps = 0
+    bSwaps = 0
+    possible2 := true    
+    for i := 0; i < len(tops); i++ {
+        if tops[i] != desired && bottoms[i] != desired {possible2 = false; break}
+        if tops[i] != desired && bottoms[i] == desired {tSwaps++}
+        if bottoms[i] != desired && tops[i] == desired {bSwaps++}
     }
-    // evaluate whether tops[0] is our ans
-    t1Count := minRotations(tops[0])
-    if t1Count == -1 {
-        // when tops[0] is not our answer, check with bottoms[0]
-        return minRotations(bottoms[0])
-    }
-    // when tops[0] is our answer
-    return t1Count
+    if !possible1 && !possible2 {return -1}
+    if possible2 {res = min(res, min(tSwaps, bSwaps))}
+    return res
 }
-
-// time = o(n) + o(n)
-// space = o(n)
-// func minDominoRotations(tops []int, bottoms []int) int {
-//     n := len(tops)
-//     freq := map[int]int{}
-//     target := -1
-//     for i := 0; i < n; i++ {
-//         t, b := tops[i], bottoms[i]
-//         freq[t]++
-//         if freq[t] >= n { target = t; break }
-//         if t == b {continue}
-//         freq[b]++
-//         if freq[b] >= n { target = b; break }
-//     }
-//     if target == -1 {return -1}
-//     if tops[0] != target && bottoms[0] != target {return -1}
-
-//     tCount, bCount := 0, 0
-//     for i := 0; i < n; i++ {
-//         t, b := tops[i], bottoms[i]
-//         if t != target {tCount++}
-//         if b != target {bCount++}
-//     }
-//     return min(tCount, bCount)
-// }
