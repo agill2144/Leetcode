1081a71f3bfb92303b1961b84976ef0be3642ced
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-25 21:28:35 -0800

    Time: 0 ms (100.00%) | Memory: 3.8 MB (87.80%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.go
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.go
@@ -1,29 +1,25 @@
 func isRobotBounded(ins string) bool {
-    // clockwise dirs
-    //                up  right  down   left   
+    //                 up, right, down, left
     dirs := [][]int{{-1,0},{0,1},{1,0},{0,-1}}
     currDir := 0
-    r, c := 0,0
-    for i := 0; i < len(ins); i++ {
-        if ins[i] == 'G' {
-            r += dirs[currDir][0]
-            c += dirs[currDir][1]
-        } else if ins[i] == 'R' {            
-            // wherever we are facing,
-            // turning right will always be +1
-            // because our dirs are written in clockwise order
-            // currDir++
-            // if currDir == 4 {currDir = 0}
-            currDir = (currDir + 1) % 4
-        } else if ins[i] == 'L' {
-            // wherever we are facing,
-            // turning left will awlays be -1
-            // because our dirs are written in clockwise order
-            // currDir--
-            // if currDir < 0 {currDir = 3}
-            currDir = (currDir+3) % 4
+    x, y := 0, 0
+    n := len(ins)
+    for i := 0; i < 2 * n; i++ {
+        if ins[i%n] == 'G' {
+            x += dirs[currDir][0]
+            y += dirs[currDir][1]
+        } else if ins[i%n] == 'R' {
+            currDir++
+            if currDir == len(dirs) {
+                currDir = 0
+            }
+        } else if ins[i%n] == 'L' {
+            currDir--
+            if currDir < 0 {
+                currDir = len(dirs)-1
+            }
         }
     }
-    if r == 0 && c == 0 {return true}
+    if x == 0 && y == 0 {return true}
     return currDir != 0
 }
