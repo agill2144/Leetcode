6b02a2ee4ff2492ee1b4f2d21a077fd3183af6e7
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-25 16:56:01 -0800

    Time: 1 ms (100.00%) | Memory: 8.6 MB (47.83%) - LeetSync

--- 561-array-partition/array-partition.go
+++ 561-array-partition/array-partition.go
@@ -1,8 +1,11 @@
 func arrayPairSum(nums []int) int {
     sort.Ints(nums)
     total := 0
-    for i := 0; i < len(nums); i+=2 {
-        total += nums[i]
+    for i := len(nums)-2; i >= 0; i -= 2 {
+        total += min(nums[i], nums[i+1])
     }
     return total
 }
+
+// 1,4,3,2
+// 1,2,3,4
