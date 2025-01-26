afbfa5caf1385739f6acb287523e7db08d95b557
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-25 16:52:21 -0800

    Time: 4 ms (40.74%) | Memory: 15.4 MB (11.11%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.go
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.go
@@ -1,57 +1,20 @@
-/*
-    greedy greedy greedy
-    maintain 2 min times with a counter keeping track of consec
-    1. smallest min
-    2. second smallest min
-
-    if this color is same as prev color
-    - increment consecutive counter
-    - set/update min and second min times
-    - once we have seen 2 consecutive counts ( 2 same colors )
-        - add min to total
-        - promote second min to mind
-        - reset second to maxInt
-        - reset counter = 1
-    
-    time = o(n)
-    space = o(1)
-*/
 func minCost(colors string, neededTime []int) int {
     total := 0
-    min := math.MaxInt64
-    second := math.MaxInt64
-    count := 0
-    for i := 0; i < len(colors); i++ {
-        if i == 0 {
-            count = 1
-            min = neededTime[i]
-            continue
-        }
-        if colors[i] == colors[i-1] {
-            count++
-            // update min and second min timers
-            if neededTime[i] < min {
-                second = min
-                min = neededTime[i]
-            } else if neededTime[i] < second {
-                second = neededTime[i]
-            }
-            
-            // have we seen 2 consecutive counts ?
-            if count == 2 {
-                count = 1
-                total += min
-                min = second
-                second = math.MaxInt64
-            }
+    prev := 0
+    for i := 1; i < len(colors); i++ {
+        if colors[i] == colors[prev] {
+            if neededTime[i] < neededTime[prev] {
+                // removing current, keeping prev
+                total += neededTime[i]
+                continue
+            } else {
+                // removing prev, keeping current
+                total += neededTime[prev]
+                prev = i
+            } 
         } else {
-            // not consecutive, starting a new potential consecutive train
-            // reset everything
-            count = 1
-            min = neededTime[i]
-            second = math.MaxInt64
+            prev = i
         }
     }
     return total
 }
-
