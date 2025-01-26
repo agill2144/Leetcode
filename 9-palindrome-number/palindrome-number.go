14f3001de645664b24240602ac736d0c9b67acfa
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-25 21:57:49 -0800

    Time: 3 ms (59.32%) | Memory: 6 MB (77.88%) - LeetSync

--- 9-palindrome-number/palindrome-number.go
+++ 9-palindrome-number/palindrome-number.go
@@ -2,7 +2,9 @@ func isPalindrome(x int) bool {
     if x < 0 {return false}
     if x != 0 && x % 10 == 0 {return false}
     rev := 0
+    rightLimit := 214748364
     for x > rev {
+        if rev > rightLimit || (rev == rightLimit && x % 10 > 7) {return false}
         rev = rev * 10 + (x % 10)
         x /= 10
     }
