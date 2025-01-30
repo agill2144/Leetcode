f98c6b5e6f8aff760d2d0f064430691f43b50dae
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-29 20:09:55 -0800

    Time: 0 ms (100.00%) | Memory: 12.1 MB (22.60%) - LeetSync

--- 134-gas-station/gas-station.go
+++ 134-gas-station/gas-station.go
@@ -1,15 +1,16 @@
 func canCompleteCircuit(gas []int, cost []int) int {
     n := len(gas)
-    destIdx := -1
+    count := 0
     curr := 0
     for i := 0; i < 2*n; i++ {
-        if (i%n) == destIdx {return destIdx}
-        if destIdx == -1 {destIdx = i%n}
         curr += gas[i%n]
         curr -= cost[i%n]
         if curr < 0 {
-            destIdx = -1
+            count = 0
             curr = 0
+        } else {
+            count++
+            if count == n {return (i+1)%n}
         }
     }
     return -1
