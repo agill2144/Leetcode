a61857d102724bf6705ee554edd9744c20ab080b
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-01-29 19:58:43 -0800

    Time: 0 ms (100.00%) | Memory: 3.9 MB (31.11%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.go
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.go
@@ -1,25 +1,23 @@
 func isRobotBounded(ins string) bool {
-    //                 up, right, down, left
     dirs := [][]int{{-1,0},{0,1},{1,0},{0,-1}}
     currDir := 0
-    x, y := 0, 0
-    n := len(ins)
-    for i := 0; i < 2 * n; i++ {
-        if ins[i%n] == 'G' {
-            x += dirs[currDir][0]
-            y += dirs[currDir][1]
-        } else if ins[i%n] == 'R' {
+    r, c := 0, 0
+    for i := 0; i < len(ins); i++ {
+        if ins[i] == 'G' {
+            r += dirs[currDir][0]
+            c += dirs[currDir][1]
+        } else if ins[i] == 'R' {
             currDir++
-            if currDir == len(dirs) {
+            if currDir == 4 {
                 currDir = 0
             }
-        } else if ins[i%n] == 'L' {
+        } else if ins[i] == 'L' {
             currDir--
             if currDir < 0 {
-                currDir = len(dirs)-1
+                currDir = 3
             }
         }
     }
-    if x == 0 && y == 0 {return true}
-    return currDir != 0
+    if currDir == 0 && r == 0 && c == 0 {return true}
+    return currDir != 0 
 }
