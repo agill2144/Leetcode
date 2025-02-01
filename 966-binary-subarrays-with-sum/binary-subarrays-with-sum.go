de2052140564f223308fe0d65c0302870184bfd6
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-01 12:36:48 -0800

    Time: 0 ms (100.00%) | Memory: 8.6 MB (80.00%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.go
@@ -1,11 +1,22 @@
 func numSubarraysWithSum(nums []int, goal int) int {
-    freq := map[int]int{0:1}
+    if goal < 0 {return 0}
+    return atMostK(nums,goal) - atMostK(nums, goal-1)
+}
+
+func atMostK(nums []int, k int) int {
+    left := 0
+    n := len(nums)
     rSum := 0
     total := 0
-    for i := 0; i < len(nums); i++ {
+    for i := 0; i < n; i++ {
         rSum += nums[i]
-        total += freq[rSum-goal]
-        freq[rSum]++
+        for rSum > k && left < i {
+            rSum -= nums[left]
+            left++
+        }
+        if rSum <= k {
+            total += (i-left+1)
+        }
     }
     return total
 }
