da3d274253e54db9cba314ec79040810acbc8974
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-01 13:25:09 -0800

    Time: 16 ms (33.47%) | Memory: 9.4 MB (42.98%) - LeetSync

--- 525-contiguous-array/contiguous-array.go
+++ 525-contiguous-array/contiguous-array.go
@@ -1,17 +1,18 @@
 func findMaxLength(nums []int) int {
-    idx := map[int]int{0:-1}
-    maxSize := 0
+    idxs := map[int]int{0:-1}
+    res := 0
     count := 0
     for i := 0; i < len(nums); i++ {
-        if nums[i] == 1 {count++}
         if nums[i] == 0 {count--}
-        val, ok := idx[count]
+        if nums[i] == 1 {count++}
+        left, ok := idxs[count]
         if ok {
-            maxSize = max(maxSize, i-val)
+            res = max(res, i-left)
         }
-        if _, ok := idx[count]; !ok {
-            idx[count] = i
+        _, ok2 := idxs[count]
+        if !ok2 {
+            idxs[count] = i
         }
     }
-    return maxSize
+    return res
 }
