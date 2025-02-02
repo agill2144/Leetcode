1aea3a198162b1566c6ee641cbd670a9d69d2393
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-01 18:56:54 -0800

    Time: 326 ms (60.00%) | Memory: 10 MB (84.00%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.go
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.go
@@ -1,14 +1,14 @@
 func shortestDistance(grid [][]int) int {
     m := len(grid)
     n := len(grid[0])
-    hCount := 0
     dists := make([][][]int, m)
     for i := 0; i < m; i++ {
         dists[i] = make([][]int, n)
         for j := 0; j < n; j++ {
-            dists[i][j] = []int{0,0}
+            dists[i][j] = make([]int, 2)
         }
     }
+    hCount := 0
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
             if grid[i][j] == 1 {
@@ -35,8 +35,11 @@ func bfs(r, c int, grid [][]int, dists [][][]int) {
     n := len(grid[0])
     dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
     q := [][]int{{r,c,0}}
-    visited := map[[2]int]bool{}
-    visited[[2]int{r,c}] = true
+	visited := make([][]bool, m)
+	for i := range visited {
+		visited[i] = make([]bool, n)
+	}
+	visited[r][c] = true
     for len(q) != 0 {
         dq := q[0]
         q = q[1:]
@@ -45,8 +48,8 @@ func bfs(r, c int, grid [][]int, dists [][][]int) {
             nr := cr + dir[0]
             nc := cc + dir[1]
             if nr >= 0 && nr < m && nc >= 0 && nc < n &&
-                 grid[nr][nc] == 0 && !visited[[2]int{nr,nc}] {
-                    visited[[2]int{nr,nc}] = true
+                 grid[nr][nc] == 0 && !visited[nr][nc] {
+                    visited[nr][nc] = true
                     dists[nr][nc][0]++
                     dists[nr][nc][1] += (cd+1)
                     q = append(q, []int{nr,nc,cd+1})
