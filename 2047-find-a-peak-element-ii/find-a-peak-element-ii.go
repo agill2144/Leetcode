775766607684a3c412768e8611cd8e990afd1fd4
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-03 13:26:19 -0800

    Time: 1 ms (0.00%) | Memory: 11.5 MB (64.71%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.go
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.go
@@ -1,23 +1,27 @@
-func findPeakGrid(mat [][]int) []int {
-    m := len(mat)
-    n := len(mat[0])
+func findPeakGrid(matrix [][]int) []int {
+    m := len(matrix)
+    n := len(matrix[0])
     left := 0
     right := m-1
+    // pick a row using bs
     for left <= right {
         mid := left + (right-left)/2
-        idx := 0
-        for j := 1; j < n; j++ {
-            if mat[mid][j] > mat[mid][idx] {
-                idx = j
-            }
+        
+        // find the max in this row
+        maxIdx := 0
+        for j := 0; j < n; j++ {
+            if matrix[mid][j] > matrix[mid][maxIdx] {maxIdx = j}
         }
-        curr := mat[mid][idx]
+
+        // compare with above and below values
         top := math.MinInt64
-        if mid-1 >= 0 {top = mat[mid-1][idx]}
+        if mid-1 >= 0 {top = matrix[mid-1][maxIdx]}
         bottom := math.MinInt64
-        if mid+1 < m {bottom = mat[mid+1][idx]}
-        if curr > top && curr > bottom {return []int{mid, idx}}
-        if top > bottom {
+        if mid+1 < m {bottom = matrix[mid+1][maxIdx]}
+        curr := matrix[mid][maxIdx]
+        if curr > top && curr > bottom {return []int{mid, maxIdx}}
+
+        if top > curr {
             right = mid-1
         } else {
             left = mid+1
