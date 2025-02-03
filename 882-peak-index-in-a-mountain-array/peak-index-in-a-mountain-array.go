817f803286f6ffeb0352ba9d012eac29417406f5
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-02 17:52:44 -0800

    Time: 0 ms (100.00%) | Memory: 10.2 MB (50.39%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.go
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.go
@@ -1,15 +1,14 @@
 func peakIndexInMountainArray(nums []int) int {
-    n := len(nums)
     left := 0
-    right := n-1
+    right := len(nums)-1
     for left <= right {
-        mid := left+(right-left)/2
-        if (mid == 0 || nums[mid] > nums[mid-1]) && (mid == n-1  || nums[mid] > nums[mid+1]) {
+        mid := left + (right-left)/2
+        if (mid == 0 || nums[mid] > nums[mid-1]) && (mid == len(nums)-1 || nums[mid] > nums[mid+1]) {
             return mid
-        } else if nums[mid+1] > nums[mid] {
+        } else if (mid == len(nums)-1 || nums[mid+1] > nums[mid]) {
             left = mid+1
         } else {
-            right=mid-1
+            right = mid-1
         }
     }
     return -1
