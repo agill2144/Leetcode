6808a06c5722d573782fa1738cca74b20f098548
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-03 13:39:06 -0800

    Time: 0 ms (100.00%) | Memory: 8.9 MB (52.24%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.go
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.go
@@ -1,23 +1,17 @@
 func singleNonDuplicate(nums []int) int {
+    n := len(nums)
     left := 0
-    right := len(nums)-1
+    right := n-1
     for left <= right {
         mid := left + (right-left)/2
-        if (mid == 0 || nums[mid] != nums[mid-1]) && (mid == len(nums)-1 || nums[mid] != nums[mid+1]) {
+        if (mid == 0 || nums[mid] != nums[mid-1]) && (mid == n-1 || nums[mid] != nums[mid+1]) {
             return nums[mid]
         }
-        if mid % 2 == 0 {
-            if mid == len(nums)-1 || nums[mid] == nums[mid+1] {
-                left = mid+1
-            } else {
-                right = mid-1
-            }
+
+        if (mid % 2 == 0 && (mid == n-1 || nums[mid] == nums[mid+1])) || (mid % 2 != 0 && (mid == 0 || nums[mid] == nums[mid-1])){
+            left = mid+1
         } else {
-            if (mid == 0 || nums[mid] == nums[mid-1]) {
-                left = mid+1
-            } else {
-                right = mid-1
-            }
+            right = mid-1
         }
     }
     return -1
