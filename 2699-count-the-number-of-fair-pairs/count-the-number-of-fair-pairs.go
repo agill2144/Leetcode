69013f834d63db4a625beebb7266cd5bd6656d7a
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-03 14:09:04 -0800

    Time: 47 ms (62.96%) | Memory: 10.2 MB (55.56%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.go
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.go
@@ -1,47 +1,46 @@
 func countFairPairs(nums []int, lower int, upper int) int64 {
+    n := len(nums)
     sort.Ints(nums)
-    var count int64 = 0
-    /*
-        0,1,7,4,4,5
-        sum >= 3 and <= 6
-    */
-    for i := 0; i < len(nums); i++ {
-        left := leftMostOnRightSideOfTarget(nums, i+1, lower-nums[i])
-        right := rightMostOnLeftSideOfTarget(nums,i+1, upper-nums[i])
+    var count int64
+    for i := 0; i < n; i++ {
+        left := searchGreaterEqualTo(nums, i+1, lower-nums[i])
+        right := searchLessEqualTo(nums, i+1, upper-nums[i])
         if left != -1 && right != -1 {
             count += int64(right-left+1)
         }
     }
-    return count    
+    return count
 }
 
-
-func leftMostOnRightSideOfTarget(nums []int, left int, target int) int {
-    right := len(nums)-1
-    ans := -1
+func searchGreaterEqualTo(nums []int, left int, target int) int {
+    n := len(nums)
+    idx := -1
+    right := n-1
     for left <= right {
         mid := left + (right-left)/2
         if nums[mid] >= target {
-            ans = mid
+            idx = mid
             right = mid-1
         } else {
             left = mid+1
         }
     }
-    return ans
+    return idx
 }
 
-func rightMostOnLeftSideOfTarget(nums []int, left int, target int) int {
-    right := len(nums)-1
-    ans := -1
+
+func searchLessEqualTo(nums []int,left int,target int) int {
+    n := len(nums)
+    idx := -1
+    right := n-1
     for left <= right {
         mid := left + (right-left)/2
         if nums[mid] <= target {
-            ans = mid
+            idx= mid
             left = mid+1
         } else {
             right = mid-1
         }
     }
-    return ans
+    return idx
 }
