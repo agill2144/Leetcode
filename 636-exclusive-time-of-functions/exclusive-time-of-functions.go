dab4bd1e04706b0bfc1e2562a8c5bf7967a8519d
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-03 21:03:04 -0800

    Time: 3 ms (61.76%) | Memory: 7.9 MB (67.65%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.go
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.go
@@ -1,6 +1,7 @@
 func exclusiveTime(n int, logs []string) []int {
     out := make([]int, n)
-    st := [][]int{} // id, start, childTime
+    st := []int{} // id
+    prevStartTime := 0
 
     for i := 0; i < len(logs); i++ {
         log := strings.Split(logs[i], ":")
@@ -8,15 +9,16 @@ func exclusiveTime(n int, logs []string) []int {
         logType := log[1]
         logTime, _ := strconv.Atoi(log[2])
         if logType == "start" {
-            st = append(st, []int{logId,logTime,0})
-        } else {
-            top := st[len(st)-1]
-            st = st[:len(st)-1]
-            id, start, cTime := top[0],top[1],top[2]
-            out[id] += (logTime-start+1) - cTime
             if len(st) > 0 {
-                st[len(st)-1][2] += logTime-start+1
+                out[st[len(st)-1]] += (logTime - prevStartTime)
             }
+            prevStartTime = logTime
+            st = append(st, logId)
+        } else if logType == "end" {
+            topId := st[len(st)-1]
+            st = st[:len(st)-1]
+            out[topId] += (logTime - prevStartTime+1)
+            prevStartTime = logTime + 1
         }
     }
 
