6d08a114a3e3dd355567fb9f807905193ecc7feb
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-03 18:42:50 -0800

    Time: 0 ms (100.00%) | Memory: 3.9 MB (73.97%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.go
+++ 678-valid-parenthesis-string/valid-parenthesis-string.go
@@ -1,17 +1,18 @@
 func checkValidString(s string) bool {
     open := 0
-    close := 0
     for i := 0; i < len(s); i++ {
-        if s[i] == '(' || s[i] == '*' {
+        if s[i] == '*' || s[i] == '(' {
             open++
         } else if s[i] == ')' {
             open--
             if open < 0 {return false}
         }
-
-        if s[len(s)-1-i] == ')' || s[len(s)-1-i] == '*' {
+    }
+    close := 0
+    for i := len(s)-1; i >= 0; i-- {
+        if s[i] == ')' || s[i] == '*' {
             close++
-        } else if s[len(s)-1-i] == '(' {
+        } else if s[i] == '(' {
             close--
             if close < 0 {return false}
         }
