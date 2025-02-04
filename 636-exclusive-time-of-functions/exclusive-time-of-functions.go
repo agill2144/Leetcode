286d78cf7dd32587a8f6cfeaa93b8a2049095818
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-03 20:57:17 -0800

    Time: 3 ms (61.76%) | Memory: 8.6 MB (8.82%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.go
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.go
@@ -1,38 +1,25 @@
 func exclusiveTime(n int, logs []string) []int {
     out := make([]int, n)
-    st := [][]int{}
-    
+    st := [][]int{} // id, start, childTime
+
     for i := 0; i < len(logs); i++ {
         log := strings.Split(logs[i], ":")
-        funcId, _ := strconv.Atoi(log[0])
+        logId, _ := strconv.Atoi(log[0])
         logType := log[1]
         logTime, _ := strconv.Atoi(log[2])
-        
         if logType == "start" {
-            // If stack is not empty, update the time of the previous function
-            if len(st) > 0 {
-                prevFuncId := st[len(st)-1][0]
-                prevStartTime := st[len(st)-1][1]
-                out[prevFuncId] += logTime - prevStartTime
-            }
-            // Push the current function onto the stack
-            st = append(st, []int{funcId, logTime})
-        } else { // "end" type
-            // Pop the top function from the stack
-            topLog := st[len(st)-1]
+            st = append(st, []int{logId,logTime,0})
+        } else {
+            top := st[len(st)-1]
             st = st[:len(st)-1]
-            
-            // Calculate and add the exclusive time for this function
-            topFuncId := topLog[0]
-            topStartTime := topLog[1]
-            out[topFuncId] += logTime - topStartTime + 1
-            
-            // If there are still functions on the stack, adjust their start time
+            id, start, cTime := top[0],top[1],top[2]
+            out[id] += (logTime-start+1) - cTime
             if len(st) > 0 {
-                st[len(st)-1][1] = logTime + 1
+                st[len(st)-1][2] += logTime-start+1
             }
         }
     }
-    
+
+
     return out
 }
