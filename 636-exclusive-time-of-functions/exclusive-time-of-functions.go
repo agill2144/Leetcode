81dcb6afcaadd53c1c103d6fb56d95b9f2312bc6
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-03 23:05:38 -0800

    Time: 1 ms (83.82%) | Memory: 8.3 MB (23.53%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.go
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.go
@@ -16,6 +16,13 @@ func exclusiveTime(n int, logs []string) []int {
             top := st[len(st)-1]
             st = st[:len(st)-1]
             out[top[0]] += (time-top[1]+1)
+            // resume parent
+            // parent has resumed at a new start time, therefore update 
+            // the parent start time to time+1
+            // what if there were multiple parents?
+            // well, when the top one is popped, processed, it will propagate and update
+            // new time to its parent in the stack
+            // each popped event is updating its parent of their new resumed start time
             if len(st) > 0 {st[len(st)-1][1] = time+1}
         }
     }
