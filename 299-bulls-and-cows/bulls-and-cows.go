b8388b23669ed84ab853909697a82266a9a88ac5
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-04 21:12:33 -0800

    Time: 0 ms (100.00%) | Memory: 4.3 MB (20.63%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.go
+++ 299-bulls-and-cows/bulls-and-cows.go
@@ -1,73 +1,94 @@
 /*
-    approach: freq map and two-ptrs
-    - inorder to match guess char with secret char
-    - we need to know whether guess char exists in secret char
-    - its also possible that a guess char repeats more than 1 time
-    - therefore we need to check the same guess char exists in secret that many times
-    - hence we will store each char in secret string into a freq map to make searching easy
-    - while building the freq map , we can also start counting bulls
-    - i.e chars that are correctly positioned 
-    - if guess[i] == secret[i], these chars are correctly positioned
-    - then we dont need to include secret[i] occurence in freq match
-        because it has fully matched with correct position in guess secret
-    - finally, we can take another pass, counting all the cows
-    - i.e if secret[i] != guess[i] and if the guess char exists in secret freq
-    - then we can increment cows counter and reduce this char freq from secret freq
-        because its current occurence can no longer used to match later
-
-
-    EDGE CASES
-    Why cant we create a freq map and check for bulls and cows later in 2nd pass together?
-    - secret = 1122 -> freq = {1:2, 2:2}
-    - guess  = 1222
-    - we end up counting extra cows because
-    - we have 3 occurence of 2s in guess
-    - but only have 2 occurence in freq map
-    - when i is at idx 1, we see char mismatch
-    - then we will check if freq map contains our guess[i] char
-    - and yes, it does, and therefore cows++ and freq[guess[i]]--
-    - when i is done at idx 1 , freq = {1:2, 2:1}
-    - when i is done at idx 2 , freq = {1:2, 2:0}
-    - when i is get at idx 3,
-        - this is not a char mismatch
-        - therefore bulls++
-        - and this char freq needs to be decremented, as its curr instance can no longer be used
-        - but doing so will make freq negative
-        - when freq goes negative for a secret char, it means
-        - that we used a occurence that was extra in guess to match with this char in secret
-        - meaning somewhere we did cows++, and therefore thats extra 
-        - to fix this, we can check if freq goes negative and if yes, 
-        - it means a extra guess char exists that was used to match with secret char
-        - as a cow char, therefore decrement cows by 1
-    - alternatively we could do this in 2 passes,
-    - count bulls first when creating freq map
-    - then count cows
-    - if there were perfect char matches, their occurence never got placed into freq map
-    - therefore occurences that exist in freq map are purely for unmatched chars
-    - and can only be used cows
-    
-
-    
-    n = len(guess || secret)
-    tc = o(2n) = o(n)
-    sc = o(1) = freq map stores digits from 0 to 9, therefore constant
-
+    approach: 1 pass with freq map
+    - if s[i] == guess[i] ; happy path, increment bulls counter
+    - otherwise
+        - we need to see if secret[i] was previously seen in guess?
+        - meaning from i -> 0 in guess string, have we had a non-matching secret[i] char?
+        - to build this non-matching guess char;
+            - we will create a negative freq of guess[i] char; i.e freq[guess[i]]--
+            - this means, we had a guess char that had not matched with a secret char
+        - now to check if this secret char can match with a previously seen non-matching guess char
+            - check in freq if secret[i] has a negative value?
+            - negative value freq are only added by guess[i] chars when they dont match
+            - so if there is a negative freq, there is a previously seen guess char thats misplaced
+            - therefore cows++
+            - if freq[secret[i]] < 0 {cows++}
+        - similarly, we need to check if guess[i] could be paired with a non-matching previously seen secret char?
+        - meaning we need to go back from i -> 0 in secret string to check if there was a non-matching guess[i] char
+        - to build non-matching secret char;
+            - we will create a positive freq for each secret[i] char that has not been paired with a guess char
+            - now guess[i] can perform a lookup in freq to check whether secret string ever added a non matching guess[i] char
+            - if there is a freq > 0, it means yes, secret string added a char that previously did not match
+            - meaning we have a secret char that matches guess char but in the wrong position
+            - if freq[guess[i]] > 0 {cows++}
+        - now add secret freq ; freq[secret[i]]++
+            - this does 2 things
+            - adds itself if it did not match previously seen non-matching guess char
+            - or 
+            - it matched with previously seen guess char, and its freq was negative, incrementing it made it 0, therefore no-longer usable
+        - now decrement guess freq; freq[guess[i]]--
+            - this also does 2 things
+            - it adds itself for the first time when there was no previously seen secret char to pair with
+            - or
+            - it matched with a previously seen secret char and now decremeting its freq because that secret char is no longer usable
+        
+        time = o(n)
+        space = o(1) // 26 chars in alphabets
 */
 func getHint(secret string, guess string) string {
+    if len(secret) != len(guess) {return ""}
     bulls := 0
     cows := 0
     freq := map[byte]int{}
     for i := 0; i < len(secret); i++ {
-        if secret[i] == guess[i] {bulls++; continue}
-        // only place chars into freq that did not match guess char positions
-        freq[secret[i]]++
-    }
-    for i := 0; i < len(guess); i++ {
-        if secret[i] == guess[i] {continue}
-        if freq[guess[i]] > 0 {
-            cows++
-            freq[guess[i]]--
+        s, g := secret[i], guess[i]
+        if s == g {
+            bulls++
+        } else {
+            // is there a secret[i] char that was added by guess?
+            // guess makes its counters negative
+            // if we run into a secret[i] whose freq is negative
+            // it means it was added by guess string
+            // therefore placed at incorrect position
+            // hence cows++
+            if freq[s] < 0 {cows++} 
+
+            // is there a guess[i] added by secret string?
+            // secret makes its counters positive
+            // if we run into guess[i] whose freq is positive
+            // it means secret string and guess string has guess[i] char
+            // but at difference positions
+            // hence cows++
+            if freq[g] > 0 {cows++}
+
+            // secret string adds its char with positive counters
+            freq[s]++
+            
+            // guess string adds its char with negaitve counters
+            freq[g]--
         }
     }
-    return fmt.Sprintf("%vA%vB",bulls, cows)
+    return fmt.Sprintf("%vA%vB", bulls, cows)
 }
+
+// func getHint(secret string, guess string) string {
+//     if len(secret) != len(guess) {return ""}
+//     bulls := 0
+//     freq := map[byte]int{}
+//     for i := 0; i < len(secret); i++ {
+//         if secret[i] == guess[i] {
+//             bulls++
+//         } else {
+//             freq[secret[i]]++
+//         }
+//     }
+//     cows := 0
+//     for i := 0; i < len(secret); i++ {
+//         if secret[i] == guess[i] {continue}
+//         if freq[guess[i]] != 0 {
+//             freq[guess[i]]--
+//             cows++
+//         }
+//     }
+//     return fmt.Sprintf("%vA%vB", bulls, cows)
+// }
