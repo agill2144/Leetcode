31c6dae46de11f1da69d8f727224287a19f0076b
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-04 23:08:49 -0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (46.27%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.go
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.go
@@ -1,5 +1,5 @@
 func exclusiveTime(n int, logs []string) []int {
-    st := [][]int{} // id, startTime
+    st := [][]int{} // funcID, startTime
     out := make([]int, n)
     for i := 0; i < len(logs); i++ {
         log := strings.Split(logs[i], ":")
@@ -8,22 +8,16 @@ func exclusiveTime(n int, logs []string) []int {
         time, _ := strconv.Atoi(log[2])
         if logType == "start" {
             if len(st) > 0 {
-                peek := st[len(st)-1]
-                out[peek[0]] += (time-peek[1])
+                out[st[len(st)-1][0]] += (time - st[len(st)-1][1])
             }
-            st = append(st, []int{id, time})
-        } else if logType == "end" {
+            st = append(st, []int{id,time})
+        } else {
             top := st[len(st)-1]
             st = st[:len(st)-1]
             out[top[0]] += (time-top[1]+1)
-            // resume parent
-            // parent has resumed at a new start time, therefore update 
-            // the parent start time to time+1
-            // what if there were multiple parents?
-            // well, when the top one is popped, processed, it will propagate and update
-            // new time to its parent in the stack
-            // each popped event is updating its parent of their new resumed start time
-            if len(st) > 0 {st[len(st)-1][1] = time+1}
+            if len(st) > 0 {
+                st[len(st)-1][1] = time+1
+            }
         }
     }
     return out
