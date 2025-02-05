42ee37cfdf63e4d169f98db948c678d8236ffdfd
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-04 20:37:52 -0800

    Time: 2 ms (47.62%) | Memory: 4.2 MB (71.43%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.go
+++ 299-bulls-and-cows/bulls-and-cows.go
@@ -58,14 +58,13 @@ func getHint(secret string, guess string) string {
     cows := 0
     freq := map[byte]int{}
     for i := 0; i < len(secret); i++ {
+        if secret[i] == guess[i] {bulls++; continue}
+        // only place chars into freq that did not match guess char positions
         freq[secret[i]]++
     }
     for i := 0; i < len(guess); i++ {
-        if guess[i] == secret[i] {
-            bulls++
-            freq[guess[i]]--
-            if freq[guess[i]] < 0 {cows--}
-        } else if freq[guess[i]] > 0 {
+        if secret[i] == guess[i] {continue}
+        if freq[guess[i]] > 0 {
             cows++
             freq[guess[i]]--
         }
