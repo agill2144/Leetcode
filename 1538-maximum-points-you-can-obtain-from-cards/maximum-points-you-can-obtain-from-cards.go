8fe34e892a4a5b1ffd78b871c8a5e34c0ee1e913
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-05 19:04:34 -0800

    Time: 0 ms (100.00%) | Memory: 10.1 MB (30.95%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.go
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.go
@@ -1,18 +1,36 @@
 func maxScore(cardPoints []int, k int) int {
     n := len(cardPoints)
-    total := 0
-    for i := 0; i < n; i++ {total += cardPoints[i]}
-    if k == n {return total}
-    left := 0
-    winSum := 0
     maxPoints := 0
-    for i := 0; i < n; i++ {
-        winSum += cardPoints[i]
-        if i-left+1 == n-k {
-            maxPoints = max(maxPoints, total-winSum)
-            winSum -= cardPoints[left]
+    rSum := 0
+    left := n-k
+    i := n-k
+    for left <= n {
+        rSum += cardPoints[i%n]
+        if i-left+1 == k {
+            maxPoints = max(maxPoints, rSum)
+            rSum -= cardPoints[left%n]
             left++
         }
+        i++
     }
     return maxPoints
 }
+// 2 pass inverted window
+// func maxScore(cardPoints []int, k int) int {
+//     n := len(cardPoints)
+//     total := 0
+//     for i := 0; i < n; i++ {total += cardPoints[i]}
+//     if k == n {return total}
+//     left := 0
+//     winSum := 0
+//     maxPoints := 0
+//     for i := 0; i < n; i++ {
+//         winSum += cardPoints[i]
+//         if i-left+1 == n-k {
+//             maxPoints = max(maxPoints, total-winSum)
+//             winSum -= cardPoints[left]
+//             left++
+//         }
+//     }
+//     return maxPoints
+// }
