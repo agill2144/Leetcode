6f17d6426343f3b05b222fecf6448009536550c6
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-05 17:52:11 -0800

    Time: 24 ms (75.05%) | Memory: 10.5 MB (33.91%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
@@ -1,30 +1,5 @@
 func findKthLargest(nums []int, k int) int {
-    mn := &minHeap{items: []int{}}
-    for i := 0; i < len(nums); i++ {
-        heap.Push(mn,nums[i])
-        if mn.Len() > k {heap.Pop(mn)}
-    }
-    return mn.items[0]
-}
-
-type minHeap struct {
-	items []int
-}
-
-func (m *minHeap) Less(i, j int) bool {
-	return m.items[i] < m.items[j]
-}
-func (m *minHeap) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-func (m *minHeap) Len() int {
-	return len(m.items)
-}
-func (m *minHeap) Push(x interface{}) {
-	m.items = append(m.items, x.(int))
-}
-func (m *minHeap) Pop() interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
+    n := len(nums)
+    sort.Ints(nums)
+    return nums[n-k]
 }
