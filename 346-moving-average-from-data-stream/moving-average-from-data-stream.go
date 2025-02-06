7bd8320f0e8327e10c72b0b0ae96bd23929dda73
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-05 17:23:52 -0800

    Time: 4 ms (32.32%) | Memory: 8.8 MB (55.49%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.go
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.go
@@ -1,25 +1,32 @@
 type MovingAverage struct {
     k int
-    left int
     sum int
+    left int
+    right int
     nums []int
 }
 
 
 func Constructor(size int) MovingAverage {
-    return MovingAverage{left:0,nums:[]int{}, k:size, sum:0}    
+    return MovingAverage{
+        sum:0,
+        k:size,
+        left:0,
+        right:0,
+        nums: make([]int, size),
+    }    
 }
 
 
 func (this *MovingAverage) Next(val int) float64 {
-    this.nums = append(this.nums, val)
-    this.sum += val
-    right := len(this.nums)-1
-    if right-this.left+1 > this.k {
-        this.sum -= this.nums[this.left]
+    if this.right - this.left == this.k {
+        this.sum -= this.nums[this.left%this.k]
         this.left++
     }
-    currSize := right-this.left+1
+    this.nums[this.right%this.k] = val
+    this.sum += val
+    this.right++
+    currSize := this.right - this.left
     return float64(this.sum) / float64(currSize)
 }
 
