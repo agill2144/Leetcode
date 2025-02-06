eabc2a2e684468f71644f644059624156b748304
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-05 18:51:46 -0800

    Time: 24 ms (75.05%) | Memory: 14.4 MB (6.33%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
@@ -2,6 +2,8 @@ func findKthLargest(nums []int, k int) int {
     n := len(nums)
     minval := slices.Min(nums)
     maxval := slices.Max(nums)
+    // why not use a bucket arr here?
+    // because values are -ve and bucket idxs cannot be -ve you dumb fuck
     freq := map[int]int{}
     for i := 0; i < n; i++ {freq[nums[i]]++}
     idx := 0
