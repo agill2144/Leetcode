8190f1f99151cd6c898713a1debc7f23850e0d35
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-05 17:48:11 -0800

    Time: 0 ms (100.00%) | Memory: 4.8 MB (52.63%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.go
+++ 249-group-shifted-strings/group-shifted-strings.go
@@ -1,21 +1,22 @@
 func groupStrings(strings []string) [][]string {
-    groups := map[string][]string{}
+    grps := map[string][]string{}    
     for i := 0; i < len(strings); i++ {
         key := hash(strings[i])
-        groups[key] = append(groups[key], strings[i])
+        grps[key] = append(grps[key],strings[i])
     }
     out := [][]string{}
-    for _, v := range groups {
+    for _, v := range grps {
         out = append(out, v)
     }
     return out
+
 }
 
 func hash(word string) string {
-    if len(word) == 1 {return "0"} 
+    if len(word) == 1 {return ""}
     res := new(strings.Builder)
     for i := 1; i < len(word); i++ {
-        diff := int(word[i]-'a') - int(word[i-1]-'a')
+        diff := int(word[i-1]-'a') - int(word[i]-'a')
         if diff < 0 {diff += 26}
         res.WriteString(fmt.Sprintf("%v-", diff))
     }
