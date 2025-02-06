cd74cc2f22cefa8e7a811050b472d6c41f2fcf18
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-05 17:39:48 -0800

    Time: 71 ms (89.86%) | Memory: 80.6 MB (41.05%) - LeetSync

--- 146-lru-cache/lru-cache.go
+++ 146-lru-cache/lru-cache.go
@@ -1,87 +1,46 @@
-type listNode struct {
-    key int
-    val int
-    next *listNode
-    prev *listNode
-}
-
 type LRUCache struct {
-    data map[int]*listNode
-    head *listNode // most recent node , append new nodes here
-    tail *listNode // least recently used node, discard from tail
-    cap int    
-}
-
-func (this *LRUCache) addNode(node *listNode) {
-    if this.head == nil {
-        this.head = node
-        this.tail = node
-        return
-    }
-    node.next = this.head
-    this.head.prev = node
-    this.head = node
-}
-
-func (this *LRUCache) removeNode(node *listNode) {
-    next := node.next
-    prev := node.prev
-    node.next = nil
-    node.prev = nil
-    // prev -> next ( if prev not nil )
-    // next <- prev ( if next not nil )
-    if prev != nil {prev.next = next}
-    if next != nil {next.prev = prev}
-
-    // update head ptr
-    if node == this.head {
-        this.head = next
-        return
-    }
-    
-    // update tail ptr
-    if node == this.tail {
-        this.tail = prev
-        return
-    }
+    dll *dll
+    capacity int
+    cache map[int]*listNode
 }
 
 
 func Constructor(capacity int) LRUCache {
     return LRUCache{
-        data: map[int]*listNode{},
-        cap: capacity,
+        cache: map[int]*listNode{},
+        capacity: capacity,
+        dll: &dll{},
     }
 }
 
 
 func (this *LRUCache) Get(key int) int {
-    node, ok := this.data[key]
-    if !ok {return -1}
-    this.removeNode(node)
-    this.addNode(node)
-    return node.val
+    node, ok := this.cache[key]
+    if ok {
+        this.dll.removeNode(node)
+        this.dll.append(node)
+        return node.val
+    }
+    return -1
 }
 
 
 func (this *LRUCache) Put(key int, value int)  {
-    node, ok := this.data[key]
+    node, ok := this.cache[key]
     if ok {
         node.val = value
-        this.removeNode(node)
-        this.addNode(node)
+        this.dll.removeNode(node)
+        this.dll.append(node)
         return
     }
-    if len(this.data) == this.cap {
-        toBeDeleted := this.tail
-        this.removeNode(toBeDeleted)
-        delete(this.data, toBeDeleted.key)
+    if len(this.cache) == this.capacity {
+        toBeDeleted := this.dll.head
+        this.dll.removeNode(toBeDeleted)
+        delete(this.cache, toBeDeleted.key)
     }
-
-    newNode := &listNode{key:key, val:value}
-    this.addNode(newNode)
-    this.data[key] = newNode
-    
+    newNode := &listNode{key:key,val:value}
+    this.dll.append(newNode)
+    this.cache[key] = newNode
 }
 
 
@@ -91,3 +50,41 @@ func (this *LRUCache) Put(key int, value int)  {
  * param_1 := obj.Get(key);
  * obj.Put(key,value);
  */
+
+type listNode struct {
+    key int
+    val int
+    next *listNode
+    prev *listNode    
+}
+
+type dll struct {
+    head *listNode // old / stale side of data
+    tail *listNode // fresh / latest side of data
+}
+
+func (d *dll) append(node *listNode) {
+    if d.head == nil {
+        d.head = node
+        d.tail = node
+        return
+    }
+    d.tail.next = node
+    node.prev = d.tail
+    d.tail = node
+}
+
+func (d *dll) removeNode(node *listNode) {
+    next := node.next
+    prev := node.prev
+    node.next = nil
+    node.prev = nil
+    if next != nil {next.prev = prev}
+    if prev != nil {prev.next = next}
+    if d.head == node {
+        d.head = next
+    }
+    if d.tail == node {
+        d.tail = prev
+    }
+}
