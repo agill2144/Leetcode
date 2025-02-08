c41f1467b07f2835a83528474271093ed5d69aef
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-08 13:19:18 -0800

    Time: 0 ms (100.00%) | Memory: 3.9 MB (95.44%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
@@ -1,38 +1,37 @@
 func strStr(haystack string, needle string) int {
-    if len(needle) > len(haystack) {return -1}
+    if len(needle) > len(haystack) { return -1 }
     lps := calcLPS(needle)
-    i := 0 // haystack ptr
-    j := 0 // needle ptr
+    i := 0
+    j := 0
     for i < len(haystack) && j < len(needle) {
         if haystack[i] == needle[j] {
             i++
             j++
         } else {
-            if j-1 >= 0 {
+            if j > 0 {
                 j = lps[j-1]
             } else {
+                // j is at 0, start a fresh match
                 i++
             }
         }
     }
-    if j == len(needle) {
-        return i-len(needle)
-    }
+    if j == len(needle) {return i-len(needle)}
     return -1
 }
 
 func calcLPS(word string) []int {
     n := len(word)
     lps := make([]int, n)
-    j := 0
     i := 1
+    j := 0
     for i < len(word) {
         if word[i] == word[j] {
             j++
             lps[i] = j
             i++
         } else {
-            if j-1 >= 0 {
+            if j > 0 {
                 j = lps[j-1]
             } else {
                 i++
