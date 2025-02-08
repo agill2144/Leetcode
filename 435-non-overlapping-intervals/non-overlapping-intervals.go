0b7c6128e258d66554112b62de0360eba2b3ec87
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-08 15:08:31 -0800

    Time: 31 ms (91.97%) | Memory: 19.1 MB (30.90%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.go
+++ 435-non-overlapping-intervals/non-overlapping-intervals.go
@@ -1,24 +1,19 @@
 func eraseOverlapIntervals(intervals [][]int) int {
-    sort.Slice(intervals, func(i, j int)bool{
-        if intervals[i][0] == intervals[j][0] {
-            return intervals[i][1] < intervals[j][1]
-        }
+    // [[1,2],[1,3],[2,3],[3,4]]   
+    sort.Slice(intervals, func(i, j int)bool {
         return intervals[i][0] < intervals[j][0]
     })
-    prev := 0
     count := 0
+    prev := 0
     for i := 1; i < len(intervals); i++ {
         start, end := intervals[i][0], intervals[i][1]
         prevEnd := intervals[prev][1]
         if start < prevEnd {
+            count++
             if end < prevEnd {
-                // keep curr, drop prev
                 prev = i
             }
-            // keep prev, drop curr
-            count++
         } else {
-            // no overlap, move prev ptr to curr i ptr
             prev = i
         }
     }
