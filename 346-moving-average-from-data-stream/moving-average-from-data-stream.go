facab586c54b24d1d31b64511a6f15ce342e869b
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-08 13:12:10 -0800

    Time: 0 ms (100.00%) | Memory: 9.5 MB (11.25%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.go
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.go
@@ -9,25 +9,19 @@ type MovingAverage struct {
 
 func Constructor(size int) MovingAverage {
     return MovingAverage{
-        sum:0,
-        k:size,
-        left:0,
-        right:0,
+        k: size,
         nums: make([]int, size),
-    }    
+    }
 }
 
 
 func (this *MovingAverage) Next(val int) float64 {
-    // right ptr is actually sitting at next cell
-    // its not at the cell that it had last written data to
-    // therefore we do not have the +1 in this window calc
-    if this.right - this.left == this.k {
-        this.sum -= this.nums[this.left%this.k]
+    if this.right-this.left == this.k {
+        this.sum -= this.nums[this.left % this.k]
         this.left++
     }
-    this.nums[this.right%this.k] = val
     this.sum += val
+    this.nums[this.right%this.k] = val
     this.right++
     currSize := this.right - this.left
     return float64(this.sum) / float64(currSize)
