b2543af09d994b5c7d43e4b969b354646665e857
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-08 15:30:43 -0800

    Time: 0 ms (100.00%) | Memory: 7.6 MB (92.31%) - LeetSync

--- 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.go
+++ 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.go
@@ -6,17 +6,12 @@
  *     Right *TreeNode
  * }
  */
- // k =2
- // [3,2,1]
-// tc = o(n * logk)
-// sc = o(k) for heap + o(n) for recursive stack
+// naive heap tc = o(nlogk)
+// without heap tc , using dq = o(n)
+//
 func closestKValues(root *TreeNode, target float64, k int) []int {
     if root == nil || k == 0 {return nil}
-    type tmpNode struct {
-        val int
-        diff float64
-    }
-    tmp := []tmpNode{}
+    dq := []int{}
     var dfs func(r *TreeNode)
     dfs = func(r *TreeNode) {
         // base
@@ -24,55 +19,22 @@ func closestKValues(root *TreeNode, target float64, k int) []int {
 
         // logic
         dfs(r.Left)
-        tmp = append(tmp, tmpNode{r.Val, abs(target-float64(r.Val))})
-        if len(tmp) > k {
-            if tmp[0].diff > tmp[len(tmp)-1].diff {
-                // drop the 0th element
-                tmp = tmp[1:]
-            } else {
-                // drop the last element
-                tmp = tmp[:len(tmp)-1]
-            }
+        dq = append(dq, r.Val)
+        if len(dq) > k {
+            if abs(float64(dq[0]) - target) < 
+                abs(float64(dq[len(dq)-1])-target) {
+                    dq = dq[:len(dq)-1]
+                } else {
+                    dq = dq[1:]
+                }
         }
         dfs(r.Right)
     }
     dfs(root)
-    out := []int{}
-    for i := 0; i < len(tmp); i++ {
-        out = append(out, tmp[i].val)
-    }
-    return out
+    return dq
 }
 
 func abs(x float64) float64 {
     if x < 0 {return x * -1.0}
     return x
-}
-
-
-type heapNode struct {
-    node int
-    diff float64
-}
-
-type maxHeap struct {
-	items []*heapNode
-}
-
-func (m *maxHeap) Less(i, j int) bool {
-	return m.items[i].diff > m.items[j].diff
-}
-func (m *maxHeap) Swap(i, j int) {
-	m.items[i], m.items[j] = m.items[j], m.items[i]
-}
-func (m *maxHeap) Len() int {
-	return len(m.items)
-}
-func (m *maxHeap) Push(x interface{}) {
-	m.items = append(m.items, x.(*heapNode))
-}
-func (m *maxHeap) Pop() interface{} {
-	out := m.items[len(m.items)-1]
-	m.items = m.items[:len(m.items)-1]
-	return out
 }
