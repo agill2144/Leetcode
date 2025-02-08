42f9d0a4f7882bc7e80174a215863a707a68dbdc
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-08 14:50:20 -0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (25.30%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.go
+++ 45-jump-game-ii/jump-game-ii.go
@@ -1,38 +1,16 @@
 func jump(nums []int) int {
-    n := len(nums)
-    i := 0
+    if len(nums) <= 1 {return 0}
+    windows := 0
     right := 0
-    count := 0
-    farthestRight := 0
-    for i < n && right < n-1 {
-        farthestRight = max(farthestRight, i+nums[i])
-        if i == right {
-            count++
-            right = farthestRight
+    i := 0
+    maxRight := 0
+    for i < len(nums) {
+        maxRight = max(maxRight,i + nums[i])
+        if i == right {windows++; right = maxRight}
+        if right >= len(nums)-1 {
+            break
         }
         i++
     }
-    return count
+    return windows
 }
-// brute force: 
-// tc = x^n
-// sc = o(n)
-// func jump(nums []int) int {
-//     n := len(nums)
-//     minJumps := math.MaxInt64
-//     var dfs func(start, count int)
-//     dfs  = func(start, count int) {
-//         if start >= n-1 {
-//             if start == n-1 {
-//                 minJumps = min(minJumps, count)
-//             }
-//             return
-//         }
-
-//         for i := nums[start]; i >= 1; i-- {
-//             dfs(start+i, count+1)
-//         }
-//     }
-//     dfs(0, 0)
-//     return minJumps
-// }
