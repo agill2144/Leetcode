0a20e586261064990cff31b3f951a91679d986f5
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-09 12:15:52 -0800

    Time: 26 ms (80.13%) | Memory: 15.4 MB (96.15%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.go
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.go
@@ -6,19 +6,15 @@ type NumMatrix struct {
 func Constructor(matrix [][]int) NumMatrix {
     m := len(matrix)
     n := len(matrix[0])
-    fullSum := make([][]int, m)
+    fullSum := make([][]int,m)
     for i := 0; i < m; i++ {
-        fullSum[i] = make([]int, n)
+        fullSum[i] = make([]int, n) 
         for j := 0; j < n; j++ {
-            leftSum := 0
-            if j-1 >= 0 {leftSum = fullSum[i][j-1]}
-            topSum := 0
-            if i-1 >= 0 {topSum = fullSum[i-1][j]}
-            overlap := 0
-            if i-1 >= 0 && j-1 >= 0 {
-                overlap = fullSum[i-1][j-1]
-            }
-            fullSum[i][j] = matrix[i][j] + leftSum + topSum - overlap
+            sum := matrix[i][j]
+            if i-1 >= 0 {sum += fullSum[i-1][j]}
+            if j-1 >= 0 {sum += fullSum[i][j-1]}
+            if i-1 >= 0 && j-1 >= 0 {sum -= fullSum[i-1][j-1]}
+            fullSum[i][j] = sum
         }
     }
     return NumMatrix{fullSum}
@@ -26,13 +22,12 @@ func Constructor(matrix [][]int) NumMatrix {
 
 
 func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
-    // get fullSum from 0,0 to row2,col2
     total := this.fullSum[row2][col2]
     // remove top sum
     if row1-1 >= 0 {total -= this.fullSum[row1-1][col2]}
-    // remove left sum
+    // remove left col sum
     if col1-1 >= 0 {total -= this.fullSum[row2][col1-1]}
-    // add back overlapping sum that got removed twice
+    // add back overlapping sum we removed twice
     if row1-1 >= 0 && col1-1 >= 0 {total += this.fullSum[row1-1][col1-1]}
     return total
 }
