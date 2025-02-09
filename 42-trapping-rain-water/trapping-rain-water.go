5c9d19a89ad10396c9b23ef7d42c5e7114490d0d
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-09 12:18:16 -0800

    Time: 0 ms (100.00%) | Memory: 8 MB (74.58%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.go
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -2,20 +2,20 @@ func trap(height []int) int {
     n := len(height)
     leftWall := 0
     left := 1
-    right := n-2
     rightWall := n-1
+    right := n-2
     total := 0
     for left <= right {
         if height[rightWall] >= height[leftWall] {
-            if height[left] <= height[leftWall] {
-                total += (height[leftWall]-height[left])
+            if height[left] <= height[leftWall]{
+                total += (height[leftWall] - height[left])
                 left++
             } else {
                 leftWall = left
             }
         } else {
             if height[right] <= height[rightWall] {
-                total += (height[rightWall]-height[right])
+                total += (height[rightWall] - height[right])
                 right--
             } else {
                 rightWall = right
