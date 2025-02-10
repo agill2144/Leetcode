e90a4d0c1fd1cb33ed7a9230f2aeac4597ccc3b1
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-09 23:05:25 -0800

    Time: 5 ms (70.27%) | Memory: 4.4 MB (81.08%) - LeetSync

--- 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.go
+++ 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.go
@@ -1,16 +1,18 @@
 func lengthOfLongestSubstringKDistinct(s string, k int) int {
     freq := map[byte]int{}
-    maxSize := 0
     left := 0
+    maxWin := 0
     for i := 0; i < len(s); i++ {
         freq[s[i]]++
         if len(freq) <= k {
-            maxSize = max(maxSize, i-left+1)
+            maxWin = max(maxWin, i-left+1)
         } else {
             freq[s[left]]--
-            if freq[s[left]] == 0 {delete(freq, s[left])}
+            if freq[s[left]] == 0 {
+                delete(freq, s[left])
+            }
             left++
-        }
+        }    
     }
-    return maxSize
+    return maxWin
 }
