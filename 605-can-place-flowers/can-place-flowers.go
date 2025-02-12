12029a763c948d0910dfdbf77cd96e790fb90275
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-11 16:24:58 -0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (51.83%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.go
+++ 605-can-place-flowers/can-place-flowers.go
@@ -1,17 +1,15 @@
 func canPlaceFlowers(flowerbed []int, n int) bool {
     if n == 0 {return true}
     for i := 0; i < len(flowerbed); i++ {
-        curr := flowerbed[i]
-        if curr == 1 {continue}
         prev := 0
-        if i-1 >= 0 {prev = flowerbed[i-1]}
         next := 0
+        if i-1 >= 0 {prev = flowerbed[i-1]}
         if i+1 < len(flowerbed) {next = flowerbed[i+1]}
-        if prev == 0 && next == 0 {
-            flowerbed[i] = 1
+        if prev == 1 || next == 1 {continue}
+        if flowerbed[i] == 0 && n > 0 {
             n--
-            if n == 0 {break}
+            flowerbed[i] = 1
         }
     }
-    return n == 0
+    return n==0
 }
