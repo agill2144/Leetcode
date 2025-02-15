5b26825348ce29096c61d08912a0cc440a64e423
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-14 21:00:10 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (47.22%) - LeetSync

--- 163-missing-ranges/missing-ranges.go
+++ 163-missing-ranges/missing-ranges.go
@@ -3,20 +3,19 @@ func findMissingRanges(nums []int, lower int, upper int) [][]int {
         return [][]int{{lower, upper}}
     }
     out := [][]int{}
-    if lower < nums[0] {
-        out = append(out, []int{lower,nums[0]-1})
+    if nums[0] > lower {
+        out = append(out, []int{lower, nums[0]-1})
     }
     for i := 1; i < len(nums); i++ {
-        prev := nums[i-1]
         curr := nums[i]
-        if prev + 1 == curr {continue}
-        
-        start := prev+1
-        end := curr-1
-        if start >= lower && end <= upper {out = append(out, []int{start, end})}
+        prev := nums[i-1]
+        if prev+1 != curr {
+            out = append(out, []int{prev+1,curr-1})
+        }
     }
+
     if nums[len(nums)-1] < upper {
-        out = append(out, []int{nums[len(nums)-1]+1, upper})
+        out = append(out, []int{nums[len(nums)-1]+1,upper})
     }
     return out
 }
