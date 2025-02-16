f289f9bec55b11d631d685986a19a04f5f7ba842
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-16 12:59:18 -0800

    Time: 2 ms (63.40%) | Memory: 8.6 MB (59.57%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.go
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.go
@@ -1,12 +1,9 @@
 func removeDuplicates(s string) string {
     st := []byte{}
     for i := 0; i < len(s); i++ {
-        pushCurr := true
-        for len(st) > 0 && st[len(st)-1] == s[i] {
+        if len(st) > 0 && st[len(st)-1] == s[i] {
             st = st[:len(st)-1]
-            pushCurr = false
-        }
-        if pushCurr {
+        } else {
             st = append(st, s[i])
         }
     }
