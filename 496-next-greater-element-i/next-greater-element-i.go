42ffcffc3e1a591c790f051384d7b5aa71b8e5ed
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-16 12:55:50 -0800

    Time: 0 ms (100.00%) | Memory: 5.2 MB (66.19%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.go
+++ 496-next-greater-element-i/next-greater-element-i.go
@@ -1,22 +1,27 @@
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
-    idx := map[int]int{}
     st := []int{} // idx
-    ng := make([]int, len(nums2))
-    for i := 0; i < len(nums2); i++ {
+    n1 := len(nums1)
+    n2 := len(nums2)
+    idx := map[int]int{}
+    ng := make([]int, n2)
+    for i := 0; i < n2; i++ {
         ng[i] = -1
         idx[nums2[i]] = i
-        for len(st) > 0 && nums2[i] > nums2[st[len(st)-1]] {
+        for len(st) != 0 && nums2[i] > nums2[st[len(st)-1]] {
             top := st[len(st)-1]
             st = st[:len(st)-1]
-            ng[top] = nums2[i]        
+            ng[top] = nums2[i]
         }
         st = append(st, i)
     }
-    res := make([]int, len(nums1))
-    for i := 0; i < len(nums1); i++ {
-        pos, ok := idx[nums1[i]]
-        if !ok {continue}
-        res[i] = ng[pos]
+    out := make([]int, n1)
+    for i := 0; i < n1; i++ {
+        val, ok := idx[nums1[i]]
+        if !ok {
+            out[i] = -1
+        } else {
+            out[i] = ng[val]
+        }
     }
-    return res
+    return out
 }
