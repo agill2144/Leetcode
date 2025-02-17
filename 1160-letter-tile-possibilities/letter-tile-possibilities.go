8b43f919b277ae0fb236d6c52aa80360c1db76c6
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-16 17:21:57 -0800

    Time: 35 ms (7.69%) | Memory: 9.2 MB (11.54%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.go
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.go
@@ -1,29 +1,19 @@
 func numTilePossibilities(tiles string) int {
     if len(tiles) <= 1 {return len(tiles)}
-    freq := map[byte]int{}
-    for i := 0; i < len(tiles); i++ {freq[tiles[i]]++}
-    count := 0
-    var dfs func()
-    dfs = func() {
+    seqs := map[string]bool{}
+    var dfs func(set map[int]bool, path string)
+    dfs = func(set map[int]bool, path string){
         // base
-
+        if len(path) > 0 {seqs[path]=true}
         // logic
-        count++
-        for k, v := range freq {
-            if v == 0 {continue}
-            // action
-            freq[k]--
-            // recurse
-            dfs()
-            // backtrack
-            freq[k]++
+        for i := 0; i < len(tiles); i++ {
+            if !set[i] {
+                set[i] = true
+                dfs(set, path + string(tiles[i]))
+                set[i] = false
+            }
         }
-        
     }
-    dfs()
-    // why -1 ?
-    // because we counted a empty path when we first start
-    // i.e when dfs first starts, our path == ""
-    // which is not valid, therefore to remove that extra +1 count, we do a -1 at the end
-    return count-1
+    dfs(map[int]bool{},"")
+    return len(seqs)
 }
