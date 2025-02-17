30cea0d3db32f2f3a0d9495fb20c06719e1b4d59
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-16 17:09:47 -0800

    Time: 7 ms (42.31%) | Memory: 3.8 MB (100.00%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.go
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.go
@@ -21,5 +21,9 @@ func numTilePossibilities(tiles string) int {
         
     }
     dfs()
+    // why -1 ?
+    // because we counted a empty path when we first start
+    // i.e when dfs first starts, our path == ""
+    // which is not valid, therefore to remove that extra +1 count, we do a -1 at the end
     return count-1
 }
