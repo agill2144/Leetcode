82b009eda849894e934fa2203fbeede021955520
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-20 12:31:27 -0800

    Time: 33 ms (6.79%) | Memory: 9.1 MB (9.88%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.go
@@ -0,0 +1,31 @@
+func getHappyString(n int, k int) string {
+    if n <= 1 && k > 3 {return ""}
+    paths := []string{}
+    choices := []string{"a","b","c"}
+    var dfs func (path string)
+    dfs = func (path string){
+        // base
+        if len(path) >= n {
+            if len(path) == n {
+                paths = append(paths, path)
+            }
+            return
+        }
+
+        // logic
+        for i := 0; i < len(choices); i++ {
+            curr := choices[i]
+            if path == "" {
+                dfs(path + curr)
+            } else {
+                if curr != string(path[len(path)-1]) {
+                    dfs(path + curr)
+                }
+            }
+        }
+    }
+    dfs("")
+    if k-1 >= len(paths) {return ""}
+    sort.Strings(paths)
+    return paths[k-1]
+}
