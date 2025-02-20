a93425bde6c437c8c487c18bbcc874f22ed87b7a
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-20 12:40:10 -0800

    Time: 0 ms (100.00%) | Memory: 5.1 MB (42.39%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.go
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.go
@@ -2,27 +2,29 @@ func getHappyString(n int, k int) string {
     if n <= 1 && k > 3 {return ""}
     paths := []string{}
     choices := []string{"a","b","c"}
-    var dfs func (path string)
-    dfs = func (path string){
+    var dfs func (path string) bool
+    dfs = func (path string) bool{
         // base
         if len(path) >= n {
             if len(path) == n {
                 paths = append(paths, path)
+                if len(paths) == k {return true}
             }
-            return
+            return false
         }
 
         // logic
         for i := 0; i < len(choices); i++ {
             curr := choices[i]
             if path == "" {
-                dfs(path + curr)
+                if dfs(path + curr) {return true}
             } else {
                 if curr != string(path[len(path)-1]) {
-                    dfs(path + curr)
+                    if dfs(path + curr) {return true}
                 }
             }
         }
+        return false
     }
     dfs("")
     if k-1 >= len(paths) {return ""}
