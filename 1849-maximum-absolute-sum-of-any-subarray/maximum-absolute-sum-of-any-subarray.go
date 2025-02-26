043b1a3b53e2dd8ecc5e412df4f8fd1c8849b88d
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-02-25 21:27:16 -0800

    Time: 1 ms (14.29%) | Memory: 9.7 MB (50.00%) - LeetSync

---
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.go
@@ -0,0 +1,16 @@
+func maxAbsoluteSum(nums []int) int {
+    minSoFar := 0
+    maxSoFar := 0
+    res := 0
+    for i := 0; i < len(nums); i++ {
+        minSoFar = min(0, minSoFar+nums[i])
+        maxSoFar = max(0, maxSoFar+nums[i])
+        res = max(abs(maxSoFar), max(abs(minSoFar),res))
+    }
+    return res
+}
+
+func abs(x int) int {
+    if x < 0 {return x * -1}
+    return x
+}
