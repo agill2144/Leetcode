fcf1b9887d67c7b9ff97b6905ad79f102ae933a2
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-03-03 21:50:11 -0800

    Time: 1 ms (20.30%) | Memory: 6.3 MB (63.29%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
@@ -1,11 +1,16 @@
 func removeDuplicates(nums []int) int {
     slow := 1
     for i := 1; i < len(nums); i++ {
-        curr := nums[i]; prev := nums[i-1]
-        if curr != prev {
+        if nums[i] != nums[i-1] {
             nums[slow] = nums[i]
             slow++
         }
     }
     return slow
 }
+
+/*
+              s
+    0,1,2,3,4,2,2,3,3,4
+                        i
+*/
