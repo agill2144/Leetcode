8e81c09472fd029b2530077549edcc27e5b96f4e
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-04-28 19:57:40 -0700

    Time: 1765 ms (5.01%) | Memory: 27 MB (5.07%) - LeetSync

--- 15-3sum/3sum.go
+++ 15-3sum/3sum.go
@@ -1,21 +1,22 @@
 func threeSum(nums []int) [][]int {
+    target := 0
     set := map[[3]int]bool{}
+    n := len(nums)
     out := [][]int{}
-    for i := 0; i < len(nums); i++ {
-        target := 0-nums[i]
-        comp := map[int]bool{}
-        for j := i+1; j < len(nums); j++ {
-            diff := target - nums[j]
-            if comp[diff] {
-                trips := []int{nums[i],diff, nums[j]}
-                sort.Ints(trips)
-                tmp := [3]int{trips[0],trips[1], trips[2]}
-                if !set[tmp] {
-                    out = append(out, trips)
-                    set[tmp] = true
+    for i := 0; i < n; i++ {
+        iTarget := target - nums[i]
+        set2 := map[int]bool{}
+        for j := i+1; j < n; j++ {
+            diff := iTarget - nums[j]
+            if set2[diff] {
+                tmp := []int{nums[i], nums[j], diff}
+                sort.Ints(tmp)
+                if !set[[3]int{tmp[0],tmp[1],tmp[2]}] {
+                    out = append(out, tmp)
+                    set[[3]int{tmp[0],tmp[1],tmp[2]}] = true
                 }
             }
-            comp[nums[j]] = true
+            set2[nums[j]] = true
         }
     }
     return out
