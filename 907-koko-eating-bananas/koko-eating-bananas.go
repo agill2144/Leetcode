e149c64539f98617141b6550a715e5b9ff9a6921
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-05-04 16:56:39 -0700

    Time: 4 ms (61.49%) | Memory: 8.2 MB (98.49%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.go
+++ 907-koko-eating-bananas/koko-eating-bananas.go
@@ -1,26 +1,19 @@
 func minEatingSpeed(piles []int, h int) int {
-    right := 0
     left := 1
-    for i := 0; i < len(piles); i++ {
-        right += piles[i]
-    }
-    ans := -1
+    right := slices.Max(piles)
+    res := -1
     for left <= right {
         mid := left + (right-left)/2
-        speed := mid
         hours := 0
-        for j := 0; j < len(piles); j++ {
-            hours += int(math.Ceil(float64(piles[j])/float64(speed)))
+        for i := 0; i < len(piles); i++ {
+            hours += int(math.Ceil(float64(piles[i])/float64(mid)))
         }
-        // when does mid not work?
-        // when hours taken were more than allowed h
-        // meaning we need increase the amount eaten per hours ( i.e left = mid+1 )
         if hours > h {
             left = mid+1
         } else {
-            ans = mid
+            res = mid
             right = mid-1
         }
     }
-    return ans
+    return res
 }
