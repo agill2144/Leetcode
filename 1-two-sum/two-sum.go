f9aefcfb97c907fddbbdbb87df7c6fc9a896af3c
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-05-18 20:18:37 -0700

    Time: 0 ms (100.00%) | Memory: 6.1 MB (19.02%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,13 +1,25 @@
-// brute force - n^2
-// using nested iterations, check all pairs
+// compliment search using hashing
+// tc = o(n)
+// sc = o(n)
 func twoSum(nums []int, target int) []int {
-    n := len(nums)
-    for i := 0; i < n; i++ {
-        for j := i+1; j < n; j++ {
-            if nums[i] + nums[j] == target {
-                return []int{i, j}
-            } 
-        }
+    idx := map[int]int{}
+    for i := 0; i < len(nums); i++ {
+        diff := target-nums[i]
+        if val, ok := idx[diff]; ok {return []int{val, i}}
+        idx[nums[i]] = i
     }
     return nil
 }
+// brute force - n^2
+// using nested iterations, check all pairs
+// func twoSum(nums []int, target int) []int {
+//     n := len(nums)
+//     for i := 0; i < n; i++ {
+//         for j := i+1; j < n; j++ {
+//             if nums[i] + nums[j] == target {
+//                 return []int{i, j}
+//             } 
+//         }
+//     }
+//     return nil
+// }
