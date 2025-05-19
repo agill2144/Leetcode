6ec8af96b0bb801f00a8bfe7db2b9328c21c500e
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-05-18 20:22:58 -0700

    Time: 0 ms (100.00%) | Memory: 6 MB (19.02%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -10,6 +10,29 @@ func twoSum(nums []int, target int) []int {
     }
     return nil
 }
+
+// sorted = 1. binary search
+// NOT POSSIBLE - WE NEED TO RETURN OG IDX, SORTING FUCKS IT UP ( unless we idx them before, then like .. why do this lol)
+// func twoSum(nums []int, target int) []int {
+//     sort.Ints(nums)
+//     for i := 0; i < len(nums); i++ {
+//         t := target-nums[i]
+//         left := i+1
+//         right := len(nums)-1
+//         for left <= right {
+//             mid := left + (right-left)/2
+//             if nums[mid] == t && mid != i {return []int{i, mid}}
+//             if t > nums[mid] {
+//                 left = mid+1
+//             } else {
+//                 right = mid-1
+//             }
+//         }
+//     }
+//     return nil
+// }
+
+
 // brute force - n^2
 // using nested iterations, check all pairs
 // func twoSum(nums []int, target int) []int {
