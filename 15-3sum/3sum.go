3f9809b36deadf04f72896cb6ea90133bf279443
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-05-19 20:44:41 -0700

    Time: 1526 ms (5.02%) | Memory: 22.1 MB (5.06%) - LeetSync

--- 15-3sum/3sum.go
+++ 15-3sum/3sum.go
@@ -1,51 +1,45 @@
 func threeSum(nums []int) [][]int {
-    sort.Ints(nums)
+    target := 0
+    n := len(nums)
+    set := map[[3]int]bool{}
     out := [][]int{}
-    for i := 0; i < len(nums); i++ {
-        if i != 0 && nums[i] == nums[i-1] {continue}
-        left := i+1
-        right := len(nums)-1
-        for left < right {
-            total := nums[i] + nums[left] + nums[right]
-            if total == 0 {
-                out = append(out, []int{nums[i], nums[left], nums[right]})
-                left++ 
-                for left < right && nums[left] == nums[left-1] {
-                    left++
+    for i := 0; i < n; i++ {
+        iTarget := target-nums[i]
+        cp := map[int]bool{}
+        for j := i+1; j < n; j++ {
+            diff := iTarget - nums[j]
+            if cp[diff] {
+                tmp := []int{nums[i], nums[j], diff}
+                sort.Ints(tmp)
+                if !set[[3]int{tmp[0], tmp[1], tmp[2]}] {
+                    set[[3]int{tmp[0], tmp[1], tmp[2]}] = true
+                    out = append(out, tmp)
                 }
-                right--
-                for left < right && nums[right] == nums[right+1] {
-                    right--
-                }
-            } else if total > 0 {
-                right--
-            } else {
-                left++
             }
+            cp[nums[j]] = true
         }
     }
     return out
-
 }
+// brute force: n^3
 // func threeSum(nums []int) [][]int {
 //     target := 0
-//     set := map[[3]int]bool{}
 //     n := len(nums)
+//     set := map[[3]int]bool{}
 //     out := [][]int{}
 //     for i := 0; i < n; i++ {
-//         iTarget := target - nums[i]
-//         set2 := map[int]bool{}
 //         for j := i+1; j < n; j++ {
-//             diff := iTarget - nums[j]
-//             if set2[diff] {
-//                 tmp := []int{nums[i], nums[j], diff}
-//                 sort.Ints(tmp)
-//                 if !set[[3]int{tmp[0],tmp[1],tmp[2]}] {
-//                     out = append(out, tmp)
-//                     set[[3]int{tmp[0],tmp[1],tmp[2]}] = true
+//             for k := j+1; k < n; k++ {
+//                 sum := nums[i] + nums[j] + nums[k]
+//                 if sum == target {
+//                     tmp := []int{nums[i], nums[j], nums[k]}
+//                     sort.Ints(tmp)
+//                     if !set[[3]int{tmp[0], tmp[1], tmp[2]}] {
+//                         set[[3]int{tmp[0], tmp[1], tmp[2]}] = true
+//                         out = append(out, tmp)
+//                     }
 //                 }
 //             }
-//             set2[nums[j]] = true
 //         }
 //     }
 //     return out
