a52abc5b62916fe5bb65c19fa56f4693df8bb359
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-16 15:31:18 -0700

    Time: 2 ms (19.24%) | Memory: 9.5 MB (52.17%) - LeetSync

--- 189-rotate-array/rotate-array.go
+++ 189-rotate-array/rotate-array.go
@@ -1,19 +1,32 @@
 func rotate(nums []int, k int)  {
     n := len(nums)
     k %= n
-    // k = 3
-    // [1,2,3,4,5,6,7]
-    // [7,6,5,4,3,2,1]
-    reverse(nums, 0, n-1)
-    // [5,6,7,4,3,2,1]
-    reverse(nums, 0, k-1)
-    reverse(nums, k, n-1)
-    
+    if k == 0 {return}
+    reverse(0, n-1, nums)
+    reverse(0, k-1, nums)
+    reverse(k, n-1, nums)
 }
 
-func reverse(nums []int, left, right int) {
+func reverse(left, right int, nums []int) {
     for left < right {
         nums[left], nums[right] = nums[right], nums[left]
-        left++; right--
+        left++
+        right--
     }
 }
+
+/*
+    end = [5,6,7,1,2,3,4]
+
+    [1,2,3,4,5,6,7]
+
+    (0, n-1)
+    [7,6,5,4,3,2,1]
+
+    (0, k-1)
+    [5,6,7,4,3,2,1]
+
+    (k, n-1)
+    [5,6,7,1,2,3,4]
+
+*/
