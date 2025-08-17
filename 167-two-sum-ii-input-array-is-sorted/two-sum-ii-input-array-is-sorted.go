1d2fc8961ded2f859302a539865a51464d8663ad
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-17 13:26:41 -0700

    Time: 0 ms (100.00%) | Memory: 7.7 MB (90.24%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
@@ -1,13 +1,31 @@
-// compliment search will obv work
-// tc = o(n)
-// sc = o(n)
+// sorted 
+// 1. two ptrs
 func twoSum(nums []int, target int) []int {
-    idx := map[int]int{}
-    for i := 0; i < len(nums); i++ {
-        diff := target - nums[i]
-        val, ok := idx[diff]
-        if ok {return []int{val+1, i+1}}
-        idx[nums[i]] = i
+    n := len(nums)
+    left := 0
+    right := n-1
+    for left < right {
+        sum := nums[left] + nums[right]
+        if sum == target {return []int{left+1, right+1}}
+        if sum > target {
+            right--
+        } else {
+            left++
+        }
     }
     return nil
 }
+
+// compliment search will obv work
+// tc = o(n)
+// sc = o(n)
+// func twoSum(nums []int, target int) []int {
+//     idx := map[int]int{}
+//     for i := 0; i < len(nums); i++ {
+//         diff := target - nums[i]
+//         val, ok := idx[diff]
+//         if ok {return []int{val+1, i+1}}
+//         idx[nums[i]] = i
+//     }
+//     return nil
+// }
