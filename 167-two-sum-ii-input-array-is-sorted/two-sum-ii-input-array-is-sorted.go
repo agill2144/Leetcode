995b1fa1f035439691e88c33df7d0bf603329180
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-17 13:25:36 -0700

    Time: 0 ms (100.00%) | Memory: 8.1 MB (18.12%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
@@ -1,43 +1,13 @@
-// sorted?
-// binary search
-// tc = o(nlogn)
-// sc = o(1)
+// compliment search will obv work
+// tc = o(n)
+// sc = o(n)
 func twoSum(nums []int, target int) []int {
-    n := len(nums)
-    for i := 0; i < n; i++ {
-        left := i+1
-        right := n-1
-        for left <= right {
-            mid := left+(right-left)/2
-            sum := nums[i] + nums[mid]
-            if sum == target {return []int{i+1, mid+1}}
-            if sum > target {
-                right = mid-1
-            } else {
-                left = mid+1
-            }
-        }
+    idx := map[int]int{}
+    for i := 0; i < len(nums); i++ {
+        diff := target - nums[i]
+        val, ok := idx[diff]
+        if ok {return []int{val+1, i+1}}
+        idx[nums[i]] = i
     }
     return nil
 }
-// sorted?
-// two ptrs
-// tc = o(n)
-// sc = o(1)
-// func twoSum(nums []int, target int) []int {
-//     n := len(nums)
-//     if n <= 1 {return nil}
-//     left := 0
-//     right := n-1
-
-//     for left < right {
-//         sum := nums[left] + nums[right]
-//         if sum == target {return []int{left+1, right+1}}
-//         if sum > target {
-//             right--
-//         } else {
-//             left++
-//         }
-//     }
-//     return nil
-// }
