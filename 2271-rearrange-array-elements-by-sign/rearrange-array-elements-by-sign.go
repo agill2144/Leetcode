7325334b3779941cfa897c0860a535c90bbe6906
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-17 12:19:14 -0700

    Time: 4 ms (88.11%) | Memory: 15.9 MB (49.65%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.go
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.go
@@ -1,16 +1,17 @@
 func rearrangeArray(nums []int) []int {
-    pos := 0
-    neg := 1
     n := len(nums)
     out := make([]int, n)
+    pos := 0
+    neg := 1
     for i := 0; i < n; i++ {
         if nums[i] > 0 {
             out[pos] = nums[i]
-            pos+=2
+            pos += 2
         } else {
             out[neg] = nums[i]
-            neg+=2
+            neg += 2
         }
     }
     return out
+
 }
