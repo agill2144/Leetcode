b3f052ebf311e1448530b4ecf5e3d23750827d10
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-18 18:31:38 -0700

    Time: 1411 ms (5.01%) | Memory: 13.1 MB (6.65%) - LeetSync

--- 15-3sum/3sum.go
+++ 15-3sum/3sum.go
@@ -1,72 +1,46 @@
 func threeSum(nums []int) [][]int {
     sort.Ints(nums)
-    n := len(nums)
-    out := [][]int{}
-    for i := 0; i < n; i++ {
-        if i != 0 && nums[i] == nums[i-1] {continue}
-        left := i+1
-        right := n-1
-        for left < right {
-            sum := nums[i] + nums[left] + nums[right]
-            if sum == 0 {
-                out = append(out, []int{nums[i], nums[left], nums[right]})
-                left++
-                for left < right && nums[left] == nums[left-1] {left++}
-                right--
-                for left < right && nums[right] == nums[right+1] {right--}
-            } else if sum > 0 {
-                right--
-            } else {
-                left++
+    set := map[[3]int]bool{}
+    res := [][]int{}
+    for i := 0; i < len(nums); i++ {
+        iTarget := 0-nums[i]
+        m := map[int]bool{}
+        for j := i+1; j < len(nums); j++ {
+            diff := iTarget - nums[j]
+            if m[diff] {
+                tmp := []int{nums[i], nums[j], diff}
+                sort.Ints(tmp)
+                if !set[[3]int{tmp[0], tmp[1], tmp[2]}] {
+                    set[[3]int{tmp[0], tmp[1], tmp[2]}] = true
+                    res = append(res, tmp)
+                }   
             }
+            m[nums[j]] = true
         }
     }
-    return out
+    return res
 }
-// n^2 - two sum compliment search using hashing
+// brute force, 3 nested loops, and check each triplet sum
+// tc = o(n * n * (n * 3log3 + 3) = o(n^3)
+// sc = o(n)
 // func threeSum(nums []int) [][]int {
-//     target := 0
-//     n := len(nums)
 //     set := map[[3]int]bool{}
-//     out := [][]int{}
-//     for i := 0; i < n; i++ {
-//         iTarget := target-nums[i]
-//         cp := map[int]bool{}
-//         for j := i+1; j < n; j++ {
-//             diff := iTarget - nums[j]
-//             if cp[diff] {
-//                 tmp := []int{nums[i], nums[j], diff}
-//                 sort.Ints(tmp)
-//                 if !set[[3]int{tmp[0], tmp[1], tmp[2]}] {
-//                     set[[3]int{tmp[0], tmp[1], tmp[2]}] = true
-//                     out = append(out, tmp)
-//                 }
-//             }
-//             cp[nums[j]] = true
-//         }
-//     }
-//     return out
-// }
-// brute force: n^3
-// func threeSum(nums []int) [][]int {
-//     target := 0
+//     res := [][]int{}
 //     n := len(nums)
-//     set := map[[3]int]bool{}
-//     out := [][]int{}
-//     for i := 0; i < n; i++ {
-//         for j := i+1; j < n; j++ {
+//     for i := 0; i < n-2; i++ {
+//         for j := i+1; j < n-1; j++ {
 //             for k := j+1; k < n; k++ {
 //                 sum := nums[i] + nums[j] + nums[k]
-//                 if sum == target {
+//                 if sum == 0 {
 //                     tmp := []int{nums[i], nums[j], nums[k]}
 //                     sort.Ints(tmp)
 //                     if !set[[3]int{tmp[0], tmp[1], tmp[2]}] {
 //                         set[[3]int{tmp[0], tmp[1], tmp[2]}] = true
-//                         out = append(out, tmp)
+//                         res = append(res, tmp)
 //                     }
 //                 }
 //             }
 //         }
-//     }
-//     return out
+//     } 
+//     return res
 // }
