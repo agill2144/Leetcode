9b8b0455a6f1e8146ca8cf43828da3d80d9b161e
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-19 08:14:37 -0700

    Time: 0 ms (100.00%) | Memory: 9.4 MB (97.33%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.go
+++ 53-maximum-subarray/maximum-subarray.go
@@ -1,10 +1,27 @@
 func maxSubArray(nums []int) int {
-    maxSum := math.MinInt64
+    n := len(nums)
     rSum := 0
-    for i := 0; i < len(nums); i++ {
+    res := math.MinInt64
+    for i := 0; i < n; i++ {
         rSum += nums[i]
-        maxSum = max(maxSum, rSum)
+        res = max(res, rSum)
         if rSum < 0 {rSum = 0}
     }
-    return maxSum
+    return res
 }
+// brute force, form all possible subarrays
+// tc = n^2
+// sc = o(1)
+// func maxSubArray(nums []int) int {
+//     n := len(nums)
+//     res := math.MinInt64
+//     sum := 0
+//     for i := 0; i < n; i++ {
+//         for j := i; j < len(nums); j++ {
+//             sum += nums[j]
+//             res = max(res, sum)
+//         }
+//         sum = 0
+//     }
+//     return res
+// }
