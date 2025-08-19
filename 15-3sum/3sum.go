46b8e7d72f315bd826214aaa1e908b61c5686b63
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-18 18:51:54 -0700

    Time: 23 ms (55.25%) | Memory: 9.8 MB (39.24%) - LeetSync

--- 15-3sum/3sum.go
+++ 15-3sum/3sum.go
@@ -1,25 +1,55 @@
 func threeSum(nums []int) [][]int {
     sort.Ints(nums)
-    set := map[[3]int]bool{}
     res := [][]int{}
     for i := 0; i < len(nums); i++ {
-        iTarget := 0-nums[i]
-        m := map[int]bool{}
-        for j := i+1; j < len(nums); j++ {
-            diff := iTarget - nums[j]
-            if m[diff] {
-                tmp := []int{nums[i], nums[j], diff}
-                sort.Ints(tmp)
-                if !set[[3]int{tmp[0], tmp[1], tmp[2]}] {
-                    set[[3]int{tmp[0], tmp[1], tmp[2]}] = true
-                    res = append(res, tmp)
-                }   
+        if i-1 >= 0 && nums[i] == nums[i-1] {continue}
+        left := i+1
+        right := len(nums)-1
+        for left < right {
+            total := nums[i] + nums[left] + nums[right]
+            if total == 0 {
+                res = append(res, []int{nums[i], nums[left], nums[right]})
+                left++
+                for left < right && nums[left] == nums[left-1] {
+                    left++
+                }
+                right--
+                for left < right && nums[right] == nums[right+1] {right--}
+            } else if total > 0 {
+                right--
+            } else {
+                left++
             }
-            m[nums[j]] = true
         }
     }
     return res
 }
+
+// tc = o(nlogn) + o(n^2)
+// sc = o(n)
+// func threeSum(nums []int) [][]int {
+//     sort.Ints(nums)
+//     set := map[[3]int]bool{}
+//     res := [][]int{}
+//     for i := 0; i < len(nums); i++ {
+//         iTarget := 0-nums[i]
+//         m := map[int]bool{}
+//         for j := i+1; j < len(nums); j++ {
+//             diff := iTarget - nums[j]
+//             if m[diff] {
+//                 tmp := []int{nums[i], nums[j], diff}
+//                 sort.Ints(tmp)
+//                 if !set[[3]int{tmp[0], tmp[1], tmp[2]}] {
+//                     set[[3]int{tmp[0], tmp[1], tmp[2]}] = true
+//                     res = append(res, tmp)
+//                 }   
+//             }
+//             m[nums[j]] = true
+//         }
+//     }
+//     return res
+// }
+
 // brute force, 3 nested loops, and check each triplet sum
 // tc = o(n * n * (n * 3log3 + 3) = o(n^3)
 // sc = o(n)
