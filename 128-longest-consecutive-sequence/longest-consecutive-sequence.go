ba43e555e65ef39e640842db127122b33cba9ccb
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-24 15:59:55 -0700

    Time: 51 ms (56.25%) | Memory: 10.8 MB (90.99%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.go
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.go
@@ -1,3 +1,9 @@
+// Make the numbers searchable by tossing them in a set
+// this also de-dupes elements
+// therefore instead of looping thru input nums, now we can loop thru the deduped set
+// and use the set to find a start number and then keep
+// forming a chain until next num exists
+// while keeping track of current chain
 func longestConsecutive(nums []int) int {
 	set := map[int]bool{}
     for i := 0; i < len(nums); i++ {
