c4ecd51e0e0f2e06f9cc047f567c6f9f17111d4d
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-24 16:12:50 -0700

    Time: 68 ms (50.00%) | Memory: 23.6 MB (40.00%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.go
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.go
@@ -1,13 +1,12 @@
 func maxSubArrayLen(nums []int, k int) int {
     idx := map[int]int{0:-1}
     res := 0
+    n := len(nums)
     rSum := 0
-    for i := 0; i < len(nums); i++ {
+    for i := 0; i < n; i++ {
         rSum += nums[i]
-        diff := rSum - k
-        prevIdx, ok := idx[diff]
-        if ok {
-            res = max(res, i-prevIdx)
+        if val, ok := idx[rSum-k]; ok {
+            res = max(res, i-val)
         }
         if _, ok := idx[rSum]; !ok {
             idx[rSum] = i
