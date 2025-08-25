3211b2cdcd042498bdabda15de1d02ef27657ac7
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-24 17:25:32 -0700

    Time: 14 ms (32.71%) | Memory: 9.1 MB (91.78%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,47 +1,20 @@
-
+// n = len(strs)
+// k = avg len of each str
+// tc = o(n * (k+klogk+k)) + o(n)
+// sc = o(n)
 func groupAnagrams(strs []string) [][]string {
-    grps := map[[26]int][]string{}
-    for i := 0; i < len(strs); i++ {
-        key := naiveHash(strs[i])
-        grps[key] = append(grps[key], strs[i])
-    }
+    // sc = o(n)
+    grps := map[string][]string{}
+    for i := 0; i < len(strs); i++ { // n
+        chars := strings.Split(strs[i],"") // k 
+        sort.Strings(chars) // klogk
+        sorted := strings.Join(chars, "") // k
+        grps[sorted] = append(grps[sorted], strs[i]) // k
+    } // n * (k + klogk + k)
     out := [][]string{}
-    for _, v := range grps {out = append(out, v)}
+    // n
+    for _, v := range grps {
+        out = append(out, v)
+    }
     return out
 }
-
-func naiveHash(key string) [26]int {
-    freq := [26]int{}
-    for i := 0; i < len(key); i++ {
-        idx := int(key[i]-'a')
-        freq[idx]++
-    }
-    return freq
-}
-
-var primes []float64 = []float64{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107}
-func hash(key string) float64 {
-    prod := 1.0
-    for i := 0; i < len(key); i++ {
-        prod *= (primes[int(key[i]-'a')])
-    }
-    return prod
-}
-// func groupAnagrams(strs []string) [][]string {
-//     groups := map[string][]string{}
-//     // n = len strs
-//     // k = avg len of each word
-//     // (n * klogk) + n
-//     // space = o(k + n)
-//     for i := 0; i < len(strs); i++ {
-//         word := strings.Split(strs[i], "")
-//         sort.Strings(word)
-//         sortStr := strings.Join(word,"")
-//         groups[sortStr] = append(groups[sortStr], strs[i])
-//     }
-//     out := [][]string{}
-//     for _, v := range groups {
-//         out = append(out, v)
-//     }
-//     return out
-// }
