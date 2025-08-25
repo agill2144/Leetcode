ca40fe7fa7af7219c907f30720cfab2a7c9b221c
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-25 16:00:05 -0700

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.68%) - LeetSync

--- 792-binary-search/binary-search.go
+++ 792-binary-search/binary-search.go
@@ -5,7 +5,11 @@ func search(nums []int, target int) int {
     for left <= right {
         mid := left + (right-left)/2
         if nums[mid] == target {return mid}
-        if target > nums[mid] {left = mid+1} else {right = mid-1}
+        if target > nums[mid] {
+            left = mid+1
+        }  else {
+            right = mid-1
+        }
     }
     return -1
 }
