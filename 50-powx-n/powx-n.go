159a7c5b58eeebc58993cbc60fd527937e0b8aa2
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-27 17:15:21 -0700

    Time: 0 ms (100.00%) | Memory: 4 MB (28.60%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,18 +1,20 @@
+// x^n can be rewritten as : (x^n/2) * (x^n/2)
+// we can recursively solve for one of the expressions
+// and multiply an extra base term when n is odd
 func myPow(x float64, n int) float64 {
-    // x^n  = x^n/2 * x^n/2
-    if n < 0 {
-        n *= -1
-        x = 1/x
-    }
-    var res float64 = 1.0
-    for n != 0 {
-        if n % 2 == 0 {
-            x *= x
-            n /= 2
-        } else {
-            res *= x
-            n--
+    if n < 0 {x = 1/x; n*=-1}
+    var dfs func(base float64, exp int) float64
+    dfs = func(base float64, exp int) float64 {
+        // base
+        if exp <= 1 {
+            if exp == 1 {return base}
+            return 1.0
         }
+        // logic
+        res := dfs(base, exp/2)
+        res *= res
+        if exp % 2 != 0 {res *= base}
+        return res
     }
-    return res
+    return dfs(x, n)
 }
