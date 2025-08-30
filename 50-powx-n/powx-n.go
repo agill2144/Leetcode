23acf6b983a28455bcad6a49dca2fc234f7663e6
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-30 16:46:22 -0700

    Time: 0 ms (100.00%) | Memory: 4.1 MB (29.15%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -9,28 +9,50 @@
     tc = o(logn)
     sc = o(logn) - recursive stack
 */
+// func myPow(x float64, n int) float64 {
+//     if n < 0 {
+//         x = 1/x
+//         n *= -1
+//     }
+//     var res float64 = 1.0
+//     for n != 0 {
+//         if n % 2 == 0 {
+//             x *= x
+//             n /= 2
+//         } else {
+//             // save the current modified base ( which is also the answer created so far to multiply later )
+//             // when n = 3
+//             // x = has become some updated value
+//             // we need to save the current base to be multiplied later
+//             // we are collecting all the base in res 
+//             res *= x
+//             // keep x going as is
+//             // when n = 1
+//             // this condition will run again and thereby saving our updated x and multiplying the saved bases in res
+//             n--
+//         }
+//     }
+//     return res
+// }
+
+
 func myPow(x float64, n int) float64 {
     if n < 0 {
         x = 1/x
         n *= -1
     }
-    var res float64 = 1.0
-    for n != 0 {
-        if n % 2 == 0 {
-            x *= x
-            n /= 2
-        } else {
-            // save the current modified base ( which is also the answer created so far to multiply later )
-            // when n = 3
-            // x = has become some updated value
-            // we need to save the current base to be multiplied later
-            // we are collecting all the base in res 
-            res *= x
-            // keep x going as is
-            // when n = 1
-            // this condition will run again and thereby saving our updated x and multiplying the saved bases in res
-            n--
+    var dfs func(base float64, exp int) float64
+    dfs = func(base float64, exp int) float64 {
+        // base
+        if exp <= 1 {
+            if exp == 1 {return base}
+            return 1.0
         }
+        // logic
+        res := dfs(base*base, exp/2)
+        if exp % 2 != 0 {res *= base}
+        return res
     }
-    return res
+    return dfs(x, n)
+
 }
