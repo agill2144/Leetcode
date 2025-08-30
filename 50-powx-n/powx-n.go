8a697a7c82deb1e617b24c6b16f27d73723da041
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-30 16:43:57 -0700

    Time: 0 ms (100.00%) | Memory: 4 MB (81.17%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,20 +1,30 @@
-// x^n can be rewritten as : (x^n/2) * (x^n/2)
-// we can recursively solve for one of the expressions
-// and multiply an extra base term when n is odd
+/*
+    binary exponentiation: repeatedly square the base, and half the exp
+    - when exp is odd, save the current base to multiply LATER (because PEMDAS = exp first and then rem base terms)
+    - until exp > 0
+*/
 func myPow(x float64, n int) float64 {
-    if n < 0 {x = 1/x; n*=-1}
-    var dfs func(base float64, exp int) float64
-    dfs = func(base float64, exp int) float64 {
-        // base
-        if exp <= 1 {
-            if exp == 1 {return base}
-            return 1.0
-        }
-        // logic
-        res := dfs(base, exp/2)
-        res *= res
-        if exp % 2 != 0 {res *= base}
-        return res
+    if n < 0 {
+        x = 1/x
+        n *= -1
     }
-    return dfs(x, n)
+    var res float64 = 1.0
+    for n != 0 {
+        if n % 2 == 0 {
+            x *= x
+            n /= 2
+        } else {
+            // save the current modified base ( which is also the answer created so far to multiply later )
+            // when n = 3
+            // x = has become some updated value
+            // we need to save the current base to be multiplied later
+            // we are collecting all the base in res 
+            res *= x
+            // keep x going as is
+            // when n = 1
+            // this condition will run again and thereby saving our updated x and multiplying the saved bases in res
+            n--
+        }
+    }
+    return res
 }
