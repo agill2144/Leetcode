e43806b665251814e4ad957972269453b796cc20
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-30 16:36:11 -0700

    Time: 0 ms (100.00%) | Memory: 5.1 MB (4.30%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
@@ -11,13 +11,21 @@ func findMin(nums []int) int {
             right--
             continue
         }
+
+        // mid could an answer
         if (mid == 0 || nums[mid] < nums[mid-1]) && (mid == n-1 || nums[mid] < nums[mid+1]) {
             ans = min(ans, nums[mid])
         } 
-
+        // when right sorted
+        // right is larger than mid
+        // therefore again check if mid is a ans
         if nums[mid] <= nums[right] {
             ans = min(ans, nums[mid])
             right = mid-1
+
+        // when left sorted
+        // left is smaller than mid
+        // therefore check if left is a ans
         } else if nums[mid] >= nums[left] {
             ans = min(ans, nums[left])
             left = mid+1
