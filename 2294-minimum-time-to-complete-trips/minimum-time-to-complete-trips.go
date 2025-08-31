6661dc44c7a376cbaea7eca7db73a0f4d1dde293
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-31 16:26:51 -0700

    Time: 68 ms (44.44%) | Memory: 10.4 MB (57.78%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.go
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.go
@@ -1,26 +1,20 @@
-// n = len(time)
-// m = (minTime*totalTrips)-minTime+1
-// tc = o(logm*n)
-// sc = o(1)
 func minimumTime(time []int, totalTrips int) int64 {
-    left := math.MaxInt64
-    for i := 0; i < len(time); i++ {
-        left = min(left, time[i])
-    }
-    right := left*totalTrips
-    var res int64
+    left := 1
+    right := slices.Max(time)*totalTrips
+    var ans int64
     for left <= right {
         mid := left + (right-left)/2
+        // how many trips can be completed if mid was atMax time allowed per bus
         trips := 0
         for i := 0; i < len(time); i++ {
             trips += (mid/time[i])
         }
         if trips >= totalTrips {
-            res = int64(mid)
+            ans = int64(mid)
             right = mid-1
         } else {
             left = mid+1
         }
     }
-    return res
+    return ans
 }
