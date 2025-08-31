e26e7343f9d50bc687bf06eaf5184b172f037d04
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-31 16:44:52 -0700

    Time: 211 ms (100.00%) | Memory: 11.1 MB (100.00%) - LeetSync

--- 2702-maximum-number-of-integers-to-choose-from-a-range-ii/maximum-number-of-integers-to-choose-from-a-range-ii.go
+++ 2702-maximum-number-of-integers-to-choose-from-a-range-ii/maximum-number-of-integers-to-choose-from-a-range-ii.go
@@ -1,8 +1,6 @@
 func maxCount(banned []int, n int, maxSum int64) int {
     set := map[int]bool{}
-    for i := 0; i < len(banned); i++ {
-        set[banned[i]] = true
-    }
+    for i := 0; i < len(banned); i++ {set[banned[i]] = true}
     left := 1
     right := n
     ans := 0
@@ -10,18 +8,17 @@ func maxCount(banned []int, n int, maxSum int64) int {
         mid := left + (right-left)/2
         ap := (mid*(mid+1))/2
         count := mid
-        for k, _ := range set {
+        for k , _ := range set {
             if k <= mid {
-                count--
                 ap -= k
+                count--
             }
         }
-        // when does mid not work ?
-        if int64(ap) > maxSum {
-            right = mid-1
-        } else {
+        if int64(ap) <= maxSum {
             ans = count
             left = mid+1
+        } else {
+            right = mid-1
         }
     }
     return ans
