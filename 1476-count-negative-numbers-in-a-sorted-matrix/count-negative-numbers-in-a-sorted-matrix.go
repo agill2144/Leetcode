aba5d7b70814e807524135e92e26ae3eeee455e7
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-08-31 17:09:13 -0700

    Time: 0 ms (100.00%) | Memory: 6.9 MB (100.00%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.go
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.go
@@ -1,16 +1,16 @@
 func countNegatives(grid [][]int) int {
-    count := 0
     m := len(grid)
     n := len(grid[0])
     r := m-1
     c := 0
+    total := 0
     for r >= 0 && c < n {
         if grid[r][c] < 0 {
-            count += (n-c)
+            total += (n-c)
             r--
         } else {
             c++
         }
     }
-    return count
+    return total
 }
