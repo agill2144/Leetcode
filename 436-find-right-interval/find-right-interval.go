0451c0be6529bc7afca08845e9ec43738e00caa5
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-04 17:11:33 -0700

    Time: 20 ms (18.92%) | Memory: 9 MB (54.05%) - LeetSync

--- 436-find-right-interval/find-right-interval.go
+++ 436-find-right-interval/find-right-interval.go
@@ -3,43 +3,31 @@ func findRightInterval(intervals [][]int) []int {
     for i := 0; i < len(intervals); i++ {
         idx[[2]int{intervals[i][0], intervals[i][1]}] = i
     }
+
     sort.Slice(intervals, func(i, j int)bool{
         return intervals[i][0] < intervals[j][0]
     })
-    out := make([]int, len(intervals))
-    // "right interval for an interval i is an interval j such that startJ >= endI and startJ is minimized"
-    // find the left-most idx on right side of intervals[i][1]
+
+    res := make([]int, len(intervals))
     for i := 0; i < len(intervals); i++ {
+        // The right interval for an interval i is an interval j 
+        // such that startj >= endi and startj is minimized. Note that i may equal j
+        // so each endi we need leftmost startj on the right side of endi
         ogIdx := idx[[2]int{intervals[i][0], intervals[i][1]}]
-        out[ogIdx] = -1
-        endI := intervals[i][1]
-        // why include ith idx as part of our search?
-        // we are looking for right interval of i
-        // therefore should'nt this be i+1 ? i.e start from i+1
-        // no, because; "Note that i may equal j"
+        res[ogIdx] = -1
         left := i
         right := len(intervals)-1
+        endi := intervals[i][1]
         for left <= right {
             mid := left + (right-left)/2
-            startJ := intervals[mid][0]
-            if startJ >= endI {
-                out[ogIdx] = idx[[2]int{intervals[mid][0], intervals[mid][1]}]
-                right = mid-1
+            startj := intervals[mid][0]
+            if startj >= endi {
+                res[ogIdx] = idx[[2]int{intervals[mid][0], intervals[mid][1]}]
+                right = mid-1                
             } else {
                 left = mid+1
             }
         }
     }
-    return out
+    return res
 }
-
-/*
-    [[3,4],[2,3],[1,2]]
-    
-    ogIdxs = {
-        [3,4]: 0,
-        [2,3]: 1,
-        [1,2]: 2
-    }
-    [[1,2], [2,3], [3,4]]
-*/
