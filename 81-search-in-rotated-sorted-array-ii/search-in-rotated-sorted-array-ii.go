0d91682d0cf7371ac78efd51cbe6858bc1e5b842
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-06 17:54:45 -0700

    Time: 0 ms (100.00%) | Memory: 5 MB (35.56%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.go
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.go
@@ -1,26 +1,26 @@
-func search(nums []int, k int) bool {
+func search(nums []int, target int) bool {
     n := len(nums)
     left := 0
     right := n-1
     for left <= right {
         mid := left + (right-left)/2
-        if nums[mid] == k {return true}
+        if nums[mid] == target {return true}
         if nums[mid] == nums[left] && nums[mid] == nums[right] {
             left++
             right--
             continue
         }
         if nums[left] <= nums[mid] {
-            if k <= nums[mid] && k >= nums[left] {
+            if target >= nums[left] && target <= nums[mid] {
                 right = mid-1
             } else {
                 left = mid+1
             }
-        } else if nums[mid] <= nums[right] {
-            if k >= nums[mid] && k <= nums[right] {
+        } else {
+            if target >= nums[mid] && target <= nums[right] {
                 left = mid+1
             } else {
-                right = mid-1
+                right= mid-1
             }
         }
     }
