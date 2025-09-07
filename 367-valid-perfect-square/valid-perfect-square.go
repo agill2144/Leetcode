522853e49b0eb8ad65bc37f30c34970380fc420c
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-06 18:42:32 -0700

    Time: 0 ms (100.00%) | Memory: 3.8 MB (76.23%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.go
+++ 367-valid-perfect-square/valid-perfect-square.go
@@ -1,12 +1,15 @@
 func isPerfectSquare(num int) bool {
-    if num == 1 {return true}
     left := 1
-    right := num/2
+    right := num
     for left <= right {
         mid := left + (right-left)/2
         sq := mid*mid
         if sq == num {return true}
-        if sq > num {right = mid-1} else {left = mid+1}
+        if sq > num {
+            right = mid-1
+        } else {
+            left = mid+1
+        }
     }
     return false
 }
