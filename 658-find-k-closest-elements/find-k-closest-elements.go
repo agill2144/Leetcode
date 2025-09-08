58bbcd3a53ef26cba979776a9b1170d78e600e45
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-07 17:27:36 -0700

    Time: 0 ms (100.00%) | Memory: 8.7 MB (85.76%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.go
+++ 658-find-k-closest-elements/find-k-closest-elements.go
@@ -1,22 +1,41 @@
-// sorted;
-// 1. two pointers
 func findClosestElements(arr []int, k int, x int) []int {
     n := len(arr)
     left := 0
-    right := n-1
-    for right-left+1 != k {
-        leftDist := abs(arr[left]-x)
-        rightDist := abs(arr[right]-x)
+    right := n-k
+    for left <= right {
+        mid := left+(right-left)/2
+        if mid + k >= len(arr) {break}
+        leftDist := x-arr[mid]
+        rightDist := arr[mid+k]-x
         if leftDist <= rightDist {
-            right--
+            right = mid-1
         } else {
-            left++
+            left = mid+1
         }
     }
-    return arr[left:right+1]
+    return arr[left:left+k]
 }
+// sorted;
+// 1. two pointers
+// tc = o(n-k) + o(k)
+// sc = o(1)
+// func findClosestElements(arr []int, k int, x int) []int {
+//     n := len(arr)
+//     left := 0
+//     right := n-1
+//     for right-left+1 != k {
+//         leftDist := abs(arr[left]-x)
+//         rightDist := abs(arr[right]-x)
+//         if leftDist <= rightDist {
+//             right--
+//         } else {
+//             left++
+//         }
+//     }
+//     return arr[left:right+1]
+// }
 
-func abs(x int) int {
-    if x < 0 {return x*-1}
-    return x
-}
+// func abs(x int) int {
+//     if x < 0 {return x*-1}
+//     return x
+// }
