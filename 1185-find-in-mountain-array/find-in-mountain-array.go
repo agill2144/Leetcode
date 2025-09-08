53aca9606ec5ba60c5b241072abf3b6ecb93662c
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-08 15:54:52 -0700

    Time: 6 ms (53.85%) | Memory: 5.2 MB (74.36%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.go
+++ 1185-find-in-mountain-array/find-in-mountain-array.go
@@ -8,54 +8,60 @@
  * func (this *MountainArray) length() int {}
  */
 
-func findInMountainArray(target int, mt *MountainArray) int {
-    n := mt.length()
+func findInMountainArray(target int, m *MountainArray) int {
+    // 1. find peak, so that we have two sorted arrays
+    // - left side is increasing
+    // - right side is decreasing
+    // could there be more than 1 peak? 
+    // - not clear from the question, but examples suggest that 
+    // - there wont be more than 1 peak
+    // mid is peak if its greater than both its adj neis
+    n := m.length()
     left := 0
     right := n-1
     peakIdx := -1
-    peakVal := -1
+    peakVal := math.MinInt64
     for left <= right {
         mid := left + (right-left)/2
-        val := mt.get(mid)
-        prev := math.MinInt64
-        next := math.MinInt64
-        if mid-1 >= 0 {prev = mt.get(mid-1)}
-        if mid+1 < n {next = mt.get(mid+1)}
-        if val > prev && val > next {
+        midVal := m.get(mid)
+        if (mid == 0 || midVal > m.get(mid-1)) && (mid == n-1 || midVal > m.get(mid+1)) {
             peakIdx = mid
-            peakVal = val
+            peakVal = midVal
             break
         }
-        if prev > val {
-            right = mid-1
-        } else {
+        if mid == n-1 || m.get(mid+1) > midVal {
             left = mid+1
+        } else {
+            right = mid-1
         }
+
     }
-    if peakIdx == -1 {panic("well shit")}
-    if target == peakVal {return peakIdx}
-    
+    if peakIdx == -1 {return -1}
+    if peakVal == target {return peakIdx}
+    // search in left half, which is sorted in asc order
     left = 0
     right = peakIdx-1
     for left <= right {
         mid := left + (right-left)/2
-        val := mt.get(mid)
-        if val == target {return mid}
-        if target > val {
+        midVal := m.get(mid)
+        if midVal == target {
+            return mid
+        } else if target > midVal {
             left = mid+1
         } else {
             right = mid-1
         }
     }
 
-    // desc order side
-    left = peakIdx
+    // search in right half, right side is sorted in desc order
+    left = peakIdx+1
     right = n-1
     for left <= right {
         mid := left + (right-left)/2
-        val := mt.get(mid)
-        if val == target {return mid}
-        if target > val {
+        midVal := m.get(mid)
+        if midVal == target {
+            return mid
+        } else if target > midVal {
             right = mid-1
         } else {
             left = mid+1
