bda50fb11e08da99ea74880eaf3eb5f8163279d8
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-07 17:21:16 -0700

    Time: 0 ms (100.00%) | Memory: 4 MB (44.00%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.go
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.go
@@ -1,12 +1,11 @@
 
 func getNoZeroIntegers(n int) []int {
     for i := 1; i < n; i++ {
-        left := i
         right := n-i
-        leftStr := fmt.Sprintf("%v", left)
+        leftStr := fmt.Sprintf("%v", i)
         rightStr := fmt.Sprintf("%v", right)
         if !strings.Contains(leftStr, "0") && !strings.Contains(rightStr, "0") {
-            return []int{left, right}
+            return []int{i, right}
         }
     }
     return nil
