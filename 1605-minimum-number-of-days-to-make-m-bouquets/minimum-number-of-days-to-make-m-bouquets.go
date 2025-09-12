f00713245a3a3320e9f4ec7a05becf0d66daf5f0
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-12 16:46:26 -0700

    Time: 6 ms (90.28%) | Memory: 10.9 MB (38.89%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.go
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.go
@@ -1,28 +1,28 @@
 func minDays(bloomDay []int, m int, k int) int {
-    left := 1
+    left := slices.Min(bloomDay)
     right := slices.Max(bloomDay)
-    res := -1
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
         count := 0
-        boqs := 0
+        bq := 0
         for i := 0; i < len(bloomDay); i++ {
             if bloomDay[i] <= mid {
                 count++
                 if count == k {
                     count = 0
-                    boqs++
+                    bq++
                 }
             } else {
                 count = 0
             }
         }
-        if boqs >= m {
-            res = mid
+        if bq >= m {
+            ans = mid
             right = mid-1
         } else {
             left = mid+1
         }
     }
-    return res
+    return ans
 }
