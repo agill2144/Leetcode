4f9058243840251dfd4aeb5178fbc7290cfbe198
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-12 16:05:05 -0700

    Time: 0 ms (100.00%) | Memory: 8.5 MB (40.93%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.go
+++ 907-koko-eating-bananas/koko-eating-bananas.go
@@ -1,19 +1,20 @@
 func minEatingSpeed(piles []int, h int) int {
     left := 1
     right := slices.Max(piles)
-    res := -1
+    k := 0
     for left <= right {
         mid := left + (right-left)/2
         hours := 0
         for i := 0; i < len(piles); i++ {
             hours += int(math.Ceil(float64(piles[i])/float64(mid)))
         }
-        if hours > h {
-            left = mid+1
-        } else {
-            res = mid
+        if hours <= h {
+            k = mid
             right = mid-1
+        } else {
+            left = mid+1
         }
     }
-    return res
+    return k
+
 }
