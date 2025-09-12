dcc02dd13da42cd95c0b0c66c8366e7a9a6e3499
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-12 16:07:25 -0700

    Time: 0 ms (100.00%) | Memory: 5.1 MB (5.21%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
@@ -1,34 +1,24 @@
 func findMin(nums []int) int {
-    n := len(nums)
-    left := 0
-    right := n-1
     ans := math.MaxInt64
+    left := 0
+    right := len(nums)-1
     for left <= right {
         mid := left + (right-left)/2
-        if nums[left] == nums[mid] && nums[mid] == nums[right] {
+        if nums[mid] == nums[left] && nums[right] == nums[mid] {
             ans = min(ans, nums[mid])
             left++
             right--
             continue
         }
 
-        // mid could an answer
-        if (mid == 0 || nums[mid] < nums[mid-1]) && (mid == n-1 || nums[mid] < nums[mid+1]) {
-            ans = min(ans, nums[mid])
-        } 
-        // when right sorted
-        // right is larger than mid
-        // therefore again check if mid is a ans
-        if nums[mid] <= nums[right] {
-            ans = min(ans, nums[mid])
-            right = mid-1
-
-        // when left sorted
-        // left is smaller than mid
-        // therefore check if left is a ans
-        } else if nums[mid] >= nums[left] {
+        if nums[left] <= nums[mid] {
+            // left sorted
             ans = min(ans, nums[left])
             left = mid+1
+        } else {
+            // right sorted
+            ans = min(ans, nums[mid])
+            right = mid-1
         }
     }
     return ans
