c697520ae438f6c5fc76eea1c7ba90b62fb7c063
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-15 14:33:04 -0700

    Time: 52 ms (60.71%) | Memory: 11.9 MB (7.14%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.go
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.go
@@ -1,10 +1,9 @@
 func countFairPairs(nums []int, lower int, upper int) int64 {
-    n := len(nums)
     sort.Ints(nums)
-    var count int64
-    for i := 0; i < n; i++ {
-        left := searchGreaterOrEqualTo(nums, i+1, lower-nums[i])
-        right := searchLessThanOrEqualTo(nums, i+1, upper-nums[i])
+    var count int64 
+    for i := 0; i < len(nums); i++ {
+        left := leftMostOnRightSide(nums, lower-nums[i], i+1)
+        right := rightMostOnLeftSide(nums, upper-nums[i],i+1)
         if left != -1 && right != -1 {
             count += int64(right-left+1)
         }
@@ -12,33 +11,32 @@ func countFairPairs(nums []int, lower int, upper int) int64 {
     return count
 }
 
-
-func searchGreaterOrEqualTo(nums []int, left int, target int) int {
-    ans := -1
+func leftMostOnRightSide(nums []int, target, left int) int {
+    idx := -1
     right := len(nums)-1
     for left <= right {
-        mid := left + (right-left)/2
+        mid := left+(right-left)/2
         if nums[mid] >= target {
-            ans = mid
+            idx = mid
             right = mid-1
         } else {
             left = mid+1
         }
     }
-    return ans
+    return idx
 }
 
-func searchLessThanOrEqualTo(nums []int, left int, target int) int {
-    ans := -1
+func rightMostOnLeftSide(nums []int, target, left int) int {
+    idx := -1
     right := len(nums)-1
     for left <= right {
-        mid := left + (right-left)/2
+        mid := left+(right-left)/2
         if nums[mid] <= target {
-            ans = mid
+            idx = mid
             left = mid+1
         } else {
             right = mid-1
         }
     }
-    return ans
+    return idx
 }
