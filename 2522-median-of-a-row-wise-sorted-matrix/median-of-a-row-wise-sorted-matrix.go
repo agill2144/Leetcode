ec5fd7a16a9c5cf623db8672c81d7941ab2a940d
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-14 17:51:28 -0700

    Time: 2 ms (0.00%) | Memory: 11.8 MB (100.00%) - LeetSync

--- 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
+++ 2522-median-of-a-row-wise-sorted-matrix/median-of-a-row-wise-sorted-matrix.go
@@ -1,28 +1,38 @@
+/*
+    - in an uniq sorted odd sized array
+    - median is at (n-1)/2 idx
+    - count num of elements <= median val will always be medianIdx+1
+
+    - but in a sorted odd size array with dupes
+    - median idx is still at (n-1)/2 idx
+    - we want the keep searching for a value whose count num of elems <= itself >= medianIdx+1
+*/
 func matrixMedian(grid [][]int) int {
     m := len(grid)
     n := len(grid[0])
-    total := m*n
-    expectedCount := (total/2)+1
     left := 1
     right := 1000000
-    res := -1
+    totalEles := m*n
+    medianIdx := (totalEles-1)/2
+    expectedCount := medianIdx+1
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
-        if count(grid, mid) >= expectedCount {
-            res = mid
+        count := countLessThanOrEqualTo(grid, mid)
+        if count >= expectedCount {
+            ans = mid
             right = mid-1
         } else {
             left = mid+1
         }
     }
-    return res
+    return ans
 }
 
-// count num of elements <= target
-func count(grid [][]int, target int) int {
-    c := 0
+func countLessThanOrEqualTo(grid [][]int, target int) int {
     m := len(grid)
     n := len(grid[0])
+    total := 0
     for i := 0; i < m; i++ {
         left := 0
         right := n-1
@@ -36,7 +46,7 @@ func count(grid [][]int, target int) int {
                 right = mid-1
             }
         }
-        c += (idx+1)
+        total += (idx+1)
     }
-    return c
+    return total
 }
