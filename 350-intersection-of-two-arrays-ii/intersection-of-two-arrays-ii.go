eb7550c85cd468641f32eb70fe474b746cae4e38
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-15 15:49:22 -0700

    Time: 0 ms (100.00%) | Memory: 4.5 MB (86.97%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
@@ -1,34 +1,20 @@
 func intersect(nums1 []int, nums2 []int) []int {
-    if len(nums2) < len(nums1) {return intersect(nums2,nums1)}
     sort.Ints(nums1)
     sort.Ints(nums2)
+    n1, n2 := 0, 0
     out := []int{}
-    left := 0
-    for i := 0; i < len(nums1); i++ {
-        target := nums1[i]
-        idx := search(nums2, left, target)
-        if idx != -1 {
-            out = append(out, nums1[i])
-            left = idx+1
+    for n1 < len(nums1) && n2 < len(nums2) {
+        n1Val := nums1[n1]
+        n2Val := nums2[n2]
+        if n1Val == n2Val {
+            out = append(out, n1Val)
+            n1++
+            n2++
+        } else if n1Val < n2Val {
+            n1++
+        } else {
+            n2++
         }
     }
     return out
-}
-
-
-func search(nums []int, left int, target int) int {
-    idx := -1
-    right := len(nums)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        if nums[mid] == target {
-            idx = mid
-            right = mid-1
-        } else if target > nums[mid] {
-            left = mid+1
-        } else {
-            right = mid-1
-        }
-    }
-    return idx
 }
