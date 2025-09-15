45da2f226af64a60918d3c5f2df8f1410716e8d1
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-15 14:37:27 -0700

    Time: 0 ms (100.00%) | Memory: 4.2 MB (32.31%) - LeetSync

--- 441-arranging-coins/arranging-coins.go
+++ 441-arranging-coins/arranging-coins.go
@@ -1,15 +1,12 @@
 func arrangeCoins(n int) int {
-    if n <= 1 {
-        if n == 1 {return 1}
-        return 0
-    }
+    if n == 1 {return 1}
     left := 1
     right := n
-    ans := 0
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
         ap := mid*(mid+1)/2
-        if ap <= n {
+        if n >= ap {
             ans = mid
             left = mid+1
         } else {
