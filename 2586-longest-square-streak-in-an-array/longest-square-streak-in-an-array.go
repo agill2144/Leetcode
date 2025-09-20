fb2a83af5fdf7705304de3812fdd72000c441576
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-19 17:00:02 -0700

    Time: 30 ms (100.00%) | Memory: 10.1 MB (90.00%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.go
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.go
@@ -1,20 +1,47 @@
 func longestSquareStreak(nums []int) int {
-    set := map[float64]bool{}
-    for i := 0; i < len(nums); i++ {
-        set[float64(nums[i])] = true
-    }
+    sort.Ints(nums)
     ans := -1
     for i := 0; i < len(nums); i++ {
-        sqrt := math.Sqrt(float64(nums[i]))
-        if !set[sqrt] {
-            count := 0
-            start := float64(nums[i])
-            for set[start] {
-                count++
-                start *= start
-            }
-            if count > 1 {ans = max(ans, count)}
+        count := 1
+        target := nums[i]
+        for search(nums, i+1, target*target) {
+            count++
+            target *= target
         }
+        if count > 1 { ans = max(ans, count)}
     }
     return ans
 }
+func search(nums []int, left, target int) bool {
+    right := len(nums)-1
+    for left <= right {
+        mid := left + (right-left)/2
+        if nums[mid] == target {return true}
+        if target > nums[mid] {
+            left = mid+1
+        } else {
+            right = mid-1
+        }
+    }
+    return false
+}
+// func longestSquareStreak(nums []int) int {
+//     set := map[float64]bool{}
+//     for i := 0; i < len(nums); i++ {
+//         set[float64(nums[i])] = true
+//     }
+//     ans := -1
+//     for i := 0; i < len(nums); i++ {
+//         sqrt := math.Sqrt(float64(nums[i]))
+//         if !set[sqrt] {
+//             count := 0
+//             start := float64(nums[i])
+//             for set[start] {
+//                 count++
+//                 start *= start
+//             }
+//             if count > 1 {ans = max(ans, count)}
+//         }
+//     }
+//     return ans
+// }
