9922b7d2ea340bdb537ac55e3c9219f5886ca92e
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-21 14:22:28 -0700

    Time: 0 ms (100.00%) | Memory: 5.1 MB (45.15%) - LeetSync

--- 39-combination-sum/combination-sum.go
+++ 39-combination-sum/combination-sum.go
@@ -1,27 +1,29 @@
 func combinationSum(candidates []int, target int) [][]int {
     out := [][]int{}
     var dfs func(start int, sum int, path []int)
-    dfs = func(start, sum int, path []int) {
+    dfs = func(start int, sum int, path []int) {
         // base
-        if sum > target {return}
-
-        // logic
-        if sum == target {
-            newL := make([]int, len(path))
-            copy(newL, path)
-            out = append(out, newL)
+        if sum >= target {
+            if sum == target {
+                newP := make([]int, len(path))
+                copy(newP, path)
+                out = append(out, newP)
+            }
             return
         }
+
+        // logic
         for i := start; i < len(candidates); i++ {
-            if sum + candidates[i] <= target {
-                sum += candidates[i]
-                path = append(path, candidates[i])
-                dfs(i, sum, path)
-                path = path[:len(path)-1]
-                sum -= candidates[i]
-            }
+            // action
+            sum += candidates[i]
+            path = append(path, candidates[i])
+            // recurse
+            dfs(i, sum, path)
+            // backtrack
+            path = path[:len(path)-1]
+            sum -= candidates[i]
         }
     }
-    dfs(0, 0, nil)
+    dfs(0,0,nil)
     return out
 }
