e2c95e966a36b1cd83c12fe5e703a46699acbeb5
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-21 14:40:27 -0700

    Time: 1 ms (42.69%) | Memory: 4.8 MB (24.66%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.go
+++ 40-combination-sum-ii/combination-sum-ii.go
@@ -1,67 +1,37 @@
-func combinationSum2(nums []int, target int) [][]int {
-    sort.Ints(nums)
+func combinationSum2(candidates []int, target int) [][]int {
+    freqM := map[int]int{}
+    for i := 0; i < len(candidates); i++ {freqM[candidates[i]]++}
+    freq := [][]int{}
+    for k, v := range freqM {freq = append(freq, []int{k,v})}
     out := [][]int{}
-    var dfs func(start, sum int, path []int)
-    dfs = func(start, sum int, path []int){
+    var dfs func(start , sum int, path []int) 
+    dfs = func(start, sum int, path []int) {
         // base
-        if sum == target {
-            newL := make([]int, len(path))
-            copy(newL, path)
-            out = append(out, newL)
-            return            
+        if sum >= target {
+            if sum == target {
+                newP := make([]int, len(path))
+                copy(newP, path)
+                out = append(out, newP)
+            }
+            return
         }
-        if start == len(nums) {return}
 
         // logic
-        for i := start; i < len(nums); i++ {
-            if sum + nums[i] > target {return}
-            // action
-            path = append(path, nums[i])
-            // recurse
-            dfs(i+1, sum+nums[i], path)
-            // backtrack
-            path = path[:len(path)-1]
-            for i+1 < len(nums) && nums[i] == nums[i+1] {i++}
+        for i := start; i < len(freq); i++ {
+            if freq[i][1] > 0 {
+                // action
+                sum += freq[i][0]
+                freq[i][1]--
+                path = append(path, freq[i][0])
+                // recurse
+                dfs(i, sum, path)
+                // backtrack
+                path = path[:len(path)-1]
+                freq[i][1]++
+                sum -= freq[i][0]
+            }
         }
     }
-    dfs(0,0, []int{})
+    dfs(0, 0, nil)
     return out
 }
-// func combinationSum2(nums []int, target int) [][]int {
-//     m := map[int]int{}
-//     n := len(nums)
-//     for i := 0; i < n; i++ {m[nums[i]]++}
-//     freq := [][]int{} // [ [val, count] ]
-//     for k, v := range m {
-//         freq = append(freq, []int{k, v})
-//     }
-//     out := [][]int{}
-//     var dfs func(start int, path []int, sum int)
-//     dfs = func(start int, path []int, sum int) {
-//         // base
-//         if sum == target {
-//             newL := make([]int, len(path))
-//             copy(newL, path)
-//             out = append(out, newL)
-//             return
-//         }
-//         if sum > target || start == len(freq) {return}
-
-//         // logic
-//         for i := start; i < len(freq); i++ {
-//             if freq[i][1] == 0 {continue}
-//             // action
-//             sum += freq[i][0]
-//             freq[i][1]--
-//             path = append(path, freq[i][0])
-//             // recurse
-//             dfs(i, path, sum)
-//             // backtrack
-//             path = path[:len(path)-1]
-//             freq[i][1]++
-//             sum -= freq[i][0]
-//         }
-//     }
-//     dfs(0, nil, 0)
-//     return out
-// }
