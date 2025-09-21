d45634e206b596c6cfed68d94d916483c6e86821
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-20 17:19:51 -0700

    Time: 24 ms (60.00%) | Memory: 10.3 MB (40.00%) - LeetSync

--- 2045-cutting-ribbons/cutting-ribbons.go
+++ 2045-cutting-ribbons/cutting-ribbons.go
@@ -1,6 +1,3 @@
-// maximize the min 
-// min = mid
-// maximize will be used to take binary search to the bigger side if mid worked
 func maxLength(ribbons []int, k int) int {
     left := 1
     right := slices.Max(ribbons)
@@ -8,8 +5,8 @@ func maxLength(ribbons []int, k int) int {
     for left <= right {
         mid := left + (right-left)/2
         count := 0
-        for j := 0; j < len(ribbons); j++ {
-            count += int(math.Floor(float64(ribbons[j])/float64(mid)))
+        for i := 0; i < len(ribbons); i++ {
+            count += int(math.Floor(float64(ribbons[i])/float64(mid)))
         }
         if count >= k {
             ans = mid
@@ -20,20 +17,3 @@ func maxLength(ribbons []int, k int) int {
     }
     return ans
 }
-// func maxLength(ribbons []int, k int) int {
-//     start := 1
-//     end := slices.Max(ribbons)
-//     ans := 0
-//     for i := start; i <= end; i++ {
-//         count := 0
-//         for j := 0; j < len(ribbons); j++ {
-//             count += int(math.Floor(float64(ribbons[j])/float64(i)))
-//         }
-//         if count >= k {
-//             ans = i
-//         } else{
-//             break
-//         }
-//     }
-//     return ans
-// }
