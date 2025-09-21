ff1b8a6e4d8aa6d160bbbe629c38c500f3c354f9
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-20 19:20:21 -0700

    Time: 0 ms (100.00%) | Memory: 4.8 MB (24.95%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.go
+++ 22-generate-parentheses/generate-parentheses.go
@@ -1,24 +1,35 @@
 func generateParenthesis(n int) []string {
-    out := []string{}
-    var dfs func(o, c int, path string)
-    dfs = func(o, c int, path string) {
-        // base
-        if o == 0 && c == 0 {
-            out = append(out, path)
-            return
-        }
+	out := []string{}
+	var dfs func(o, c int, path string, tmp []string)
+	dfs = func(o, c int, path string, tmp []string) {
+		// base
+		if o == 0 && c == 0 {
+			out = append(out, strings.Join(tmp, ""))
+			return
+		}
+		// logic
+		if o > 0 {
+            // action
+			tmp = append(tmp, "(")
+            // recurse
+			dfs(o-1, c, path+"(", tmp)
+            // backtrack
+			tmp = tmp[:len(tmp)-1]
+		}
 
-        // logic
-        if o > 0 {
-            dfs(o-1,c, path + "(")
-        }
-        // n = 3
-        // ()() open = 2; close = 1
-        //
-        if c > o {
-            dfs(o, c-1, path + ")")
-        }
-    }
-    dfs(n,n,"")
-    return out
+        // only close if we have opened more than we have closed
+        // and thats only possible if closing avail count > opening avail count
+        // meaning we have more close parans than open parans
+        // i.e we have opened more than we have closed
+        // i.e we can def close atleast one now ...
+        // hence this logic
+		if c > o {
+			tmp = append(tmp, ")")
+			dfs(o, c-1, path+")", tmp)
+			tmp = tmp[:len(tmp)-1]
+		}
+	}
+	dfs(n, n, "", []string{})
+	return out
+
 }
