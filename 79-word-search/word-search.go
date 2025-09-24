abb32e9b50ad654ed620e19a876bca066c015bfa
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-23 23:46:23 -0700

    Time: 131 ms (50.21%) | Memory: 4.1 MB (59.06%) - LeetSync

--- 79-word-search/word-search.go
+++ 79-word-search/word-search.go
@@ -1,26 +1,34 @@
 func exist(board [][]byte, word string) bool {
     m := len(board)
     n := len(board[0])
-    dirs := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
-    var visited byte = '0'
-    var dfs func(r, c, w int)bool
-    dfs = func(r,c,w int) bool {
+    dirs := [][]int{
+        {1,0},
+        {-1,0},
+        {0,-1},
+        {0,1},
+    }
+    var dfs func(ptr,r,c int) bool
+    dfs = func(ptr,r,c int) bool {
         // base
-        if w == len(word) {return true}
-        if r < 0 || r == m || c < 0 || c == n || board[r][c] != word[w] {return false}
+        if ptr == len(word) {return true}
+        if r < 0 || r == m || c < 0 || c == n || board[r][c] != word[ptr] {return false}
 
         // logic
+        // action
+        // visit this cell
         tmp := board[r][c]
-        board[r][c] = visited
+        board[r][c] = '.'
         for _, dir := range dirs {
-            if dfs(r+dir[0], c+dir[1], w+1) {board[r][c] = tmp; return true}
+            if dfs(ptr+1, r+dir[0], c+dir[1]) {board[r][c] = tmp; return true}
         }
         board[r][c] = tmp
         return false
     }
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
-            if board[i][j] == word[0] && dfs(i,j,0) {return true}
+            if board[i][j] == word[0] {
+                if dfs(0, i, j) {return true}
+            }
         }
     }
     return false
