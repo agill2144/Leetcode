df71ac66fd0fe1807cde575748d8e8a8ceb52a7d
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-23 23:25:01 -0700

    Time: 14 ms (98.97%) | Memory: 23.5 MB (41.58%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.go
+++ 131-palindrome-partitioning/palindrome-partitioning.go
@@ -1,22 +1,26 @@
 func partition(s string) [][]string {
-    out := [][]string{}
-    var dfs func(start int, path []string) 
+    out := [][]string{}    
+    var dfs func(start int, path []string)
     dfs = func(start int, path []string) {
         // base
         if start == len(s) {
-            newL := make([]string, len(path))
-            copy(newL, path)
-            out = append(out, newL)
+            newP := make([]string, len(path))
+            copy(newP, path)
+            out = append(out, newP)
             return
         }
+
 
         // logic
         for i := start; i < len(s); i++ {
             subStr := s[start:i+1]
             if isPalindrome(subStr) {
+                // action
                 path = append(path, subStr)
+                // recurse
                 dfs(i+1, path)
-                path = path[:len(path)-1]
+                // backtrack
+                path = path[:len(path)-1]                
             }
         }
     }
@@ -25,10 +29,10 @@ func partition(s string) [][]string {
 }
 
 func isPalindrome(s string) bool {
-    if len(s) <=1 {return true}
+    if len(s) == 1 {return true}
     left := 0
     right := len(s)-1
-    for left <= right {
+    for left < right {
         if s[left] != s[right] {return false}
         left++
         right--
