761206bddab73ff14a7b066c6d593854723f6e64
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-28 17:22:03 -0700

    Time: 47 ms (68.43%) | Memory: 4.1 MB (80.16%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.go
+++ 37-sudoku-solver/sudoku-solver.go
@@ -1,52 +1,50 @@
 func solveSudoku(board [][]byte)  {
-    n := 9
+    n := len(board)
     rows := [10][10]bool{}
     cols := [10][10]bool{}
     boxes := [3][3][10]bool{}
-    for i := 0; i < n; i++ {
-        for j := 0; j < n; j++ {
+    for i := 0; i < len(board); i++ {
+        for j := 0; j < len(board[0]); j++ {
             if board[i][j] != '.' {
                 intVal := int(board[i][j]-'0')
+                r, c := i/3, j/3
                 rows[i][intVal] = true
                 cols[j][intVal] = true
-                boxes[i/3][j/3][intVal] = true
+                boxes[r][c][intVal] = true
             }
         }
     }
     var dfs func(r, c int) bool
     dfs = func(r, c int) bool {
         // base
-        if r == n-1 && c == n {return true}
         if c == n {
             return dfs(r+1, 0)
+        }
+        if r == n {
+            return true
         }
         if board[r][c] != '.' {
             return dfs(r, c+1)
         }
-        
 
         // logic
         for i := 1; i <= 9; i++ {
             inRow := rows[r][i]
             inCol := cols[c][i]
             inBox := boxes[r/3][c/3][i]
-            canUse := !inRow && !inCol && !inBox
-            if canUse {
+            if !inRow && !inCol && !inBox {
                 rows[r][i] = true
                 cols[c][i] = true
                 boxes[r/3][c/3][i] = true
                 board[r][c] = byte(i+'0')
-
-                if dfs(r,c+1) {return true}
-
-                board[r][c] = '.'
-                boxes[r/3][c/3][i] = false
-                cols[c][i] = false
+                if dfs(r, c+1) {return true}
                 rows[r][i] = false
+                cols[c][i] = false
+                boxes[r/3][c/3][i] = false
+                board[r][c] = '.'
             }
         }
         return false
     }
     dfs(0,0)
-    
 }
