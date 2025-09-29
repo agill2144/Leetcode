94a9a0ec4d9561e53ae6f010ffe0e37a8a1424c0
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-28 23:40:51 -0700

    Time: 0 ms (100.00%) | Memory: 4.4 MB (4.45%) - LeetSync

--- 78-subsets/subsets.go
+++ 78-subsets/subsets.go
@@ -1,32 +1,12 @@
 func subsets(nums []int) [][]int {
     out := [][]int{{}}
     for i := 0; i < len(nums); i++ {
-        for _, p := range out {
-            newP := make([]int, len(p))
-            copy(newP, p)
+        for _, ele := range out {
+            newP := make([]int, len(ele))
+            copy(newP, ele)
             newP = append(newP, nums[i])
             out = append(out, newP)
         }
     }
     return out
 }
-// func subsets(nums []int) [][]int {
-//     out := [][]int{}
-//     var dfs func(start int, path []int)
-//     dfs = func(start int, path []int) {
-//         // base
-//         newP := make([]int, len(path))
-//         copy(newP, path)
-//         out = append(out, newP)
-//         if start == len(nums) {return}
-
-//         // logic
-//         for i := start; i < len(nums); i++ {
-//             path = append(path, nums[i])
-//             dfs(i+1, path)
-//             path = path[:len(path)-1]            
-//         }   
-//     }
-//     dfs(0, nil)
-//     return out
-// }
