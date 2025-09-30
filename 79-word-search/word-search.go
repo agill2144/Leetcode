8af7dcb1cf37b09bb685189417bc071f43174d48
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-29 21:37:21 -0700

    Time: 134 ms (44.91%) | Memory: 4.2 MB (31.42%) - LeetSync

--- 79-word-search/word-search.go
+++ 79-word-search/word-search.go
@@ -2,24 +2,22 @@ func exist(board [][]byte, word string) bool {
     m := len(board)
     n := len(board[0])
     dirs := [][]int{
-        {1,0},
         {-1,0},
+        {1,0},
         {0,-1},
         {0,1},
     }
-    var dfs func(ptr,r,c int) bool
-    dfs = func(ptr,r,c int) bool {
+    var dfs func(r, c, ptr int) bool
+    dfs = func(r, c, ptr int) bool {
         // base
         if ptr == len(word) {return true}
         if r < 0 || r == m || c < 0 || c == n || board[r][c] != word[ptr] {return false}
 
-        // logic
-        // action
-        // visit this cell
+        // visit current cell
         tmp := board[r][c]
         board[r][c] = '.'
         for _, dir := range dirs {
-            if dfs(ptr+1, r+dir[0], c+dir[1]) {board[r][c] = tmp; return true}
+            if dfs(r+dir[0],c+dir[1],ptr+1) {return true}
         }
         board[r][c] = tmp
         return false
@@ -27,7 +25,7 @@ func exist(board [][]byte, word string) bool {
     for i := 0; i < m; i++ {
         for j := 0; j < n; j++ {
             if board[i][j] == word[0] {
-                if dfs(0, i, j) {return true}
+                if dfs(i,j,0) {return true}
             }
         }
     }
