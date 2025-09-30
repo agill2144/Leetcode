5dbccff7392ed0f7b9e4edf808bf158981b5feb1
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-29 21:53:56 -0700

    Time: 48 ms (68.40%) | Memory: 4.1 MB (80.14%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.go
+++ 37-sudoku-solver/sudoku-solver.go
@@ -1,47 +1,41 @@
 func solveSudoku(board [][]byte)  {
-    n := len(board)
     rows := [10][10]bool{}
     cols := [10][10]bool{}
     boxes := [3][3][10]bool{}
     for i := 0; i < len(board); i++ {
         for j := 0; j < len(board[0]); j++ {
             if board[i][j] != '.' {
-                intVal := int(board[i][j]-'0')
-                r, c := i/3, j/3
-                rows[i][intVal] = true
-                cols[j][intVal] = true
-                boxes[r][c][intVal] = true
+                val := int(board[i][j]-'0')
+                rows[i][val] = true
+                cols[j][val] = true
+                boxes[i/3][j/3][val] = true
             }
         }
     }
+    n := len(board)
     var dfs func(r, c int) bool
     dfs = func(r, c int) bool {
         // base
-        if c == n {
-            return dfs(r+1, 0)
-        }
-        if r == n {
-            return true
-        }
-        if board[r][c] != '.' {
-            return dfs(r, c+1)
-        }
+        if c == n {return dfs(r+1, 0)}
+        if r == n {return true}
+        if board[r][c] != '.' {return dfs(r,c+1)}
 
         // logic
         for i := 1; i <= 9; i++ {
             inRow := rows[r][i]
             inCol := cols[c][i]
             inBox := boxes[r/3][c/3][i]
-            if !inRow && !inCol && !inBox {
+            canUse := !inRow && !inCol && !inBox
+            if canUse {
+                board[r][c] = byte(i+'0')
                 rows[r][i] = true
                 cols[c][i] = true
                 boxes[r/3][c/3][i] = true
-                board[r][c] = byte(i+'0')
-                if dfs(r, c+1) {return true}
+                if dfs(r,c+1) {return true}
+                board[r][c] = '.'
                 rows[r][i] = false
                 cols[c][i] = false
                 boxes[r/3][c/3][i] = false
-                board[r][c] = '.'
             }
         }
         return false
