830302911e5152126cf030af420cafa3c1d897ee
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-09-30 23:56:22 -0700

    Time: 4 ms (18.18%) | Memory: 8 MB (90.91%) - LeetSync

--- 1192-divide-chocolate/divide-chocolate.go
+++ 1192-divide-chocolate/divide-chocolate.go
@@ -1,4 +1,3 @@
-// maximize the atMin sweetness
 func maximizeSweetness(sweetness []int, k int) int {
     left := math.MaxInt64
     right := 0
@@ -9,54 +8,21 @@ func maximizeSweetness(sweetness []int, k int) int {
     ans := -1
     for left <= right {
         mid := left + (right-left)/2
-        rSum := 0
         count := 0
+        rSum := 0
         for i := 0; i < len(sweetness); i++ {
             rSum += sweetness[i]
             if rSum >= mid {
-                count++
                 rSum = 0
-                if count >= k+1 {break}
+                count++
             }
         }
-        // when does mid not work?
-        // when we could not make atleast k+1 pieces
-        if count < k+1 {
-            // per chunk atMin sweetness is to high
-            // hence reduce the atMin and try again in-hope to cut more pieces
-            right = mid-1
-        } else {
+        if count >= k+1 {
             ans = mid
-            // we want max such ans, continue searching right
             left = mid+1
+        } else {
+            right = mid-1
         }
     }
     return ans
-
 }
-// func maximizeSweetness(sweetness []int, k int) int {
-//     start := math.MaxInt64
-//     end := 0
-//     for i := 0; i < len(sweetness); i++ {
-//         start = min(start, sweetness[i])
-//         end += sweetness[i]
-//     }
-//     ans := -1
-//     for i := start; i <= end; i++ {
-//         rSum := 0
-//         count := 0
-//         for j := 0; j < len(sweetness); j++ {
-//             rSum += sweetness[j]
-//             if rSum >= i {
-//                 rSum = 0
-//                 count++
-//             }
-//         }
-//         if count >= k+1 {
-//             ans = i
-//         } else {
-//             break
-//         }
-//     }
-//     return ans
-// }
