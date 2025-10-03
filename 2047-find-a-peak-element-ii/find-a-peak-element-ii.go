71309e763655a6bfaa4bdc58646e01b5b092bf84
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-10-03 14:48:43 -0700

    Time: 0 ms (100.00%) | Memory: 12 MB (16.67%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.go
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.go
@@ -1,24 +1,3 @@
-/*
-    - random binary search on rows or cols
-    - in this case, i am doing it on rows
-    - then once we have selected a row (mid)
-    - we find the max value in that row 
-        - selecting a cell in such manner
-        - ensures we have the highest value in this row
-        - solves for left / right neighor comparisons
-            - because we picked the highest val in that row
-    - now we have a cell and a row(mid) , we need to see if this cell is > above and bottom cell
-    - if it is, we return and call it done
-    - if the above cell val > current cell val
-        - take the search above ( right = mid-1 )
-    - if the bottom cell val > current cell val
-        - take the search below ( left = mid+1 )
-
-    m = len(rows)
-    n = len(cols)
-    tc = o(logm * n)
-    sc = o(1)
-*/
 func findPeakGrid(mat [][]int) []int {
     m := len(mat)
     n := len(mat[0])
@@ -26,26 +5,22 @@ func findPeakGrid(mat [][]int) []int {
     right := m-1
     for left <= right {
         mid := left + (right-left)/2
-        // find max in this row
-        maxVal := mat[mid][0]
-        maxValIdx := 0
+        colIdx := 0
+        midVal := mat[mid][0]
         for j := 1; j < n; j++ {
-            if mat[mid][j] > maxVal {
-                maxVal = mat[mid][j]
-                maxValIdx = j
+            if mat[mid][j] > midVal {
+                midVal = mat[mid][j]
+                colIdx = j
             }
         }
-
-        // now compare with above and bottom cell
         above := math.MinInt64
-        if mid-1 >= 0 {above = mat[mid-1][maxValIdx]}
-        bottom := math.MinInt64
-        if mid+1 < m {bottom = mat[mid+1][maxValIdx]}
-        curr := maxVal
-        if curr > above && curr > bottom {
-            return []int{mid, maxValIdx}
+        if mid-1 >= 0 {above = mat[mid-1][colIdx]}
+        below := math.MinInt64
+        if mid+1 < m {below = mat[mid+1][colIdx]}
+        if midVal > above && midVal > below {
+            return []int{mid, colIdx}
         }
-        if above > curr {
+        if above > midVal {
             right = mid-1
         } else {
             left = mid+1
