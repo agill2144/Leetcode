aff15a8fba5a1d83a44929f0f288f30c9e040795
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-10-03 17:11:51 -0700

    Time: 0 ms (100.00%) | Memory: 6.2 MB (92.88%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.go
+++ 2-add-two-numbers/add-two-numbers.go
@@ -6,22 +6,21 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    carry := 0
-    dummy := &ListNode{}
+    dummy := &ListNode{Val: 0}
     tail := dummy
+    carry := 0
     for l1 != nil || l2 != nil {
-        l1Val := 0
-        l2Val := 0
-        if l1 != nil {l1Val = l1.Val; l1=l1.Next}
-        if l2 != nil {l2Val = l2.Val; l2=l2.Next}
+        l1Val := 0; if l1 != nil {l1Val = l1.Val; l1 = l1.Next}
+        l2Val := 0; if l2 != nil {l2Val = l2.Val; l2 = l2.Next}
         sum := carry + l1Val + l2Val
-        tail.Next = &ListNode{Val:sum%10}
+        tail.Next = &ListNode{Val: sum % 10}
         tail = tail.Next
         carry = sum / 10
     }
     if carry != 0 {
         tail.Next = &ListNode{Val: carry}
         carry = 0
+        tail = tail.Next
     }
     return dummy.Next
 }
