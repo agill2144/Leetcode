0ea0c29ebb6de589cae90a9d2ed9b0ac4ca0bed3
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-10-04 15:27:52 -0700

    Time: 0 ms (100.00%) | Memory: 8.8 MB (46.61%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.go
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.go
@@ -1,12 +1,11 @@
 func kthSmallest(matrix [][]int, k int) int {
-    m := len(matrix)
-    n := len(matrix[0])
+    n := len(matrix)
     left := matrix[0][0]
-    right := matrix[m-1][n-1]
+    right := matrix[n-1][n-1]
     ans := -1
     for left <= right {
         mid := left + (right-left)/2
-        c := countOnLeft(matrix, mid)
+        c := countLessOrEqual(matrix, mid)
         if c >= k {
             ans = mid
             right = mid-1
@@ -17,11 +16,11 @@ func kthSmallest(matrix [][]int, k int) int {
     return ans
 }
 
-func countOnLeft(matrix [][]int, target int) int {
-    m := len(matrix)
-    n := len(matrix[0])
-    r := m-1; c := 0
+func countLessOrEqual(matrix [][]int, target int) int {
     count := 0
+    n := len(matrix)
+    r := n-1
+    c := 0
     for r >= 0 && c < n {
         if matrix[r][c] <= target {
             count += (r+1)
@@ -32,36 +31,3 @@ func countOnLeft(matrix [][]int, target int) int {
     }
     return count
 }
-// anything kth element = heap
-// tc = o(mn * logk)
-// sc = o(k)
-// func kthSmallest(matrix [][]int, k int) int {
-//     mx := &maxHeap{items: []int{}}
-//     for i := 0; i < len(matrix); i++ {
-//         for j := 0; j < len(matrix[i]); j++ {
-//             heap.Push(mx, matrix[i][j])
-//             if mx.Len() > k {heap.Pop(mx)}
-//         }
-//     }
-//     return mx.items[0]
-// }
-
-// /*
-// type Interface interface {
-// 	sort.Interface
-// 	Push(x any) // add x as element Len()
-// 	Pop() any   // remove and return element Len() - 1.
-// }
-// */
-// type maxHeap struct {
-//     items []int
-// }
-// func (m *maxHeap) Len() int {return len(m.items)}
-// func (m *maxHeap) Swap(i, j int) {m.items[i], m.items[j] = m.items[j], m.items[i]}
-// func (m *maxHeap) Less(i, j int) bool {return m.items[i] > m.items[j]}
-// func (m *maxHeap) Push(x interface{}) {m.items = append(m.items, x.(int))}
-// func (m *maxHeap) Pop() interface{} {
-//     out := m.items[len(m.items)-1]
-//     m.items = m.items[:len(m.items)-1]
-//     return out
-// }
