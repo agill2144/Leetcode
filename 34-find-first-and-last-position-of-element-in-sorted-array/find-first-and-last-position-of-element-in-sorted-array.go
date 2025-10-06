9f138a69caba8208c01276a31c9a3a9037737f69
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-10-06 16:47:45 -0700

    Time: 0 ms (100.00%) | Memory: 6.5 MB (35.06%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.go
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.go
@@ -8,25 +8,25 @@ func searchRange(nums []int, target int) []int {
         if nums[mid] == target {
             leftIdx = mid
             right = mid-1
-        } else if target > nums[mid] {
-            left = mid+1
-        } else {
+        } else if target < nums[mid] {
             right = mid-1
+        } else {
+            left = mid+1
         }
     }
     if leftIdx == -1 {return []int{-1,-1}}
-    rightIdx := -1
     left = leftIdx
     right = n-1
+    rightIdx := leftIdx
     for left <= right {
         mid := left + (right-left)/2
         if nums[mid] == target {
             rightIdx = mid
             left = mid+1
-        } else if target < nums[mid] {
-            right = mid-1
-        } else {
+        } else if target > nums[mid] {
             left = mid+1
+        } else {
+            right = mid-1
         }
     }
     return []int{leftIdx, rightIdx}
