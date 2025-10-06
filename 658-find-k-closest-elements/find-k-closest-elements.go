d615814b1f3dcc5e537d2b08eb696f3302609e11
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-10-06 14:29:55 -0700

    Time: 0 ms (100.00%) | Memory: 8.9 MB (59.43%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.go
+++ 658-find-k-closest-elements/find-k-closest-elements.go
@@ -1,18 +1,34 @@
 func findClosestElements(arr []int, k int, x int) []int {
-    n := len(arr)
     left := 0
-    right := n-1
-    for right-left+1 != k {
-        leftDist := abs(arr[left]-x)
-        rightDist := abs(arr[right]-x)
-        if rightDist >= leftDist {
-            right--
+    right := len(arr)-k
+    for left <= right {
+        mid := left + (right-left)/2
+        if mid+k >= len(arr) {break}
+        startDist := x - arr[mid]
+        endDist := arr[mid+k] - x
+        if endDist >= startDist {
+            right = mid-1
         } else {
-            left++
+            left = mid+1
         }
     }
-    return arr[left:right+1]
+    return arr[left:left+k]
 }
+// func findClosestElements(arr []int, k int, x int) []int {
+//     n := len(arr)
+//     left := 0
+//     right := n-1
+//     for right-left+1 != k {
+//         leftDist := abs(arr[left]-x)
+//         rightDist := abs(arr[right]-x)
+//         if rightDist >= leftDist {
+//             right--
+//         } else {
+//             left++
+//         }
+//     }
+//     return arr[left:right+1]
+// }
 // func findClosestElements(arr []int, k int, x int) []int {
 //     mx := &maxHeap{items: []*node{}}    
 //     for i := 0; i < len(arr); i++ {
