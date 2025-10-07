1ba6e840957f4c431a5cb57eaf528dae0b2561dc
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-10-06 17:27:34 -0700

    Time: 0 ms (100.00%) | Memory: 4.4 MB (95.35%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
@@ -1,20 +1,48 @@
 func intersect(nums1 []int, nums2 []int) []int {
+    if len(nums2) < len(nums1) {return intersect(nums2, nums1)}
+    // nums1 is always the smaller array
     sort.Ints(nums1)
     sort.Ints(nums2)
-    n1, n2 := 0, 0 
+    idx := 0
     out := []int{}
-    for n1 < len(nums1) && n2 < len(nums2) {
-        n1Val := nums1[n1]
-        n2Val := nums2[n2]
-        if n1Val == n2Val {
-            out = append(out, n1Val)
-            n1++
-            n2++
-        } else if n1Val < n2Val {
-            n1++
-        } else {
-            n2++
+    for i := 0; i < len(nums1); i++ {
+        target := nums1[i]
+        left := idx
+        right := len(nums2)-1
+        ansIdx := -1
+        for left <= right {
+            mid := left + (right-left)/2
+            if nums2[mid] >= target {
+                if nums2[mid] == target {ansIdx = mid}
+                right = mid-1
+            } else {
+                left = mid+1
+            }
+        }
+        if ansIdx != -1 {
+            out = append(out, nums1[i])
+            idx = ansIdx+1
         }
     }
     return out
 }
+// func intersect(nums1 []int, nums2 []int) []int {
+//     sort.Ints(nums1)
+//     sort.Ints(nums2)
+//     n1, n2 := 0, 0 
+//     out := []int{}
+//     for n1 < len(nums1) && n2 < len(nums2) {
+//         n1Val := nums1[n1]
+//         n2Val := nums2[n2]
+//         if n1Val == n2Val {
+//             out = append(out, n1Val)
+//             n1++
+//             n2++
+//         } else if n1Val < n2Val {
+//             n1++
+//         } else {
+//             n2++
+//         }
+//     }
+//     return out
+// }
