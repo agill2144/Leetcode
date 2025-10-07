7e8e1eaf4da69d5135419b3bc018b36040f395b5
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-10-06 17:47:03 -0700

    Time: 0 ms (100.00%) | Memory: 4.1 MB (22.73%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.go
+++ 410-split-array-largest-sum/split-array-largest-sum.go
@@ -1,28 +1,34 @@
+/*
+    largest sum of any subarray is minimized.
+    - binary search hint
+    - mid = atMax
+    - use "minimize" to search on correct side of mid if mid is an answer
+    - mid is answer when it took <= k-1 split to satisfy atMax sum per chunk
+*/
 func splitArray(nums []int, k int) int {
-    left := math.MinInt64
+    left := -1
     right := 0
     for i := 0; i < len(nums); i++ {
-        right += nums[i]
         left = max(left, nums[i])
+        right += nums[i]
     }
     ans := -1
     for left <= right {
         mid := left + (right-left)/2
-        sum := 0
-        count := 1
-        for j := 0; j < len(nums); j++ {
-            sum += nums[j]
-            if sum > mid {
-                count++
-                sum = nums[j]
+        rSum := 0
+        splits := 0
+        for i := 0; i < len(nums); i++ {
+            rSum += nums[i]
+            if rSum > mid {
+                rSum = nums[i]
+                splits++
             }
         }
-        // when does mid not work?
-        if count > k {
-            left = mid+1
-        } else {
-            ans= mid
+        if splits <= k-1 {
+            ans = mid
             right = mid-1
+        } else {
+            left = mid+1
         }
     }
     return ans
