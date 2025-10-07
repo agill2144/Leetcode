1e2d9914440302f0855543db2e1c969ab1bfa3b4
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-10-06 17:24:44 -0700

    Time: 0 ms (100.00%) | Memory: 4.5 MB (88.48%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.go
@@ -1,62 +1,20 @@
-// What if nums1's size is small compared to nums2's size? Which algorithm is better?
-// nums1 = 2
-// nums2 = 5
-// then looping over small is 2 elements * log base 2 on nums2 (5) = 2.x
-// so tc = o(n1 * log n2) = 2 * 2.x = 4.x iterations
-// vs in two ptrs = 2+5 = 7 iterations
-// hence binary search is better when nums1 is smaller in size compared to nums2
 func intersect(nums1 []int, nums2 []int) []int {
-    if len(nums2) < len(nums1) {return intersect(nums2, nums1)}
     sort.Ints(nums1)
     sort.Ints(nums2)
+    n1, n2 := 0, 0 
     out := []int{}
-    left := 0
-    for i := 0; i < len(nums1); i++ {
-        idx := search(nums2, nums1[i], left)
-        if idx != -1 {
-            left = idx+1
-            out = append(out, nums1[i])
+    for n1 < len(nums1) && n2 < len(nums2) {
+        n1Val := nums1[n1]
+        n2Val := nums2[n2]
+        if n1Val == n2Val {
+            out = append(out, n1Val)
+            n1++
+            n2++
+        } else if n1Val < n2Val {
+            n1++
+        } else {
+            n2++
         }
     }
     return out
 }
-
-func search(nums []int, target int, left int) int {
-    idx := -1
-    right := len(nums)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        if nums[mid] == target {
-            idx = mid
-            right = mid-1
-        } else if target < nums[mid] {
-            right = mid-1
-        } else {
-            left = mid+1
-        }
-    }
-    return idx
-}
-// two ptrs
-// tc = o(n1 + n2) ; if we dont count sorting
-// sc = o(1)
-// func intersect(nums1 []int, nums2 []int) []int {
-//     sort.Ints(nums1)
-//     sort.Ints(nums2)
-//     n1, n2 := 0, 0
-//     out := []int{}
-//     for n1 < len(nums1) && n2 < len(nums2) {
-//         n1Val := nums1[n1]
-//         n2Val := nums2[n2]
-//         if n1Val == n2Val {
-//             out = append(out, n1Val)
-//             n1++
-//             n2++
-//         } else if n1Val < n2Val {
-//             n1++
-//         } else {
-//             n2++
-//         }
-//     }
-//     return out
-// }
