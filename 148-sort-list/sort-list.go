e94270e44347cdb4f50dc52f3a247ea616cabd46
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-10-06 22:18:46 -0700

    Time: 25 ms (11.15%) | Memory: 9.1 MB (72.12%) - LeetSync

--- 148-sort-list/sort-list.go
+++ 148-sort-list/sort-list.go
@@ -26,37 +26,24 @@ func sortList(head *ListNode) *ListNode {
         dummy := &ListNode{Val:-1}
         tail := dummy 
         l1, l2 := h1,h2
-        for l1 != nil && l2 != nil {
-            l1Next := l1.Next
-            l2Next := l2.Next
-            if l1.Val < l2.Val {
-                l1.Next = nil
+        for l1 != nil || l2 != nil {
+            l1Val := math.MaxInt64
+            l2Val := math.MaxInt64
+            if l1 != nil {l1Val = l1.Val}
+            if l2 != nil {l2Val = l2.Val}
+            if l1Val < l2Val {
+                next := l1.Next; l1.Next = nil
                 tail.Next = l1
                 tail = tail.Next
-                l1 = l1Next
+                l1 = next
             } else {
-                l2.Next = nil
+                next := l2.Next; l2.Next = nil
                 tail.Next = l2
                 tail = tail.Next
-                l2 = l2Next
+                l2 = next
             }
         }
-        for l1 != nil {
-            l1Next := l1.Next
-            l1.Next = nil
-            tail.Next = l1
-            tail = tail.Next
-            l1 = l1Next
-        }
-        for l2 != nil {
-            l2Next := l2.Next
-            l2.Next = nil
-            tail.Next = l2
-            tail = tail.Next
-            l2 = l2Next
-        }
-        newHead := dummy.Next
-        return newHead
+        return dummy.Next
     }
     return dfs(head)
 }
