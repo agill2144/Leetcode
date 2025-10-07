644ac011a1a0fb345ef357f242dc513b2f6d6fab
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-10-06 17:19:13 -0700

    Time: 19 ms (81.25%) | Memory: 10.1 MB (81.25%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.go
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.go
@@ -1,12 +1,35 @@
+/*
+    maximum number of balls in a bag. You want to minimize your penalty
+    - atMax = mid
+    - search for smallest such answer 
+        - i.e if mid is a valid answer
+        - search left, for a even smaller answer
+    
+    - atMax will be max num of balls allowerd per bag
+    - to count num of operations needs to split a bag of nums[i] balls
+        - ceil(nums[i] / atMax)-1
+    - nums[i] = 8
+    - atMax(or mid) = 2
+    - num of operations needed to satisfy 2 balls per bag is ; (8/2)-1
+    - its possible nums[i] = 7
+    - hence it would take 1 more operation (not 1 less), hence the ceil 
+
+    - nums[i] = 12
+    - atMax = 2
+    - if we think about splitting evenly, this would actually take 7 operations
+    - but our formula suggests: (12/2)-1 = 5 operations
+    - its NOT ABOUT SPLITTING EVENLY, ITS ABOUT SPLITTING OPTIMALLY
+
+*/
 func minimumSize(nums []int, maxOperations int) int {
     left := 1
     right := slices.Max(nums)
-    ans := 0
+    ans := -1
     for left <= right {
         mid := left + (right-left)/2
         ops := 0
         for i := 0; i < len(nums); i++ {
-            ops += int(math.Ceil(float64(nums[i])/float64(mid))-1.0)            
+            ops += int(math.Ceil(float64(nums[i])/float64(mid))-1.0)
         }
         if ops <= maxOperations {
             ans = mid
