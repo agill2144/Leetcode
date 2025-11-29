8e385cc478494974e43404464807241e5dce8514
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-11-29 15:29:48 -0800

    Time: 0 ms (100.00%) | Memory: 4.8 MB (32.31%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.go
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.go
@@ -1,12 +1,21 @@
+/*
+    approach: freq map
+    - put smaller nums into a freq map
+    - then iterate over other nums , check if exists in freq and if it does save it , decrement its counter
+    m = len(nums1)
+    n = len(nums2)
+    tc = o(m)+o(n)
+    sc = o(n)
+*/
 func intersection(nums1 []int, nums2 []int) []int {
-    freq := map[int]bool{}
+    freq := map[int]int{}    
     for i := 0; i < len(nums1); i++ {
-        freq[nums1[i]] = true
+        freq[nums1[i]]++
     }
     out := []int{}
     for i := 0; i < len(nums2); i++ {
-        if freq[nums2[i]] {
-            delete(freq,nums2[i])
+        if freq[nums2[i]] > 0 {
+            freq[nums2[i]] = 0
             out = append(out, nums2[i])
         }
     }
