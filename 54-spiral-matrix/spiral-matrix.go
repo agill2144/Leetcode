3725e322b8210fdf538f91f43eadab397b99f3ea
agill2144 <120353980+agill2144@users.noreply.github.com>
2025-12-01 17:09:19 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (9.78%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.go
+++ 54-spiral-matrix/spiral-matrix.go
@@ -1,39 +1,35 @@
 func spiralOrder(matrix [][]int) []int {
     m := len(matrix)
     n := len(matrix[0])
-    top := 0
-    bottom := m-1
-    left := 0
-    right := n-1
+    l, r := 0,n-1
+    t, b := 0,m-1
     out := []int{}
     for len(out) != m*n {
-
-        // left to right , same row
-        for i := left; i <= right; i++ {
-            out = append(out, matrix[top][i])
+        // left to right
+        for j := l; j <= r; j++ {
+            out = append(out, matrix[t][j])
         }
-        top++
-        if len(out) == m*n {break}
-        
-        // top to bottom on right side
-        for i := top; i <= bottom; i++ {
-            out = append(out, matrix[i][right])
-        }
-        right--
+        t++
         if len(out) == m*n {break}
 
-        // right to left on bottom side
-        for i := right; i >= left; i-- {
-            out = append(out, matrix[bottom][i])
+        // top to bottom
+        for i := t; i <= b; i++ {
+            out = append(out, matrix[i][r])
         }
-        bottom--
+        r--
         if len(out) == m*n {break}
 
-        // bottom to top on left side
-        for i := bottom; i >= top; i-- {
-            out = append(out, matrix[i][left])
+        // right to left
+        for j := r; j >= l; j-- {
+            out = append(out, matrix[b][j])
         }
-        left++
+        b--
+
+        // bottom to top
+        for i := b; i >= t; i-- {
+            out = append(out, matrix[i][l])
+        }
+        l++
     }
     return out
 
