b0255fc25e3511daed17e569051f6b8c5e5654fe
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-01-12 16:51:20 -0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (23.56%) - LeetSync

--- 48-rotate-image/rotate-image.go
+++ 48-rotate-image/rotate-image.go
@@ -1,18 +1,17 @@
 func rotate(matrix [][]int)  {
-    n := len(matrix)
-    for i := 0; i < n; i++ {
+    m := len(matrix)
+    n := len(matrix[0])
+    for i := 0; i < m; i++ {
         for j := i+1; j < n; j++ {
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         }
     }
-    for i := 0; i < n; i++ {
-        left := 0
-        right := n-1
+    for i := 0; i < m; i++ {
+        left := 0; right := n-1
         for left < right {
-            matrix[i][left], matrix[i][right] = matrix[i][right], matrix[i][left] 
+            matrix[i][left], matrix[i][right] = matrix[i][right], matrix[i][left]
             left++
             right--
         }
     }
-    
 }
