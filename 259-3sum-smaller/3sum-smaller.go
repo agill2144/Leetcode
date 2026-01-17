83199094f084b922788c43b5739dfbfd17efe67c
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-01-17 15:46:42 -0800

    Time: 10 ms (52.38%) | Memory: 4.8 MB (42.86%) - LeetSync

--- 259-3sum-smaller/3sum-smaller.go
+++ 259-3sum-smaller/3sum-smaller.go
@@ -2,6 +2,9 @@ func threeSumSmaller(nums []int, target int) int {
     count := 0
     sort.Ints(nums)
     for i := 0; i < len(nums); i++ {
+        // no need to escape if i val is same as prev
+        // because the question says just count 
+        // does not say count unique
         left := i+1
         right := len(nums)-1
         for left < right {
