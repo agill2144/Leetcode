cf873c5bc5ab6e51d4c3c9d45e8e89b6d5434750
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-01-17 13:48:22 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (42.55%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
@@ -1,23 +1,37 @@
 func twoSum(nums []int, target int) []int {
-    for i := 0; i < len(nums); i++ {
-        tg := target-nums[i]
-        if idx := search(i+1,tg, nums); idx != -1 {
-            return []int{i+1, idx+1}
-        }
-    }
-    return nil   
-}
-
-func search(left,target int, nums []int) int {
+    left := 0
     right := len(nums)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        if nums[mid] == target {return mid}
-        if target > nums[mid] {
-            left = mid+1
+    for left < right {
+        sum := nums[left] + nums[right]
+        if sum == target {return []int{left+1, right+1}}
+        if sum > target {
+            right--
         } else {
-            right = mid-1
+            left++
         }
     }
-    return -1
+    return nil
 }
+// func twoSum(nums []int, target int) []int {
+//     for i := 0; i < len(nums); i++ {
+//         tg := target-nums[i]
+//         if idx := search(i+1,tg, nums); idx != -1 {
+//             return []int{i+1, idx+1}
+//         }
+//     }
+//     return nil
+// }
+
+// func search(left,target int, nums []int) int {
+//     right := len(nums)-1
+//     for left <= right {
+//         mid := left + (right-left)/2
+//         if nums[mid] == target {return mid}
+//         if target > nums[mid] {
+//             left = mid+1
+//         } else {
+//             right = mid-1
+//         }
+//     }
+//     return -1
+// }
