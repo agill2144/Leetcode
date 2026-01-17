f42184e03e3a4704d8d1dad456376c6554046ef5
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-01-17 15:25:36 -0800

    Time: 24 ms (49.05%) | Memory: 9.5 MB (66.77%) - LeetSync

--- 15-3sum/3sum.go
+++ 15-3sum/3sum.go
@@ -1,23 +1,27 @@
 func threeSum(nums []int) [][]int {
-    set := map[[3]int]bool{}
-    res := [][]int{}
-    for i := 0; i < len(nums); i++ {
-        iTarget := 0-nums[i]
-        m := map[int]bool{}
-        for j := i+1; j < len(nums); j++ {
-            diff := iTarget - nums[j]
-            if m[diff] {
-                tmp := []int{nums[i], nums[j], diff}
-                sort.Ints(tmp)
-                if !set[[3]int{tmp[0], tmp[1], tmp[2]}] {
-                    set[[3]int{tmp[0], tmp[1], tmp[2]}] = true
-                    res = append(res, tmp)
-                }   
-            }
-            m[nums[j]] = true
-        }
-    }
-    return res
+	sort.Ints(nums)
+	out := [][]int{}
+	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {continue}
+		left := i+1
+		right := len(nums)-1
+		target := 0-nums[i]
+		for left < right {
+			total := nums[left] + nums[right]
+			if total == target {
+				out = append(out, []int{nums[i], nums[left], nums[right]})
+				left++
+				for left < right && nums[left] == nums[left-1] {left++}
+				right--
+				for left < right && nums[right] == nums[right+1] {right--}
+			} else if total > target {
+				right--
+			} else {
+				left++
+			}
+		}
+	}
+	return out
 }
 // brute force, 3 nested loops, and check each triplet sum
 // tc = o(n * n * (n * 3log3 + 3) = o(n^3)
