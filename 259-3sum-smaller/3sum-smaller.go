1a0a437e15c710826022b2c708a80ce55fc1cd3e
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-01-17 15:45:29 -0800

    Time: 12 ms (23.81%) | Memory: 4.7 MB (80.95%) - LeetSync

---
+++ 259-3sum-smaller/3sum-smaller.go
@@ -0,0 +1,33 @@
+func threeSumSmaller(nums []int, target int) int {
+    count := 0
+    sort.Ints(nums)
+    for i := 0; i < len(nums); i++ {
+        left := i+1
+        right := len(nums)-1
+        for left < right {
+            total := nums[i] + nums[left] + nums[right]
+            if total < target {
+                count += (right-left)
+                left++
+            } else {
+                right--
+            }
+        }
+    }
+    return count
+}
+
+// brute force
+// func threeSumSmaller(nums []int, target int) int {
+//     count := 0
+//    for i := 0; i < len(nums); i++ {
+//         for j := i+1; j < len(nums); j++ {
+//             for k := j+1; k < len(nums); k++ {
+//                 if nums[i] + nums[j] + nums[k] < target {
+//                     count++
+//                 }
+//             }
+//         }
+//     }
+//     return count
+// }
