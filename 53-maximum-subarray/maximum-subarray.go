f6e7dac56a24006b8ceda3771568030f15eef5f5
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-01-17 16:45:01 -0800

    Time: 0 ms (100.00%) | Memory: 9.8 MB (81.41%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.go
+++ 53-maximum-subarray/maximum-subarray.go
@@ -1,13 +1,15 @@
+// kadanes greedy algo, try to evaluate each idx as a new starting point
+// start a new eval if rSum < 0 
+// same logic can be applied to gas station problem too ( eval whether a gas station works, drop and start new if it does not work in the middle .. )
 func maxSubArray(nums []int) int {
-    n := len(nums)
-    rSum := 0
-    res := math.MinInt64
-    for i := 0; i < n; i++ {
-        rSum += nums[i]
-        res = max(res, rSum)
-        if rSum < 0 {rSum = 0}
-    }
-    return res
+	rSum := 0 
+	res := math.MinInt64
+	for i := 0; i < len(nums); i++ {
+		rSum += nums[i]
+		res = max(res, rSum)
+		if rSum < 0 {rSum = 0}
+	}
+	return res
 }
 // brute force, form all possible subarrays
 // tc = n^2
