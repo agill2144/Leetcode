de94db680684f5a58d4197c64aaf2e9d52a8f31e
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-01-17 16:25:29 -0800

    Time: 2 ms (91.04%) | Memory: 4.5 MB (100.00%) - LeetSync

--- 18-4sum/4sum.go
+++ 18-4sum/4sum.go
@@ -3,21 +3,19 @@ func fourSum(nums []int, target int) [][]int {
     out := [][]int{}
     for i := 0; i < len(nums); i++ {
         if i > 0 && nums[i] == nums[i-1] {continue}
-        iTarget := target-nums[i]
         for j := i+1; j < len(nums); j++ {
             if j != i+1 && nums[j] == nums[j-1] {continue}
-            jTarget := iTarget-nums[j]
             left := j+1
             right := len(nums)-1
             for left < right {
-                total := nums[left] + nums[right]
-                if total == jTarget {
+                total := nums[left] + nums[right] + nums[i] + nums[j]
+                if total == target {
                     out = append(out, []int{nums[i], nums[j], nums[left], nums[right]})
                     left++
                     for left < right && nums[left] == nums[left-1] {left++}
                     right--
                     for left < right && nums[right] == nums[right+1] {right--}
-                } else if total > jTarget {
+                } else if total > target {
                     right--
                 } else {
                     left++
