0ac70e6154b8c63ab789a29e25b1e1f321edb1c3
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-01-17 16:28:26 -0800

    Time: 0 ms (100.00%) | Memory: 4 MB (55.20%) - LeetSync

--- 75-sort-colors/sort-colors.go
+++ 75-sort-colors/sort-colors.go
@@ -1,15 +1,23 @@
-// 2 ptrs on each end collecting their values
 func sortColors(nums []int)  {
-	z,o,t := 0,0,0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == 0 {z++}
-		if nums[i] == 1 {o++}
-		if nums[i] == 2 {t++}
-	}
-	for i := 0; i < len(nums); i++ {
-		if z > 0 {nums[i] = 0; z--; continue}
-		if o > 0 {nums[i] = 1; o--; continue}
-		if t > 0 {nums[i] = 2; t--}
-	}
-
+    sort.Ints(nums)
 }
+
+// because there are only 3 values possible, we can use counting sort
+// where we count the occurence of each val in its own counter var
+// then write it back in the asc order
+// tc = o(2n) = o(n)
+// sc = o(1)
+// func sortColors(nums []int)  {
+// 	z,o,t := 0,0,0
+// 	for i := 0; i < len(nums); i++ {
+// 		if nums[i] == 0 {z++}
+// 		if nums[i] == 1 {o++}
+// 		if nums[i] == 2 {t++}
+// 	}
+// 	for i := 0; i < len(nums); i++ {
+// 		if z > 0 {nums[i] = 0; z--; continue}
+// 		if o > 0 {nums[i] = 1; o--; continue}
+// 		if t > 0 {nums[i] = 2; t--}
+// 	}
+
+// }
