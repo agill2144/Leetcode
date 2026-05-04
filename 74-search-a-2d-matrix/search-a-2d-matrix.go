424f1264c314a6eb4c0ed1f26e250ba5cc4d87b5
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-05-04 16:07:33 -0700

    Time: 0 ms (100.00%) | Memory: 4.4 MB (29.09%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.go
+++ 74-search-a-2d-matrix/search-a-2d-matrix.go
@@ -1,17 +1,11 @@
+// ha fuck you
 func searchMatrix(matrix [][]int, target int) bool {
+    if matrix == nil || len(matrix) == 0 {return false}
     m := len(matrix)
     n := len(matrix[0])
-    left := 0
-    right := (m*n)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        r := mid / n
-        c := mid % n
-        if matrix[r][c] == target {return true}
-        if target > matrix[r][c] {
-            left = mid+1
-        } else {
-            right = mid-1
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if matrix[i][j] == target {return true}
         }
     }
     return false
