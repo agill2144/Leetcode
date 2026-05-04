6ba7271a133810ad86204dc03894298d2166b5aa
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-05-04 15:52:08 -0700

    Time: 0 ms (100.00%) | Memory: 4.4 MB (20.88%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
@@ -1,19 +1,18 @@
 func search(nums []int, target int) int {
-    n := len(nums)
     left := 0
-    right := n-1
+    right := len(nums)-1
     for left <= right {
         mid := left + (right-left)/2
-        if nums[mid] == target {
-            return mid
-        }
+        if nums[mid] == target {return mid}
         if nums[left] <= nums[mid] {
+            // left sorted
             if target >= nums[left] && target <= nums[mid] {
                 right = mid-1
             } else {
                 left = mid+1
             }
         } else {
+            // right sorted
             if target >= nums[mid] && target <= nums[right] {
                 left = mid+1
             } else {
