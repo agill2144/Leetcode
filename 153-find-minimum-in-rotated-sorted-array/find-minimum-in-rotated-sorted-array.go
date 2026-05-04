31511be9a012d32e68cfa69060826d9f19899c86
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-05-04 16:29:48 -0700

    Time: 0 ms (100.00%) | Memory: 4.2 MB (84.12%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -1,49 +1,24 @@
-/*
-    approach: binary search
-    - mid is min if its value is smaller than its adj neighbors
-    - min is always on the unsorted half
-    time = o(logn)
-    space = o(1)
-*/
 func findMin(nums []int) int {
-   n := len(nums)
+    n := len(nums)
     left := 0
     right := n-1
     for left <= right {
         mid := left + (right-left)/2
-        // if we are looking at a sorted window
-        // exit early because min will always be nums[left]
-        if nums[mid] < nums[right] && nums[left] < nums[mid] {return nums[left]}
+        // we are looking at a sorted win, min is always nums[left]
+        if nums[left] < nums[mid] && nums[mid] < nums[right] {return nums[left]}
 
-        // mid is min if mid < mid-1 and mid < mid+1
+        // can mid be the min? check n exit early if it is
         if (mid == 0 || nums[mid] < nums[mid-1]) && (mid == n-1 || nums[mid] < nums[mid+1]) {
             return nums[mid]
         }
-         
 
+        // otherwise min is always on the unsorted half compared to mid
         if nums[left] <= nums[mid] {
-            // left sorted, take search on right side
+            // left sorted
             left = mid+1
         } else {
-            // right side is sorted, min is always on unsorted half
-            // therefore take search on left side
             right = mid-1
         }
     }
     return -1
 }
-/*
-    approach: brute force
-    - linear scan
-    - until we find the first desc order
-    - if in linear scan, we didnt find a desc order
-    - then the whole array is sorted, therefore value at idx 0 is min
-    time = o(n)
-    space = o(1)
-*/
-// func findMin(nums []int) int {
-//     for i := 1; i < len(nums); i++ {
-//         if nums[i] < nums[i-1] {return nums[i]}
-//     }
-//     return nums[0]
-// }
