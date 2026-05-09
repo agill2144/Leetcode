050d06627100fb860ae078f8fc5a2582985a8d29
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-05-09 16:15:52 -0700

    Time: 39 ms (5.93%) | Memory: 9.6 MB (61.20%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,18 +1,45 @@
-// n = num of words in strings
-// k = max len of a word in strs
-// tc = o(n * (k + klogk + k))
-// sc = o(n)
 func groupAnagrams(strs []string) [][]string {
-    sortedMap := map[string][]string{}
+    m := map[string][]string{}
     for i := 0; i < len(strs); i++ {
-        chars := strings.Split(strs[i],"")
-        sort.Strings(chars)
-        sorted := strings.Join(chars, "")
-        sortedMap[sorted] = append(sortedMap[sorted], strs[i])
+        hashKey := hash(strs[i])
+        m[hashKey] = append(m[hashKey], strs[i])
     }
     out := [][]string{}
-    for _, v := range sortedMap {
+    for _, v := range m {
         out = append(out, v)
     }
     return out
 }
+func hash(word string) string {
+    freq := make([]int, 26)
+    for i := 0; i < len(word); i++ {
+        idx := int(word[i]-'a')
+        freq[idx]++
+    }
+    out := new(strings.Builder)
+    for i := 0; i < len(freq); i++ {
+        out.WriteString(fmt.Sprintf("%v",freq[i]))
+        if i != len(freq)-1 {
+            out.WriteString("-")
+        }
+    }
+    return out.String()
+}
+// n = num of words in strings
+// k = max len of a word in strs
+// tc = o(n * (k + klogk + k))
+// sc = o(n)
+// func groupAnagrams(strs []string) [][]string {
+//     sortedMap := map[string][]string{}
+//     for i := 0; i < len(strs); i++ {
+//         chars := strings.Split(strs[i],"")
+//         sort.Strings(chars)
+//         sorted := strings.Join(chars, "")
+//         sortedMap[sorted] = append(sortedMap[sorted], strs[i])
+//     }
+//     out := [][]string{}
+//     for _, v := range sortedMap {
+//         out = append(out, v)
+//     }
+//     return out
+// }
