103d5a5da4c6740c110a656d3dca3a0bcaa30b13
agill2144 <120353980+agill2144@users.noreply.github.com>
2026-05-09 16:32:24 -0700

    Time: 9 ms (62.85%) | Memory: 9.4 MB (72.73%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,7 +1,3 @@
-// n = num of words in strings
-// k = max len of a word in strs
-// tc = o(n * k)
-// sc = o(n)
 func groupAnagrams(strs []string) [][]string {
     m := map[string][]string{}
     for i := 0; i < len(strs); i++ {
@@ -14,24 +10,54 @@ func groupAnagrams(strs []string) [][]string {
     }
     return out
 }
-// k = max len of word
-// tc = o(k) + o(26) = o(k)
-// sc = o(26) + o(k) = o(k)
 func hash(word string) string {
-    freq := make([]int, 26)
+    primes := []int64{
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+        31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+        73, 79, 83, 89, 97, 101,
+    }
+    prod := big.NewInt(1)
     for i := 0; i < len(word); i++ {
-        idx := int(word[i]-'a')
-        freq[idx]++
+        idx := word[i] - 'a'
+        prod.Mul(prod, big.NewInt(primes[idx]))
     }
-    out := new(strings.Builder)
-    for i := 0; i < len(freq); i++ {
-        out.WriteString(fmt.Sprintf("%v",freq[i]))
-        if i != len(freq)-1 {
-            out.WriteString("-")
-        }
-    }
-    return out.String()
+    return prod.String() // exact, no precision loss
 }
+
+// n = num of words in strings
+// k = max len of a word in strs
+// tc = o(n * k)
+// sc = o(n)
+// func groupAnagrams(strs []string) [][]string {
+//     m := map[string][]string{}
+//     for i := 0; i < len(strs); i++ {
+//         hashKey := hash(strs[i])
+//         m[hashKey] = append(m[hashKey], strs[i])
+//     }
+//     out := [][]string{}
+//     for _, v := range m {
+//         out = append(out, v)
+//     }
+//     return out
+// }
+// // k = max len of word
+// // tc = o(k) + o(26) = o(k)
+// // sc = o(26) + o(k) = o(k)
+// func hash(word string) string {
+//     freq := make([]int, 26)
+//     for i := 0; i < len(word); i++ {
+//         idx := int(word[i]-'a')
+//         freq[idx]++
+//     }
+//     out := new(strings.Builder)
+//     for i := 0; i < len(freq); i++ {
+//         out.WriteString(fmt.Sprintf("%v",freq[i]))
+//         if i != len(freq)-1 {
+//             out.WriteString("-")
+//         }
+//     }
+//     return out.String()
+// }
 // n = num of words in strings
 // k = max len of a word in strs
 // tc = o(n * (k + klogk + k))
